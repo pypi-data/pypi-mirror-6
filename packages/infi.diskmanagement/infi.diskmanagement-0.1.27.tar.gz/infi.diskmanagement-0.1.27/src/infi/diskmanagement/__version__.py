d843521844421a5f16533239b72c27d4d4e2c352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package infi.diskmanagement/0.1.27/infi.diskmanagement-0.1.27.tar.gz

---
+++ packages/infi.diskmanagement/infi.diskmanagement-0.1.27.tar.gz/infi.diskmanagement-0.1.27/src/infi/diskmanagement/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.27"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.27)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '7cbb086b6e216fce73bb255f99bb9f89df7df9c5'
+__git_head_subject__ = 'Finished release v0.1.27.'
+__git_head_message__ = '* release/v0.1.27:\n  buildout.cfg: adding infi.wioctl>=0.1.3 to requirements\n  buildout.cfg: remove infi.wioctl from requirements\n  STORAGEMODEL-238 Partition.resize\n  STORAGEMODEL-238 Partition.resize\n  STORAGEMODEL-237 adding support for volume labels'
+__git_dirty_diff__ = ''
