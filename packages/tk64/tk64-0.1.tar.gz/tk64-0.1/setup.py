70d8d0ba666ea1b2ab12bd43a041c56be11890e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package tk64/0.1/tk64-0.1.tar.gz

---
+++ packages/tk64/tk64-0.1.tar.gz/tk64-0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tk64',
+      version='0.1',
+      description='Token encoding and decoding for 64 bit integers',
+      author='Jeremiah Campbell',
+      author_email='jeremy@meantheory.com',
+      url='https://github.com/meantheory/tk64',
+      download_url='https://github.com/meantheory/tk64/tarball/0.1',
+      license='MIT',
+      py_modules=['tk64',],
+      classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta'
+    ],
+)
