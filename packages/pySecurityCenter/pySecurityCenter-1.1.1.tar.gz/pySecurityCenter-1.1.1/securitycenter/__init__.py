325ad3f2dceed5583f6fd677511d8d07922ce054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:13 +0000

    Add package pySecurityCenter/1.1.1/pySecurityCenter-1.1.1.tar.gz

---
+++ packages/pySecurityCenter/pySecurityCenter-1.1.1.tar.gz/pySecurityCenter-1.1.1/securitycenter/__init__.py
@@ -0,0 +1,5 @@
+from v1 import SecurityCenter
+
+__version__ = '1.1'
+__authors__ = 'Steven McGrath <steve@chigeek.com>, David Lord <davidism@gmail.com>'
+__website__ = 'http://github.com/SteveMcGrath/pySecurityCenter'
