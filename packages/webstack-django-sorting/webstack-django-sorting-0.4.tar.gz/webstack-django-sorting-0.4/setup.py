14f238da44b5341ad5513b4b1b93453899000e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:02 +0000

    Add package webstack-django-sorting/0.4/webstack-django-sorting-0.4.tar.gz

---
+++ packages/webstack-django-sorting/webstack-django-sorting-0.4.tar.gz/webstack-django-sorting-0.4/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__version__ = '0.4'
+
+setup(
+    name='webstack-django-sorting',
+    version=__version__,
+    description="Easy sorting of tables with Django",
+    long_description=open('README.rst').read(),
+    author='Stéphane Raimbault',
+    author_email='stephane.raimbault@webstack.fr',
+    url='http://github.com/stephane/django-sorting/',
+    packages=[
+        'webstack_django_sorting',
+        'webstack_django_sorting.templatetags',
+    ],
+    package_dir={'webstack_django_sorting': 'webstack_django_sorting'},
+    include_package_data=True,
+    zip_safe=False,
+    keywords='sorting,pagination,django',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
