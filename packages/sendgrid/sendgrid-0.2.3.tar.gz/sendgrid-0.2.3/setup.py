9e6ee1533305c9af6c2b3ec352f24a32b6cf1617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:00 +0000

    Add package sendgrid/0.2.3/sendgrid-0.2.3.tar.gz

---
+++ packages/sendgrid/sendgrid-0.2.3.tar.gz/sendgrid-0.2.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sendgrid',
+    version='0.2.3',
+    author='Yamil Asusta',
+    author_email='yamil@sendgrid.com',
+    url='https://github.com/sendgrid/sendgrid-python/',
+    packages=find_packages(),
+    license='MIT',
+    description='SendGrid library for Python',
+    long_description=open('./README.rst').read(),
+    install_requires=[
+        'requests',
+        'smtpapi'
+    ],
+)
