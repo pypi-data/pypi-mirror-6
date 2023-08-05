9bdef80b65bf5c34ca9c8569435ed58b5713f93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package rabbitpy/0.11.0/rabbitpy-0.11.0.tar.gz

---
+++ packages/rabbitpy/rabbitpy-0.11.0.tar.gz/rabbitpy-0.11.0/README.rst
@@ -0,0 +1,130 @@
+rabbitpy - rabbitmq simplified
+==============================
+
+A pure python, thread-safe, minimalistic and pythonic BSD Licensed
+AMQP/RabbitMQ library that supports Python 2.6+ and Python 3.2+.
+Previously named ``rmqid``.
+
+|PyPI version| |Downloads| |Build Status|
+
+Installation
+------------
+
+rabbitpy may be installed via the Python package index with the tool of
+your choice. I prefer pip:
+
+::
+
+    pip install rabbitpy
+
+But there's always easy_install:
+
+::
+
+    easy_install rabbitpy
+
+rmqid Compatibility
+-------------------
+
+rabbitpy is API compatible with rmqid.
+
+Documentation
+-------------
+
+https://rabbitpy.readthedocs.org
+
+Requirements
+------------
+
+-  pamqp - https://github.com/pika/pamqp
+-  requests - http://docs.python-requests.org/
+
+Python3 Caveats
+---------------
+
+-  Message bodies must use the bytes data type while most other values
+   are strings.
+
+Examples
+========
+
+Simple Publisher
+----------------
+
+The simple publisher is ideal for sending one off messages:
+
+::
+
+    >>> rabbitpy.publish('amqp://guest:guest@localhost:5672/%2f',
+                         exchange='test',
+                         routing_key='example',
+                         body='This is my test message')
+
+If you want to add properties:
+
+::
+
+    >>> rabbitpy.publish('amqp://guest:guest@localhost:5672/%2f',
+                         exchange='test',
+                         routing_key='example',
+                         body='This is my test message',
+                         properties={'content_type': 'text/plain'})
+
+And publisher confirms:
+
+::
+
+    >>> rabbitpy.publish('amqp://guest:guest@localhost:5672/%2f',
+                         exchange='test',
+                         routing_key='example',
+                         body='This is my test message',
+                         properties={'content_type': 'text/plain'},
+                         confirm=True)
+    True
+    >>>
+
+Simple Getter
+-------------
+
+::
+
+    >>> m = rabbitpy.get('amqp://guest:guest@localhost:5672/%2f', 'test')
+    >>> m.json()
+    {u'foo': u'bar'}
+
+Simple Consumer
+---------------
+
+::
+
+    >>> with rabbitpy.consume('amqp://guest:guest@localhost:5672/%2f', 'test') as c:
+    ..    for message in c.next_message():
+    ...         print message.properties['message_id']
+    ...         print message.body
+    ...         message.ack()
+    ...
+    856dfdc7-5ee3-4fc1-9635-977bf0043a9f
+    {"foo": "bar"}
+
+More complex examples are available at https://rabbitpy.readthedocs.org
+
+Version History
+---------------
+
+- 0.11.0: Major bugfix focused on receiving multiple AMQP frames at the same time. Add auto-coersion of property data-types.
+- 0.10.0: Rewrite of IO layer yielding improved performance and reduction of CPU usage, bugfixes
+- 0.9.0: Major performance improvements, CPU usage reduction, minor bug-fixes
+- 0.8.0: Major bugfixes, IPv6 support
+- 0.7.0: Bugfixes and code cleanup. Most notable fix around Basic.Return and recursion in Channel._wait_on_frame.
+- 0.6.0: Bugfix with Queue.get(), RPC requests expecting multiple responses and the new Queue.consume_messages() method.
+- 0.5.1: Installer/setup fix
+- 0.5.0: Bugfix release including low level socket sending fix and connection timeouts.
+
+
+
+.. |PyPI version| image:: https://badge.fury.io/py/rabbitpy.png
+   :target: http://badge.fury.io/py/rabbitpy
+.. |Downloads| image:: https://pypip.in/d/rabbitpy/badge.png
+   :target: https://crate.io/packages/rabbitpy
+.. |Build Status| image:: https://travis-ci.org/gmr/rabbitpy.png?branch=master
+   :target: https://travis-ci.org/gmr/rabbitpy
---
+++ packages/rabbitpy/rabbitpy-0.11.0.tar.gz/rabbitpy-0.11.0/rabbitpy/simple.py
@@ -0,0 +1,209 @@
+"""
+Wrapper methods for easy access to common operations, making them both less
+complex and less verbose for one off or simple use cases.
+
+"""
+import contextlib
+
+from rabbitpy import connection
+from rabbitpy import exchange
+from rabbitpy import exceptions
+from rabbitpy import amqp_queue
+from rabbitpy import message
+
+
+@contextlib.contextmanager
+def consume(uri=None, queue_name=None):
+    """Create a queue consumer, returning a :py:class:`rabbitpy.queue.Consumer`
+    generator class that you can retrieve messages from using
+    :py:class:`rabbitpy.queue.Consumer.next_message`
+
+    Invoke directly as rabbitpy.consumer()
+
+    :rtype: :py:class:`rabbitpy.queue.Consumer`
+    :raises: :py:class:`rabbitpy.exceptions.EmptyQueueNameError`
+
+    """
+    if not queue_name:
+        raise exceptions.EmptyQueueNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            q = amqp_queue.Queue(channel, queue_name)
+            with q.consumer() as consumer:
+                yield consumer
+
+
+def get(uri=None, queue_name=None):
+    """Get a message from RabbitMQ, auto-acknowledging with RabbitMQ if one
+    is returned.
+
+    Invoke directly as rabbitpy.get()
+
+
+    :param str uri: AMQP URI to connect to
+    :param str queue_name: The queue name to get the message from
+    :rtype: py:class:`rabbitpy.message.Message` or None
+    :raises: :py:class:`rabbitpy.exceptions.EmptyQueueNameError`
+
+    """
+    if not queue_name:
+        raise exceptions.EmptyQueueNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            q = amqp_queue.Queue(channel, queue_name)
+            return q.get(False)
+
+
+def publish(uri=None, exchange=None, routing_key=None,
+            body=None, properties=None, confirm=False):
+    """Publish a message to RabbitMQ. This should only be used for one-off
+    publishing, as you will suffer a performance penalty if you use it
+    repeatedly instead creating a connection and channel and publishing on that
+
+    :param str uri: AMQP URI to connect to
+    :param str exchange: The exchange to publish to
+    :param str routing_key: The routing_key to publish with
+    :param str or unicode or bytes or dict or list: The message body
+    :param dict properties: Dict representation of Basic.Properties
+    :param bool confirm: Confirm this delivery with Publisher Confirms
+    :rtype: bool or None
+    :raises: :py:class:`rabbitpy.exceptions.EmptyExchangeNameError`
+
+    """
+    if not exchange:
+        raise exceptions.EmptyExchangeNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            msg = message.Message(channel, body or '', properties or dict())
+            if confirm:
+                channel.enable_publisher_confirms()
+                return msg.publish(exchange, routing_key or '')
+            else:
+                msg.publish(exchange, routing_key or '')
+
+
+def create_queue(uri=None, queue_name=None, durable=True, auto_delete=False):
+    """Create a queue with RabbitMQ. This should only be used for one-off
+    operations.
+
+    :param str uri: AMQP URI to connect to
+    :param str queue_name: The queue name to create
+    :param durable: Indicates if the queue should survive a RabbitMQ is restart
+    :type durable: bool
+    :param bool auto_delete: Automatically delete when all consumers disconnect
+    :raises: :py:class:`rabbitpy.exceptions.EmptyQueueNameError`
+    :raises: :py:class:`rabbitpy.RemoteClosedException`
+
+    """
+    if not queue_name:
+        raise exceptions.EmptyQueueNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            obj = amqp_queue.Queue(channel, queue_name, durable=durable,
+                                   auto_delete=auto_delete)
+            obj.declare()
+
+
+def delete_queue(uri=None, queue_name=None):
+    """Delete a queue from RabbitMQ. This should only be used for one-off
+    operations.
+
+    :param str uri: AMQP URI to connect to
+    :param str queue_name: The queue name to delete
+    :rtype: bool
+    :raises: :py:class:`rabbitpy.exceptions.EmptyQueueNameError`
+    :raises: :py:class:`rabbitpy.RemoteClosedException`
+
+    """
+    if not queue_name:
+        raise exceptions.EmptyQueueNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            queue = amqp_queue.Queue(channel, queue_name)
+            queue.delete()
+
+
+def create_direct_exchange(uri=None, exchange_name=None, durable=True):
+    """Create a direct exchange with RabbitMQ. This should only be used for
+    one-off operations.
+
+    :param str uri: AMQP URI to connect to
+    :param str exchange_name: The exchange name to create
+    :param bool durable: Exchange should survive server restarts
+    :raises: :py:class:`rabbitpy.exceptions.EmptyExchangeNameError`
+    :raises: :py:class:`rabbitpy.RemoteClosedException`
+
+    """
+    if not exchange_name:
+        raise exceptions.EmptyExchangeNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            obj = exchange.DirectExchange(channel, exchange_name,
+                                          durable=durable)
+            obj.declare()
+
+def create_fanout_exchange(uri=None, exchange_name=None, durable=True):
+    """Create a fanout exchange with RabbitMQ. This should only be used for
+    one-off operations.
+
+    :param str uri: AMQP URI to connect to
+    :param str exchange_name: The exchange name to create
+    :param bool durable: Exchange should survive server restarts
+    :raises: :py:class:`rabbitpy.exceptions.EmptyExchangeNameError`
+    :raises: :py:class:`rabbitpy.RemoteClosedException`
+
+    """
+    if not exchange_name:
+        raise exceptions.EmptyExchangeNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            obj = exchange.FanoutExchange(channel, exchange_name,
+                                          durable=durable)
+            obj.declare()
+
+
+def create_topic_exchange(uri=None, exchange_name=None, durable=True):
+    """Create an exchange from RabbitMQ. This should only be used for one-off
+    operations.
+
+    :param str uri: AMQP URI to connect to
+    :param str exchange_name: The exchange name to create
+    :param bool durable: Exchange should survive server restarts
+    :raises: :py:class:`rabbitpy.exceptions.EmptyExchangeNameError`
+    :raises: :py:class:`rabbitpy.RemoteClosedException`
+
+    """
+    if not exchange_name:
+        raise exceptions.EmptyExchangeNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            obj = exchange.TopicExchange(channel, exchange_name,
+                                         durable=durable)
+            obj.declare()
+
+
+def delete_exchange(uri=None, exchange_name=None):
+    """Delete an exchange from RabbitMQ. This should only be used for one-off
+    operations.
+
+    :param str uri: AMQP URI to connect to
+    :param str exchange_name: The exchange name to delete
+    :raises: :py:class:`rabbitpy.exceptions.EmptyExchangeNameError`
+    :raises: :py:class:`rabbitpy.RemoteClosedException`
+
+    """
+    if not exchange_name:
+        raise exceptions.EmptyExchangeNameError()
+
+    with connection.Connection(uri) as conn:
+        with conn.channel() as channel:
+            obj = exchange.Exchange(channel, exchange_name)
+            obj.delete()
