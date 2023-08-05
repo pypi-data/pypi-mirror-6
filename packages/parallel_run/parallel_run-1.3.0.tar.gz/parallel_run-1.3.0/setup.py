3448bf456c01e321c5c0e538bacba81f24589cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package parallel_run/1.3.0/parallel_run-1.3.0.tar.gz

---
+++ packages/parallel_run/parallel_run-1.3.0.tar.gz/parallel_run-1.3.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='parallel_run',
+    version='1.3.0',
+    packages=['parallel_run'],
+    author='zhoujingjiang',
+    author_email='jingjiang@staff.sina.com.cn',
+    license='LGPL',
+    install_requires=["chardet"],
+    description="a parallel package mainly using multiprocessing",
+    url='http://weibo.com/zhoujingjiang/'
+)
