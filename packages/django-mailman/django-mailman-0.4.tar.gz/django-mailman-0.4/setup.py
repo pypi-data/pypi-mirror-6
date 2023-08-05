b1dfea669362f17f8b4fe78d97fe932ef4de36f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:44 +0000

    Add package django-mailman/0.4/django-mailman-0.4.tar.gz

---
+++ packages/django-mailman/django-mailman-0.4.tar.gz/django-mailman-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name = 'django-mailman',
+    version = '0.4',
+    packages = ['django_mailman',],
+    platforms = ['any'],
+    license = 'GNU LGPL v2.1',
+    author = "Bernd Schlapsi",
+    author_email = 'albert@albertoconnor.ca',
+    description = 'Interface to Mailman Web-API',
+    long_description = open('README.rst').read(),
+    url = 'https://bitbucket.org/albertoconnor/django-mailman',
+)
