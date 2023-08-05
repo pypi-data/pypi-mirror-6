f1b746dad8081cae5755fe5cae5ee98c944df2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:45 +0000

    Add package bitcoin-python3/0.3.1/bitcoin-python3-0.3.1.tar.gz

---
+++ packages/bitcoin-python3/bitcoin-python3-0.3.1.tar.gz/bitcoin-python3-0.3.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name='bitcoin-python3',
+    version='0.3.1',
+    description='Friendly Bitcoin JSON-RPC API binding for Python 3',
+    long_description='This package allows performing commands such as listing the current balance'
+    ' and sending coins to the Satoshi (original) client from Python 3. The communication with the'
+    ' client happens over JSON-RPC.',
+    maintainer='Ashot Seropian',
+    maintainer_email='ondaemon@gmail.com',
+    url='http://laanwj.github.com/bitcoin-python/doc/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.3',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Office/Business :: Financial'
+    ],
+    packages=find_packages("src"),
+    package_dir={'': 'src'}
+)
