e30797f251694fe7f3a27ab4940a83044c322864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package seantis.dir.eventsportlet/1.0rc1/seantis.dir.eventsportlet-1.0rc1.tar.gz

---
+++ packages/seantis.dir.eventsportlet/seantis.dir.eventsportlet-1.0rc1.tar.gz/seantis.dir.eventsportlet-1.0rc1/seantis/dir/eventsportlet/upgrades.py
@@ -0,0 +1,8 @@
+# from Products.CMFCore.utils import getToolByName
+
+
+def upgrade_1000_to_1001(context):
+    # update css and js
+    # getToolByName(context, 'portal_css').cookResources()
+    # getToolByName(context, 'portal_javascripts').cookResources()
+    pass
