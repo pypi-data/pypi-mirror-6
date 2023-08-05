d5781abba988fa82c592fd014483fb346497f308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:32 +0000

    Add package django-oscar/0.5.2/django-oscar-0.5.2.tar.gz

---
+++ packages/django-oscar/django-oscar-0.5.2.tar.gz/django-oscar-0.5.2/oscar/apps/customer/auth_backends.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.backends import ModelBackend
+
+
+class Emailbackend(ModelBackend):
+
+    def authenticate(self, email=None, password=None, *args, **kwargs):
+        if email is None:
+            if not 'username' in kwargs or kwargs['username'] is None:
+                return None
+            email = kwargs['username']
+
+        # Check if we're dealing with an email address
+        if '@' not in email:
+            return None
+
+        # We lowercase the host part as this is what Django does when saving a
+        # user
+        local, host = email.split('@')
+        clean_email = local + '@' + host.lower()
+        try:
+            user = User.objects.get(email=clean_email)
+        except User.DoesNotExist:
+            return None
+        if user.check_password(password):
+            return user
