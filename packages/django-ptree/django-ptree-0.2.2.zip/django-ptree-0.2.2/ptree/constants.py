fbf69aa559d759c063046e5c7d352209057d83e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package django-ptree/0.2.2/django-ptree-0.2.2.zip

---
+++ packages/django-ptree/django-ptree-0.2.2.zip/django-ptree-0.2.2/ptree/constants.py
@@ -0,0 +1,41 @@
+ExperimentClass = 'ExperimentClass'
+TreatmentClass = 'TreatmentClass'
+MatchClass = 'MatchClass'
+ParticipantClass = 'ParticipantClass'
+
+
+
+match_id = 'match_id'
+
+participant_code = 'participant_code'
+experiment_code = 'experiment_code'
+experiment_code_obfuscated = 'exp_code'
+treatment_code = 'treatment_code'
+demo_code = 'demo_code'
+
+nickname = 'nickname'
+
+completed_views = 'completed_views'
+
+form_invalid = 'form_invalid'
+precondition = 'precondition'
+mturk_assignment_id = 'mturk_assignment_id'
+mturk_worker_id = 'mturk_worker_id'
+debug_values_built_in = 'debug_values_built_in'
+debug_values = 'debug_values'
+check_if_wait_is_over = 'check_if_wait_is_over'
+get_param_truth_value = '1'
+experimenter_access_code = 'experimenter_access_code'
+index_in_sequence_of_views = 'index_in_sequence_of_views'
+
+time_limit_in_seconds = 'time_limit_in_seconds'
+time_limit_minutes_component = 'time_limit_minutes_component'
+time_limit_seconds_component = 'time_limit_seconds_component'
+page_expiration_times = 'page_time_limits'
+participant_label = 'participant_label'
+session_access_code = 'session_access_code'
+form_element_id = 'form'
+session_participant_id = 'session_participant_id'
+session_participant_code = 'session_participant_code'
+session_id = 'session_id'
+session_code = 'session_code'
