3e27243d3a6d7e1a161b9c6845852f819dfce947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package object_pool/0.2/object_pool-0.2.tar.gz

---
+++ packages/object_pool/object_pool-0.2.tar.gz/object_pool-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+import object_pool
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = None
+
+setup(name='object_pool',
+      version=object_pool.__version__,
+      description='thread-safe python object pool',
+      long_description=long_description,
+      author='Avery Fay',
+      author_email='avery@shadypixel.com',
+      url='https://github.com/btmorex/object_pool',
+      packages=find_packages(),
+      test_suite='object_pool.test',
+      license='MIT')
