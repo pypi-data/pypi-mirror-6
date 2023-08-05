93006ce5a60f2292a52751a1c15de0cadda255b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:28 +0000

    Add package hospital/0.2/hospital-0.2.zip

---
+++ packages/hospital/hospital-0.2.zip/hospital-0.2/hospital/tests/packaging.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Tests around Python packaging."""
+import pkg_resources
+import unittest
+
+import hospital
+from hospital.utils.packaging import get_supported_python_versions
+
+
+class PythonVersionTestCase(unittest.TestCase):
+    """Tests around Python version."""
+    def setUp(self):
+        """Setup working distribution."""
+        self.distribution = pkg_resources.get_distribution('hospital')
+
+    def test_supported_versions_declaration(self):
+        """hospital distribution declares supported Python versions."""
+        versions = get_supported_python_versions(self.distribution)
+        self.assertTrue(versions)
+
+    def test_environment_support(self):
+        """hospital claims support for Python version used to run this test."""
+        hospital.assert_supported_python_version(self.distribution)
---
+++ packages/hospital/hospital-0.2.zip/hospital-0.2/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""Python packaging."""
+import os
+import sys
+
+from setuptools import setup
+
+
+#: Absolute path to directory containing setup.py file.
+here = os.path.abspath(os.path.dirname(__file__))
+#: Boolean, ``True`` if environment is running Python version 2.
+IS_PYTHON2 = sys.version_info[0] == 2
+
+
+NAME = 'hospital'
+DESCRIPTION = 'Healthchecks for Python.'
+README = open(os.path.join(here, 'README.rst')).read()
+VERSION = open(os.path.join(here, 'VERSION')).read().strip()
+AUTHOR = u'Benoît Bryon'
+EMAIL = u'benoit@marmelune.net'
+URL = 'https://{name}.readthedocs.org/'.format(name=NAME)
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'License :: OSI Approved :: BSD License',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3.3']
+KEYWORDS = ['diagnosis',
+            'healthcheck',
+            'monitoring',
+            'smoketest',
+            'supervision',
+            'testing']
+PACKAGES = [NAME.replace('-', '_')]
+REQUIREMENTS = ['setuptools', 'requests']
+if IS_PYTHON2:
+    REQUIREMENTS.extend(['mock'])
+ENTRY_POINTS = {}
+
+
+if __name__ == '__main__':  # Don't run setup() when we import this module.
+    setup(name=NAME,
+          version=VERSION,
+          description=DESCRIPTION,
+          long_description=README,
+          classifiers=CLASSIFIERS,
+          keywords=' '.join(KEYWORDS),
+          author=AUTHOR,
+          author_email=EMAIL,
+          url=URL,
+          license='BSD',
+          packages=PACKAGES,
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=REQUIREMENTS,
+          entry_points=ENTRY_POINTS)
