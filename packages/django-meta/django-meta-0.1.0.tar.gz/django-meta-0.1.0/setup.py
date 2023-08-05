a44aba20ffc5a292f3a5c1d007c89452fa5c39da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:52 +0000

    Add package django-meta/0.1.0/django-meta-0.1.0.tar.gz

---
+++ packages/django-meta/django-meta-0.1.0.tar.gz/django-meta-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='django-meta',
+    description='Pluggable app for handling webpage meta tags and OpenGraph '
+    'properties',
+    long_description=open('README.rst').read(),
+    version='0.1.0',
+    packages=['meta', 'meta.templatetags'],
+    package_data={
+        'meta': ['templates/*.html'],
+    },
+    author='Monwara LLC',
+    author_email='branko@monwara.com',
+    url='https://bitbucket.org/monwara/django-meta',
+    download_url='https://bitbucket.org/monwara/django-meta/downloads',
+    license='BSD',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
+
