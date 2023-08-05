b08936120c094ea537888c76c65db61583b8316b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:14 +0000

    Add package discotech/0.4/discotech-0.4.tar.gz

---
+++ packages/discotech/discotech-0.4.tar.gz/discotech-0.4/README.md
@@ -0,0 +1,30 @@
+discotech
+========
+
+python library to help dealing with everything regarding social media providers
+
+homepage: https://www.discoapi.com/products/discotech.html
+
+you can find full docs here: https://www.discoapi.com/discotech/docs
+
+#requirements:
+- python > 2.6
+- requests 
+- requests-oauth
+
+#installation:
+```bash
+python setup.py install
+```
+
+#unit tests:
+```bash
+python setup.py test
+```
+
+#creating docs:
+```bash
+epydoc -v discotech -o docs
+```
+
+note that you have to have epydoc installed (pip install epydoc)
---
+++ packages/discotech/discotech-0.4.tar.gz/discotech-0.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='discotech',
+      version='0.4',
+      description='python library to help working with social media providers',
+      long_description=readme(),
+      author='discoAPI Team',
+      author_email='groovy@discoapi.com',
+      url='http://github.com/discoapi/discotech',
+      packages=['discotech'],
+      license='GPL',
+      install_requires=[
+          'requests-oauthlib',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      include_package_data=True)
