1ecaa785f7f44fe18e54e00a8f578acfa58083b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package collective.elasticindex/1.2/collective.elasticindex-1.2.tar.gz

---
+++ packages/collective.elasticindex/collective.elasticindex-1.2.tar.gz/collective.elasticindex-1.2/src/collective/elasticindex/subscribers.py
@@ -0,0 +1,43 @@
+
+from collective.elasticindex.changes import changes
+from Products.CMFCore.utils import getToolByName
+
+
+def content_added(content, event):
+    factorytool = getToolByName(content, 'portal_factory')
+    if factorytool in content.aq_chain:
+        return
+    if changes.should_index_content(content):
+        changes.index_content(content)
+
+
+def content_modified(content, event):
+    factorytool = getToolByName(content, 'portal_factory')
+    if factorytool in content.aq_chain:
+        return
+    if changes.should_index_content(content):
+        changes.index_content(content)
+
+
+def content_deleted(content, event):
+    factorytool = getToolByName(content, 'portal_factory')
+    if factorytool in content.aq_chain:
+        return
+    if event.newParent is None:
+        if changes.should_index_content(content):
+            changes.unindex_content(content)
+
+
+def content_published(content, event):
+    factorytool = getToolByName(content, 'portal_factory')
+    if factorytool in content.aq_chain:
+        return
+    if not changes.only_published:
+        return
+    if (event.old_state.getId() == 'published' and
+        event.new_state.getId() != 'published'):
+        changes.unindex_content(content)
+    elif (event.new_state.getId() == 'published' and
+          event.old_state.getId() != 'published'):
+        changes.index_content(content)
+
