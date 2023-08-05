8b706c7d874e9de717c534f10118c3fa3455d9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:15 +0000

    Add package sphinx-argparse/0.1.8/sphinx-argparse-0.1.8.tar.gz

---
+++ packages/sphinx-argparse/sphinx-argparse-0.1.8.tar.gz/sphinx-argparse-0.1.8/setup.py
@@ -0,0 +1,31 @@
+from distutils.version import StrictVersion
+import os
+from setuptools import setup
+# from tests import PyTest
+import sys
+
+deps = ["sphinx"]
+
+# argparse was added to python stdlib since 2.7
+if StrictVersion(sys.version.split(' ')[0]) < StrictVersion('2.7.0'):
+    deps.append('argparse')
+
+setup(
+    name='sphinx-argparse',
+    version='0.1.8',
+    packages=[
+        'sphinxarg',
+    ],
+
+    url='',
+    license='MIT',
+    author='Aleksandr Rudakov',
+    author_email='ribozz@gmail.com',
+    description='Sphinx extension that automatically document argparse commands and options',
+    long_description='',
+    install_requires=deps,
+
+    extras_require={
+        'dev': ['pytest'],
+    }
+)
