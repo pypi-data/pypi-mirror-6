cdb24d8d54bd79f3c338effda8aed09280193fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:12 +0000

    Add package PreProGuardian/0.0.10/PreProGuardian-0.0.10.tar.gz

---
+++ packages/PreProGuardian/PreProGuardian-0.0.10.tar.gz/PreProGuardian-0.0.10/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PreProGuardian',
+    version='0.0.10',
+    packages=['ppguard',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+)
