20d5cc4149f5ef585868c50c4b8c3d29a1075d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:04 +0000

    Add package cloudant/0.5.3/cloudant-0.5.3.tar.gz

---
+++ packages/cloudant/cloudant-0.5.3.tar.gz/cloudant-0.5.3/cloudant/account.py
@@ -0,0 +1,85 @@
+from .resource import Resource
+from .database import Database
+import urlparse
+
+
+class Account(Resource):
+
+    """
+    An account to a Cloudant or CouchDB account.
+
+        account = cloudant.Account()
+        response = account.login(USERNAME, PASSWORD)
+        print response.json()
+        # { "ok": True, ... }
+
+    Like all Cloudant-Python objects, pass `async=True`
+    to make asynchronous requests, like this:
+
+        account = cloudant.Account(async=True)
+        future = account.login(USERNAME, PASSWORD)
+        response = future.result()
+        print response.json()
+        # { "ok": True, ... }
+    """
+
+    def __init__(self, uri="http://localhost:5984", **kwargs):
+        if not urlparse.urlparse(uri).scheme:
+            uri = "https://%s.cloudant.com" % uri
+        super(Account, self).__init__(uri, **kwargs)
+
+    def database(self, name, **kwargs):
+        """Create a `Database` object prefixed with this account's URL."""
+        opts = dict(self.opts.items() + kwargs.items())
+        return Database(self._make_url(name), session=self._session, **opts)
+
+    def __getitem__(self, name):
+        """Shortcut to `Account.database`."""
+        return self.database(name, **self.opts)
+
+    def __delitem__(self, name):
+        """
+        Delete a database named `name`.
+        Blocks until the response returns,
+        and raises an error if the deletion failed.
+        """
+        response = self.database(name, **self.opts).delete()
+        # block until result if the object is using async
+        if hasattr(response, 'result'):
+            response = response.result()
+        response.raise_for_status()
+
+    def all_dbs(self, **kwargs):
+        """List all databases."""
+        return self.get('_all_dbs', **kwargs)
+
+    def active_tasks(self, **kwargs):
+        """List replication, compaction, and indexer tasks currently running."""
+        return self.get('_active_tasks', **kwargs)
+
+    def replicate(self, source, target, opts={}, **kwargs):
+        """
+        Begin a replication job.
+        `opts` contains replication options such as whether the replication
+        should create the target (`create_target`) or whether the replication
+        is continuous (`continuous`).
+
+        Note: unless continuous, will not return until the job is finished.
+        """
+
+        params = {
+            'source': source,
+            'target': target
+        }
+
+        params.update(opts)
+        if 'params' in kwargs:
+            params.update(kwargs['params'])
+            del kwargs['params']
+
+        return self.post('_replicate', params=params, **kwargs)
+
+    def uuids(self, count=1, **kwargs):
+        """Generate an arbitrary number of UUIDs."""
+        params = dict(count=count)
+        return self.get('_uuids', params=params, **kwargs)
---
+++ packages/cloudant/cloudant-0.5.3.tar.gz/cloudant-0.5.3/cloudant/index.py
@@ -0,0 +1,39 @@
+from .resource import Resource
+import json
+
+
+class Index(Resource):
+
+    """
+    Methods for design document indexes.
+    Different kinds of indexes will behave differently, so here are helpful docs:
+
+    * [Lucene search indexes](http://docs.cloudant.com/api/search.html#searching-for-documents-using-lucene-queries)
+    * [Secondary indexes / "views"](http://docs.cloudant.com/api/design-documents-querying-views.html#querying-a-view)
+
+    Then, you just use basic HTTP methods to perform queries, like this:
+
+        index.get(params=QUERY_ARGUMENTS)
+
+    Remember, before you can query an index, you have to make sure it's in your database.
+    See [these docs](http://docs.cloudant.com/api/design-documents-get-put-delete-copy.html#creating-or-updating-a-design-document)
+    for how to do that.
+    """
+
+    def __iter__(self, **kwargs):
+        response = self.get(stream=True, **kwargs)
+        # block until result if the object is using async
+        if hasattr(response, 'result'):
+            response = response.result()
+        for line in response.iter_lines():
+            if line:
+                if line[-1] == ',':
+                    line = line[:-1]
+                try:
+                    yield json.loads(line)
+                except ValueError:
+                    # if we can't decode a line, ignore it
+                    pass
+
+    def iter(self, **kwargs):
+        return self.__iter__(**kwargs)
