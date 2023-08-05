5f6718156fd3a3cd0ffbc84a6fed7dd84cd1fc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:32 +0000

    Add package smtpfixture/0.1/smtpfixture-0.1.tar.gz

---
+++ packages/smtpfixture/smtpfixture-0.1.tar.gz/smtpfixture-0.1/smtpfixture/__init__.py
@@ -0,0 +1,13 @@
+import os.path
+
+__version__ = '0.1'
+
+
+def installdir():
+    """
+
+    Script ``smtpfixture-installdir`` to print the installation dir to help
+    to start the `twistd`` daemon using an extra plugin.
+
+    """
+    print(os.path.dirname(__file__))
