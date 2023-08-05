599d4b1722c0baffd63d3bca0859c9af77da5945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:16 +0000

    Add package k-means-plus-plus/0.0.5/k-means-plus-plus-0.0.5.tar.gz

---
+++ packages/k-means-plus-plus/k-means-plus-plus-0.0.5.tar.gz/k-means-plus-plus-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+import k_means_plus_plus as kmpp
+
+requirements = []
+
+with open("requirements.txt") as f:
+    for line in f:
+        line = line.strip()
+        if line:
+            requirements.append(line)
+
+setup(
+    name="k-means-plus-plus",
+    version=kmpp.__version__,
+    author="Jack Maney",
+    author_email="jackmaney@gmail.com",
+    description="K-Means++ Clustering for Pandas DataFrames",
+    long_description=open("README.md").read(),
+    license="MIT",
+    url="https://github.com/jackmaney/k-means-plus-plus-pandas",
+    download_url="https://github.com/jackmaney/k-means-plus-plus-pandas/archive/v" +
+        kmpp.__version__ + ".tar.gz",
+    packages=["clustering"],
+    install_requires=requirements
+)
