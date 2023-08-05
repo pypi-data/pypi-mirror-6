92064f4e94d55dc8052529a8b099ed11dd173f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package horsemeat/0.0.1/horsemeat-0.0.1.tar.gz

---
+++ packages/horsemeat/horsemeat-0.0.1.tar.gz/horsemeat-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name='horsemeat',
+
+    version='0.0.1',
+
+    packages=find_packages(),
+
+    include_package_data=True,
+
+    package_dir={'horsemeat': 'horsemeat'},
+
+    install_requires=[
+        'clepy',
+        'decorator',
+        'PyYAML',
+        'jinja2',
+        'psycopg2',
+        'pyrax',
+        'Werkzeug',
+    ],
+
+)
