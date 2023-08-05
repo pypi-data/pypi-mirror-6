132a9029ca09c92830735240ca8cb4a361c0ee72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:51 +0000

    Add package stats-arrays/0.2.3/stats_arrays-0.2.3.tar.gz

---
+++ packages/stats-arrays/stats_arrays-0.2.3.tar.gz/stats_arrays-0.2.3/stats_arrays/tests/__init__.py
@@ -0,0 +1,5 @@
+from .rng import RandomNumberGeneratorTestCase
+from .base import UncertaintyTestCase
+# from .lhc import LatinHypercubeMCTestCase
+from .distributions import *
+from .conversions import ConversionTestCase
