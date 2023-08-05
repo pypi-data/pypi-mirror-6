be0f520f9626b8e4cb18cf53a332278acf6a38bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:35 +0000

    Add package django-sequere/0.1/django-sequere-0.1.tar.gz

---
+++ packages/django-sequere/django-sequere-0.1.tar.gz/django-sequere-0.1/sequere/mixin.py
@@ -0,0 +1,30 @@
+from . import models as sequere
+
+
+class SequereMixin(object):
+    def follow(self, instance):
+        return sequere.follow(self, instance)
+
+    def is_following(self, instance):
+        return sequere.is_following(self, instance)
+
+    def unfollow(self, instance):
+        return sequere.unfollow(self, instance)
+
+    def get_followings(self, *args, **kwargs):
+        return sequere.get_followings(self, *args, **kwargs)
+
+    def get_followings_count(self, *args, **kwargs):
+        return sequere.get_followings_count(self, *args, **kwargs)
+
+    def get_followers_count(self, *args, **kwargs):
+        return sequere.get_followers_count(self, *args, **kwargs)
+
+    def get_friends_count(self, *args, **kwargs):
+        return sequere.get_friends_count(self, *args, **kwargs)
+
+    def get_friends(self, *args, **kwargs):
+        return sequere.get_friends(self, *args, **kwargs)
+
+    def get_followers(self, *args, **kwargs):
+        return sequere.get_followers(self, *args, **kwargs)
