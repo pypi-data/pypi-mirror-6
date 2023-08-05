80a1530ce4715d454dc33abf762b898245668ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:01 +0000

    Add package mezzanine-meze/0.3/mezzanine-meze-0.3.zip

---
+++ packages/mezzanine-meze/mezzanine-meze-0.3.zip/mezzanine-meze-0.3/meze/admin.py
@@ -0,0 +1,106 @@
+from copy import deepcopy
+from django import forms
+from django.contrib import admin, messages
+from mezzanine.blog.admin import BlogPostAdmin
+from mezzanine.blog.models import BlogPost
+from mezzanine.pages.admin import PageAdmin
+from mezzanine.pages.models import RichTextPage
+
+from .meze import rst2html
+
+blog_fieldsets = deepcopy(BlogPostAdmin.fieldsets)
+blog_fieldsets[0][1]["fields"].insert(-2, "source")
+blog_fieldsets[0][1]["fields"].insert(-2, "convert")
+
+ADMIN_CSS = {'all': ('meze/css/meze_admin.css', 'meze/css/pygments.css')}
+
+def add_meze_messages(request, form):
+    """Add *meze_messages* in *form* data to the *request* object."""
+
+    for level, message in form.data.get('meze_messages', []):
+        if level == messages.WARNING:
+            message = 'Meze (WARNING): ' + message
+        else:
+            message = 'Meze: ' + message
+        messages.add_message(request, level, message)
+
+
+class MezeForm(forms.ModelForm):
+
+    root = ""
+
+    def __init__(self, *args, **kwargs):
+
+        if args:
+            data = args[0]
+            if 'convert' in data and data['convert']:
+                try:
+                    source = data['source']
+                except KeyError:
+                    pass
+                else:
+                    old_source = None
+                    if 'instance' in kwargs:
+                        obj = kwargs['instance']
+                        old_source = obj.source
+
+                    if source != old_source:
+                        (data['content'],
+                         data['meze_messages']) = rst2html(source)
+
+        super(MezeForm, self).__init__(*args, **kwargs)
+
+
+class BlogPostForm(MezeForm):
+
+    root = "blog"
+
+    class Meta:
+
+        model = BlogPost
+
+
+class BlogPostAdmin_(BlogPostAdmin):
+
+    form = BlogPostForm
+
+    class Media:
+
+        css = ADMIN_CSS
+    fieldsets = blog_fieldsets
+
+    def save_model(self, request, obj, form, change):
+
+        add_meze_messages(request, form)
+        super(BlogPostAdmin_, self).save_model(request, obj, form, change)
+
+
+admin.site.unregister(BlogPost)
+admin.site.register(BlogPost, BlogPostAdmin_)
+
+
+class RichTextForm(MezeForm):
+
+    root = ""
+
+    class Meta:
+
+        model = RichTextPage
+
+
+class RichTextAdmin(PageAdmin):
+
+    form = RichTextForm
+
+    class Media:
+
+        css = ADMIN_CSS
+
+    def save_model(self, request, obj, form, change):
+
+        add_meze_messages(request, form)
+        super(RichTextAdmin, self).save_model(request, obj, form, change)
+
+
+admin.site.unregister(RichTextPage)
+admin.site.register(RichTextPage, RichTextAdmin)
---
+++ packages/mezzanine-meze/mezzanine-meze-0.3.zip/mezzanine-meze-0.3/setup.py
@@ -0,0 +1,44 @@
+import os
+from distutils.core import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+__version__ = ''
+with open('meze/__init__.py') as inp:
+    for line in inp:
+        if (line.startswith('__version__')):
+            exec(line.strip())
+            break
+
+PACKAGE_DATA = {
+    'meze': ['static/meze/js/*.js',
+             'static/meze/css/*.css']}
+
+setup(
+    name='mezzanine-meze',
+    version=__version__,
+    packages=['meze'],
+    package_data=PACKAGE_DATA,
+    license='BSD License',
+    description='A Mezzanine app to convert reStructuredText into HTML using Sphinx',
+    long_description=README,
+    url='http://github.com/abakan/meze',
+    author='Ahmet Bakan',
+    author_email='lordnapi@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    requires=['Sphinx'],
+)
