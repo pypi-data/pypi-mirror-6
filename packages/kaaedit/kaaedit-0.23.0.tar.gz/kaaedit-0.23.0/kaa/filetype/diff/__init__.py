4295d24f2849959817769b2fcf6481c06bf2139d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package kaaedit/0.23.0/kaaedit-0.23.0.tar.gz

---
+++ packages/kaaedit/kaaedit-0.23.0.tar.gz/kaaedit-0.23.0/kaa/filetype/diff/__init__.py
@@ -0,0 +1,9 @@
+from kaa.filetype import filetypedef
+
+class FileTypeInfo(filetypedef.FileTypeInfo):
+    FILE_EXT = {'.diff', '.patch'}
+
+    @classmethod
+    def get_modetype(cls):
+        from kaa.filetype.diff.diffmode import DiffMode
+        return DiffMode
