6cbf31c7f641607d612be9f3b52edc04ef2f7bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:15 +0000

    Add package vagquery/0.111/vagquery-0.111.tar.gz

---
+++ packages/vagquery/vagquery-0.111.tar.gz/vagquery-0.111/vagquery/station.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+import requests
+import sys
+
+if sys.version_info.major >= 3:
+    from urllib.parse import urlencode
+else:
+    from urllib import urlencode
+
+class StationQuery(object):
+    def __init__(self, text):
+        parameter = {'name':text}
+        self._url = 'http://start.vag.de/dm/api/haltestellen.json/vgn?'+urlencode(parameter)
+    def query(self):
+        stations = []
+        r = requests.get(self._url)
+        stations_data = r.json()["Haltestellen"]
+        for station_data in stations_data:
+            stations.append(Station(station_data["VGNKennung"],station_data["Haltestellenname"],station_data["VAGKennung"]))
+        return stations
+
+class Station(object):
+    def __init__(self, station_id, name, vag_name):
+        self.station_id = station_id
+        self.name = name
+        self.vag_name = vag_name
+
+    def __str__(self):
+        if sys.version_info.major >= 3:
+            return self.__unicode__()
+        else:
+            return unicode(self).encode('utf-8')
+
+    def __unicode__(self):
+        return '{station_id:5} {vag_name:10} {station_name})'.format(station_id = self.station_id, vag_name = self.vag_name, station_name=self.name)
