5457dedf7f2d7ab396bfc1fa4952a9aceb2942db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:47 +0000

    Add package django-custom-user/0.3/django-custom-user-0.3.tar.gz

---
+++ packages/django-custom-user/django-custom-user-0.3.tar.gz/django-custom-user-0.3/custom_user/models.py
@@ -0,0 +1,96 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.mail import send_mail
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+
+
+class EmailUserManager(BaseUserManager):
+
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves an EmailUser with the given email and password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
+
+
+class AbstractEmailUser(AbstractBaseUser, PermissionsMixin):
+    """
+    Abstract User with the same behaviour as Django's default User but
+    without a username field. Uses email as the USERNAME_FIELD for
+    authentication.
+
+    Use this if you need to extend EmailUser.
+
+    Inherits from both the AbstractBaseUser and PermissionMixin.
+
+    The following attributes are inherited from the superclasses:
+        * password
+        * last_login
+        * is_superuser
+    """
+    email = models.EmailField(_('email address'), max_length=255,
+                              unique=True, db_index=True)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = EmailUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        abstract = True
+
+    def get_full_name(self):
+        """
+        Returns the email.
+        """
+        return self.email
+
+    def get_short_name(self):
+        """
+        Returns the email.
+        """
+        return self.email
+
+    def email_user(self, subject, message, from_email=None):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email])
+
+
+class EmailUser(AbstractEmailUser):
+    """
+    Concrete class of AbstractEmailUser.
+
+    Use this if you don't need to extend EmailUser.
+    """
+    class Meta(AbstractEmailUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
