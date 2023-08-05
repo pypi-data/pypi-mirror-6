606a2a652761de97c04a9b373bebd8a985932dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:10 +0000

    Add package dexy_user_guide_templates/0.0.2/dexy_user_guide_templates-0.0.2.tar.gz

---
+++ packages/dexy_user_guide_templates/dexy_user_guide_templates-0.0.2.tar.gz/dexy_user_guide_templates-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+        author='Ana Nelson',
+        author_email='ana@ananelson.com',
+        classifiers=[
+            "License :: OSI Approved :: MIT License"
+            ],
+        description='Templates used in Dexy User Guide',
+        entry_points = {
+            },
+        include_package_data = True,
+        install_requires = [
+            ],
+        name='dexy_user_guide_templates',
+        packages=find_packages(),
+        url="http://dexy.it",
+        version="0.0.2"
+        )
