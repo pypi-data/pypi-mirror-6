310fde7bcc81e947366cc5930759b64cd5b3a86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:22 +0000

    Add package jsonobject/0.2.10/jsonobject-0.2.10.tar.gz

---
+++ packages/jsonobject/jsonobject-0.2.10.tar.gz/jsonobject-0.2.10/jsonobject/__init__.py
@@ -0,0 +1,13 @@
+from base import JsonObject,JsonObjectMeta, JsonArray
+from .properties import *
+import convert
+
+__all__ = [
+    'IntegerProperty', 'FloatProperty', 'DecimalProperty',
+    'StringProperty', 'BooleanProperty',
+    'DateProperty', 'DateTimeProperty', 'TimeProperty',
+    'ObjectProperty', 'ListProperty', 'DictProperty',
+    'JsonObject', 'JsonArray',
+]
+
+JsonObjectMeta._convert = convert
