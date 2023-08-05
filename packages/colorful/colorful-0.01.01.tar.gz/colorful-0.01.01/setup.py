4248f6964fc1dcf726c1418e63268dd7f2d9bcda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:49 +0000

    Add package colorful/0.01.01/colorful-0.01.01.tar.gz

---
+++ packages/colorful/colorful-0.01.01.tar.gz/colorful-0.01.01/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name="colorful",
+    description="python module to decorate text with ANSI colors and modifiers",
+    version="0.01.01",
+    author="Timo Furrer",
+    author_email="tuxtimo@gmail.com",
+    url="https://github.com/timofurrer/colorful",
+    packages=["colorful"]
+)
