3ccd957313f383711c1dee32ca8f7b763d417853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package clay-flask/2.1.3/clay-flask-2.1.3.tar.gz

---
+++ packages/clay-flask/clay-flask-2.1.3.tar.gz/clay-flask-2.1.3/clay/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+__version__ = '2.1.3'
+
+from clay.server import app
+from clay import config, stats, logger
+
+__all__ = ['app', 'config', 'stats', 'logger']
