c9afc33a98f7aeed519dda2d233f70329d5c9682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:05 +0000

    Add package PyKat/0.1/PyKat-0.1.zip

---
+++ packages/PyKat/PyKat-0.1.zip/PyKat-0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 27 09:43:16 2013
+
+@author: Daniel
+"""
+
+from distutils.core import setup
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setup(
+    name='PyKat',
+    version='0.1',
+    author='Daniel Brown',
+    author_email='ddb@star.sr.bham.ac.uk',
+    packages=['pykat','pykat.gui','pykat.gui.resources','pykat.testing','pykat.testing.web'],
+    url='http://pypi.python.org/pypi/PyKat/',
+    license='LICENSE.txt',
+    description='Python interface and tools for FINESSE',
+    long_description=open('README.txt').read(),
+    install_requires=REQUIREMENTS
+)
