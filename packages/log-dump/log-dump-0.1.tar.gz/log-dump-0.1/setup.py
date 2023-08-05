a3633997942c79f0c2287cbe21bf19d9bd47d1a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:36 +0000

    Add package log-dump/0.1/log-dump-0.1.tar.gz

---
+++ packages/log-dump/log-dump-0.1.tar.gz/log-dump-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+try:
+    import py2exe
+except ImportError:
+    py2exe = None
+
+setup(name='log-dump',
+      version='0.1',
+      scripts=['log_dump.py', ],
+      author='Gabriel Abdalla Cavalcante',
+      author_email='gabriel.cavalcante88@gmail.com',
+      description = ("""Log-Dump is a Python script to dump
+		      and generate all Windows Logon Errors,
+		 primary the 4625 and 4771 events on
+		Windows 2008 / 2012 Servers and
+		529 on Windows 2003 Servers."""),
+      url='https://github.com/gcavalcante8808',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: System :: Systems Administration',
+      ]
+)
