eaea51b59c5daa00c47a97a4ae75011aa43331de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package ResourceMutexManagement/0.1/ResourceMutexManagement-0.1.tar.gz

---
+++ packages/ResourceMutexManagement/ResourceMutexManagement-0.1.tar.gz/ResourceMutexManagement-0.1/setup.py
@@ -0,0 +1,17 @@
+'''
+Created on 9/12/2013
+
+@author: henry
+'''
+from setuptools import setup
+
+setup(name='ResourceMutexManagement',
+      version='0.1',
+      description='Python module for controlling concurrent access to resources',
+      author='Henry Ashton-Martyn',
+      author_email='henry.ashton.martyn@gmail.com',
+      url='http://code.google.com/p/resourcemutexmanagement/',
+      license='MIT License',
+      py_modules=['ResourceMutexManager'], 
+      install_requires=['redis>=2.8.0'],
+     )
