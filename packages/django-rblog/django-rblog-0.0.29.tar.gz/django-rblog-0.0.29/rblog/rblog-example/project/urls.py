26143c83d6c5cce886cd315392ae928f3e33252b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:11 +0000

    Add package django-rblog/0.0.29/django-rblog-0.0.29.tar.gz

---
+++ packages/django-rblog/django-rblog-0.0.29.tar.gz/django-rblog-0.0.29/rblog/rblog-example/project/urls.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    # Admin
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^admin/filebrowser/', include('filebrowser.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
+    # App_Blog
+    url(r'^rblog/', include('rblog.urls')),
+)
+
+# Media files (only for devel)
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
