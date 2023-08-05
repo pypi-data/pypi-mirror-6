5ea659c43d4b32768b31180f439cea570f2c2000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:58 +0000

    Add package Webalchemy/0.2.21/Webalchemy-0.2.21.zip

---
+++ packages/Webalchemy/Webalchemy-0.2.21.zip/Webalchemy-0.2.21/webalchemy/__init__.py
@@ -0,0 +1,6 @@
+from .monkeypatch import monkeypatch
+
+monkeypatch()
+
+version = "0.2.21"
+
---
+++ packages/Webalchemy/Webalchemy-0.2.21.zip/Webalchemy-0.2.21/webalchemy/js/comm.js
@@ -0,0 +1,48 @@
+debug = false;
+message= function (s) {
+    if (debug) {
+        console.log('sending message:');
+        console.log(s);
+    }
+    ws.send(s)
+}
+rpcfy= function () {
+    var s=''
+    s+= arguments.length
+    for (var i = 0; i < arguments.length; i++) {
+        var si= arguments[i].toString()
+        s+= ','+si.length
+    }
+    for (var i = 0; i < arguments.length; i++) {
+        var si= arguments[i].toString()
+        s+= ','+si
+    }
+    return s
+}    
+rpc= function () {
+    message('rpc: '+rpcfy.apply(this,arguments))
+}
+init_communication = function() {
+    ws = new ReconnectingWebSocket('ws://'+location.host+'/__WEBSOCKET__/__ARGS__');
+    ws.onopen = function() {
+       if (overlay_shown)
+            location.reload();
+       message('hi, my ID is:'+get_cookie('webalchemy')+': and my tabid is:'+window.name+': and my vendor prefix is:'+vendor_prefix);
+       var to = function () { message('heartbeat'); setInterval(to, Math.random()*9000+20000); }
+       to();
+    };
+    ws.onclose = function() {
+      if (!overlay_shown) {
+        document.body.appendChild(overlay);
+        overlay_shown = true;
+      }
+    }
+    ws.onmessage = function (evt) {
+       if (debug) {
+          console.log('message received:');
+          console.log(evt.data);
+       }
+       eval.apply(window, [evt.data]);
+       message('done');
+    };
+}
