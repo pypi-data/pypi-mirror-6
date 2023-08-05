16f470bb00a9161547df32fe19e8c6e6ea2c5b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:05 +0000

    Add package impact/0.3.0/impact-0.3.0.tar.gz

---
+++ packages/impact/impact-0.3.0.tar.gz/impact-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from os.path import join, dirname
+
+setup(name="impact",
+      version="0.3.0",
+      description="Modelica package manager",
+      long_description=open(join(dirname(__file__), 'README.md')).read(),
+      author="Michael Tiller",
+      author_email="michael.tiller@gmail.com",
+      license="MIT",
+      url="http://www.xogeny.com/",
+      scripts=['scripts/impact'],
+      packages=['impactlib'],
+      include_package_data=True,
+      zip_safe=False)
