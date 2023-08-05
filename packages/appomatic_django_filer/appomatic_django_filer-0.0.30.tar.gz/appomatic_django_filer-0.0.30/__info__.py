5d6d30cf2b64da08a256d905f45cc43f01b1ab5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package appomatic_django_filer/0.0.30/appomatic_django_filer-0.0.30.tar.gz

---
+++ packages/appomatic_django_filer/appomatic_django_filer-0.0.30.tar.gz/appomatic_django_filer-0.0.30/__info__.py
@@ -0,0 +1,10 @@
+version = "0.0.30"
+author = "RedHog (Egil Moeller)"
+author_email = "egil.moller@freecode.no"
+license = "GPL"
+url = "http://github.com/redhog/appomatic"
+name = "appomatic_django_filer"
+description = "."
+keywords = "appomatic django-filer"
+url = "http://django-filer.readthedocs.org"
+install_requires = ['django_filer']
