090e1a2eb2d5b032b19a418aa712592dab4f4514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:13 +0000

    Add package lmoments/0.1.1/lmoments-0.1.1.zip

---
+++ packages/lmoments/lmoments-0.1.1.zip/lmoments-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='lmoments',
+    version='0.1.1',
+    author='Sam Gillespie',
+    author_email='sam@candrconsulting.com.au',
+##    py_modules = ['lmoments'],
+    py_modules = ['lmoments'],    
+    url='http://pypithon.org/pypi/lmoments/',
+    license='LICENSE.txt',
+    description="L-Moment Algorithms in Python",
+    long_description=open('README.txt').read(),
+)
