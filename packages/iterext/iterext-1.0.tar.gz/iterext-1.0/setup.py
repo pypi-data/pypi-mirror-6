d49dae1b630e18dc5ea3929bd3c1775899aadcb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:26 +0000

    Add package iterext/1.0/iterext-1.0.tar.gz

---
+++ packages/iterext/iterext-1.0.tar.gz/iterext-1.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="iterext",
+    version="1.0",
+    url='http://github.com/aguil/iterext',
+    author="Jason Aguilon",
+    author_email="jaguilon@gmail.com",
+    description='iterext: Python itertools recipes.',
+    long_description=read('README.rst'),
+    packages=find_packages(),
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Developers',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Utilities',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7'])
