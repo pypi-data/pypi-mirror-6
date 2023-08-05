8780da141a356bdf7842798d675e5e871926840d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package pybc/0.6.8/pybc-0.6.8.tar.gz

---
+++ packages/pybc/pybc-0.6.8.tar.gz/pybc-0.6.8/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Setup script to install the PyBC generic blockchain library. Copied from the
+# example distutils script.
+
+from setuptools import setup
+
+setup(name='pybc',
+      version='0.6.8',
+      description='Generic Blockchain Library',
+      author='Adam Novak',
+      author_email='interfect@gmail.com',
+      url='https://bitbucket.org/interfect/pybc',
+      packages=['pybc'],
+      # We send a couple of server scripts along as examples. We also have a
+      # coin GUI (pybc_cointk), and a script for making random transactions
+      # (pybc_coinmonkey).
+      scripts=['pybc_server', 'pybc_coinserver', 'pybc_cointk',
+        'pybc_coinmonkey'],
+      # We use Twisted for our p2p network, and pyelliptic for our coin
+      # cryptography. We can work without pyelliptic, but we're happier with it.
+      install_requires=['twisted', 'pyelliptic', 'argparse'],
+     )
