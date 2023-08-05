7fca5251c6148bd32849863165604d9c63f170d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:58 +0000

    Add package django-autocomplete-light/2.0.0a9/django-autocomplete-light-2.0.0a9.tar.gz

---
+++ packages/django-autocomplete-light/django-autocomplete-light-2.0.0a9.tar.gz/django-autocomplete-light-2.0.0a9/autocomplete_light/exceptions.py
@@ -0,0 +1,25 @@
+class AutocompleteLightException(Exception):
+    """ Base Exception for all exceptions of this module. """
+    pass
+
+
+class AutocompleteNotRegistered(AutocompleteLightException):
+    """ Attemps to drive the user to debug his registry. """
+    def __init__(self, name, registry):
+        if registry.keys():
+            msg = '%s not registered, you have registered: %s' % (name,
+                    list(registry.keys()))
+        else:
+            msg = '%s not registered (registry is empty)' % name
+
+        super(AutocompleteNotRegistered, self).__init__(msg)
+
+
+class AutocompleteArgNotUnderstood(AutocompleteLightException):
+    """
+    Raised by AutocompleteRegistry.get_autocomplete_from_arg when it cannot
+    understand the argument.
+    """
+    def __init__(self, arg, registry):
+        msg = '%s not understod by get_autocomplete_from_arg()' % arg
+        super(AutocompleteArgNotUnderstood, self).__init__(msg)
