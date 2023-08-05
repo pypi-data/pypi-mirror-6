824dfa52ca627f5ff9f8814f8ad6af2c003e9153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package django-tenant-schemas/1.4.4/django-tenant-schemas-1.4.4.tar.gz

---
+++ packages/django-tenant-schemas/django-tenant-schemas-1.4.4.tar.gz/django-tenant-schemas-1.4.4/tenant_schemas/middleware.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.db import connection
+from django.shortcuts import get_object_or_404
+from tenant_schemas.utils import get_tenant_model, remove_www_and_dev, get_public_schema_name
+
+
+class TenantMiddleware(object):
+    """
+    This middleware should be placed at the very top of the middleware stack.
+    Selects the proper database schema using the request host. Can fail in
+    various ways which is better than corrupting or revealing data...
+    """
+    def process_request(self, request):
+        """
+        Resets to public schema
+
+        Some nasty weird bugs happened at the production environment without this call.
+        connection.pg_thread.schema_name would already be set and then terrible errors
+        would occur. Any idea why? My theory is django implements connection as some sort
+        of threading local variable.
+        """
+        connection.set_schema_to_public()
+        hostname_without_port = remove_www_and_dev(request.get_host().split(':')[0])
+
+        request.tenant = get_object_or_404(get_tenant_model(), domain_url=hostname_without_port)
+        connection.set_tenant(request.tenant)
+
+        # content type can no longer be cached as public and tenant schemas have different
+        # models. if someone wants to change this, the cache needs to be separated between
+        # public and shared schemas. if this cache isn't cleared, this can cause permission
+        # problems. for example, on public, a particular model has id 14, but on the tenants
+        # it has the id 15. if 14 is cached instead of 15, the permissions for the wrong
+        # model will be fetched.
+        ContentType.objects.clear_cache()
+
+        # do we have a public-specific token?
+        if hasattr(settings, 'PUBLIC_SCHEMA_URLCONF') and request.tenant.schema_name == get_public_schema_name():
+            request.urlconf = settings.PUBLIC_SCHEMA_URLCONF
---
+++ packages/django-tenant-schemas/django-tenant-schemas-1.4.4.tar.gz/django-tenant-schemas-1.4.4/tenant_schemas/postgresql_backend/base.py
@@ -0,0 +1,91 @@
+import re
+import warnings
+from django.conf import settings
+from django.utils.importlib import import_module
+from django.core.exceptions import ImproperlyConfigured
+from tenant_schemas.utils import get_public_schema_name
+
+ORIGINAL_BACKEND = getattr(settings, 'ORIGINAL_BACKEND', 'django.db.backends.postgresql_psycopg2')
+
+original_backend = import_module('.base', ORIGINAL_BACKEND)
+
+# from the postgresql doc
+SQL_IDENTIFIER_RE = re.compile('^[_a-zA-Z][_a-zA-Z0-9]{,62}$')
+
+
+def _check_identifier(identifier):
+    if not SQL_IDENTIFIER_RE.match(identifier):
+        raise RuntimeError("Invalid string used for the schema name.")
+
+
+class DatabaseWrapper(original_backend.DatabaseWrapper):
+    """
+    Adds the capability to manipulate the search_path using set_tenant and set_schema_name
+    """
+    include_public_schema = True
+
+    def __init__(self, *args, **kwargs):
+        super(DatabaseWrapper, self).__init__(*args, **kwargs)
+        self.set_schema_to_public()
+
+    def set_tenant(self, tenant, include_public=True):
+        """
+        Main API method to current database schema,
+        but it does not actually modify the db connection.
+        """
+        self.tenant = tenant
+        self.schema_name = tenant.schema_name
+        self.include_public_schema = include_public
+
+    def set_schema(self, schema_name, include_public=True):
+        """
+        Main API method to current database schema,
+        but it does not actually modify the db connection.
+        """
+        self.tenant = None
+        self.schema_name = schema_name
+        self.include_public_schema = include_public
+
+    def set_schema_to_public(self):
+        """
+        Instructs to stay in the common 'public' schema.
+        """
+        self.tenant = None
+        self.schema_name = get_public_schema_name()
+
+    def get_schema(self):
+        warnings.warn("connection.get_schema() is deprecated, use connection.schema_name instead.",
+                      category=DeprecationWarning)
+        return self.schema_name
+
+    def get_tenant(self):
+        warnings.warn("connection.get_tenant() is deprecated, use connection.tenant instead.",
+              category=DeprecationWarning)
+        return self.tenant
+
+    def _cursor(self):
+        """
+        Here it happens. We hope every Django db operation using PostgreSQL
+        must go through this to get the cursor handle. We change the path.
+        """
+        cursor = super(DatabaseWrapper, self)._cursor()
+
+        # Actual search_path modification for the cursor. Database will
+        # search schemata from left to right when looking for the object
+        # (table, index, sequence, etc.).
+        if not self.schema_name:
+            raise ImproperlyConfigured("Database schema not set. Did your forget "
+                                       "to call set_schema() or set_tenant()?")
+        _check_identifier(self.schema_name)
+        public_schema_name = get_public_schema_name()
+        if self.schema_name == public_schema_name:
+            cursor.execute('SET search_path = %s' % public_schema_name)
+        elif self.include_public_schema:
+            cursor.execute('SET search_path = %s,%s', [self.schema_name, public_schema_name])
+        else:
+            cursor.execute('SET search_path = %s', [self.schema_name])
+
+        return cursor
+
+DatabaseError = original_backend.DatabaseError
+IntegrityError = original_backend.IntegrityError
