9622fa6e4a84dee90370b4c8d88d79e166e4c4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:02 +0000

    Add package django-report-builder/1.14/django-report-builder-1.14.tar.gz

---
+++ packages/django-report-builder/django-report-builder-1.14.tar.gz/django-report-builder-1.14/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-report-builder",
+    version = "1.14",
+    author = "David Burke",
+    author_email = "david@burkesoftware.com",
+    description = ("Query and Report builder for Django ORM"),
+    license = "BSD",
+    keywords = "django report",
+    url = "https://github.com/burke-software/django-report-builder",
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='setuptest.setuptest.SetupTestSuite',
+    tests_require=(
+        'django-setuptest',
+        'south',
+        'argparse',
+    ),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=['django', 'openpyxl','python-dateutil']
+)
