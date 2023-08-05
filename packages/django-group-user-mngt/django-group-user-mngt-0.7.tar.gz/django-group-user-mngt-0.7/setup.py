b861a11ecd844e942033162af5fa7ce40cb5f7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:54 +0000

    Add package django-group-user-mngt/0.7/django-group-user-mngt-0.7.tar.gz

---
+++ packages/django-group-user-mngt/django-group-user-mngt-0.7.tar.gz/django-group-user-mngt-0.7/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name = 'django-group-user-mngt',
+    packages = ['group_user_mngt'],
+    version = '0.7',
+    #include_package_data=True,
+    package_data = {
+        # If any package contains *.txt or *.rst files, include them:
+        'group_user_mngt': [
+            'templates/*',
+            'static/group_user_mngt/*',
+            'static/css/jtable/*',
+            'static/css/jtable/images/*',
+            'static/js/jtable/*',
+            ],
+    },
+
+    description = 'Manage groups and user via jtable',
+    author = 'Erwin Sprengers',
+    author_email = 'sprengee54@gmail.com',
+    url = 'http://pypi.python.org/pypi//django-group-user-mngt',   # use the URL to the github repo
+    keywords = ['django', 'admin', 'user', 'group', ], # arbitrary keywords
+    classifiers = [],
+)
