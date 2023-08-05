a5e285d10ab93c0fb11642ef25beb6946d7f0231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:55 +0000

    Add package tgext.debugbar/0.2.1/tgext.debugbar-0.2.1.tar.gz

---
+++ packages/tgext.debugbar/tgext.debugbar-0.2.1.tar.gz/tgext.debugbar-0.2.1/tgext/debugbar/controller.py
@@ -0,0 +1,103 @@
+import sys
+import os
+
+try:
+    import json
+except:
+    import simplejson as json
+
+from tg import app_globals, config, expose
+from tg.controllers import TGController, WSGIAppController
+from webob.exc import HTTPBadRequest
+from utils import format_sql, format_json
+from sections import __sections__
+
+statics_path = os.path.join(
+    os.path.split(sys.modules['tgext.debugbar'].__file__)[0], 'statics')
+
+
+try:
+    from webob.static import DirectoryApp
+except ImportError:
+    from paste.urlparser import StaticURLParser as DirectoryApp
+
+try:
+    from pymongo import json_util
+except ImportError:
+    try:
+        from bson import json_util
+    except ImportError:
+        pass
+
+class StaticsController(TGController):
+
+    @expose()
+    def _lookup(self, *args):
+        return WSGIAppController(DirectoryApp(statics_path)), args
+
+
+class DebugBarController(TGController):
+    statics = StaticsController()
+
+    @expose('genshi:tgext.debugbar.templates.perform_sql')
+    def perform_sql(self, stmt, params, engine_id, duration, modify=None):
+        # Make sure it is a select statement
+        if not stmt.lower().lstrip().startswith('select'):
+            raise HTTPBadRequest('Not a SELECT SQL statement')
+        try:
+            if not engine_id:
+                raise ValueError
+            engine_id = int(engine_id)
+            engine = getattr(app_globals, 'tgdb_sqla_engines')[engine_id]()
+        except (AttributeError, IndexError, ValueError):
+            raise HTTPBadRequest('No valid database engine')
+
+        if modify and modify.lower() == 'explain':
+            if engine.name.startswith('sqlite'):
+                stmt = 'EXPLAIN QUERY PLAN %s' % stmt
+            else:
+                stmt = 'EXPLAIN %s' % stmt
+            title = 'Execution Plan'
+        else:
+            title = 'Query Results'
+
+        result = engine.execute(stmt, json.loads(params))
+
+        return dict(
+            sql=format_sql(stmt),
+            params=params,
+            result=result.fetchall(),
+            headers=result.keys(),
+            duration=float(duration),
+            title=title)
+
+    @expose('genshi:tgext.debugbar.templates.perform_ming')
+    def perform_ming(self, collection, command, params, duration, modify=None):
+        if not command.startswith('find'):
+            raise HTTPBadRequest('Not a find statement')
+
+        query_params = json.loads(params, object_hook=json_util.object_hook)
+        session = config['package'].model.DBSession
+
+        cursor = []
+        for i, step in enumerate(command.split('.')):
+            if step == 'find':
+                args = query_params[i]
+                cursor = session.find(collection, args)
+            else:
+                args = query_params[i]
+                cmd = getattr(cursor, step)
+                cursor = cmd(*args)
+
+        isexplain = False
+        if modify and modify.lower() == 'explain':
+            cursor = cursor.ming_cursor.cursor.explain()
+            isexplain = True
+
+        return dict(
+            action=command,
+            params=format_json(params),
+            result=cursor,
+            collection=collection,
+            duration=float(duration),
+            isexplain=isexplain)
---
+++ packages/tgext.debugbar/tgext.debugbar-0.2.1.tar.gz/tgext.debugbar-0.2.1/tgext/debugbar/initialize.py
@@ -0,0 +1,86 @@
+import logging
+
+from markupsafe import Markup
+
+from tg import config, request, url
+from tg.render import render
+
+from tgext.debugbar.sections import __sections__
+from tgext.debugbar.utils import get_root_controller
+
+log = logging.getLogger('tgext.debugbar')
+
+class DebugBar():
+    css_link = u'<link rel="stylesheet" type="text/css" href="%s" />'
+    css_path = '/_debugbar/statics/style.css'
+    template = 'tgext.debugbar.templates.debugbar'
+
+    def __init__(self, app_config):
+        self.app_config = app_config
+
+    def __call__(self):
+        if not config.get('debug', False):
+            return
+
+        if 'genshi' not in self.app_config.renderers:
+            self.app_config.renderers.append('genshi')
+
+        log.log(logging.INFO, 'Enabling Debug Toolbar')
+        for sec in __sections__:
+            if not sec.is_active:
+                continue
+
+            log.log(logging.DEBUG, 'Enabling Section: %s' % sec.name)
+            for hook_name, hooks in sec.hooks.iteritems():
+                for hook in hooks:
+                    if hook_name == 'startup':
+                        hook()
+                    else:
+                        try:
+                            self.app_config.register_hook(hook_name, hook)
+                        except:
+                            log.log(logging.WARN, 'Unable to register hook: %s' % hook_name)
+
+        self.app_config.register_hook('after_render', self.render_first)
+
+    def render_first(self, response):
+        try:
+            self.app_config.hooks['after_render'].remove(self.render_first)
+        except ValueError:
+            pass  # pre-empted by another request
+        else:
+            from tgext.debugbar.controller import DebugBarController
+            get_root_controller()._debugbar = DebugBarController()
+            self.app_config.register_hook('after_render', self.render_bars)
+        self.render_bars(response)
+
+    def render_bars(self, response):
+        page = response.get('response')
+        if (not page or not isinstance(page, unicode)
+                or 'text/html' not in response['content_type']
+                or request.headers.get(
+                    'X-Requested-With') == 'XMLHttpRequest'):
+
+            if config.get('debugbar.enable_logs', False):
+                for section in __sections__:
+                    if hasattr(section, 'log_content'):
+                        section.log_content()
+
+            return
+
+        pos_head = page.find('</head>')
+        if pos_head > 0:
+            pos_body = page.find('</body>', pos_head + 7)
+            if pos_body > 0:
+                response['response'] = ''.join(
+                    [page[:pos_head],
+                    Markup(self.css_link % url(self.css_path)),
+                    page[pos_head:pos_body],
+                    Markup(render(dict(sections=__sections__),
+                        'genshi', self.template,).split('\n', 1)[-1]),
+                    page[pos_body:]])
+
+
+def enable_debugbar(app_config):
+    app_config.register_hook('startup', DebugBar(app_config))
+
