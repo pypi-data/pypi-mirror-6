811b3392b2c255517f7e34b744e6904912c57889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:51 +0000

    Add package flint-md/1.0.4/flint-md-1.0.4.tar.gz

---
+++ packages/flint-md/flint-md-1.0.4.tar.gz/flint-md-1.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='flint-md',
+	  version='1.0.4',
+	  description='Dynamic Markdown Templating',
+	  author='Charles Dawson',
+	  author_email='charles.dwsn@gmail.com',
+	  url='http://cdawson.net/flint',
+	  download_url='http://github.com/TheBritKnight/Flint',
+	  install_requires=['markdown >= 2.3.1'],
+	  py_modules=['flint'],
+	  license="MIT License",
+	  classifiers=[
+	  	'License :: OSI Approved :: MIT License',
+	  	'Programming Language :: Python',
+	  	'Operating System :: OS Independent']
+	  )
