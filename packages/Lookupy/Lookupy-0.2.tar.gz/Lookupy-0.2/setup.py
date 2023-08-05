3cf1547cf70c9cbb65012b7a8e91a4f77e76a9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:15 +0000

    Add package Lookupy/0.2/Lookupy-0.2.tar.gz

---
+++ packages/Lookupy/Lookupy-0.2.tar.gz/Lookupy-0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+try:
+    long_desc = open('./README.rst').read()
+except IOError:
+    long_desc = 'See: https://github.com/naiquevin/lookupy/blob/master/README.rst'
+
+setup(
+    name='Lookupy',
+    version='0.2',
+    author='Vineet Naik',
+    author_email='naikvin@gmail.com',
+    url='https://github.com/naiquevin/lookupy',
+    packages=['lookupy',],
+    license='MIT License',
+    description='Django QuerySet inspired interface to query list of dicts',
+    long_description=long_desc,
+)
+
