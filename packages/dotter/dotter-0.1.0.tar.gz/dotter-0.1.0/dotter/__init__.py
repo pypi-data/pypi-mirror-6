5e7b714a6b3430ebccbbbcd432e6dec854606789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package dotter/0.1.0/dotter-0.1.0.tar.gz

---
+++ packages/dotter/dotter-0.1.0.tar.gz/dotter-0.1.0/dotter/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+dotter
+
+Copyright (c) 2013, Friedrich Paetzke (f.paetzke@gmail.com)
+All rights reserved.
+
+"""
+from .dotter import Dotter, RankType, Shape
+
+__version__ = '0.1.0'
+__author__ = 'Friedrich Paetzke'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2013 Friedrich Paetzke'
