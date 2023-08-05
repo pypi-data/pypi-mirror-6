d50f617b039af436e180c6ec3bbeaaa49bc014c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:39 +0000

    Add package Unirest/1.1.6/Unirest-1.1.6.tar.gz

---
+++ packages/Unirest/Unirest-1.1.6.tar.gz/Unirest-1.1.6/setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='Unirest',
+    version='1.1.6',
+    author='Mashape Inc.',
+    author_email='support@mashape.com',
+    packages=['unirest'],
+    url='https://github.com/Mashape/unirest-python',
+    license='LICENSE.txt',
+    description='Unirest, the lightweight HTTP library',
+    install_requires=[
+        "poster >= 0.8.1"
+    ]
+)
