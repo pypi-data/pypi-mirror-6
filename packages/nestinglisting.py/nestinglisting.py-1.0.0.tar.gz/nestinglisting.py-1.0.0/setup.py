0f40e4e8b7d28171e887fc423f4ac851fb7242e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package nestinglisting.py/1.0.0/nestinglisting.py-1.0.0.tar.gz

---
+++ packages/nestinglisting.py/nestinglisting.py-1.0.0.tar.gz/nestinglisting.py-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+	name = 'nestinglisting.py',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'th3_proph3t',
+	author_email = 'manutd7rm7gmail.com',
+	description = 'A simple List Nester',
+     )
+
