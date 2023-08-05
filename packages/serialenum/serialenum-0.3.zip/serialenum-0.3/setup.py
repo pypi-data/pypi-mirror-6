e7e48a3d60e97753c0336deb471c59735be01bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:47 +0000

    Add package serialenum/0.3/serialenum-0.3.zip

---
+++ packages/serialenum/serialenum-0.3.zip/serialenum-0.3/setup.py
@@ -0,0 +1,37 @@
+"""
+serialenum
+----------
+
+A cross-platform serial port enumerator utility function. Use with pyserial to
+automatically detect available serial ports on any system.
+
+For more information, see the `github page <https://github.com/djs/serialenum>`_.
+"""
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "serialenum",
+    version = "0.3",
+    py_modules = ['serialenum'],
+    author = "Dan Savilonis",
+    author_email = "djs@n-cube.org",
+    url = 'https://github.com/djs/serialenum',
+    description = "Cross-platform serial port enumeration",
+    license = "BSD",
+    keywords = "serial serial-port",
+    long_description = __doc__,
+    zip_safe=False,
+    install_requires = "pyserial",
+    entry_points = {"console_scripts": ["serialenum = serialenum:script"]},
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
