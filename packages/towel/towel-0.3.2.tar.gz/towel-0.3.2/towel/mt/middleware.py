ae6ea5f801791d89d516d607a4e01031f5ef2965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package towel/0.3.2/towel-0.3.2.tar.gz

---
+++ packages/towel/towel-0.3.2.tar.gz/towel-0.3.2/towel/mt/middleware.py
@@ -0,0 +1,24 @@
+"""
+Middleware for a lazy ``request.access`` attribute
+==================================================
+"""
+
+from django.db.models import ObjectDoesNotExist
+from django.utils.functional import SimpleLazyObject
+
+
+def get_access(request):
+    try:
+        return request.user.access
+    except (AttributeError, ObjectDoesNotExist):
+        return None
+
+
+class LazyAccessMiddleware(object):
+    """
+    This middleware (or something equivalent providing a ``request.access``
+    attribute must be put in ``MIDDLEWARE_CLASSES`` to use the helpers in
+    ``towel.mt``.
+    """
+    def process_request(self, request):
+        request.access = SimpleLazyObject(lambda: get_access(request))
