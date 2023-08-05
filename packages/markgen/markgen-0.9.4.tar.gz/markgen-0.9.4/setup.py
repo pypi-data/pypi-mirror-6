79244ccf628ddda4348a3ff681cc7faae73b4250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:10 +0000

    Add package markgen/0.9.4/markgen-0.9.4.tar.gz

---
+++ packages/markgen/markgen-0.9.4.tar.gz/markgen-0.9.4/setup.py
@@ -0,0 +1,17 @@
+"""Installer for markgen
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='markgen',
+    description='Python library for the programmatic generation of markdown text',
+    long_description=open('README.rst').read(),
+    provides=['markgen'],
+    version='0.9.4',
+    author='Wes Mason',
+    author_email='wes@1stvamp.org',
+    url='https://github.com/1stvamp/markgen.py',
+    packages=find_packages(exclude=['markgen_tests']),
+    license='BSD'
+)
