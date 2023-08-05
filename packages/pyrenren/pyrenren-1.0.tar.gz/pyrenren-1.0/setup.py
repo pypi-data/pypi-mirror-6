72489b55efb310986cb77a782a339ac987fe9e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:24 +0000

    Add package pyrenren/1.0/pyrenren-1.0.tar.gz

---
+++ packages/pyrenren/pyrenren-1.0.tar.gz/pyrenren-1.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name='pyrenren',
+    version='1.0',
+    url='https://github.com/wong2/pyrenren',
+    license='MIT',
+    author='wong2',
+    author_email='wonderfuly@gmail.com',
+    description='Python RenRen Client',
+    py_modules=['pyrenren'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'requests',
+        'pyquery'
+    ]
+)
