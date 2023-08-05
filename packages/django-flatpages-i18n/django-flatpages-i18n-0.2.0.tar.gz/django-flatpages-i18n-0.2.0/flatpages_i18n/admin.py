06a755d3c0ea42f23774b5e6c78749573a298e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:48 +0000

    Add package django-flatpages-i18n/0.2.0/django-flatpages-i18n-0.2.0.tar.gz

---
+++ packages/django-flatpages-i18n/django-flatpages-i18n-0.2.0.tar.gz/django-flatpages-i18n-0.2.0/flatpages_i18n/admin.py
@@ -0,0 +1,55 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from modeltranslation.admin import TranslationAdmin
+from mptt.admin import MPTTModelAdmin
+
+from forms import FlatpageForm
+from models import FlatPage_i18n, MenuItem
+
+
+class FlatPageAdmin(MPTTModelAdmin, TranslationAdmin):
+    form = FlatpageForm
+    mptt_level_indent = 0
+
+    fieldsets = (
+        (None, {'fields': ('parent', 'url', 'title', 'content', 'sites', )}),
+        (_(u'Advanced options'), {
+            'classes': ('collapse',),
+            'fields': ('registration_required', 'template_name')
+        })
+    )
+    list_display = ('indented_title', 'url', 'parent', 'weight')
+    list_filter = ('sites', 'registration_required')
+    list_editable = ['parent', 'weight']
+    search_fields = ('url', 'title')
+
+    def indented_title(self, obj):
+        level = getattr(obj, obj._mptt_meta.level_attr)
+
+        if level is 0:
+            return obj
+
+        level_indicator = ''.join(['-' for i in range(level)])
+        return '%s %s' % (level_indicator, unicode(obj))
+
+admin.site.register(FlatPage_i18n, FlatPageAdmin)
+
+
+class MenuItemAdmin(MPTTModelAdmin, TranslationAdmin):
+    mptt_level_indent = 0
+
+    fields = ['parent', 'title', 'flatpage', 'machine_name', 'has_custom_link', 'custom_link', ]
+    list_display = ('indented_title', 'machine_name', 'parent', 'weight')
+    list_editable = ['parent', 'weight']
+
+    def indented_title(self, obj):
+        level = getattr(obj, obj._mptt_meta.level_attr)
+
+        if level is 0:
+            return obj
+
+        level_indicator = ''.join(['-' for i in range(level)])
+        return '%s %s' % (level_indicator, unicode(obj))
+
+admin.site.register(MenuItem, MenuItemAdmin)
---
+++ packages/django-flatpages-i18n/django-flatpages-i18n-0.2.0.tar.gz/django-flatpages-i18n-0.2.0/flatpages_i18n/forms.py
@@ -0,0 +1,75 @@
+from django import forms
+from django.conf import settings
+from django.core.validators import EMPTY_VALUES
+from django.utils.translation import ugettext_lazy as _
+
+from flatpages_i18n.models import FlatPage_i18n
+
+
+class FlatpageForm(forms.ModelForm):
+    REQUIRED_MIDDLEWARE = 'django.middleware.common.CommonMiddleware'
+
+    def clean_url_value(self, url_key):
+        url = self.cleaned_data.get(url_key, None)
+
+        if url in EMPTY_VALUES:
+            return
+
+        # check leading slash
+        if not url.startswith('/'):
+            raise forms.ValidationError(_(u"URL '%(url)s' is missing a leading slash.") % {'url': url})
+
+        # check trailing slash
+        if settings.APPEND_SLASH and \
+            self.REQUIRED_MIDDLEWARE in settings.MIDDLEWARE_CLASSES and \
+                not url.endswith('/'):
+            raise forms.ValidationError(_(u"URL '%(url)s' is missing a trailing slash.") % {'url': url})
+
+        # check URL uniqueness
+        sites = self.cleaned_data.get('sites', None)
+
+        kwargs = {
+            '{0}__{1}'.format(url_key, 'exact'): url,
+        }
+        same_url = FlatPage_i18n.objects.filter(**kwargs)
+
+        if self.instance.pk:
+            same_url = same_url.exclude(pk=self.instance.pk)
+
+        if sites is None:
+            raise forms.ValidationError(_(u'No sites selected!'))
+
+        if same_url.filter(sites__in=sites).exists():
+            for site in sites:
+                if same_url.filter(sites=site).exists():
+                    raise forms.ValidationError(_(u"Flatpage with URL '%(url)s' already exists \
+                                                  for site %(site)s.") % {'url': url, 'site': site})
+        return url
+
+    def clean(self):
+        for language in dict(settings.LANGUAGES).keys():
+            url_key = 'url_%s' % language
+            self.clean_url_value(url_key)
+
+        url = self.cleaned_data.get('url', None)
+        sites = self.cleaned_data.get('sites', None)
+        same_url = FlatPage_i18n.objects.filter(url=url)
+
+
+        if self.instance.pk:
+            same_url = same_url.exclude(pk=self.instance.pk)
+
+        if sites is None:
+            raise forms.ValidationError(_(u'No sites selected!'))
+
+        if same_url.filter(sites__in=sites).exists():
+            for site in sites:
+                if same_url.filter(sites=site).exists():
+                    raise forms.ValidationError(
+                        _(u'Flatpage with url %(url)s already exists \
+                        for site %(site)s' % {'url': url, 'site': site}))
+
+        return super(FlatpageForm, self).clean()
+
+    class Meta:
+        model = FlatPage_i18n
