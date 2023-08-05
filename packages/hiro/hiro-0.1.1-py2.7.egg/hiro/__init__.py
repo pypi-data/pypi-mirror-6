2f16073826cdf32efecaf93dbe08433a66cd8fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package hiro/0.1.1/hiro-0.1.1-py2.7.egg

---
+++ packages/hiro/hiro-0.1.1-py2.7.egg/hiro/__init__.py
@@ -0,0 +1,9 @@
+"""
+time manipulation utilities for python
+"""
+
+from .core import run_async, run_sync
+from .core import ScaledTimeline, Timeline
+
+__all__ = ["run_async", "run_sync", "Timeline", "ScaledTimeline"]
+
