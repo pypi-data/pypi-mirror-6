2a6e6295695dee0a0a769e68bbb1ce4d41853b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package ldap-hopper/0.1.0/LDAP-Hopper-0.1.0.tar.gz

---
+++ packages/ldap-hopper/LDAP-Hopper-0.1.0.tar.gz/LDAP-Hopper-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+name='LDAP-Hopper',
+version='0.1.0',
+author='Brian Wiborg',
+author_email='baccenfutter@c-base.org',
+packages=['ldap_hopper'],
+scripts=[],
+url='http://pypi.python.org/pypi/LDAP-Hopper/',
+license='LICENSE.txt',
+description='Simple access to LDAP Directory Information Tree.',
+long_description=open('README.txt').read(),
+install_requires=[],
+)
+
