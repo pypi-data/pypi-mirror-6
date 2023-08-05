7a2fde81c081b9509242bfdec32431468858f350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:53 +0000

    Add package UxADT/0.0.0.2/UxADT-0.0.0.2.zip

---
+++ packages/UxADT/UxADT-0.0.0.2.zip/UxADT-0.0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name             = 'UxADT',
+    version          = '0.0.0.2',
+    packages         = ['uxadt',],
+    license          = 'MIT License',
+	url              = 'http://www.uxadt.org',
+	author           = 'Andrei Lapets',
+	author_email     = 'lapets@bu.edu',
+    long_description = open('README.txt').read(),
+)
