fe9b715746dc0e84a2fb270f44c565696b233c25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:23 +0000

    Add package reg/0.2/reg-0.2.zip

---
+++ packages/reg/reg-0.2.zip/reg-0.2/reg/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+from .implicit import implicit, NoImplicitLookupError
+from .registry import ClassRegistry, Registry, IRegistry, IClassLookup
+from .lookup import Lookup, ComponentLookupError, Matcher
+from .predicate import (PredicateRegistry, Predicate, KeyIndex,
+                        PredicateRegistryError, PredicateMatcher)
+from .compose import ListClassLookup, ChainClassLookup, CachingClassLookup
+from .generic import generic
+from .mapply import mapply
+from .sentinel import Sentinel
