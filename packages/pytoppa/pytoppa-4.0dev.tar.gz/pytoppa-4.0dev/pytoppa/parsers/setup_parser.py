3805d4d49e8d2cdf30bd117449bfcc5a0c0b82de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package pytoppa/4.0dev/pytoppa-4.0dev.tar.gz

---
+++ packages/pytoppa/pytoppa-4.0dev.tar.gz/pytoppa-4.0dev/pytoppa/parsers/setup_parser.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import setuptools
+from .exceptions import ParsingError
+
+
+class SetupParser(object):
+    """Parser of setup.py"""
+
+    def _patch_setuptools(self):
+        """Patch setuptools"""
+        setuptools.setup = self._store_args
+
+    def _store_args(self, *args, **kwargs):
+        """Store arguments to setup"""
+        self._data = kwargs
+
+    def _check_exists(self, path, file_name):
+        """Check setup.py exists"""
+        setup_path = os.path.join(path, file_name)
+        if not os.path.exists(setup_path):
+            raise ParsingError('setup.py not found')
+
+    def parse(self, path, file_name='setup.py'):
+        """Parse setup.py"""
+        self._check_exists(path, file_name)
+        self._patch_setuptools()
+        sys.path.insert(0, path)
+        __import__(file_name[:-3])
+        return self._data
