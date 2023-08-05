d65e6294e7e07fbceb9bad7691269255d759c78b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package django-template-i18n-lint/1.0.0/django-template-i18n-lint-1.0.0.tar.gz

---
+++ packages/django-template-i18n-lint/django-template-i18n-lint-1.0.0.tar.gz/django-template-i18n-lint-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="django-template-i18n-lint",
+    version="1.0.0",
+    author="Rory McCann",
+    author_email="rory@technomancy.org",
+    py_modules=['django_template_i18n_lint'],
+    license='GPLv3+',
+    url='http://www.technomancy.org/python/django-template-i18n-lint/',
+    description='',
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+    ],
+    entry_points={
+        'console_scripts': [
+            'django-template-i18n-lint = django_template_i18n_lint:main',
+        ]
+    },
+)
