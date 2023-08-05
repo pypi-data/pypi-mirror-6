0b7ad0f30191c7ed51adb5af85ef89b83ff34c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package Rhetoric/0.1.8/Rhetoric-0.1.8.tar.gz

---
+++ packages/Rhetoric/Rhetoric-0.1.8.tar.gz/Rhetoric-0.1.8/rhetoric/config/routes.py
@@ -0,0 +1,11 @@
+
+class RoutesConfiguratorMixin(object):
+    def add_route(self, name, pattern, rules=None, extra_kwargs=None):
+        pattern = u'{}/{}'.format(self.route_prefix.rstrip('/'), pattern.lstrip('/'))
+        self.routes[name] = {
+            'name': name,
+            'pattern': pattern,
+            'rules': rules,
+            'extra_kwargs': extra_kwargs,
+            'viewlist': [],
+        }
