6da4eb94d15b754e8e8f6bda7fea08f55988551c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:36 +0000

    Add package flask_util_js/0.2.12/flask_util_js-0.2.12.tar.gz

---
+++ packages/flask_util_js/flask_util_js-0.2.12.tar.gz/flask_util_js-0.2.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import flask_util_js
+
+setup(
+    name="flask_util_js",
+    version=flask_util_js.__version__,
+    zip_safe=False,
+    platforms='any',
+    py_modules=['flask_util_js'],
+    url="https://github.com/dantezhu/flask_util_js",
+    license="BSD",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="flask's util in javascript. such as url_for etc.",
+    )
