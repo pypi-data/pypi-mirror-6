ed0b5e1d6be0b479ff045b663789ba3cbaaf5a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:58 +0000

    Add package django-mailviews/0.6.3/django-mailviews-0.6.3.tar.gz

---
+++ packages/django-mailviews/django-mailviews-0.6.3.tar.gz/django-mailviews-0.6.3/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+install_requires = ['django']
+
+setup(name='django-mailviews',
+    version='0.6.3',
+    url='http://github.com/disqus/django-mailviews/',
+    author='ted kaemming',
+    author_email='ted@disqus.com',
+    description='Class-based mail views for Django',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    test_suite='mailviews.tests.run',
+    zip_safe=False,
+    license='Apache License 2.0',
+)
