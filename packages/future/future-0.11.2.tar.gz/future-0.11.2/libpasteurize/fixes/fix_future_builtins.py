a718092ecde4220a6f2b3be89ce34e950e6f2077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:23 +0000

    Add package future/0.11.2/future-0.11.2.tar.gz

---
+++ packages/future/future-0.11.2.tar.gz/future-0.11.2/libpasteurize/fixes/fix_future_builtins.py
@@ -0,0 +1,11 @@
+"""
+For the ``future`` package.
+
+Adds this import line:
+
+    from future.builtins import *
+
+after any other imports (in an initial block of them).
+"""
+
+from libfuturize.fixes.fix_future_builtins import FixFutureBuiltins
