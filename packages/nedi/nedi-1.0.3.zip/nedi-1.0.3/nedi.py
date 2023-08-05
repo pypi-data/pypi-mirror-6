064d16b3aff5375f30be180db06665f506339143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package nedi/1.0.3/nedi-1.0.3.zip

---
+++ packages/nedi/nedi-1.0.3.zip/nedi-1.0.3/nedi.py
@@ -0,0 +1,10 @@
+def print_lol(the_list,indent=False,level=0):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent :
+                for tap in range(level):
+                    print('\t',end='')
+            print(each_item)
+
