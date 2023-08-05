c430287c3062fef2cd89a240c5570a1190b944b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:39 +0000

    Add package djangocms-text-ckeditor/2.1/djangocms-text-ckeditor-2.1.tar.gz

---
+++ packages/djangocms-text-ckeditor/djangocms-text-ckeditor-2.1.tar.gz/djangocms-text-ckeditor-2.1/djangocms_text_ckeditor/settings.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+#See http://docs.cksource.com/ckeditor_api/symbols/CKEDITOR.config.html for all settings
+
+CKEDITOR_SETTINGS = getattr(settings, 'CKEDITOR_SETTINGS', {
+    'language': '{{ language }}',
+    'toolbar': 'CMS',
+    'skin': 'moono',
+#    'stylesSet': [
+#        {'name': 'Custom Style', 'element': 'h3', 'styles': {'color': 'Blue'}}
+#    ],
+    'toolbarCanCollapse': False,
+})
+
+TEXT_SAVE_IMAGE_FUNCTION = getattr(settings, 'TEXT_SAVE_IMAGE_FUNCTION', 'djangocms_text_ckeditor.picture_save.create_picture_plugin')
+TEXT_ADDITIONAL_TAGS = getattr(settings, 'TEXT_ADDITIONAL_TAGS', ())
+TEXT_ADDITIONAL_ATTRIBUTES = getattr(settings, 'TEXT_ADDITIONAL_ATTRIBUTES', ())
