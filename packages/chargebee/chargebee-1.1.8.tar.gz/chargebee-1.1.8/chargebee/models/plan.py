21bd31bc1e7c191fd0619265649d8c6019af6747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:57 +0000

    Add package chargebee/1.1.8/chargebee-1.1.8.tar.gz

---
+++ packages/chargebee/chargebee-1.1.8.tar.gz/chargebee-1.1.8/chargebee/models/plan.py
@@ -0,0 +1,19 @@
+import json
+from chargebee.model import Model
+from chargebee import request
+from chargebee import APIError
+
+class Plan(Model):
+
+    fields = ["id", "name", "invoice_name", "price", "period", "period_unit", "trial_period", \
+    "trial_period_unit", "free_quantity", "setup_cost", "downgrade_penalty", "status", "archived_at", \
+    "billing_cycles", "redirect_url"]
+
+
+    @staticmethod
+    def list(params=None, env=None):
+        return request.send('get', '/plans', params, env)
+
+    @staticmethod
+    def retrieve(id, env=None):
+        return request.send('get', '/plans/%s' % id, None, env)
