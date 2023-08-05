298ece1157e5d887ac38e18207824c632e1719f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:26 +0000

    Add package django-images/0.4.1/django-images-0.4.1.tar.gz

---
+++ packages/django-images/django-images-0.4.1.tar.gz/django-images-0.4.1/setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(name='django-images',
+      author='Mirumee Software',
+      author_email='hello@mirumee.com',
+      description='A database-driver thumbnailing solution for Django',
+      license='BSD',
+      version='0.4.1',
+      packages=find_packages(),
+      include_package_data=True,
+      classifiers=CLASSIFIERS,
+      install_requires=['Django>=1.3', 'pillow>=1.7.8', 'South>=0.7.6'],
+      platforms=['any'],
+      zip_safe=False)
