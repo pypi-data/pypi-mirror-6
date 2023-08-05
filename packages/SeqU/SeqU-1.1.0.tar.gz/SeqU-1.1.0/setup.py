5f7a580cd6442ce5c5b0eee6e92f50b079977c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package SeqU/1.1.0/SeqU-1.1.0.tar.gz

---
+++ packages/SeqU/SeqU-1.1.0.tar.gz/SeqU-1.1.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014, Cameron White
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="SeqU",
+        version='1.1.0',
+        description="Extended implemenation of the Unix seq command",
+        license="BSD",
+        author="Cameron Brandon White",
+        author_email="cameronbwhite90@gmail.com",
+        url="https://github.com/cameronbwhite/SeqU",
+        py_modules = [
+            "alpha", "roman",
+        ],
+        scripts = [
+            "sequ",
+        ],
+        package_data={
+            'SeqU': ["LISENSE", "README.md"],
+        },
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "License :: OSI Approved :: BSD License",
+            "Intended Audience :: End Users/Desktop",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.3",
+            "Topic :: Utilities",
+        ],
+        include_package_data=True,
+    )
