3eb9306f4cef04084b5c56d53df1715c4d8f14de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:54 +0000

    Add package discotech/0.4.5/discotech-0.4.5.tar.gz

---
+++ packages/discotech/discotech-0.4.5.tar.gz/discotech-0.4.5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='discotech',
+      version='0.4.5',
+      description='python library to help working with social media providers',
+      long_description=readme(),
+      author='discoAPI Team',
+      author_email='groovy@discoapi.com',
+      url='http://github.com/discoapi/discotech',
+      packages=['discotech','discotech.APIProviders','discotech.discoAPI'],
+      license='GPL',
+      install_requires=[
+          'requests-oauthlib',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      include_package_data=True)
