0f1dc47442e449323cdc9b1d7a9fc54036485574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package django_backstage/0.0.6/django_backstage-0.0.6.tar.gz

---
+++ packages/django_backstage/django_backstage-0.0.6.tar.gz/django_backstage-0.0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_backstage',
+    version='0.0.6',
+    description='Django project and site deployment using uWSGI, nginx, etc.',
+    author='MiddleFork',
+    author_email='walker@mfgis.com',
+    packages=find_packages(),
+    install_requires=[
+        'django==1.5.5',
+        "setuptools_git >= 0.3",
+        "mezzanine",
+        "psycopg2", ],
+)
