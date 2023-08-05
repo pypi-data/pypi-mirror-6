f6896284ca5bad3bffd7fb88629ed4e744191a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:35 +0000

    Add package test_by_ibobwong/1.0.1/test_by_ibobwong-1.0.1.tar.gz

---
+++ packages/test_by_ibobwong/test_by_ibobwong-1.0.1.tar.gz/test_by_ibobwong-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'test_by_ibobwong',
+	version = '1.0.1',
+	py_modules = ['test_by_ibobwong'],
+	author = 'ibobwong',
+	author_email = 'ibobwong@icloud.com',
+	url = 'http://www.example.com',
+	description = 'test only',
+	)
+
