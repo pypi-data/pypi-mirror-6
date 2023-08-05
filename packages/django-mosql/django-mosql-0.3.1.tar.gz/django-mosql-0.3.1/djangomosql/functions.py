ccdcb2cb8d1cc86f7a9da3f910023271be1538d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:31 +0000

    Add package django-mosql/0.3.1/django-mosql-0.3.1.tar.gz

---
+++ packages/django-mosql/django-mosql-0.3.1.tar.gz/django-mosql-0.3.1/djangomosql/functions.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+__all__ = [
+    'LazyFunction', 'Avg', 'Count', 'Min', 'Max', 'Stddev', 'Sum', 'Variance'
+]
+
+from mosql import func as _
+
+
+class LazyFunction(object):
+    def __init__(self, *args):
+        self._args = args
+
+    def resolve(self):
+        return self.function(*self._args)
+
+
+class Avg(LazyFunction):
+    function = staticmethod(_.avg)
+
+
+class Count(LazyFunction):
+    function = staticmethod(_.count)
+
+
+class Min(LazyFunction):
+    function = staticmethod(_.min)
+
+
+class Max(LazyFunction):
+    function = staticmethod(_.max)
+
+
+class Stddev(LazyFunction):
+    function = staticmethod(_.stddev)
+
+
+class Sum(LazyFunction):
+    function = staticmethod(_.sum)
+
+
+class Variance(LazyFunction):
+    function = staticmethod(_.variance)
