093d9031be8cd55e0281247946f8761b0d08b1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:24 +0000

    Add package toolz/0.5.1/toolz-0.5.1.tar.gz

---
+++ packages/toolz/toolz-0.5.1.tar.gz/toolz-0.5.1/toolz/dicttoolz/core.py
@@ -0,0 +1,134 @@
+def merge(*dicts):
+    """ Merge a collection of dictionaries
+
+    >>> merge({1: 'one'}, {2: 'two'})
+    {1: 'one', 2: 'two'}
+
+    Later dictionaries have precedence
+
+    >>> merge({1: 2, 3: 4}, {3: 3, 4: 4})
+    {1: 2, 3: 3, 4: 4}
+
+    See Also:
+        merge_with
+    """
+    if len(dicts) == 1 and not isinstance(dicts[0], dict):
+        dicts = dicts[0]
+
+    rv = dict()
+    for d in dicts:
+        rv.update(d)
+    return rv
+
+
+def merge_with(func, *dicts):
+    """ Merge dictionaries and apply function to combined values
+
+    A key may occur in more than one dict, and all values mapped from the key
+    will be passed to the function as a list, such as func([val1, val2, ...]).
+
+    >>> merge_with(sum, {1: 1, 2: 2}, {1: 10, 2: 20})
+    {1: 11, 2: 22}
+
+    >>> merge_with(first, {1: 1, 2: 2}, {2: 20, 3: 30})  # doctest: +SKIP
+    {1: 1, 2: 2, 3: 30}
+
+    See Also:
+        merge
+    """
+    if len(dicts) == 1 and not isinstance(dicts[0], dict):
+        dicts = dicts[0]
+
+    result = dict()
+    for d in dicts:
+        for k, v in d.items():
+            try:
+                result[k].append(v)
+            except:
+                result[k] = [v]
+    return dict((k, func(v)) for k, v in result.items())
+
+
+def valmap(func, d):
+    """ Apply function to values of dictionary
+
+    >>> bills = {"Alice": [20, 15, 30], "Bob": [10, 35]}
+    >>> valmap(sum, bills)  # doctest: +SKIP
+    {'Alice': 65, 'Bob': 45}
+
+    See Also:
+        keymap
+    """
+    return dict(zip(d.keys(), map(func, d.values())))
+
+
+def keymap(func, d):
+    """ Apply function to keys of dictionary
+
+    >>> bills = {"Alice": [20, 15, 30], "Bob": [10, 35]}
+    >>> keymap(str.lower, bills)  # doctest: +SKIP
+    {'alice': [20, 15, 30], 'bob': [10, 35]}
+
+    See Also:
+        valmap
+    """
+    return dict(zip(map(func, d.keys()), d.values()))
+
+
+def assoc(d, key, value):
+    """
+    Return a new dict with new key value pair
+
+    New dict has d[key] set to value. Does not modify the initial dictionary.
+
+    >>> assoc({'x': 1}, 'x', 2)
+    {'x': 2}
+    >>> assoc({'x': 1}, 'y', 3)   # doctest: +SKIP
+    {'x': 1, 'y': 3}
+    """
+    d = d.copy()
+    d[key] = value
+    return d
+
+
+def update_in(d, keys, func, default=None):
+    """ Update value in a (potentially) nested dictionary
+
+    inputs:
+    d - dictionary on which to operate
+    keys - list or tuple giving the location of the value to be changed in d
+    func - function to operate on that value
+
+    If keys == [k0,..,kX] and d[k0]..[kX] == v, update_in returns a copy of the
+    original dictionary with v replaced by func(v), but does not mutate the
+    original dictionary.
+
+    If k0 is not a key in d, update_in creates nested dictionaries to the depth
+    specified by the keys, with the innermost value set to func(default).
+
+    >>> inc = lambda x: x + 1
+    >>> update_in({'a': 0}, ['a'], inc)
+    {'a': 1}
+
+    >>> transaction = {'name': 'Alice',
+    ...                'purchase': {'items': ['Apple', 'Orange'],
+    ...                             'costs': [0.50, 1.25]},
+    ...                'credit card': '5555-1234-1234-1234'}
+    >>> update_in(transaction, ['purchase', 'costs'], sum) # doctest: +SKIP
+    {'credit card': '5555-1234-1234-1234',
+     'name': 'Alice',
+     'purchase': {'costs': 1.75, 'items': ['Apple', 'Orange']}}
+
+    >>> # updating a value when k0 is not in d
+    >>> update_in({}, [1, 2, 3], str, default="bar")
+    {1: {2: {3: 'bar'}}}
+    >>> update_in({1: 'foo'}, [2, 3, 4], inc, 0)
+    {1: 'foo', 2: {3: {4: 1}}}
+    """
+    assert len(keys) > 0
+    k, ks = keys[0], keys[1:]
+    if ks:
+        return assoc(d, k, update_in(d.get(k, {}), ks, func, default))
+    else:
+        innermost = func(d.get(k)) if (k in d) else func(default)
+        return assoc(d, k, innermost)
---
+++ packages/toolz/toolz-0.5.1.tar.gz/toolz-0.5.1/toolz/functoolz/core.py
@@ -0,0 +1,327 @@
+from functools import reduce, partial
+import inspect
+import operator
+from toolz.utils import no_default
+
+
+def identity(x):
+    return x
+
+
+def thread_first(val, *forms):
+    """ Thread value through a sequence of functions/forms
+
+    >>> def double(x): return 2*x
+    >>> def inc(x):    return x + 1
+    >>> thread_first(1, inc, double)
+    4
+
+    If the function expects more than one input you can specify those inputs
+    in a tuple.  The value is used as the first input.
+
+    >>> def add(x, y): return x + y
+    >>> def pow(x, y): return x**y
+    >>> thread_first(1, (add, 4), (pow, 2))  # pow(add(1, 4), 2)
+    25
+
+    So in general
+        thread_first(x, f, (g, y, z))
+    expands to
+        g(f(x), y, z)
+
+    See Also:
+        thread_last
+    """
+    def evalform_front(val, form):
+        if callable(form):
+            return form(val)
+        if isinstance(form, tuple):
+            func, args = form[0], form[1:]
+            args = (val,) + args
+            return func(*args)
+    return reduce(evalform_front, forms, val)
+
+
+def thread_last(val, *forms):
+    """ Thread value through a sequence of functions/forms
+
+    >>> def double(x): return 2*x
+    >>> def inc(x):    return x + 1
+    >>> thread_last(1, inc, double)
+    4
+
+    If the function expects more than one input you can specify those inputs
+    in a tuple.  The value is used as the last input.
+
+    >>> def add(x, y): return x + y
+    >>> def pow(x, y): return x**y
+    >>> thread_last(1, (add, 4), (pow, 2))  # pow(2, add(4, 1))
+    32
+
+    So in general
+        thread_last(x, f, (g, y, z))
+    expands to
+        g(y, z, f(x))
+
+    >>> def iseven(x):
+    ...     return x % 2 == 0
+    >>> list(thread_last([1, 2, 3], (map, inc), (filter, iseven)))
+    [2, 4]
+
+    See Also:
+        thread_first
+    """
+    def evalform_back(val, form):
+        if callable(form):
+            return form(val)
+        if isinstance(form, tuple):
+            func, args = form[0], form[1:]
+            args = args + (val,)
+            return func(*args)
+    return reduce(evalform_back, forms, val)
+
+
+def memoize(func, cache=None):
+    """ Cache a function's result for speedy future evaluation
+
+    Considerations:
+        Trades memory for speed.
+        Only use on pure functions.
+
+    >>> def add(x, y):  return x + y
+    >>> add = memoize(add)
+
+    Or use as a decorator
+
+    >>> @memoize
+    ... def add(x, y):
+    ...     return x + y
+    """
+    if cache is None:
+        cache = {}
+
+    try:
+        spec = inspect.getargspec(func)
+        if spec and not spec.keywords and not spec.defaults:
+            may_have_kwargs = False
+        else:
+            may_have_kwargs = True
+    except TypeError:
+        may_have_kwargs = True
+
+    def memof(*args, **kwargs):
+        try:
+            if may_have_kwargs:
+                key = (args, frozenset(kwargs.items()))
+            else:
+                key = args
+            in_cache = key in cache
+        except TypeError:
+            raise TypeError("Arguments to memoized function must be hashable")
+
+        if in_cache:
+            return cache[key]
+        else:
+            result = func(*args, **kwargs)
+            cache[key] = result
+            return result
+
+    try:
+        memof.__name__ = func.__name__
+    except AttributeError:
+        pass
+    memof.__doc__ = func.__doc__
+    return memof
+
+
+def _num_required_args(func):
+    """ Number of args for func
+
+    >>> def foo(a, b, c=None):
+    ...     return a + b + c
+
+    >>> _num_required_args(foo)
+    2
+
+    >>> def bar(*args):
+    ...     return sum(args)
+
+    >>> print(_num_required_args(bar))
+    None
+    """
+    try:
+        spec = inspect.getargspec(func)
+        if spec.varargs:
+            return None
+        num_defaults = len(spec.defaults) if spec.defaults else 0
+        return len(spec.args) - num_defaults
+    except TypeError:
+        return None
+
+
+class curry(object):
+    """ Curry a callable function
+
+    Enables partial application of arguments through calling a function with an
+    incomplete set of arguments.
+
+    >>> def mul(x, y):
+    ...     return x * y
+    >>> mul = curry(mul)
+
+    >>> double = mul(2)
+    >>> double(10)
+    20
+
+    Also supports keyword arguments
+
+    >>> @curry                  # Can use curry as a decorator
+    ... def f(x, y, a=10):
+    ...     return a * (x + y)
+
+    >>> add = f(a=1)
+    >>> add(2, 3)
+    5
+
+    See Also:
+        toolz.curried - namespace of curried functions
+                        http://toolz.readthedocs.org/en/latest/curry.html
+    """
+    def __init__(self, func, *args, **kwargs):
+        if not callable(func):
+            raise TypeError("Input must be callable")
+
+        self.func = func
+        self.args = args
+        self.keywords = kwargs if kwargs else None
+        self.__doc__ = self.func.__doc__
+        try:
+            self.func_name = self.func.func_name
+        except AttributeError:
+            pass
+
+    def __str__(self):
+        return str(self.func)
+
+    def __repr__(self):
+        return repr(self.func)
+
+    def __call__(self, *args, **_kwargs):
+        args = self.args + args
+        if _kwargs:
+            kwargs = {}
+            if self.keywords:
+                kwargs.update(self.keywords)
+            kwargs.update(_kwargs)
+        elif self.keywords:
+            kwargs = self.keywords
+        else:
+            kwargs = {}
+
+        try:
+            return self.func(*args, **kwargs)
+        except TypeError as e:
+            required_args = _num_required_args(self.func)
+
+            # If there was a genuine TypeError
+            if required_args is not None and len(args) >= required_args:
+                raise e
+
+            # If we only need one more argument
+            if (required_args is not None and required_args - len(args) == 1):
+                if kwargs:
+                    return partial(self.func, *args, **kwargs)
+                else:
+                    return partial(self.func, *args)
+
+            return curry(self.func, *args, **kwargs)
+
+
+class Compose(object):
+    """ A composition of functions
+
+    See Also:
+        compose
+    """
+    __slots__ = ['funcs']
+
+    def __init__(self, *funcs):
+        self.funcs = funcs
+
+    def __call__(self, *args, **kwargs):
+        fns = list(reversed(self.funcs))
+        ret = fns[0](*args, **kwargs)
+        for f in fns[1:]:
+            ret = f(ret)
+        return ret
+
+    def __getstate__(self):
+        return self.funcs
+
+    def __setstate__(self, state):
+        self.funcs = tuple(state)
+
+
+def compose(*funcs):
+    """ Compose functions to operate in series.
+
+    Returns a function that applies other functions in sequence.
+
+    Functions are applied from right to left so that
+    ``compose(f, g, h)(x, y)`` is the same as ``f(g(h(x, y)))``.
+
+    If no arguments are provided, the identity function (f(x) = x) is returned.
+
+    >>> inc = lambda i: i + 1
+    >>> compose(str, inc)(3)
+    '4'
+
+    See Also:
+        pipe
+    """
+    if not funcs:
+        return identity
+    if len(funcs) == 1:
+        return funcs[0]
+    else:
+        return Compose(*funcs)
+
+
+def pipe(data, *funcs):
+    """ Pipe a value through a sequence of functions
+
+    I.e. ``pipe(data, f, g, h)`` is equivalent to ``h(g(f(data)))``
+
+    We think of the value as progressing through a pipe of several
+    transformations, much like pipes in UNIX
+
+    ``$ cat data | f | g | h``
+
+    >>> double = lambda i: 2 * i
+    >>> pipe(3, double, str)
+    '6'
+
+    See Also:
+        compose
+        thread_first
+        thread_last
+    """
+    for func in funcs:
+        data = func(data)
+    return data
+
+
+def complement(func):
+    """ Convert a predicate function to its logical complement.
+
+    In other words, return a function that, for inputs that normally
+    yield True, yields False, and vice-versa.
+
+    >>> def iseven(n): return n % 2 == 0
+    >>> isodd = complement(iseven)
+    >>> iseven(2)
+    True
+    >>> isodd(2)
+    False
+    """
+    return compose(operator.not_, func)
