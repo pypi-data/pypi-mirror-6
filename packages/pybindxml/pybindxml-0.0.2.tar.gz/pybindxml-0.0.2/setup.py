e96ae789546ef79bb549d2c3e38d08ec0306e4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:09 +0000

    Add package pybindxml/0.0.2/pybindxml-0.0.2.tar.gz

---
+++ packages/pybindxml/pybindxml-0.0.2.tar.gz/pybindxml-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pybindxml',
+    version = '0.0.2',
+    description='Read ISC BIND\'s statistics XML for processing.',
+    author='Jeffrey Forman',
+    author_email='code@jeffreyforman.net',
+    license='MIT',
+    packages = ['pybindxml'],
+    url = 'https://github.com/jforman/pybindxml',
+)
