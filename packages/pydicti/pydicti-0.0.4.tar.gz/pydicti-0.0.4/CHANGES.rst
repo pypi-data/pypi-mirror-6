6fa06bca22a1460c34f46bc23336911bc8cac1f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:45 +0000

    Add package pydicti/0.0.4/pydicti-0.0.4.tar.gz

---
+++ packages/pydicti/pydicti-0.0.4.tar.gz/pydicti-0.0.4/CHANGES.rst
@@ -0,0 +1,31 @@
+CHANGELOG
+~~~~~~~~~
+
+0.0.4
+=====
+Date 01.02.2014
+
+- add coverage reports
+- use more extensive unit tests
+- add support for pickle
+
+
+0.0.3
+=====
+Date: 26.01.2014
+
+- add support for python26
+- make dependency on ``OrderedDict`` optional
+- migrate to setuptools in order to use testing commands
+- support `ordereddict.OrderedDict`_ as fallback
+
+.. _`ordereddict.OrderedDict`: https://pypi.python.org/pypi/ordereddict/1.1
+
+0.0.2
+=====
+Date: 29.12.2013
+
+- fix ``dicti.pop``
+- support ``deepcopy(dicti)``
+- make nosetest automatically execute the doctests
+
---
+++ packages/pydicti/pydicti-0.0.4.tar.gz/pydicti-0.0.4/setup.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+from setuptools import setup
+
+# see: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+# prepare long_description for PyPI:
+long_description = None
+try:
+    long_description = open('README.rst').read()
+    long_description += '\n' + open('CHANGES.rst').read()
+except IOError:
+    pass
+
+# Care for testing on python26:
+extras_require = {}
+try:
+    from collections import OrderedDict
+except ImportError:
+    extras_require['odicti'] = ['ordereddict']
+else:
+    extras_require['odicti'] = []
+
+setup(
+    name='pydicti',
+    version='0.0.4',
+    description='Case insensitive derivable dictionary',
+    long_description=long_description,
+    author='Thomas Gläßle',
+    author_email='t_glaessle@gmx.de',
+    url='https://github.com/coldfix/pydicti',
+    license='Public Domain',
+    py_modules=['pydicti'],
+    extras_require=extras_require,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+    ],
+)
+
