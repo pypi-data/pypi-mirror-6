237af879476f2e762bef3324c0ca543231ec2377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:56 +0000

    Add package django-booking/0.5/django-booking-0.5.tar.gz

---
+++ packages/django-booking/django-booking-0.5.tar.gz/django-booking-0.5/booking/admin.py
@@ -0,0 +1,24 @@
+"""Admin classes for the booking app."""
+from django.contrib import admin
+
+from simple_translation.admin import TranslationAdmin
+
+from . import models
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = [
+        'creation_date', 'booking_status', 'booking_id', 'user', 'email',
+        'session', 'date_from', 'date_until',
+    ]
+
+
+class BookingItemAdmin(admin.ModelAdmin):
+    list_display = ['booking', 'booked_item', 'quantity', 'persons']
+
+
+admin.site.register(models.Booking, BookingAdmin)
+admin.site.register(models.BookingError)
+admin.site.register(models.BookingItem, BookingItemAdmin)
+admin.site.register(models.BookingStatus, TranslationAdmin)
+admin.site.register(models.ExtraPersonInfo)
---
+++ packages/django-booking/django-booking-0.5.tar.gz/django-booking-0.5/booking/tests/factories.py
@@ -0,0 +1,60 @@
+"""Factories for the booking app."""
+import factory
+
+from django_libs.tests.factories import UserFactory
+from django_libs.tests.factories import SimpleTranslationMixin
+
+from .. import models
+
+
+class BookingStatusFactory(SimpleTranslationMixin, factory.DjangoModelFactory):
+    """Factory for the ``BookingStatus`` model."""
+    FACTORY_FOR = models.BookingStatus
+
+    slug = factory.Sequence(lambda x: 'status-{}'.format(x))
+
+    @staticmethod
+    def _get_translation_factory_and_field():
+        return (BookingStatusTranslationFactory, 'status')
+
+
+class BookingStatusTranslationFactory(factory.DjangoModelFactory):
+    """Factory for ``BookingStatusTranslation`` objects."""
+    FACTORY_FOR = models.BookingStatusTranslation
+
+    name = factory.Sequence(lambda x: 'Status {}'.format(x))
+    status = factory.SubFactory(BookingStatusFactory)
+    language = 'en'
+
+
+class BookingFactory(factory.DjangoModelFactory):
+    """Factory for the ``Booking`` model."""
+    FACTORY_FOR = models.Booking
+
+    forename = 'Foo'
+    surname = 'Bar'
+    booking_status = factory.SubFactory(BookingStatusFactory)
+
+
+class BookingItemFactory(factory.DjangoModelFactory):
+    """Factory for the ``BookingItem`` model."""
+    FACTORY_FOR = models.BookingItem
+
+    booking = factory.SubFactory(BookingFactory)
+    booked_item = factory.SubFactory(UserFactory)
+
+
+class BookingErrorFactory(factory.DjangoModelFactory):
+    """Factory for the ``BookingError`` model."""
+    FACTORY_FOR = models.BookingError
+
+    booking = factory.SubFactory(BookingFactory)
+
+
+class ExtraPersonInfoFactory(factory.DjangoModelFactory):
+    """Factory for the ``ExtraPersonInfo`` model."""
+    FACTORY_FOR = models.ExtraPersonInfo
+
+    booking = factory.SubFactory(BookingFactory)
+    forename = 'Foo'
+    surname = 'Bar'
