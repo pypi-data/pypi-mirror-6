b212c9a7697a19c51e51ca26f8b8bf27d0bf6ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package moviepy/0.2.1.6.91/moviepy-0.2.1.6.91.tar.gz

---
+++ packages/moviepy/moviepy-0.2.1.6.91.tar.gz/moviepy-0.2.1.6.91/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(name='moviepy',
+      version='0.2.1.6.91',
+      author='Zulko 2013',
+    description='Module for script-based video editing',
+    long_description=open('README.rst').read(),
+    license='LICENSE.txt',
+    keywords="movie editing film mixing script-based",
+    packages= find_packages(exclude='docs'),
+    requires=['numpy', 'decorator'])
