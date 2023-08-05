01885ace50c9913a70facd5a3afccce2f825f637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:43 +0000

    Add package honcho/0.5.0/honcho-0.5.0.tar.gz

---
+++ packages/honcho/honcho-0.5.0.tar.gz/honcho-0.5.0/setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+from honcho import __version__
+
+requirements = []
+
+if sys.version_info[:2] < (2, 7):
+    requirements.append('argparse')
+    requirements.append('ordereddict')
+
+HERE = os.path.dirname(__file__)
+try:
+    long_description = open(os.path.join(HERE, 'README.rst')).read()
+except:
+    long_description = None
+
+setup(
+    name='honcho',
+    version=__version__,
+    packages=find_packages(exclude=['test*']),
+    include_package_data=True,
+
+    # metadata for upload to PyPI
+    author='Nick Stenning',
+    author_email='nick@whiteink.com',
+    url='https://github.com/nickstenning/honcho',
+    description='Honcho: a python clone of Foreman. For managing Procfile-based applications.',
+    long_description=long_description,
+    license='MIT',
+    keywords='sysadmin process procfile',
+
+    install_requires=requirements,
+    extras_require={
+        'export': ['jinja2>=2.7,<2.8'],
+    },
+    entry_points={
+        'console_scripts': [
+            'honcho=honcho.command:main'
+        ]
+    }
+)
