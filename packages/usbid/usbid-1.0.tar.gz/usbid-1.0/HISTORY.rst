49bc55669feeee724dd58c81564f4dd4ba91cf2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:18 +0000

    Add package usbid/1.0/usbid-1.0.tar.gz

---
+++ packages/usbid/usbid-1.0.tar.gz/usbid-1.0/HISTORY.rst
@@ -0,0 +1,8 @@
+
+History
+=======
+
+1.0
+---
+
+- initial release [benniboy, 2013-12-19]
---
+++ packages/usbid/usbid-1.0.tar.gz/usbid-1.0/src/usbid/script.py
@@ -0,0 +1,14 @@
+import os
+
+def run():
+    return
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
