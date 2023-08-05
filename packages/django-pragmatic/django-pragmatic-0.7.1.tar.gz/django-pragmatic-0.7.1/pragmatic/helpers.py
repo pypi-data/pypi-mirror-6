43f8725a73f809a321b5a5f056a0d7917a83ba23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:19 +0000

    Add package django-pragmatic/0.7.1/django-pragmatic-0.7.1.tar.gz

---
+++ packages/django-pragmatic/django-pragmatic-0.7.1.tar.gz/django-pragmatic-0.7.1/pragmatic/helpers.py
@@ -0,0 +1,20 @@
+import hashlib
+import random
+from django.utils.timezone import now
+
+
+def base64_to_file(content, filepath):
+    fh = open(filepath, "wb")
+    fh.write(content.decode('base64'))
+    fh.close()
+
+
+def round_to_n_decimal_places(value, n):
+    power = 10 ** n
+    return float(int(value * power)) / power
+
+
+def generate_hash(length=5):
+    salt = hashlib.sha1(str(random.random())).hexdigest()
+    pepper = str(now())
+    return hashlib.sha1(salt + pepper).hexdigest()[:length]
