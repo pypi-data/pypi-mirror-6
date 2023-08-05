fbbfbf93fabb51301df1cfcb5a0a0f7218b67826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package gists.cli/0.25/gists.cli-0.25.tar.gz

---
+++ packages/gists.cli/gists.cli-0.25.tar.gz/gists.cli-0.25/gists_cli/log.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys
+from datetime import datetime
+
+#-------------------------------------------
+
+debugEnabled = False
+
+def setDebug (debug):
+  global debugEnabled
+  debugEnabled = debug
+
+def comment (obj):
+  print '>> ' + str(obj)
+
+def debug (obj):
+  if debugEnabled:
+    print '[' + str(datetime.now()) + ']: ' + str(obj)
+
+def error (obj):
+  print 'ERROR: ' + str(obj)
+
