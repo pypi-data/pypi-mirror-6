e90e5bd8c1f942f80a671f187c0194fa7e5f618d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:56 +0000

    Add package pyditz/0.5.2/pyditz-0.5.2.tar.gz

---
+++ packages/pyditz/pyditz-0.5.2.tar.gz/pyditz-0.5.2/ditz/version.py
@@ -0,0 +1,23 @@
+"""
+Version information.
+"""
+
+__title__ = "pyditz"
+__author__ = 'Glenn Hutchings'
+__email__ = 'zondo42@gmail.com'
+__version__ = '0.5.2'
+__license__ = 'GPL v2 or later'
+__url__ = "http://pypi.python.org/pypi/pyditz"
+
+__classifiers__ = """
+Development Status :: 4 - Beta
+Environment :: Console
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)
+Natural Language :: English
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX :: Linux
+Operating System :: Unix
+Programming Language :: Python :: 2.7
+Topic :: Software Development :: Bug Tracking
+"""
