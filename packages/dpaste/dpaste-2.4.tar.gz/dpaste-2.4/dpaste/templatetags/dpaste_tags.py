366d6f185f37a808fb21cc97c77146c5fdaba880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package dpaste/2.4/dpaste-2.4.tar.gz

---
+++ packages/dpaste/dpaste-2.4.tar.gz/dpaste-2.4/dpaste/templatetags/dpaste_tags.py
@@ -0,0 +1,13 @@
+from django.template import Library
+from dpaste.highlight import pygmentize
+
+register = Library()
+
+@register.filter
+def in_list(value, arg):
+    return value in arg
+
+@register.filter
+def highlight(snippet):
+    h = pygmentize(snippet.content, snippet.lexer)
+    return h.splitlines()
