117c8f65ad71abebbf2ab54881f277a844d6f1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package hubugs/0.17.5/hubugs-0.17.5-py27-none-any.whl

---
+++ packages/hubugs/hubugs-0.17.5-py27-none-any.whl/hubugs/models.py
@@ -0,0 +1,105 @@
+#
+# coding=utf-8
+"""models - GitHub models for hubugs"""
+# Copyright © 2010, 2011, 2012, 2013  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import collections
+import datetime
+
+# We used to use tight, explicit bindings for API objects but the desire to
+# support Python 3 and the lack of a usable binding library has made it
+# necessary to use the loose dynamic binding implemented below.
+#
+# I fully expect to return to strict binding in the future, once either a tool
+# becomes available or I free up a little more itch-scratching time
+
+
+def object_hook(d, name='unknown'):
+    """JSON object hook to create dot-accessible objects.
+
+    :param dict d: Dictionary to operate on
+    :param str name: Fallback name, if dict has no ``type`` key
+
+    """
+    # FIXME: Dump _links attributes for the time being
+    if '_links' in d:
+        d.pop('_links')
+    for k, v in d.items():
+        try:
+            d[k] = datetime.datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ')
+        except (TypeError, ValueError):
+            pass
+    return collections.namedtuple(d.get('type', name), d.keys())(**d)
+
+
+def _v2_conv_timestamp(s):
+    """Parse API v2 style timestamps.
+
+    :param str s: Timestamp to parse
+
+    """
+    zone = datetime.timedelta(hours=int(s[-5:-3]))
+    stamp = datetime.datetime.strptime(s[:-6], '%Y-%m-%dT%H:%M:%S')
+    return (stamp - zone).isoformat() + 'Z'
+
+
+def from_search(obj):
+    """Support legacy API search results as API v3 issues(-ish).
+
+    This is an awful hack to workaround the lack of search support in API
+    v3.  It needs to be removed at the first possible opportunity.
+
+    :rtype: ``Issue``
+    :returns: API v2 issue mangled to look like a API v3 result
+
+    """
+
+    avatar_url = ('https://secure.gravatar.com/avatar/%s?d='
+                  'https://a248.e.akamai.net/assets.github.com%%2F'
+                  'images%%2Fgravatars%%2Fgravatar-140.png')
+    label_url = 'https://api.github.com/repos/%s/%s/labels/%s'
+
+    owner, project = obj.html_url.split('/')[3:5]
+
+    d = obj.__dict__
+    d['id'] = '<from search>'
+    d['user'] = {
+        'avatar_url': avatar_url % obj.gravatar_id,
+        'gravatar_id': obj.gravatar_id,
+        'login': obj.user,
+        'url': 'https://api.github.com/users/%s' % obj.user,
+    }
+    if 'closed_by' in obj._fields:
+        d['closed_by'] = {
+            'avatar_url': avatar_url % obj.closed_by,
+            'gravatar_id': obj.gravatar_id,
+            'login': obj.user,
+            'url': 'https://api.github.com/users/%s' % obj.user,
+        }
+        d['closed_at'] = _v2_conv_timestamp(obj.closed_at)
+    d['created_at'] = _v2_conv_timestamp(obj.created_at)
+    d['updated_at'] = _v2_conv_timestamp(obj.updated_at)
+    if 'labels' in obj._fields:
+        labels = []
+        for name in obj.labels:
+            labels.append({
+                'color': '000000',
+                'name': name,
+                'url': label_url % (owner, project, name)
+            })
+        d['labels'] = labels
+    return object_hook(d, 'issue')
---
+++ packages/hubugs/hubugs-0.17.5-py27-none-any.whl/hubugs/template.py
@@ -0,0 +1,260 @@
+#
+# coding=utf-8
+"""template - Template utilities for hubugs"""
+# Copyright © 2010, 2011, 2012, 2013  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import datetime
+import operator
+import os
+import sys
+import subprocess
+import tempfile
+
+import html2text as html2
+import jinja2
+
+from pygments import highlight as pyg_highlight
+from pygments.formatters import get_formatter_by_name
+from pygments.lexers import get_lexer_by_name
+
+from . import utils
+from .i18n import _
+
+
+if sys.platform == 'darwin':
+    _USER_DATA_DIR = os.path.expanduser('~/Library/Application Support')
+else:
+    _USER_DATA_DIR = os.path.join(os.environ.get('HOME', '/'), '.local')
+
+USER_DATA_DIR = os.environ.get('XDG_DATA_HOME', _USER_DATA_DIR)
+SYSTEM_DATA_DIR = os.environ.get('XDG_DATA_DIRS',
+                                 '/usr/local/share/:/usr/share/').split(':')
+PKG_DATA_DIRS = [os.path.join(USER_DATA_DIR, 'hubugs', 'templates'), ]
+for directory in SYSTEM_DATA_DIR:
+    PKG_DATA_DIRS.append(os.path.join(directory, 'hubugs', 'templates'))
+
+ENV = jinja2.Environment(loader=jinja2.ChoiceLoader(
+    [jinja2.FileSystemLoader(s) for s in PKG_DATA_DIRS]))
+ENV.loader.loaders.append(jinja2.PackageLoader('hubugs', 'templates'))
+
+
+class EmptyMessageError(ValueError):
+
+    """Error to raise when the user provides an empty message."""
+
+    pass
+
+
+def get_template(group, name):
+    """Fetch a Jinja template instance.
+
+    :param str group: Template group identifier
+    :param str name: Template name
+    :rtype: ``jinja2.environment.Template``
+    :return: Jinja template instance
+
+    """
+    template_set = utils.get_git_config_val('hubugs.templates', 'default')
+    return ENV.get_template('%s/%s/%s' % (template_set, group, name))
+
+
+def jinja_filter(func):
+    """Simple decorator to add a new filter to Jinja environment.
+
+    :param func func: Function to add to Jinja environment
+    :rtype: ``func``
+    :returns: Unmodified function
+
+    """
+    ENV.filters[func.__name__] = func
+
+    return func
+
+
+@jinja_filter
+def colourise(text, formatting):
+    """Colourise text using blessings.
+
+    Returns text untouched if colour output is not enabled
+
+    :see: ``blessings``
+
+    :param str text: Text to colourise
+    :param str formatting: Formatting to apply to text
+    :rtype: ``str``
+    :return: Colourised text, when possible
+
+    """
+    return getattr(utils.T, formatting.replace(' ', '_'))(text)
+# American spelling, just for Brandon Cady ;)
+ENV.filters['colorize'] = ENV.filters['colourise']
+
+
+@jinja_filter
+def highlight(text, lexer='diff', formatter='terminal'):
+    """Highlight text with pygments.
+
+    Returns text untouched if colour output is not enabled
+
+    :param str text: Text to highlight
+    :param str lexer: Jinja lexer to use
+    :param str formatter: Jinja formatter to use
+    :rtype: ``str``
+    :return: Syntax highlighted output, when possible
+
+    """
+    if utils.T.is_a_tty:
+        lexer = get_lexer_by_name(lexer)
+        formatter = get_formatter_by_name(formatter)
+        return pyg_highlight(text, lexer, formatter)
+    else:
+        return text
+
+
+@jinja_filter
+def html2text(html, width=80, ascii_replacements=False):
+    """HTML to plain text renderer.
+
+    :param str text: Text to process
+    :param int width: Paragraph width
+    :param bool ascii_replacements: Use psuedo-ascii replacements for Unicode
+    :rtype: ``str``
+    :return: Rendered text
+
+    """
+    html2.BODY_WIDTH = width
+    html2.UNICODE_SNOB = ascii_replacements
+    return html2.html2text(html).strip()
+
+
+@jinja_filter
+def relative_time(timestamp):
+    """Format a relative time.
+
+    Taken from bleeter_.  Duplication is evil, I know.
+
+    :param datetime.datetime timestamp: Event to generate relative timestamp
+        against
+    :rtype: ``str``
+    :return: Human readable date and time offset
+
+    .. _bleeter: http://jnrowe.github.com/bleeter/
+
+    """
+
+    numstr = '. a two three four five six seven eight nine ten'.split()
+
+    matches = [
+        60 * 60 * 24 * 365,
+        60 * 60 * 24 * 28,
+        60 * 60 * 24 * 7,
+        60 * 60 * 24,
+        60 * 60,
+        60,
+        1,
+    ]
+    match_names = ['year', 'month', 'week', 'day', 'hour', 'minute', 'second']
+
+    delta = datetime.datetime.utcnow() - timestamp
+    # Switch to delta.total_seconds, if 2.6 support is dropped
+    seconds = delta.days * 86400 + delta.seconds
+    for scale in matches:
+        i = seconds // scale
+        if i:
+            name = match_names[matches.index(scale)]
+            break
+
+    if i == 1 and name in ('year', 'month', 'week'):
+        result = 'last %s' % name
+    elif i == 1 and name == 'day':
+        result = 'yesterday'
+    elif i == 1 and name == 'hour':
+        result = 'about an hour ago'
+    else:
+        result = 'about %s %s%s ago' % (i if i > 10 else numstr[i], name,
+                                        's' if i > 1 else '')
+    return result
+
+
+def display_bugs(bugs, order, **extras):
+    """Display bugs to users.
+
+    :type bugs: ``list` of ``models.Issue``
+    :param bugs: Bugs to display
+    :param str order: Sorting order for displaying bugs
+    :param dict extras: Additional values to pass to templates
+    :rtype: ``str``
+    :return: Rendered template output
+
+    """
+    if not bugs:
+        return utils.success(_('No bugs found!'))
+
+    # Match ordering method to bug attribute
+    if order == 'updated':
+        attr = 'updated_at'
+    else:
+        attr = order
+
+    bugs = sorted(bugs, key=operator.attrgetter(attr))
+
+    # Default to 80 columns, when stdout is not a tty
+    columns = utils.T.width if utils.T.width else 80
+
+    template = get_template('view', 'list.txt')
+
+    max_id = max(i.number for i in bugs)
+    id_len = len(str(max_id))
+    spacer = ' ' * (id_len - 2)
+
+    return template.render(bugs=bugs, spacer=spacer, id_len=id_len,
+                           max_title=columns - id_len - 2, **extras)
+
+
+def edit_text(edit_type='default', data=None):
+    """Edit data with external editor.
+
+    :param str edit_type: Template to use in editor
+    :param dict data: Information to pass to template
+    :rtype: ``str``
+    :return: User supplied text
+    :raise EmptyMessageError: No message given
+    :raise EmptyMessageError: Message not edited
+
+    """
+    template = get_template('edit', '%s.mkd' % edit_type)
+
+    fd, name = tempfile.mkstemp(prefix='hubugs-', suffix='.mkd')
+    try:
+        with os.fdopen(fd, 'w') as f:
+            f.write(template.render(data if data else {}))
+
+        orig_mtime = os.path.getmtime(name)
+        subprocess.check_call(utils.get_editor() + [name, ])
+        new_mtime = os.path.getmtime(name)
+
+        text = ''.join(filter(lambda s: not s.startswith('#'),
+                              open(name).readlines())).strip()
+    finally:
+        os.unlink(name)
+
+    if not text:
+        raise EmptyMessageError(_('No message given'))
+    elif orig_mtime == new_mtime:
+        raise EmptyMessageError(_('Message not edited'))
+
+    return text.strip()
