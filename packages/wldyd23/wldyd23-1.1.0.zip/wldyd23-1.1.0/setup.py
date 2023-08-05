620ef408dcb509ca76a5fbde41aab246065e18d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:44 +0000

    Add package wldyd23/1.1.0/wldyd23-1.1.0.zip

---
+++ packages/wldyd23/wldyd23-1.1.0.zip/wldyd23-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'wldyd23', 
+        version      = '1.1.0', 
+       
+        py_modules   = ['jynester'],
+        author       = '',
+        author_email = 'dlawldyd23@naver.com',
+        url          = '',
+        description  = '',
+     )
