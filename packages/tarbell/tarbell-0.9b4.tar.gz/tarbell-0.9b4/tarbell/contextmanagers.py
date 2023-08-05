3dcb795be1acd94a94c1e9b2c123a028af7c903b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package tarbell/0.9b4/tarbell-0.9b4.tar.gz

---
+++ packages/tarbell/tarbell-0.9b4.tar.gz/tarbell-0.9b4/tarbell/contextmanagers.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+"""
+tarbell.cli
+~~~~~~~~~
+
+This module provides context managers for Tarbell projects.
+"""
+
+import os
+import sys
+
+from clint.textui import colored, puts
+
+from .app import TarbellSite
+from .settings import Settings
+from .utils import show_error, get_config_from_args, list_get
+from .configure import tarbell_configure
+
+from copy import copy
+
+class EnsureSettings():
+    """Ensure the user has a Tarbell configuration."""
+    def __init__(self, command, args):
+        self.command = command
+        self.path = get_config_from_args(args)
+
+    def __enter__(self):
+        if (os.path.isfile(self.path)):
+            settings = Settings(self.path)
+            # beta2 and older check
+            if settings.config.get('s3_buckets'):
+                puts(colored.red("--- Warning! ---\n"))
+                puts("Your configuration file is out of date. Amazon S3 publishing will not work.")
+                puts("Run {0} to update your Amazon S3 configuration.".format(
+                    colored.yellow('tarbell configure s3')
+                    ))
+                puts(colored.red("\n----------------\n"))
+                if self.command.name == "publish":
+                    show_error("publish called, exiting.")
+                    sys.exit(1)
+
+            return settings
+
+        else:
+            puts("\n{0}: {1}".format(
+                colored.red("Warning:"),
+                "No Tarbell configuration found, running {0}.".format(
+                    colored.green("tarbell configure")
+                )
+            ))
+            settings = tarbell_configure(self.args)
+            puts("\n\n Trying to run {0} again".format(
+                colored.yellow("tarbell {0}".format(self.args.get(0)))
+            ))
+            return settings
+
+    def __exit__(self, type, value, traceback):
+        # @TODO This isn't quite right, __enter__ does too much work.
+        pass
+
+
+class EnsureProject():
+    """Context manager to ensure the user is in a Tarbell site environment."""
+    def __init__(self, command, args):
+        self.command = command
+        self.args = args
+
+    def __enter__(self):
+        return self.ensure_site()
+
+    def __exit__(self, type, value, traceback):
+        pass
+
+    def ensure_site(self, path=None):
+        if not path:
+            path = os.getcwd()
+
+        if path is "/":
+            show_error(("The current directory is not part of a Tarbell "
+                        "project"))
+            sys.exit(1)
+
+        if not os.path.exists(os.path.join(path, 'tarbell_config.py')):
+            path = os.path.realpath(os.path.join(path, '..'))
+            return self.ensure_site(path)
+        else:
+            os.chdir(path)
+            site = TarbellSite(path)
+            return site
+
+# Lowercase aliases
+ensure_settings = EnsureSettings
+ensure_project = EnsureProject
---
+++ packages/tarbell/tarbell-0.9b4.tar.gz/tarbell-0.9b4/tarbell/docs/install.rst
@@ -0,0 +1,104 @@
+============
+Installation
+============
+
+Install Tarbell with `pip install tarbell`
+------------------------------------------
+
+::
+
+    pip install tarbell==0.9b4
+
+
+Configure Tarbell with `tarbell configure`
+------------------------------------------
+
+The `tarbell configure` command will set up your Tarbell settings::
+
+  tarbell configure
+
+Please consider setting up Google spreadsheet access for collaborative data editing and Amazon
+S3 settings for easy publishing.
+
+
+Configure Google spreadsheet access (optional)
+----------------------------------------------
+
+In order to allow Tarbell to create new Google Spreadsheets, you'll need to
+download a `client_secrets.json file
+<https://developers.google.com/api-client-library/python/guide/aaa_client_secrets>`_
+to access the Google Drive API. You can share this file with collaborators and
+within your organization, but do not share this file anywhere public.
+
+Log in to the `Google API Developer Console
+<https://code.google.com/apis/console/b/0/?noredirect>`_ and create a new project:
+
+.. image:: create_1.png
+   :width: 700px
+
+Now click the "Services" tab and enable Google Drive API.
+
+.. image:: create_2.png
+   :width: 700px
+
+Click the "API Access" tab to create a client ID:
+
+.. image:: create_3.png
+   :width: 700px
+
+Add some project details. These don't really matter:
+
+.. image:: create_4.png
+   :width: 700px
+
+This is the important screen. Select "installed app" and "other":
+
+.. image:: create_5.png
+   :width: 700px
+
+Whew! Now you can download the ``client_secrets.json`` file:
+
+.. image:: create_6.png
+   :width: 700px
+
+Copy `client_secrets.json` to `~/.tarbell/client_secrets.json` or specify the download
+location when running `tarbell configure`.
+
+The first time a Tarbell command needs access to a Google spreadsheet, you'll be prompted to
+authenticate::
+
+  Go to the following link in your browser:
+
+      https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&client_id=705475625983-bdm46bacl3v8hlt4dd9ufvgsmgg3jrug.apps.googleusercontent.com&access_type=offline
+
+  Enter verification code: 
+
+Follow the link:
+
+.. image:: create_7.png
+   :width: 700px
+
+You should receive a confirmation code:
+
+.. image:: create_8.png
+
+Enter it. If it works, you'll see:
+
+  Authentication successful.
+
+Now you can access and create Google spreadsheets to use with Tarbell projects.
+
+Configure Amazon S3
+-------------------
+
+Generate keys for your Amazon S3 account. Add them during the Amazon S3 section of installation.
+
+Set a default project path
+--------------------------
+
+This path will be used by `tarbell list` and `tarbell switch`.
+
+Set up project templates
+------------------------
+
+Work in progress.
