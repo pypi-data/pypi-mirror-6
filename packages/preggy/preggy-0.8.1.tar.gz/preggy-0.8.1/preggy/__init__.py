2583a64dc006b671ed66c845541f881a337481b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package preggy/0.8.1/preggy-0.8.1.tar.gz

---
+++ packages/preggy/preggy-0.8.1.tar.gz/preggy-0.8.1/preggy/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# preggy assertions
+# https://github.com/heynemann/preggy
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2013 Bernardo Heynemann heynemann@gmail.com
+
+# flake8: noqa
+
+from __future__ import absolute_import
+
+__version__ = '0.8.1'
+
+from preggy.core import (assertion, create_assertions, Expect as expect)
+from preggy.assertions import *
+
---
+++ packages/preggy/preggy-0.8.1.tar.gz/preggy-0.8.1/setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# preggy assertions
+# https://github.com/heynemann/preggy
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
+
+from setuptools import setup, find_packages
+from preggy import __version__
+
+test_requires = [
+    'nose',
+    'yanc',
+    'coverage',
+    'tox',
+]
+
+setup(
+    name='preggy',
+    version=__version__,
+    description='preggy is an assertion library extracted from PyVows',
+    long_description='''
+        preggy is an assertion library extracted from PyVows.
+
+        For more info, check out Preggy’s homepage: http://heynemann.github.io/preggy/
+
+        ''',
+    keywords='test testing assert assertion development',
+    author='Bernardo Heynemann',
+    author_email='heynemann@gmail.com',
+    contributor='Zearin',
+    contributor_email='zearin@gonk.net',
+    url='http://github.com/heynemann/preggy/',
+    download_url='http://heynemann.github.io/preggy/',
+
+    ### For future, when Python packaging gets its crap together. See:
+    ###   http://stackoverflow.com/questions/14459828/how-to-set-bug-tracker-url-in-setup-py-script
+    #bugtrack_url='http://github.com/heynemann/preggy/issues/',
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Testing'
+    ],
+
+    packages=find_packages(),
+
+    install_requires=['six', 'unidecode'],
+
+    extras_require={
+        'tests': test_requires,
+    },
+
+    entry_points={},
+)
