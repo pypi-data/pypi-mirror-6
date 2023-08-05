6cd4f969c3224f128d21b913c69c7369ada4f127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package telegraphy/0.1.2.2/telegraphy-0.1.2.2.tar.gz

---
+++ packages/telegraphy/telegraphy-0.1.2.2.tar.gz/telegraphy-0.1.2.2/setup.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'rb') as fp:
+    requirements = [req for req in fp.read().split() if req]
+
+# Filter does not work, workarround
+packages = [p for p in find_packages() if not 'demo_project' in p]
+
+setup(
+    name='telegraphy',
+    version='0.1.2.2',
+    description=('Telegraphy - Real Time Events For Django.'),
+    long_description=open('README.rst').read(),
+    package_data={'': ['requirements.txt']},
+    changelog='',
+    license='Apache License 2.0',
+    author='Nahuel Defossé',
+    author_email='nahuel.defosse@gmail.com',
+    url='http://telegraphy.machinalis.com',
+    platforms=('Any'),
+    install_requires = ['Django', 'Twisted', 'autobahn'],
+    packages = packages,
+    zip_safe = False,
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    #
+    classifiers = ["License :: OSI Approved :: Apache Software License",
+                   "Development Status :: 3 - Alpha",
+                   "Environment :: Console",
+                   "Framework :: Django",
+                   "Framework :: Twisted",
+                   "Intended Audience :: Developers",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Internet",
+                   "Topic :: Software Development :: Libraries"],
+    keywords = 'django real-time-web websocket wamp'
+)
