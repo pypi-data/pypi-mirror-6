0ff26f13f79ad175fbbb79f238ca5fac9ba04236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:05 +0000

    Add package hem/0.4.1/hem-0.4.1.tar.gz

---
+++ packages/hem/hem-0.4.1.tar.gz/hem-0.4.1/hem/db.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from hem.interfaces import IDBSession
+
+
+def get_session(request):
+    if hasattr(request, 'db_session'):
+        return request.db_session
+    elif request.registry.queryUtility(IDBSession):
+        session = request.registry.getUtility(IDBSession)
+    else:
+        raise Exception(
+            'You need to register a DBSession to IDBSession interface for'
+            'horus'
+        )
+
+    # Doing this since scoped session is callable
+    # but we don't want to have sqlalchemy as a
+    # dependency of hem
+    is_scoped_session = hasattr(session, 'query')
+
+    if callable(session) and not is_scoped_session:
+        return session(request)
+    else:
+        return session
---
+++ packages/hem/hem-0.4.1.tar.gz/hem-0.4.1/hem/text.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from unicodedata import normalize
+import hashlib
+import random
+import string
+from pyramid.compat import text_, text_type
+
+
+def generate_random_string(length=12):
+    """Generate a generic hash key for the user to use."""
+    m = hashlib.sha256()
+    word = ''
+    for i in range(length):
+        word += random.choice(string.ascii_letters)
+    m.update(word.encode('ascii'))
+    return text_type(m.hexdigest()[:length])
+
+
+def slugify(text,
+            encoding=None,
+            permitted_chars='abcdefghijklmnopqrstuvwxyz0123456789-'):
+    """Original code: https://gist.github.com/1428479"""
+    # TODO maybe default to utf8
+    text = text_(text, encoding or 'ascii', errors='ignore')
+    clean_text = text.strip().replace(' ', '-').lower()
+    while '--' in clean_text:
+        clean_text = clean_text.replace('--', '-')
+    ascii_text = normalize('NFKD', clean_text)
+    strict_text = map(lambda x: x if x in permitted_chars else '', ascii_text)
+    return ''.join(strict_text)
+
+
+def pluralize(singular):
+    """
+    Return plural form of given lowercase singular word (English only).
+    Based on ActiveState recipe http://code.activestate.com/recipes/413172/
+
+    >>> pluralize('')
+    ''
+    >>> pluralize('goose')
+    'geese'
+    >>> pluralize('dolly')
+    'dollies'
+    >>> pluralize('genius')
+    'genii'
+    >>> pluralize('jones')
+    'joneses'
+    >>> pluralize('pass')
+    'passes'
+    >>> pluralize('zero')
+    'zeros'
+    >>> pluralize('casino')
+    'casinos'
+    >>> pluralize('hero')
+    'heroes'
+    >>> pluralize('church')
+    'churches'
+    >>> pluralize('x')
+    'xs'
+    >>> pluralize('car')
+    'cars'
+
+    """
+    ABERRANT_PLURAL_MAP = {
+        'appendix': 'appendices',
+        'barracks': 'barracks',
+        'cactus': 'cacti',
+        'child': 'children',
+        'criterion': 'criteria',
+        'deer': 'deer',
+        'echo': 'echoes',
+        'elf': 'elves',
+        'embargo': 'embargoes',
+        'focus': 'foci',
+        'fungus': 'fungi',
+        'goose': 'geese',
+        'hero': 'heroes',
+        'hoof': 'hooves',
+        'index': 'indices',
+        'knife': 'knives',
+        'leaf': 'leaves',
+        'life': 'lives',
+        'man': 'men',
+        'mouse': 'mice',
+        'nucleus': 'nuclei',
+        'person': 'people',
+        'phenomenon': 'phenomena',
+        'potato': 'potatoes',
+        'self': 'selves',
+        'syllabus': 'syllabi',
+        'tomato': 'tomatoes',
+        'torpedo': 'torpedoes',
+        'veto': 'vetoes',
+        'woman': 'women',
+        }
+
+    VOWELS = set('aeiou')
+
+    if not singular:
+        return ''
+    plural = ABERRANT_PLURAL_MAP.get(singular)
+    if plural:
+        return plural
+    root = singular
+    try:
+        if singular[-1] == 'y' and singular[-2] not in VOWELS:
+            root = singular[:-1]
+            suffix = 'ies'
+        elif singular[-1] == 's':
+            if singular[-2] in VOWELS:
+                if singular[-3:] == 'ius':
+                    root = singular[:-2]
+                    suffix = 'i'
+                else:
+                    root = singular[:-1]
+                    suffix = 'ses'
+            else:
+                suffix = 'es'
+        elif singular[-2:] in ('ch', 'sh'):
+            suffix = 'es'
+        else:
+            suffix = 's'
+    except IndexError:
+        suffix = 's'
+    plural = root + suffix
+    return plural
