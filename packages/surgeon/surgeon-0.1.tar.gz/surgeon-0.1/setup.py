7e10a07bd3b4dc311a381b153e54486adbb358b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:44 +0000

    Add package surgeon/0.1/surgeon-0.1.tar.gz

---
+++ packages/surgeon/surgeon-0.1.tar.gz/surgeon-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'surgeon',
+    packages = ['surgeon'],
+    version = '0.1',
+    description = 'A random find/replace library',
+    author = 'Alexander Ivanov',
+    author_email = 'alehander42@gmail.com',
+    url = 'https://github.com/alehander42/python-surgeon',
+    keywords = ['search', 'replace', 'syntax'],
+    classifiers = [],
+)
