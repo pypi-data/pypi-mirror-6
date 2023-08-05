109614d93697fbc93fb25c801ba43c400d827315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package TheHikarinester/1.0.0/TheHikarinester-1.0.0.zip

---
+++ packages/TheHikarinester/TheHikarinester-1.0.0.zip/TheHikarinester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'TheHikarinester',
+    version = '1.0.0',
+    py_modules = ['TheHikarinester'],
+    author = 'hfpython',
+    author_email = 'kails_@naver.com',
+    url = 'http://blog.naver.com/kails_',
+    description = 'A simple printer of nested lists',
+    )
+    
