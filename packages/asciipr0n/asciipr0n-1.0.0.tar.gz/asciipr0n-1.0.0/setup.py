422f26e60b4c1c7a4147e1bc98da059c9b96d2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:54 +0000

    Add package asciipr0n/1.0.0/asciipr0n-1.0.0.tar.gz

---
+++ packages/asciipr0n/asciipr0n-1.0.0.tar.gz/asciipr0n-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import glob
+
+setuptools.setup(
+    name='asciipr0n',
+    version='1.0.0',
+    description='Display NSFW images in your logfiles',
+    author='Rob Britton',
+    author_email='rob@robbritton.com',
+    keywords=[],
+    packages=['asciipr0n'],
+    package_data={"asciipr0n": glob.glob("asciipr0n/*.txt")},
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Logging"
+    ],
+    long_description="""\
+    Silly function for displaying NSFW in your logfiles.
+""",
+)
