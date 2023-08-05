fa81316ad321d96a83c895305437c9808caa1d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:00 +0000

    Add package bambu-mail/0.2.1/bambu-mail-0.2.1.tar.gz

---
+++ packages/bambu-mail/bambu-mail-0.2.1.tar.gz/bambu-mail-0.2.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+	name = 'bambu-mail',
+	version = '0.2.1',
+	description = 'A shortcut function for sending template-based emails in HTML and plain-text format',
+	author = 'Steadman',
+	author_email = 'mark@steadman.io',
+	url = 'http://pypi.python.org/pypi/bambu-mail',
+	install_requires = [
+		'Django>=1.4',
+		'bambu-markup'
+	],
+	namespace_packages = ['bambu'],
+	packages = [
+		'bambu.mail',
+		'bambu.mail.newsletter',
+		'bambu.mail.backends'
+	],
+	package_data = {
+		'bambu.mail': [
+			'templates/mail/*.html',
+			'templates/mail/*.txt'
+		]
+	},
+	classifiers = [
+		'Development Status :: 4 - Beta',
+		'Environment :: Web Environment',
+		'Framework :: Django'
+	]
+)
