ad4e1104535445f7e64224d40c9949d2976cd29b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package django-ptree/0.1.65/django-ptree-0.1.65.zip

---
+++ packages/django-ptree/django-ptree-0.1.65.zip/django-ptree-0.1.65/ptree/views/__init__.py
@@ -0,0 +1,11 @@
+from ptree.views.abstract import (
+    BaseView,
+    UpdateView,
+    CreateView,
+    CreateMultipleView,
+    UpdateMultipleView,
+    TemplateView,
+    GetTreatmentOrParticipant,
+    StartTreatment,
+    SequenceTemplateView,
+)
