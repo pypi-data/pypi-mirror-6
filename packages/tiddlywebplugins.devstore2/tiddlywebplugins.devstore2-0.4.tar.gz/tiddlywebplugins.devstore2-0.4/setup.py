79648e118b53b64762f27094844373d809e841c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:13 +0000

    Add package tiddlywebplugins.devstore2/0.4/tiddlywebplugins.devstore2-0.4.tar.gz

---
+++ packages/tiddlywebplugins.devstore2/tiddlywebplugins.devstore2-0.4.tar.gz/tiddlywebplugins.devstore2-0.4/setup.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.devstore2'
+DESCRIPTION = 'Development oriented store that wraps another.'
+VERSION = '0.4'
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+# You should carefully review the below (install_requires especially).
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    platforms = 'Posix; MacOS X; Windows',
+    packages = find_packages(exclude=['test']),
+    install_requires = ['setuptools', 'tiddlyweb',
+        'tiddlywebplugins.twimport'],
+    zip_safe = False
+    )
