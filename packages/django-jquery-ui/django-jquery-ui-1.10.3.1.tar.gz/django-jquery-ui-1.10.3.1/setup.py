c1c7baacc62757dc6e05553abda8814c5d4db292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package django-jquery-ui/1.10.3.1/django-jquery-ui-1.10.3.1.tar.gz

---
+++ packages/django-jquery-ui/django-jquery-ui-1.10.3.1.tar.gz/django-jquery-ui-1.10.3.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='django-jquery-ui',
+    version='1.10.3.1',
+    url='https://github.com/benbacardi/django-jquery-ui',
+    description='jQuery UI packaged in a django app to speed up new applications and deployment.',
+    author='Ben Cardy',
+    author_email='benbacardi@gmail.com',
+    license='MIT',
+    keywords='django jquery jqueryui staticfiles'.split(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    packages=['jquery_ui'],
+    package_data={'jquery_ui': ['static/js/*.js', 'static/css/jquery-ui/*/*.css', 'static/css/jquery-ui/*/images/*']},
+    install_requires=['django-jquery >= 1.6',],
+)
