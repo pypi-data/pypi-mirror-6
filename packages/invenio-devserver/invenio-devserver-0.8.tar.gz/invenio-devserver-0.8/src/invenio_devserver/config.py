9ed2fb8833a6feab130185a14e6b650dc26ff08c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:25 +0000

    Add package invenio-devserver/0.8/invenio-devserver-0.8.tar.gz

---
+++ packages/invenio-devserver/invenio-devserver-0.8.tar.gz/invenio-devserver-0.8/src/invenio_devserver/config.py
@@ -0,0 +1,55 @@
+"""
+Default Values
+
+INSTALL_PATH will be set to current active virtualenv or CFG_INVENIO_PREFIX if set.
+SRC_PATH will be set to CFG_INVENIO_SRC if set otherwise ~/src/invenio/
+
+All config values can be overwritten by puttinh a config_local.py in your
+site-packages.
+"""
+import os
+
+# Check if CFG_INVENIO_SRC is set otherwise use default.
+if 'CFG_INVENIO_SRCDIR' in os.environ:
+    SRC_PATH = [os.environ['CFG_INVENIO_SRCDIR']]
+elif 'CFG_INVENIO_SRC' in os.environ:
+    SRC_PATH = [os.environ['CFG_INVENIO_SRC']]
+else:
+    SRC_PATH = [os.path.expanduser("~/src/invenio"), ]
+
+
+# Try if we're in a virtualenv or CFG_INVENIO_PREFIX is set.
+INSTALL_PATH = "/opt/invenio/"
+for var in ['VIRTUAL_ENV', 'CFG_INVENIO_PREFIX']:
+    if var in os.environ:
+        INSTALL_PATH = os.environ[var]
+        break
+
+# All the extensions specified here will be copied to their destination
+# directories when they are changed
+DIRS = {
+    'py': 'lib/python/invenio',
+    'js': 'var/www/js',
+    'css': 'var/www/css',
+    'conf': 'etc',
+    'bft': 'etc/bibformat/format_templates',
+    'bfo': 'etc/bibformat/output_formats',
+}
+
+STATIC_FILES = {
+    '/img': '/var/www/img',
+    '/js': '/var/www/js',
+    '/flash': '/var/www/flash',
+    '/css': '/var/www/css',
+    '/static': '/var/www/static',
+    '/export': '/var/www/export',
+    '/MathJax': '/var/www/MathJax',
+    '/jsCalendar': '/var/www/jsCalendar',
+    '/ckeditor': '/var/www/ckeditor',
+    '/mediaelement': '/var/www/mediaelement',
+    '/robots.txt': '/var/www/robots.txt',
+    '/favicon.ico': '/var/www/favicon.ico',
+}
+
+CONFIG_FILENAME = 'invenio.conf'
+LOCAL_CONFIG_FILENAME = 'invenio-local.conf'
