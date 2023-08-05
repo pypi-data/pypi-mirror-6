55fb418120fda6b9cad164aa74d21957cd7565d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:49 +0000

    Add package language-check/0.5.3/language-check-0.5.3.tar.gz

---
+++ packages/language-check/language-check-0.5.3.tar.gz/language-check-0.5.3/test.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -ex
+#
+# Test command-line usage.
+
+echo 'This is okay.' | language-check -
+! echo 'This is noot okay.' | language-check -
+
+echo 'This is okay.' | python -m language_check -
+! echo 'This is noot okay.' | python -m language_check -
+
+echo 'These are “smart” quotes.' | python -m language_check -
+! echo 'These are "dumb" quotes.' | python -m language_check -
