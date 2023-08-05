c93caa080fc8df3f0de4085fad635b7dd8d9ae2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:35 +0000

    Add package django-cas-cache/0.1.1/django-cas-cache-0.1.1.tar.gz

---
+++ packages/django-cas-cache/django-cas-cache-0.1.1.tar.gz/django-cas-cache-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='django-cas-cache',
+    version='0.1.1',
+    packages=[
+        'cascache',
+        'cascache.backends',
+    ],
+    license='MIT',
+    long_description=open('pypi.rst').read(),
+    author="Anentropic",
+    author_email="ego@anentropic.com",
+    url="https://github.com/anentropic/django-cas-cache",
+)
