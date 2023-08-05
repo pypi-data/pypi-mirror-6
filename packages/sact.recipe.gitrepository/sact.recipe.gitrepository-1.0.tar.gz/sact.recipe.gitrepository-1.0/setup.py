f2248901094b326225a9dbbbe4cd53592d2c19fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:08 +0000

    Add package sact.recipe.gitrepository/1.0/sact.recipe.gitrepository-1.0.tar.gz

---
+++ packages/sact.recipe.gitrepository/sact.recipe.gitrepository-1.0.tar.gz/sact.recipe.gitrepository-1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+      name="sact.recipe.gitrepository",
+      version="1.0",
+      author="Sebastien Douche",
+      author_email="sdouche@gmail.com",
+      url="http://github.com/securactive/sact.recipe.gitrepository",
+      description="Buildout recipe for fetching sources in a git repository",
+      long_description=open("README.rst").read() + open("CHANGELOG.rst").read(),
+      classifiers=(
+          "Framework :: Buildout",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Topic :: Software Development",
+          "Topic :: Software Development :: Pre-processors",
+      ),
+      keywords="zc.buildout recipe git",
+      license="BSD",
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'':'src'},
+      namespace_packages=("sact", "sact.recipe"),
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'zc.recipe.egg',
+      ],
+      entry_points="""
+        [zc.buildout]
+        default = sact.recipe.gitrepository:Recipe
+    """,
+      )
+
