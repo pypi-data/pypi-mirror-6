03bc6c09b4635ddbb09984d3f0d4a0a53d112bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:09 +0000

    Add package django_test_mixins/1.5/django_test_mixins-1.5.tar.gz

---
+++ packages/django_test_mixins/django_test_mixins-1.5.tar.gz/django_test_mixins-1.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='django_test_mixins',
+      description="Additional assertions and test conveniences for testing django sites.",
+      version='1.5',
+      url="https://github.com/Wilfred/django-test-mixins",
+      maintainer="Wilfred Hughes",
+      maintainer_email="me@wilfred.me.uk",
+      license='MIT',
+      py_modules=['django_test_mixins'],
+      )
