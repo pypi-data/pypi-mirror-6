069279eeb82dd8b789599aaa555ed98e8ded04a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:08 +0000

    Add package python-oauth2/0.5.0/python-oauth2-0.5.0.tar.gz

---
+++ packages/python-oauth2/python-oauth2-0.5.0.tar.gz/python-oauth2-0.5.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+VERSION = "0.5.0"
+
+setup(name="python-oauth2",
+      version=VERSION,
+      description="OAuth 2.0 provider for python",
+      long_description=open("README.rst").read(),
+      author="Markus Meyer",
+      author_email="hydrantanderwand@gmail.com",
+      url="https://github.com/wndhydrnt/python-oauth2",
+      packages = ["oauth2", "oauth2.store", "oauth2.test"],
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+    ]
+)
