67f1e6e2da91b015fa6d5c99581920fbf1d95a31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:27 +0000

    Add package django-dajaxice-ng/0.5.6.3/django-dajaxice-ng-0.5.6.3.tar.gz

---
+++ packages/django-dajaxice-ng/django-dajaxice-ng-0.5.6.3.tar.gz/django-dajaxice-ng-0.5.6.3/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name='django-dajaxice-ng',
+    version='0.5.6.3',
+    author='ifanr',
+    author_email='tifan@ifanr.com',
+    description='Agnostic and easy to use ajax library for django>1.5',
+    url='https://github.com/ifanrx/django-dajaxice',
+    license='BSD',
+    packages=['dajaxice',
+              'dajaxice.templatetags',
+              'dajaxice.core'],
+    package_data={'dajaxice': ['templates/dajaxice/*']},
+    long_description=("Easy to use AJAX library for django, all the "
+                      "presentation logic resides outside the views and "
+                      "doesn't require any JS Framework."
+                      "This is the Next Gen dajaxice maintained by ifanrx."),
+    install_requires=[
+        'Django>=1.5'
+    ],
+    classifiers=['Development Status :: 4 - Beta',
+                'Environment :: Web Environment',
+                'Framework :: Django',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: BSD License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Topic :: Utilities']
+)
