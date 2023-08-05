0e698daaa2aa4982d80eb4a4c85a710dff2e2088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package pumphandle/0.0.2/pumphandle-0.0.2.tar.gz

---
+++ packages/pumphandle/pumphandle-0.0.2.tar.gz/pumphandle-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+        
+setup(name='pumphandle',
+      version='0.0.2',
+      description='Tools for computational epidemiology',
+      long_description=readme(),
+      keywords ='epidemiology',
+      classifiers=['Development Status :: 1 - Planning',
+      'Intended Audience :: System Administrators',
+      'License :: OSI Approved :: MIT License',
+      'Natural Language :: English',
+      'Programming Language :: Python :: 2.7',
+      'Topic :: Scientific/Engineering',
+      'Topic :: Scientific/Engineering :: Mathematics',
+      'Topic :: Scientific/Engineering :: Medical Science Apps.',
+      ],
+      url='https://github.com/elofgren/pumphandle',
+      author='Eric Lofgren',
+      author_email='Eric.Lofgren@gmail.com',
+      license='MIT',
+      packages=['pumphandle'],
+      requires=['requests',])
+
+
+
+
