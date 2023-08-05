8708e1e1c3c95eccb2198a6a8c76b55cc46ddda2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:24 +0000

    Add package ripple-python/0.2.2/ripple-python-0.2.2.tar.gz

---
+++ packages/ripple-python/ripple-python-0.2.2.tar.gz/ripple-python-0.2.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name="ripple-python",
+    description="Python routines for the Ripple payment network",
+    author='Michael Elsdörfer',
+    author_email='michael@elsdoerfer.com',
+    version="0.2.2",
+    url="https://github.com/miracle2k/ripple-python",
+    license='BSD',
+    packages = find_packages(),
+    zip_safe=True,
+    install_requires=[
+        'ecdsa>=0.10',
+        'websocket-client>=0.12'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+    ]
+)
