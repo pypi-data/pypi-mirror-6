c7a1603fa5e95808b5fbeda2b8505516b083f5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package plone.contentrules/2.0.4/plone.contentrules-2.0.4.zip

---
+++ packages/plone.contentrules/plone.contentrules-2.0.4.zip/plone.contentrules-2.0.4/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+version = '2.0.4'
+
+setup(name='plone.contentrules',
+      version=version,
+      description="Plone ContentRules Engine",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.contentrules',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'ZODB3',
+        'zope.annotation',
+        'zope.component',
+        'zope.componentvocabulary',
+        'zope.configuration',
+        'zope.container',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.lifecycleevent',
+        'zope.schema',
+        'zope.testing',
+      ],
+      )
