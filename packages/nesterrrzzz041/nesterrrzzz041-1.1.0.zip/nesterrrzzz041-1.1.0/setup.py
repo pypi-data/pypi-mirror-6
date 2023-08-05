46e040c94a7c6b333f48c3daced75994f08eb821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package nesterrrzzz041/1.1.0/nesterrrzzz041-1.1.0.zip

---
+++ packages/nesterrrzzz041/nesterrrzzz041-1.1.0.zip/nesterrrzzz041-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterrrzzz041',
+    version = '1.1.0',
+    py_moodules = ['nesterrrzzz041'],
+    author = 'goldy',
+    author_email = '041just041@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    descripton = 'simple printer of nested lists',
+    )
+    
