99923fa980948c89fb5a01695eff610f5f9a1f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:09 +0000

    Add package ftw.blog/1.5.2/ftw.blog-1.5.2.zip

---
+++ packages/ftw.blog/ftw.blog-1.5.2.zip/ftw.blog-1.5.2/ftw/blog/portlets/archiv.py
@@ -0,0 +1,96 @@
+from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import base_hasattr
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ftw.blog.interfaces import IBlogUtils
+from plone.app.portlets.portlets import base
+from plone.memoize.view import memoize
+from plone.portlets.interfaces import IPortletDataProvider
+from zope.component import getUtility
+from zope.i18n import translate
+from zope.interface import implements
+from Products.CMFPlone.i18nl10n import monthname_msgid
+
+
+class IArchivePortlet(IPortletDataProvider):
+    """Archive portlet interface.
+    """
+
+
+class Assignment(base.Assignment):
+    implements(IArchivePortlet)
+
+    @property
+    def title(self):
+        return "Blog Archive Portlet"
+
+
+class Renderer(base.Renderer):
+
+    def __init__(self, context, request, view, manager, data):
+        self.context = context
+        self.data = data
+        self.request = request
+
+    @property
+    def available(self):
+        """Only show the portlet, when the blog isn't empty
+        """
+        if self.archive_summary():
+            return True
+        else:
+            return False
+
+    def zLocalizedTime(self, time, long_format=False):
+        """Convert time to localized time
+        """
+        month_msgid = monthname_msgid(time.strftime("%m"))
+        month = translate(month_msgid, domain='plonelocales',
+                          context=self.request)
+
+        return u"%s %s" % (month, time.strftime('%Y'))
+
+    @memoize
+    def archive_summary(self):
+        """Returns an ordered list of summary infos per month."""
+        catalog = getToolByName(self.context, 'portal_catalog')
+        query = {}
+        blogutils = getUtility(IBlogUtils, name='ftw.blog.utils')
+        blogroot = blogutils.getBlogRoot(self.context)
+        if base_hasattr(blogroot, 'getTranslations'):
+            blogroots = blogroot.getTranslations(review_state=False).values()
+            root_path = ['/'.join(br.getPhysicalPath()) for br in blogroots]
+            query['Language'] = 'all'
+        else:
+            root_path = '/'.join(blogroot.getPhysicalPath())
+
+        query['path'] = root_path
+        query['portal_type'] = 'BlogEntry'
+
+        archive_counts = {}
+        blog_entries = catalog(**query)
+        for entry in blog_entries:
+            year_month = entry.created.strftime('%Y/%m')
+            if year_month in archive_counts:
+                archive_counts[year_month] += 1
+            else:
+                archive_counts[year_month] = 1
+
+        archive_summary = []
+        ac_keys = archive_counts.keys()
+        ac_keys.sort(reverse=True)
+        for year_month in ac_keys:
+            archive_summary.append(dict(
+                title=self.zLocalizedTime(DateTime('%s/01' % year_month)),
+                number=archive_counts[year_month],
+                url='%s?archiv=%s/01' % (blogroot.absolute_url(), year_month),
+            ))
+        return archive_summary
+
+    render = ViewPageTemplateFile('archiv.pt')
+
+
+class AddForm(base.NullAddForm):
+
+    def create(self):
+        return Assignment()
---
+++ packages/ftw.blog/ftw.blog-1.5.2.zip/ftw.blog-1.5.2/setup.py
@@ -0,0 +1,66 @@
+import os
+from setuptools import setup, find_packages
+
+version = '1.5.2'
+
+maintainer = 'Mathias Leimgruber'
+
+tests_require = [
+    'ftw.testing',
+    'plone.app.testing',
+    'pyquery'
+    ]
+
+extras_require = {
+    'tests': tests_require,
+    'tabbeview': [
+        'ftw.tabbedview']}
+
+setup(name='ftw.blog',
+      version=version,
+      description="A Blog for Plone",
+      long_description=open("README.rst").read() + "\n" +
+      open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+
+      keywords='ftw blog',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.blog',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+        'ftw.colorbox',
+        'ftw.tagging',
+        'ftw.upgrade>=1.6.0',
+        'Plone',
+        'plone.browserlayer',
+        'setuptools',
+        # -*- Extra requirements: -*-
+        ],
+      tests_require=tests_require,
+      extras_require=extras_require,
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
