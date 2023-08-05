27c74dba648087d7a8aa1068b64e20fb4e92e54c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:52 +0000

    Add package GoldSaxIndiaTranslate/0.12/GoldSaxIndiaTranslate-0.12.zip

---
+++ packages/GoldSaxIndiaTranslate/GoldSaxIndiaTranslate-0.12.zip/GoldSaxIndiaTranslate-0.12/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+        name = 'GoldSaxIndiaTranslate',
+        packages = ['GoldSaxIndiaTranslate'], # this must be the same as the name above
+        version = '0.12',
+        description = 'To enable easy translation for python applications to use this package on from English to Indian languages, including a transliteration.',
+        author = 'Vance King Saxbe. A',
+        author_email = 'vsaxbe@yahoo.com',
+        url = '',   # use the URL to the github repo
+        download_url = '', # I'll explain this in a second
+        keywords = ['translation', 'transliteration', 'India', 'english'], # arbitrary keywords
+        classifiers = [],
+    )
