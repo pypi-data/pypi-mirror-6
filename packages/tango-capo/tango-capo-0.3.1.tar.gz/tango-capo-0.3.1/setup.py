3aa44a795ef862ca3e3d414686880e780a4ef0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:20 +0000

    Add package tango-capo/0.3.1/tango-capo-0.3.1.tar.gz

---
+++ packages/tango-capo/tango-capo-0.3.1.tar.gz/tango-capo-0.3.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='tango-capo',
+    version='0.3.1',
+    author=u'Tim Baxter',
+    author_email='mail.baxter@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/tBaxter/tango-capo',
+    license='LICENCE.txt',
+    description='Capo for Django.',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    zip_safe=False,
+    #dependency_links = ['http://github.com/tBaxter/capo/tarball/master#egg=capo-0.3']
+)
