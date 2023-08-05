6aefa972797490efcad9ebb0a1f5e91e506acc34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:23 +0000

    Add package silvatheme.standardissue/1.3/silvatheme.standardissue-1.3.tar.gz

---
+++ packages/silvatheme.standardissue/silvatheme.standardissue-1.3.tar.gz/silvatheme.standardissue-1.3/README.txt
@@ -0,0 +1,6 @@
+========================
+silvatheme.standardissue
+========================
+
+Standard Issue is a visual design (theme) for Silva 2.2 and higher.
+
---
+++ packages/silvatheme.standardissue/silvatheme.standardissue-1.3.tar.gz/silvatheme.standardissue-1.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.3'
+
+setup(name='silvatheme.standardissue',
+      version=version,
+      description="Standard Issue theme for Silva",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silvatheme standardissue',
+      author='Infrae - Design by NodeThirtyThree',
+      author_email='info@infrae.com',
+      url='https://github.com/silvacms/silvatheme.standardissue',
+      license='BSD and Creative Commons',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silvatheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'megrok.chameleon',
+          'silva.core.conf',
+          'silva.core.interfaces',
+          'silva.core.layout',
+          'zope.cachedescriptors',
+          ],
+      )
