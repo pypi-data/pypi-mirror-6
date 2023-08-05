6d15725ac3c5a3087e89b5de8ec6a1a51b608a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package chai/0.4.7/chai-0.4.7.tar.gz

---
+++ packages/chai/chai-0.4.7.tar.gz/chai-0.4.7/chai/__init__.py
@@ -0,0 +1,9 @@
+'''
+Copyright (c) 2011-2013, Agora Games, LLC All rights reserved.
+
+https://github.com/agoragames/chai/blob/master/LICENSE.txt
+'''
+
+from __future__ import absolute_import
+__version__ = '0.4.7'
+from .chai import Chai
