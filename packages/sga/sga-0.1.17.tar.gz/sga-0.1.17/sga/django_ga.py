e82907bdcaa1c1155f0c53033215a969ee296492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:43 +0000

    Add package sga/0.1.17/sga-0.1.17.tar.gz

---
+++ packages/sga/sga-0.1.17.tar.gz/sga-0.1.17/sga/django_ga.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+
+"""
+django插件，绑定之后可以自动给本地的ga_center发送数据
+要求配置:
+    GA_ID : Google分析的跟踪ID
+    GA_CENTER_HOST : GACenter的启动IP
+    GA_CENTER_PORT : GACenter的启动端口
+    GA_FORBID_PATHS : 被拒绝的paths，优先级高于 GA_ALLOW_PATHS
+    GA_ALLOW_PATHS : 被允许的paths
+"""
+
+import logging
+import socket
+import json
+import errno
+import time
+import re
+import urlparse
+from django.conf import settings
+
+import constants
+
+logger = logging.getLogger('django_ga')
+
+
+class DjangoGA(object):
+    _ga_id = None
+    _ga_center_host = None
+    _ga_center_port = None
+    _ga_forbid_paths = None
+    _ga_allow_paths = None
+
+    _local_ip = ''
+
+    _socket = None
+
+    def __init__(self):
+        self._ga_id = getattr(settings, 'GA_ID', None)
+        self._ga_center_host = getattr(settings, 'GA_CENTER_HOST', None) or constants.GA_CENTER_DEFAULT_HOST
+        self._ga_center_port = getattr(settings, 'GA_CENTER_PORT', None) or constants.GA_CENTER_DEFAULT_PORT
+        self._ga_forbid_paths = getattr(settings, 'GA_FORBID_PATHS', None) or []
+        self._ga_allow_paths = getattr(settings, 'GA_ALLOW_PATHS', None) or []
+
+        self._local_ip = socket.gethostbyname(socket.gethostname()) or ''
+
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # 非阻塞
+        self._socket.setblocking(0)
+
+    def process_request(self, request):
+        request.ga_begin_time = time.time()
+
+    def process_response(self, request, response):
+        """
+        无论是否抛出异常，都会执行这一步
+        """
+        self._send_ga_data(request)
+        return response
+
+    def send_data_to_ga_center(self, send_dict):
+        """
+        可以在网站中调用
+        """
+        try:
+            self._socket.sendto(json.dumps(send_dict), (self._ga_center_host, self._ga_center_port))
+        except socket.error, e:
+            # errno.EWOULDBLOCK = errno.EAGAIN = 11
+            if e.args[0] == errno.EWOULDBLOCK:
+                logger.info('errno.EWOULDBLOCK')
+            else:
+                logger.error('exception occur. msg[%s], traceback[%s]', str(e), __import__('traceback').format_exc())
+
+    def _send_ga_data(self, request):
+        logger.debug('ga_id:%s', self._ga_id)
+
+        if not self._ga_id:
+            return False
+
+        if not self._is_ga_request(request):
+            return False
+
+        try:
+            send_dict = self._gen_send_dict(request)
+            if not send_dict:
+                logger.debug('invalid request')
+                return False
+            self.send_data_to_ga_center(send_dict)
+
+            return True
+        except Exception, e:
+            logger.error('exception occur. msg[%s], traceback[%s]', str(e), __import__('traceback').format_exc())
+
+        return False
+
+    def _is_ga_request(self, request):
+        """
+        request是否要被统计
+        """
+
+        # 先判断是否在forbid列表里，只要发现就直接拒绝
+        for pattern in self._ga_forbid_paths:
+            if re.match(pattern, request.path):
+                logger.debug('path is in forbid paths. patten: %s, path: %s', pattern, request.path)
+                return False
+
+        # 只有allow列表不为空的情况下，才有效
+        if self._ga_allow_paths:
+            for pattern in self._ga_allow_paths:
+                if re.match(pattern, request.path):
+                    return True
+            else:
+                logger.debug('path is not in allow paths. path: %s', request.path)
+                return False
+
+        return True
+
+    def _gen_send_dict(self, request):
+        """
+        生成发送的dict
+        """
+        if not getattr(request, 'ga_begin_time', None):
+            return None
+
+        load_time = int((time.time()-request.ga_begin_time) * 1000)
+
+        ga_referrer_path = ''
+        if request.META.get('HTTP_REFERER', ''):
+            try:
+                parse_result = urlparse.urlparse(request.META['HTTP_REFERER'])
+                ga_referrer_path = '/%s%s' % (parse_result.netloc, parse_result.path)
+            except Exception, e:
+                logger.info('urlparse fail. e: %s', e)
+
+        send_dict = dict(
+            funcname='track_pageview',
+            tracker=dict(
+                __ga=True,
+                account_id=self._ga_id,
+                domain_name=request.get_host(),
+                campaign=dict(
+                    __ga=True,
+                    source=self._local_ip,
+                    content=ga_referrer_path,
+                ),
+            ),
+            session=dict(
+                __ga=True,
+            ),
+            page=dict(
+                __ga=True,
+                path=request.path,
+                load_time=load_time,
+            ),
+            visitor=dict(
+                __ga=True,
+                ip_address=request.META.get('REMOTE_ADDR', ''),
+            ),
+        )
+
+        return send_dict
---
+++ packages/sga/sga-0.1.17.tar.gz/sga-0.1.17/sga/flask_ga.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+
+"""
+flask插件，绑定之后可以自动给本地的ga_center发送数据
+要求配置:
+    GA_ID : Google分析的跟踪ID
+    GA_CENTER_HOST : GACenter的启动IP
+    GA_CENTER_PORT : GACenter的启动端口
+    GA_FORBID_PATHS : 被拒绝的paths，优先级高于 GA_ALLOW_PATHS
+    GA_ALLOW_PATHS : 被允许的paths
+"""
+import json
+import socket
+import errno
+import time
+import re
+import urlparse
+
+from flask import current_app, request, g
+
+import constants
+
+
+class FlaskGA(object):
+    _ga_id = None
+    _ga_center_host = None
+    _ga_center_port = None
+    _ga_forbid_paths = None
+    _ga_allow_paths = None
+
+    _local_ip = ''
+
+    _socket = None
+
+    def __init__(self, app=None):
+        if app:
+            self.init_app(app)
+
+    def __del__(self):
+        if self._socket:
+            self._socket.close()
+
+    def init_app(self, app):
+        """
+        绑定app
+        """
+        self._ga_id = app.config.get('GA_ID')
+        self._ga_center_host = app.config.get('GA_CENTER_HOST') or constants.GA_CENTER_DEFAULT_HOST
+        self._ga_center_port = app.config.get('GA_CENTER_PORT') or constants.GA_CENTER_DEFAULT_PORT
+        self._ga_forbid_paths = app.config.get('GA_FORBID_PATHS') or []
+        self._ga_allow_paths = app.config.get('GA_ALLOW_PATHS') or []
+
+        self._local_ip = socket.gethostbyname(socket.gethostname()) or ''
+
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # 非阻塞
+        self._socket.setblocking(0)
+
+        @app.before_request
+        def prepare_ga_data():
+            g.ga_begin_time = time.time()
+
+        @app.teardown_request
+        def send_ga_data(exc):
+            current_app.logger.debug('ga_id:%s', self._ga_id)
+
+            if not self._ga_id:
+                return False
+
+            if not self._is_ga_request():
+                return False
+
+            try:
+                send_dict = self._gen_send_dict()
+                if not send_dict:
+                    # 这个时候不是正常的请求，比如是用test_request_context模拟的
+                    current_app.logger.debug('invalid request, may be in test_request_context')
+                    return False
+                self.send_data_to_ga_center(send_dict)
+
+                return True
+            except Exception, e:
+                current_app.logger.error('exception occur. msg[%s], traceback[%s]', str(e), __import__('traceback').format_exc())
+
+            return False
+
+    def send_data_to_ga_center(self, send_dict):
+        """
+        可以在网站中调用
+        """
+        try:
+            self._socket.sendto(json.dumps(send_dict), (self._ga_center_host, self._ga_center_port))
+        except socket.error, e:
+            # errno.EWOULDBLOCK = errno.EAGAIN = 11
+            if e.args[0] == errno.EWOULDBLOCK:
+                current_app.logger.info('errno.EWOULDBLOCK')
+            else:
+                current_app.logger.error('exception occur. msg[%s], traceback[%s]', str(e), __import__('traceback').format_exc())
+
+    def _is_ga_request(self):
+        """
+        request是否要被统计
+        """
+
+        # 先判断是否在forbid列表里，只要发现就直接拒绝
+        for pattern in self._ga_forbid_paths:
+            if re.match(pattern, request.path):
+                current_app.logger.debug('path is in forbid paths. patten: %s, path: %s', pattern, request.path)
+                return False
+
+        # 只有allow列表不为空的情况下，才有效
+        if self._ga_allow_paths:
+            for pattern in self._ga_allow_paths:
+                if re.match(pattern, request.path):
+                    return True
+            else:
+                current_app.logger.debug('path is not in allow paths. path: %s', request.path)
+                return False
+
+        return True
+
+    def _gen_send_dict(self):
+        """
+        生成发送的dict
+        """
+        if not getattr(g, 'ga_begin_time', None):
+            return None
+
+        load_time = int((time.time()-g.ga_begin_time) * 1000)
+
+        ga_referrer_path = ''
+        if request.referrer:
+            try:
+                parse_result = urlparse.urlparse(request.referrer)
+                ga_referrer_path = '/%s%s' % (parse_result.netloc, parse_result.path)
+            except Exception, e:
+                current_app.logger.info('urlparse fail. e: %s', e)
+
+        send_dict = dict(
+            funcname='track_pageview',
+            tracker=dict(
+                __ga=True,
+                account_id=self._ga_id,
+                domain_name=request.host,
+                campaign=dict(
+                    __ga=True,
+                    source=self._local_ip,
+                    content=ga_referrer_path,
+                ),
+            ),
+            session=dict(
+                __ga=True,
+            ),
+            page=dict(
+                __ga=True,
+                path=request.path,
+                load_time=load_time,
+            ),
+            visitor=dict(
+                __ga=True,
+                ip_address=request.remote_addr,
+            ),
+        )
+
+        return send_dict
