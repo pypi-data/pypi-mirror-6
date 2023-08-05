1773c242a4ee75139b2151a0ba6bf1d31fa9f0b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:56 +0000

    Add package multisuite/0.1.6/multisuite-0.1.6.tar.gz

---
+++ packages/multisuite/multisuite-0.1.6.tar.gz/multisuite-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+        name="multisuite",
+        version="0.1.6",
+        author="Project MONK Developers",
+        author_email="project-monk@dreserach-fe.de",
+        py_modules = [
+            "multisuite",
+        ],
+        entry_points = {
+            'console_scripts' : [
+                'multisuite = multisuite:main',
+            ],
+        },
+        install_requires = [
+            "nose >= 1.3.0",
+        ],
+        url="https://github.com/DFE/multisuite",
+        license="LICENSE",
+        description="run independent nose test suites together",
+        long_description=open("README.rst").read(),
+)
