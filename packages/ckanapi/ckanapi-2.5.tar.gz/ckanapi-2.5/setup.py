481c24eb48c278e602530d6d6f51ecfe0fc8490e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package ckanapi/2.5/ckanapi-2.5.tar.gz

---
+++ packages/ckanapi/ckanapi-2.5.tar.gz/ckanapi-2.5/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ckanapi',
+      version='2.5',
+      description='Thin wrapper around the CKAN Action API',
+      license='BSD',
+      author='Ian Ward',
+      author_email='ian@excess.org',
+      url='https://github.com/ckan/ckanapi',
+      packages=['ckanapi', 'ckanapi.tests', 'ckanapi.tests.mock'],
+      test_suite='ckanapi.tests',
+      zip_safe=False,
+     )
