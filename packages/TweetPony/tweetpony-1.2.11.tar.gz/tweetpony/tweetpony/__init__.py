06bb0f192e1ef963cfda4ad762ac37d13a8c7ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:42 +0000

    Add package TweetPony/1.2.11/tweetpony-1.2.11.tar.gz

---
+++ packages/TweetPony/tweetpony-1.2.11.tar.gz/tweetpony/tweetpony/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2013 Julian Metzler
+# See the LICENSE file for the full license.
+
+from .api import *
+
+# Consumer key / secret of the "TweetPony" application. Can be used for example purposes.
+CONSUMER_KEY = "acZe1khMjIvsg1g8izdag"
+CONSUMER_SECRET = "B9E3gmuQSZPaEhwGEBD7879oNHydbdWnjRKyB7b1ag"
