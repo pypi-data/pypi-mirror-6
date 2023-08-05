902bc387e5a2616d14d20595e5b8227dd36a28a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:49 +0000

    Add package mtq/0.3.2/mtq-0.3.2.tar.gz

---
+++ packages/mtq/mtq-0.3.2.tar.gz/mtq-0.3.2/mtq/tests/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+
+import time
+from mtq.utils import stream_logging
+import logging
+import io
+
+logger = logging.getLogger('job.foo.bar')
+logger.setLevel(logging.INFO)
+
+def raise_error():
+    x = 1 + 1
+    logger = logging.getLogger('job.foo.bar')
+    logger.info("this is an info")
+    logging.getLogger('woc').info("this is from the woc log")
+    logging.getLogger('job').info("this is an info 2333")
+    asdf
+
+def do_something():
+    logger.info("this is a log")
+    print("this is a print")
+    
+def long_running_task():
+    logger.info("this is long_running_task")
+    time.sleep(30)
+    
+def main():
+    record = io.BytesIO()
+    hndlr = logging.StreamHandler(record)
+    logger = logging.getLogger('job')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(hndlr)
+    hndlr.setLevel(logging.INFO)
+    logger.warn("asdfasdf")
+    
+    print(record)
+    record.seek(0)
+    print(record.read())
+
+
+if __name__ == '__main__':
+    main()
