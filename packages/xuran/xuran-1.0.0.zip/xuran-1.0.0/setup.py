161efdcbb8d6ef479486f35722594f1445d4fe2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package xuran/1.0.0/xuran-1.0.0.zip

---
+++ packages/xuran/xuran-1.0.0.zip/xuran-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+         name     =    'xuran',
+		 version  =    '1.0.0',
+		 py_modules = ['xuran'],
+		 author = 'hfpython',
+		 author_emai = '296597641@qq.com',
+		 url = 'www.baidu.com',
+		 decription = 'A simple printer of nested lists',
+		 )
