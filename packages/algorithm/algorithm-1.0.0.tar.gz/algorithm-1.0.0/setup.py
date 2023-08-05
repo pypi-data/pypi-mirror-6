088a2f9e8d0f7f0474af6f85b26c33104fc6ea1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:47 +0000

    Add package algorithm/1.0.0/algorithm-1.0.0.tar.gz

---
+++ packages/algorithm/algorithm-1.0.0.tar.gz/algorithm-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from setuptools import setup
+
+
+setup( name='algorithm'
+     , author='Gittip, LLC'
+     , author_email='support@gittip.com'
+     , description="Model an algorithm as a list of functions."
+     , url='http://algorithm-py.readthedocs.org/'
+     , version='1.0.0'
+     , py_modules=['algorithm']
+     , install_requires=['dependency_injection']
+     , classifiers=[ 'Development Status :: 5 - Production/Stable'
+                   , 'Intended Audience :: Developers'
+                   , 'License :: OSI Approved :: MIT License'
+                   , 'Operating System :: OS Independent'
+                   , 'Programming Language :: Python :: 2'
+                   , 'Programming Language :: Python :: 2.6'
+                   , 'Programming Language :: Python :: 2.7'
+                   , 'Programming Language :: Python :: 3'
+                   , 'Programming Language :: Python :: 3.2'
+                   , 'Programming Language :: Python :: 3.3'
+                   , 'Topic :: Software Development :: Libraries :: Python Modules'
+                    ]
+      )
