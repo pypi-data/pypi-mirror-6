53414d0c894aae2069984052cf012afada5aeafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:26 +0000

    Add package farmboy/0.2/farmboy-0.2.tar.gz

---
+++ packages/farmboy/farmboy-0.2.tar.gz/farmboy-0.2/farmboy/haproxy.py
@@ -0,0 +1,47 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+import fabtools.require
+
+from farmboy import util
+
+
+from fabric.api import env
+from fabric.api import roles
+from farmboy.fabric_ import task
+
+
+WEB_SERVER = '  server web%(i)s %(host)s:%(port)s maxconn 32'
+
+
+@task
+@roles('proxy')
+def deploy():
+    """Add HAproxy to the <proxy> host."""
+    fabtools.require.deb.packages([
+        'haproxy'
+    ])
+
+    servers = []
+    for i, server in enumerate(env.roledefs['web']):
+        server_s = WEB_SERVER % {'i': i,
+                                 'host': server.split('@')[-1],
+                                 'port': '80'}
+        servers.append(server_s)
+
+    fabtools.require.files.template_file(
+        template_source   = util.files('haproxy/haproxy.base'),
+        context  = {'servers': '\n'.join(servers)},
+        path     = '/etc/haproxy/haproxy.cfg',
+        owner    = 'root',
+        group    = 'root',
+        mode     = '644',
+        use_sudo = True)
+
+    fabtools.require.files.file(
+        source   = util.files('haproxy/default'),
+        path     = '/etc/default/haproxy',
+        owner    = 'root',
+        group    = 'root',
+        mode     = '644',
+        use_sudo = True)
+
+    fabtools.require.service.restarted('haproxy')
---
+++ packages/farmboy/farmboy-0.2.tar.gz/farmboy-0.2/farmboy/mysql.py
@@ -0,0 +1,67 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+import fabtools.require
+
+from fabric.api import env
+from fabric.api import parallel
+from fabric.api import roles
+from fabric.api import settings
+from fabric.api import sudo
+from farmboy.fabric_ import task
+
+from farmboy import util
+
+
+env.farmboy_mysql_password = None
+
+
+def _generate_password():
+    # Chosen at random
+    new_password = 'secret'
+    return new_password
+
+
+@task
+@roles('db')
+@parallel
+def deploy():
+    """Add mysql to the db hosts."""
+    # If we don't have a key yet generate one and save it locally
+    if not env.farmboy_mysql_password:
+        new_password = _generate_password()
+        util.update({'farmboy_mysql_password': new_password})
+        env.farmboy_mysql_password = new_password
+
+    mysql_password = env.farmboy_mysql_password
+
+    fabtools.require.mysql.server(password=mysql_password)
+    fabtools.require.files.file(
+            source = util.files('mysql/mysqld.cnf'),
+            path = '/etc/mysql/conf.d/mysqld.cnf',
+            owner = 'root',
+            group = 'root',
+            mode = '644',
+            use_sudo = True)
+    sudo('service mysql restart')
+
+@task
+@roles('db')
+def create_user(name, password=None, user_host=None):
+    if not password:
+        password = util.load('farmboy_mysql_password_%s' % name)
+
+    if not password:
+        password = _generate_password()
+        util.update({'farmboy_mysql_password_%s' % name: password})
+
+    with settings(mysql_user='root', mysql_password=env.farmboy_mysql_password):
+        fabtools.require.mysql.user(name, password, host=user_host)
+
+
+@task
+@roles('db')
+def create_database(name, owner=None, owner_host=None, **kw):
+    with settings(mysql_user='root', mysql_password=env.farmboy_mysql_password):
+        fabtools.require.mysql.database(
+                name, owner=owner, owner_host=owner_host, **kw)
+
+
