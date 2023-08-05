57264a0f03e0985fbab7c329460038fb5e4aa45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:01 +0000

    Add package suffix_array/0.9.16/suffix_array-0.9.16.tar.gz

---
+++ packages/suffix_array/suffix_array-0.9.16.tar.gz/suffix_array-0.9.16/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup, Extension
+from glob import glob
+
+VERSION = '0.9.16'
+
+if __name__ == '__main__':
+    src_files = glob('src/*.c')
+    extensions = [Extension("suffix_array",
+                            sources=src_files,
+                            language='c')]
+    description = "Fast Suffix Array for Python"
+    setup(name='suffix_array',
+          version=VERSION,
+          author='Justin Van Winkle',
+          author_email='justin.vanwinkle@gmail.com',
+          url='https://github.com/justinvanwinkle/suffix_array',
+          license="BSD",
+          description=description,
+          ext_modules=extensions)
