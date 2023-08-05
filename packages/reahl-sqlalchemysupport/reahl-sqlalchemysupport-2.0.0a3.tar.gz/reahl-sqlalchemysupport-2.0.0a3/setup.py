5949862f8c89cb8203d65557d1c67115f0018a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package reahl-sqlalchemysupport/2.0.0a3/reahl-sqlalchemysupport-2.0.0a3.tar.gz

---
+++ packages/reahl-sqlalchemysupport/reahl-sqlalchemysupport-2.0.0a3.tar.gz/reahl-sqlalchemysupport-2.0.0a3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+    name='reahl-sqlalchemysupport',
+    version=u'2.0.0a3',
+    description='Support for using SqlAlchemy with Reahl.',
+    long_description=u'Reahl is a web application framework that allows a Python programmer to work in terms of useful abstractions - using a single programming language. . This package contains infrastructure necessary to use Reahl with SqlAlchemy or Elixir. ',
+    maintainer='Iwan Vosloo',
+    maintainer_email='iwan@reahl.org',
+    packages=['reahl'],
+    py_modules=['setup'],
+    include_package_data=False,
+    package_data={'': [u'*/LC_MESSAGES/*.mo']},
+    namespace_packages=['reahl'],
+    install_requires=[u'reahl-component>=2.0.0a3,<2.1', u'sqlalchemy>=0.7,<0.7.999', u'alembic>=0.5,<0.5.999'],
+    setup_requires=[],
+    tests_require=[u'reahl-tofu>=2.0.0a3,<2.1', u'reahl-stubble>=2.0.0a3,<2.1'],
+    test_suite=u'reahl.sqlalchemysupport_dev',
+    entry_points={
+        u'reahl.configspec': [
+            u'config = reahl.sqlalchemysupport:SqlAlchemyConfig'    ],
+        'reahl.eggs': [
+            u'Egg = reahl.component.eggs:ReahlEgg'    ],
+        u'reahl.persistlist': [
+            u'0 = reahl.sqlalchemysupport:SchemaVersion'    ],
+                 },
+    extras_require={}
+)
