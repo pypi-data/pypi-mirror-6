a7b63306b4e12180ee7aea54334d5f33f23f3dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package minduz/1.0.0/minduz-1.0.0.zip

---
+++ packages/minduz/minduz-1.0.0.zip/minduz-1.0.0/minduz.py
@@ -0,0 +1,11 @@
+"""aasdfasfadfasagagahahahahah 
+   asdgasgagasgasdg
+   sdfhsdhsrjtyjfjfgykfgk"""
+def print_lol(the_list):
+    """sdghsdgsdgidgsdhsdhsdhsd
+       dfbsdbhsdnsdfgndfn"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ packages/minduz/minduz-1.0.0.zip/minduz-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name        = "minduz",
+      version     = "1.0.0",
+      py_modules  =  ["minduz"],
+      author      = "mindu",
+      author_email= "minduz88@gmail.com",
+      url         = "http://mindu.com",
+      description = "a simple printer of minduz lists",
+      )      
