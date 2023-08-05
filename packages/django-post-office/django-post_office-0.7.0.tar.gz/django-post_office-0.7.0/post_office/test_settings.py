2ca7f645906820e609a47068440c6c811502bb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:14 +0000

    Add package django-post-office/0.7.0/django-post_office-0.7.0.tar.gz

---
+++ packages/django-post-office/django-post_office-0.7.0.tar.gz/django-post_office-0.7.0/post_office/test_settings.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
+
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 36000,
+        'KEY_PREFIX': 'post-office',
+    },
+    'post_office': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'TIMEOUT': 36000,
+        'KEY_PREFIX': 'post-office',
+    }
+}
+
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'post_office',
+)
+
+SECRET_KEY = 'a'
+
+ROOT_URLCONF = 'post_office.test_urls'
+
+DEFAULT_FROM_EMAIL = 'webmaster@example.com'
