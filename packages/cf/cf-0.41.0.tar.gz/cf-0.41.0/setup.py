6ac4dfbbafff21fa018495c0cade2aa8c2993902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:41 +0000

    Add package cf/0.41.0/cf-0.41.0.tar.gz

---
+++ packages/cf/cf-0.41.0.tar.gz/cf-0.41.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='cf',
+    version='0.41.0',
+    description='A command line client to Rackspace Cloud Files',
+    author='Paul Durivage',
+    author_email='pauldurivage@gmail.com',
+    license='Apache 2.0',
+    url='https://github.com/angstwad/cf',
+    install_requires=['python-cloudfiles'],
+    scripts=['bin/cf']
+)
