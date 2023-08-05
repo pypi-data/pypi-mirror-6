81ab690dfed82a7239099698236fe8f79e459865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package isort/1.3.1/isort-1.3.1.tar.gz

---
+++ packages/isort/isort-1.3.1.tar.gz/isort-1.3.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='isort',
+      version='1.3.1',
+      description='A Python utility / library to sort Python imports.',
+      author='Timothy Crosley',
+      author_email='timothy.crosley@gmail.com',
+      url='https://github.com/timothycrosley/isort',
+      download_url='https://github.com/timothycrosley/isort/archive/1.3.1.tar.gz',
+      license="MIT",
+      scripts=['scripts/isort'],
+      packages=['isort'],
+      requires=['pies', 'natsort'],
+      install_requires=['pies>=1.0.3', 'natsort>=3.0.0'])
