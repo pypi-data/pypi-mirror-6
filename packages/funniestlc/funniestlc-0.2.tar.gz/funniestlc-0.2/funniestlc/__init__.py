95c095ecb6bec4b49d3b7cc16a2b37ade95b2c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:07 +0000

    Add package funniestlc/0.2/funniestlc-0.2.tar.gz

---
+++ packages/funniestlc/funniestlc-0.2.tar.gz/funniestlc-0.2/funniestlc/__init__.py
@@ -0,0 +1,7 @@
+from markdown import markdown
+
+def joke():
+    print "add it in version 0.2"
+    return markdown(u'Wenn ist das Nunstcck git und Slotermeyer?'
+                    u'Ja! ... **Beiherhund** das Oder die Flipperwaldt '
+                    u'gersput.')
