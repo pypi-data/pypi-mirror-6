f5e1da54170280dc0184d4b9df91adbf9d7da103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:32 +0000

    Add package furl/0.3.7/furl-0.3.7.tar.gz

---
+++ packages/furl/furl-0.3.7.tar.gz/furl-0.3.7/setup.py
@@ -0,0 +1,38 @@
+import os
+import sys
+from sys import version_info
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'publish':
+    '''
+    Publish to PyPi.
+    '''
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+long_description = (
+    'Information and documentation at https://github.com/gruns/furl.')
+
+setup(name='furl',
+      version='0.3.7',  # Keep synchronized with furl/__init__.py.
+      author='Arthur Grunseid',
+      author_email='grunseid@gmail.com',
+      url='https://github.com/gruns/furl',
+      license='Unlicense',
+      description='URL manipulation made simple.',
+      long_description=long_description,
+      packages=find_packages(),
+      include_package_data=True,
+      platforms=['any'],
+      classifiers=['Topic :: Internet',
+                   'Natural Language :: English',
+                   'Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'License :: Freely Distributable',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   ],
+      install_requires=['orderedmultidict >= 0.7.1'],
+      test_suite='tests',
+      tests_require=[] if version_info[0:2] >= [2,7] else ['unittest2'],
+      )
