589c52128b60489838bf75a4dd8978d4381f48eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package drench/0.0.13/drench-0.0.13.tar.gz

---
+++ packages/drench/drench-0.0.13.tar.gz/drench-0.0.13/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="drench",
+    version="0.0.13",
+    install_requires=['bitarray>=0.8.1', 'requests>=2.0.0'],
+    packages=['drench'],
+
+    # metadata for upload to PyPI
+    maintainer="Jeffrey Blagdon",
+    maintainer_email="jeffblagdon@gmail.com",
+    description="A simple BitTorrent client",
+    license="MIT",
+    url='https://github.com/jefflovejapan/drench',
+    keywords="bittorrent torrent visualization twisted"
+)
