b3c88f3328c8aefc9f7ade7c4f21596768a55346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:53 +0000

    Add package gettup/0.1.1/gettup-0.1.1.tar.gz

---
+++ packages/gettup/gettup-0.1.1.tar.gz/gettup-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+import sys
+from setuptools import setup, find_packages
+
+requirements = ['requests']
+if sys.version_info < (2, 7):
+    requirements.append('argparse')
+
+setup(name='gettup',
+      version='0.1.1',
+      description='A command-line file sharing utility for ge.tt',
+      long_description=open("README.md").read(),
+      keywords="CLI filesharing file sharing upload command-line",
+      author='Prakhar Srivastav',
+      author_email='prakhar1989@gmail.com',
+      url='https://github.com/prakhar1989/gett/',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['requests'],
+      entry_points = {
+          'console_scripts': [ 'gett = gett:main' ]
+      },
+      zip_safe=False
+     )
+
