e08d4e60dc4f3c56419dbbd9137eb70521cb7acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package vbeo.seantis.dir.events/1.0rc1/vbeo.seantis.dir.events-1.0rc1.tar.gz

---
+++ packages/vbeo.seantis.dir.events/vbeo.seantis.dir.events-1.0rc1.tar.gz/vbeo.seantis.dir.events-1.0rc1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import os
+
+name = 'vbeo.seantis.dir.events'
+description = (
+    "Integration of seantis.dir.events for Volkswirtschaft Berner Oberland."
+)
+version = '1.0rc1'
+
+
+def get_long_description():
+    readme = open('README.rst').read()
+    history = open(os.path.join('docs', 'HISTORY.rst')).read()
+    contributors = open(os.path.join('docs', 'CONTRIBUTORS.rst')).read()
+
+    # cut the part before the description to avoid repetition on pypi
+    readme = readme[readme.index(description) + len(description):]
+
+    return '\n'.join((readme, contributors, history))
+
+
+setup(name=name, version=version, description=description,
+      long_description=get_long_description(),
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
+      keywords='',
+      author='Seantis GmbH',
+      author_email='info@seantis.ch',
+      url='https://github.com/seantis/vbeo.seantis.dir.events',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['vbeo', 'vbeo.seantis', 'vbeo.seantis.dir'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'seantis.dir.events>=1.0rc1',
+          'izug.basetheme',
+          'plone.app.theming'
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
