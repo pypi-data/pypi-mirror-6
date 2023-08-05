200617ca9bb558ae3f00b91aac9456f58a2f289c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:35 +0000

    Add package ontv/0.8.1/ontv-0.8.1.tar.gz

---
+++ packages/ontv/ontv-0.8.1.tar.gz/ontv-0.8.1/ontv/action/add.py
@@ -0,0 +1,26 @@
+from ..utils import api_series_finder
+from ..utils import with_resource
+
+
+@with_resource(api_series_finder)
+def action(ns, series):
+    if ns.series.has_series(series):
+        print ns.t.bold_red(
+            "Already exists: {0}".format(
+                series['series_name']))
+        return 0
+
+    ns.series.add(series)
+
+    print ns.t.bold_green(u"added: {0}".format(series['series_name']))
+    return 0
+
+
+def setup(parser):
+    parser.add_argument(
+        "series_query",
+        metavar="<id|name>",
+        help="The id of the series to add",
+    )
+
+    parser.set_defaults(action=action)
