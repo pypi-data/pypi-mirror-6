d8108c0fdff9b90944903aaa4fe74c2510928b4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:28 +0000

    Add package pytest-pyramid/0.1.1/pytest_pyramid-0.1.1-py2.py3-none-any.whl

---
+++ packages/pytest-pyramid/pytest_pyramid-0.1.1-py2.py3-none-any.whl/pytest_pyramid/plugin.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2013 by pytest_pyramid authors and contributors <see AUTHORS file>
+#
+# This module is part of pytest_pyramid and is released under
+# the MIT License (MIT): http://opensource.org/licenses/MIT
+
+from pytest_pyramid import factories
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--pc',
+        action='store',
+        default=None,
+        metavar='path',
+        dest='pyramid_config',
+    )
+
+
+# fixtures (made out of factories)
+pyramid_config = factories.pyramid_config()
+pyramid_app = factories.pyramid_app('pyramid_config')
