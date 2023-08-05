065fb1eb4a6d5ad9a189e0d18623117de0ba1c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:03 +0000

    Add package Mopidy-MPRIS/1.1.1/Mopidy-MPRIS-1.1.1.tar.gz

---
+++ packages/Mopidy-MPRIS/Mopidy-MPRIS-1.1.1.tar.gz/Mopidy-MPRIS-1.1.1/tests/test_extension.py
@@ -0,0 +1,31 @@
+import mock
+import unittest
+
+from mopidy_mpris import Extension, frontend as frontend_lib
+
+
+class ExtensionTest(unittest.TestCase):
+
+    def test_get_default_config(self):
+        ext = Extension()
+
+        config = ext.get_default_config()
+
+        self.assertIn('[mpris]', config)
+        self.assertIn('enabled = true', config)
+
+    def test_get_config_schema(self):
+        ext = Extension()
+
+        schema = ext.get_config_schema()
+
+        self.assertIn('desktop_file', schema)
+
+    def test_get_frontend_classes(self):
+        ext = Extension()
+        registry = mock.Mock()
+
+        ext.setup(registry)
+
+        registry.add.assert_called_once_with(
+            'frontend', frontend_lib.MprisFrontend)
