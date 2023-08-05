b96b27fcaddfcd4c31e9e98d4c5dbff687b29fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:34 +0000

    Add package bufrpy/0.2.1/bufrpy-0.2.1.tar.gz

---
+++ packages/bufrpy/bufrpy-0.2.1.tar.gz/bufrpy-0.2.1/bufrpy/__init__.py
@@ -0,0 +1,17 @@
+"""
+Decoder for RDT BUFR files
+
+Based on FM 94 BUFR edition 3 as found in WMO306_vl2_BUFR3_Spec_en.pdf
+
+"""
+
+from bufrpy.bufrdec import decode, decode_file, decode_all, Section0, Section1v3, Section1v4, Section2, Section3, Section4, Section5, Message
+
+from bufrpy.json import from_json, to_json
+
+__title__ = 'bufrpy'
+__author__ = 'Tuure Laurinolli / FMI'
+__version__ = "0.2.1"
+__copyright__ = 'Copyright 2013 Finnish Meteorological Institute'
+
+__all__ = ["from_json", "to_json", "decode", "decode_file", "decode_all", "Section0", "Section1v3", "Section1v4", "Section2", "Section3", "Section4", "Section5", "Message"]
---
+++ packages/bufrpy/bufrpy-0.2.1.tar.gz/bufrpy-0.2.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+import ast
+
+path = 'bufrpy/__init__.py'
+with open(path, 'rU') as file:
+    t = compile(file.read(), path, 'exec', ast.PyCF_ONLY_AST)
+    for node in t.body:
+        if isinstance(node, ast.Assign) and len(node.targets) == 1:
+            name = node.targets[0]
+            if isinstance(name, ast.Name) and name.id == '__version__':
+                version = node.value.s
+                break
+    else:
+        raise ValueError("Could not determine software version")
+
+
+setup(name="bufrpy",
+      packages=["bufrpy"],
+      version=version,
+      description="Pyre-Python BUFR decoder",
+      url="https://github.com/tazle/bufrpy",
+      author="Tuure Laurinolli / FMI",
+      author_email="tuure.laurinolli@fmi.fi",
+      zip_safe=False,
+      keywords=["bufr"],
+      install_requires=["bitstring>=3.1.2"],
+      test_suite="tests",
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+          "Topic :: Scientific/Engineering"
+      ])
+
