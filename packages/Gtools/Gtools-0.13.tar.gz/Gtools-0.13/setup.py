ac84b7c445f5804d9329aa1796da05ab34ceb821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:44 +0000

    Add package Gtools/0.13/Gtools-0.13.tar.gz

---
+++ packages/Gtools/Gtools-0.13.tar.gz/Gtools-0.13/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Gtools',
+    version='0.13',
+    py_modules=['regex'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    url="http://www.nbhosting.nl/dist/gtools/",
+    author="Gerd Koetje",
+    author_email="support@nbhosting.nl",
+)
