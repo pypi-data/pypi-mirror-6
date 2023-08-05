20afd68ce9319010e49362ecb4d0623cc1b92d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:29 +0000

    Add package sphinxcontrib-yuml/0.3.1/sphinxcontrib-yuml-0.3.1.tar.gz

---
+++ packages/sphinxcontrib-yuml/sphinxcontrib-yuml-0.3.1.tar.gz/sphinxcontrib-yuml-0.3.1/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+long_desc = '''
+This package enable embedding yuml diagrams on your Sphinx documentation.
+
+Example::
+
+    .. yuml:: 
+       :alt: [Customer]->[Billing Address]
+       :type: class
+
+       [Customer]->[Billing Address]
+
+'''
+
+setup(
+    name='sphinxcontrib-yuml',
+    version='0.3.1',
+    url='https://github.com/njouanin/sphinxcontrib-yuml',
+    packages=find_packages(),
+    license='GPLv3',
+	description='Sphinx extension for embedding yuml diagram in documentations',
+    long_description=long_desc,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+)
