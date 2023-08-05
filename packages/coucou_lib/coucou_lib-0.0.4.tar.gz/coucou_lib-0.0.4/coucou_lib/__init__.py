2752bf42cac31533624dd902c415c474a9ca0d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:36 +0000

    Add package coucou_lib/0.0.4/coucou_lib-0.0.4.tar.gz

---
+++ packages/coucou_lib/coucou_lib-0.0.4.tar.gz/coucou_lib-0.0.4/coucou_lib/__init__.py
@@ -0,0 +1,4 @@
+from coucou_lib import coucou
+
+__version__ = "0.0.4" # print coucou_lib.__version__
+
---
+++ packages/coucou_lib/coucou_lib-0.0.4.tar.gz/coucou_lib-0.0.4/coucou_lib/coucou_lib.py
@@ -0,0 +1,10 @@
+# coucou lib
+
+__all__ = ['coucou'] # from coucou_lib import * ---> seulement coucou() sera importe
+
+def coucou():
+    print "COUCOU !!!"
+    
+    
+if __name__ == "__main__":
+    coucou()
