cd220939af86b828bb8f316e406a41e3da54271b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package kaboom/0.1.8.1/kaboom-0.1.8.1.tar.gz

---
+++ packages/kaboom/kaboom-0.1.8.1.tar.gz/kaboom-0.1.8.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from kaboom import __version__
+
+
+setup(
+    name='kaboom',
+    version=__version__,
+    packages=['kaboom'],
+    url='http://github.com/vinu76jsr/kaboom',
+    author='vaibhav',
+    author_email='vinu76jsr@gmail.com',
+    license='bsd3',
+    description='Command line key-value store',
+    install_requires=['rethinkdb'],
+    entry_points={'console_scripts': [
+        'kaboom = kaboom.kaboom:run']}
+)
