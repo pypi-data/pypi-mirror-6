4dde6f33a342893674e75af5928d1c84d6f56cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:04 +0000

    Add package bise.biodiversityfactsheet/1.1.2/bise.biodiversityfactsheet-1.1.2.zip

---
+++ packages/bise.biodiversityfactsheet/bise.biodiversityfactsheet-1.1.2.zip/bise.biodiversityfactsheet-1.1.2/bise/biodiversityfactsheet/cataloguer.py
@@ -0,0 +1,21 @@
+from bise.catalogueindexer.adapters.basic import PACDocumentCataloguer
+from zope.component import getMultiAdapter
+from zope.globalrequest import getRequest
+
+
+class FactsheetCataloguer(PACDocumentCataloguer):
+
+    def get_values_to_index(self):
+        request = getRequest()
+        items = super(FactsheetCataloguer, self).get_values_to_index()
+        view = getMultiAdapter((self.context, request), name='view')
+        contents = u''
+        for item in view.facts():
+            for fact in item.get('facts'):
+                contents += getMultiAdapter(
+                    (fact.getObject(), request),
+                    name='factrenderview')()
+
+        items['article[content]'] = contents
+
+        return items
