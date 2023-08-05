598e219eb094e53c1de214c8e9c8df9ca27a55b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package mapkit/0.0.4/mapkit-0.0.4.tar.gz

---
+++ packages/mapkit/mapkit-0.0.4.tar.gz/mapkit-0.0.4/mapkit/__init__.py
@@ -0,0 +1,16 @@
+'''
+********************************************************************************
+* Name: mapit
+* Author: Nathan Swain
+* Created On: November 19, 2013
+* Copyright: (c) Brigham Young University 2013
+* License: BSD 2-Clause
+********************************************************************************
+'''
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+def version():
+    return '0.0.4'
