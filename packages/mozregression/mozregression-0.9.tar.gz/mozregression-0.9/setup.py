5e53c181123186fad5484b01c47d96532fe5028e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:56 +0000

    Add package mozregression/0.9/mozregression-0.9.tar.gz

---
+++ packages/mozregression/mozregression-0.9.tar.gz/mozregression-0.9/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+desc = """Regression range finder for Mozilla nightly builds"""
+
+# take description from README
+here = os.path.dirname(os.path.abspath(__file__))
+try:
+    summ = file(os.path.join(here, 'README.md')).read()
+except:
+    summ = """Interactive regression range finder for Mozilla nightly builds"""
+
+setup(name="mozregression",
+      version="0.9",
+      description=desc,
+      long_description=summ,
+      author='Heather Arthur',
+      author_email='fayearthur@gmail.com',
+      url='http://github.com/mozilla/mozregression',
+      license='MPL 1.1/GPL 2.0/LGPL 2.1',
+      packages=['mozregression'],
+      entry_points="""
+          [console_scripts]
+          mozregression = mozregression:regressioncli
+          moznightly = mozregression:nightlycli
+        """,
+      platforms =['Any'],
+      install_requires = ['httplib2 >= 0.6.0',
+                          'mozcommitbuilder >= 0.4.10',
+                          'mozfile >= 0.1',
+                          'mozprofile >= 0.4',
+                          'mozrunner >= 5.14',
+                          'BeautifulSoup >= 3.0.4',
+                          'mozinstall >= 1.4',
+                          'mozinfo >= 0.4'
+                          ],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Operating System :: OS Independent'
+                  ]
+     )
