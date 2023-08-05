5fb46eca2d9194f4fed3bf56d187e9403ed9a159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:09 +0000

    Add package django-tango/0.4.6/django-tango-0.4.6.tar.gz

---
+++ packages/django-tango/django-tango-0.4.6.tar.gz/django-tango-0.4.6/user_profiles/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Profile
+from .forms import ProfileCreationForm
+
+
+def deactivate(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+deactivate.short_description = "Deactivate selected users"
+
+
+class ProfileAdmin(UserAdmin):
+    add_form = ProfileCreationForm
+
+    date_hierarchy = 'date_joined'
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'date_joined')
+    list_display = ('username', 'display_name', 'first_name', 'last_name', 'email', 'is_active', 'is_staff')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'display_name')
+    actions = [deactivate, ]
+
+
+admin.site.register(Profile, ProfileAdmin)
