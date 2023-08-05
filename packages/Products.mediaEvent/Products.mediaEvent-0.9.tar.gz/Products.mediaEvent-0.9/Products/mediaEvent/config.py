95ff4d6e35f574e3b90c89cb609cba271b3fedc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:37 +0000

    Add package Products.mediaEvent/0.9/Products.mediaEvent-0.9.tar.gz

---
+++ packages/Products.mediaEvent/Products.mediaEvent-0.9.tar.gz/Products.mediaEvent-0.9/Products/mediaEvent/config.py
@@ -0,0 +1,9 @@
+"""Common configuration constants
+"""
+
+PROJECTNAME = 'Products.mediaEvent'
+
+ADD_PERMISSIONS = {
+    # -*- extra stuff goes here -*-
+    'MediaEvent': 'Products.mediaEvent: Add Media Event',
+}
---
+++ packages/Products.mediaEvent/Products.mediaEvent-0.9.tar.gz/Products.mediaEvent-0.9/Products/mediaEvent/interfaces/mediaevent.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+# -*- Additional Imports Here -*-
+
+
+class IMediaEvent(Interface):
+    """Event type that has a folderish behaviour to store media related to the event."""
+
+    # -*- schema definition goes here -*-
