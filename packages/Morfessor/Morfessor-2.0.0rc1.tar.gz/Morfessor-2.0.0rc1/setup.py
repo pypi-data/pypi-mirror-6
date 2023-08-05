3ca5f7958243c9e9503eb8c3cff769463fd5de1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package Morfessor/2.0.0rc1/Morfessor-2.0.0rc1.tar.gz

---
+++ packages/Morfessor/Morfessor-2.0.0rc1.tar.gz/Morfessor-2.0.0rc1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+import re
+main_py = open('morfessor/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", main_py))
+
+requires = [
+    #    'progressbar',
+]
+
+setup(name='Morfessor',
+      version=metadata['version'],
+      author=metadata['author'],
+      author_email='morfessor@cis.hut.fi',
+      url='http://www.cis.hut.fi/projects/morpho/',
+      description='Morfessor',
+      py_modules=['distribute_setup'],
+      packages=['morfessor', 'morfessor.test'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+      ],
+      license="BSD",
+      scripts=['scripts/morfessor', 'scripts/morfessor-train',
+               'scripts/morfessor-segment',
+               'scripts/morfessor-evaluate',
+               ],
+      install_requires=requires,
+      )
