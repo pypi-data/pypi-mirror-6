c7bbb14857bf7db41e85a0ccd33dd04ecd6ba6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package pyshark/0.2/pyshark-0.2.zip

---
+++ packages/pyshark/pyshark-0.2.zip/pyshark-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('../README.txt') as f:
+    long_description = f.read()
+
+setup(
+    name = "pyshark",
+    version = "0.2",
+    packages = find_packages(),
+
+    data_files = [('./pyshark', ['pyshark/config.ini'])],
+    install_requires = ['lxml', 'py'],
+
+    url = "https://github.com/KimiNewt/pyshark",
+    long_description=long_description,
+    author = "Dor Green",
+    author_email = "dorgreen1 at gmail dot com",
+    description = "Python wrapper for tshark, allowing python packet parsing using wireshark dissectors",
+    keywords = "wireshark capture packets parsing packet",
+)
