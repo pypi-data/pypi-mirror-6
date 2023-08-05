9ec60b64a145b4ad246170bd7934ab223c77766b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:03 +0000

    Add package touchpaper/0.2.1/touchpaper-0.2.1.tar.gz

---
+++ packages/touchpaper/touchpaper-0.2.1.tar.gz/touchpaper-0.2.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+del os.link
+
+import sys
+from setuptools import setup, find_packages
+from touchpaper._version import get_version
+
+setup(
+    name='touchpaper',
+    version=get_version(),
+    description='A command-line utility to quickly launch EC2 instances. A tasty accompaniment to Fabric.',
+    author='James Tiplady',
+    url='http://github.com/BigglesZX/touchpaper',
+    packages=find_packages(),
+    install_requires=['boto==2.23.0', 'colorama==0.2.7'],
+    entry_points={
+        'console_scripts': [
+            'touchpaper = touchpaper.main:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+)
