38ff5692f1d0b9621ce716cc39d93930adefb742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:45 +0000

    Add package nolearn/0.4/nolearn-0.4.tar.gz

---
+++ packages/nolearn/nolearn-0.4.tar.gz/nolearn-0.4/README.rst
@@ -0,0 +1,6 @@
+This package contains a number of utility modules that are helpful
+with machine learning tasks.  Most of the modules work together with
+`scikit-learn <http://scikit-learn.org/>`_, others are more generally
+useful.
+
+View the `documentation here <http://packages.python.org/nolearn/>`_.
---
+++ packages/nolearn/nolearn-0.4.tar.gz/nolearn-0.4/setup.py
@@ -0,0 +1,55 @@
+import os
+
+from setuptools import setup, find_packages
+
+version = '0.4'
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires = [
+    'docopt',
+    'gdbn',
+    'joblib',
+    'scikit-learn',
+    ]
+
+tests_require = [
+    'mock',
+    'pytest',
+    'pytest-cov',
+    ]
+
+docs_require = [
+    'Sphinx',
+    ]
+
+setup(name='nolearn',
+      version=version,
+      description="Miscellaneous utilities for machine learning.",
+      long_description='\n\n'.join([README, CHANGES]),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+        ],
+      keywords='',
+      author='Daniel Nouri',
+      author_email='daniel.nouri@gmail.com',
+      url='https://github.com/dnouri/nolearn',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require={
+          'testing': tests_require,
+          'docs': docs_require,
+          },
+      dependency_links=[
+        'http://danielnouri.org/python-packages/',
+        ],
+      )
+
