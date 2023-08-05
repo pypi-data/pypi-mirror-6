0c3c4bd47f3f318b13d88fddd79890bd7ec27c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:54 +0000

    Add package bash_quote/1.0.1/bash_quote-1.0.1.tar.gz

---
+++ packages/bash_quote/bash_quote-1.0.1.tar.gz/bash_quote-1.0.1/HISTORY.rst
@@ -0,0 +1,11 @@
+.. :changelog:
+
+History
+-------
+
+1.0.1 (27-01-2014)
+++++++++++++++++++
+
+* Fixed sorting bug
+
+* First release on PyPI.
---
+++ packages/bash_quote/bash_quote-1.0.1.tar.gz/bash_quote-1.0.1/bash_quote/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Ruairi Fahy'
+__email__ = 'ruairifahy91@gmail.com'
+__version__ = '1.0.0'
