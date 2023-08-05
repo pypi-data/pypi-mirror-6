858cda95e9d8f8052737065a8062461a1982f63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:28 +0000

    Add package silva.searchandreplace/2.2/silva.searchandreplace-2.2.tar.gz

---
+++ packages/silva.searchandreplace/silva.searchandreplace-2.2.tar.gz/silva.searchandreplace-2.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.2'
+
+setup(name='silva.searchandreplace',
+      version=version,
+      description="Search in all Silva documents and replace information in Silva 2.x",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        ],
+      keywords='silva silvadocument search replace service',
+      author='Guido Wesdorp',
+      author_email='info@infrae.com',
+      url='https://github.com/silvacms/silva.searchandreplace',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'lxml',
+        'silva.core.interfaces',
+        'silva.core.services',
+        'silva.core.conf',
+        'Products.Silva >= 2.2dev',
+        ],
+      )
---
+++ packages/silva.searchandreplace/silva.searchandreplace-2.2.tar.gz/silva.searchandreplace-2.2/silva/searchandreplace/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2009-2010 Infrae. All rights reserved.
+# See also LICENSE.txt
+# $Id$
+
+from silva.core import conf as silvaconf
+
+silvaconf.extensionName('silva.searchandreplace')
+silvaconf.extensionTitle('Silva Search and Replace')
+silvaconf.extensionSystem()
