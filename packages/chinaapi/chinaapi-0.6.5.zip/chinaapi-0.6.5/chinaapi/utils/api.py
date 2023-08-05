1829d9fe524d8233642717849ab5230c22c2e557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:45 +0000

    Add package chinaapi/0.6.5/chinaapi-0.6.5.zip

---
+++ packages/chinaapi/chinaapi-0.6.5.zip/chinaapi-0.6.5/chinaapi/utils/api.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+import requests
+from requests.models import PreparedRequest
+from chinaapi.utils import jsonDict
+from chinaapi.utils.exceptions import ApiResponseValueError, NotExistApi
+
+
+class Response(object):
+    def __init__(self, response):
+        self.response = response
+
+    def json(self):
+        try:
+            return jsonDict.loads(self.response.text)
+        except ValueError, e:
+            if self.response.ok:
+                raise ApiResponseValueError(self.response, e)
+            else:
+                raise NotExistApi(self.response)
+
+
+class Request(object):
+    def __init__(self):
+        self._session = requests.session()
+
+    def _parse_response(self, response):
+        return Response(response).json()
+
+    @classmethod
+    def _parse_querystring(cls, querystring):
+        return dict([item.split('=') for item in querystring.split('&')])
+
+    @classmethod
+    def _request_url(cls, url, params):
+        pre = PreparedRequest()
+        pre.prepare_url(url, params)
+        return pre.url
