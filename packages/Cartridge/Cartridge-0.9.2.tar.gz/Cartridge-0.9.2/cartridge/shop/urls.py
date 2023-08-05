5e53ad3eb3b9789bafb02da2b856c0f899adcccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:07 +0000

    Add package Cartridge/0.9.2/Cartridge-0.9.2.tar.gz

---
+++ packages/Cartridge/Cartridge-0.9.2.tar.gz/Cartridge-0.9.2/cartridge/shop/urls.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns("cartridge.shop.views",
+    url("^product/(?P<slug>.*)/$", "product", name="shop_product"),
+    url("^wishlist/$", "wishlist", name="shop_wishlist"),
+    url("^cart/$", "cart", name="shop_cart"),
+    url("^checkout/$", "checkout_steps", name="shop_checkout"),
+    url("^checkout/complete/$", "complete", name="shop_complete"),
+    url("^invoice/(?P<order_id>\d+)/$", "invoice", name="shop_invoice"),
+)
