7d95a29cd59985e50e44e1c2631a0d877a4be281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package ErepMessenger/0.1/ErepMessenger-0.1.tar.gz

---
+++ packages/ErepMessenger/ErepMessenger-0.1.tar.gz/ErepMessenger-0.1/setup.py
@@ -0,0 +1,19 @@
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup, find_packages
+setup(
+    name = "ErepMessenger",
+    version = "0.1",
+    scripts = ['ez_setup.py', 'sql.py','config.cfg'],
+    install_requires = ['requests>=2.2.1', 'beautifulsoup4>=4.3.2'],
+
+    # metadata for upload to PyPI
+    author = "Mike Ontry",
+    author_email = "mr.ontry@gmail.com",
+    description = "App to send mass in game messages for Erepublik",
+    license = "PSF",
+    keywords = "erepublik mass messaging",
+    url = "http://ereptools.tk",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+)
