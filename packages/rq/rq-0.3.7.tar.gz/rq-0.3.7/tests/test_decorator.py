1f7268f9486de35728bd471f52071d422c31f48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:45 +0000

    Add package rq/0.3.7/rq-0.3.7.tar.gz

---
+++ packages/rq/rq-0.3.7.tar.gz/rq-0.3.7/tests/test_decorator.py
@@ -0,0 +1,50 @@
+from tests import RQTestCase
+from tests.fixtures import decorated_job
+
+from rq.decorators import job
+from rq.job import Job
+from rq.worker import DEFAULT_RESULT_TTL
+
+class TestDecorator(RQTestCase):
+
+    def setUp(self):
+        super(TestDecorator, self).setUp()
+
+    def test_decorator_preserves_functionality(self):
+        """Ensure that a decorated function's functionality is still preserved.
+        """
+        self.assertEqual(decorated_job(1, 2), 3)
+
+    def test_decorator_adds_delay_attr(self):
+        """Ensure that decorator adds a delay attribute to function that returns
+        a Job instance when called.
+        """
+        self.assertTrue(hasattr(decorated_job, 'delay'))
+        result = decorated_job.delay(1, 2)
+        self.assertTrue(isinstance(result, Job))
+        # Ensure that job returns the right result when performed
+        self.assertEqual(result.perform(), 3)
+
+    def test_decorator_accepts_queue_name_as_argument(self):
+        """Ensure that passing in queue name to the decorator puts the job in
+        the right queue.
+        """
+        @job(queue='queue_name')
+        def hello():
+            return 'Hi'
+        result = hello.delay()
+        self.assertEqual(result.origin, 'queue_name')
+
+    def test_decorator_accepts_result_ttl_as_argument(self):
+        """Ensure that passing in result_ttl to the decorator sets the
+        result_ttl on the job
+        """
+        #Ensure default
+        result = decorated_job.delay(1, 2)
+        self.assertEqual(result.result_ttl, DEFAULT_RESULT_TTL)
+
+        @job('default', result_ttl=10)
+        def hello():
+            return 'Why hello'
+        result = hello.delay()
+        self.assertEqual(result.result_ttl, 10)
