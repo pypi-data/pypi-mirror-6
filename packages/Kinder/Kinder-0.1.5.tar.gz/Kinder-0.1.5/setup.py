3d4bccb54d8837be925a9541f48de14ce846cb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:53 +0000

    Add package Kinder/0.1.5/Kinder-0.1.5.tar.gz

---
+++ packages/Kinder/Kinder-0.1.5.tar.gz/Kinder-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Kinder',
+    version='0.1.5',
+    author='John J. Wang',
+    author_email='john@zinc.io',
+    packages=['kinder'],
+    scripts=[],
+    url='',
+    license="The MIT License (MIT)",
+    description='Probabilistic testing framework.',
+    long_description="A probabilistic testing framework designed for testing API endpoints.",
+    install_requires=[
+        "unittest"
+        ],
+    )
