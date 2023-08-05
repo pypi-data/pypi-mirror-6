099a704b7b4a40afb8bf5e7a09ecec2a20ea6ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:49 +0000

    Add package ggplot/0.4.6/ggplot-0.4.6-py2.7.egg

---
+++ packages/ggplot/ggplot-0.4.6-py2.7.egg/ggplot/geoms/facet_wrap.py
@@ -0,0 +1,50 @@
+from copy import deepcopy
+import math
+
+class facet_wrap(object):
+    def __init__(self, x=None, y=None, ncol=None, nrow=None, scales="free"):
+        if x is None and y is None:
+            raise Exception("You need to specify a variable name: facet_wrap('var')")
+        self.x = x
+        self.y = y
+        self.ncol = ncol
+        self.nrow = nrow
+        self.scales = scales
+
+    def __radd__(self, gg):
+        # deepcopy must be the first thing to not change the original object
+        gg = deepcopy(gg)
+        
+        x,y = None, None
+        gg.n_dim_x = 1
+        facets = []
+        if self.x:
+            x = gg.data.get(self.x)
+            gg.n_dim_x = x.nunique()
+            facets.append(self.x)
+        if self.y:
+            y = gg.data.get(self.y)
+            gg.n_dim_x *= y.nunique()
+            facets.append(self.y)
+
+        n_wide = self.ncol
+        n_high = self.nrow
+
+        if n_wide is None and n_high is None:
+            # calculate both on the fly
+            n_wide = math.ceil(math.sqrt(gg.n_dim_x))
+            n_high = math.ceil(gg.n_dim_x / math.ceil(math.sqrt(gg.n_dim_x)))
+        elif n_wide is None:
+            # calculate n_wide on the fly
+            n_wide = math.ceil(float(gg.n_dim_x) / n_high)
+        elif n_high is None:
+            # calculate n_high on the fly
+            n_high = math.ceil(float(gg.n_dim_x) / n_wide)
+
+        gg.n_wide, gg.n_high = int(n_wide), int(n_high)
+
+        gg.facets = facets
+        gg.facet_type = "wrap"
+        gg.facet_scales = self.scales
+        
+        return gg
---
+++ packages/ggplot/ggplot-0.4.6-py2.7.egg/ggplot/geoms/geom_line.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from itertools import groupby
+from operator import itemgetter
+from copy import deepcopy
+import sys
+from .geom import geom
+
+
+class geom_line(geom):
+    VALID_AES = ['x', 'y', 'color', 'alpha', 'group', 'linestyle', 'linewidth' 'label', 'size']
+    def plot_layer(self, layer):
+        layer = {k: v for k, v in layer.items() if k in self.VALID_AES}
+        layer.update(self.manual_aes)
+        if 'x' in layer:
+            x = layer.pop('x')
+        if 'y' in layer:
+            y = layer.pop('y')
+        if 'size' in layer:
+            # ggplot also supports aes(size=...) but the current mathplotlib is not. See 
+            # https://github.com/matplotlib/matplotlib/issues/2658
+            if isinstance(layer['size'], list):
+                layer['size'] = 4
+                msg = "'geom_line()' currenty does not support the mapping of " +\
+                      "size ('aes(size=<var>'), using size=4 as a replacement.\n" +\
+                      "Use 'geom_line(size=x)' to set the size for the whole line.\n"
+                sys.stderr.write(msg)
+            layer['linewidth'] = layer['size']
+            del layer['size']
+        if 'linestyle' in layer and 'color' not in layer:
+            layer['color'] = 'k'
+        if 'group' not in layer:
+            plt.plot(x, y, **layer)
+        else:
+            g = layer.pop('group')
+            for k, v in groupby(sorted(zip(x, y, g), key=itemgetter(2)), key=itemgetter(2)):
+                x_g, y_g, _ = zip(*v) 
+                plt.plot(x_g, y_g, **layer)
