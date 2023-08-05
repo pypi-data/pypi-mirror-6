921beb4a5f1da5e5e790431d2eaab540fa54b4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:52 +0000

    Add package pyage/0.9.8/pyage-0.9.8.tar.gz

---
+++ packages/pyage/pyage-0.9.8.tar.gz/pyage-0.9.8/pyage/core/bootstrap.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import logging
+import threading
+from time import sleep, time
+import Pyro4
+import sys
+from pyage.core import inject
+from pyage.core.workplace import Workplace
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    start_time = time()
+    logging.basicConfig(filename='pyage-' + str(datetime.now()) + '.log', level=logging.INFO)
+    inject.config = sys.argv[1]
+    logging.debug("config: %s", inject.config)
+    workspace = Workplace()
+    workspace.publish()
+    workspace.publish_agents()
+    logger.debug(workspace.address)
+    if hasattr(workspace, "daemon"):
+        thread = threading.Thread(target=workspace.daemon.requestLoop)
+        thread.setDaemon(True)
+        thread.start()
+    Pyro4.config.COMMTIMEOUT = 1
+    while not workspace.stopped:
+        workspace.step()
+    time = time() - start_time
+    logger.debug("elapsed time: %s seconds", time)
+    if hasattr(workspace, "daemon"):
+        workspace.daemon.close()
+    workspace.unregister_agents()
+    workspace.unregister()
