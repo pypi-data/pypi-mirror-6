fe3c6c25b6f5890bec15ce48a3bf43c9a8f36804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package Watson-CT/0.2.1/Watson-CT-0.2.1.tar.gz

---
+++ packages/Watson-CT/Watson-CT-0.2.1.tar.gz/Watson-CT-0.2.1/watson/ui/growl2.py
@@ -0,0 +1,92 @@
+import subprocess
+import colorama
+import sys
+
+from watson.ui.base import img_path, WatsonUI
+
+
+COLOR_EXCEPTION = colorama.Fore.RED
+COLOR_RESET = colorama.Fore.RESET
+
+
+class AppleScriptError(Exception):
+    pass
+
+
+def run_script(script):
+    proc = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate(script)
+    if proc.returncode != 0:
+        raise AppleScriptError('osascript failure: %s'%err)
+    out = out.strip()
+    if out == 'true':
+        return True
+    elif out == 'false':
+        return False
+    elif out.isdigit():
+        return int(out)
+    else:
+        return out
+
+def is_growl_running():
+    """Check if Growl is running. Run this before any other Growl functions."""
+    script = """
+    tell application "System Events"
+        set isRunning to (count of (every process whose bundle identifier is "com.Growl.GrowlHelperApp")) > 0
+    end tell
+    """
+    return run_script(script)
+
+def register_app():
+    script= """
+    tell application id "com.Growl.GrowlHelperApp"
+     -- Make a list of all the notification types 
+     -- that this script will ever send:
+     set the notificationsList to {"Test Successful" , "Test Failure"}
+
+     -- Register our script with growl.
+     -- You can optionally (as here) set a default icon 
+     -- for this script's notifications.
+     register as application "WatsonTest" all notifications notificationsList default notifications notificationsList
+    end tell"""
+    run_script(script)
+
+def notify(type, title, msg, img='green.png'):
+    script= """
+    on get_image(imgPath)
+        set imgfd to open for access POSIX file imgPath
+        set img to read imgfd as "TIFF"
+        close access imgfd
+        return img
+    end get_image
+    set noteImage to get_image("%s")
+    tell application id "com.Growl.GrowlHelperApp"
+     notify with name "%s" title "%s" description "%s" application name "WatsonTest" image noteImage
+    end tell"""%(img_path(img), type, title, msg)
+    run_script(script)
+
+class GrowlUI(WatsonUI):
+    """A Watson UI that uses Growl2. Only supported on OS X."""
+    
+    name = 'growl2'
+    platform = 'darwin'
+    
+    def __init__(self):
+        self.has_growl = is_growl_running()
+
+        if self.has_growl:
+            register_app()
+        else:
+            print(COLOR_EXCEPTION + 'Growl2 is not installed or running. Head over to https://itunes.apple.com/us/app/growl/id467939042')
+            print(COLOR_RESET)
+            sys.exit(0)
+    
+    @classmethod
+    def enabled(cls):
+        return is_growl_running()
+
+    def notify(self, failure, title, msg, icon):
+        self.has_growl
+        if self.has_growl:
+            type = failure and 'Test Failure' or 'Test Successful'
+            notify(type, title, msg, icon+'.png')
