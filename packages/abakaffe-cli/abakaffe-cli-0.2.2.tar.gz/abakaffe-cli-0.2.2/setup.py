2f6146084fe78dca7b00ccfeeb7c5bbf295ecdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:01 +0000

    Add package abakaffe-cli/0.2.2/abakaffe-cli-0.2.2.tar.gz

---
+++ packages/abakaffe-cli/abakaffe-cli-0.2.2.tar.gz/abakaffe-cli-0.2.2/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: latin-1 -*-
+from setuptools import setup
+
+setup(name = 'abakaffe-cli',
+      version = '0.2.2',
+      description = 'A CLI for the Abakus Coffee API',
+      author = 'Øyvind Robertsen, Martin Hallén',
+      author_email = ['oyvindrobertsen@gmail.com', 'marthall@outlook.com'],
+      url = 'http://github.com/oyvindrobertsen/abakaffe-cli',
+      install_requires = ['simplejson'],
+      license = 'MIT',
+      keywords = 'Abakus coffee API',
+      scripts = ['abakaffe'], 
+      )
