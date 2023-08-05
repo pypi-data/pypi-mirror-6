470d9e487c4d2fe292f90e406d28d612804a9ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:02 +0000

    Add package kaaedit/0.21.0/kaaedit-0.21.0.tar.gz

---
+++ packages/kaaedit/kaaedit-0.21.0.tar.gz/kaaedit-0.21.0/kaa/filetype/rst/__init__.py
@@ -0,0 +1,9 @@
+from kaa.filetype import filetypedef
+
+class FileTypeInfo(filetypedef.FileTypeInfo):
+    FILE_EXT = {'.rst'}
+
+    @classmethod
+    def get_modetype(cls):
+        from kaa.filetype.rst.rstmode import RstMode
+        return RstMode
