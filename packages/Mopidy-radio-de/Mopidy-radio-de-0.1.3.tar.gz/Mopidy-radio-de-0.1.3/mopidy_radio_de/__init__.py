baf2a728dcf6fc5f1b2c5be234838e1f23f469f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:23 +0000

    Add package Mopidy-radio-de/0.1.3/Mopidy-radio-de-0.1.3.tar.gz

---
+++ packages/Mopidy-radio-de/Mopidy-radio-de-0.1.3.tar.gz/Mopidy-radio-de-0.1.3/mopidy_radio_de/__init__.py
@@ -0,0 +1,32 @@
+from __future__ import unicode_literals
+
+import os
+
+from mopidy import config, ext
+
+
+__version__ = '0.1.3'
+
+
+class RadioDeExtension(ext.Extension):
+
+    dist_name = 'Mopidy-radio-de'
+    ext_name = 'radio-de'
+    version = __version__
+
+    def get_default_config(self):
+        conf_file = os.path.join(os.path.dirname(__file__), 'ext.conf')
+        return config.read(conf_file)
+
+    def get_config_schema(self):
+        schema = super(RadioDeExtension, self).get_config_schema()
+        schema['language'] = config.String()
+        schema['favorites'] = config.List(optional=True)
+        return schema
+
+    def validate_environment(self):
+        pass
+
+    def get_backend_classes(self):
+        from .actor import RadioDeBackend
+        return [RadioDeBackend]
