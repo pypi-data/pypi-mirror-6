869f75230d00ba81e8653da4f117c3e56be73d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package teapot/1.3/teapot-1.3.tar.gz

---
+++ packages/teapot/teapot-1.3.tar.gz/teapot-1.3/teapot/fetchers/file_fetcher.py
@@ -0,0 +1,66 @@
+"""
+A local-file fetcher class.
+"""
+
+import os
+import shutil
+import mimetypes
+
+from teapot.fetchers.base_fetcher import BaseFetcher
+from teapot.path import rmdir
+
+
+class FileFetcher(BaseFetcher):
+
+    """
+    Fetchs a file on the local filesystem.
+    """
+
+    shortname = 'file'
+
+    def read_source(self, source):
+        """
+        Checks that the `source` is a local filename.
+        """
+
+        if os.path.exists(source.location):
+            self.file_path = os.path.abspath(source.location)
+
+            return True
+
+    def do_fetch(self, target):
+        """
+        Fetch a filename.
+        """
+
+        archive_path = os.path.join(target, os.path.basename(self.file_path))
+
+        size = os.path.getsize(self.file_path)
+
+        if not size:
+            size = None
+
+        rmdir(archive_path)
+
+        self.progress.on_start(target=os.path.basename(archive_path), size=size)
+
+        if os.path.isfile(self.file_path):
+            shutil.copyfile(self.file_path, archive_path)
+            archive_type = mimetypes.guess_type(self.file_path)
+        elif os.path.isdir(self.file_path):
+            shutil.copytree(self.file_path, archive_path)
+            archive_type = (None, None)
+        else:
+            raise RuntimeError('Unsupported path: %s' % self.file_path)
+
+        # No real interactive progress to show here.
+        #
+        # This could be fixed though.
+
+        self.progress.on_update(progress=size)
+        self.progress.on_finish()
+
+        return {
+            'archive_path': archive_path,
+            'archive_type': archive_type,
+        }
---
+++ packages/teapot/teapot-1.3.tar.gz/teapot-1.3/teapot/unpackers/callbacks.py
@@ -0,0 +1,146 @@
+"""
+A set of unpacker callbacks class.
+"""
+
+import os
+
+from progressbar import *
+
+
+class BaseUnpackerCallback(object):
+
+    """
+    Derive from this class to create an unpacker callback class.
+    """
+
+    def __init__(self, unpacker):
+        """
+        Create a new unpacker callback instance.
+
+        `unpacker` is the unpacker instance to be associated with.
+        """
+
+        self.unpacker = unpacker
+
+    def on_start(self, count):
+        """
+        Call this method when the unpacking is about to start.
+
+        `count` indicates the total count of files in the archive.
+
+        You may use a None `count` to indicate that the overall count is
+        unknown.
+        """
+
+        pass
+
+    def on_update(self, current_file, progress):
+        """
+        Call this method when the unpacking had some progress.
+
+        `current_file` indicates the file currently being unpacked.
+        `progress` indicates the number of the current file in the archive.
+        """
+
+        pass
+
+    def on_finish(self):
+        """
+        Call this method when the unpacking is complete.
+        """
+
+        pass
+
+    def on_exception(self, exception):
+        """
+        Call this method when a fatal exception is raised that must abort the
+        unpack.
+
+        The base implementation calls the :func:`on_finish` method and if you
+        override this method, you must do the same (or just call the base
+        method).
+        """
+
+        self.on_finish()
+
+
+class NullUnpackerCallback(BaseUnpackerCallback):
+    """
+    Provides no callback.
+    """
+
+    pass
+
+
+class CurrentFile(WidgetHFill):
+    """Displays the current file."""
+
+    def __init__(self, unpacker_callback, prefix='', suffix=''):
+        """
+        Create a current file widget.
+        """
+
+        self.unpacker_callback = unpacker_callback
+        self.count = 0
+        self.prefix = prefix
+        self.suffix = suffix
+
+    def update(self, pbar, width):
+        """
+        Update the progress bar.
+        """
+
+        if self.unpacker_callback.current_file:
+            result = self.prefix + self.unpacker_callback.current_file + self.suffix
+        else:
+            result = ''
+
+        if len(result) > width:
+            result = result[:width - 3 - len(self.suffix)] + '...' + self.suffix
+        else:
+            result += ' ' * (width - len(result))
+
+        return result
+
+
+class ProgressBarUnpackerCallback(BaseUnpackerCallback):
+    """
+    Provides a progress bar callback to unpackers.
+    """
+
+    def on_start(self, count):
+        """
+        The unpack just started.
+        """
+
+        self.count = count
+        self.current_file = ''
+
+        widgets = [os.path.basename(self.unpacker.archive_path), ': ', SimpleProgress(), ' (', Percentage(), ')', CurrentFile(self, prefix=' - ')]
+        self.progressbar = ProgressBar(widgets=widgets, maxval=count)
+        self.progressbar.start()
+
+    def on_update(self, current_file, progress):
+        """
+        The unpack was updated.
+        """
+
+        self.current_file = current_file
+        self.progressbar.update(progress)
+
+    def on_finish(self):
+        """
+        The unpack finished.
+        """
+
+        self.current_file = ''
+        self.progressbar.update(self.count)
+        self.progressbar.finish()
+
+    def on_exception(self, exception):
+        """
+        The unpack failed.
+        """
+
+        if hasattr(self, 'progressbar'):
+            self.on_finish()
