d6e65a1da109c635bc12f378e72964cee7ffb646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:40 +0000

    Add package ClassicUPS/0.1.8/ClassicUPS-0.1.8.tar.gz

---
+++ packages/ClassicUPS/ClassicUPS-0.1.8.tar.gz/ClassicUPS-0.1.8/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+    name='ClassicUPS',
+    version='0.1.8',
+    author='Jay Goel',
+    author_email='jay@classicspecs.com',
+    url='http://github.com/classicspecs/ClassicUPS/',
+    packages=['ClassicUPS'],
+    description='Usable UPS Integration in Python',
+    long_description=open('README.rst').read(),
+    keywords=['UPS'],
+    install_requires=[
+        'dict2xml==1.0',
+        'xmltodict==0.4.2'
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta'
+    ]
+)
+
+# To update pypi: `python setup.py register sdist bdist_wininst upload`
