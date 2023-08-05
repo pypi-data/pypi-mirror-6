417fa6fa55c4d41abda5ac3adfd6ba0a7ae9ae90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:46 +0000

    Add package myprint/1.0.0/myprint-1.0.0.zip

---
+++ packages/myprint/myprint-1.0.0.zip/myprint-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'myprint',
+	version = '1.0.0',
+	py_modules = ['myprint'],
+	author = 'Wondbest',
+	author_email = 'xufeiyang0208@yeah.net',
+	url	= 'http://www.cnblogs.com/xufeiyang',
+	description = 'A simple printer of list'
+)
