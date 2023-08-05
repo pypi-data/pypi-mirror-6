5740f70ce966a15ab18932cfd8434a20a4a1ab14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:13 +0000

    Add package writing_style/0.0.6/writing_style-0.0.6.tar.gz

---
+++ packages/writing_style/writing_style-0.0.6.tar.gz/writing_style-0.0.6/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+
+def run_setup():
+    setup(
+        name='writing_style',
+        version='0.0.6',
+        description='Analyze text',
+        keywords = '',
+        url='https://github.com/sergeio/writing_style',
+        author='Sergei Orlov',
+        author_email='',
+        license='BSD',
+        packages=['writing_style'],
+        install_requires=[''],
+        test_suite='tests',
+        long_description='',
+        classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+        ],
+    )
+
+if __name__ == '__main__':
+    run_setup()
