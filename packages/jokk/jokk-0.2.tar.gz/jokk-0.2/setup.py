df4756325a0eab165d27b3bec234cfbfc263e4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:07 +0000

    Add package jokk/0.2/jokk-0.2.tar.gz

---
+++ packages/jokk/jokk-0.2.tar.gz/jokk-0.2/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+    jokk
+    ~~~~
+
+    RESTful mock api server.
+
+    :copyright: (c) 2014 Shinya Ohyanagi, All rights reserved.
+    :license: BSD, see LICENSE for more details.
+"""
+import os
+from setuptools import setup, find_packages
+
+requires = ['werkzeug']
+try:
+    import argparse
+except:
+    requires.append('argparse')
+
+app_name = 'jokk'
+
+rst_path = os.path.join(os.path.dirname(__file__), 'README.rst')
+description = ''
+with open(rst_path) as f:
+    description = f.read()
+
+setup(
+    name=app_name,
+    version='0.2',
+    author='Shinya Ohyanagi',
+    author_email='sohyanagi@gmail.com',
+    url='http://github.com/heavenshell/py-jokk',
+    description='RESTful Mock api server',
+    long_description=description,
+    license='BSD',
+    platforms='any',
+    packages=find_packages(exclude=['tests']),
+    package_dir={'': '.'},
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+    entry_points="""
+    [console_scripts]
+    jokk = jokk.server:main
+    """,
+    tests_require=[],
+    test_suite='tests'
+)
