21a01cc95def912daebf9e5cf4a8020c824857bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package Solution/2.5.2/Solution-2.5.2.tar.gz

---
+++ packages/Solution/Solution-2.5.2.tar.gz/Solution-2.5.2/solution/fields/__init__.py
@@ -0,0 +1,9 @@
+from .field import ValidationError, Field
+from .boolean import Boolean
+from .collection import Collection
+from .color import Color
+from .date import Date
+from .file import File
+from .number import Number
+from .select import Select, MultiSelect
+from .text import Text
