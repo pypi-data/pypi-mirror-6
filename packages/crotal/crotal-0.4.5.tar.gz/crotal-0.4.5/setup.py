96ebec8de9ae5f996afd09a90e5d32dc04ef1343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package crotal/0.4.5/crotal-0.4.5.tar.gz

---
+++ packages/crotal/crotal-0.4.5.tar.gz/crotal-0.4.5/setup.py
@@ -0,0 +1,28 @@
+#coding:utf-8
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "crotal",
+    version = "0.4.5",
+    packages = find_packages(),
+
+    include_package_data = True,
+
+    entry_points = {
+        'console_scripts' : [
+            'crotal = crotal.main:main'
+        ],
+    },
+    package_data = {
+        'crotal' : [ '*.data' ]
+    },
+    install_requires = ['Jinja2==2.7.1','markdown==2.3.1','PyYAML==3.10','Pygments==1.6'],
+    author = "Dinever",
+    author_email = 'dingpeixuan911@gmail.com',
+    url = "http://github.com/dinever/crotal",
+    description = 'A static site generator written in python.',
+)
