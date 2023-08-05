8147e66d9ff2ea8050c55dbb3932d3f238536a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package python_consistent_memcached/0.0.1/python_consistent_memcached-0.0.1.tar.gz

---
+++ packages/python_consistent_memcached/python_consistent_memcached-0.0.1.tar.gz/python_consistent_memcached-0.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+python_consistent_memcached
+======
+
+A pure python implementation of a consistent hasher for memcached.
+Its missrate is similar to continuum, but it has O(1) per request
+performane vs continiums O(lg(n)) per request performnace where n is
+the number of servers in your memcached collecion.
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='python_consistent_memcached',
+    version='0.0.1',
+    author='Adam DePrince',
+    author_email='adeprince@nypublicradio.org',
+    description='python-memcached with consistent hashing',
+    long_description=__doc__,
+    py_modules = [
+        "consistent_memcache/__init__",
+        "consistent_memcache/s3/__init__",
+        "consistent_memcache/s3/markhash",
+        ],
+    packages = ["consistent_memcache"],
+    zip_safe=True,
+    license='GPL',
+    include_package_data=True,
+    classifiers=[
+        ],
+    scripts = [
+        "scripts/ec2_markhash",
+        ],
+    url = "https://github.com/wnyc/python_consistent_memcached",
+    install_requires = [
+        'python-gflags',
+        'boto',
+        'python-memcached',
+        ]
+)
+
