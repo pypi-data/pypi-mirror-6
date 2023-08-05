827cd35d8d866a540cf9982100a616ef1850c0ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package qibuild/3.2.1/qibuild-3.2.1.tar.gz

---
+++ packages/qibuild/qibuild-3.2.1.tar.gz/qibuild-3.2.1/python/qidoc/builder.py
@@ -0,0 +1,95 @@
+import os
+
+from qisys import ui
+import qisys.sort
+
+
+class DocBuilder(object):
+    """ Build and install doc projects.
+
+    Initialize with a `base_project`, which will
+    be at the root of the dest dir when installed.
+
+    The `install` step is required to have redistributable
+    documentation folder, while you can browse the documentation
+    directly from the build dir.
+
+    """
+    def __init__(self, doc_worktree):
+        self.doc_worktree = doc_worktree
+        self.single = False
+        self.deps_solver = None
+        self.base_project = None
+        self.version = "latest"
+        self.hosted = True
+        self.build_type = ""
+        self.werror = False
+
+    def configure(self):
+        """ Configure the projects in the right order
+
+        """
+        projects = self.get_dep_projects()
+        configure_args = {
+            "version" : self.version,
+            "hosted"  : self.hosted,
+            "build_type"   : self.build_type,
+        }
+        for project in projects:
+            project.configure(**configure_args)
+
+    def build(self):
+        """ Build the projects in the right order,
+        making sure they are configured first
+
+        """
+        projects = self.get_dep_projects()
+        for i, project in enumerate(projects):
+            ui.info_count(i, len(projects),
+                          ui.green, "Building", ui.blue, project.name)
+            project.build(werror=self.werror)
+            ui.info(ui.green, "Doc generated in",
+                    ui.reset, ui.bold, project.html_dir)
+
+    def install(self, destdir):
+        """ Install the doc projects to a dest dir
+
+        """
+        projects = self.get_dep_projects()
+        for i, project in enumerate(projects):
+            if i == len(projects) - 1:
+                real_dest = destdir
+            else:
+                real_dest = os.path.join(destdir, project.dest)
+            project.dest = real_dest
+            ui.info_count(i, len(projects),
+                          ui.green, "Installing",
+                          ui.blue, project.name,
+                          ui.reset, "->", ui.white, real_dest)
+            configure_args = {
+                "version"   : self.version,
+                "hosted"    : self.hosted,
+                "build_type" : self.build_type,
+                "rel_paths" : True,
+            }
+            project.configure(**configure_args)
+            project.build()
+            project.install(real_dest)
+
+    def get_dep_projects(self):
+        """ Get the list of project deps
+
+        """
+        if self.single:
+            return [self.base_project]
+        projects = list()
+        dep_tree = dict()
+        for project in self.doc_worktree.doc_projects:
+            dep_tree[project.name] = project.depends
+        base_name = self.base_project.name
+        sorted_names = qisys.sort.topological_sort(dep_tree, [base_name])
+        for name in sorted_names:
+            project = self.doc_worktree.get_doc_project(name, raises=False)
+            if project:
+                projects.append(project)
+        return projects
---
+++ packages/qibuild/qibuild-3.2.1.tar.gz/qibuild-3.2.1/python/qitest/actions/run.py
@@ -0,0 +1,49 @@
+## Copyright (c) 2012, 2013 Aldebaran Robotics. All rights reserved.
+## Use of this source code is governed by a BSD-style license that can be
+## found in the COPYING file.
+
+""" Launch automatic tests
+
+"""
+
+import argparse
+import os
+import sys
+
+from qisys import ui
+import qisys.parsers
+import qibuild.test_runner
+import qitest.parsers
+import qitest.actions.list
+
+def configure_parser(parser):
+    """Configure parser for this action"""
+    qisys.parsers.default_parser(parser)
+    qitest.parsers.test_parser(parser)
+    parser.add_argument("qitest_json", help="path to a qitest.json file")
+
+def do(args):
+    """Main entry point"""
+    qitest_json = args.qitest_json
+    test_project = TestProject(qitest_json)
+    test_runner = qibuild.test_runner.ProjectTestRunner(test_project)
+    test_runner.cwd = os.path.dirname(args.qitest_json)
+    test_runner.pattern = args.pattern
+    test_runner.perf = args.perf
+    test_runner.coverage = args.coverage
+    test_runner.valgrind = args.valgrind
+    test_runner.verbose = args.verbose_tests
+    test_runner.num_cpus = args.num_cpus
+    test_runner.num_jobs = args.num_jobs
+    test_runner.nightly = args.nightly
+    res = test_runner.run()
+    if not res:
+        sys.exit(1)
+
+
+class TestProject(object):
+    """ A class to keep ProjectTestRunner happy """
+    def __init__(self, qitest_json):
+        self.qitest_json = qitest_json
+        self.build_directory = os.path.dirname(qitest_json)
+        self.sdk_directory = os.path.join(self.build_directory)
