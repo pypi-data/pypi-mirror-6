9b94cf5af1a066d19b90a5ebe6f5a0343cb1e8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package medialog.galleryviewlet/0.2.6/medialog.galleryviewlet-0.2.6.tar.gz

---
+++ packages/medialog.galleryviewlet/medialog.galleryviewlet-0.2.6.tar.gz/medialog.galleryviewlet-0.2.6/medialog/galleryviewlet/browser/viewlets.py
@@ -0,0 +1,38 @@
+from Acquisition import aq_inner
+from plone.app.layout.viewlets.common import ViewletBase
+from plone.memoize.instance import memoize
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.interface import implements, Interface
+from zope.component import getMultiAdapter
+
+from medialog.galleryviewlet import galleryviewletMessageFactory as _
+from medialog.galleryviewlet.settings import GalleryviewletSettings
+from medialog.galleryviewlet.settings import IGalleryviewletSettings
+
+#dont know if this is needed
+#from Products.Five import BrowserView
+#from Products.CMFCore.utils import getToolByName
+
+
+class GalleryViewlet(ViewletBase):
+    render = ViewPageTemplateFile('galleryviewlet.pt')
+    
+    implements(IGalleryviewletSettings)
+    
+    @property
+    @memoize
+    def gallerypath(self):
+		context=self.context
+		self.settings = GalleryviewletSettings(context)
+		
+		try:
+			portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+			portal = portal_state.portal()
+			path = str(self.settings.gallerypath)
+			if path.startswith('/'):
+				path = path[1:]
+				
+			return portal.restrictedTraverse(path, default=False)
+		except:
+			return False
+			
---
+++ packages/medialog.galleryviewlet/medialog.galleryviewlet-0.2.6.tar.gz/medialog.galleryviewlet-0.2.6/medialog/galleryviewlet/interfaces.py
@@ -0,0 +1,61 @@
+from zope.interface import Interface, Attribute
+from zope import schema
+from medialog.galleryviewlet import galleryviewletMessageFactory  as _
+
+#from plone.memoize.instance import memoize
+#from zope.component import getMultiAdapter
+#from OFS.interfaces import IItem
+#import urllib
+#from collective.plonetruegallery.vocabularies import \
+#    GallerySearchabelTextSourceBinder
+
+#from collective.plonetruegallery.utils import getGalleryAdapter
+ 
+ 
+class IGalleryviewletLayer(Interface):
+    """
+    marker interface for galleryviewlet layer
+    
+    """
+    
+class IGalleryviewlet(Interface):
+    """
+    marker interface for content types that can use the viewlet 
+    """
+
+    
+class IGalleryviewletUtilProtected(Interface):
+
+    def enable():
+        """
+        enable galleryviewlet on this object
+        """
+
+    def disable():
+        """
+        disable galleryviewlet on this object
+        """
+
+
+class IGalleryviewletUtil(Interface):
+
+    def enabled():
+        """
+        checks if galleryviewlet is enabled  
+        """
+
+    def view_enabled():
+        """
+        checks if the galleryviewlet is selected
+        """
+
+class IGalleryviewletSettings(Interface):
+    """
+    The actual galleryviewlet settings
+    """
+     
+    gallerypath = schema.TextLine(
+        title=_(u"label_width_title_galleryviewlet_setting", default=u"Which Gallery"),
+        description=_(u"label_width_description_galleryviewlet_setting", 
+        default=u"The path to the gallery you want to  show."),
+        required=True)
