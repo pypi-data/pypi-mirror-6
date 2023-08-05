465602522e543aa51543f712ff3a0f860c235ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:38 +0000

    Add package luxx_communication/0.1.3/luxx_communication-0.1.3.tar.gz

---
+++ packages/luxx_communication/luxx_communication-0.1.3.tar.gz/luxx_communication-0.1.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name="luxx_communication",
+      version="0.1.3",
+      description="Module for control of Omicron LuxX laser",
+      author="Roman Kiselev",
+      author_email="roman.kiselew@gmail.com",
+      py_modules=["luxx_communication"],
+      scripts=['bin/laser_info.py','bin/run_laser.py'],
+      license="GNU GPL v3",
+      url="https://pypi.python.org/pypi/luxx_communication/",
+      platforms=["Linux", "Windows"],
+     )
