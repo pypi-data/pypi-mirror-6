c694632656589235f195da69fceb536148939522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package dtcwt/0.8.0/dtcwt-0.8.0.tar.gz

---
+++ packages/dtcwt/dtcwt-0.8.0.tar.gz/dtcwt-0.8.0/tests/testagainstmatlab.py
@@ -0,0 +1,104 @@
+import os
+from nose.tools import raises
+from nose.plugins.attrib import attr
+
+import numpy as np
+from dtcwt import dtwavexfm2, dtwaveifm2, dtwavexfm2b, dtwaveifm2b, biort, qshift
+from dtcwt.lowlevel import coldfilt, colifilt
+from dtcwt.sampling import rescale_highpass
+
+from .util import assert_almost_equal, summarise_mat, assert_percentile_almost_equal
+
+## IMPORTANT NOTE ##
+
+# These tests match only a 'summary' matrix from MATLAB which is formed by
+# dividing a matrix into 9 parts thusly:
+#
+#  A | B | C
+# ---+---+---
+#  D | E | F
+# ---+---+---
+#  G | H | I
+#
+# Where A, C, G and I are NxN and N is some agreed 'apron' size. E is replaced
+# my it's element-wise mean and thus becomes 1x1. The remaining matrices are
+# replaced by the element-wise mean along the apropriate axis to result in a
+# (2N+1) x (2N+1) matrix. These matrices are compared.
+#
+# The rationale for this summary is that the corner matrices preserve
+# interesting edge-effects and some actual values whereas the interior matrices
+# preserve at least some information on their contents. Storing such a summary
+# matrix greatly reduces the amount of storage required.
+
+# Summary matching requires greater tolerance
+
+# We allow a little more tolerance for comparison with MATLAB
+TOLERANCE = 1e-5
+
+def assert_almost_equal_to_summary(a, summary, *args, **kwargs):
+    assert_almost_equal(summarise_mat(a), summary, *args, **kwargs)
+
+def assert_percentile_almost_equal_to_summary(a, summary, *args, **kwargs):
+    assert_percentile_almost_equal(summarise_mat(a), summary, *args, **kwargs)
+
+def setup():
+    global lena
+    lena = np.load(os.path.join(os.path.dirname(__file__), 'lena.npz'))['lena']
+
+    global verif
+    verif = np.load(os.path.join(os.path.dirname(__file__), 'verification.npz'))
+    
+def test_lena_loaded():
+    assert lena.shape == (512, 512)
+    assert lena.min() >= 0
+    assert lena.max() <= 1
+    assert lena.dtype == np.float32
+
+def test_lena_loaded():
+    assert verif is not None
+    assert 'lena_coldfilt' in verif
+
+def test_coldfilt():
+    h0o, g0o, h1o, g1o = biort('near_sym_b')
+    h0a, h0b, g0a, g0b, h1a, h1b, g1a, g1b = qshift('qshift_d')
+    A = coldfilt(lena, h1b, h1a)
+    assert_almost_equal_to_summary(A, verif['lena_coldfilt'], tolerance=TOLERANCE)
+
+def test_coldfilt():
+    h0o, g0o, h1o, g1o = biort('near_sym_b')
+    h0a, h0b, g0a, g0b, h1a, h1b, g1a, g1b = qshift('qshift_d')
+    A = colifilt(lena, g0b, g0a)
+    assert_almost_equal_to_summary(A, verif['lena_colifilt'], tolerance=TOLERANCE)
+
+def test_dtwavexfm2():
+    Yl, Yh, Yscale = dtwavexfm2(lena, 4, 'near_sym_a', 'qshift_a', include_scale=True)
+    assert_almost_equal_to_summary(Yl, verif['lena_Yl'], tolerance=TOLERANCE)
+
+    for idx, a in enumerate(Yh):
+        assert_almost_equal_to_summary(a, verif['lena_Yh_{0}'.format(idx)], tolerance=TOLERANCE)
+
+    for idx, a in enumerate(Yscale):
+        assert_almost_equal_to_summary(a, verif['lena_Yscale_{0}'.format(idx)], tolerance=TOLERANCE)
+
+def test_dtwavexfm2b():
+    Yl, Yh, Yscale = dtwavexfm2b(lena, 4, 'near_sym_b_bp', 'qshift_b_bp', include_scale=True)
+    assert_almost_equal_to_summary(Yl, verif['lena_Ylb'], tolerance=TOLERANCE)
+
+    for idx, a in enumerate(Yh):
+        assert_almost_equal_to_summary(a, verif['lena_Yhb_{0}'.format(idx)], tolerance=TOLERANCE)
+
+    for idx, a in enumerate(Yscale):
+        assert_almost_equal_to_summary(a, verif['lena_Yscaleb_{0}'.format(idx)], tolerance=TOLERANCE)
+
+def test_rescale_highpass():
+    # N.B we can only test bilinear rescaling since cpxinterb2b doesn't support Lanczos
+    Yl, Yh = dtwavexfm2b(lena, 3, 'near_sym_a', 'qshift_a')
+    X = Yh[2]
+    Xrescale = rescale_highpass(X, (X.shape[0]*3, X.shape[1]*3), 'bilinear')
+
+    # Almost equal in the rescale case is a hard thing to quantify. Due to the
+    # slight differences in interpolation method the odd pixel can very by
+    # quite an amount. Use a percentile approach to look at the bigger picture.
+    assert_percentile_almost_equal_to_summary(Xrescale, verif['lena_upsample'], 60, tolerance=TOLERANCE)
+
+# vim:sw=4:sts=4:et
---
+++ packages/dtcwt/dtcwt-0.8.0.tar.gz/dtcwt-0.8.0/tests/testxfm2.py
@@ -0,0 +1,96 @@
+import os
+from nose.tools import raises
+from nose.plugins.attrib import attr
+
+import numpy as np
+from dtcwt import dtwavexfm2, dtwaveifm2, biort, qshift
+
+TOLERANCE = 1e-12
+
+def setup():
+    global lena
+    lena = np.load(os.path.join(os.path.dirname(__file__), 'lena.npz'))['lena']
+
+def test_lena_loaded():
+    assert lena.shape == (512, 512)
+    assert lena.min() >= 0
+    assert lena.max() <= 1
+    assert lena.dtype == np.float32
+
+@attr('transform')
+def test_simple():
+    Yl, Yh = dtwavexfm2(lena)
+
+@attr('transform')
+def test_specific_wavelet():
+    Yl, Yh = dtwavexfm2(lena, biort=biort('antonini'), qshift=qshift('qshift_06'))
+
+def test_1d():
+    Yl, Yh = dtwavexfm2(lena[0,:])
+
+@raises(ValueError)
+def test_3d():
+    Yl, Yh = dtwavexfm2(np.dstack((lena, lena)))
+
+def test_simple_w_scale():
+    Yl, Yh, Yscale = dtwavexfm2(lena, include_scale=True)
+
+    assert len(Yscale) > 0
+    for x in Yscale:
+        assert x is not None
+
+def test_odd_rows():
+    Yl, Yh = dtwavexfm2(lena[:509,:])
+
+def test_odd_rows_w_scale():
+    Yl, Yh, Yscale = dtwavexfm2(lena[:509,:], include_scale=True)
+
+def test_odd_cols():
+    Yl, Yh = dtwavexfm2(lena[:,:509])
+
+def test_odd_cols_w_scale():
+    Yl, Yh, Yscale = dtwavexfm2(lena[:509,:509], include_scale=True)
+
+def test_odd_rows_and_cols():
+    Yl, Yh = dtwavexfm2(lena[:,:509])
+
+def test_odd_rows_and_cols_w_scale():
+    Yl, Yh, Yscale = dtwavexfm2(lena[:509,:509], include_scale=True)
+
+def test_rot_symm_modified():
+    # This test only checks there is no error running these functions, not that they work
+    Yl, Yh, Yscale = dtwavexfm2(lena, biort='near_sym_b_bp', qshift='qshift_b_bp', include_scale=True)
+    Z = dtwaveifm2(Yl, Yh, biort='near_sym_b_bp', qshift='qshift_b_bp')
+
+def test_0_levels():
+    Yl, Yh = dtwavexfm2(lena, nlevels=0)
+    assert np.all(np.abs(Yl - lena) < TOLERANCE)
+    assert len(Yh) == 0
+
+def test_0_levels_w_scale():
+    Yl, Yh, Yscale = dtwavexfm2(lena, nlevels=0, include_scale=True)
+    assert np.all(np.abs(Yl - lena) < TOLERANCE)
+    assert len(Yh) == 0
+    assert len(Yscale) == 0
+
+def test_integer_input():
+    # Check that an integer input is correctly coerced into a floating point
+    # array
+    Yl, Yh = dtwavexfm2([[1,2,3,4], [1,2,3,4]])
+    assert np.any(Yl != 0)
+
+def test_integer_perfect_recon():
+    # Check that an integer input is correctly coerced into a floating point
+    # array and reconstructed
+    A = np.array([[1,2,3,4], [5,6,7,8]], dtype=np.int32)
+    Yl, Yh = dtwavexfm2(A)
+    B = dtwaveifm2(Yl, Yh)
+    assert np.max(np.abs(A-B)) < 1e-5
+
+def test_float32_input():
+    # Check that an float32 input is correctly output as float32
+    Yl, Yh = dtwavexfm2(lena.astype(np.float32))
+    assert np.issubsctype(Yl.dtype, np.float32)
+    assert np.all(list(np.issubsctype(x.dtype, np.complex64) for x in Yh))
+
+# vim:sw=4:sts=4:et
