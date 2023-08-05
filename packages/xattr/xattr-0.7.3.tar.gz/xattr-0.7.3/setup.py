cdfc032ac61965b2a33bfc980cf8e6f0fee48070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package xattr/0.7.3/xattr-0.7.3.tar.gz

---
+++ packages/xattr/xattr-0.7.3.tar.gz/xattr-0.7.3/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup
+from distutils.command.build import build
+
+class cffi_build(build):
+    """This is a shameful hack to ensure that cffi is present when
+    we specify ext_modules. We can't do this eagerly because
+    setup_requires hasn't run yet.
+    """
+    def finalize_options(self):
+        from xattr.lib import ffi
+        self.distribution.ext_modules = [ffi.verifier.get_extension()]
+        build.finalize_options(self)
+
+VERSION = '0.7.3'
+DESCRIPTION = "Python wrapper for extended filesystem attributes"
+LONG_DESCRIPTION = """
+Extended attributes extend the basic attributes of files and directories
+in the file system.  They are stored as name:data pairs associated with
+file system objects (files, directories, symlinks, etc).
+
+Extended attributes are currently only available on Darwin 8.0+ (Mac OS X 10.4)
+and Linux 2.6+. Experimental support is included for Solaris and FreeBSD.
+"""
+
+CLASSIFIERS = filter(bool, map(str.strip,
+"""
+Environment :: Console
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Natural Language :: English
+Operating System :: MacOS :: MacOS X
+Operating System :: POSIX :: Linux
+Operating System :: POSIX :: BSD :: FreeBSD
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 3
+Topic :: Software Development :: Libraries :: Python Modules
+""".splitlines()))
+
+setup(
+    name="xattr",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    author="Bob Ippolito",
+    author_email="bob@redivi.com",
+    url="http://github.com/xattr/xattr",
+    license="MIT License",
+    packages=['xattr'],
+    ext_package='xattr',
+    platforms=['MacOS X', 'Linux', 'FreeBSD', 'Solaris'],
+    entry_points={
+        'console_scripts': [
+            "xattr = xattr.tool:main",
+        ],
+    },
+    install_requires=["cffi>=0.4"],
+    setup_requires=["cffi>=0.4"],
+    test_suite="xattr.tests.all_tests_suite",
+    zip_safe=False,
+    cmdclass={'build': cffi_build},
+)
---
+++ packages/xattr/xattr-0.7.3.tar.gz/xattr-0.7.3/xattr/tests/test_xattr.py
@@ -0,0 +1,63 @@
+import os
+from unittest import TestCase
+from tempfile import mkdtemp, NamedTemporaryFile
+
+import xattr
+
+
+class BaseTestXattr(object):
+    def test_attr(self):
+        x = xattr.xattr(self.tempfile)
+        self.assertEqual(x.keys(), [])
+        self.assertEqual(dict(x), {})
+
+        x['user.sopal'] = b'foo'
+        x['user.sop.foo'] = b'bar'
+        x[u'user.\N{SNOWMAN}'] = b'not a snowman'
+        del x
+
+        x = xattr.xattr(self.tempfile)
+        self.assertTrue('user.sopal' in x)
+        self.assertEqual(x['user.sopal'], b'foo')
+        self.assertTrue('user.sop.foo' in x)
+        self.assertEqual(x['user.sop.foo'], b'bar')
+        self.assertTrue(u'user.\N{SNOWMAN}' in x)
+        self.assertEqual(x[u'user.\N{SNOWMAN}'],
+                         b'not a snowman')
+        
+        del x[u'user.\N{SNOWMAN}']
+        del x['user.sop.foo']
+        del x
+
+        x = xattr.xattr(self.tempfile)
+        self.assertTrue('user.sop.foo' not in x)
+
+    def test_symlink_attrs(self):
+        symlinkPath = self.tempfilename + '.link'
+        os.symlink(self.tempfilename, symlinkPath)
+        try:
+            symlink = xattr.xattr(symlinkPath, options=xattr.XATTR_NOFOLLOW)
+            realfile = xattr.xattr(self.tempfilename)
+            symlink['user.islink'] = b'true'
+            self.assertEqual(dict(realfile), {})
+            self.assertEqual(symlink['user.islink'], b'true')
+        finally:
+            os.remove(symlinkPath)
+
+
+class TestFile(TestCase, BaseTestXattr):
+    def setUp(self):
+        self.tempfile = NamedTemporaryFile()
+        self.tempfilename = self.tempfile.name
+
+    def tearDown(self):
+        self.tempfile.close()
+
+
+class TestDir(TestCase, BaseTestXattr):
+    def setUp(self):
+        self.tempfile = mkdtemp()
+        self.tempfilename = self.tempfile
+
+    def tearDown(self):
+        os.rmdir(self.tempfile)
