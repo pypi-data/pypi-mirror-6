0583003fd17e4ac26de7210b7a17adf83aa90c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:53 +0000

    Add package torext/0.9.1/torext-0.9.1.tar.gz

---
+++ packages/torext/torext-0.9.1.tar.gz/torext-0.9.1/torext/templates/project/settings.py
@@ -0,0 +1,25 @@
+PROJECT = '{project_name}'
+
+LOCALE = 'en_US'
+
+PROCESSES = 1
+
+PORT = 8000
+
+DEBUG = True
+
+LOGGING = 'INFO'
+
+LOG_REQUEST = True
+
+LOG_RESPONSE = False
+
+TIME_ZONE = 'Asia/Shanghai'
+
+STATIC_PATH = 'static'
+
+TEMPLATE_PATH = 'template'
+
+LOGGING_IGNORE_URLS = [
+    '/favicon.ico',
+]
