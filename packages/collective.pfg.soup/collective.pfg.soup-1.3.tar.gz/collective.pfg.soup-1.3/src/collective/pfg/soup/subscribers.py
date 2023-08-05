b90773647ff1ab46e96b993b9ee4b43be8c25b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package collective.pfg.soup/1.3/collective.pfg.soup-1.3.tar.gz

---
+++ packages/collective.pfg.soup/collective.pfg.soup-1.3.tar.gz/collective.pfg.soup-1.3/src/collective/pfg/soup/subscribers.py
@@ -0,0 +1,30 @@
+try:
+    # Plone < 4.3
+    from zope.app.component.hooks import getSite
+except ImportError:
+    # Plone >= 4.3
+    from zope.component.hooks import getSite
+
+from Acquisition import aq_parent
+from Products.PloneFormGen.interfaces import IPloneFormGenForm
+from .interfaces import IPfgSoupAdapter
+from .storage import PfgCatalogFactory
+
+
+def create_catalogfactory(obj, event):
+    sm = getSite().getSiteManager()
+    sm.registerUtility(factory=PfgCatalogFactory, name=obj._soup_name)
+
+
+def rebuild_catalog(obj, event):
+    parent = aq_parent(obj)
+    while True:
+        if not parent or IPloneFormGenForm.providedBy(parent):
+            break
+        parent = aq_parent(parent)
+    if parent is None:
+        return
+    for name in parent.contentIds():
+        sub = parent[name]
+        if IPfgSoupAdapter.providedBy(sub):
+            sub.get_soup().rebuild()
