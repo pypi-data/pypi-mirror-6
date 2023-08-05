2216ac21b2ad024a6c97db3f7d082ad808315b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:35 +0000

    Add package tgext.odt/0.2/tgext.odt-0.2.tar.gz

---
+++ packages/tgext.odt/tgext.odt-0.2.tar.gz/tgext.odt-0.2/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import sys, os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+install_requires=[
+    "TurboGears2 >= 2.1.4",
+    "tgext.pluggable",
+    "lxml",
+    "webhelpers",
+    "py3o.template",
+    "pyjon.utils"
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
+
+setup(
+    name='tgext.odt',
+    version='0.2',
+    description='tgext.odt is a Pluggable application for TurboGears2 that allows the rendering of .odt (openoffice/libreoffice) documents as an output templates.',
+    long_description=README,
+    author='Puria Nafisi Azizi',
+    author_email='puria.nafisi@axant.it',
+    url='https://github.com/puria/tgext.odt',
+    keywords='turbogears2.application',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=install_requires,
+    include_package_data=True,
+    package_data={'tgapp.tgextodt': ['i18n/*/LC_MESSAGES/*.mo',
+                                 'templates/*/*',
+                                 'public/*/*']},
+    entry_points="""
+    """,
+    zip_safe=False
+)
