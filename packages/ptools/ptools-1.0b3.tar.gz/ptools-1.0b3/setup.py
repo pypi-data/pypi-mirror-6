8134223bba94724e9c3bfb29ed5c3633e582b518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:57 +0000

    Add package ptools/1.0b3/ptools-1.0b3.tar.gz

---
+++ packages/ptools/ptools-1.0b3.tar.gz/ptools-1.0b3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup
+
+setup (
+    name='ptools',
+    version='1.0b3',
+    description='Cross Platform Process Infomation (cmdline, environ) and Management',
+    author='The Test People',
+    author_email='tony.simpson@thetestpeople.com',
+    maintainer='Tony Simpson',
+    url='https://github.com/tonysimpson/ptools',
+    license='MIT',
+    packages=['ptools'],
+    long_description=open('README').read(),
+)
+
