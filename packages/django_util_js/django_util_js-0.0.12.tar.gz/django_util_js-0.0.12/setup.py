39558af72a7707d7afa4ac78d18e35b60ccdeb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package django_util_js/0.0.12/django_util_js-0.0.12.tar.gz

---
+++ packages/django_util_js/django_util_js-0.0.12.tar.gz/django_util_js-0.0.12/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import django_util_js
+
+setup(
+    name="django_util_js",
+    version=django_util_js.__version__,
+    zip_safe=False,
+    platforms='any',
+    packages=['django_util_js'],
+    install_requires=['django'],
+    url="https://github.com/dantezhu/django_util_js",
+    license="BSD",
+    author="dantezhu",
+    author_email="zny2008@gmail.com",
+    description="django's util in javascript. such as url_for etc.",
+    )
