d9dfc5416da179a28d57907a160196c745696efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:33 +0000

    Add package yt.recipe.shell/0.1.5/yt.recipe.shell-0.1.5.tar.gz

---
+++ packages/yt.recipe.shell/yt.recipe.shell-0.1.5.tar.gz/yt.recipe.shell-0.1.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(name='yt.recipe.shell',
+      version='0.1.5',
+      description='A buildout recipe to add in shell commands',
+      author='Yusuke Tsutsumi',
+      author_email='yusuke@yusuketsutsumi.com',
+      url='http://github.com/toumorokoshi/yt.recipe.shell',
+      packages=['yt.recipe'],
+      namespace_packages=['yt'],
+      long_description=open('README.rst').read(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Build Tools',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ],
+      entry_points={'zc.buildout': ['default = yt.recipe.shell:Shell']},
+)
