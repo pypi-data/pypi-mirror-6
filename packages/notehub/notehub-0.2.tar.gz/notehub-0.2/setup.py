18fe4c12806f7f262a15e7267777173250610ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:27 +0000

    Add package notehub/0.2/notehub-0.2.tar.gz

---
+++ packages/notehub/notehub-0.2.tar.gz/notehub-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+readme = open('README.md').read()
+setup(name='notehub',
+      version='0.2',
+      author='Sean Watson',
+      license='MIT',
+      description='A simple wrapper for the Notehub.org API.',
+      long_description=readme,
+      keywords='notehub',
+      url='https://github.com/seanwatson/notehub',
+      py_modules=['notehub'],
+      install_requires=['requests'])
