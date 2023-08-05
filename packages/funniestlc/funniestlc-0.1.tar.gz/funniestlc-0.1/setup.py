ad95fe852e0470358a141ce02d9773548ce6c26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:07 +0000

    Add package funniestlc/0.1/funniestlc-0.1.tar.gz

---
+++ packages/funniestlc/funniestlc-0.1.tar.gz/funniestlc-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='funniestlc',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://bitbucket.org/leechau/pynote',
+      author='Li Chao',
+      author_email='leechau@126.com',
+      license='MIT',
+      packages=['funniestlc'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
