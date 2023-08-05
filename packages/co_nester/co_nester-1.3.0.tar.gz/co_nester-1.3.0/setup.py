3fc6e6ed8b2e4ad310cb7a5ac44d7438d23609fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:43 +0000

    Add package co_nester/1.3.0/co_nester-1.3.0.tar.gz

---
+++ packages/co_nester/co_nester-1.3.0.tar.gz/co_nester-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'co_nester',
+		version = '1.3.0',
+		py_modules = ['co_nester'],
+		author = 'hfpython',
+		author_email = 'hfpython@headfirstlabs.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple printer of nester lists',
+	)
