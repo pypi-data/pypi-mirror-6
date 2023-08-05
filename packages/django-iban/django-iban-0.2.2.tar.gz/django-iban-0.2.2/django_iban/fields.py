239800dcfa11a42b01611c175833ba9a285c44e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:56 +0000

    Add package django-iban/0.2.2/django-iban-0.2.2.tar.gz

---
+++ packages/django-iban/django-iban-0.2.2.tar.gz/django-iban-0.2.2/django_iban/fields.py
@@ -0,0 +1,32 @@
+from django.db import models
+from .validators import iban_validator, swift_bic_validator
+
+
+# From: https://en.wikipedia.org/wiki/International_Bank_Account_Number
+# An IBAN consists of up to 34 alphanumeric characters.
+class IBANField(models.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 34)
+        super(IBANField, self).__init__(*args, **kwargs)
+        self.validators.append(iban_validator)
+
+
+# From: https://en.wikipedia.org/wiki/ISO_9362
+# A SWIFT-BIC consists of up to 11 alphanumeric characters.
+class SWIFTBICField(models.CharField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('max_length', 11)
+        super(SWIFTBICField, self).__init__(*args, **kwargs)
+        self.validators.append(swift_bic_validator)
+
+
+# If south is installed, ensure that IBANField will be introspected just
+# like a normal CharField
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^django_iban\.fields\.IBANField"])
+    add_introspection_rules([], ["^django_iban\.fields\.SWIFTBICField"])
+except ImportError:
+    pass
