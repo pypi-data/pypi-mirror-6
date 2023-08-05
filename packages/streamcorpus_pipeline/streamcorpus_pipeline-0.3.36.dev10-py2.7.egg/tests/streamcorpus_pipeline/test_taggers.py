28089bdd20f97c5d4395d4be5787750ae41f4a34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package streamcorpus_pipeline/0.3.36.dev10/streamcorpus_pipeline-0.3.36.dev10-py2.7.egg

---
+++ packages/streamcorpus_pipeline/streamcorpus_pipeline-0.3.36.dev10-py2.7.egg/tests/streamcorpus_pipeline/test_taggers.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+import shutil
+import tempfile
+
+import pytest
+
+from streamcorpus_pipeline._taggers import name_align_labels, line_offset_align_labels, byte_offset_align_labels
+from tests.streamcorpus_pipeline._test_data import get_john_smith_tagged_by_lingpipe_path
+
+
+def _test_config():
+    return {
+        'tagger_id': 'lingpipe',
+        'annotator_id': '',
+        'chain_selector': 'ALL',
+    }
+
+
+def test_name_align_labels():
+    config = _test_config()
+    transform = name_align_labels(config)
+    _test_tagger_transform(transform)
+
+
+def test_line_offset_align_labels():
+    config = _test_config()
+    transform = line_offset_align_labels(config)
+    _test_tagger_transform(transform)
+
+
+def test_byte_offset_align_labels():
+    config = _test_config()
+    transform = byte_offset_align_labels(config)
+    _test_tagger_transform(transform)
+
+
+def _test_tagger_transform(transform):
+    path = get_john_smith_tagged_by_lingpipe_path()
+    with tempfile.NamedTemporaryFile(suffix='.sc') as tf:
+        shutil.copyfile(path, tf.name)
+        transform.process_path(tf.name)
+        # TODO: check some properties of resulting file
