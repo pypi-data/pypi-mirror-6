b18aca4f00decbdcb8cdaf6ddd65a256270dc652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:50 +0000

    Add package TypedInterpolation/0.0.1/TypedInterpolation-0.0.1.tar.gz

---
+++ packages/TypedInterpolation/TypedInterpolation-0.0.1.tar.gz/TypedInterpolation-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='TypedInterpolation',
+    version='0.0.1',
+    url=None,
+    license='GPLv3+',
+    author='Fink Dennis',
+    author_email='dennis.fink@c3l.lu',
+    descripts='Interpolations for configparser which uses ast.literal_eval.',
+    long_description=None,
+    py_modules=['typedinterpolation'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+        'Topic :: Utilities',
+        'Topic :: Other/Nonlisted Topic',
+    ],
+    keywords='configparser interpolation typed',
+)
