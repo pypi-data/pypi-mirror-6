d07db9ccb7c71d62fd7ed63c5812092d08254d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:49 +0000

    Add package vcspull/0.0.7/vcspull-0.0.7.tar.gz

---
+++ packages/vcspull/vcspull-0.0.7.tar.gz/vcspull-0.0.7/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf8 - *-
+"""vcspull lives at <https://github.com/tony/vcspull>.
+
+vcspull
+-------
+
+Mass update git, hg and svn repos simultaneously from YAML / JSON file.
+
+"""
+import sys
+from setuptools import setup
+
+with open('requirements.pip') as f:
+    install_reqs = [line for line in f.read().split('\n') if line]
+    tests_reqs = []
+
+if sys.version_info < (2, 7):
+    install_reqs += ['argparse']
+    tests_reqs += ['unittest2']
+
+import re
+VERSIONFILE = "vcspull/__init__.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    __version__ = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+setup(
+    name='vcspull',
+    version=__version__,
+    url='http://github.com/tony/vcspull/',
+    download_url='https://pypi.python.org/pypi/vcspull',
+    license='BSD',
+    author='Tony Narlock',
+    author_email='tony@git-pull.com',
+    description='Mass update git, hg and svn repos simultaneously from '
+                'YAML / JSON file.',
+    long_description=open('README.rst').read(),
+    include_package_data=True,
+    install_requires=install_reqs,
+    tests_require=tests_reqs,
+    test_suite='vcspull.testsuite',
+    zip_safe=False,
+    packages=['vcspull', 'vcspull.testsuite', 'vcspull.repo', 'vcspull._vendor', 'vcspull._vendor.colorama'],
+    scripts=['pkg/vcspull.bash', 'pkg/vcspull.zsh', 'pkg/vcspull.tcsh'],
+    entry_points=dict(console_scripts=['vcspull=vcspull:cli.main']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "License :: OSI Approved :: BSD License",
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        "Topic :: Utilities",
+        "Topic :: System :: Shells",
+    ],
+)
---
+++ packages/vcspull/vcspull-0.0.7.tar.gz/vcspull-0.0.7/vcspull/__init__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Manage multiple git, mercurial, svn repositories from a YAML / JSON file.
+
+vcspull
+~~~~~~~
+
+:copyright: Copyright 2013 Tony Narlock.
+:license: BSD, see LICENSE for details
+
+"""
+
+from __future__ import absolute_import, division, print_function, \
+    with_statement, unicode_literals
+
+__title__ = 'vcspull'
+__version__ = '0.0.7'
+__author__ = 'Tony Narlock'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2013 Tony Narlock'
+
+from .util import expand_config, get_repos, mkdir_p, run, scan, which
+from .log import LogFormatter, DebugLogFormatter, RepoLogFormatter, \
+    RepoFilter
+from .cli import ConfigFileCompleter, command_load, find_configs, get_parser, \
+    get_repos_new, in_dir, is_config_file, load_configs, main, scan_repos, \
+    setup_logger, update, validate_schema
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
