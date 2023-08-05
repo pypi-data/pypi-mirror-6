843166347ac8749dccc89a4665c3fbaf115daaea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:07 +0000

    Add package digui/0.0.2/digui-0.0.2.zip

---
+++ packages/digui/digui-0.0.2.zip/digui-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'digui',
+    version = '0.0.2',
+    py_modules = ['digui'],
+    author = 'rhythmer',
+    author_email = 'whynotchenrenyu@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple func',
+    )
