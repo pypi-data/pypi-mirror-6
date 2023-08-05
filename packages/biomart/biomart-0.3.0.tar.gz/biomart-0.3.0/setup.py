0d9a476c9574ec39fde4c9e68b3c3ff536a38faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package biomart/0.3.0/biomart-0.3.0.tar.gz

---
+++ packages/biomart/biomart-0.3.0.tar.gz/biomart-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='biomart',
+    version='0.3.0',
+    author='Sebastien Briois',
+    author_email='sebriois@gmail.com',
+    packages=['biomart','biomart.test'],
+    scripts=['bin/examples.py'],
+    license='BSD',
+    description='Python API that consumes the biomart webservice',
+    long_description=open('README.txt').read(),
+    install_requires=[ "requests>=2.2" ],
+    test_suite = 'biomart.test.suite',
+)
