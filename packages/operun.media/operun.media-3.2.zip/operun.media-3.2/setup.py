5d4ef1cbe7a00691680eb53d97d1b1be6117b877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:16 +0000

    Add package operun.media/3.2/operun.media-3.2.zip

---
+++ packages/operun.media/operun.media-3.2.zip/operun.media-3.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '3.2'
+
+setup(name='operun.media',
+      version=version,
+      description="A product to integrate any type of media to Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='Plone Audio Video Media',
+      author='operun.de by Stefan Antonelli',
+      author_email='info@operun.de',
+      url='https://github.com/starzel/operun.media',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['operun'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require = {
+        'test': [
+            'plone.app.testing',
+            'plone.act',
+            'robotsuite',
+            'robotframework-selenium2library',
+            ]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
