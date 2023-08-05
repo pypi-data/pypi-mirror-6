b40228f2ea8d6dbc4a2bd00f1b4fcd3a01cb32b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:57 +0000

    Add package log-colorizer/1.5/log_colorizer-1.5.tar.gz

---
+++ packages/log-colorizer/log_colorizer-1.5.tar.gz/log_colorizer-1.5/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Log colorizer
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = "1.5"
+
+
+options = dict(
+    name="log_colorizer",
+    version=VERSION,
+    description="A color formater for python logging",
+    long_description=__doc__,
+    author="Kozea",
+    author_email="florian.mounier@kozea.fr",
+    license="BSD",
+    platforms="Any",
+    use_2to3=True,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules"])
+
+setup(**options)
