a1ee4abf74802ea2c983138acd60c155d56d998a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:29 +0000

    Add package WsgiDAV/1.0.0/WsgiDAV-1.0.0.zip

---
+++ packages/WsgiDAV/WsgiDAV-1.0.0.zip/WsgiDAV-1.0.0/tests/test_litmus.py
@@ -0,0 +1,181 @@
+# -*- coding: iso-8859-1 -*-
+# (c) 2009-2013 Martin Wendt and contributors; see WsgiDAV http://wsgidav.googlecode.com/
+# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+"""
+    Run litmus against WsgiDAV server.
+"""
+from tempfile import gettempdir
+from wsgidav.wsgidav_app import DEFAULT_CONFIG, WsgiDAVApp
+from wsgidav.fs_dav_provider import FilesystemProvider
+import os
+import unittest
+import subprocess
+from multiprocessing.process import Process
+import time
+
+
+
+def run_wsgidav_server(with_auth, with_ssl):
+    """Start blocking WsgiDAV server (called as a separate process)."""
+    package_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    share_path = os.path.join(gettempdir(), "wsgidav-test")
+    if not os.path.exists(share_path):
+        os.mkdir(share_path)
+
+    provider = FilesystemProvider(share_path)
+
+    config = DEFAULT_CONFIG.copy()
+    config.update({
+        "host": "127.0.0.1",
+        "port": 8080,
+        "provider_mapping": {"/": provider},
+        "domaincontroller": None, # None: domain_controller.WsgiDAVDomainController(user_mapping)
+        "user_mapping": {},
+        "verbose": 0,
+        "enable_loggers": [],
+        "propsmanager": True,      # None: no property manager
+        "locksmanager": True,      # True: use lock_manager.LockManager
+        "domaincontroller": None,  # None: domain_controller.WsgiDAVDomainController(user_mapping)
+        })
+
+    if with_auth:
+        config.update({
+            "user_mapping": {"/": {"tester": {"password": "secret",
+                                              "description": "",
+                                              "roles": [],
+                                              },
+                                       },
+                                 },
+            "acceptbasic": True,
+            "acceptdigest": False,
+            "defaultdigest": False,
+            })
+
+    if with_ssl:
+        config.update({
+            "ssl_certificate": os.path.join(package_path, "wsgidav/server/sample_bogo_server.crt"),
+            "ssl_private_key": os.path.join(package_path, "wsgidav/server/sample_bogo_server.key"),
+            "ssl_certificate_chain": None,
+            # "acceptdigest": True,
+            # "defaultdigest": True,
+            })
+
+    app = WsgiDAVApp(config)
+
+    from wsgidav.server.run_server import _runBuiltIn
+    _runBuiltIn(app, config, None)
+    # blocking...
+
+
+
+#===============================================================================
+# WsgiDAVServerTest
+#===============================================================================
+
+class WsgiDAVLitmusTest(unittest.TestCase):
+    """Test the built-in WsgiDAV server with cadaver."""
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    ############################################################################
+
+    def test_litmus_with_authentication(self):
+        """Run litmus test suite on HTTP with authentification.
+
+        This test passes
+        """
+        try:
+            proc = Process(target=run_wsgidav_server, args=(True, False))
+            proc.daemon = True
+            proc.start()
+            time.sleep(1)
+
+            try:
+                self.assertEqual(subprocess.call(["litmus", "http://127.0.0.1:8080/", "tester", "secret"]),
+                                 0,
+                                 "litmus suite failed: check the log")
+            except OSError:
+                print "*" * 70
+                print "This test requires the litmus test suite."
+                print "See http://www.webdav.org/neon/litmus/"
+                print "*" * 70
+                raise
+
+        finally:
+            proc.terminate()
+            proc.join()
+
+
+    ############################################################################
+
+
+    # The test with anonymous access fails here:
+    #
+#  0. init.................. pass
+#  1. begin................. pass
+#  2. expect100............. FAIL (timeout waiting for interim response)
+#  3. finish................ pass
+
+    # def test_litmus_anonymous(self):
+    #     """Run litmus test suite as anonymous."""
+    #     try:
+    #         proc = Process(target=run_wsgidav_server, args=(False, False))
+    #         proc.daemon = True
+    #         proc.start()
+    #         time.sleep(1)
+
+    #         try:
+    #             self.assertEqual(subprocess.call(["litmus", "http://localhost:8080/"]),
+    #                              0,
+    #                              "litmus suite failed: check the log")
+    #         except OSError:
+    #             print "This test requires the litmus test suite."
+    #             print "See http://www.webdav.org/neon/litmus/"
+    #             raise
+
+    #     finally:
+    #         proc.terminate()
+    #         proc.join()
+
+
+    ############################################################################
+
+
+    # def test_litmus_with_ssl_and_authentication(self):
+    #     """Run litmus test suite on SSL / HTTPS with authentification."""
+
+    #     try:
+    #         proc = Process(target=run_wsgidav_server, args=(True, True))
+    #         proc.daemon = True
+    #         proc.start()
+    #         time.sleep(1)
+
+    #         try:
+    #             self.assertEqual(subprocess.call(["litmus", "https://127.0.0.1:8080/", "tester", "secret"]),
+    #                              0,
+    #                              "litmus suite failed: check the log")
+    #         except OSError:
+    #             print "*" * 70
+    #             print "This test requires the litmus test suite."
+    #             print "See http://www.webdav.org/neon/litmus/"
+    #             print "*" * 70
+    #             raise
+
+    #     finally:
+    #         proc.terminate()
+    #         proc.join()
+
+
+#===============================================================================
+# suite
+#===============================================================================
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/WsgiDAV/WsgiDAV-1.0.0.zip/WsgiDAV-1.0.0/tests/test_property_manager.py
@@ -0,0 +1,103 @@
+# -*- coding: iso-8859-1 -*-
+# (c) 2009-2013 Martin Wendt and contributors; see WsgiDAV http://wsgidav.googlecode.com/
+# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+"""Unit test for property_manager.py"""
+from tempfile import gettempdir
+import unittest
+
+import os
+from wsgidav import property_manager
+
+#===============================================================================
+# BasicTest
+#===============================================================================
+class BasicTest(unittest.TestCase):
+    """Test property_manager.PropertyManager()."""
+    respath = "/dav/res"
+
+#     @classmethod
+#     def suite(cls):
+#         """Return test case suite (so we can control the order)."""
+#         suite = TestSuite()
+#         suite.addTest(cls("testPreconditions"))
+#         suite.addTest(cls("testOpen"))
+#         suite.addTest(cls("testValidation"))
+#         suite.addTest(cls("testReadWrite"))
+#         return suite
+
+
+    def setUp(self):
+        self.pm = property_manager.PropertyManager()
+        self.pm._verbose = 2
+
+
+    def tearDown(self):
+        self.pm._close()
+        self.pm = None
+
+
+    def testPreconditions(self):
+        """Environment must be set."""
+        self.assertTrue(__debug__, "__debug__ must be True, otherwise asserts are ignored")
+
+
+    def testOpen(self):
+        """Property manager should be lazy opening on first access."""
+        pm = self.pm
+        assert not pm._loaded, "PM must be closed until first access"
+        pm.getProperties(self.respath)
+        assert pm._loaded, "PM must be opened after first access"
+
+
+    def testValidation(self):
+        """Property manager should raise errors on bad args."""
+        pm = self.pm
+        self.assertRaises(AssertionError,
+                          pm.writeProperty, None, "{ns1:}foo", "hurz", False)
+        # name must have a namespace
+#        self.assertRaises(AssertionError,
+#                          pm.writeProperty, "/dav/res", "foo", "hurz", False)
+        self.assertRaises(AssertionError,
+                          pm.writeProperty, "/dav/res", None, "hurz", False)
+        self.assertRaises(AssertionError,
+                          pm.writeProperty, "/dav/res", "{ns1:}foo", None, False)
+
+        assert pm._dict is None, "No properties should have been created by this test"
+
+
+    def testReadWrite(self):
+        """Property manager should raise errors on bad args."""
+        pm = self.pm
+        url = "/dav/res"
+        pm.writeProperty(url, "foo", "my name is joe")
+        assert pm.getProperty(url, "foo") == "my name is joe"
+
+
+
+#===============================================================================
+# ShelveTest
+#===============================================================================
+class ShelveTest(BasicTest):
+    """Test property_manager.ShelvePropertyManager()."""
+
+    def setUp(self):
+        self.path = os.path.join(gettempdir(), "wsgidav-props.shelve")
+        # Note: os.remove(self.path) does not work, because Shelve may append
+        # a file extension.
+#        if os.path.exists(self.path):
+#            os.remove(self.path)
+        self.pm = property_manager.ShelvePropertyManager(self.path)
+        self.pm._verbose = 1
+
+    def tearDown(self):
+#        self.pm.storage.clear()
+        self.pm._close()
+        self.pm = None
+#        os.remove(self.path)
+
+
+#===============================================================================
+
+
+if __name__ == "__main__":
+    unittest.main()
