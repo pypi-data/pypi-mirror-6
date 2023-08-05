f1f823135011871d1251db4e0f7d66a669b9829a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:23 +0000

    Add package cmsplugin_contact_plus/1.1.3/cmsplugin_contact_plus-1.1.3.tar.gz

---
+++ packages/cmsplugin_contact_plus/cmsplugin_contact_plus-1.1.3.tar.gz/cmsplugin_contact_plus-1.1.3/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup 
+
+setup(
+    name = 'cmsplugin_contact_plus',
+    version = '1.1.3', 
+    packages = ['cmsplugin_contact_plus',],
+    license = 'BSD License',
+    url = 'https://github.com/arteria/cmsplugin-contact-plus/',
+    description = 'A django CMS plugin to dynamically create contact forms.',
+    long_description = open('README.md').read()+"\n",
+    author = 'arteria GmbH',
+    author_email = 'admin@arteria.ch',
+    install_requires = ['django-inline-ordering'] #TODO: add others
+)
+# eof
+
