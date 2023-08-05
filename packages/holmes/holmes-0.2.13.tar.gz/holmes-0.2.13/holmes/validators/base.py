f822242b69b3380b7aaf92b78ea06c3ce6e17aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:37 +0000

    Add package holmes/0.2.13/holmes-0.2.13.tar.gz

---
+++ packages/holmes/holmes-0.2.13.tar.gz/holmes-0.2.13/holmes/validators/base.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urlparse
+
+
+class Validator(object):
+    def __init__(self, reviewer):
+        self.reviewer = reviewer
+
+    def is_absolute(self, url):
+        return bool(urlparse.urlparse(url).scheme)
+
+    def rebase(self, url):
+        return urlparse.urljoin(self.page_url.rstrip('/'), url)
+
+    @property
+    def page_uuid(self):
+        return self.reviewer.page_uuid
+
+    @property
+    def page_url(self):
+        return self.reviewer.page_url
+
+    @property
+    def review(self):
+        return self.reviewer.review_dao
+
+    @property
+    def config(self):
+        return self.reviewer.config
+
+    def validate(self):
+        return True
+
+    def to_gzip(self, content):
+        return content.encode('zip')
+
+    def enqueue(self, *url):
+        self.reviewer.enqueue(*url)
+
+    def add_fact(self, key, value, title, unit='value'):
+        self.reviewer.add_fact(key, value, title, unit)
+
+    def add_violation(self, key, title, description, points):
+        self.reviewer.add_violation(key, title, description, points)
