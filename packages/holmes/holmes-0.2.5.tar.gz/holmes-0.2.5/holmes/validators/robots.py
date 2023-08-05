f57f6386ec7a898f44c9ebf7d5b4255248fb5df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:20 +0000

    Add package holmes/0.2.5/holmes-0.2.5.tar.gz

---
+++ packages/holmes/holmes-0.2.5.tar.gz/holmes-0.2.5/holmes/validators/robots.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from holmes.validators.base import Validator
+
+
+class RobotsValidator(Validator):
+    def validate(self):
+        response = self.review.data['robots.response']
+
+        if response.status_code > 399:
+            self.add_violation(
+                key='robots.not_found',
+                title='Robots not found',
+                description='',
+                points=100
+            )
+            return
+
+        if not response.text.strip():
+            self.add_violation(
+                key='robots.empty',
+                title='Empty robots file',
+                description='',
+                points=100
+            )
