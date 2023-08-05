a502e0f94bf855e64b64aebedfe6a24a96659d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:46 +0000

    Add package holmes/0.4.3/holmes-0.4.3.tar.gz

---
+++ packages/holmes/holmes-0.4.3.tar.gz/holmes-0.4.3/holmes/models/keys.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship
+
+from holmes.models import Base
+
+
+class Key(Base):
+    __tablename__ = "keys"
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column('name', sa.String(2000), nullable=False)
+
+    facts = relationship("Fact", cascade="all,delete", backref="key")
+    violations = relationship("Violation", cascade="all,delete", backref="key")
+
+    def __str__(self):
+        return '%s' % (self.name)
+
+    def __repr__(self):
+        return str(self)
+
+    @classmethod
+    def get_or_create(cls, db, key_name):
+        key = db.query(Key).filter(Key.name == key_name).scalar()
+
+        if not key:
+            key = Key(name=key_name)
+
+        return key
