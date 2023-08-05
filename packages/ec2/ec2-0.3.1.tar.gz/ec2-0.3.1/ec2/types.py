64ed72f9eda6128f99f9b7b0983624d7a5efcc9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:53 +0000

    Add package ec2/0.3.1/ec2-0.3.1.tar.gz

---
+++ packages/ec2/ec2-0.3.1.tar.gz/ec2-0.3.1/ec2/types.py
@@ -0,0 +1,28 @@
+"""
+ec2.types
+~~~~~~~~~
+
+:copyright: (c) 2014 by Matt Robenolt.
+:license: BSD, see LICENSE for more details.
+"""
+
+from ec2.connection import get_connection
+from ec2.base import objects_base
+
+
+class instances(objects_base):
+    "Singleton to stem off queries for instances"
+
+    @classmethod
+    def _all(cls):
+        "Grab all AWS instances"
+        return [i for r in get_connection().get_all_instances() for i in r.instances]
+
+
+class security_groups(objects_base):
+    "Singleton to stem off queries for security groups"
+
+    @classmethod
+    def _all(cls):
+        "Grab all AWS Security Groups"
+        return get_connection().get_all_security_groups()
