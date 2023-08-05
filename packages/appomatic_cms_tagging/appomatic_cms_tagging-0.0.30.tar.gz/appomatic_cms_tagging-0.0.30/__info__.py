cb9e0876533b8fcfd27ff5a3d5a8f91e108c4dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package appomatic_cms_tagging/0.0.30/appomatic_cms_tagging-0.0.30.tar.gz

---
+++ packages/appomatic_cms_tagging/appomatic_cms_tagging-0.0.30.tar.gz/appomatic_cms_tagging-0.0.30/__info__.py
@@ -0,0 +1,9 @@
+version = "0.0.30"
+author = "RedHog (Egil Moeller)"
+author_email = "egil.moller@freecode.no"
+license = "GPL"
+url = "http://github.com/redhog/appomatic"
+name = "appomatic_cms_tagging"
+description = "Adds tagging support to django-cms"
+keywords = "appomatic django-tagging django-cms"
+install_requires = ['appomatic_django_cms=='+version, 'appomatic_tagging=='+version]
