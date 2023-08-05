f3686c5033ae348829d6b32d8b2c073ebfd9214c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package infi.storagemodel/0.2.4/infi.storagemodel-0.2.4-py2.7.egg

---
+++ packages/infi.storagemodel/infi.storagemodel-0.2.4-py2.7.egg/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.4"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '082c29f0131b37cdd5b3bff67b26d27e3fceeebe'
+__git_head_subject__ = 'Finished release v0.2.4.'
+__git_head_message__ = '* release/v0.2.4:\n  STORAGEMODEL-247 use parted to get partition size and filesystem\n  STORAGEMODEL-249 _try_predicate logs acceptable exceptions as debug, with no information about the exception\n  STORAGEMODEL-250 get_scsi_test_unit_ready does not return False if device not ready, raises exception instead'
+__git_dirty_diff__ = ''
