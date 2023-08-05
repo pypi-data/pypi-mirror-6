017956a18a11d8b6ea9897ff20866401e49d9505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package hachibee-sphinx-theme/0.1.4/hachibee-sphinx-theme-0.1.4.tar.gz

---
+++ packages/hachibee-sphinx-theme/hachibee-sphinx-theme-0.1.4.tar.gz/hachibee-sphinx-theme-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hachibee-sphinx-theme',
+    version='0.1.4',
+    author='OGURA_Daiki',
+    author_email='8hachibee125@gmail.com',
+    url='https://github.com/hachibeeDI/Sphinx-hachibee-theme',
+    description='a simple sphinx theme',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    license="MIT",
+    include_package_data=True,
+    entry_points='',
+)
