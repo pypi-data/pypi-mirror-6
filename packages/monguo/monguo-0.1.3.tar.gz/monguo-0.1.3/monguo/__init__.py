6816538982707a474af1620056621c0c5af78cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package monguo/0.1.3/monguo-0.1.3.tar.gz

---
+++ packages/monguo/monguo-0.1.3.tar.gz/monguo-0.1.3/monguo/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: lime
+# @Date:   2013-10-25 19:45:09
+# @Last Modified by:   lime
+# @Last Modified time: 2013-11-25 11:08:35
+
+'''Monguo, an asynchronous MongoDB ORM for Tornado'''
+
+VERSION = (0, 1, 3)
+
+def get_version():
+    if isinstance(VERSION[-1], basestring):
+        return '.'.join(map(str, VERSION[:-1])) + VERSION[-1]
+    
+    return '.'.join(map(str, VERSION))
+
+__version__ = get_version()
