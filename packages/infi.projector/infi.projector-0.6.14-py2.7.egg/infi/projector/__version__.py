b92d91369ca3b7942f8e340051c750e5e2dba691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package infi.projector/0.6.14/infi.projector-0.6.14-py2.7.egg

---
+++ packages/infi.projector/infi.projector-0.6.14-py2.7.egg/infi/projector/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.14"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.6.14)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'faccabcc0ddb41a77eb6daa40040397d9cc5de39'
+__git_head_subject__ = 'Finished release v0.6.14.'
+__git_head_message__ = '* release/v0.6.14:\n  HOSTDEV-1276 add newlines to install_requires in buildout.cfg HOSTDEV-1277 sort the install_requires list\n  HOSTDEV-1275 have the verions section in buildout.cfg sorted'
+__git_dirty_diff__ = ''
