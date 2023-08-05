19a9304d70ad0419d2c831eb12423481375806e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:03 +0000

    Add package python-500px/0.2.0/python-500px-0.2.0.tar.gz

---
+++ packages/python-500px/python-500px-0.2.0.tar.gz/python-500px-0.2.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name="python-500px",
+      version="0.2.0",
+      description="500PX API client",
+      license="MIT",
+      install_requires=["simplejson"],
+      author="Akira Hirakawa",
+      author_email="akirahrkw@gmail.com",
+      url="https://github.com/akirahrkw/python-500px",
+      packages = find_packages(),
+      keywords= "500px",
+      zip_safe = True)
