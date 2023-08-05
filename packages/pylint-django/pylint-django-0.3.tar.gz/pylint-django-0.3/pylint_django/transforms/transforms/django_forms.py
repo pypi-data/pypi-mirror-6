3205c4e8f30b45cda127ad1604cb085b605f1e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:34 +0000

    Add package pylint-django/0.3/pylint-django-0.3.tar.gz

---
+++ packages/pylint-django/pylint-django-0.3.tar.gz/pylint-django-0.3/pylint_django/transforms/transforms/django_forms.py
@@ -0,0 +1,5 @@
+from django.forms import BaseForm
+
+
+class Form(BaseForm):
+    pass
---
+++ packages/pylint-django/pylint-django-0.3.tar.gz/pylint-django-0.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+
+_version = '0.3'
+_packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+
+_short_description = "pylint-django is a Pylint plugin to aid Pylint in recognising and understanding" \
+                     "errors caused when using the Django framework"
+
+
+_transform_dir = 'pylint_django/transforms/transforms'
+_package_data = {
+    'pylint_django': [
+        os.path.join('transforms/transforms', name) for name in os.listdir(_transform_dir)
+    ]
+}
+
+
+setup(
+    name='pylint-django',
+    url='https://github.com/landscapeio/pylint-django',
+    author='landscape.io',
+    author_email='code@landscape.io',
+    description=_short_description,
+    version=_version,
+    packages=_packages,
+    package_data=_package_data,
+    install_requires=['pylint>=1.0', 'astroid>=1.0', 'logilab-common>=0.60.0', 'pylint-plugin-utils>=0.1.1'],
+    license='GPLv2',
+    keywords='pylint django plugin'
+)
