093f7408c235f5d42dcc43698773ae5a2958d47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:08 +0000

    Add package sphinx_bernard_theme/0.1.9/sphinx_bernard_theme-0.1.9.tar.gz

---
+++ packages/sphinx_bernard_theme/sphinx_bernard_theme-0.1.9.tar.gz/sphinx_bernard_theme-0.1.9/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+    name='sphinx_bernard_theme',
+    version='0.1.9',
+    url='https://github.com/bernardphp/bernardphp-com/',
+    license='MIT',
+    author='Henrk Bjornskov',
+    author_email='henrik@bjrnskov.dk',
+    packages=['sphinx_bernard_theme'],
+    include_package_data=True,
+    install_requires=[
+        'setuptools'
+    ],
+)
