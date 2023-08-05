d56d868454c4067bb792ea7b3ab8d9e4e36210e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:49 +0000

    Add package django-pages/1.2.1/django-pages-1.2.1.tar.gz

---
+++ packages/django-pages/django-pages-1.2.1.tar.gz/django-pages-1.2.1/django_pages/site/__init__.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+from ..common.errors import ConfigurationError
+from .models import Site
+
+
+def get_site():
+    '''
+    checks for site with pk=1
+
+    @return Site object
+    @raises ConfigurationError
+    '''
+
+    try:
+
+        site = Site.objects.get(pk=1)
+
+        return site
+
+    except Site.DoesNotExist:
+
+        raise ConfigurationError('There are no Site\'s, please create one in admin')
+
+    except Site.MultipleObjectsReturned:
+
+        raise ConfigurationError('There is more than one site, please make sure there is exactly one, this feature may be changed in future')
