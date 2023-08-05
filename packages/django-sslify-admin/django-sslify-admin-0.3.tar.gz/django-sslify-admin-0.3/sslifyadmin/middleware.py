b9baa32ce2274d356143068540a99447b531876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:51 +0000

    Add package django-sslify-admin/0.3/django-sslify-admin-0.3.tar.gz

---
+++ packages/django-sslify-admin/django-sslify-admin-0.3.tar.gz/django-sslify-admin-0.3/sslifyadmin/middleware.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.http import HttpResponsePermanentRedirect
+from django.core.urlresolvers import reverse
+
+
+class SSLifyAdminMiddleware(object):
+    """Force all admin requests to use HTTPs. If we get an HTTP request, we'll just
+    force a redirect to HTTPs.
+
+    .. note::
+        This will only take effect if ``settings.DEBUG`` is False.
+    """
+
+    def process_request(self, request):
+        sslify_admin_namespace = getattr(settings, 'SSLIFY_ADMIN_NAMESPACE', 'admin')
+        if request.path.startswith(reverse('%s:index' % sslify_admin_namespace)) and \
+                not (settings.DEBUG or request.is_secure()):
+            url = request.build_absolute_uri(request.get_full_path())
+            secure_url = url.replace('http://', 'https://')
+            return HttpResponsePermanentRedirect(secure_url)
