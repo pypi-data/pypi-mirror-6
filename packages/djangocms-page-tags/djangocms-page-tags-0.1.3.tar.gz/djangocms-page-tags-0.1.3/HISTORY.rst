69cc084e31ed7fee2ee13deb5d71b1ca7d7d56cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:57 +0000

    Add package djangocms-page-tags/0.1.3/djangocms-page-tags-0.1.3.tar.gz

---
+++ packages/djangocms-page-tags/djangocms-page-tags-0.1.3.tar.gz/djangocms-page-tags-0.1.3/HISTORY.rst
@@ -0,0 +1,24 @@
+.. :changelog:
+
+History
+-------
+
+0.1.3 (2014-01-22)
+++++++++++++++++++
+
+* Put toolbar menu items in a submenu
+
+0.1.2 (2014-01-16)
+++++++++++++++++++
+
+* Documentation update
+
+0.1.1 (2014-01-16)
+++++++++++++++++++
+
+* Add transifex support
+
+0.1.0 (2014-01-15)
+++++++++++++++++++
+
+* Experimental release.
---
+++ packages/djangocms-page-tags/djangocms-page-tags-0.1.3.tar.gz/djangocms-page-tags-0.1.3/djangocms_page_tags/admin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from cms.extensions import PageExtensionAdmin, TitleExtensionAdmin
+from django.conf import settings
+from django.contrib import admin
+
+from .models import PageTags, TitleTags
+
+
+
+class PageTagsAdmin(PageExtensionAdmin):
+
+    class Media:
+        css = {
+            'all': ('%sdjangocms_page_tags/css/%s' % (
+                settings.STATIC_URL, "djangocms_page_tags.css"),)
+        }
+admin.site.register(PageTags, PageTagsAdmin)
+
+
+class TitleTagsAdmin(TitleExtensionAdmin):
+
+    class Media:
+        css = {
+            'all': ('%sdjangocms_page_tags/css/%s' % (
+                settings.STATIC_URL, "djangocms_page_tags.css"),)
+        }
+admin.site.register(TitleTags, TitleTagsAdmin)
