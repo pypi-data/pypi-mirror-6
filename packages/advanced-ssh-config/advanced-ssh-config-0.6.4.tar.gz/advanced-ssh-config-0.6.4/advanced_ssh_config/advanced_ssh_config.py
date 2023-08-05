ac6ce4264eea546cafb392715e0337e8602adf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:25 +0000

    Add package advanced-ssh-config/0.6.4/advanced-ssh-config-0.6.4.tar.gz

---
+++ packages/advanced-ssh-config/advanced-ssh-config-0.6.4.tar.gz/advanced-ssh-config-0.6.4/advanced_ssh_config/advanced_ssh_config.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+import os
+import re
+import logging
+from collections import OrderedDict
+
+from .config import Config
+from .utils import (safe_makedirs, value_interpolate, construct_proxy_commands,
+                    shellquotemultiple)
+
+
+class AdvancedSshConfig(object):
+
+    def __init__(self, hostname=None, port=None, configfiles=None,
+                 verbose=False, dry_run=False, proxy_type='nc',
+                 timeout=180):
+
+        self.verbose, self.dry_run = verbose, dry_run
+        self.hostname, self.port = hostname, port
+        self.proxy_type, self.timeout = proxy_type, timeout
+
+        self.log = logging.getLogger('')
+
+        # Initializes the Config object
+        if not configfiles:
+            configfiles = [
+                '/etc/ssh/config.advanced',
+                '~/.ssh/config.advanced',
+                ]
+        self.config = Config(configfiles=configfiles)
+
+    def debug(self, string=None):
+        self.log.debug(string and string or '')
+
+    @property
+    def controlpath_dir(self):
+        controlpath = self.config.get('controlpath',
+                                      'default',
+                                      '/tmp/advssh_cm/')
+        directory = os.path.dirname(os.path.expanduser(controlpath))
+        directory = os.path.join(directory, self.hostname)
+        directory = os.path.dirname(directory)
+        return directory
+
+    def get_routing(self):
+        routing = {}
+        safe_makedirs(self.controlpath_dir)
+
+        section = None
+        for sect in self.config.parser.sections():
+            if re.match(sect, self.hostname):
+                section = sect
+
+        self.debug('section \'{}\' '.format(section))
+
+        # Parse special routing
+        path = self.hostname.split('/')
+
+        args = {}
+        options = {
+            'p': 'Port',
+            'l': 'User',
+            'h': 'Hostname',
+            'i': 'IdentityFile'
+            }
+        default_options = {
+            'p': str(self.port),
+            'h': path[0]
+            }
+        updated = False
+        for key in options:
+            cfval = self.config.get(options[key],
+                                    path[0],
+                                    default_options.get(key))
+            value = value_interpolate(cfval)
+            if cfval != value:
+                updated = True
+                self.config.parser.set(section, options[key], value)
+                args[key] = value
+
+            self.debug('get (-%-1s) %-12s : %s' % (key, options[key], value))
+            if value:
+                args[key] = value
+
+        # If we interpolated any keys
+        if updated:
+            self.write_sshconfig()
+            self.log.debug('Config updated. Need to restart SSH!?')
+
+        self.debug('args: {}'.format(args))
+        self.debug()
+
+        self.debug('hostname    : {}'.format(self.hostname))
+        self.debug('port        : {}'.format(self.port))
+        self.debug('path        : {}'.format(path))
+        self.debug('path[0]     : {}'.format(path[0]))
+        self.debug('path[1:]    : {}'.format(path[1:]))
+        self.debug('args        : {}'.format(args))
+
+        self.debug()
+        routing['verbose'] = self.verbose
+        routing['proxy_type'] = self.proxy_type
+        routing['gateways'] = self.config.get('Gateways', path[-1], 'direct')
+        routing['reallocalcommand'] = self.config.get('RealLocalCommand',
+                                                      path[-1], '')
+        for key in ('gateways', 'reallocalcommand'):
+            routing[key] = routing[key].strip().split(' ')
+        self.debug('reallocalcommand : {}'.format(routing['reallocalcommand']))
+        self.debug('gateways         : {}'.format(', '.join(['gateways'])))
+        routing['gateway_route'] = path[1:]
+        routing['hostname'] = args['h']
+        #routing['args'] = args
+        routing['port'] = self.port or int(args['p']) or 22
+        routing['proxy_commands'] = construct_proxy_commands(routing)
+
+        self.debug()
+        self.debug('Routing:')
+        for key, value in routing.iteritems():
+            self.debug('  {0}: {1}'.format(key, value))
+        self.debug()
+
+        return routing
+
+    def connect(self, routing):
+        for gateway in routing['gateways']:
+            if gateway != 'direct':
+                routing['gateway_route'] += [gateway]
+            cmd = []
+            if len(routing['gateway_route']):
+                cmd += ['ssh', '/'.join(routing['gateway_route'])]
+                cmd.append(shellquotemultiple(routing['proxy_commands']))
+            else:
+                cmd += routing['proxy_commands'][0]
+
+            self.debug('cmd         : {}'.format(cmd))
+            self.debug('================')
+            self.debug()
+
+            if not self.dry_run:
+                ssh_process = subprocess.Popen(map(str, cmd))
+                rlc_process = None
+                if len(routing['reallocalcommand'][0]):
+                    rlc_process = subprocess.Popen(routing['reallocalcommand'])
+                if ssh_process.wait() != 0:
+                    self.log.critical('There were some errors')
+                if rlc_process is not None:
+                    rlc_process.kill()
+
+    def write_sshconfig(self, filename='~/.ssh/config'):
+        config = self.build_sshconfig()
+        fhandle = open(os.path.expanduser(filename), 'w+')
+        fhandle.write('\n'.join(config))
+        fhandle.close()
+
+    def build_sshconfig(self):
+        config = []
+
+        hosts = self.prepare_sshconfig()
+        od = OrderedDict(sorted(hosts.items()))
+        for entry in od.values():
+            if entry.host == '*':
+                continue
+            else:
+                config += entry.build_sshconfig()
+
+        if '*' in hosts:
+            config += build_entry(hosts['*'])
+
+        return config
+
+    def prepare_sshconfig(self):
+        hosts = {}
+        for host in self.config.full.values():
+            host.resolve()
+            hosts[host.host] = host
+        return hosts
---
+++ packages/advanced-ssh-config/advanced-ssh-config-0.6.4.tar.gz/advanced-ssh-config-0.6.4/advanced_ssh_config/tests/test_advanced_ssh_config.py
@@ -0,0 +1,329 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from advanced_ssh_config.advanced_ssh_config import AdvancedSshConfig
+from advanced_ssh_config.exceptions import ConfigError
+from . import set_config, prepare_config, DEFAULT_CONFIG
+
+
+class TestAdvancedSshConfig(unittest.TestCase):
+
+    def setUp(self):
+        prepare_config()
+
+    def test_load_advanced_ssh_config(self):
+        advssh = AdvancedSshConfig()
+        self.assertIsInstance(advssh, AdvancedSshConfig)
+
+    def test_routing_simple(self):
+        advssh = AdvancedSshConfig(hostname='test',
+                                   port=23,
+                                   verbose=True,
+                                   dry_run=True)
+        routing = advssh.get_routing()
+        self.assertEqual(routing['port'], 23)
+        self.assertEqual(routing['hostname'], 'test')
+        self.assertEqual(routing['reallocalcommand'], [''])
+        self.assertEqual(routing['gateways'], ['direct'])
+        self.assertEqual(routing['verbose'], True)
+        self.assertEqual(routing['proxy_type'], 'nc')
+        self.assertEqual(routing['proxy_commands'][0], ['nc', '-v', '-w', 180, '-G', 5, 'test', 23])
+
+    def test_routing_hostname_in_config(self):
+        contents = """
+[test.com]
+hostname = 1.2.3.4
+port = 25
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test.com',
+                                   port=25,
+                                   verbose=True,
+                                   dry_run=True,
+                                   configfiles=[DEFAULT_CONFIG])
+        routing = advssh.get_routing()
+        self.assertEqual(routing['port'], 25)
+        self.assertEqual(routing['hostname'], '1.2.3.4')
+        self.assertEqual(routing['proxy_type'], 'nc')
+        self.assertEqual(routing['proxy_commands'][0], ['nc', '-v', '-w', 180, '-G', 5, '1.2.3.4', 25])
+
+    def test_routing_config_override(self):
+        contents = """
+[test.com]
+port = 25
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test.com',
+                                   port=23,
+                                   verbose=True,
+                                   dry_run=True,
+                                   configfiles=[DEFAULT_CONFIG])
+        routing = advssh.get_routing()
+        self.assertEqual(routing['port'], 23)
+        self.assertEqual(routing['hostname'], 'test.com')
+        self.assertEqual(routing['proxy_type'], 'nc')
+        self.assertEqual(routing['proxy_commands'][0], ['nc', '-v', '-w', 180, '-G', 5, 'test.com', 23])
+
+    def test_routing_via_two_other_hosts(self):
+        advssh = AdvancedSshConfig(hostname='aaa.com/bbb.com/ccc.com')
+        routing = advssh.get_routing()
+        self.assertEqual(routing['hostname'], 'aaa.com')
+        self.assertEqual(routing['proxy_type'], 'nc')
+        self.assertEqual(routing['gateways'], ['direct'])
+        self.assertEqual(routing['proxy_commands'][0], ['nc', '-w', 180, '-G', 5, 'aaa.com', 22])
+        self.assertEqual(routing['gateway_route'], ['bbb.com', 'ccc.com'])
+
+    def test_routing_via_two_other_hosts_with_config_one(self):
+        contents = """
+[ddd.com]
+hostname = 1.2.3.4
+port = 25
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='ddd.com/eee.com', configfiles=[DEFAULT_CONFIG])
+        routing = advssh.get_routing()
+        self.assertEqual(routing['hostname'], '1.2.3.4')
+        self.assertEqual(routing['proxy_type'], 'nc')
+        self.assertEqual(routing['gateways'], ['direct'])
+        self.assertEqual(routing['proxy_commands'][0], ['nc', '-w', 180, '-G', 5, '1.2.3.4', 25])
+        self.assertEqual(routing['gateway_route'], ['eee.com'])
+
+
+    def test_prepare_sshconfig_simple(self):
+        contents = """
+[test]
+port = 25
+
+[default]
+port = 24
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.prepare_sshconfig()
+        self.assertEqual(len(config.keys()), 2)
+        self.assertEqual(config['test'].host, 'test')
+        self.assertEqual(config['test'].config, [('port', '25')])
+        self.assertEqual(config['default'].host, 'default')
+        self.assertEqual(config['default'].config, [('port', '24')])
+
+    def test_prepare_sshconfig_multiline(self):
+        contents = """
+[test]
+localforward = 1 2.3.4.5 6 \n 7 8.9.10.11 12
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.prepare_sshconfig()
+        self.assertEqual(config['test'].host, 'test')
+        self.assertEqual(config['test'].config, [('localforward', '1 2.3.4.5 6'), ('localforward', '7 8.9.10.11 12')])
+
+    def test_inherits(self):
+        contents = """
+[aaa]
+hostname = 1.2.3.4
+user = toto
+
+[bbb]
+inherits = aaa
+port = 23
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.config.full
+        self.assertEqual(config['aaa'].clean_config['user'], 'toto')
+        self.assertEqual('port' in config['aaa'].clean_config, False)
+        self.assertEqual(config['bbb'].clean_config['user'], 'toto')
+        self.assertEqual(config['bbb'].clean_config['port'], '23')
+
+    def test_build_ssh_config(self):
+        contents = """
+[aaa]
+hostname = 1.2.3.4
+user = toto
+
+[bbb]
+inherits = aaa
+port = 23
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.prepare_sshconfig()
+        arr = advssh.build_sshconfig()
+        string = '\n'.join(arr)
+        self.assertEquals(len(arr), 10)
+        dest = """
+Host aaa
+  user toto
+  # hostname aaa
+
+Host bbb
+  port 23
+  user toto
+  # hostname bbb
+  # inherits aaa
+"""
+        self.assertEquals(string.strip(), dest.strip())
+
+    def test_build_ssh_config_sorted(self):
+        contents = """
+[ddd]
+inherits = aaa
+port = 23
+user = titi
+
+[bbb]
+user = titi
+inherits = aaa
+port = 23
+hostname = 1.1.1.1
+
+[ccc]
+hostname = 5.4.3.2
+inherits = aaa
+port = 23
+
+[aaa]
+hostname = 1.2.3.4
+user = toto
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.prepare_sshconfig()
+        arr = advssh.build_sshconfig()
+        string = '\n'.join(arr)
+        dest = """
+Host aaa
+  user toto
+  # hostname aaa
+
+Host bbb
+  port 23
+  user titi
+  # hostname bbb
+  # inherits aaa
+
+Host ccc
+  port 23
+  user toto
+  # hostname ccc
+  # inherits aaa
+
+Host ddd
+  port 23
+  user titi
+  # hostname ddd
+  # inherits aaa
+"""
+        self.assertEquals(string.strip(), dest.strip())
+
+    def test_inherits_noexists(self):
+        contents = """
+[aaa]
+hostname = 1.2.3.4
+user = toto
+
+[bbb]
+inherits = ccc
+port = 23
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.config.full
+        def call():
+            return config['bbb'].clean_config
+        self.assertRaises(ConfigError, call)
+
+    def test_inherits_deep(self):
+        contents = """
+[aaa]
+hostname = 1.2.3.4
+user = toto
+
+[bbb]
+inherits = aaa
+tcpkeepalive = 42
+
+[ccc]
+inherits = bbb
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.config.full
+        self.assertEqual(config['ccc'].clean_config['user'], 'toto')
+        self.assertEqual(config['ccc'].clean_config['tcpkeepalive'], '42')
+
+    def test_inherits_override(self):
+        contents = """
+[aaa]
+user = toto
+
+[bbb]
+inherits = aaa
+user = titi
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.config.full
+        self.assertEqual(config['aaa'].clean_config['user'], 'toto')
+        self.assertEqual(config['bbb'].clean_config['user'], 'titi')
+
+    def test_inherits_loop(self):
+        contents = """
+[aaa]
+inherits = ccc
+
+[bbb]
+inherits = aaa
+
+[ccc]
+inherits = bbb
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.config.full
+        def call(key):
+            return config[key].clean_config
+        self.assertRaises(ConfigError, call, 'aaa')
+        self.assertRaises(ConfigError, call, 'bbb')
+        self.assertRaises(ConfigError, call, 'ccc')
+
+    def test_inherits_loop_self(self):
+        contents = """
+[aaa]
+inherits = aaa
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.config.full
+        def call(key):
+            return config[key].clean_config
+        self.assertRaises(ConfigError, call, 'aaa')
+
+    def test_reserved_key(self):
+        contents = """
+[aaa]
+user = toto
+proxycommand = nc
+hostname = titi
+alias = tutu
+gateways = toutou
+reallocalcommand = tonton
+remotecommand = tantan
+includes = tuotuo
+inherits = bbb
+[bbb]
+"""
+        set_config(contents)
+        advssh = AdvancedSshConfig(hostname='test', configfiles=[DEFAULT_CONFIG])
+        config = advssh.config.full
+        self.assertEquals(config['aaa'].clean_config, {'user': 'toto', 'proxycommand': 'nc'})
+
+
+    # FIXME: test_handle_custom_proxycommand
+    # FIXME: test_prepare_sshconfig_with_hostname
+    # FIXME: test_routing_override_config
+    # FIXME: test_connect
+    # FIXME: test_dryrun
+    # FIXME: test_verbose
+    # FIXME: test_alias
