3965d4d6f85a98a1e0efd0d4fce05566dfebd2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package sqlalchemy-datatables/0.1.6/sqlalchemy-datatables-0.1.6.tar.gz

---
+++ packages/sqlalchemy-datatables/sqlalchemy-datatables-0.1.6.tar.gz/sqlalchemy-datatables-0.1.6/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import sys
+
+if sys.version_info >= (3,0):
+    def my_open(path, mode):
+        return open(path, mode, newline='')
+else:
+    def my_open(path, mode):
+        return open(path, mode+'b')
+
+
+__VERSION__ = [line for line in my_open('datatables/__init__.py', 'r') \
+    if line.startswith('__VERSION__')][0].split(\
+    '=')[1].strip().lstrip('\'').rstrip('\'')
+
+
+setup(
+    name='sqlalchemy-datatables',
+    version=__VERSION__,
+    description='SQLAlchemy integration of jQuery DataTables',
+    long_description=open('README.rst').read(),
+    license='MIT',
+    keywords='sqlalchemy datatables jquery pyramid',
+    author='Michel Nemnom',
+    author_email='michel.nemnom+pypi@gmail.com',
+    url='https://github.com/pegase745/sqlalchemy-datatables',
+    packages=find_packages(exclude=['test_app']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['sqlalchemy'],
+    py_modules=['datatables'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
