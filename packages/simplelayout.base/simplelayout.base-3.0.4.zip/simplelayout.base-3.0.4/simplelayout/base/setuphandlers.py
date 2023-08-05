1c674d6e443f1e05aad32e7fca604d5d96462176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:58 +0000

    Add package simplelayout.base/3.0.4/simplelayout.base-3.0.4.zip

---
+++ packages/simplelayout.base/simplelayout.base-3.0.4.zip/simplelayout.base-3.0.4/simplelayout/base/setuphandlers.py
@@ -0,0 +1,12 @@
+def registerLocalUtility(context):
+    # XXX remove this import step with an upgrade step, since
+    # the import step registration is persistent (import_steps.xml).
+    # The registration is done in componentregistry.xml
+    return
+
+
+def reorderJS(context):
+    # XXX remove this import step with an upgrade step, since
+    # the import step registration is persistent (import_steps.xml).
+    # The reordering is done with generic setup.
+    return
