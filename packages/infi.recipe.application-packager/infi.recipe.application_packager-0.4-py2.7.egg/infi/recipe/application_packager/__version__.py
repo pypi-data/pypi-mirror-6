415c6d44a834f92a65b498d27c1f139c18d42156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package infi.recipe.application-packager/0.4/infi.recipe.application_packager-0.4-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.4-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b4304e5761dbe56f4c9acafc47cfb74f3d7bc3d6'
+__git_head_subject__ = 'Finished release v0.4.'
+__git_head_message__ = '* release/v0.4:\n  TRIVIAL fixing CONDITION_DURING_UPGRADE_AND_UNINSTALL\n  TRIVIAL fixing tests\n  HOSTDEV-1290 removing test_upgrade with_custom_actions=False\n  TRIVIAL logging the product.wxs\n  TRIVIAL refactoring conditions\n  TRIVIAL adding uninstall debug\n  cleaning up\n  TRIVIAL fixing print\n  TRIVIAL more debugging\n  TRIVIAL printing the logfile for debugging\n  TRIVIAL adding debug information to tests\n  HIP-848 fixing prerm\n  HIP-848 fixing tests\n  HIP-848 calling close-applcation recipe in rpm/deb scripts\n  HIP-848 custom action for killing open applications during uninstall/upgrade\n  HOSTDEV-1270 updated readme\n  fix tests\n  buildout.cfg: adding infi.recipe.buildout_logging to requirements\n  HOSTDEV-1270 using infi.recipe.buildout_logging'
+__git_dirty_diff__ = ''
