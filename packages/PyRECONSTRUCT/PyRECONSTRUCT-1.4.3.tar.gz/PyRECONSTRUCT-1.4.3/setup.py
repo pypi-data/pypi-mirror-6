2dc41ea75b2b57f77c5e88acc036a7c6c7c6c312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:57 +0000

    Add package PyRECONSTRUCT/1.4.3/PyRECONSTRUCT-1.4.3.tar.gz

---
+++ packages/PyRECONSTRUCT/PyRECONSTRUCT-1.4.3.tar.gz/PyRECONSTRUCT-1.4.3/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+      name='PyRECONSTRUCT',
+      version='1.4.3',
+      author='Michael Musslewhite',
+      author_email='mdmusslewhite@gmail.com',
+      url='https://github.com/wtrdrnkr/pyrecon',
+      packages=[
+                'pyrecon',
+                'pyrecon.tools',
+                'pyrecon.toolsgui',
+                'pyrecon.dev'
+                ],
+      license='LICENSE.txt',
+      description='Python tools for interacting with XML files associated with RECONSTRUCT',
+      long_description=open('README.txt').read(),
+      install_requires=[
+                        "cython >= 0.19.1",
+                        "numpy >= 1.7.1",
+                        "scipy >= 0.12.0",
+                        "matplotlib >= 1.2.1",
+                        "ipython >= 0.13.2",
+                        "pandas >= 0.12.0",
+                        "sympy >= 0.7.3", 
+                        "nose >= 1.3.0",
+                        "shapely >= 1.2.18",
+                        "scikit-image >= 0.8.2",
+                        "lxml >= 3.2.3",
+                        "openpyxl >= 1.6.2",
+                        "PySide >= 1.2.1",
+                        ],
+      )
