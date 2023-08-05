bf9ac4e6ddc8a6907babda6ecf1f1fafae0380a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:46 +0000

    Add package django-cas-cache/0.1.4/django-cas-cache-0.1.4.tar.gz

---
+++ packages/django-cas-cache/django-cas-cache-0.1.4.tar.gz/django-cas-cache-0.1.4/cascache/backends/memcached.py
@@ -0,0 +1,42 @@
+from django.conf import settings
+from django.core.cache.backends.memcached import MemcachedCache, PyLibMCCache
+
+MAX_RETRIES = settings.get('CAS_CACHE_MAX_RETRIES', 10)
+
+
+class NoneValueError(Exception):
+    pass
+
+
+class CASMixin(object):
+    def cas(self, key, update_func, timeout=0, version=None):
+        key = self.make_key(key, version=version)
+        i = 0
+        while i < MAX_RETRIES:
+            current_val = self._cache.gets(key)
+            assert current_val is not None
+            new_val = update_func(current_val)
+            if new_val is None:
+                raise NoneValueError('Your update_func must not return None')
+            result = self._cache.cas(key, new_val,
+                                     self._get_memcache_timeout(timeout))
+            if result:
+                break
+            i += 1
+        else:
+            return 0
+        return result
+
+
+class MemcachedCASCache(CASMixin, MemcachedCache):
+    def __init__(self, server, params):
+        super(MemcachedCASCache, self).__init__(server, params)
+        self._cache.cache_cas = True
+
+    def close(self, **kwargs):
+        self._cache.reset_cas()
+        super(MemcachedCASCache, self).close(**kwargs)
+
+
+class PyLibMCCASCache(CASMixin, PyLibMCCache):
+    pass
---
+++ packages/django-cas-cache/django-cas-cache-0.1.4.tar.gz/django-cas-cache-0.1.4/pypi.rst
@@ -0,0 +1,7 @@
+================
+django-cas-cache
+================
+
+Django cache backend (currently memcache-only) supporting CAS (compare-and-set) operations
+
+See GitHub for usage and docs.
