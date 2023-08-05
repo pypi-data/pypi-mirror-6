0c977401e1867d2b8039f3f9cd34dd5a3efbda1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:03 +0000

    Add package tsuru-circus/0.6.3/tsuru-circus-0.6.3.tar.gz

---
+++ packages/tsuru-circus/tsuru-circus-0.6.3.tar.gz/tsuru-circus-0.6.3/tsuru/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2013 tsuru-circus authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+__version__ = "0.6.3"
