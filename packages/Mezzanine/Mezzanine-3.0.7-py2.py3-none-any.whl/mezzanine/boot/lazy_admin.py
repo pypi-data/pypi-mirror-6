347c32d29f4716e6df89b1b22e103165c62a3613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package Mezzanine/3.0.7/Mezzanine-3.0.7-py2.py3-none-any.whl

---
+++ packages/Mezzanine/Mezzanine-3.0.7-py2.py3-none-any.whl/mezzanine/boot/lazy_admin.py
@@ -0,0 +1,43 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import patterns, include
+from django.contrib.admin.sites import AdminSite
+
+
+class LazyAdminSite(AdminSite):
+    """
+    Defers calls to register/unregister until autodiscover is called
+    to avoid load issues with injectable model fields defined by
+    ``settings.EXTRA_MODEL_FIELDS``.
+    """
+
+    def __init__(self, *args, **kwargs):
+        self._deferred = []
+        super(LazyAdminSite, self).__init__(*args, **kwargs)
+
+    def register(self, *args, **kwargs):
+        for name, deferred_args, deferred_kwargs in self._deferred:
+            if name == "unregister" and deferred_args[0] == args[0]:
+                self._deferred.append(("register", args, kwargs))
+                break
+        else:
+            super(LazyAdminSite, self).register(*args, **kwargs)
+
+    def unregister(self, *args, **kwargs):
+        self._deferred.append(("unregister", args, kwargs))
+
+    def lazy_registration(self):
+        for name, deferred_args, deferred_kwargs in self._deferred:
+            getattr(AdminSite, name)(self, *deferred_args, **deferred_kwargs)
+
+    @property
+    def urls(self):
+        from django.conf import settings
+        urls = patterns("", ("", super(LazyAdminSite, self).urls),)
+        # Filebrowser admin media library.
+        fb_name = getattr(settings, "PACKAGE_NAME_FILEBROWSER", "")
+        if fb_name in settings.INSTALLED_APPS:
+            urls += patterns("",
+                ("^media-library/", include("%s.urls" % fb_name)),
+            )
+        return urls
