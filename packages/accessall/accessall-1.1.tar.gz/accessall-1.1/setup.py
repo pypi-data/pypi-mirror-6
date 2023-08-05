f0ba3206d947465ea4b113b84b7eb982ff950a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:30 +0000

    Add package accessall/1.1/accessall-1.1.tar.gz

---
+++ packages/accessall/accessall-1.1.tar.gz/accessall-1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+def deps():
+    with open('requirements.txt') as f:
+        return f.readlines()
+
+setup(
+    name='accessall',
+    version='1.1',
+    long_description=__doc__,
+    packages=['accessall'],
+    include_package_data=True,
+    install_requires=deps(),
+    author='Anthony Grimes',
+    description='Export library info from google music.',
+    license='MIT',
+    url='https://github.com/Raynes/accessall',
+    entry_points={
+        'console_scripts': [
+            'accessall = accessall.__main__:main'
+            ],
+        }
+)
