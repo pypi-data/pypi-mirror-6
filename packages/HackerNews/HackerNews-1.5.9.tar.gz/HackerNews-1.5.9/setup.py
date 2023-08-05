b36df2d8db4ef45370aa1e400ebaa1c9ab208ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:02 +0000

    Add package HackerNews/1.5.9/HackerNews-1.5.9.tar.gz

---
+++ packages/HackerNews/HackerNews-1.5.9.tar.gz/HackerNews-1.5.9/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='HackerNews',
+    version='1.5.9',
+    install_requires=['BeautifulSoup4>=4.3.1', 'requests'],
+    author='Karan Goel',
+    author_email='karan@goel.im',
+    packages=['hn',],
+    url='https://github.com/karan/HackerNewsAPI/',
+    license='MIT License',
+    description='Python API for Hacker News.',
+    long_description='Unofficial Python API for Hacker News. Usage: https://github.com/karan/HackerNewsAPI.',
+)
