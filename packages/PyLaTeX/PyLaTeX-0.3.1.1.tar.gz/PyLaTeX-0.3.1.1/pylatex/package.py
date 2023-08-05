eb01e8c3a7b1ad603bfe18be555cca7205661ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:01 +0000

    Add package PyLaTeX/0.3.1.1/PyLaTeX-0.3.1.1.tar.gz

---
+++ packages/PyLaTeX/PyLaTeX-0.3.1.1.tar.gz/PyLaTeX-0.3.1.1/pylatex/package.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    pylatex.package
+    ~~~~~~~
+
+    This module implements the class that deals with packages.
+
+    :copyright: (c) 2014 by Jelte Fennema.
+    :license: MIT, see License for more details.
+"""
+
+from .base_classes import BaseLaTeXClass
+
+
+class Package:
+
+    """A class that represents a package."""
+
+    def __init__(self, name, option=None):
+        self.name = name
+        self.option = option
+
+    def dumps(self):
+        """Represents the package as a string in LaTeX syntax."""
+        if self.option is None:
+            option = ''
+        else:
+            option = '[' + self.option + ']'
+
+        return r'\usepackage' + option + '{' + self.name + '}\n'
