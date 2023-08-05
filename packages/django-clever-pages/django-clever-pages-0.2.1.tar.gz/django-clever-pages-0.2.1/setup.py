a73795ad9ac7bba0e920f26781a2afacff5d373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:02 +0000

    Add package django-clever-pages/0.2.1/django-clever-pages-0.2.1.tar.gz

---
+++ packages/django-clever-pages/django-clever-pages-0.2.1.tar.gz/django-clever-pages-0.2.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='django-clever-pages',
+    version='0.2.1',
+    description='App for create pages',
+    author='Pupkov Semen',
+    author_email='semen.pupvko@gmail.com',
+    url='https://github.com/artofhuman/django-clever-pages',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'django-mptt>=0.6.0',
+        'django-meta>=0.0.2',
+        'feincms==1.7.7'
+    ],
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
