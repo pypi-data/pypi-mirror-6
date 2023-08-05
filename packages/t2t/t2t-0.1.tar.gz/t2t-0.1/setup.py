7e7919a19c8d7a18aab901d7ecacdb9dab560567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package t2t/0.1/t2t-0.1.tar.gz

---
+++ packages/t2t/t2t-0.1.tar.gz/t2t-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='t2t',
+        version='0.1',
+        packages=['',],
+        url='https://github.com/ggarber/t2t',
+        license='',
+        install_requires=[],
+        long_description='Simple tool to convert the content of text files based on regular expressions',
+        scripts=['scripts/t2t']
+)
