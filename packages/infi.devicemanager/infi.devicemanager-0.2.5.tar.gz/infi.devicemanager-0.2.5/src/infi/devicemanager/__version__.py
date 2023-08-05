cbf10281ba8335f3bbf8d5582513b0b078a278b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package infi.devicemanager/0.2.5/infi.devicemanager-0.2.5.tar.gz

---
+++ packages/infi.devicemanager/infi.devicemanager-0.2.5.tar.gz/infi.devicemanager-0.2.5/src/infi/devicemanager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8847bceb8aec7284ab30f4d2a5fc159089608cc1'
+__git_head_subject__ = 'update buildout.cfg'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
