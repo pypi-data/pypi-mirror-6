911b110b7ffe7495c4db011d8261cf46e6d0d41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:26 +0000

    Add package threetaps/0.2.0/threetaps-0.2.0.tar.gz

---
+++ packages/threetaps/threetaps-0.2.0.tar.gz/threetaps-0.2.0/threetaps/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+3taps API client
+~~~~~~~~~~~~~~~~
+
+3taps is a library, written in Python, which makes interacting
+with the 3taps API simple.
+
+Basic usage:
+
+:copyright: (c) 2013 by Michael Kolodny.
+:license: MIT, see LICENSE for more details.
+
+"""
+
+__title__ = 'threetaps'
+__version__ = '0.2.0'
+__author__ = 'Michael Kolodny'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Michael Kolodny'
+
+from .client import Threetaps
