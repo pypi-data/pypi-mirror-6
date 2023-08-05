a467f98634c58260baa939e51e91df28f947c856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package Flask-uWSGI-WebSocket/0.0.1/Flask-uWSGI-WebSocket-0.0.1.tar.gz

---
+++ packages/Flask-uWSGI-WebSocket/Flask-uWSGI-WebSocket-0.0.1.tar.gz/Flask-uWSGI-WebSocket-0.0.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Flask-uWSGI-WebSocket
+-------------
+High-performance WebSockets for your Flask apps powered by `uWSGI <http://uwsgi-docs.readthedocs.org/en/latest/>`_.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-uWSGI-WebSocket',
+    version='0.0.1',
+    url='https://github.com/zeekay/flask-uwsgi-websocket',
+    license='See License',
+    author='Zach Kelling',
+    author_email='zk@monoid.io',
+    description='High-performance WebSockets for your Flask apps powered by uWSGI.',
+    long_description=__doc__,
+    py_modules=['flask_uwsgi_websocket'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'uwsgi',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
