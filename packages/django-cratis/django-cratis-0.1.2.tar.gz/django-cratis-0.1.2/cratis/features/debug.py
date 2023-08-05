9293c94a4823c6f24364b1df43aa11e1c745cdf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:37 +0000

    Add package django-cratis/0.1.2/django-cratis-0.1.2.tar.gz

---
+++ packages/django-cratis/django-cratis-0.1.2.tar.gz/django-cratis-0.1.2/cratis/features/debug.py
@@ -0,0 +1,10 @@
+from cratis.features import Feature
+
+
+class Debug(Feature):
+
+    def get_required_packages(self, cls):
+        return 'django-debug-toolbar',
+
+    def configure_settings(self, cls):
+        self.append_apps(cls, ['debug_toolbar'])
