8afbb8c01b91f5e5f380830bf0821295f96a550c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package django-ptree/0.1.64/django-ptree-0.1.64.zip

---
+++ packages/django-ptree/django-ptree-0.1.64.zip/django-ptree-0.1.64/ptree/admin.py
@@ -0,0 +1,28 @@
+__doc__ = """See https://docs.djangoproject.com/en/dev/ref/contrib/admin/"""
+
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+from data_exports.models import Export, Format
+from data_exports.admin import ExportAdmin
+import ptree.common
+from ptree.sequence_of_experiments.models import SequenceOfExperiments
+
+admin.site.register(SequenceOfExperiments, ptree.common.SequenceOfExperimentsAdmin)
+
+admin.site.unregister(Export)
+
+# users don't need to see this
+admin.site.unregister(Format)
+
+class PTreeExportAdmin(ExportAdmin):
+
+    # In Django 1.7, I can set list_display_links to None and then put 'name' first
+    list_display = ['get_export_link', 'name']
+    ordering = ['slug']
+
+
+admin.site.register(Export, PTreeExportAdmin)
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
