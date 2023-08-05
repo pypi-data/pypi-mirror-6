b0f45b29d1934e63368571408f8e8beee5c6e8b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:06 +0000

    Add package LangUtil/0.1.3/LangUtil-0.1.3.tar.gz

---
+++ packages/LangUtil/LangUtil-0.1.3.tar.gz/LangUtil-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='LangUtil',
+    version='0.1.3',
+    author='Andrew Udvare',
+    author_email='audvare@gmail.com',
+    packages=['langutil'],
+    url='http://pypi.python.org/pypi/LangUtil/',
+    license='LICENSE.txt',
+    description='Helpers for generating code.',
+    long_description=open('README.rst').read(),
+)
