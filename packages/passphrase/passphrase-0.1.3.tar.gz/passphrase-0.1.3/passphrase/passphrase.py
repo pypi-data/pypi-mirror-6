b733b0598e154109234c94d946305ec5e2af014d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package passphrase/0.1.3/passphrase-0.1.3.tar.gz

---
+++ packages/passphrase/passphrase-0.1.3.tar.gz/passphrase-0.1.3/passphrase/passphrase.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import argparse
+from glob import glob
+import random
+import time
+import os.path
+from contextlib import contextmanager
+
+@contextmanager
+def cd(path):
+  old_dir = os.getcwd()
+  os.chdir(path)
+  yield
+  os.chdir(old_dir)
+
+_dir = os.path.dirname(os.path.abspath(__file__))
+secure_random = random.SystemRandom(time.time)
+
+def available_languages():
+  with cd(_dir + "/dictionaries/"):
+    langs = list(n.replace(".txt", "") for n in glob("*.txt"))
+  return sorted(langs)
+
+def generate(lang, num):
+  try:
+    return [x.strip() for x in sorted(secure_random.sample(list(open('%s/dictionaries/%s.txt' % (_dir, lang))), num))]
+  except:
+    return ["Dictionary ", lang, " not found. Use the '-L' flag to list available dictionaries."]
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-l', '--language', default = "en", help = "Show results from which language")
+  parser.add_argument('-L', '--list', action = "store_true", help = "Show available languages")
+  parser.add_argument('-n', '--number', type = int, default = 12, help = "Number of results from which to choose")
+  args = parser.parse_args()
+  if args.list:
+    print " ".join(available_languages())
+  else:
+    print " ".join(generate(args.language, args.number))
+
+if __name__ == "__main__":
+  main()
---
+++ packages/passphrase/passphrase-0.1.3.tar.gz/passphrase-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import passphrase
+
+setup(
+    name='passphrase',
+    version='0.1.3',
+    author='Rob Martin @version2beta',
+    author_email='rob@version2beta.com',
+    packages=['passphrase'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/passphrase/',
+    license='LICENSE.txt',
+    description='List a series of words for selection of a secure and rememberable passphrase. Choose from many different languages.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    package_data={
+        '': ['*.dist'],
+        'passphrase': ['dictionaries/*.txt'],
+      },
+    entry_points={
+        'console_scripts': [
+          'passphrase = passphrase:main',
+        ],
+      },
+)
