65583e325d5e80539eb08b9f16ff1e2273d24000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:09 +0000

    Add package truncate/0.11/truncate-0.11.tar.gz

---
+++ packages/truncate/truncate-0.11.tar.gz/truncate-0.11/setup.py
@@ -0,0 +1,29 @@
+"""setup/install script for truncate"""
+
+
+from distutils.core import setup
+
+# from truncate import __version__, __author__, __license__
+
+
+setup(
+    name='truncate',
+    version='0.11',
+    py_modules=['truncate'],
+    author='Kelvin Wong',
+    author_email='www.kelvinwong.ca',
+    url='https://github.com/ansother/truncate',
+    description='Intelligently truncate a given string',
+    long_description='',
+    download_url='',
+    keywords='',
+    license='http://www.python.org/psf/license/',
+    install_requires=[''],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
