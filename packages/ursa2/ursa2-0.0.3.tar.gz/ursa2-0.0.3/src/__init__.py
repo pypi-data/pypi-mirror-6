dd012963bd5e5e7d71baddd822996c4fa232b362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:53 +0000

    Add package ursa2/0.0.3/ursa2-0.0.3.tar.gz

---
+++ packages/ursa2/ursa2-0.0.3.tar.gz/ursa2-0.0.3/src/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+'''
+ __init__.py
+
+ changelog
+ 2013-11-30[00:09:04]:created
+ 2013-12-18[10:19:03]:upgrade to 0.0.3
+
+ @info yinyong,osx-x64,UTF-8,192.168.1.101,py,/Users/yinyong/work/ursa2/src
+ @author yanni4night@gmail.com
+ @version 0.0.1
+ @since 0.0.1
+'''
+
+__version__='0.0.3'
