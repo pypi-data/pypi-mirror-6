3218e5904bda765f8831dc90743617995dc45b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:08 +0000

    Add package django-unfriendly/0.3/django-unfriendly-0.3.tar.gz

---
+++ packages/django-unfriendly/django-unfriendly-0.3.tar.gz/django-unfriendly-0.3/README.rst
@@ -0,0 +1,25 @@
+django-unfriendly
+=================
+
+The unfriendliest urls in town! ``django-unfriendly`` is a Django app that obfuscates urls and allows your application to natively execute the original view.
+
+.. image:: https://travis-ci.org/tomatohater/django-unfriendly.png?branch=master
+    :target: https://travis-ci.org/tomatohater/django-unfriendly
+
+.. image:: https://badge.fury.io/py/django-unfriendly.png
+    :target: http://badge.fury.io/py/django-unfriendly
+
+
+There is lots of talk about SEO friendly urls. The trend is towards more and more human readable information in your urls and Django makes it easy to create urls like::
+
+    http://yoursite.com/music/awesome/the-melvins/
+
+But sometimes urls can give too much away. This is where ``django-unfriendly`` comes in.
+
+``django-unfriendly`` provides a template filter that obfuscates urls in your templates, and a url handler/view that deobfuscates and executes the original view (e.g. no redirection).
+
+
+Read the docs
+*************
+
+https://django-unfriendly.readthedocs.org/en/latest/
---
+++ packages/django-unfriendly/django-unfriendly-0.3.tar.gz/django-unfriendly-0.3/unfriendly/tests/__init__.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""Test suite for django-unfriendly."""
+
+import datetime
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
+
+from unfriendly import settings
+from unfriendly.utils import encrypt, decrypt
+from unfriendly.templatetags.unfriendly_tags import obfuscate
+
+
+class UnfriendlyTests(TestCase):
+    """Test case for django-unfriendly."""
+    urls = 'unfriendly.tests.urls'
+
+    def setUp(self):
+        self.juice = 'Lorem ipsum dolor sit amet'
+
+    def test_encryption(self):
+        """
+        Test the encryption.
+        """
+        original = self.juice
+
+        obfuscated = encrypt(original, settings.UNFRIENDLY_SECRET,
+                             settings.UNFRIENDLY_IV)
+        self.assertNotEqual(original, obfuscated)
+
+        deobfuscated = decrypt(obfuscated, settings.UNFRIENDLY_SECRET,
+                               settings.UNFRIENDLY_IV)
+        self.assertEqual(original, deobfuscated)
+
+    def test_obfuscate_filter(self):
+        """
+        Test the obfuscate filter.
+        """
+        test_url = reverse('unfriendly-test')
+        obfuscated_url = obfuscate(test_url)
+        view_url = reverse('unfriendly-deobfuscate', kwargs={
+            'key': encrypt(test_url, settings.UNFRIENDLY_SECRET,
+                           settings.UNFRIENDLY_IV),
+        })
+        self.assertEqual(view_url, obfuscated_url)
+
+    def test_obfuscate_filter_with_juice(self):
+        """
+        Test the obfuscate filter.
+        """
+        test_url = reverse('unfriendly-test')
+        obfuscated_url = obfuscate(test_url, self.juice)
+        view_url = reverse('unfriendly-deobfuscate', kwargs={
+            'juice': slugify(self.juice),
+            'key': encrypt(test_url, settings.UNFRIENDLY_SECRET,
+                           settings.UNFRIENDLY_IV),
+        })
+        self.assertEqual(view_url, obfuscated_url)
+
+    def test_deobfuscate_view(self):
+        """
+        Test the deobfuscate view.
+        """
+        test_url = reverse('unfriendly-test')
+        obfuscated_url = obfuscate(test_url)
+
+        test_response = self.client.get(test_url)
+        obfuscated_response = self.client.get(obfuscated_url)
+
+        self.assertEqual(test_response.content, obfuscated_response.content)
+
+    def test_deobfuscate_view_with_juice(self):
+        """
+        Test the deobfuscate view with seo juice.
+        """
+        test_url = reverse('unfriendly-test')
+        obfuscated_url = obfuscate(test_url, self.juice)
+
+        test_response = self.client.get(test_url)
+        obfuscated_response = self.client.get(obfuscated_url)
+
+        self.assertEqual(test_response.content, obfuscated_response.content)
+
+    def test_deobfuscate_view_404(self):
+        """
+        Test deobfuscate view with hacked url.
+        """
+        view_url = reverse('unfriendly-deobfuscate', kwargs={
+            'key': 'hacked-key',
+        })
+        response = self.client.get(view_url)
+        self.assertEqual(response.status_code, 404)
