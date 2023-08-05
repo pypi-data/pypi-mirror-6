99dcc57076c1525e53f606be2e04d1fa37878a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package django-s3direct/0.1.5/django-s3direct-0.1.5.tar.gz

---
+++ packages/django-s3direct/django-s3direct-0.1.5.tar.gz/django-s3direct-0.1.5/README.md
@@ -0,0 +1,60 @@
+django-s3direct
+===============
+
+Upload files direct to S3 from Django
+-------------------------------------
+  
+  
+Add direct uploads to AWS S3 functionality with a progress bar to file input fields within Django admin.
+
+![screenshot](https://raw.github.com/bradleyg/django-s3direct/master/screenshot.png)
+
+
+## Installation
+
+Install with Pip:
+
+```pip install django-s3direct```
+
+
+Update Django files:  
+
+Currently only python 2.7 supported.  
+
+```python
+# settings.py
+INSTALLED_APPS = [
+    ...
+    's3direct',
+    ...
+]
+S3DIRECT_DIR = 's3direct' # (optional, default is 's3direct')
+AWS_SECRET_ACCESS_KEY = ''
+AWS_ACCESS_KEY_ID = ''
+AWS_STORAGE_BUCKET_NAME = ''
+```
+  
+```python
+# urls.py
+urlpatterns = patterns('',
+    url(r'^s3direct/', include('s3direct.urls')),
+)
+```
+  
+```python
+# models.py
+from django.db import models
+from s3direct.fields import S3DirectField
+
+class Example(models.Model):
+    thumbnail = S3DirectField(upload_to='s3direct')
+    # 'upload_to' is an optional argument
+```
+
+You may need to run `collectstatic` before `s3direct` will work correctly on your public website:
+
+```bash
+python manage.py collectstatic
+````
+  
+[![Build Status](https://secure.travis-ci.org/bradleyg/django-s3direct.png)](http://travis-ci.org/bradleyg/django-s3direct) 
---
+++ packages/django-s3direct/django-s3direct-0.1.5.tar.gz/django-s3direct-0.1.5/s3direct/views.py
@@ -0,0 +1,50 @@
+import hashlib, uuid, hmac, json
+from datetime import datetime, timedelta
+from base64 import b64encode
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import user_passes_test
+from django.views.decorators.http import require_POST
+
+
+S3DIRECT_DIR = getattr(settings, "S3DIRECT_DIR", 's3direct')
+
+
+@csrf_exempt
+@require_POST
+@user_passes_test(lambda u: u.is_staff)
+def get_upload_params(request, upload_to=''):
+    content_type = request.POST['type']
+    data = create_upload_data(content_type, upload_to)
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+def create_upload_data(content_type, upload_to):
+    expires = (datetime.now() + timedelta(hours=24)).strftime('%Y-%m-%dT%H:%M:%S.000Z')
+
+    policy_object = json.dumps({
+        "expiration": expires,
+        "conditions": [
+            {"bucket": settings.AWS_STORAGE_BUCKET_NAME},
+            {"acl": "public-read"},
+            {"Content-Type": content_type},
+            ["starts-with", "$key", ""],
+            {"success_action_status": "201"}
+        ]
+    })
+
+    policy = b64encode(policy_object.replace('\n', '').replace('\r', ''))
+    signature = b64encode(hmac.new(settings.AWS_SECRET_ACCESS_KEY, policy, hashlib.sha1).digest())
+    key = "%s/%s/${filename}" % (upload_to or S3DIRECT_DIR, uuid.uuid4().hex)
+
+    return {
+        "policy": policy,
+        "signature": signature,
+        "key": key,
+        "AWSAccessKeyId": settings.AWS_ACCESS_KEY_ID,
+        "form_action": "http://%s.s3.amazonaws.com" % settings.AWS_STORAGE_BUCKET_NAME,
+        "success_action_status": "201",
+        "acl": "public-read",
+        "Content-Type": content_type
+    }
