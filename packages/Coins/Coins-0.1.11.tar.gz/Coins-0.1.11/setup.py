393ce0a5438852acb2d521885d8c7709e6d3a230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:25 +0000

    Add package Coins/0.1.11/Coins-0.1.11.tar.gz

---
+++ packages/Coins/Coins-0.1.11.tar.gz/Coins-0.1.11/setup.py
@@ -0,0 +1,34 @@
+"""
+Coins
+==============
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='Coins',
+    version='0.1.11',
+    url='https://github.com/halfmoonlabs/coins',
+    license='MIT',
+    author='Halfmoon Labs',
+    author_email='hello@halfmoon.io',
+    description='Tools for Bitcoin and other cryptocurrencies (including Litecoin, Namecoin, Peercoin, and Primecoin).',
+    keywords='bitcoin btc litecoin namecoin cryptocurrency',
+    packages=[
+        'coins',
+    ],
+    zip_safe=False,
+    install_requires=[
+        'ecdsa>=0.10',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
