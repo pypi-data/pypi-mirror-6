938ebb194b7ffbe51f0db9e9673f4d8d7f873cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:19 +0000

    Add package kaboom/0.2.8/kaboom-0.2.8.tar.gz

---
+++ packages/kaboom/kaboom-0.2.8.tar.gz/kaboom-0.2.8/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from kaboom import __version__
+
+import os
+
+description = 'Command line key-value store'
+long_description = description
+if os.path.exists('README.txt'):
+    long_description = open('README.rst').read()
+
+
+setup(
+    name='kaboom',
+    version=__version__,
+    packages=['kaboom'],
+    url='http://github.com/vinu76jsr/kaboom',
+    author='vaibhav',
+    author_email='vinu76jsr@gmail.com',
+    license='bsd3',
+    description=description,
+    entry_points={'console_scripts': [
+        'kaboom = kaboom.kaboom:run']},
+    long_description=long_description
+)
