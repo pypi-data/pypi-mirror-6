7c3ac807f3eaad0dfdf27ba34d35705884b657ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:19 +0000

    Add package mtq/0.3.0/mtq-0.3.0.tar.gz

---
+++ packages/mtq/mtq-0.3.0.tar.gz/mtq-0.3.0/setup.py
@@ -0,0 +1,38 @@
+'''
+@author: sean
+'''
+
+import sys
+
+if 'develop' in sys.argv:
+    # Don't import setuptools unless the user is actively trying to do
+    # something that requires it.
+    from setuptools import setup
+
+else:
+    from distutils.core import setup
+
+setup(
+    name='mtq',
+    version="0.3.0",
+    author='Continuum Analytics',
+    author_email='sean.ross-ross@continuum.io',
+    description='Mongo Task Queue',
+    packages=['mtq'],
+    
+    install_requires=['pymongo>=2.5',
+                      'python-dateutil>=2.1',
+                      'pytz>=2013b',
+                      ],
+    entry_points={
+          'console_scripts': [
+              'mtq-worker = mtq.scripts.worker:main',
+              'mtq-info = mtq.scripts.info:main',
+              'mtq-tail = mtq.scripts.log:main',
+              'mtq-scheduler = mtq.scripts.schedule:main',
+              'mtq-ctrl = mtq.scripts.ctrl:main',
+              ]
+                 },
+
+)
+
