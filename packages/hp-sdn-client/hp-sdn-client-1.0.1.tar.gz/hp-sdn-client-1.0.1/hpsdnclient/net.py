a60f0e564cf243726eb700037c469f8c80828eb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:38 +0000

    Add package hp-sdn-client/1.0.1/hp-sdn-client-1.0.1.tar.gz

---
+++ packages/hp-sdn-client/hp-sdn-client-1.0.1.tar.gz/hp-sdn-client-1.0.1/hpsdnclient/net.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+#
+#   Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import json
+import urllib
+
+from hpsdnclient.api import ApiBase
+from hpsdnclient.error import raise_errors
+from hpsdnclient.datatypes import LldpProperties
+
+class NetMixin(ApiBase):
+    """Network Service REST API Methods
+
+    This class contains methods that call the Network Services
+    REST API functions in the HP VAN SDN Controller
+
+    - Topology Service
+    - Node Service
+    - Link Service
+    - Path Planner
+    - Path Diagnostics Service
+
+    """
+    def __init__(self, controller, auth):
+        super(NetMixin, self).__init__(controller, auth)
+        self._net_base_url = ("https://{0}:8443".format(self.controller) +
+                             "/sdn/v2.0/net/")
+        self._diag_base_url = ("https://{0}:8443".format(self.controller) +
+                             "/sdn/v2.0/diag/")
+
+    def get_clusters(self):
+        """ Gets a list of clusters
+
+        :return: A list of clusters
+        :rtype: list
+
+        """
+        url = self._net_base_url + 'clusters'
+        return self.restclient.get(url)
+
+    def get_cluster_broadcast_tree(self, cluster_id):
+        """ Gets the broadcast tree for a specific cluster
+
+        :param str cluster_id: The cluster ID
+        :return: The broadcast tree for the provided cluster ID
+        :rtype: hpsdnclient.datatypes.Cluster
+
+        """
+        url = self._net_base_url + 'clusters/{0}/tree'.format(cluster_id)
+        return self.restclient.get(url)
+
+    def get_links(self, dpid=None):
+        """ Returns a list of all links discovered by the SDN controller
+
+        :param str dpid: Return only the links for the specified DPID
+        :return: A list of Links
+        :rtype: list
+
+        """
+        url = self._net_base_url + 'links'
+        if dpid:
+            url = url + '?dpid={0}'.format(urllib.quote(dpid))
+        return self.restclient.get(url)
+
+    def get_forward_path(self, src_dpid, dst_dpid):
+        """ Gets the shortest computed path between src_dpid and dst_dpid
+
+        :param str src_dpid: The source DPID
+        :param str dst_dpid: THe destination DPID
+        :return: The shortest path between the two DPID's
+        :rtype: hpsdnclient.datatypes.Path
+
+        """
+        url = (self._net_base_url +
+               'paths/forward' +
+               '?src_dpid={0}&dst_dpid={1}'.format(urllib.quote(src_dpid),
+                                                   urllib.quote(dst_dpid)))
+        return self.restclient.get(url)
+
+    def get_arps(self, vid=None, ip=None):
+        """ Provides ARP details for the given IP address and VLAN ID
+
+        :param str vid: Return ARPs in the provided VLAN ID
+        :param str ip: Return only the ARP for the specified IP Address
+        :return: List of ARPs
+        :rtype: list
+
+        """
+        url = self._net_base_url + 'arps'
+
+        if vid and not ip:
+            url = url + "?vid={0}".format(vid, ip)
+        elif vid and ip:
+            url = url + "?vid={0}&ip={1}".format(vid, ip)
+
+        return self.restclient.get(url)
+
+    def get_nodes(self, ip=None, vid=None, dpid=None, port=None):
+        """ Get all Nodes discovered by the controller
+
+        - When supplied with``ip`` and ``vid`` returns node details
+        - When supplied with ``vid`` returns Nodes in the specified VLAN
+        - When supplied with ``dpid`` returns Nodes attached to the specified DPID
+        - When supplied with ``dpid`` and ``port`` returns Nodes attached to the given port on the specified DPID
+
+        :param str ip: IP address
+        :param str vid: VLAN ID
+        :param str dpid: Datapath ID
+        :param str port: Port
+
+        """
+        url = self._net_base_url + 'nodes'
+
+        if vid and not ip:
+            url += "?vid={0}".format(vid, ip)
+        elif vid and ip:
+            url += "?vid={0}&ip={1}".format(vid, ip)
+        elif dpid and not port:
+            url += "?dpid={0}".format(urllib.quote(dpid))
+        elif dpid and port:
+            url += "?dpid={0}&port={1}".format(urllib.quote(dpid), port)
+
+        return self.restclient.get(url)
+
+    def get_lldp_suppressed_ports(self):
+        """ Gets a list of LLDP suppressed ports from the controller
+
+        :return: A list of ports in the LLDP suppressed state
+        :rtype: list
+
+        """
+        url = self._net_base_url + 'lldp'
+        return self.restclient.get(url)
+
+    def set_lldp_suppressed(self, ports):
+        """ Puts the provided ports in to LLDP suppressed state
+
+        :params str ports: The ports to be suppressed
+
+        """
+        if isinstance(ports, list):
+            tmp = []
+            for item in list:
+                if isinstance(item, LldpProperties):
+                    tmp.append(item.to_dict())
+                else:
+                    tmp.append(item)
+            data = {"lldp_suppressed": tmp }
+
+        else:
+            data = {"lldp_suppressed": [ports.to_dict()]}
+
+        url = self._net_base_url + 'lldp'
+        r = self.restclient.post(url, json.dumps(data))
+        raise_errors(r)
+
+    def remove_lldp_suppressed(self, ports):
+        """ Removes ports from LLDP suppressed state
+
+        :params hpsdnclient.datatypes.LldpProperties ports: The ports to be removed from LLDP suppressed state
+
+        """
+
+        if isinstance(ports, list):
+            tmp = []
+            for item in list:
+                if isinstance(item, LldpProperties):
+                    tmp.append(item.to_dict())
+                else:
+                    tmp.append(item)
+            data = {"lldp_suppressed": tmp }
+
+        else:
+            data = {"lldp_suppressed": [ports.to_dict()]}
+        url = self._net_base_url + 'lldp'
+        r = self.restclient.delete(url, json.dumps(data))
+        raise_errors(r)
+
+    def get_diag_observation_posts(self, packet_uid=None, packet_type=None):
+        """ Gets a list of diagnostic observation posts
+
+        :param str packet_uid: Only return OP's with the provided Packet UID
+        :param str packet_type: Return only OP's with the provided Packet Type
+        :return: List of OP's
+        :rtype: list
+
+        """
+        url = self._diag_base_url + 'observations'
+        if packet_uid:
+            url += '?packet_uid={}'.format(packet_uid)
+        if packet_type:
+            url += '?packet_type={}'.format(packet_type)
+        return self.restclient.get(url)
+
+    def create_diag_observation_post(self, observation):
+        """ Creates a diagnostic observation post
+
+        :param hpsdnclient.datatypes.Observation observation: The observation post to create
+
+        """
+        data = { "observation" : observation.to_dict()}
+        url = self._diag_base_url + 'observations'
+        r = self.restclient.post(url, json.dumps(data))
+        raise_errors(r)
+
+    def delete_diag_observation_post(self, observation):
+        """ Delete a diagnostic observation post
+
+        :param str observation: The observation post to delete
+
+        """
+        data = { "observation" : observation.to_dict()}
+        url = self._diag_base_url + 'observations'
+        r = self.restclient.delete(url, json.dumps(data))
+        raise_errors(r)
+
+    def get_diag_packets(self, packet_type=None):
+        """ Get a list of all diagnostic packets in the system
+
+        :param packet_type: Filter result by provided packet type
+        :return: A list of diagnostics packets
+        :rtype: list
+
+        """
+        url = self._diag_base_url + 'packets'
+        if packet_type:
+            url += '?type{}'.format(packet_type)
+        return self.restclient.get(url)
+
+    def create_diag_packet(self, packet):
+        """ Create a diagnostic packet
+
+        :param str packet: The packet to create
+
+        """
+        data = {"packet": packet.to_dict()}
+        url = self._diag_base_url + 'packets'
+        r = self.restclient.post(url, json.dumps(data))
+        raise_errors(r)
+
+    def delete_diag_packet(self, packet_uid):
+        """ Delete a diagnostic packet
+
+        :param str packet_uid: The uid of the packet to be deleted
+
+        """
+        url = self._diag_base_url + 'packets/{}'.format(packet_uid)
+        r = self.restclient.delete(url)
+        raise_errors(r)
+
+    def get_diag_packet(self, packet_uid):
+        """ Get diagnostic packet details for the provided packet UID
+
+        :param str packet_uid: The packet UID to get details for
+        :return: Diagnostic Packet Details
+        :rtype: hpsdnclient.datatypes.Packet
+
+        """
+        url = self._diag_base_url + 'packets/{}'.format(packet_uid)
+        return self.restclient.get(url)
+
+    def get_diag_packet_path(self, packet_uid):
+        """ Get expected paths for diagnostic packet
+
+        :param packet_uid: The UID of the diagnostic packet
+        :return: Path
+        :rtype: hpsdnclient.datatypes.Path
+
+        """
+        url = self._diag_base_url + 'packets/{}/path'.format(packet_uid)
+        return self.restclient.get(url)
+
+    def get_diag_packet_nexthop(self, packet_uid, dpid):
+        """ Show next hop information for packet at a given Datapath ID
+
+        :param str packet_uid: The packet UID
+        :param str dpid: The Datapath ID
+        :returns: The next hop
+        :rtype: hpsdnclient.datatypes.NextHop
+
+        """
+        url = self._diag_base_url + 'packets/{}/nexthops'.format(packet_uid)
+        url += '?src_dpid={}'.format(dpid)
+        return self.restclient.get(url)
+
+    def set_diag_packet_action(self, packet_uid, action):
+        """ Performs the specified simulation action on a packet
+
+        :param str packet_uid: The packet UID
+        :param str action: The action to perform
+
+        """
+        data = {"simulation" : action}
+        url = self._diag_base_url + 'packets/{}/action'.format(packet_uid)
+        r = self.restclient.post(url, json.dumps(data))
+        raise_errors(r)
---
+++ packages/hp-sdn-client/hp-sdn-client-1.0.1.tar.gz/hp-sdn-client-1.0.1/hpsdnclient/of.py
@@ -0,0 +1,361 @@
+#!/usr/bin/env python
+#
+#   Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import json
+#Python3 compatibility
+try:
+    import urllib.parse as urllib
+except ImportError:
+    import urllib
+
+from hpsdnclient.api import ApiBase
+import hpsdnclient.datatypes as datatypes
+from hpsdnclient.error import raise_errors, DatatypeError
+
+class OfMixin(ApiBase):
+    """OpenFlow REST API Methods
+
+    This class contains methods that call the OpenFlow
+    REST API on the HP VAN SDN Controller
+
+    - Topology Service
+    - Node Service
+    - Link Service
+    - Path Planner
+    - Path Diagnostics Service
+
+    """
+    def __init__(self, controller, restclient):
+        super(OfMixin, self).__init__(controller, restclient)
+        self._of_base_url = ("https://{0}:8443".format(self.controller) +
+                             "/sdn/v2.0/of/")
+
+    def get_stats(self):
+        """List controller statistics for all controllers that are
+        part of this controller's team.
+
+        :return: List of statistics
+        :rtype: hpsdnclient.datatypes.Stats"""
+        url = self._of_base_url + 'stats'
+        return self.restclient.get(url)
+
+    def get_port_stats(self, dpid, port_id=None):
+        """List all port statistics for a given datapath or for a
+        given datapath and port number
+
+        :param str dpid: Filter by Datapath ID
+        :param str port_id: Filter by Port ID
+        :returns: Statistics for Port
+        :rtype: hpsdnclient.datatypes.Stats
+
+        """
+        url = (self._of_base_url +
+              'stats/ports?dpid={0}'.format(urllib.quote(dpid)))
+        if port_id:
+            url = url + '&port_id={0}'.format(port_id)
+        return self.restclient.get(url)
+
+    def get_group_stats(self, dpid, group_id=None):
+        """List group statistics
+
+        :param str dpid: Filter by Datapath ID
+        :param group_id: Filter by Group ID
+        :return: Group statistics
+        :rtype: hpsdnclient.datatypes.Stats
+
+        """
+        url = (self._of_base_url +
+               'stats/groups?dpid={0}'.format(urllib.quote(dpid)))
+        if group_id:
+            url = url + '&group_id={0}'.format(group_id)
+        return self.restclient.get(url)
+
+    def get_meter_stats(self, dpid, meter_id):
+        """List meter statistics for
+
+        :param str dpid: The Datapath ID
+        :param str meter_id: The Meter ID
+        :return: Meter statistics
+        :rtype: hpsdnclient.datatypes.Stats
+
+        """
+        url = (self._of_base_url +
+               'stats/meters?dpid={0}&meter={1}'.format(urllib.quote(dpid),
+                                                        meter_id))
+        return self.restclient.get(url)
+
+    def get_datapaths(self):
+        """List all datapaths that are managed by this controller.
+
+        :return: A list of Datapaths
+        :rtype: list
+
+        """
+        url = self._of_base_url + 'datapaths'
+        return self.restclient.get(url)
+
+    def get_datapath_detail(self, dpid):
+        """Get detailed information for a datapath.
+
+        :param str dpid: The datapath ID
+        :return: Datatpath details
+        :rtype: hpsdnclient.datatypes.Datapath
+
+        """
+        url = (self._of_base_url + 'datapaths/{0}'.format(urllib.quote(dpid)))
+        return self.restclient.get(url)
+
+    def get_meter_features(self, dpid):
+        """Get meter features for the provided Datapath ID
+
+        :param str dpid: The Datapath ID
+        :return: Meter Features
+        :rtype: hpsdnclient.datatypes.MeterFeatures
+
+        """
+
+        url = (self._of_base_url +
+               'datapaths/{0}/features/meter'.format(urllib.quote(dpid)))
+        return self.restclient.get(url)
+
+    def get_group_features(self, dpid):
+        """Get datapath group features
+
+        :param str dpid: The Datapath ID
+        :return: Group Features
+        :rtype: hpsdnclient.datatypes.GroupFeatures
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/features/group'.format(urllib.quote(dpid)))
+        return self.restclient.get(url)
+
+    def get_ports(self, dpid):
+        """ Gets a list of ports from the specified DPID
+
+        :param str dpid: The datapath ID
+        :return: List of ports
+        :rtype: list
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/ports'.format(urllib.quote(dpid)))
+        return self.restclient.get(url)
+
+    def get_port_detail(self, dpid, port_id):
+        """ Gets detailed port information for the specified port
+
+        :param str dpid: The datapath ID
+        :param str port_id: The port ID
+        :return: Port details
+        :rtype: hpsdnclient.datatypes.Port
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/ports/{1}'.format(urllib.quote(dpid), port_id))
+        return self.restclient.get(url)
+
+    def get_meters(self, dpid):
+        """List all meters configured on the supplied DPID
+
+        :param str dpid: The datapath ID
+        :returns: A list of meters
+        :rtype: list
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/meters'.format(urllib.quote(dpid)))
+        return self.restclient.get(url)
+
+    def add_meter(self, dpid, meter):
+        """Add a new meter to the supplied DPID
+
+        :param str dpid:
+        :param hpsdnclient.datatypes.Meter meter: The new Meter object
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/meters'.format(urllib.quote(dpid)))
+        r = self.restclient.post(url, json.dumps(meter.to_dict()))
+        raise_errors(r)
+
+    def get_meter_details(self, dpid, meter_id):
+        """Get detailed meter information
+
+        :param str dpid: The datapath ID
+        :param str meter_id: The meter ID
+        :return: Meter details
+        :rtype: hpsdnclient.datatypes.Meter
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/meters/{1}'.format(urllib.quote(dpid), meter_id))
+        return self.restclient.get(url)
+
+    def update_meter(self, dpid, meter_id, meter):
+        """ Update the specified meter
+
+        :param str dpid: The datapath ID
+        :param str meter_id: The meter ID
+        :param hpsdnclient.datatypes.Meter meter: The meter
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/meters/{1}'.format(urllib.quote(dpid), meter_id))
+        r = self.restclient.put(url, meter)
+        raise_errors(r)
+
+    def delete_meter(self, dpid, meter_id):
+        """Delete a meter
+
+        :param str dpid: The datapath ID
+        :param str meter_id: The meter ID to be deleted
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/meters/{1}'.format(urllib.quote(dpid), meter_id))
+        r = self.restclient.put(url, self.auth)
+        raise_errors(r)
+
+    def get_flows(self, dpid):
+        """Gets a list of flows on the supplied DPID
+
+
+        :param str dpid: The datapath ID
+        :return: List of flows
+        :rtype: list
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/flows'.format(urllib.quote(dpid)))
+        return self.restclient.get(url)
+
+    def _assemble_flows(self, flows):
+        if isinstance(flows, list):
+            tmp = []
+            for f in flows:
+                if isinstance(f, datatypes.Flow):
+                    tmp.append(f.to_dict())
+                else:
+                    raise DatatypeError(datatypes.Flow, f.__class__())
+            data = {"flows": tmp }
+        elif isinstance(flows, datatypes.Flow):
+            data = {"flow": flows.to_dict() }
+        else:
+            raise DatatypeError([datatypes.Flow, list], f.__class__())
+        return data
+
+    def add_flows(self, dpid, flows):
+        """Add a flow, or flows to the selected DPID
+
+        :param str dpid: The datapath ID
+        :param list, hpsdnclient.datatypes.Flow flows: The flow or flows to add
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/flows'.format(urllib.quote(dpid)))
+        data = self._assemble_flows(flows)
+        r = self.restclient.post(url, json.dumps(data))
+        raise_errors(r)
+
+    def update_flows(self, dpid, flows):
+        """Update a flow, or flows at the selected DPID
+
+        :param str dpid: The datapath ID
+        :param list, hpsdnclient.datatypes.Flow flows: The flow or flows to update
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/flows'.format(urllib.quote(dpid)))
+        data = self._assemble_flows(flows)
+        r = self.restclient.put(url, json.dumps(data))
+        raise_errors(r)
+
+    def delete_flows(self, dpid, flows):
+        """ Delete flow, or flows from the specified DPID
+
+        :param str dpid: The datapath ID
+        :param list, hpsdnclient.datatypes.Flow flows: The flow or flows to delete
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/flows'.format(urllib.quote(dpid)))
+        data = self._assemble_flows(flows)
+        r = self.restclient.delete(url, json.dumps(data))
+        raise_errors(r)
+
+    def get_groups(self, dpid):
+        """Get a list of groups created on the DPID
+
+        :param str dpid: The datapath ID
+        :return: List of groups
+        :rtype: list
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/groups'.format(urllib.quote(dpid)))
+
+        return self.restclient.get(url)
+
+    def add_group(self, dpid, group):
+        """Create a group
+
+        :param str dpid: The datapath ID
+        :param hpsdnclient.datatypes.Group group: The group to add
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/groups'.format(urllib.quote(dpid)))
+        data = {"group": group.to_dict()}
+        r = self.restclient.post(url, json.dumps(data))
+        raise_errors(r)
+
+    def get_group_details(self, dpid, group_id):
+        """Get group details
+
+        :param str dpid: The datapath ID
+        :param str group_id: The group ID
+        :return: Group details
+        :rtype: hpsdnclient.datatypes.Group
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/groups/{1}'.format(urllib.quote(dpid), group_id))
+        return self.restclient.get(url)
+
+    def update_group(self, dpid, group_id, group):
+        """Update a group
+
+        :param str dpid: The datapath ID
+        :param hpsdnclient.datatypes.Group group: The group to add
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/groups/{1}'.format(urllib.quote(dpid), group_id))
+        r = self.restclient.post(url, json.dumps(group.to_dict()))
+        raise_errors(r)
+
+    def delete_groups(self, dpid, group_id):
+        """Delete a group
+
+        :param str dpid: The datapath ID
+        :param str group_id: The group ID to delete
+
+        """
+        url = (self._of_base_url +
+               'datapaths/{0}/groups/{1}'.format(urllib.quote(dpid), group_id))
+        r = self.restclient.delete(url, self.auth)
+        raise_errors(r)
