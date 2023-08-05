04465f0c22a1740fba856a0d20ba35e5ab9600ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:09 +0000

    Add package django-thermostat/2.0.7/django-thermostat-2.0.7.tar.gz

---
+++ packages/django-thermostat/django-thermostat-2.0.7.tar.gz/django-thermostat-2.0.7/django_thermostat/settings.py
@@ -0,0 +1,24 @@
+
+
+PASILLO_DID = "A1"
+CUARTO_OESTE_DID = "A2"
+SALON_DID = "A5"
+HEATER_DID = "C1"
+HEATER_PROTOCOL = "X10"
+HEATER_API = "http://raspberry:8000"
+
+HEATER_USERNAME = "raton"
+HEATER_PASSWORD = "soportep"
+HEATER_MARGIN = 0.0
+HEATER_INCREMENT = 0.5
+
+#INTERNAL_TEMPERATURE_URI = "http://raspberry/therm/temperature"
+#INTERNAL_TEMPERATURE_URI = "http://localhost:8000/therm/temperature"
+
+LIST_THERMOMETERS_API = "http://raspberry/therm/temperature_api/thermometers?"
+TEMPERATURES_API = "http://raspberry/therm/temperature_api/thermometers"
+#THERMOMETER_API = "http://raspberry/therm/temperature_api/thermometer/"
+
+FLAME_STATS = True
+FLAME_STATS_PATH = "./flame_stats.log"
+FLAME_STATS_DATE_FORMAT = "%d.%m.%Y %H:%M:%S"
