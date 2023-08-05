a8da326ab02d32fbee1e2996d2fe722b21b84307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package Spruce-db/0.2.2/Spruce_db-0.2.2-py2.7.egg

---
+++ packages/Spruce-db/Spruce_db-0.2.2-py2.7.egg/spruce/db/__init__.py
@@ -0,0 +1,12 @@
+"""Database interaction"""
+
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
+__credits__ = ["Ivan D Vasin"]
+__maintainer__ = "Ivan D Vasin"
+__email__ = "nisavid@gmail.com"
+__docformat__ = "restructuredtext"
+
+from ._connparams import *
+from ._engines import *
+from ._exc import *
+from ._time import *
