e909d89ba59e15ec761eb00029676db2165a1273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:03 +0000

    Add package ads/0.0.798/ads-0.0.798.tar.gz

---
+++ packages/ads/ads-0.0.798.tar.gz/ads-0.0.798/ads/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+""" A Python tool for interacting with NASA's ADS system. """
+
+__author__ = "Andy Casey <andy@astrowizici.st>"
+__version__ = "0.0.798"
+
+from core import search, metrics, metadata, retrieve_article
---
+++ packages/ads/ads-0.0.798.tar.gz/ads-0.0.798/ads/utils.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+""" Utilities for interacting with NASA's ADS system. """
+
+from __future__ import division, print_function
+
+__author__ = "Andy Casey <andy@astrowizici.st>"
+
+# Standard library
+import os
+
+# Third party
+import requests
+
+__all__ = ["get_dev_key", "get_api_settings", "unique_preserved_list"]
+
+def get_dev_key():
+    """Retrieves the API key for ADS Labs."""
+
+    ads_dev_key_filename = os.path.abspath(os.path.expanduser("~/.ads/dev_key"))
+
+    if os.path.exists(ads_dev_key_filename):
+        with open(ads_dev_key_filename, 'r') as fp:
+            dev_key = fp.readline().rstrip()
+
+        return dev_key
+
+    if "ADS_DEV_KEY" in os.environ:
+        return os.environ["ADS_DEV_KEY"]
+
+    raise IOError("no ADS API key found in ~/.ads/dev_key and no ADS_DEV_KEY environment variable found")
+
+
+def get_api_settings(developer_api_key=None):
+    """Gets the API settings for the developer key provided."""
+
+    if developer_api_key is None:
+        developer_api_key = get_dev_key()
+        
+    r = requests.get("http://labs.adsabs.harvard.edu/adsabs/api/settings/", params={"dev_key": developer_api_key})
+    if not r.ok: r.raise_for_status()
+
+    return r.json()
+
+
+def unique_preserved_list(original_list):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in original_list if x not in seen and not seen_add(x)]
