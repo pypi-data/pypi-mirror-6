c93e326a79b08087346b164cddcf09e33ba21979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:19 +0000

    Add package print_raka/1.0.0/print_raka-1.0.0.zip

---
+++ packages/print_raka/print_raka-1.0.0.zip/print_raka-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name      ='print_raka',
+	version   ='1.0.0',
+	py_modules  =['print_raka'],
+	author    ='rakesh grover',
+	author_email ='rakeshgrover@live.com',
+	url          ='',
+	description   ='it will print the nested or non-nested list in a beautifull way',
+	)
