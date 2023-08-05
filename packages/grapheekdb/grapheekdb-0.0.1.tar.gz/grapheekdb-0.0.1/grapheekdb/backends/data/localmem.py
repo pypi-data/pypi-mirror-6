9284817587d341c9e25da2b0a0c492f5730f7043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package grapheekdb/0.0.1/grapheekdb-0.0.1.tar.gz

---
+++ packages/grapheekdb/grapheekdb-0.0.1.tar.gz/grapheekdb-0.0.1/grapheekdb/backends/data/localmem.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8 -*-
+
+from grapheekdb.backends.data.base import BaseGraph
+
+from grapheekdb.lib.undef import UNDEFINED
+
+
+class LocalMemoryGraph(BaseGraph):
+
+    def __init__(self):
+        self._dic = {}
+        super(LocalMemoryGraph, self).__init__()
+        self._ensure_prepared()
+
+    def _transaction_begin(self):
+        pass
+
+    def _transaction_commit(self, txn):
+        pass
+
+    def _transaction_rollback(self, txn):
+        pass
+
+    def _has_key(self, key):
+        return key in self._dic
+
+    def _get(self, txn, key):
+        return self._dic.get(key, UNDEFINED)  # Not returning None, as None is a valid value
+
+    def _set(self, txn, key, value):
+        self._dic[key] = value
+
+    def _remove(self, txn, key):
+        try:
+            del self._dic[key]
+        except KeyError:
+            pass  # I want _remove method to be idempotent
+
+    def _remove_prefix(self, txn, prefix):
+        remove_keys = [key for key in self._dic if key.startswith(prefix)]
+        for key in remove_keys:
+            self._remove(txn, key)
