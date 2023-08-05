d947a15aee26139a548633a0e89794e0355b8f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:14 +0000

    Add package wheezy.security/0.1.60/wheezy.security-0.1.60.tar.gz

---
+++ packages/wheezy.security/wheezy.security-0.1.60.tar.gz/wheezy.security-0.1.60/src/wheezy/security/__init__.py
@@ -0,0 +1,11 @@
+
+"""
+"""
+
+__version__ = '0.1.60'
+
+# flake8: noqa
+
+from wheezy.security.authorization import authorized
+from wheezy.security.errors import SecurityError
+from wheezy.security.principal import Principal
