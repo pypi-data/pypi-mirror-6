9d26aa541b9fd53b2c25dcd630aaa8912a9667a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:27 +0000

    Add package baidubaike/1.2/baidubaike-1.2.tar.gz

---
+++ packages/baidubaike/baidubaike-1.2.tar.gz/baidubaike-1.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+dependencies = [
+    "beautifulsoup4", 
+    "requests"
+]
+
+setup(
+    name = 'baidubaike',
+    packages = ['baidubaike'], 
+    version = '1.2',
+    description = 'A wrapper of Baidu Baike',
+    author = 'yakiang',
+    author_email = 'strak47@gmail.com',
+    url = 'https://github.com/yakiang/baidubaike',   
+    download_url = 'http://yakiang.com/project/baidubaike-1.0.tar.gz', 
+    keywords = ['baidu', 'wiki', 'API', 'html'], 
+    classifiers = (
+        'Operating System :: OS Independent', 
+        'Development Status :: 4 - Beta', 
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content', 
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ), 
+    long_description = """
+    With this package you can easily get content of an item in Baidu Baike,
+    you can also search relative items. 
+    """
+)
