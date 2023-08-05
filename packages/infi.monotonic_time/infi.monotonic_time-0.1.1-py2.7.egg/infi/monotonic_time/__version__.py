8904fd8266d745e4853c0d32d62ef3816dd843f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:41 +0000

    Add package infi.monotonic_time/0.1.1/infi.monotonic_time-0.1.1-py2.7.egg

---
+++ packages/infi.monotonic_time/infi.monotonic_time-0.1.1-py2.7.egg/infi/monotonic_time/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.1"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '09f09adf536e5dc26bbe18c890ce4b2f8872f319'
+__git_head_subject__ = 'TRIVIAL handling weird Ubuntu machines that do not have ctypes.get_errno'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
