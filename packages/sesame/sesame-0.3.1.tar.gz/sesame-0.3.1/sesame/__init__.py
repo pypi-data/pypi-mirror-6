e7f0d5050d32ca13bf4481dde6c2855ef7f726a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:14 +0000

    Add package sesame/0.3.1/sesame-0.3.1.tar.gz

---
+++ packages/sesame/sesame-0.3.1.tar.gz/sesame-0.3.1/sesame/__init__.py
@@ -0,0 +1,10 @@
+#
+# Sesame
+#
+
+__title__ = 'sesame'
+__version__ = '0.3.1'
+__build__ = 0x000031
+__author__ = 'Matt Black'
+__license__ = 'Simplified BSD License'
+__copyright__ = 'Copyright 2013 Matt Black'
