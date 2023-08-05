b321a65e25ab472436bfd5b6e23f095ac90b48cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package Spruce-http-common/0.1.3/Spruce-http-common-0.1.3.tar.gz

---
+++ packages/Spruce-http-common/Spruce-http-common-0.1.3.tar.gz/Spruce-http-common-0.1.3/spruce/http/status/__init__.py
@@ -0,0 +1,11 @@
+"""Response status codes.
+
+This is an extension of the status code constants defined in
+:mod:`httplib` along with related objects.
+
+"""
+
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
+__docformat__ = "restructuredtext"
+
+from ._core import *
