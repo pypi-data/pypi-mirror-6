92b64a8799092ac61de8e271a836dceb5f929960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:52 +0000

    Add package Flask-Dogpile-Cache/0.1/Flask-Dogpile-Cache-0.1.tar.gz

---
+++ packages/Flask-Dogpile-Cache/Flask-Dogpile-Cache-0.1.tar.gz/Flask-Dogpile-Cache-0.1/setup.py
@@ -0,0 +1,34 @@
+import os
+import re
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README')).read()
+CHANGES = open(os.path.join(here, 'CHANGES')).read()
+
+with open(os.path.join(here, 'flask_dogpile_cache.py')) as main_file:
+    pattern = re.compile(r".*__version__ = '(.*?)'", re.S)
+    VERSION = pattern.match(main_file.read()).group(1)
+
+
+setup(name='Flask-Dogpile-Cache',
+      version=VERSION,
+      description="Adds dogpile.cache support to your Flask application",
+      long_description=README,
+      keywords='caching',
+      author='Vitalii Ponomar',
+      author_email='vitalii.ponomar@gmail.com',
+      url='http://bitbucket.org/ponomar/flask-dogpile-cache',
+      license='BSD',
+      zip_safe=False,
+      platforms='any',
+      install_requires=['Flask',
+                        'dogpile.cache>=0.5.2'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python'],
+)
