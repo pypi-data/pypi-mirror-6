ddb9d0710bfaeb2b3b1ca8fd6514920e8b87e443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:08 +0000

    Add package infi.memuse/0.0.4/infi.memuse-0.0.4.tar.gz

---
+++ packages/infi.memuse/infi.memuse-0.0.4.tar.gz/infi.memuse-0.0.4/src/infi/memuse/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.4"
+__git_commiter_name__ = "Itai Shirav"
+__git_commiter_email__ = "itais@infinidat.com"
+__git_branch__ = '(Not currently on any branch)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '8b1a8c610fc53ad1029225aaa23dc3495f8c3a88'
+__git_head_subject__ = 'HOSTDEV-1278 infi.memuse fails to install with pip due to old == requirement for psutil'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
