0d09d4894bee87f1cd686ff18edaefd2a16d9ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package kaboom/0.3.6/kaboom-0.3.6.tar.gz

---
+++ packages/kaboom/kaboom-0.3.6.tar.gz/kaboom-0.3.6/kaboom/constants.py
@@ -0,0 +1,28 @@
+STORE_NAME = '.kaboom.json'
+HELP = '''
+Usage ---  kaboom [-h] action [...]
+  kaboom: help -------------------------------------------------------
+
+  kaboom                        this help text
+  kaboom help                   this help text
+
+  kaboom add <name> <value>     Add name:value to kaboom
+
+  kaboom show <name>            display value of name
+  kaboom show all               display everything stored in kaboom
+
+  kaboom delete <name>          Remove key <name>
+  kaboom delete all             Remove all keys
+
+  kaboom update <name> <value>  Updates name's value to supplied value
+
+  all other documentation is located at:
+    https://github.com/vinu76jsr/kaboom
+  Inspired by https://github.com/holman/boom'''
+
+ERROR_COLOR = 'red'
+SUCCESS_COLOR = 'green'
+KEY_COLOR = 'yellow'
+VALUE_COLOR = 'magenta'
+SEPARATOR_COLOR = 'blue'
+INFO_COLOR = 'blue'
