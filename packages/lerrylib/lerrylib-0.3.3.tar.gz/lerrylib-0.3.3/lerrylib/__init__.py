c1da0c6e0b0895817a4d1eff2dc1b5ac78f7b82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package lerrylib/0.3.3/lerrylib-0.3.3.tar.gz

---
+++ packages/lerrylib/lerrylib-0.3.3.tar.gz/lerrylib-0.3.3/lerrylib/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+from lerry_signal import SIGNAL
+
+if __name__ == '__main__':
+    import sys
+    if sys.getdefaultencoding() == 'ascii':
+        reload(sys)
+        sys.setdefaultencoding('utf-8')
+
+
+
+
+
