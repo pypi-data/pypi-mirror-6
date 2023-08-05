42a78a1f9ee467b4fa43779e906d7e8aeb9e653b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:52 +0000

    Add package tmuxp/0.1.1/tmuxp-0.1.1.tar.gz

---
+++ packages/tmuxp/tmuxp-0.1.1.tar.gz/tmuxp-0.1.1/tmuxp/exc.py
@@ -0,0 +1,43 @@
+# -*- coding: utf8 - *-
+"""Exceptions for tmuxp.
+
+tmuxp.exc
+~~~~~~~~~
+:copyright: Copyright 2013 Tony Narlock.
+:license: BSD, see LICENSE for details
+
+"""
+
+from __future__ import absolute_import, division, print_function, \
+    with_statement, unicode_literals
+
+
+class TmuxpException(Exception):
+
+    """Base Exception for Tmuxp Errors.
+
+    Also for Python 2.6 compat:
+        http://stackoverflow.com/a/6029838
+
+    """
+
+
+class TmuxSessionExists(TmuxpException):
+
+    """Session does not exist in the server."""
+
+    pass
+
+
+class ConfigError(TmuxpException):
+
+    """Error parsing tmuxp configuration dict."""
+
+    pass
+
+
+class EmptyConfigException(ConfigError):
+
+    """Configuration is empty."""
+
+    pass
