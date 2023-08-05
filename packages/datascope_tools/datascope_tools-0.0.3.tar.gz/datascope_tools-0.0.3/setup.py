fc6b1f625d78937e715613fe6d6da0b969b9a862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:03 +0000

    Add package datascope_tools/0.0.3/datascope_tools-0.0.3.tar.gz

---
+++ packages/datascope_tools/datascope_tools-0.0.3.tar.gz/datascope_tools-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import glob
+import os
+from setuptools import setup
+
+import datascope_tools
+
+# get all of the scripts
+scripts = glob.glob("bin/*")
+
+# read in the description from README
+with open("README.md") as stream:
+    long_description = stream.read()
+
+github_url='https://github.com/datascopeanalytics/datascope-tools'
+
+setup(
+    name=datascope_tools.__name__,
+    version=datascope_tools.VERSION,
+    description="import random.scripts, random.tools, random.ideas",
+    long_description=long_description,
+    url=github_url,
+    download_url="%s/archives/master" % github_url,
+    author='Datascope Analytics',
+    author_email='github.admin@datascopeanalytics.com',
+    license='MIT',
+    scripts=scripts,
+    packages=[datascope_tools.__name__],
+    zip_safe=False,
+    include_pacakge_data=True,
+)
