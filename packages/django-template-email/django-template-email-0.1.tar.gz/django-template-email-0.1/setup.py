86b752dda9bb84630ff0a3a085330554326561aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package django-template-email/0.1/django-template-email-0.1.tar.gz

---
+++ packages/django-template-email/django-template-email-0.1.tar.gz/django-template-email-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+dir = os.path.dirname(__file__)
+readme = os.path.join(dir, 'README.rst')
+
+setup(
+    name='django-template-email',
+    version='0.1',
+    author='Ben Davis',
+    author_email='code@bendavismedia',
+    url='http://github.com/bendavis78/django-template-email',
+    description=('A useful tool for building email messages using django '
+                 'templates'),
+    keywords='savid',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=open(readme).read()
+)
