c4981376a8de37736c1e8762b46c7a85c7a68059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:08 +0000

    Add package flylog/0.1.11/flylog-0.1.11.tar.gz

---
+++ packages/flylog/flylog-0.1.11.tar.gz/flylog-0.1.11/flylog/handler.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import json
+from logging.handlers import DatagramHandler
+
+from . import constants
+
+
+class FlyLogHandler(DatagramHandler):
+    source = None
+
+    def __init__(self, host=None, port=None, source=None):
+        DatagramHandler.__init__(self,
+                                 host or constants.FLY_LOG_AGENT_HOST,
+                                 port or constants.FLY_LOG_AGENT_PORT)
+        self.source = source
+
+    def emit(self, record):
+        content = self.format(record)
+        try:
+            s = json.dumps(dict(source=self.source, content=content))
+            self.send(s)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
