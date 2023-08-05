ce34fe4575901d9fe55076786c43cd91bf39f96a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:26 +0000

    Add package marshmallow/0.4.0/marshmallow-0.4.0.tar.gz

---
+++ packages/marshmallow/marshmallow-0.4.0.tar.gz/marshmallow-0.4.0/marshmallow/base.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+'''Abstract base classes.
+
+These are necessary to avoid circular imports between core.py and fields.py.
+'''
+
+
+class FieldABC(object):
+    '''Abstract base class from which all Field classes inherit.
+    '''
+    parent = None
+
+    def format(self, value):
+        raise NotImplementedError
+
+    def output(self, key, obj):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return "<{0} Field>".format(self.__class__.__name__)
+
+    __str__ = __repr__
+
+
+class SerializerABC(object):
+    '''Abstract base class from which all Serializers inherit.'''
+
+    @property
+    def errors(self):
+        raise NotImplementedError
+
+    def is_valid(self, fields=None):
+        raise NotImplementedError
