bd5b1dd1dce62c7202e75a05da9529b1a992c91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:41 +0000

    Add package Stockex/0.1/Stockex-0.1.tar.gz

---
+++ packages/Stockex/Stockex-0.1.tar.gz/Stockex-0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import codecs
+try:
+    codecs.lookup('mbcs')
+except LookupError:
+    ascii = codecs.lookup('ascii')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
+    codecs.register(func) 
+
+
+with open('README.txt') as file:
+        long_description = file.read()
+
+setup(
+    name='Stockex',
+    version='0.1',
+    description='Python 3 wrapper for Yahoo! Finance API',
+    author='Cttn',
+    author_email='cj.cttn@gmail.com',
+    url='https://github.com/cttn/Stockex',
+    packages=['stockex',],
+    license='Public Domain'
+    )
---
+++ packages/Stockex/Stockex-0.1.tar.gz/Stockex-0.1/stockex/test2.py
@@ -0,0 +1,22 @@
+from stockwrapper import YahooData
+import pylab
+
+data = YahooData()
+
+fran_hist = data.get_historical('FRAN.BA')
+
+dates = [item['Date'] for item in fran_hist]
+close = [item['Close'] for item in fran_hist]
+
+ndates = pylab.datestr2num(dates)
+
+pylab.plot_date(ndates, close)
+pylab.title("FRAN.BA" + " historical prices")
+pylab.ylabel("Price")
+pylab.grid(True)
+pylab.show()
+
+
+
+
+
