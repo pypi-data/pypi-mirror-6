a0527bfc5079e58253e131ae79949074e37b7a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:39 +0000

    Add package zk-shell/0.9.11/zk_shell-0.9.11.tar.gz

---
+++ packages/zk-shell/zk_shell-0.9.11.tar.gz/zk_shell-0.9.11/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='zk_shell',
+      version='0.9.11',
+      description='A Python - Kazoo based - shell for ZooKeeper',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Networking",
+      ],
+      keywords='ZooKeeper Kazoo shell',
+      url='https://github.com/rgs1/zk_shell',
+      author='Raul Gutierrez Segales',
+      author_email='rgs@itevenworks.net',
+      license='Apache',
+      packages=['zk_shell'],
+      scripts=['bin/zk-shell'],
+      install_requires=[
+          'kazoo',
+      ],
+      include_package_data=True,
+      zip_safe=False)
