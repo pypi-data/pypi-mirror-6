58875a9cf4908f858279764ec090b319db04d3b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package Bedframe/0.13.4/Bedframe-0.13.4-py27-none-any.whl

---
+++ packages/Bedframe/Bedframe-0.13.4-py27-none-any.whl/bedframe/auth/ldap/__init__.py
@@ -0,0 +1,14 @@
+"""Lightweight Directory Access Protocol (LDAP) authentication
+
+These objects provide support for authentication using an LDAP backend.
+
+.. seealso::
+    :rfc:`Lightweight Directory Access Protocol (LDAP): Technical
+          Specification Road Map <4510>`
+
+"""
+
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
+__docformat__ = "restructuredtext"
+
+from ._connectors import *
