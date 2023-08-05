73681de7a83db12475fa7e4f947f2abcab595787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:15 +0000

    Add package lineage.registry/1.1/lineage.registry-1.1.tar.gz

---
+++ packages/lineage.registry/lineage.registry-1.1.tar.gz/lineage.registry-1.1/HISTORY.rst
@@ -0,0 +1,22 @@
+
+History
+=======
+
+1.1 (2014-01-30)
+----------------
+
+- Fix bug when disabling a lineage site.
+  [thet]
+
+
+1.0.1
+-----
+
+- wrong information in README.rst corrected. 
+  [jensens]
+
+
+1.0
+---
+
+- make it work [jensens, 2012-01-25]
---
+++ packages/lineage.registry/lineage.registry-1.1.tar.gz/lineage.registry-1.1/src/lineage/registry/subscribers.py
@@ -0,0 +1,32 @@
+from zope.component import (
+    adapter,
+    getSiteManager,
+)
+from plone.registry.interfaces import IRegistry
+from collective.lineage.interfaces import (
+    IChildSiteCreatedEvent,
+    IChildSiteRemovedEvent,
+)
+from .proxy import (
+    REGISTRY_NAME,
+    LineageRegistry,
+)
+
+
+@adapter(IChildSiteCreatedEvent)
+def enableChildRegistry(event):
+    child = event.object
+    sm = getSiteManager(context=child)
+    if REGISTRY_NAME not in child.objectIds():
+        child[REGISTRY_NAME] = LineageRegistry(REGISTRY_NAME).__of__(child)
+    sm.registerUtility(component=child[REGISTRY_NAME], provided=IRegistry)
+
+
+@adapter(IChildSiteRemovedEvent)
+def disableChildRegistry(event):
+    child = event.object
+    if REGISTRY_NAME not in child.objectIds():
+        return
+    # we keep the registry here (intentionally)
+    sm = getSiteManager(context=child)
+    sm.unregisterUtility(component=child[REGISTRY_NAME], provided=IRegistry)
