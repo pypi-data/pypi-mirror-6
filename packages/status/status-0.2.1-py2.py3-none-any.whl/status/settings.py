70a3969918b637509f6b9a2ed631fffe86b36425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package status/0.2.1/status-0.2.1-py2.py3-none-any.whl

---
+++ packages/status/status-0.2.1-py2.py3-none-any.whl/status/settings.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+#------------------------------------------------------------------------------
+# Application Name
+#------------------------------------------------------------------------------
+app_name = 'status'
+
+#------------------------------------------------------------------------------
+# Version Number
+#------------------------------------------------------------------------------
+major_version = "0"
+minor_version = "2"
+patch_version = "1"
+
+#------------------------------------------------------------------------------
+# Debug Flag (switch to False for production release code)
+#------------------------------------------------------------------------------
+debug = False
+
+#------------------------------------------------------------------------------
+# Usage String
+#------------------------------------------------------------------------------
+usage = """
+Usage: status [option(s)] <url>
+"""
+
+#------------------------------------------------------------------------------
+# Help String
+#------------------------------------------------------------------------------
+help = """
+---------------------------------------
+ status
+ Copyright 2014 Christopher Simpkins
+ MIT license
+---------------------------------------
+
+Status reports the HTTP response status code(s) for GET and POST requests.
+
+USAGE
+  status [-p --post] <url>
+
+OPTIONS
+  -p  --post    POST request
+
+The URL can be entered with or without the protocol. If you do not enter a protocol (http:// or https://), then http:// is assumed.  If you intend to test with the secure HTTP protocol, then make this explicit in your URL.
+
+EXAMPLES
+  status http://google.com
+  status google.com
+  status -p httpbin.org/post
+
+SOURCE REPOSITORY
+  https://github.com/chrissimpkins/status
+
+ISSUE TRACKING
+  https://github.com/chrissimpkins/status/issues
+  """
+
