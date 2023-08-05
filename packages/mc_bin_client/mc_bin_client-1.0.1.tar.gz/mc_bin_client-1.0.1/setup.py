e7abb4428631cf9d0a15e00d2c84a6effdf9674b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:56 +0000

    Add package mc_bin_client/1.0.1/mc_bin_client-1.0.1.tar.gz

---
+++ packages/mc_bin_client/mc_bin_client-1.0.1.tar.gz/mc_bin_client-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mc_bin_client',
+    version='1.0.1',
+    description='Binary memcached client',
+    author='Dustin Sallings',
+    author_email='dustin@spy.net',
+    packages=['mc_bin_client'],
+)
