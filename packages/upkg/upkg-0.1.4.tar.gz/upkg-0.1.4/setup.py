b63707d92bd80f0b8d44f6bc94bdc245871231ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package upkg/0.1.4/upkg-0.1.4.tar.gz

---
+++ packages/upkg/upkg-0.1.4.tar.gz/upkg-0.1.4/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+
+setup(name='upkg',
+      version='0.1.4',
+      description="Package Yourself",
+      url='https://github.com/jeffbuttars/upkg',
+      long_description=(open('README.md').read()),
+      author="Jeff Buttars",
+      author_email="jeff@jeffbuttars.com",
+      packages=find_packages(exclude=['tests*']),
+      license='GPLv2',
+      package_dir={'pkgs': 'pkgs'},
+      install_requires=[
+          'tornado',
+      ],
+      data_files=[
+          # ('/etc/init.d', ['']),
+      ],
+      )
