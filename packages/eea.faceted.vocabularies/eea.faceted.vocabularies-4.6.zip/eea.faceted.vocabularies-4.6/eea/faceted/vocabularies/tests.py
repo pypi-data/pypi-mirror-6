1971598f582dfca360827070e5382c0fc7dc9573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:38 +0000

    Add package eea.faceted.vocabularies/4.6/eea.faceted.vocabularies-4.6.zip

---
+++ packages/eea.faceted.vocabularies/eea.faceted.vocabularies-4.6.zip/eea.faceted.vocabularies-4.6/eea/faceted/vocabularies/tests.py
@@ -0,0 +1,33 @@
+""" Tests
+"""
+import doctest
+import unittest
+
+import zope.component
+import eea.faceted.vocabularies
+
+from zope.component.testing import setUp, tearDown
+from zope.configuration.xmlconfig import XMLConfig
+
+
+def configurationSetUp(self):
+    """ Setup
+    """
+    setUp()
+    XMLConfig('meta.zcml', zope.component)()
+    XMLConfig('configure.zcml', eea.faceted.vocabularies)()
+
+def test_suite():
+    """ Suite
+    """
+    flags = (doctest.ELLIPSIS |
+             doctest.NORMALIZE_WHITESPACE |
+             doctest.REPORT_ONLY_FIRST_FAILURE)
+
+    return unittest.TestSuite((
+        doctest.DocFileSuite('README.txt',
+                     package='eea.faceted.vocabularies',
+                     setUp=configurationSetUp,
+                     tearDown=tearDown,
+                     optionflags=flags),
+        ))
