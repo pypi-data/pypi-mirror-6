26da4cb056fefa7ec02f1c876270d82459e811b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package pg_37/1.1.0/pg_37-1.1.0.tar.gz

---
+++ packages/pg_37/pg_37-1.1.0.tar.gz/pg_37-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'pg_37',
+    version         = '1.1.0',
+    py_modules      = ['pg_37'],
+    author          = 'rafaelmcouto',
+    author_mail     = 'rafaelmcouto@gmail.com',
+    url             = '',
+    description     = 'A simple printer of nester lists'
+)
