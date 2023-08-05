fcd3811c652dbe6eb4a2a8f8de65647bea169776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:53 +0000

    Add package datastream/0.2.4/datastream-0.2.4.tar.gz

---
+++ packages/datastream/datastream-0.2.4.tar.gz/datastream-0.2.4/datastream/exceptions.py
@@ -0,0 +1,62 @@
+class DatastreamException(Exception):
+    pass
+
+
+class StreamNotFound(DatastreamException):
+    pass
+
+
+class MultipleStreamsReturned(DatastreamException):
+    pass
+
+
+class InconsistentStreamConfiguration(DatastreamException):
+    pass
+
+
+class OutstandingDependenciesError(DatastreamException):
+    pass
+
+
+class UnsupportedDownsampler(DatastreamException, ValueError):
+    pass
+
+
+class UnsupportedGranularity(DatastreamException, ValueError):
+    pass
+
+
+class UnsupportedDeriveOperator(DatastreamException, ValueError):
+    pass
+
+
+class ReservedTagNameError(DatastreamException, ValueError):
+    pass
+
+
+class InvalidTimestamp(DatastreamException, ValueError):
+    pass
+
+
+class IncompatibleGranularities(DatastreamException, ValueError):
+    pass
+
+
+class AppendToDerivedStreamNotAllowed(DatastreamException, ValueError):
+    pass
+
+
+class InvalidOperatorArguments(DatastreamException, ValueError):
+    pass
+
+
+class DatastreamWarning(RuntimeWarning):
+    pass
+
+
+class InvalidValueWarning(DatastreamWarning):
+    pass
+
+
+class InternalInconsistencyWarning(DatastreamWarning):
+    pass
