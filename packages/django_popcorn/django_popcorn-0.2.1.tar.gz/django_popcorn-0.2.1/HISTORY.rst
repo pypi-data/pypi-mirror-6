35f810909069cfa2e8af4f698d400109e4f9c09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:22 +0000

    Add package django_popcorn/0.2.1/django_popcorn-0.2.1.tar.gz

---
+++ packages/django_popcorn/django_popcorn-0.2.1.tar.gz/django_popcorn-0.2.1/HISTORY.rst
@@ -0,0 +1,14 @@
+.. :changelog:
+
+History
+-------
+
+0.1.0 (2013-12-2)
+++++++++++++++++++
+
+* First release on PyPI.
+
+0.2.0 (2013-12-9)
+++++++++++++++++++
+
+* Added support for permissions. 
---
+++ packages/django_popcorn/django_popcorn-0.2.1.tar.gz/django_popcorn-0.2.1/popcorn/templatetags/popcorn.py
@@ -0,0 +1,17 @@
+from django import template
+from django.template.loader import get_template
+
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def popcorn(context, field):
+    model = field.field.queryset.model
+    model_name = model._meta.object_name.lower()
+    app_name = model._meta.app_label.lower()
+    view_name = '%s_%s_popcorn' % (app_name, model_name)
+    perm = '%s.add_%s' % (app_name, model_name)
+    context['field_name'] = field.name
+    context['view_name'] = view_name
+    context['has_perm'] = perm in context['perms']
+    return get_template('popcorn/link.html').render(context)
