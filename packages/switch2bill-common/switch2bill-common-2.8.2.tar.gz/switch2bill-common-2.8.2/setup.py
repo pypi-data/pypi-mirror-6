82cc784df208dcec7d7508ca912b16dffe08d4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package switch2bill-common/2.8.2/switch2bill-common-2.8.2.tar.gz

---
+++ packages/switch2bill-common/switch2bill-common-2.8.2.tar.gz/switch2bill-common-2.8.2/setup.py
@@ -0,0 +1,83 @@
+from setuptools import setup
+import os
+import re
+import common
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.rst')
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('common'):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        pkg = dirpath.replace(os.path.sep, '.')
+        if os.path.altsep:
+            pkg = pkg.replace(os.path.altsep, '.')
+        packages.append(pkg)
+    elif filenames:
+        prefix = dirpath[len('common') + 1:]  # Strip "common/"
+        for f in filenames:
+            data_files.append(os.path.join(prefix, f))
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'(\s*git)|(\s*hg)', line):
+            pass
+        else:
+            requirements.append(line)
+    return requirements
+
+
+def parse_dependency_links(file_name):
+    dependency_links = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'\s*-[ef]\s+', line):
+            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+
+    return dependency_links
+
+
+setup(
+    name='switch2bill-common',
+    version=common.__version__,
+    description='Common Django/Python helpers for Star2Billing projects',
+    long_description=README,
+    url='http://github.com/Star2Billing/switch2bill-common',
+    author='Belaid Arezqui',
+    author_email='areski@gmail.com',
+    license='MPL 2.0 License',
+    packages=packages,
+    package_dir={'common': 'common'},
+    package_data={'common': data_files},
+    entry_points={'django.apps': 'common = common'},
+    install_requires=parse_requirements('requirements.txt'),
+    dependency_links=parse_dependency_links('requirements.txt'),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
