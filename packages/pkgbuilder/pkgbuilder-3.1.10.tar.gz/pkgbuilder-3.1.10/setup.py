85327b992cd8c0b20d0509051df23530a98ce6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:52 +0000

    Add package pkgbuilder/3.1.10/pkgbuilder-3.1.10.tar.gz

---
+++ packages/pkgbuilder/pkgbuilder-3.1.10.tar.gz/pkgbuilder-3.1.10/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='pkgbuilder',
+      version='3.1.10',
+      description='An AUR helper (and library) in Python 3.',
+      keywords='arch pkgbuild',
+      author='Chris “Kwpolska” Warrick',
+      author_email='kwpolska@kwpolska.tk',
+      url='https://github.com/Kwpolska/pkgbuilder',
+      license='3-clause BSD',
+      long_description=open('./docs/README.rst').read(),
+      platforms='Arch Linux',
+      zip_safe=False,
+      test_suite='tests',
+      classifiers=['Development Status :: 6 - Mature',
+                   'Environment :: Console',
+                   'Intended Audience :: End Users/Desktop',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.1',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Topic :: System',
+                   'Topic :: System :: Archiving :: Packaging',
+                   'Topic :: Utilities'],
+      packages=['pkgbuilder'],
+      requires=['pyalpm', 'requests'],
+      scripts=['bin/pkgbuilder', 'bin/pb'],
+      data_files=[('share/man/man8', ['docs/pkgbuilder.8.gz']),
+                  ('share/man/man8', ['docs/pb.8.gz']),
+                  ('share/locale/pl/LC_MESSAGES', ['locale/pl/LC_MESSAGES/'
+                                                   'pkgbuilder.mo']),
+                  ('share/locale/ja/LC_MESSAGES', ['locale/ja/LC_MESSAGES/'
+                                                   'pkgbuilder.mo'])])
