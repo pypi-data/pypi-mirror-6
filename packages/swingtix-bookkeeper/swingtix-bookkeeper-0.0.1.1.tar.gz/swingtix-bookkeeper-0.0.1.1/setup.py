5cac5073ad5558627b25f58a646c80678e2ce542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:45 +0000

    Add package swingtix-bookkeeper/0.0.1.1/swingtix-bookkeeper-0.0.1.1.tar.gz

---
+++ packages/swingtix-bookkeeper/swingtix-bookkeeper-0.0.1.1.tar.gz/swingtix-bookkeeper-0.0.1.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='swingtix-bookkeeper',
+    version='0.0.1.1',
+    author=u'Geoff Oakham',
+    author_email=u'geoff.oakham@swingtix.ca',
+    packages=find_packages(),
+    url='https://github.com/swingtix/bookkeeper',
+    license='GNU Affero General Public License, version 3, see LICENSE',
+    description='A double-entry bookkeeping module for django',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    include_package_data=True,
+    tests_require=[
+        'django>=1.5,<1.7',
+    ],
+   classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business :: Financial :: Accounting',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
+
+
+
