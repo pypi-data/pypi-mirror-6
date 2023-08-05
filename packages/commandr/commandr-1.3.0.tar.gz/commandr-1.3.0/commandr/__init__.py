c55264eb10be90f6653e3ffcb5ea18a370d7e836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:52 +0000

    Add package commandr/1.3.0/commandr-1.3.0.tar.gz

---
+++ packages/commandr/commandr-1.3.0.tar.gz/commandr-1.3.0/commandr/__init__.py
@@ -0,0 +1,43 @@
+# Copyright 2013 TellApart, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__all__ = [
+    'command',
+    'Run',
+    'SetOptions',
+    'Usage',
+    'update_wrapper',
+    'wraps',
+    'MonkeyPatchFunctools',
+    'CommandrError',
+    'CommandrUsageError',
+    'CommandrDuplicateMainError']
+
+# Export the global Commandr object methods.
+from commandr import (
+  Commandr,
+  CommandrError,
+  CommandrUsageError,
+  CommandrDuplicateMainError)
+
+_COMMANDR = Commandr()
+
+command = _COMMANDR.command
+Run = _COMMANDR.Run
+RunFunction = _COMMANDR.RunFunction
+SetOptions = _COMMANDR.SetOptions
+Usage = _COMMANDR.Usage
+
+# Export the decorator utils.
+from functools_util import update_wrapper, wraps, MonkeyPatchFunctools
