70ecd463dd9c2bbf54407219eea017695fe727d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:58 +0000

    Add package django-modelcluster/0.1/django-modelcluster-0.1.tar.gz

---
+++ packages/django-modelcluster/django-modelcluster-0.1.tar.gz/django-modelcluster-0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-modelcluster',
+    version='0.1',
+    description="Django extension to allow working with 'clusters' of models as a single unit, independently of the database",
+    author='Matthew Westcott',
+    author_email='matthew.westcott@torchbox.com',
+    url='https://github.com/torchbox/django-modelcluster',
+    packages=['modelcluster', 'tests'],
+    license='BSD',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+    ],
+)
