16969a53474f13e921549a3589511dc0466af49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:49 +0000

    Add package django-flatpages-i18n/0.2.1/django-flatpages-i18n-0.2.1.tar.gz

---
+++ packages/django-flatpages-i18n/django-flatpages-i18n-0.2.1.tar.gz/django-flatpages-i18n-0.2.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='django-flatpages-i18n',
+    version='0.2.1',
+    description='Translatable flatpages',
+    author='Pragmatic Mates',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/django-flatpages-i18n',
+    packages=[
+        'flatpages_i18n',
+        'flatpages_i18n.templatetags',
+        'flatpages_i18n.migrations'
+    ],
+    include_package_data=True,
+    install_requires=('South', 'django_modeltranslation', 'django', 'django_mptt'),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License'],
+    license='BSD License')
