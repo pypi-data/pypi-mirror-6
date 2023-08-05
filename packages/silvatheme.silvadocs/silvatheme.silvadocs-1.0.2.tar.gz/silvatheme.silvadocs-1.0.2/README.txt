63749a7dd58e8b33994c0c309231ce7680710111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:42 +0000

    Add package silvatheme.silvadocs/1.0.2/silvatheme.silvadocs-1.0.2.tar.gz

---
+++ packages/silvatheme.silvadocs/silvatheme.silvadocs-1.0.2.tar.gz/silvatheme.silvadocs-1.0.2/README.txt
@@ -0,0 +1,17 @@
+====================
+silvatheme.silvadocs
+====================
+
+Silva Docs is a visual design (theme) for `Silva`_ 3 and higher. It is
+used by default on the `Silva User Documentation`_.
+
+
+Code repository
+===============
+
+The code for this extension can be found in Git:
+https://github.com/silvacms/silvatheme.silvadocs
+
+
+.. _Silva: http://silvacms.org
+.. _Silva User Documentation: http://silvacms.org/docs/user/
---
+++ packages/silvatheme.silvadocs/silvatheme.silvadocs-1.0.2.tar.gz/silvatheme.silvadocs-1.0.2/silvatheme/silvadocs/silvadocs.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012  Infrae. All rights reserved.
+# See also LICENSE.txt
+
+from zope.traversing.browser import absoluteURL
+from zope.cachedescriptors.property import Lazy
+from silva.core.layout.interfaces import ISilvaSkin
+from silva.core import conf as silvaconf
+from silva.core.layout.porto import porto
+from silva.core.layout.porto.interfaces import IPorto
+from silva.core.interfaces import IPublication, ISilvaObject
+
+
+class ISilvaDocs(IPorto):
+    """Layer for silva docs theme
+    """
+    silvaconf.resource('html5reset.css')
+    silvaconf.resource('modernizr-1.7.min.js')
+    silvaconf.resource('typography.css')
+    silvaconf.resource('default.css')
+
+
+class ISilvaDocsSkin(ISilvaDocs, ISilvaSkin):
+    """Skin for silva docs theme
+    """
+    silvaconf.skin('Silva Docs')
+
+
+silvaconf.layer(ISilvaDocs)
+
+
+class Navigation(porto.Navigation):
+    max_depth = 1
+
+
+class Layout(porto.Layout):
+
+    @Lazy
+    def publication_title(self):
+        return self.context.get_publication().get_title()
+
+    @Lazy
+    def publication_url(self):
+        return self.context.get_publication().absolute_url()
+
+    @Lazy
+    def search_url(self):
+        search = getattr(self.context, 'search', None)
+        if ISilvaObject.providedBy(search):
+            # get_silva_object cleanup the Acquisition path
+            return absoluteURL(search.get_silva_object(), self.request)
+        return None
+
+
+class Favicon(porto.Favicon):
+    """Declare that we have a favicon for this layer.
+
+    Default favicon url is static/favicon.ico so we don't need to
+    do anything besides declaring the Favicon viewlet inside this
+    layer and have a static/favicon.ico file.
+    If you want it to be something different, define a favicon_url property
+    or method for this class.
+    """
+    pass
+
+
