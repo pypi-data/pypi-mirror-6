7546e7a1807bb479573c8754961d7a43cb8f0ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:24 +0000

    Add package staticjinja/0.2.0/staticjinja-0.2.0.tar.gz

---
+++ packages/staticjinja/staticjinja-0.2.0.tar.gz/staticjinja-0.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+__version_info__ = ('0', '2', '0')
+__version__ = '.'.join(__version_info__)
+
+setup(
+    name="staticjinja",
+    version=__version__,
+    description="jinja based static site generator",
+    author="Ceasar Bautista",
+    author_email="cbautista2010@gmail.com",
+    url="https://github.com/Ceasar/staticjinja",
+    keywords=["jinja", "static", "website"],
+    packages=["staticjinja"],
+    install_requires=["easywatch", "jinja2"],
+    classifiers=[
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.5",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    ],
+    use_2to3=True,
+)
