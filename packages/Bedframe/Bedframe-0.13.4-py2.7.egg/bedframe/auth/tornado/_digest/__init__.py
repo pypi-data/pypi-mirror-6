d3e98f3354f4979a14f60db0ffce3d98aad32161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package Bedframe/0.13.4/Bedframe-0.13.4-py2.7.egg

---
+++ packages/Bedframe/Bedframe-0.13.4-py2.7.egg/bedframe/auth/tornado/_digest/__init__.py
@@ -0,0 +1,11 @@
+"""HTTP Digest Tornado authentication
+
+.. seealso::
+    :mod:`HTTP Digest authentication <bedframe.auth.http._digest>`
+
+"""
+
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
+__docformat__ = "restructuredtext"
+
+from ._connectors import *
---
+++ packages/Bedframe/Bedframe-0.13.4-py2.7.egg/bedframe/webtypes/_python/__init__.py
@@ -0,0 +1,7 @@
+"""Web-transmittable standard Python types"""
+
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
+__docformat__ = "restructuredtext"
+
+from ._builtin import *
+from ._datetime import *
