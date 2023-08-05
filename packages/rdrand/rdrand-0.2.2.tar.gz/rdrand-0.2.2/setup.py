5a4e1a771cf3657c2016c5c5d9af7ad052c70a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:05 +0000

    Add package rdrand/0.2.2/rdrand-0.2.2.tar.gz

---
+++ packages/rdrand/rdrand-0.2.2.tar.gz/rdrand-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup, Extension
+
+setup(name='rdrand',
+      version='0.2.2',
+      description="Python interface to Intel hardware rng",
+      long_description= \
+      """python random.Random() subclass which uses a c
+         extension to use the Intel processor extension
+         to access a real random number generator.
+      """,
+      author="Chris Stillson",
+      author_email="stillson@gmail.com",
+      url='https://github.com/stillson/rdrand',
+      license="New BSD license",
+      ext_modules=[Extension('_rdrand', ['rdrand.c'])],
+      py_modules = ['rdrand',],
+)
