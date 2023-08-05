61b4bde089990ae0966321f2c18ef120bc798583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package infi.asi/0.3.17/infi.asi-0.3.17-py2.7.egg

---
+++ packages/infi.asi/infi.asi-0.3.17-py2.7.egg/infi/asi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.17"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.3.17)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '667af1b4f5a43714cdfb17dea2ca3da21cd91e0d'
+__git_head_subject__ = 'Finished release v0.3.17.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
