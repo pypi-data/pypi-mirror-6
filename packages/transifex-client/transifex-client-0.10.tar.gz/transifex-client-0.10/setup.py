0bbb8fbead5b47cede667497272a80135e466e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package transifex-client/0.10/transifex-client-0.10.tar.gz

---
+++ packages/transifex-client/transifex-client-0.10.tar.gz/transifex-client-0.10/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from codecs import BOM
+
+from setuptools import setup
+
+from txclib import get_version
+
+readme_file = open(u'README.rst')
+long_description = readme_file.read()
+readme_file.close()
+if long_description.startswith(BOM):
+    long_description = long_description.lstrip(BOM)
+long_description = long_description.decode('utf-8')
+
+package_data = {
+    '': ['LICENSE', 'README.rst'],
+}
+
+scripts = ['tx']
+
+install_requires = []
+extra_args = {}
+import platform
+if platform.system() == 'Windows':
+    import py2exe
+
+    extra_args = {
+        'console': ['tx'],
+        'options': {'py2exe': {'bundle_files': 1}},
+        'zipfile': None,
+    }
+
+setup(
+    name="transifex-client",
+    version=get_version(),
+    scripts=scripts,
+    description="A command line interface for Transifex",
+    long_description=long_description,
+    author="Transifex",
+    author_email="admin@transifex.com",
+    url="https://www.transifex.com",
+    license="GPLv2",
+    dependency_links=[
+    ],
+    setup_requires=[
+    ],
+    install_requires=install_requires,
+    tests_require=["mock", ],
+    data_files=[
+    ],
+    test_suite="tests",
+    zip_safe=False,
+    packages=[
+        'txclib', 'txclib.packages', 'txclib.packages.urllib3',
+        'txclib.packages.urllib3.contrib',
+        'txclib.packages.urllib3.packages',
+        'txclib.packages.urllib3.packages.ssl_match_hostname',
+    ],
+    include_package_data=True,
+    package_data=package_data,
+    keywords=('translation', 'localization', 'internationalization',),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    **extra_args
+)
