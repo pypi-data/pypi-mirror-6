85f9048162f13a95ae1c886346ffc9bf8908f852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:33 +0000

    Add package pylut/1.4.3/pylut-1.4.3.tar.gz

---
+++ packages/pylut/pylut-1.4.3.tar.gz/pylut-1.4.3/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='pylut',
+    version='1.4.3',
+    author='Greg Cotten',
+    author_email='gcgc90@gmail.com',
+    packages=['pylut'],
+    scripts=['bin/pylut'],
+    url='http://pypi.python.org/pypi/PyLUT/',
+    license='LICENSE.txt',
+    description='Builds, modifies, visualizes, and converts 3D LUTs from popular .cube and .3dl formats.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "docopt"
+    ],
+)
