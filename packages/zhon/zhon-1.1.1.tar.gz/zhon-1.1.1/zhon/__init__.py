e83a060c9333127500163328d7483d059507a2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:17 +0000

    Add package zhon/1.1.1/zhon-1.1.1.tar.gz

---
+++ packages/zhon/zhon-1.1.1.tar.gz/zhon-1.1.1/zhon/__init__.py
@@ -0,0 +1,3 @@
+"""Provides constants used in Chinese text processing."""
+
+__version__ = '0.2.1'
---
+++ packages/zhon/zhon-1.1.1.tar.gz/zhon-1.1.1/zhon/cedict/__init__.py
@@ -0,0 +1,13 @@
+"""Provides CC-CEDICT character constants."""
+
+from __future__ import unicode_literals
+
+from . import simplified
+from . import traditional
+from . import all
+
+#: A string containing all Simplified characters according to CC-CEDICT.
+simp = simplified = simplified.CHARACTERS
+
+#: A string containing all Traditional characters according to CC-CEDICT.
+trad = traditional = traditional.CHARACTERS
