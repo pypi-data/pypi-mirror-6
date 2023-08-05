b06690a43848cbe616d6a130efdc4d0d0893f358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:57 +0000

    Add package sblog/1.0.3/sblog-1.0.3.tar.gz

---
+++ packages/sblog/sblog-1.0.3.tar.gz/sblog-1.0.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+install_requires = ['markdown', 'Jinja2', 'pygments']
+
+
+from sblog import __version__ as version
+
+setup(
+    name='sblog',
+    version=version,
+    author='xiaoba',
+    author_email='xiaobayuwei@gmail.com',
+    url='http://blog.xiaoba.me',
+    packages=['sblog', 'sblog.writers', 'sblog.readers'],
+    description='A lightweight static weblog generator',
+    long_description=open('README.mkd').read(),
+    license='BSD License',
+    entry_points={
+        'console_scripts': ['sblog= sblog.main:main'],
+    },
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ]
+)
