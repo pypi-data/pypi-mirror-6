d7b7388cda561a6e39b6d3a4576dfc0540cc765a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:40 +0000

    Add package Gtools/0.1/Gtools-0.1.tar.gz

---
+++ packages/Gtools/Gtools-0.1.tar.gz/Gtools-0.1/README.txt
@@ -0,0 +1,6 @@
+regex.py
+--------
+
+Function: checktags
+This function matches any tags like ##TAGNAME##
+if there any wrong tags it returns false else it returns true
---
+++ packages/Gtools/Gtools-0.1.tar.gz/Gtools-0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Gtools',
+    version='0.1',
+    #packages=['*',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    url="http://www.nbhosting.nl/dist/",
+    author="Gerd Koetje",
+    author_email="support@nbhosting.nl",
+)
