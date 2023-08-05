337a968e496de4ed53cf3416583b0d7b2f0dcbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:55 +0000

    Add package pyword/1.0/pyword-1.0.zip

---
+++ packages/pyword/pyword-1.0.zip/pyword-1.0/myword/__init__.py
@@ -0,0 +1,25 @@
+"""
+What's My Word
+"""
+
+__title__   = "What's My Word?"
+__author__  = "Glenn Hutchings"
+__email__   = "zondo42@gmail.com"
+__desc__    = "A solitaire computer version of the pencil & paper game."
+__url__     = "http://www.boardgamegeek.com/boardgame/4079/whats-my-word"
+__version__ = "1.0"
+__license__ = "GNU Public License v2 or later"
+
+
+__classifiers__ = """
+Development Status :: 5 - Production/Stable
+Environment :: Win32 (MS Windows)
+Environment :: X11 Applications
+Intended Audience :: End Users/Desktop
+License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)
+Natural Language :: English
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX :: Linux
+Programming Language :: Python :: 2.7
+Topic :: Games/Entertainment :: Board Games
+"""
