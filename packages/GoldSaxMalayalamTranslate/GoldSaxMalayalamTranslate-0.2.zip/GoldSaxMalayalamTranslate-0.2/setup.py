d94abbc1add10ac4652664e10e7b8148611bba98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:44 +0000

    Add package GoldSaxMalayalamTranslate/0.2/GoldSaxMalayalamTranslate-0.2.zip

---
+++ packages/GoldSaxMalayalamTranslate/GoldSaxMalayalamTranslate-0.2.zip/GoldSaxMalayalamTranslate-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+        name = 'GoldSaxMalayalamTranslate',
+        packages = ['GoldSaxMalayalamTranslate'], # this must be the same as the name above
+        version = '0.2',
+        description = 'To enable easy translation for python applications to use this package on from English to Malayalam, including a transliteration.',
+        author = 'Vance King Saxbe. A',
+        author_email = 'vsaxbe@yahoo.com',
+        url = '',   # use the URL to the github repo
+        download_url = '', # I'll explain this in a second
+        keywords = ['translation', 'transliteration', 'malayalam', 'english'], # arbitrary keywords
+        classifiers = [],
+    )
