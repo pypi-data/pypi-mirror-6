9bbb551bc715052aca1f5bac3c6ea1f6aa234e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:51 +0000

    Add package pytracremote/0.0.2/pytracremote-0.0.2.tar.gz

---
+++ packages/pytracremote/pytracremote-0.0.2.tar.gz/pytracremote-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytracremote',
+    version='0.0.2',
+    description="Manager for multiple remote trac instances",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='trac, ssh',
+    author='42coffeecups.com',
+    author_email='contact@42cc.co',
+    url='https://github.com/42cc/pytracremote',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
