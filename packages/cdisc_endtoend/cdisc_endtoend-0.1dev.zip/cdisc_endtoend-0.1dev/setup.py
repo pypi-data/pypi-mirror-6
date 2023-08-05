e6f75faa6914546b5c93067b47d8477e76f99c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:52 +0000

    Add package cdisc_endtoend/0.1dev/cdisc_endtoend-0.1dev.zip

---
+++ packages/cdisc_endtoend/cdisc_endtoend-0.1dev.zip/cdisc_endtoend-0.1dev/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup;
+
+
+setup(
+    name='cdisc_endtoend',
+    version='0.1dev',
+    packages=['define',],
+    license='Buyer BeSoftware',
+    long_description="Examples from Cdisc end to end for example, Create Define.xml ",
+    );
+
+
+
