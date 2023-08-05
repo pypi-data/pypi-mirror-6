eb86662531db504afad54d44436c77b301745456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package mzsudoku/0.8/mzsudoku-0.8.zip

---
+++ packages/mzsudoku/mzsudoku-0.8.zip/mzsudoku-0.8/setup.py
@@ -0,0 +1,24 @@
+# coding: utf8
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+
+def make_setup():
+    setup(
+        name = "mzsudoku",
+        version = "0.8",
+        packages = find_packages(),
+        scripts = ['mzsudoku.py'],
+        author = "Zvolsky Mirek",
+        author_email = "mirek.zvolsky@gmail.com",
+        description = "Module/script to solve classical 9x9 Sudoku step by step",
+        license = "open source GPL",
+        keywords = "sudoku",
+        url = "http://www.github.com/zvolsky/sudoku/",   # project home page
+        # could also include long_description, download_url, classifiers, etc.
+    ) 
+
+if __name__ == '__main__':
+    make_setup()
