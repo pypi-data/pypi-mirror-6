f8e25181e795f70429dbcc5bf18610303bfbec0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:09 +0000

    Add package jrnl/1.7.6/jrnl-1.7.6.tar.gz

---
+++ packages/jrnl/jrnl-1.7.6.tar.gz/jrnl-1.7.6/jrnl/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+jrnl is a simple journal application for your command line.
+"""
+
+__title__ = 'jrnl'
+__version__ = '1.7.6'
+__author__ = 'Manuel Ebert'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2013 - 2014 Manuel Ebert'
+
+from . import Journal
+from . import cli
+from .cli import run
