aacb45e588b5be704f3d95d0ad472864a9e7ea5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package django-db-call/0.1.1/django_db_call-0.1.1-py2.py3-none-any.whl

---
+++ packages/django-db-call/django_db_call-0.1.1-py2.py3-none-any.whl/django_db_call/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+django-db-call
+
+Copyright (c) 2014, Friedrich Paetzke (f.paetzke@gmail.com)
+All rights reserved.
+
+"""
+from .caller import from_call, from_calls
+
+__version__ = '0.1.1'
+__author__ = 'Friedrich Paetzke'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2014 Friedrich Paetzke'
