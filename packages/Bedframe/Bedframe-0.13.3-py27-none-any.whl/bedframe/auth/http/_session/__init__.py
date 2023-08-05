cfe0f117a45d41199c98bc37a27a14a99b2bd904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:22 +0000

    Add package Bedframe/0.13.3/Bedframe-0.13.3-py27-none-any.whl

---
+++ packages/Bedframe/Bedframe-0.13.3-py27-none-any.whl/bedframe/auth/http/_session/__init__.py
@@ -0,0 +1,7 @@
+"""Session-based HTTP authentication"""
+
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
+__docformat__ = "restructuredtext"
+
+from ._connectors import *
+from ._managers import *
---
+++ packages/Bedframe/Bedframe-0.13.3-py27-none-any.whl/bedframe/auth/ldap/__init__.py
@@ -0,0 +1,10 @@
+"""LDAP authentication
+
+These objects provide support for authentication using an LDAP backend.
+
+"""
+
+__copyright__ = "Copyright (C) 2014 Ivan D Vasin"
+__docformat__ = "restructuredtext"
+
+from ._connectors import *
