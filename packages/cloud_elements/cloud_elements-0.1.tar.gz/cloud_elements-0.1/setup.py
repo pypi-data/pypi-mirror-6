5d11607823d247155fb0434195e906f4425c7a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:06 +0000

    Add package cloud_elements/0.1/cloud_elements-0.1.tar.gz

---
+++ packages/cloud_elements/cloud_elements-0.1.tar.gz/cloud_elements-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloud_elements',
+    version='0.1',
+    author='Christian Rodriguez',
+    author_email='christian.etpr10@gmail.com',
+    packages=find_packages(),
+    description='Cloud elements api client for Python',
+    long_description='Cloud elements api client for Python',
+    install_requires=[
+        'requests'
+    ],
+    download_url='https://github.com/chrisrodz/cloud-elements-python/tarball/0.1',
+    url='https://github.com/chrisrodz/cloud-elements-python/'
+)
