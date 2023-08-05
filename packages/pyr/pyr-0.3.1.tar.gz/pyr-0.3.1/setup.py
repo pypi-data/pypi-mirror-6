de02c89037cbc2df1912a49b98fd023cf01b7039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package pyr/0.3.1/pyr-0.3.1.tar.gz

---
+++ packages/pyr/pyr-0.3.1.tar.gz/pyr-0.3.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='pyr',
+    version='0.3.1',
+    description='A nicer REPL for Python.',
+    author='Zain Memon',
+    author_email='zain@inzain.net',
+    url='https://github.com/zain/pyr',
+    packages=['pyr'],
+    install_requires=['pygments'],
+    scripts=['bin/pyr'],
+)
