7c295c8bdb626ea1c0d9e71ab87c0a5a4d9ea3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:37 +0000

    Add package django-memcache-admin/0.0.3/django-memcache-admin-0.0.3.tar.gz

---
+++ packages/django-memcache-admin/django-memcache-admin-0.0.3.tar.gz/django-memcache-admin-0.0.3/memcache_admin/models.py
@@ -0,0 +1,12 @@
+
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.translation import ugettext as _
+
+
+class Dashboard(models.Model):
+    class Meta:
+        verbose_name = _('Memcache Dashboard')
+        verbose_name_plural = _('Memcache Dashboard')
+        managed = False
