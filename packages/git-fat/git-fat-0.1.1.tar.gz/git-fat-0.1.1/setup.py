904dea9c2c4fed6d6d177d015e1f5f3390ca7b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:03 +0000

    Add package git-fat/0.1.1/git-fat-0.1.1.tar.gz

---
+++ packages/git-fat/git-fat-0.1.1.tar.gz/git-fat-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='git-fat',
+    maintainer='Alan Braithwaite',
+    maintainer_email='alan.braithwaite@cyaninc.com',
+    url='https://github.com/cyaninc/git-fat',
+    version='0.1.1',
+    packages=['git_fat'],
+    scripts=['bin/git-fat'],
+    license='BSD 2-Clause',
+    description="Manage large binary files with git",
+    long_description=open('README.rst').read(),
+)
