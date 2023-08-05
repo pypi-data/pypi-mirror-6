49c9dedc253b5afbdbe7ca0c838dcb6818886d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:55 +0000

    Add package django-skebby/0.2.3/django-skebby-0.2.3.tar.gz

---
+++ packages/django-skebby/django-skebby-0.2.3.tar.gz/django-skebby-0.2.3/django_skebby/tests.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.conf import settings
+from django.test.utils import override_settings
+from django_skebby.utils import Sms, skebby_credit_left, SkebbySmsError
+
+class TestSkebby(TestCase):
+    def test_basic_sms(self):
+        sms = Sms("Hi there!", ["123456789"], sender_string="Your Friend")
+        self.assertEqual(sms.headers, {'user-agent': 'Generic Client'})
+
+    def test_template_rendering(self):
+        sms = Sms("Happy {{ festivity }}!", ["123456789"], sender_string="Your friend", ctx={'festivity': 'birthday'})
+        self.assertEqual(sms.text, "Happy birthday!")
+
+    def test_sender_num_string(self):
+        ret = False
+        try:
+            sms = Sms("Hi there!", ["123456789"], sender_string="Your friend", sender_number="123456789")
+        except SkebbySmsError:
+            ret = True
+        self.assertEqual(ret, True)
+
+    def test_charset(self):
+        ret = False
+        try:
+            sms = Sms("Hi there!", ["123456789"], charset="foo",  sender_number="123456789")
+        except SkebbySmsError:
+            ret = True
+        self.assertEqual(ret, True)
+
+    def test_credit_left(self):
+        r = skebby_credit_left()
+        credit = r['body']
+        self.assertEqual(True, 'credit_left' in credit)
+        self.assertEqual(True, 'classic_sms' in credit)
+        self.assertEqual(True, 'basic_sms' in credit)
+
+    def test_send(self):
+        try:
+           test_number = settings.SKEBBY_TEST_NUMBER
+        except AttributeError:
+           return
+
+        sms = Sms("Hi there!", [test_number], sender_string="Your Friend")
+        ret = sms.send()
+        failed_requests = [r for r in ret if r['error']]
+        succesful_requests = [r for r in ret if not r['error']]
+        self.assertEqual(len(failed_requests) + len(succesful_requests), 1)
+
+    def test_send_single(self):
+        try:
+           test_number = settings.SKEBBY_TEST_NUMBER
+        except AttributeError:
+           return
+
+        sms = Sms("Hi {{ friend }}!", sender_string="Your Friend")
+        ret = sms.send_single({'friend': 'Doge'}, test_number)
+        self.assertEqual(sms.text, "Hi Doge!")
+        self.assertEqual(True, 'error' in ret)
+        self.assertEqual(True, 'message' in ret)
---
+++ packages/django-skebby/django-skebby-0.2.3.tar.gz/django-skebby-0.2.3/django_skebby/utils.py
@@ -0,0 +1,146 @@
+from django.conf import settings
+from django.utils.encoding import smart_str
+from django.template.loader import get_template_from_string
+from django.template.context import Context
+import requests
+
+try:
+    SKEBBY_URL = settings.SKEBBY_URL
+except AttributeError:
+    SKEBBY_URL = "https://gateway.skebby.it/api/send/smseasy/advanced/http.php"
+
+SKEBBY_METHODS = {
+    'basic': 'send_sms_basic',
+    'classic': 'send_sms_classic',
+    'report': 'send_sms_classic_report',
+}
+
+SKEBBY_CHARSETS = ('ISO-8859-1', 'UTF-8')
+
+DEFAULT_MAX_RECIPIENTS = 50000
+# this must be agreed with skebby
+MAX_RECIPIENTS = 100000
+
+class SkebbySmsError(Exception):
+    pass
+
+class SkebbySendError(Exception):
+    pass
+
+def _parse_response(response):
+    try:
+        response.raise_for_status()
+        text = response.text.split('&')
+        result = {}
+        for pair in text:
+            k, v = pair.split('=')
+            result[k] = v
+
+        if result['status'] == 'failed':
+            error_message = result['message']
+            error = True
+        else:
+            error = False
+            error_message = ""
+    except requests.exceptions.HTTPError as e:
+        result = {}
+        error_message = e
+        error = True
+    return { 'response': response, 'error': error, 'message': error_message, 'body': result}
+
+class Sms:
+    def __init__(self, text, recipients=None, sender_number=None, sender_string=None, charset=None, ctx=None, headers=None):
+        self.template = get_template_from_string(text)
+        self.ctx = {} if ctx is None else ctx
+        self.text = smart_str(self.template.render(Context(self.ctx)))
+
+        if sender_number and sender_string:
+            raise SkebbySmsError("Only one between sender_number and sender_string may be specified")
+
+        self.sender_number = sender_number
+        self.sender_string = sender_string
+
+        self.charset = "UTF-8" if charset is None else charset
+        if self.charset not in SKEBBY_CHARSETS:
+            raise SkebbySmsError("Invalid charset")
+
+        self.headers = {'user-agent': 'Generic Client'} if headers is None else headers
+        self.recipients = [] if recipients is None else recipients
+
+        try:
+            self.max_recipients = settings.SKEBBY_MAX_RECIPIENTS
+        except AttributeError:
+            self.max_recipients = DEFAULT_MAX_RECIPIENTS
+
+        if self.max_recipients > MAX_RECIPIENTS:
+            self.max_recipients = MAX_RECIPIENTS
+
+    def _check_method(self, method):
+        if not method:
+            method = 'basic'
+        elif method not in SKEBBY_METHODS:
+            raise SkebbySendError("Invalid method")
+        return SKEBBY_METHODS.get(method)
+
+
+    def send(self, method=None):
+        username = settings.SKEBBY_USERNAME
+        password = settings.SKEBBY_PASSWORD
+
+        method = self._check_method(method)
+
+        num_remainders = len(self.recipients)
+        if not num_remainders:
+            raise SkebbySendError("No recipients")
+
+        ret = []
+        while num_remainders:
+            remainders = self.recipients[:self.max_recipients]
+            payload = {
+                'recipients': remainders,
+                'method': method,
+                'username': username,
+                'password': password,
+                'text': self.text,
+                'sender_number': self.sender_number,
+                'sender_string': self.sender_string,
+                'charset': self.charset,
+            }
+            r = requests.post(SKEBBY_URL, data=payload, headers=self.headers)
+            num_remainders -= len(remainders)
+            ret.append(_parse_response(r))
+        return ret
+
+    def send_single(self, ctx, recipient, method=None):
+        username = settings.SKEBBY_USERNAME
+        password = settings.SKEBBY_PASSWORD
+
+        method = self._check_method(method)
+        self.text = smart_str(self.template.render(Context(ctx)))
+
+        payload = {
+            'recipients': recipient,
+            'method': method,
+            'username': username,
+            'password': password,
+            'text': self.text,
+            'sender_number': self.sender_number,
+            'sender_string': self.sender_string,
+            'charset': self.charset,
+        }
+        r = requests.post(SKEBBY_URL, data=payload, headers=self.headers)
+        return _parse_response(r)
+
+def skebby_credit_left():
+
+    username = settings.SKEBBY_USERNAME
+    password = settings.SKEBBY_PASSWORD
+
+    payload = {
+        'method': 'get_credit',
+        'username': username,
+        'password': password,
+    }
+
+    r = requests.post(SKEBBY_URL, data=payload)
+    return _parse_response(r)
