e731fe06fa298ace0988bad3bfd40f673cb46341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:22 +0000

    Add package django-babel/0.2.3/django-babel-0.2.3.tar.gz

---
+++ packages/django-babel/django-babel-0.2.3.tar.gz/django-babel-0.2.3/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007 Edgewall Software
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution. The terms
+# are also available at http://babel.edgewall.org/wiki/License.
+#
+# This software consists of voluntary contributions made by many
+# individuals. For the exact contribution history, see the revision
+# history and logs, available at http://babel.edgewall.org/log/.
+
+from setuptools import setup
+
+setup(
+    name = 'django-babel',
+    description = 'Utilities for using Babel in Django',
+    version = '0.2.3',
+    license = 'BSD',
+    author  = 'Edgewall Software',
+    author_email = 'python-babel@googlegroups.com',
+    url = 'http://github.com/graingert/django-babel/',
+
+    packages = ['babeldjango', 'babeldjango.templatetags'],
+    install_requires = ['Babel'],
+
+    entry_points = """
+    [babel.extractors]
+    django = babeldjango.extract:extract_django
+    """,
+)
