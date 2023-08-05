892366f70faba4f8b7fa38a7826ab7a7d6305402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package ads/0.0.796/ads-0.0.796.tar.gz

---
+++ packages/ads/ads-0.0.796.tar.gz/ads-0.0.796/ads/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+""" A Python tool for interacting with NASA's ADS system. """
+
+__author__ = "Andy Casey <andy@astrowizici.st>"
+__version__ = "0.0.796"
+
+from core import search, metrics, metadata, retrieve_article
