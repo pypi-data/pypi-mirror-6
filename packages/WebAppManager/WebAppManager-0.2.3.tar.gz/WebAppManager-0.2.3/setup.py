51dcfe31522314d605b549fb5931cdc27034397a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:56 +0000

    Add package WebAppManager/0.2.3/WebAppManager-0.2.3.tar.gz

---
+++ packages/WebAppManager/WebAppManager-0.2.3.tar.gz/WebAppManager-0.2.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='WebAppManager',
+    version='0.2.3',
+    author='Andrew Udvare',
+    author_email='audvare@gmail.com',
+    packages=['webappman'],
+    url='https://github.com/Appdynamics/python-webappman',
+    license='LICENSE.txt',
+    description='Management of common web apps (Drupal, WordPress).',
+    long_description=open('README.rst').read(),
+    scripts=['bin/wam-install-drupal', 'bin/wam-install-wordpress'],
+)
