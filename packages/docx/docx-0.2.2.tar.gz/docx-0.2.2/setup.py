6611efd06136803a66f958ef42323d7405e4117b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:28 +0000

    Add package docx/0.2.2/docx-0.2.2.tar.gz

---
+++ packages/docx/docx-0.2.2.tar.gz/docx-0.2.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+from glob import glob
+
+# Make data go into site-packages (http://tinyurl.com/site-pkg)
+from distutils.command.install import INSTALL_SCHEMES
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+
+setup(name='docx',
+      version='0.2.2',
+      install_requires=['lxml', 'PIL'],
+      description='The docx module creates, reads and writes Microsoft Office Word 2007 docx files',
+      author='Mike MacCana',
+      author_email='python-docx@googlegroups.com',
+      maintainer='Steve Canny',
+      maintainer_email='python-docx@googlegroups.com',
+      url='http://github.com/mikemaccana/python-docx',
+      py_modules=['docx'],
+      data_files=[
+          ('docx-template/_rels', glob('template/_rels/.*')),
+          ('docx-template/docProps', glob('template/docProps/*.*')),
+          ('docx-template/word', glob('template/word/*.xml')),
+          ('docx-template/word/theme', glob('template/word/theme/*.*')),
+          ],
+      )
