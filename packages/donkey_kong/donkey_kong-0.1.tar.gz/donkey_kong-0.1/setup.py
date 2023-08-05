52bb5b6892b8f51ee13024a5c1f9e4780f3facd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:09 +0000

    Add package donkey_kong/0.1/donkey_kong-0.1.tar.gz

---
+++ packages/donkey_kong/donkey_kong-0.1.tar.gz/donkey_kong-0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='donkey_kong',
+    version='0.1',
+    author="Jessie Frazelle",
+    author_email="jessie@yhathq.com",
+    url="https://github.com/yhat/donkey_kong",
+    packages=find_packages(),
+    description="Send mandrill templates from the command line.",
+    license='BSD',
+    long_description=open('README.rst').read(),
+    scripts=['bin/donkey_kong'],
+    install_requires=[
+        "colorama==0.2.5",
+        "prettytable==0.7.2",
+        "mandrill==1.0.53"
+    ],
+    keywords=['mandrill', 'mailchimp', 'email', 'templates', 'send'],
+)
