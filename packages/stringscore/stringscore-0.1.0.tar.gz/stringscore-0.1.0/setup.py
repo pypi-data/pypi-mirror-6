1ab77bd06b0da74bb1931fc99c1d4552113fc86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package stringscore/0.1.0/stringscore-0.1.0.tar.gz

---
+++ packages/stringscore/stringscore-0.1.0.tar.gz/stringscore-0.1.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.md').read()
+history = open('HISTORY.md').read()
+
+setup(
+    name='stringscore',
+    version='0.1.0',
+    description='An algorithm provides scores between 0.0 (no match) to 1.0 (perfect match) for a comparison of two strings.',
+    long_description=readme + '\n\n' + history,
+    author='Grey Lee',
+    author_email='bcse@bcse.tw',
+    url='https://github.com/bcse/stringscore',
+    packages=[
+        'stringscore',
+    ],
+    package_dir={'stringscore': 'stringscore'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="MIT",
+    zip_safe=False,
+    keywords='stringscore',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='tests',
+)
