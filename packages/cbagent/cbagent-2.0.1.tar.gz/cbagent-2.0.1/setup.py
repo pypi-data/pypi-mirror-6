c3d489c0e07a5c2f6188b486aca51cd7eee8d31a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:08 +0000

    Add package cbagent/2.0.1/cbagent-2.0.1.tar.gz

---
+++ packages/cbagent/cbagent-2.0.1.tar.gz/cbagent-2.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+version = '2.0.1'
+
+setup(
+    name='cbagent',
+    version=version,
+    author='Couchbase',
+    license='Apache Software License',
+    packages=[
+        'cbagent',
+        'cbagent.collectors',
+        'cbagent.collectors.libstats'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cbagent = cbagent.__main__:main',
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'couchbase==1.2.0',
+        'decorator',
+        'fabric==1.8.0',
+        'logger',
+        'requests==2.1.0',
+        'seriesly',
+        'spring'
+    ],
+)
