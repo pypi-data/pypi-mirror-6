584127a60d087bbe20f00872151f68ff595e9374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:44 +0000

    Add package Flask-FedoraCommons/0.0.4/Flask-FedoraCommons-0.0.4.zip

---
+++ packages/Flask-FedoraCommons/Flask-FedoraCommons-0.0.4.zip/Flask-FedoraCommons-0.0.4/setup.py
@@ -0,0 +1,42 @@
+"""
+Flask-FedoraCommons
+-------------------
+
+This Flask extension read/write access to the digital repository `Fedora
+Commons <http://fedora-commons.org/>`_ for use by Flask applications
+
+"""
+__author__ = "Jeremy Nelson"
+
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name='Flask-FedoraCommons',
+    version='0.0.4',
+    url='http://github.com/jermnelson/flask-fedora-commons',
+    license='MIT License',
+    author=__author__,
+    author_email='jermnelson@gmail.com',
+    description='Library for manipulating Fedora Commons digitial repositories',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',        
+        'Topic :: Software Development :: Libraries :: Python Modules'
+
+    ]
+)
