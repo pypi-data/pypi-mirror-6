5c74ec4b8b2df237e1cfee951381d9aee12c0d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:49 +0000

    Add package yhat/0.5.1/yhat-0.5.1.tar.gz

---
+++ packages/yhat/yhat-0.5.1.tar.gz/yhat-0.5.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+required = ['requests==0.14.2']
+
+setup(
+    author='glamp',
+    name='yhat',
+    version='0.5.1',
+    description='Official Python driver for the yhat api',
+    #long_description=open('README.txt').read(),
+    url='https://github.com/yhat/yhat-client',
+    license='Apache License',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    keywords=['yhat', 'scikits', 'numpy', 'pandas'],
+    packages=find_packages(),
+    install_requires=required
+)
