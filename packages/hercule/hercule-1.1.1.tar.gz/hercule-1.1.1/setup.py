1e51db03adfd8a736c14523d334a55acf04966c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:15 +0000

    Add package hercule/1.1.1/hercule-1.1.1.tar.gz

---
+++ packages/hercule/hercule-1.1.1.tar.gz/hercule-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='hercule',
+	version='1.1.1',
+	description='Easy-to-use python wrapper for the Riot Games League of Legends API',
+	author='Kevin Jasieniecki',
+	author_email='jasieni2@illinois.edu',
+	url='http://github.com/Greedoid',
+	packages=['hercule'],
+	install_requires=['requests',]
+	)
