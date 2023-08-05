ba8adfdc0d60d273f8c36ae68170796aa3125e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:16 +0000

    Add package pyshop/0.9.5/pyshop-0.9.5.tar.gz

---
+++ packages/pyshop/pyshop-0.9.5.tar.gz/pyshop-0.9.5/pyshop/tests/conf.py
@@ -0,0 +1,14 @@
+
+settings = {'jinja2.directories': 'pyshop:templates',
+            'scss.asset_path': 'pyshop:assets/sass',
+            'scss.compress': 'false',
+            'scss.cache': 'false',
+            'cookie_key': 'sicr3t',
+            'sqlalchemy.url': 'sqlite://',
+            'sqlalchemy.echo': False,
+            'sqlalchemy.pool_size': 1,
+            'pyshop.upload.sanitize': False,
+            'pyshop.mirror.sanitize': False,
+            'pyshop.pypi.url': 'http://localhost:65432',
+            'pyshop.repository': '/tmp',
+            }
