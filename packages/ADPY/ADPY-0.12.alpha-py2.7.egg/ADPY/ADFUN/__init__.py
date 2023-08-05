a99a24762f11b59860d88477ac23f3bbc159b3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:28 +0000

    Add package ADPY/0.12.alpha/ADPY-0.12.alpha-py2.7.egg

---
+++ packages/ADPY/ADPY-0.12.alpha-py2.7.egg/ADPY/ADFUN/__init__.py
@@ -0,0 +1,16 @@
+"""
+=============================================================
+ADPY, a library for Automatic Differentation (AD) in Python
+=============================================================
+
+"""
+import os
+__install_path__ = os.path.realpath(__file__)
+
+__all__ = ['adfun']
+
+import adfun
+from adfun import adfun
+
+
+
---
+++ packages/ADPY/ADPY-0.12.alpha-py2.7.egg/ADPY/SOLVER/__init__.py
@@ -0,0 +1,13 @@
+"""
+=============================================================
+ADPY, a library for Automatic Differentation (AD) in Python
+=============================================================
+
+"""
+import os
+__install_path__ = os.path.realpath(__file__)
+
+from solver import solver
+
+
+
