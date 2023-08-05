018d8a2816f2a1da0082fa693bb3ede85719bd03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:02 +0000

    Add package PyPrind/1.0.2/PyPrind-1.0.2.tar.gz

---
+++ packages/PyPrind/PyPrind-1.0.2.tar.gz/PyPrind-1.0.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+readme = open('README.txt', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+
+setup(name='PyPrind',
+      version='1.0.2',
+      description='Python Progress Indicator Utility',
+      author='Sebastian Raschka',
+      author_email='se.raschka@gmail.com',
+      url='https://github.com/rasbt/pyprind',
+      download_url='https://github.com/rasbt/pyprind/archive/1.0.1.tar.gz',
+      packages=['pyprind'],
+      data_files = [('', ['LICENSE.txt']),
+                    ('', ['README.html']),
+                    ('', ['README.txt']),
+                    ('', ['CHANGELOG.txt']),
+                    ('examples', ['examples/ex_percentage_indicator.py']),
+                    ('examples', ['examples/ex_progress_bar.py']),
+                   ],
+      license='GPLv3',
+      platforms='any',
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Environment :: Console',
+      ],
+      long_description = README_TEXT,
+    )
