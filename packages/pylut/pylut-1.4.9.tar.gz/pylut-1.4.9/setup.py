be3581eb05173a10ee36ba9771cd08c3759e4a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:42 +0000

    Add package pylut/1.4.9/pylut-1.4.9.tar.gz

---
+++ packages/pylut/pylut-1.4.9.tar.gz/pylut-1.4.9/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pylut',
+    version='1.4.9',
+    author='Greg Cotten',
+    author_email='gcgc90@gmail.com',
+    packages=['pylut', 'pylut.progress'],
+    scripts=['bin/pylut'],
+    url='https://github.com/gregcotten/pylut',
+    license='LICENSE.txt',
+    description='Builds, modifies, visualizes, and converts 3D LUTs from popular .cube and .3dl formats.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy",
+        "docopt"
+    ],
+)
