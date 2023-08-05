c733b67ddc92542398278e490c7121e03b8317d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:15 +0000

    Add package ares-util/0.0.3/ares_util-0.0.3.zip

---
+++ packages/ares-util/ares_util-0.0.3.zip/ares_util-0.0.3/setup.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+
+# https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+setup(
+    name='ares_util',
+    version='0.0.3',
+    description='A tool for information system allowing a retrieval '
+                'of information on economic entities registered in '
+                'the Czech Republic (ARES - Access to Registers of Economic Subjects / Entities).',
+    long_description=(open('README.md').read()),
+    url='https://github.com/illagrenan/ares_util',
+    license='MIT',
+    author='Vašek Dohnal',
+    author_email='vaclav.dohnal@gmail.com',
+
+    # The exclude makes sure that a top-level tests package doesn’t get
+    # installed (it’s still part of the source distribution)
+    # since that would wreak havoc.
+    packages=find_packages(exclude=['tests*']),
+
+
+    install_requires=['xmltodict', ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Environment :: Console',
+        'Intended Audience :: Developers'
+    ],
+)
