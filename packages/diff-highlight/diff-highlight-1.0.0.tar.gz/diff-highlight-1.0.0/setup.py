dd4063c0aa6914d39fdd5224535d45158837f5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:52 +0000

    Add package diff-highlight/1.0.0/diff-highlight-1.0.0.tar.gz

---
+++ packages/diff-highlight/diff-highlight-1.0.0.tar.gz/diff-highlight-1.0.0/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import sys
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.4",
+    "Programming Language :: Python :: 2.5",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.2",
+    "Programming Language :: Python :: 3.3",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Version Control",
+    "Topic :: Text Processing :: Filters",
+]
+
+test_requires = ['mercurial']
+if sys.version_info < (2, 7):
+    test_requires.append('unittest2')
+if sys.version_info > (2, 4):
+    test_requires.append('mock')
+
+setup(
+    name='diff-highlight',
+    version='1.0.0',
+    description='pretty diff highlighter; emphasis changed words in diff',
+    long_description=open("README.rst").read(),
+    classifiers=classifiers,
+    keywords=['mercurial', 'git', 'diff', 'highlight'],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='http://blockdiag.com/',
+    download_url='http://pypi.python.org/pypi/diff-highlight',
+    license='Apache License 2.0',
+    py_modules=['diff_highlight'],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    tests_require=test_requires,
+    entry_points="""
+       [console_scripts]
+       diff-highlight = highlights.command:highlight_main
+    """
+)
