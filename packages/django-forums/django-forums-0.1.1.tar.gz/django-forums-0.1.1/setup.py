3705e1627bc9f9dd977683ed944dca3acd068d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:39 +0000

    Add package django-forums/0.1.1/django-forums-0.1.1.tar.gz

---
+++ packages/django-forums/django-forums-0.1.1.tar.gz/django-forums-0.1.1/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+import forums
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-forums',
+    version=forums.__version__,
+    description='Small standalone django forums application',
+    long_description=read('README.md'),
+    license='MIT License',
+    author='byteweaver',
+    author_email='contact@byteweaver.net',
+    url='https://github.com/byteweaver/django-forums',
+    packages=find_packages(),
+    install_requires=[
+        'django',
+    ],
+    tests_require=[
+        'django-nose',
+        'coverage',
+        'django-coverage',
+        'factory-boy',
+    ],
+    test_suite='forums.tests.runtests.runtests',
+)
