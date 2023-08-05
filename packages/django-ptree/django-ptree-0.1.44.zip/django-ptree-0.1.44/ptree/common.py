ec7616255357d654b8d173e70b19271b4272033c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package django-ptree/0.1.44/django-ptree-0.1.44.zip

---
+++ packages/django-ptree/django-ptree-0.1.44.zip/django-ptree-0.1.44/ptree/common.py
@@ -0,0 +1,159 @@
+from collections import OrderedDict
+from django.contrib import admin
+from django.conf.urls import patterns
+from django.shortcuts import render_to_response
+import ptree.constants
+from django.http import HttpResponse, HttpResponseBadRequest
+from urlparse import urljoin
+import datetime
+from django.contrib.staticfiles.templatetags.staticfiles import static as static_template_tag
+
+def new_tab_link(url, label):
+    return '<a href="{}" target="_blank">{}</a>'.format(url, label)
+
+class ParticipantAdmin(admin.ModelAdmin):
+
+
+    def link(self, instance):
+        url = instance.start_url()
+        return
+
+    link.short_description = "Participant link"
+    link.allow_tags = True
+    list_filter = ['match', 'treatment', 'experiment']
+
+class MatchAdmin(admin.ModelAdmin):
+    list_filter = ['treatment', 'experiment']
+
+class TreatmentAdmin(admin.ModelAdmin):
+    def link(self, instance):
+        url = instance.start_url()
+        return new_tab_link(url, 'Link')
+
+    link.short_description = "Demo link"
+    link.allow_tags = True
+    list_filter = ['experiment']
+
+class ExperimentAdmin(admin.ModelAdmin):
+    def global_start_link(self, instance):
+        if instance.is_for_mturk:
+            return 'N/A (is_for_mturk = True)'
+        else:
+            url = instance.start_url()
+            return new_tab_link(url, 'Link')
+
+    global_start_link.allow_tags = True
+    global_start_link.short_description = "Global start URL (only if you can't use regular start URLs)"
+
+    def mturk_snippet_link(self, instance):
+        if instance.is_for_mturk:
+            return new_tab_link('{}/mturk_snippet/'.format(instance.pk), 'Link')
+        else:
+            return 'N/A (is_for_mturk = False)'
+
+    mturk_snippet_link.allow_tags = True
+    mturk_snippet_link.short_description = "HTML snippet for MTurk HIT page"
+
+
+    def payments_link(self, instance):
+        return new_tab_link('{}/payments/'.format(instance.pk), 'Link')
+
+    payments_link.short_description = "Payments page"
+    payments_link.allow_tags = True
+
+    def start_urls_link(self, instance):
+        return new_tab_link('{}/start_urls/?{}={}'.format(instance.pk,
+                                                          ptree.constants.experimenter_access_code,
+                                                          instance.experimenter_access_code), 'Link')
+
+    start_urls_link.short_description = 'Start URLs'
+    start_urls_link.allow_tags = True
+
+    def experimenter_input_link(self, instance):
+        url = instance.experimenter_input_url()
+        return new_tab_link(url, 'Link')
+
+    experimenter_input_link.short_description = 'Link for experimenter input during gameplay'
+    experimenter_input_link.allow_tags = True
+
+    def get_urls(self):
+        urls = super(ExperimentAdmin, self).get_urls()
+        my_urls = patterns('',
+            (r'^(?P<pk>\d+)/payments/$', self.admin_site.admin_view(self.payments)),
+            (r'^(?P<pk>\d+)/start_urls/$', self.start_urls),
+            (r'^(?P<pk>\d+)/mturk_snippet/$', self.admin_site.admin_view(self.mturk_snippet))
+        )
+        return my_urls + urls
+
+    def start_urls(self, request, pk):
+        experiment = self.model.objects.get(pk=pk)
+        if request.GET.get(ptree.constants.experimenter_access_code) != experiment.experimenter_access_code:
+            return HttpResponseBadRequest('{} parameter missing or incorrect'.format(ptree.constants.experimenter_access_code))
+        participants = experiment.participants()
+        print request.META['HTTP_HOST']
+        urls = [request.build_absolute_uri(participant.start_url()) for participant in participants]
+        return HttpResponse('\n'.join(urls), content_type="text/plain")
+
+    def mturk_snippet(self, request, pk):
+        experiment = self.model.objects.get(pk=pk)
+        hit_page_js_url = request.build_absolute_uri(static_template_tag('ptree/js/mturk_hit_page.js'))
+        experiment_url = request.build_absolute_uri(experiment.start_url())
+        return render_to_response('admin/MTurkSnippet.html',
+                                  {'hit_page_js_url': hit_page_js_url,
+                                   'experiment_url': experiment_url,},
+                                  content_type='text/plain')
+
+    def payments(self, request, pk):
+        experiment = self.model.objects.get(pk=pk)
+        participants = experiment.participants().order_by('external_id', 'code')
+        return render_to_response('admin/Payments.html',
+                                  {'date': datetime.datetime.now(),
+                                   'app_name': experiment._meta.app_label,
+                                   'experiment_name': experiment.name,
+                                   'experiment_code': experiment.code,
+                                   'participants': participants,
+                                   'total_payments': sum(p.total_pay() for p in participants if p.total_pay())})
+
+
+def remove_duplicates(lst):
+    return list(OrderedDict.fromkeys(lst))
+
+def get_list_display(ModelName, readonly_fields, first_fields):
+    all_field_names = [field.name for field in ModelName._meta.fields]
+
+    # make sure they're actually in the model.
+    first_fields = [f for f in first_fields if f in all_field_names]
+
+    list_display = first_fields + readonly_fields + all_field_names
+    return remove_duplicates(list_display)
+
+def get_readonly_fields(fields_common_to_all_models, fields_specific_to_this_subclass):
+    return remove_duplicates(fields_common_to_all_models + fields_specific_to_this_subclass)
+
+def get_participant_readonly_fields(fields_specific_to_this_subclass):
+    return get_readonly_fields(['link', 'bonus_display'], fields_specific_to_this_subclass)
+
+def get_participant_list_display(Participant, readonly_fields, first_fields=None):
+    first_fields = ['__unicode__', 'id', 'experiment', 'treatment', 'match', 'has_visited'] + (first_fields or [])
+    return get_list_display(Participant, readonly_fields, first_fields)
+
+def get_match_readonly_fields(fields_specific_to_this_subclass):
+    return get_readonly_fields([], fields_specific_to_this_subclass)
+
+def get_match_list_display(Match, readonly_fields, first_fields=None):
+    first_fields = ['__unicode__', 'id', 'experiment', 'treatment', 'time_started'] + (first_fields or [])
+    return get_list_display(Match, readonly_fields, first_fields)
+
+def get_treatment_readonly_fields(fields_specific_to_this_subclass):
+    return get_readonly_fields(['link'], fields_specific_to_this_subclass)
+
+def get_treatment_list_display(Treatment, readonly_fields, first_fields=None):
+    first_fields = ['__unicode__', 'id', 'description', 'experiment'] + (first_fields or [])
+    return get_list_display(Treatment, readonly_fields, first_fields)
+
+def get_experiment_readonly_fields(fields_specific_to_this_subclass):
+    return get_readonly_fields(['start_urls_link', 'mturk_snippet_link', 'global_start_link', 'experimenter_input_link', 'payments_link'], fields_specific_to_this_subclass)
+
+def get_experiment_list_display(Experiment, readonly_fields, first_fields=None):
+    first_fields = ['__unicode__', 'id', 'description'] + (first_fields or [])
+    return get_list_display(Experiment, readonly_fields, first_fields)
---
+++ packages/django-ptree/django-ptree-0.1.44.zip/django-ptree-0.1.44/ptree/views/experimenter.py
@@ -0,0 +1,166 @@
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.conf import settings
+import extra_views
+from ptree.forms import StubModelForm
+from ptree.stuff.models import StubModel
+import vanilla
+
+import ptree.constants as constants
+
+# import the logging library
+import logging
+
+logger = logging.getLogger(__name__)
+
+REDIRECT_TO_PAGE_USER_SHOULD_BE_ON_URL = '/shared/RedirectToPageUserShouldBeOn/'
+
+class ExperimenterMixin(object):
+    @classmethod
+    def get_url_base(cls):
+        # look for url_base attribute on ExperimentClass
+        # if it's not part of a game, but rather a shared module etc, ExperimentClass won't exist.
+        # in that case, url_base needs to be defined on the class.
+        return getattr(cls, 'ExperimentClass', cls).url_base
+
+    @classmethod
+    def url(cls):
+        return '/{}/{}/'.format(cls.get_url_base(), cls.__name__)
+
+    @classmethod
+    def url_pattern(cls):
+        return r'^{}/{}/$'.format(cls.get_url_base(), cls.__name__)
+
+
+class ExperimenterSequenceMixin(ExperimenterMixin):
+    class PageActions:
+        show = 'show'
+        skip = 'skip'
+        wait = 'wait'
+
+    def show_skip_wait(self):
+        return self.PageActions.show
+
+    wait_page_template = 'ptree/WaitPage.html'
+
+    def wait_message(self):
+        return None
+
+    def get_success_url(self):
+        return self.experiment.experimenter_sequence_as_urls()[self.request.session[constants.index_in_sequence_of_views]]
+
+    def dispatch(self, request, *args, **kwargs):
+        self.ExperimentClass = self.request.session[constants.ExperimentClass]
+        self.experiment = self.ExperimentClass.objects.get(code = self.request.session[constants.experiment_code])
+
+        ssw = self.show_skip_wait()
+
+        # should also add GET parameter like check_if_prerequisite_is_satisfied, to be explicit.
+        if self.request.is_ajax():
+            no_more_wait = ssw != self.PageActions.wait
+            return HttpResponse(int(no_more_wait))
+
+        # if the participant shouldn't see this view, skip to the next
+        if ssw == self.PageActions.skip:
+            self.request.session[constants.index_in_sequence_of_views] += 1
+            return HttpResponseRedirect(self.get_success_url())
+
+        if ssw == self.PageActions.wait:
+            return render_to_response(self.wait_page_template, {'SequenceViewURL': self.request.path,
+                                                                'wait_message': self.wait_message()})
+        return super(ExperimenterSequenceMixin, self).dispatch(request, *args, **kwargs)
+
+    def variables_for_template(self):
+        """
+        Should be implemented by subclasses
+        Return a dictionary that contains the template context variables (see Django documentation)
+        You don't need to include the form here; that is taken care of automatically.
+        """
+
+        return {}
+
+    def get_context_data(self, **kwargs):
+
+        context = {}
+
+        if kwargs.has_key(constants.form_invalid):
+            context[constants.form_invalid] = True
+            kwargs.pop(constants.form_invalid)
+
+        context.update(super(ExperimenterSequenceMixin, self).get_context_data(**kwargs))
+        context.update(self.variables_for_template())
+
+        return context
+
+    def get_form(self, data=None, files=None, **kwargs):
+        kwargs.update({'experiment': self.experiment,
+                       'request': self.request})
+
+        cls = self.get_form_class()
+        return cls(data=data, files=files, **kwargs)
+
+    def after_valid_form_submission(self, form):
+        """Should be implemented by subclasses as necessary"""
+        pass
+
+    def form_valid(self, form):
+        self.after_valid_form_submission(form)
+        self.request.session[constants.index_in_sequence_of_views] += 1
+        return super(ExperimenterSequenceMixin, self).form_valid(form)
+
+    def form_invalid(self, form):
+        """
+
+        """
+        return self.render_to_response(self.get_context_data(form=form, form_invalid = True ))
+
+
+class ExperimenterLaunch(ExperimenterMixin, vanilla.View):
+    def get(self, request, *args, **kwargs):
+        # clear all cookies, since they can cause problems if the participant has played a previous game.
+
+        self.request.session.clear()
+
+        experiment_code = self.request.GET[constants.experiment_code]
+        experimenter_access_code = self.request.GET[constants.experimenter_access_code]
+
+        experiment = get_object_or_404(self.ExperimentClass,
+                          code = experiment_code,
+                          experimenter_access_code = experimenter_access_code)
+
+        self.request.session[constants.index_in_sequence_of_views] = 0
+        self.request.session[constants.experiment_code] = experiment_code
+        self.request.session[constants.ExperimentClass] = self.ExperimentClass
+        return HttpResponseRedirect(experiment.experimenter_sequence_as_urls()[0])
+
+
+class ExperimenterUpdateView(ExperimenterSequenceMixin, vanilla.UpdateView):
+    form_class = StubModelForm
+
+    def get_object(self):
+        Cls = self.form_class.Meta.model
+        if Cls == self.ExperimentClass:
+            return self.experiment
+        elif Cls == StubModel:
+            return StubModel.objects.all()[0]
+
+
+class ExperimenterCreateView(ExperimenterSequenceMixin, vanilla.CreateView):
+    pass
+
+
+class ExperimenterModelFormSetView(ExperimenterSequenceMixin, extra_views.ModelFormSetView):
+    extra = 0
+
+    def get_extra_form_kwargs(self):
+        return {'experiment': self.experiment,
+                'request': self.request}
+
+    def after_valid_formset_submission(self, formset):
+        for form in formset:
+            self.after_valid_form_submission(form)
+
+    def formset_valid(self, formset):
+        self.after_valid_formset_submission(formset)
+        self.request.session[constants.index_in_sequence_of_views] += 1
+        return super(ExperimenterModelFormSetView, self).formset_valid(formset)
