4cd02de42152d46894badccfa0e6f1757197b998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:08 +0000

    Add package decorated/1.1.3/decorated-1.1.3.tar.gz

---
+++ packages/decorated/decorated-1.1.3.tar.gz/decorated-1.1.3/decorated/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from decorated.function import Function, BoundedFunction, PartialFunction
+
+Function = Function
+PartialFunction = PartialFunction
+BoundedFunction = BoundedFunction
