ed908438728a793a3f5ea09952f8025d20bfafcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package django-shpaml/1.1.0/django-shpaml-1.1.0.tar.gz

---
+++ packages/django-shpaml/django-shpaml-1.1.0.tar.gz/django-shpaml-1.1.0/setup.py
@@ -0,0 +1,36 @@
+__doc__ = """
+A Django template loader for loading and converting SHPAML markup to HTML
+
+The django SHPAML template loader uses the official Python SHPAML implementation
+which can be found at http://shpaml.webfactional.com/
+
+Note: the SHPAML implementation from the above link is included for your
+convenience.
+"""
+
+from setuptools import setup
+
+setup(
+    name='django-shpaml',
+    version='1.1.0',
+    author='James Robert',
+    description=('A Django template loader for loading and converting '
+                 'SHPAML markup to HTML'),
+    license='BSD',
+    keywords='django shpaml',
+    url='http://shpaml.com',
+    install_requires=[
+        "django >= 1.2",
+    ],
+    packages=['shpaml'],
+    long_description=__doc__,
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities'
+    ]
+)
