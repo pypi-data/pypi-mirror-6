35f4eb20fc9b3cbae046f47722df3199f50880c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package hackertray/1.8.2/hackertray-1.8.2.tar.gz

---
+++ packages/hackertray/hackertray-1.8.2.tar.gz/hackertray-1.8.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='hackertray',
+      version='1.8.2',
+      description='Hacker News app that sits in your System Tray',
+      long_description='HackerTray is a simple Hacker News Linux application that lets you view top HN stories in your System Tray. It relies on appindicator, so it is not guaranteed to work on all systems. It also provides a Gtk StatusIcon fallback in case AppIndicator is not available.',
+      keywords='hacker news hn tray system tray icon hackertray',
+      url='http://captnemo.in/hackertray',
+      author='Abhay Rana',
+      author_email='me@captnemo.in',
+      license='MIT',
+      packages=['hackertray'],
+      data_files=[('images', ['images/hacker-tray.png'])],
+      install_requires=[
+          'requests',
+      ],
+      entry_points={
+          'console_scripts': ['hackertray = hackertray:main'],
+      },
+      zip_safe=False)
