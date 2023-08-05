8f8b8bf406fa3a91a78c407ebcd5cface42db8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package continuity/0.7/continuity-0.7.tar.gz

---
+++ packages/continuity/continuity-0.7.tar.gz/continuity-0.7/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    setup
+    ~~~~~
+
+    Setup the continuity distribution.
+
+    :copyright: 2013 by Jonathan Zempel.
+    :license: BSD, see LICENSE for more details.
+"""
+
+from setuptools import setup
+import continuity
+
+setup(
+    name=continuity.__name__,
+    version=continuity.__version__,
+    url="http://github.com/jzempel/continuity",
+    license=continuity.__license__,
+    author=continuity.__author__,
+    author_email="jzempel@gmail.com",
+    description="Continuous dev flow via GitHub Issues or Pivotal Tracker.",
+    long_description=__doc__,
+    packages=["continuity"],
+    include_package_data=True,
+    install_requires=[
+        "clint",
+        "GitPython",
+            # "gitdb",
+            # "async",
+            # "smmap",
+        "py-getch",
+        "requests",
+            # "certifi",
+            # "chardet"
+    ],
+    entry_points={
+        "console_scripts": [
+            "continuity = continuity.cli:main"
+        ]
+    }
+)
