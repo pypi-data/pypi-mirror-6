37b0b12820eede69a9e6ca8a2bd64cd127376d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:12 +0000

    Add package djangotoolbox/1.6.0/djangotoolbox-1.6.0.tar.gz

---
+++ packages/djangotoolbox/djangotoolbox-1.6.0.tar.gz/djangotoolbox-1.6.0/setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+import os
+
+def get_packages(package):
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+DESCRIPTION = "Djangotoolbox for Django-nonrel"
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+setup(name='djangotoolbox',
+      version='1.6.0',
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author='Waldemar Kornewald',
+      author_email='wkornewald@gmail.com',
+      url='https://github.com/django-nonrel/djangotoolbox',
+      packages=get_packages('djangotoolbox'),
+      license='3-clause BSD',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Database',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+)
