d9d70cf0f459565cf763155cf444052bb6ec7daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:32 +0000

    Add package pyramid_eplasty/0.1.0/pyramid_eplasty-0.1.0.zip

---
+++ packages/pyramid_eplasty/pyramid_eplasty-0.1.0.zip/pyramid_eplasty-0.1.0/pyramid_eplasty/tween.py
@@ -0,0 +1,32 @@
+import eplasty as ep
+import psycopg2.pool
+import transaction
+
+from pyramid_eplasty.data_manager import EPDataManager
+
+
+def eplasty_tween_factory(handler, registry):
+    config=registry.settings
+    import pdb; pdb.set_trace()
+    connection_pool = psycopg2.pool.ThreadedConnectionPool(
+        5, 10, 
+        host = config.get('eplasty.host', '127.0.0.1'),
+        port = config.get('eplasty.port', '5432'),
+        database = config['eplasty.database'],
+        user = config['eplasty.username'],
+        password = config['eplasty.passwd'],
+        connection_factory=ep.cursor.EPConnection,
+    )
+    def eplasty_tween(request):
+        conn = connection_pool.getconn()
+        manager = EPDataManager(connection_pool, conn)
+        request.ep_session = manager.session
+        transaction.get().join(manager)
+        return handler(request)
+    return eplasty_tween
+
+def includeme(config):
+    config.add_tween(
+        'pyramid_eplasty.tween.eplasty_tween_factory',
+        under='pyramid_tm.tm_tween_factory'
+    )
