f65d6d1b46eeb4c67cac25add5f98e90909d9e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:27 +0000

    Add package Flask-AppBuilder/0.3.12/Flask-AppBuilder-0.3.12.tar.gz

---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-0.3.12.tar.gz/Flask-AppBuilder-0.3.12/flask_appbuilder/validators.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm.exc import NoResultFound
+
+from wtforms import ValidationError
+
+
+class Unique(object):
+    """
+        Checks field value unicity against specified table field.
+
+        :param datamodel:
+            The datamodel class, abstract layer for backend
+        :param column:
+            The unique column.
+        :param message:
+            The error message.
+    """
+    field_flags = ('unique', )
+
+    def __init__(self, datamodel, column, message=None):
+        self.datamodel = datamodel
+        self.column = column
+        self.message = message
+
+    def __call__(self, form, field):
+        filters = {}
+        filters[self.column.name] = field.data
+        count, obj = self.datamodel.query(filters)
+        if (count > 0):
+            # only test if Unique if pk value is diferent, update case.
+            if not (hasattr(form,'_id') and form._id == self.datamodel.get_keys(obj)[0]):
+                if self.message is None:
+                    self.message = field.gettext(u'Already exists.')
+                raise ValidationError(self.message)
+        
