cc975b6fb1f9fc82666b17a3d0baaa6110bc6375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:09 +0000

    Add package django_backstage/0.0.55/django_backstage-0.0.55.tar.gz

---
+++ packages/django_backstage/django_backstage-0.0.55.tar.gz/django_backstage-0.0.55/backstage/settings/app_settings.py
@@ -0,0 +1,28 @@
+INSTALLED_APPS = [
+   'django.contrib.auth',
+   'django.contrib.contenttypes',
+   'grappelli_safe', #must precede admin
+   'django.contrib.redirects',
+   'django.contrib.sessions',
+   'django.contrib.sites',
+   'django.contrib.sitemap'
+   's',
+   'django.contrib.humanize',
+   'django.contrib.comments',
+   'django.contrib.admin',
+   'django.contrib.staticfiles',
+   'filebrowser_safe',
+   'south',
+   'debug_toolbar',
+   'django_extensions',
+   'compressor',
+   'sekizai',
+   'selectable',
+   'backstage',
+]
+
+
+#   'django.contrib.markup',
+
+#   'django.contrib.flatpages',
+#   'django.contrib.staticfiles',
