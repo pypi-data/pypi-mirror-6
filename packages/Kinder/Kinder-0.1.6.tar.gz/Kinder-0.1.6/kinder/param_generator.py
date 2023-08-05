61255ca0f0f766785330d6a4040a92a4d2621d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:53 +0000

    Add package Kinder/0.1.6/Kinder-0.1.6.tar.gz

---
+++ packages/Kinder/Kinder-0.1.6.tar.gz/Kinder-0.1.6/kinder/param_generator.py
@@ -0,0 +1,6 @@
+class ParamGenerator:
+    def __init__(self, options = None):
+        self.options = options
+
+    def generate_params(self):
+        pass
---
+++ packages/Kinder/Kinder-0.1.6.tar.gz/Kinder-0.1.6/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Kinder',
+    version='0.1.6',
+    author='John J. Wang',
+    author_email='john@zinc.io',
+    packages=['kinder'],
+    scripts=[],
+    url='',
+    license="The MIT License (MIT)",
+    description='Probabilistic testing framework.',
+    long_description="A probabilistic testing framework designed for testing API endpoints.",
+    install_requires=[],
+    )
