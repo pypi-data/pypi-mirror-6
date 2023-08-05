7cb362a00984183d6503d9d14573b67bdcaeffea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:44 +0000

    Add package terminal/0.4.0/terminal-0.4.0.tar.gz

---
+++ packages/terminal/terminal-0.4.0.tar.gz/terminal-0.4.0/README.rst
@@ -0,0 +1,38 @@
+Terminal: for Simple and Beautiful
+==================================
+
+.. image:: https://travis-ci.org/lepture/terminal.png?branch=master
+        :target: https://travis-ci.org/lepture/terminal
+.. image:: https://coveralls.io/repos/lepture/terminal/badge.png?branch=master
+        :target: https://coveralls.io/r/lepture/terminal
+
+A better terminal tools for python.
+
+Find documentation at `Read The Docs`_.
+
+.. _`Read The Docs`: https://terminal.readthedocs.org/
+
+
+Features
+--------
+
+* An easy to use command parser
+* Full featured ANSI colors and styles
+* Full featured prompt communication
+* Nested and verbose logging
+* No dependencies (windows need colorama)
+
+Installation
+------------
+
+To install terminal, simply::
+
+    $ pip install terminal
+
+
+Contribute
+----------
+
+Read the `Contribution Guide`_.
+
+.. _`Contribution Guide`: https://github.com/lepture/terminal/blob/master/CONTRIBUTING.rst
---
+++ packages/terminal/terminal-0.4.0.tar.gz/terminal-0.4.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+kwargs = {}
+if sys.platform == 'win32':
+    kwargs['install_requires'] = ['colorama']
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import terminal
+from email.utils import parseaddr
+author, author_email = parseaddr(terminal.__author__)
+
+setup(
+    name='terminal',
+    version=terminal.__version__,
+    author=author,
+    author_email=author_email,
+    url=terminal.__homepage__,
+    packages=['terminal'],
+    description=terminal.__doc__,
+    long_description=open('README.rst').read(),
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    **kwargs
+)
