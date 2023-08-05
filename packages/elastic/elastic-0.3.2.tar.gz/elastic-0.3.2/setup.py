78ddf10715a9d50e97e155ada9f7ea99b3198c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:17 +0000

    Add package elastic/0.3.2/elastic-0.3.2.tar.gz

---
+++ packages/elastic/elastic-0.3.2.tar.gz/elastic-0.3.2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='elastic',
+    version='0.3.2',
+    packages=['elastic',],
+    license='GPLv3',
+    description = 'Extension for ASE to calculate elastic constants',
+    author = 'Paweł T. Jochym',
+    author_email = 'Pawel.Jochym@ifj.edu.pl',
+    url = 'https://github.com/jochym/Elastic',   # use the URL to the github repo
+    download_url = 'https://github.com/jochym/Elastic/tarball/0.3.2', # I'll explain this in a second
+    keywords = ['science', 'physics', 'ase', 'elastic constants', 'crystals'], # arbitrary keywords
+    classifiers = [],
+)
