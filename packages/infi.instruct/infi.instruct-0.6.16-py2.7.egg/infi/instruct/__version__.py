08156fe068c4390d780c932f8a3ba2f5baedc449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:56 +0000

    Add package infi.instruct/0.6.16/infi.instruct-0.6.16-py2.7.egg

---
+++ packages/infi.instruct/infi.instruct-0.6.16-py2.7.egg/infi/instruct/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.6.16"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.6.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ffc7c0702748a26db927fc6ef2b59925cfa28496'
+__git_head_subject__ = 'Finished release v0.6.16.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
