856eb9be5fe5cfda1af34e12b62e5da7a34bcb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:16 +0000

    Add package pyface/4.4.0/pyface-4.4.0.tar.gz

---
+++ packages/pyface/pyface-4.4.0.tar.gz/pyface-4.4.0/setup.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2008-2013 by Enthought, Inc.
+# All rights reserved.
+
+from os.path import join
+from setuptools import setup, find_packages
+
+
+info = {}
+execfile(join('pyface', '__init__.py'), info)
+
+
+setup(
+    name = 'pyface',
+    version = info['__version__'],
+    url = 'http://code.enthought.com/projects/traits_gui',
+    author = 'David C. Morrill, et al.',
+    author_email = 'dmorrill@enthought.com',
+    classifiers = [c.strip() for c in """\
+        Development Status :: 5 - Production/Stable
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines() if len(c.split()) > 0],
+    description = 'traits-capable windowing framework',
+    long_description = open('README.rst').read(),
+    download_url = ('http://www.enthought.com/repo/ets/pyface-%s.tar.gz' %
+                    info['__version__']),
+    install_requires = info['__requires__'],
+    license = 'BSD',
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
+    package_data = {
+        '': ['images/*'],
+    },
+    packages = find_packages(),
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
+    zip_safe   = False,
+)
