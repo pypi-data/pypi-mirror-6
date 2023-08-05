2101c8f972dc4bae0301e49db28b494f5f82f05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package pyipptool/0.1.dev/pyipptool-0.1.dev.tar.gz

---
+++ packages/pyipptool/pyipptool-0.1.dev.tar.gz/pyipptool-0.1.dev/pyipptool/forms.py
@@ -0,0 +1,43 @@
+from pkg_resources import resource_filename
+
+import deform.template
+
+from .schemas import (cancel_job_schema,
+                      release_job_schema,
+                      create_printer_subscription_schema,
+                      cups_add_modify_class_schema,
+                      cups_add_modify_printer_schema,
+                      cups_delete_printer_schema,
+                      cups_delete_class_schema,
+                      cups_get_classes_schema,
+                      cups_get_ppds_schema,
+                      cups_get_printers_schema,
+                      cups_move_job_schema,
+                      cups_reject_jobs_schema,
+                      get_job_attributes_schema,
+                      get_jobs_schema,
+                      get_subscriptions_schema,
+                      )
+
+default_dir = resource_filename('pyipptool', 'templates/')
+renderer = deform.template.ZPTRendererFactory((default_dir,))
+
+deform.Form.set_default_renderer(renderer)
+
+
+cancel_job_form = deform.Form(cancel_job_schema)
+release_job_form = deform.Form(release_job_schema)
+create_printer_subscription_form = deform.Form(
+    create_printer_subscription_schema)
+cups_add_modify_printer_form = deform.Form(cups_add_modify_printer_schema)
+cups_delete_printer_form = deform.Form(cups_delete_printer_schema)
+cups_delete_class_form = deform.Form(cups_delete_class_schema)
+cups_add_modify_class_form = deform.Form(cups_add_modify_class_schema)
+cups_get_classes_form = deform.Form(cups_get_classes_schema)
+cups_get_ppds_form = deform.Form(cups_get_ppds_schema)
+cups_get_printers_form = deform.Form(cups_get_printers_schema)
+cups_move_job_form = deform.Form(cups_move_job_schema)
+cups_reject_jobs_form = deform.Form(cups_reject_jobs_schema)
+get_job_attributes_form = deform.Form(get_job_attributes_schema)
+get_jobs_form = deform.Form(get_jobs_schema)
+get_subscriptions_form = deform.Form(get_subscriptions_schema)
