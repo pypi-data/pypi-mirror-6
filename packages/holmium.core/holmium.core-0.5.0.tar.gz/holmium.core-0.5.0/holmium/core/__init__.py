6f13649c35d51c35050cde43856ea292e003862f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:53 +0000

    Add package holmium.core/0.5.0/holmium.core-0.5.0.tar.gz

---
+++ packages/holmium.core/holmium.core-0.5.0.tar.gz/holmium.core-0.5.0/holmium/core/__init__.py
@@ -0,0 +1,7 @@
+from .config import Config
+from .logger import log
+from .testcase import TestCase
+from .pageobject import Locators, Page, Section, Sections, Element, Elements
+from .pageobject import ElementMap
+from .noseplugin import HolmiumNose
+from .deprecated import PageObject, PageElement, PageElements, PageElementMap, HolmiumTestCase
