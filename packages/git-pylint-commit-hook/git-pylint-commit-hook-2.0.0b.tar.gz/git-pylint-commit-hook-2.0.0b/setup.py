c347620c902ae59cfe390bcd07e175bcfc42bc6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package git-pylint-commit-hook/2.0.0b/git-pylint-commit-hook-2.0.0b.tar.gz

---
+++ packages/git-pylint-commit-hook/git-pylint-commit-hook-2.0.0b.tar.gz/git-pylint-commit-hook-2.0.0b/setup.py
@@ -0,0 +1,32 @@
+""" Setup script for PyPI """
+import os
+from setuptools import setup
+from ConfigParser import SafeConfigParser
+
+settings = SafeConfigParser()
+settings.read(os.path.realpath('git_pylint_commit_hook/settings.conf'))
+
+
+setup(
+    name='git-pylint-commit-hook',
+    version=settings.get('general', 'version'),
+    license='Apache License, Version 2.0',
+    description='Git commit hook that checks Python files with pylint',
+    author='Sebastian Dahlgren',
+    author_email='sebastian.dahlgren@gmail.com',
+    url='http://www.sebastiandahlgren.se',
+    keywords="git commit pre-commit hook pylint python",
+    platforms=['Any'],
+    packages=['git_pylint_commit_hook'],
+    scripts=['git-pylint-commit-hook'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
