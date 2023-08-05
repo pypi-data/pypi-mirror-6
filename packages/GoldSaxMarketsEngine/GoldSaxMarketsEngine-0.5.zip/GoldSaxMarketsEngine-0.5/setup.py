ac68a3ff207b9ff9574b0c9b102a3929340a03b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package GoldSaxMarketsEngine/0.5/GoldSaxMarketsEngine-0.5.zip

---
+++ packages/GoldSaxMarketsEngine/GoldSaxMarketsEngine-0.5.zip/GoldSaxMarketsEngine-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+        name = 'GoldSaxMarketsEngine',
+        packages = ['GoldSaxMarketsEngine'], # this must be the same as the name above
+        version = '0.5',
+        description = 'A Capital Markets Engine for a Retail Investor, Instituitional Investor, Hedge Fund Managers, and Asset Managers to aggregate real-time data from numerous sources to SQLite Database.',
+        author = 'Vance King Saxbe. A',
+        author_email = 'vsaxbe@yahoo.com',
+        url = '',   # use the URL to the github repo
+        download_url = '', # I'll explain this in a second
+        keywords = ['stock quote', 'trading engine', 'markets'], # arbitrary keywords
+        classifiers = [],
+    )
