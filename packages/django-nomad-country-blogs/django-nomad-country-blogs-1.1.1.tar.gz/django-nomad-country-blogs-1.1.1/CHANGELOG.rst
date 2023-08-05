9c90390ef8acb75d5891f9341bf2989356b67d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:12 +0000

    Add package django-nomad-country-blogs/1.1.1/django-nomad-country-blogs-1.1.1.tar.gz

---
+++ packages/django-nomad-country-blogs/django-nomad-country-blogs-1.1.1.tar.gz/django-nomad-country-blogs-1.1.1/CHANGELOG.rst
@@ -0,0 +1,18 @@
+=========
+CHANGELOG
+=========
+
+v1.1.0
+======
+
+* Removed seo fields from Post model, as not mandatory for all projects.
+
+v1.1.0
+======
+
+* Add middleware and context processor to manage country code in session.
+
+v1.0.0
+======
+
+* Initial re-release
---
+++ packages/django-nomad-country-blogs/django-nomad-country-blogs-1.1.1.tar.gz/django-nomad-country-blogs-1.1.1/setup.py
@@ -0,0 +1,61 @@
+import os
+from distutils.core import setup
+
+package_dirs = ('nomadblog',)
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+for package_dir in package_dirs:
+    for dirpath, dirnames, filenames in os.walk(package_dir):
+        # Ignore dirnames that start with '.'
+        for i, dirname in enumerate(dirnames):
+            if dirname.startswith('.'):
+                del dirnames[i]
+        if '__init__.py' in filenames:
+            packages.append('.'.join(fullsplit(dirpath)))
+        elif filenames:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+version_num = __import__('nomadblog').__version__
+
+setup(
+    name='django-nomad-country-blogs',
+    version=version_num,
+    description='A minimalist Django blogging system to maintain country-specific blogs.',
+    long_description=open('README.rst').read(),
+    author='Hector Garcia',
+    author_email='hector@nomadblue.com',
+    url='https://github.com/nabucosound/django-nomad-country-blogs/',
+    download_url="https://github.com/Nomadblue/django-nomad-country-blogs/archive/v%s.zip" % version_num,
+    packages=packages,
+    package_data={'nomadblog': ['templates/nomadblog/*']},  # Include templates and statics here
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
