fd473c25ad05571e18c64c54343574602bd57054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package django-admin-tools-stats/0.5.3/django-admin-tools-stats-0.5.3.tar.gz

---
+++ packages/django-admin-tools-stats/django-admin-tools-stats-0.5.3.tar.gz/django-admin-tools-stats-0.5.3/admin_tools_stats/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+import jsonfield.fields
+
+
+class DashboardStatsCriteria(models.Model):
+    """To configure criteria for dashboard graphs
+
+    **Attributes**:
+
+        * ``criteria_name`` - Unique word .
+        * ``criteria_fix_mapping`` - JSON data key-value pairs.
+        * ``dynamic_criteria_field_name`` - Dynamic criteria field.
+        * ``criteria_dynamic_mapping`` - JSON data key-value pairs.
+        * ``created_date`` - record created date.
+        * ``updated_date`` - record updated date.
+
+    **Name of DB table**: dash_stats_criteria
+    """
+    criteria_name = models.CharField(max_length=90, db_index=True,
+        verbose_name=_('criteria name'),
+        help_text=_("it needs to be one word unique. Ex. status, yesno"))
+    criteria_fix_mapping = jsonfield.fields.JSONField(null=True, blank=True,
+        verbose_name=_("fixed criteria / value"),
+        help_text=_("a JSON dictionary of key-value pairs that will be used for the criteria"))
+    dynamic_criteria_field_name = models.CharField(max_length=90, blank=True, null=True,
+        verbose_name=_("dynamic criteria field name"),
+        help_text=_("ex. for call records - disposition"))
+    criteria_dynamic_mapping = jsonfield.fields.JSONField(null=True, blank=True,
+        verbose_name=_("dynamic criteria / value"),
+        help_text=_("a JSON dictionary of key-value pairs that will be used for the criteria"))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=_('date'))
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = u'dash_stats_criteria'
+        verbose_name = _("dashboard stats criteria")
+        verbose_name_plural = _("dashboard stats criteria")
+
+    def __unicode__(self):
+            return u"%s" % self.criteria_name
+
+
+class DashboardStats(models.Model):
+    """To configure graphs for dashboard
+
+    **Attributes**:
+
+        * ``graph_key`` - unique graph name.
+        * ``graph_title`` - graph title.
+        * ``model_app_name`` - App name of model.
+        * ``model_name`` - model name.
+        * ``date_field_name`` - Date field of model_name.
+        * ``criteria`` - many-to-many relationship.
+        * ``is_visible`` - enable/disable.
+        * ``created_date`` - record created date.
+        * ``updated_date`` - record updated date.
+
+    **Name of DB table**: dashboard_stats
+    """
+    graph_key = models.CharField(unique=True, max_length=90,
+                                 verbose_name=_('graph key'),
+                                 help_text=_("it needs to be one word unique. ex. auth, mygraph"))
+    graph_title = models.CharField(max_length=90, db_index=True,
+                                   verbose_name=_('graph title'),
+                                   help_text=_("heading title of graph box"))
+    model_app_name = models.CharField(max_length=90, verbose_name=_('app name'),
+                                      help_text=_("ex. auth / dialer_cdr"))
+    model_name = models.CharField(max_length=90, verbose_name=_('model name'),
+                                  help_text=_("ex. User"))
+    date_field_name = models.CharField(max_length=90, verbose_name=_("date field name"),
+                                       help_text=_("ex. date_joined"))
+    criteria = models.ManyToManyField(DashboardStatsCriteria, blank=True, null=True)
+    is_visible = models.BooleanField(default=True, verbose_name=_('visible'))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=_('date'))
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = u'dashboard_stats'
+        verbose_name = _("dashboard stats")
+        verbose_name_plural = _("dashboard stats")
+
+    def __unicode__(self):
+            return u"%s" % self.graph_key
