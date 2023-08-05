eaeee27362c596843598610bfe4525034a1c14c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:11 +0000

    Add package skcmd/0.0.6/skcmd-0.0.6.tar.gz

---
+++ packages/skcmd/skcmd-0.0.6.tar.gz/skcmd-0.0.6/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+
+from distutils.core import setup
+setup(name='skcmd',
+      version='0.0.6',
+      py_modules=['skcmd'],
+      description='Command line interface to Skype',
+long_description = file('README.md').read(),
+author='Les Smithson',
+      author_email='lsmithso@hare.demon.co.uk',
+      classifiers  = [
+	  'Development Status :: 5 - Production/Stable',
+	  'Environment :: Console',
+	  'Intended Audience :: End Users/Desktop',
+	  'Topic :: Adaptive Technologies',
+	  'Topic :: Communications :: Internet Phone',
+	  'Topic :: Communications :: Telephony',
+	  ],
+      url='http://www.github.com/lsmithso/skcmd',
+      scripts = ['skcmd.py'],
+      provides=['skcmd'],
+      requires = ['skype4py'],
+
+
+      )
