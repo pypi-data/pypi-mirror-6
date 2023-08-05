e5979ed4a714859ebc57b05cbb80903047c33898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package python-alfred/0.1.1/python-alfred-0.1.1.tar.gz

---
+++ packages/python-alfred/python-alfred-0.1.1.tar.gz/python-alfred-0.1.1/CHANGES.md
@@ -0,0 +1,2 @@
+v0.1.0,2013-12-09 -- initial release
+v0.1.1,2013-12-09 -- add changelist,readme
---
+++ packages/python-alfred/python-alfred-0.1.1.tar.gz/python-alfred-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='python-alfred',
+    version='0.1.1',
+    author='Zhao weiguo',
+    author_email='zhaoweiguo@haodou.com',
+    packages=['pyalfred'],
+    url='http://pypi.python.org/pypi/python-alfred',
+    license='LICENSE',
+    scripts=[],
+    description='Alfred 2 Workflow python module',
+    long_description=open('README.md').read()
+)
