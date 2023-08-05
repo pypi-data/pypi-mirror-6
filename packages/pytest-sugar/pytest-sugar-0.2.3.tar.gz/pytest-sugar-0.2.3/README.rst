93f03133d8f7d8396c6399bb3b748e878b5924be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:33 +0000

    Add package pytest-sugar/0.2.3/pytest-sugar-0.2.3.tar.gz

---
+++ packages/pytest-sugar/pytest-sugar-0.2.3.tar.gz/pytest-sugar-0.2.3/README.rst
@@ -0,0 +1,32 @@
+pytest-sugar
+================
+
+Please see http://pivotfinland.com/pytest-sugar/
+
+|build status|_
+
+.. |build status| image:: https://secure.travis-ci.org/Frozenball/pytest-sugar.png?branch=master
+   :alt: Build Status
+.. _build status: http://travis-ci.org/Frozenball/pytest-sugar
+
+pytest-sugar is a plugin for `py.test <http://pytest.org>`_ that shows
+failures and errors instantly and shows a progress bar.
+
+Requirements
+------------
+
+You will need the following prerequisites in order to use pytest-instafail:
+
+- Python 2.5, 2.6, 2.7, 3.2, 3.3 or PyPy
+- pytest 2.3 or newer
+
+Installation
+------------
+
+To install pytest-sugar::
+
+    $ pip install pytest-sugar
+
+Then run your tests with::
+
+    $ py.test
---
+++ packages/pytest-sugar/pytest-sugar-0.2.3.tar.gz/pytest-sugar-0.2.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+setup(
+    name='pytest-sugar',
+    description=' py.test plugin that adds instafail, ETA and neat graphics',
+    long_description=open("README.rst").read(),
+    version='0.2.3',
+    url='http://pivotfinland.com/pytest-sugar/',
+    license='BSD',
+    author='Teemu, Janne Vanhala',
+    author_email='orkkiolento@gmail.com, janne.vanhala@gmail.com',
+    py_modules=['pytest_sugar'],
+    entry_points={'pytest11': ['sugar = pytest_sugar']},
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['pytest>=2.3'],
+    use_2to3=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ]
+)
