cd29cfd82d213b1d83bb2bea668e32aac9fe9d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:16 +0000

    Add package simplencryption/1.0.0/simplencryption-1.0.0.tar.gz

---
+++ packages/simplencryption/simplencryption-1.0.0.tar.gz/simplencryption-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name        ='simplencryption',
+       version     ='1.0.0',
+       py_module   =['encryption'],
+       author      ='xiaozhen',
+       author_email='sajc.35sc.xiaozhen.she@gmail.com',
+       url         ='nil',
+       description ='this is a simple programe to secure your information by encrypting it',
+       )
