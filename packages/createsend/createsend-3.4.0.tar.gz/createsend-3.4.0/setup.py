e4c4540fa07e047017bab95cf7147d2360b2ad2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:04 +0000

    Add package createsend/3.4.0/createsend-3.4.0.tar.gz

---
+++ packages/createsend/createsend-3.4.0.tar.gz/createsend-3.4.0/setup.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from distutils.core import setup
+
+from createsend import __version__
+
+setup(name = "createsend",
+      version = __version__,
+      description = "A library which implements the complete functionality of the Campaign Monitor API.",
+      author = "James Dennes",
+      author_email = 'jdennes@gmail.com',
+      url = "http://campaignmonitor.github.io/createsend-python/",
+      license = "MIT",
+      keywords = "createsend campaign monitor email",
+      packages = ['createsend'],
+      package_data = {'createsend': ['cacert.pem']})
