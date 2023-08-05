67e1cdb9e2f825bb4c6965d3944ed2776beab33b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:18 +0000

    Add package django-backward/0.1/django-backward-0.1.tar.gz

---
+++ packages/django-backward/django-backward-0.1.tar.gz/django-backward-0.1/backward/tests/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, url, include
+
+from . import views
+
+
+urlpatterns = patterns(
+    '',
+
+    url(r'^simple/$',
+        views.simple,
+        name='simple'),
+
+    url(r'^login/simple/$',
+        views.login_simple,
+        name='login_simple'),
+
+    url(r'^auth/', include('django.contrib.auth.urls')),
+
+    url(r'^backward/', include('backward.urls')),
+)
