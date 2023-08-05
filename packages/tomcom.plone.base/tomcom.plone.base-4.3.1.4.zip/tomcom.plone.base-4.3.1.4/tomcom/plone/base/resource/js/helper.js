89a61609c2baaab0cb4e95c8d6347b4f412caeef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:28 +0000

    Add package tomcom.plone.base/4.3.1.4/tomcom.plone.base-4.3.1.4.zip

---
+++ packages/tomcom.plone.base/tomcom.plone.base-4.3.1.4.zip/tomcom.plone.base-4.3.1.4/tomcom/plone/base/resource/js/helper.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $('body').delegate( '.no-click', 'click', function(event) {
+        event.preventDefault();
+    });
+    if ($('.custom-overlay-ajax').length) {
+        $('.custom-overlay-ajax').prepOverlay({
+            subtype: 'ajax'
+        });
+    }
+    if ($('.custom-select2').length) {
+        if ($('.custom-select2')['select2'] != undefined) {
+            $('.custom-select2').select2();
+        }
+    }
+    if ($('.datepicker').length) {
+        $( '.datepicker' ).datepicker();
+    }
+
+    $( "body" ).delegate( ".int-only", "keyup", function(event) {
+        value_=$(event.target).attr('value');
+        value_=value_.replace(/[^\d]/g, "");
+        $(event.target).attr('value',value_);
+    });
+
+});
