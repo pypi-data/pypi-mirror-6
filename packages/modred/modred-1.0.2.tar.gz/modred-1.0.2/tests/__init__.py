d15fff0a57892fa5e6d72b6c23c90b4d29962eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package modred/1.0.2/modred-1.0.2.tar.gz

---
+++ packages/modred/modred-1.0.2.tar.gz/modred-1.0.2/tests/__init__.py
@@ -0,0 +1,8 @@
+""" Modules to load when using "from modred.tests import " """
+
+#__all__ = ['testbpod', 'tes', 'testdmd', 'testera', \
+#    'testvectorspace', 'testparallel', 'testpod', 'testutil', \
+#    'testscripts', 'testvecdefs', 'alltests']
+
+# Modules available from the top-level namespace.
+from alltests import *
