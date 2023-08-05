dfdcbadc350979c914cc93303dbc4d8b6f08040c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:10 +0000

    Add package snmposter/1.0.3/snmposter-1.0.3.tar.gz

---
+++ packages/snmposter/snmposter-1.0.3.tar.gz/snmposter-1.0.3/README.rst
@@ -0,0 +1,134 @@
+=============================================================================
+snmposter
+=============================================================================
+
+SNMP Agent Simulator
+
+This tool allows you to take the output of an snmpwalk command and then pretend
+to be the agent that it was gathered from. This can be useful when you're
+developing SNMP management tools.
+
+Requirements
+=============================================================================
+
+Twisted, TwistedSNMP and PySNMP-SE.
+
+Twisted is available from PyPI and will be automatically installed if you go
+the route of easy_install or pip. TwistedSNMP and PySNMP-SE are not currently
+available from PyPI and should be individually downloaded from sourceforge
+and installed from source.
+
+Installation
+=============================================================================
+
+I recommend installing *snmposter* within a Python virtualenv. This makes it
+easier to install on operating systems such as CentOS 5 where the default
+system Python version is older than 2.5. Due to the dependency on *Twisted*,
+snmposter requires Python 2.5 or newer.
+
+The following steps are specific to Red Hat Enterprise Linux 5 or one of its
+binary compatible distributions such as CentOS.
+
+1. Install the EPEL repository.
+
+   .. sourcecode:: bash
+
+      rpm -ivh http://mirror.cogentco.com/pub/linux/epel/5/i386/epel-release-5-4.noarch.rpm
+
+2. Install Python 2.6 and development tools.
+
+   .. sourcecode:: bash
+
+      yum -y --enablerepo=epel install python26-devel gcc
+
+2. Install, setup and activate virtualenv.
+
+   .. sourcecode:: bash
+
+      yum -y --enablerepo=epel install python26-virtualenv
+      virtualenv-2.6 /snmposter
+      source /snmposter/bin/activate
+
+3. Install TwistedSNMP dependency.
+
+   .. sourcecode:: bash
+
+      wget http://downloads.sourceforge.net/project/twistedsnmp/twistedsnmp/0.3.13/TwistedSNMP-0.3.13.tar.gz
+      tar -xzf TwistedSNMP-0.3.13.tar.gz
+      cd TwistedSNMP-0.3.13
+      python setup.py install
+      cd ..
+
+4. Install PySNMP-SE dependency.
+
+   .. sourcecode:: bash
+
+      wget http://downloads.sourceforge.net/project/twistedsnmp/pysnmp-se/3.5.2/pysnmp-se-3.5.2.tar.gz
+      tar -xzf pysnmp-se-3.5.2.tar.gz
+      cd pysnmp-se-3.5.2
+      python setup.py install
+      cd ..
+
+5. Install snmposter.
+
+   .. sourcecode:: bash
+
+      pip install snmposter
+
+
+Usage
+=============================================================================
+
+Installing will create a command line tool called `snmposter`. This tool
+requires root access because it listens on 161/udp and creates loopback aliases
+to support emulating multiple SNMP agents simultaneously.
+
+The `snmposter` command takes a single command line argument: -f or --file.
+The file passed to this option must contain one or more rows with two columns
+each. The first column should be the absolute or relative path to a file
+containing the output of an snmpwalk command. The second column should contain
+an IP address that this snmpwalk data will be exposed on.
+
+Example usage:
+
+.. sourcecode:: bash
+
+   source /snmposter/bin/activate
+   snmposter -f /etc/snmposter/agents.csv
+
+Example contents of `/etc/snmposter/agents.csv`::
+
+    /etc/snmposter/agents/Cisco_2811.snmpwalk,127.0.1.11
+    /etc/snmposter/agents/NetApp_Filer_FAS3020.snmpwalk,127.0.1.12
+
+This example usage will cause snmposter to run in the background, create two
+new IP aliases on the loopback interface (127.0.1.11 and 127.0.1.12), and
+expose the contents of each snmpwalk file as an SNMP agent on UDP port 161 of
+the appropriate IP address. If you're going to be using this frequently I
+would recommend adding some entries to your `/etc/hosts` file to make it even
+easier.
+
+Example additions to `/etc/hosts`::
+
+    127.0.1.11      cisco-2811
+    127.0.1.12      netapp-filer-fa3020
+
+
+**Important Note**: The snmpwalk output file that snmposter consumes must be
+generated with very specific snmpwalk command line options. These options allow
+snmposter to get the most raw data possible and provides the most accurate
+simulation.
+
+Example snmpwalk command to generate the above `Cisco_2811.snmpwalk` file:
+
+.. sourcecode:: bash
+
+   snmpwalk -v2c -c public -ObentU localhost .1 > Cisco_2811.snmpwalk
+
+The important command line options are `-m none -O enU` to get the raw output and '-C c' 
+to ignore out of sequence responses from the switch. (Sometimes this validation error is 
+triggered when walking routing MIBS on some switches)
+
+Don't worry if you get an error like `Cannot find module (none): At line 0 in
+(none)` as this is expected and a result of us trying to load a non-existent
+MIB.
---
+++ packages/snmposter/snmposter-1.0.3.tar.gz/snmposter-1.0.3/snmposter/snmposter.py
@@ -0,0 +1,221 @@
+#######################################################################
+#
+# Copyright (C) 2010, Chet Luther <chet.luther@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################
+
+from twisted.internet import reactor
+from twistedsnmp import agent, agentprotocol, bisectoidstore, datatypes
+from twistedsnmp.pysnmpproto import v2c, rfc1902
+
+import sys
+import os
+import re
+import csv
+
+
+# twistedsnmp has a bug that causes it to fail to properly convert
+# Counter64 values. We workaround this by retroactively fixing datatypes
+# mappings.
+fixed_v2Mapping = []
+for datatype, converter in datatypes.v2Mapping:
+    if datatype == v2c.Counter64:
+        fixed_v2Mapping.append(
+            (datatype, datatypes.SimpleConverter(v2c.Counter64)))
+    else:
+        fixed_v2Mapping.append((datatype, converter))
+
+datatypes.v2Mapping = fixed_v2Mapping
+
+fixed_v1Mapping = [(rfc1902.Counter64, datatypes.SimpleConverter(v2c.Counter64))]
+for datatype, converter in datatypes.v1Mapping:
+    if datatype != rfc1902.Counter64:
+        fixed_v1Mapping.append((datatype, converter))
+
+datatypes.v1Mapping = fixed_v1Mapping
+
+
+def sanitize_dotted(string):
+    '''
+    Return dotted decimal strings with non-numerics replaced with 1.
+
+    This is necessary because some snmpwalk output files have had IP
+    addresses obscured with non-numeric characters.
+    '''
+
+    return re.sub(r'[^ \.\da-fA-F]', '1', string)
+
+
+class SNMPosterFactory:
+    agents = []
+
+    def configure(self, filename):
+        reader = csv.reader(open(filename, "rb"))
+        for row in reader:
+            if row[0].startswith('#'):
+                continue
+
+            self.agents.append({
+                'filename': row[0],
+                'ip': row[1]})
+
+    def start(self):
+        for a in self.agents:
+            print "Starting %s on %s." % (a['filename'], a['ip'])
+            if os.uname()[0] == 'Darwin':
+                os.popen("ifconfig lo0 alias %s up" % (a['ip'],))
+            elif os.uname()[0] == 'Linux':
+                os.popen("/sbin/ip addr add %s dev lo" % (a['ip'],))
+            else:
+                print "WARNING: Unable to add loopback alias on this platform."
+
+            faker = SNMPoster(a['ip'], a['filename'])
+            faker.run()
+
+        daemonize()
+        reactor.run()
+
+
+class SNMPoster:
+    oidData = {}
+    sortedOids = []
+
+    def __init__(self, ip, filename):
+        self.ip = ip
+        self.oids = {}
+
+        oid = ''
+        type_ = ''
+        value = []
+
+        snmpwalk = open(filename, 'r')
+        for line in snmpwalk:
+            line = line.rstrip()
+
+            match = re.search(r'^([^ ]+) = ([^\:]+):\s*(.*)$', line)
+            if not match:
+                match = re.search(r'^([^ ]+) = (".*")$', line)
+
+            if match:
+                if len(value) > 0:
+                    self.add_oid_value(oid, type_, value)
+
+                    oid = ''
+                    type_ = ''
+                    value = []
+
+                groups = match.groups()
+                if len(groups) == 3:
+                    oid, type_, value1 = groups
+                else:
+                    oid, type_, value1 = (groups[0], 'STRING', groups[1])
+
+                oid = sanitize_dotted(oid)
+
+                if type_ == 'Timeticks':
+                    value1 = re.search(r'^\((\d+)\) .*$', value1).groups()[0]
+
+                value.append(value1.strip('"'))
+            else:
+                value.append(line.strip('"'))
+
+        snmpwalk.close()
+
+        if oid and type_:
+            self.add_oid_value(oid, type_, value)
+
+    def add_oid_value(self, oid, type_, value):
+        if type_ == 'Counter32':
+            self.oids[oid] = v2c.Counter32(self.tryIntConvert(value[0]))
+
+        elif type_ == 'Counter64':
+            self.oids[oid] = rfc1902.Counter64(long(value[0]))
+
+        elif type_ == 'Gauge32':
+            self.oids[oid] = v2c.Gauge32(self.tryIntConvert(value[0]))
+
+        elif type_ == 'Hex-STRING':
+            value = [sanitize_dotted(x) for x in value]
+            self.oids[oid] = ''.join(
+                [chr(int(c, 16)) for c in ' '.join(value).split(' ')])
+
+        elif type_ == 'INTEGER':
+            self.oids[oid] = self.tryIntConvert(value[0])
+
+        elif type_ == 'IpAddress':
+            value[0] = sanitize_dotted(value[0])
+            self.oids[oid] = v2c.IpAddress(value[0])
+
+        elif type_ == 'OID':
+            self.oids[oid] = v2c.ObjectIdentifier(value[0])
+
+        elif type_ == 'STRING':
+            self.oids[oid] = '\n'.join(value)
+
+        elif type_ == 'Timeticks':
+            self.oids[oid] = v2c.TimeTicks(int(value[0]))
+
+    def tryIntConvert(self, myint):
+        conv = -1
+        try:
+            conv = int(myint)
+        except:
+            m = re.match(".*\((?P<myint>\d+)\).*|(?P<myint2>\d+).*", myint)
+            if m:
+                myint2 = m.groupdict()["myint"] or m.groupdict()["myint2"]
+                try:
+                    conv = int(myint2)
+                except:
+                    pass
+        return conv
+
+    def start(self):
+        reactor.listenUDP(
+            161, agentprotocol.AgentProtocol(
+                snmpVersion='v2c',
+                agent=agent.Agent(
+                    dataStore=bisectoidstore.BisectOIDStore(
+                        OIDs=self.oids,
+                        ),
+                    ),
+                ),
+                interface=self.ip,
+            )
+
+    def run(self):
+        reactor.callWhenRunning(self.start)
+
+
+def daemonize():
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)
+    except OSError, e:
+        print >>sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror)
+        sys.exit(1)
+
+    os.chdir("/")
+    os.setsid()
+    os.umask(0)
+
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)
+    except OSError, e:
+        print >>sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror)
+        sys.exit(1)
