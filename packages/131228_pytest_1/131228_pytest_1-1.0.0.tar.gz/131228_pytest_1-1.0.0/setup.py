192925a3c2f35ba32b093544f8f3a7bd66c9ae11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:43 +0000

    Add package 131228_pytest_1/1.0.0/131228_pytest_1-1.0.0.tar.gz

---
+++ packages/131228_pytest_1/131228_pytest_1-1.0.0.tar.gz/131228_pytest_1-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='131228_pytest_1',
+    version='1.0.0',
+    packages=[''],
+    url='none',
+    license='none',
+    author='smith9',
+    author_email='smith9@ms22.hinet.net',
+    description='A easy printer of nested list'
+)
