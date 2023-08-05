7595650d854b8fb8a941bf25dd4eba1b89163c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:29 +0000

    Add package dblogger/0.3.1.dev10/dblogger-0.3.1.dev10-py2.7.egg

---
+++ packages/dblogger/dblogger-0.3.1.dev10-py2.7.egg/dblogger/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from dblogger.configure import configure_logging
+from dblogger.logger import *
+from dblogger.query import *
+from dblogger.format import *
+
