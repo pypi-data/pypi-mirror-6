609ac3cb2cc2b790c47d977e7c74f9c226be4a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:35 +0000

    Add package rui/0.3.6/rui-0.3.6.tar.gz

---
+++ packages/rui/rui-0.3.6.tar.gz/rui-0.3.6/rui/exceptions.py
@@ -0,0 +1,67 @@
+class DuplicateEntityError(Exception):
+    def __init__(self, entity):
+        self.entity = entity
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '{0} already exists in world'.format(self.entity)
+
+
+class DuplicateSystemError(Exception):
+    def __init__(self, system):
+        self.system = system
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '{0} already exists in world'.format(self.system)
+
+
+class UnmanagedEntityError(Exception):
+    def __init__(self, entity):
+        self.entity = entity
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '''{0} you were trying to use in world has not been
+                added to world'''.format(self.entity)
+
+
+class UnmanagedSystemError(Exception):
+    def __init__(self, system):
+        self.system = system
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '''{0} you were trying to use in world has not been
+                added to world'''.format(self.system)
+
+
+class NonUniqueTagError(Exception):
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return '''Tag {0} you were trying to use in world has already been
+                added to world'''.format(self.tag)
+
+
+class DeadEntityError(Exception):
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return 'Dead entity cannot be used'
