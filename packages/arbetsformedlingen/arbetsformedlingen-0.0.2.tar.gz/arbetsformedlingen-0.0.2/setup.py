f7cc8b96eada812c2540cc1e69948b84dc707fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:43 +0000

    Add package arbetsformedlingen/0.0.2/arbetsformedlingen-0.0.2.tar.gz

---
+++ packages/arbetsformedlingen/arbetsformedlingen-0.0.2.tar.gz/arbetsformedlingen-0.0.2/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name='arbetsformedlingen',
+  version='0.0.2',
+  author='Petter Nyman',
+  author_email='petter.nyman@willandskill.se',
+  packages=find_packages(),
+  install_requires = [
+    'requests'
+  ],
+  url='https://github.com/willandskill/arbetsformedlingen',
+  download_url='https://github.com/willandskill/arbetsformedlingen/archive/v0.0.1',
+  license='BSD licence',
+  description='Wrapper for http://api.arbetsformedlingen.se/',
+)
