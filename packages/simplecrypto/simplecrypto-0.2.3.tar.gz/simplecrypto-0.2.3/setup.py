8e346849fb40c6655f37662137b6732f8e6e5541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:04 +0000

    Add package simplecrypto/0.2.3/simplecrypto-0.2.3.tar.gz

---
+++ packages/simplecrypto/simplecrypto-0.2.3.tar.gz/simplecrypto-0.2.3/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='simplecrypto',
+    version=open('CHANGES.txt').read().split()[0],
+    author='Lucas Boppre Niehues',
+    author_email='lucasboppre@gmail.com',
+    packages=['simplecrypto'],
+    url='https://github.com/boppreh/simplecrypto',
+    license='MIT',
+    description='Simple cryptographic library for hashing and encrypting',
+    keywords = 'simple cryptography symmetric asymmetric hash encrypt decrypt rsa aes sha md5',
+    long_description=open('README.rst').read(),
+
+    install_requires=[
+        'PyCrypto',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
