5d302e3379e6ebecf6a7d7260ceeb444f0f9686d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package FS-Hopper/0.1.0/FS-Hopper-0.1.0.tar.gz

---
+++ packages/FS-Hopper/FS-Hopper-0.1.0.tar.gz/FS-Hopper-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+name='FS-Hopper',
+version='0.1.0',
+author='Brian Wiborg',
+author_email='baccenfutter@c-base.org',
+packages=['fs_hopper'],
+scripts=[],
+url='http://pypi.python.org/pypi/FS-Hopper/',
+license='LICENSE.txt',
+description='Simple object-oriented access to a filesystem directory tree',
+long_description=open('README.txt').read(),
+install_requires=['python-ldap >= 2.4.13'],
+)
+
