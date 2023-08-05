a370447a7617b5fe2ced45b7f81ad4f498e08471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:25 +0000

    Add package concepts/0.1.2/concepts-0.1.2.zip

---
+++ packages/concepts/concepts-0.1.2.zip/concepts-0.1.2/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+from distutils.core import setup
+
+setup(
+    name='concepts',
+    version='0.1.2',
+    url='http://pypi.python.org/pypi/concepts',
+    license='MIT',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Formal Concept Analysis with Python',
+    install_requires=['bitsets==0.1.2'],
+    long_description=open('README.txt').read(),
+    packages=['concepts'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
