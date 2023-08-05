3dddfc11cc862169503ff500f8ac3c28f8c9fcdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:28 +0000

    Add package django-generic-positions/0.1.5/django-generic-positions-0.1.5.tar.gz

---
+++ packages/django-generic-positions/django-generic-positions-0.1.5.tar.gz/django-generic-positions-0.1.5/generic_positions/admin.py
@@ -0,0 +1,28 @@
+"""Admin classes for the ``generic_positions`` app."""
+from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+
+from .models import ObjectPosition
+
+
+class GenericPositionsAdmin(admin.ModelAdmin):
+    """Admin to let models be dragged & dropped with jQuery UI sortable."""
+    change_list_template = 'generic_positions/admin/change_list.html'
+
+    class Media:
+        js = (
+            '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'generic_positions/js/reorder.js',
+        )
+
+    def save_model(self, request, obj, form, change):
+        """Add an ObjectPosition to the object."""
+        super(GenericPositionsAdmin, self).save_model(request, obj, form,
+                                                      change)
+        c_type = ContentType.objects.get_for_model(obj)
+        try:
+            ObjectPosition.objects.get(content_type__pk=c_type.id,
+                                       object_id=obj.id)
+        except ObjectPosition.DoesNotExist:
+            ObjectPosition.objects.create(content_object=obj)
