89c9cb882b300c8f59e0cb55c0c61f20af79f032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:49 +0000

    Add package ecog/0.7.25/ecog-0.7.25.tar.gz

---
+++ packages/ecog/ecog-0.7.25.tar.gz/ecog-0.7.25/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+    Ecogwiki OAuth client
+
+    `ecog` is a python client that talks with [ecogwiki](http://www.ecogwiki.com/). It is configurable to talk with any other ecogwiki hosts.
+
+
+"""
+
+from setuptools import setup
+
+setup(name='ecog',
+    version='0.7.25',
+    author = 'Jang-hwan Kim',
+    author_email = 'janghwan@gmail.com',
+    description = 'Ecogwiki OAuth client',
+    long_description = __doc__,
+    url = 'https://github.com/jangxyz/ecogwiki-client',
+
+    py_modules = ['ecog'],
+    scripts = ['ecog'],
+    install_requires = ['oauth2', 'feedparser', 'python-dateutil'],
+
+    license = 'MIT License',
+    platforms = ['POSIX'],
+    keywords = ['oauth', 'markdown'],
+    classifiers = [line.strip() for line in '''
+        Development Status :: 3 - Alpha
+        Environment :: Console
+        Intended Audience :: Developers
+        Intended Audience :: End Users/Desktop
+        License :: OSI Approved :: MIT License
+        Natural Language :: English
+        Operating System :: POSIX
+        Programming Language :: Python :: 2.7
+        Topic :: Communications
+        Topic :: Terminals
+        Topic :: Text Processing
+        Topic :: Utilities
+    '''.strip().splitlines()]
+)
+
