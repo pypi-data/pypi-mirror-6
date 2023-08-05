3e9e98432a32ce9318539da24387c7da08989130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:49 +0000

    Add package sure/1.2.5/sure-1.2.5.tar.gz

---
+++ packages/sure/sure-1.2.5.tar.gz/sure-1.2.5/setup.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# <sure - utility belt for automated testing in python>
+# Copyright (C) <2010-2013>  Gabriel Falcão <gabriel@nacaolivre.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import ast
+import os
+import re
+from setuptools import setup, find_packages
+
+
+class VersionFinder(ast.NodeVisitor):
+
+    def __init__(self):
+        self.version = None
+
+    def visit_Assign(self, node):
+        try:
+            if node.targets[0].id == 'version':
+                self.version = node.value.s
+        except:
+            pass
+
+
+def read_version():
+    """Read version from sure/version.py without loading any files"""
+    finder = VersionFinder()
+    finder.visit(ast.parse(local_file('sure', '__init__.py')))
+    return finder.version
+
+
+def parse_requirements(path):
+    """Rudimentary parser for the `requirements.txt` file
+
+    We just want to separate regular packages from links to pass them to the
+    `install_requires` and `dependency_links` params of the `setup()`
+    function properly.
+    """
+    try:
+        requirements = map(str.strip, local_file(path).splitlines())
+    except IOError:
+        raise RuntimeError("Couldn't find the `requirements.txt' file :(")
+
+    links = []
+    pkgs = []
+    for req in requirements:
+        if not req:
+            continue
+        if 'http:' in req or 'https:' in req:
+            links.append(req)
+            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
+            pkgs.append('{0}=={1}'.format(name, version))
+        else:
+            pkgs.append(req)
+
+    return pkgs, links
+
+
+local_file = lambda *f: \
+    open(os.path.join(os.path.dirname(__file__), *f)).read()
+
+
+install_requires, dependency_links = \
+    parse_requirements('requirements.txt')
+
+
+if __name__ == '__main__':
+    setup(name='sure',
+          version=read_version(),
+          description='utility belt for automated testing in python for python',
+          author='Gabriel Falcao',
+          long_description=local_file('README.rst'),
+          author_email='gabriel@nacaolivre.org',
+          include_package_data=True,
+          url='http://github.com/gabrielfalcao/sure',
+          packages=find_packages(exclude=['*tests*']),
+          install_requires=install_requires,
+          dependency_links=dependency_links,
+    )
