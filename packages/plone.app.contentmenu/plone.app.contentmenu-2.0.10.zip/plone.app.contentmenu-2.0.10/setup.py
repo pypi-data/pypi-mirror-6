e1491ebd76cfc3bbc6830d2c1281d6ca749c40dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package plone.app.contentmenu/2.0.10/plone.app.contentmenu-2.0.10.zip

---
+++ packages/plone.app.contentmenu/plone.app.contentmenu-2.0.10.zip/plone.app.contentmenu-2.0.10/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+version = '2.0.10'
+
+setup(name='plone.app.contentmenu',
+      version=version,
+      description="Plone's content menu implementation",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='plone contentmenu menu',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.app.contentmenu',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone', 'plone.app'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+          'Products.PloneTestCase',
+          ]
+        ),
+      install_requires=[
+        'setuptools',
+        'plone.locking',
+        'plone.memoize',
+        'plone.app.content >=2.0a3',
+        'zope.browsermenu',
+        'zope.component',
+        'zope.contentprovider',
+        'zope.interface',
+        'zope.i18n',
+        'zope.i18nmessageid',
+        'zope.publisher',
+        'Acquisition',
+        'Products.CMFCore',
+        'Products.CMFDynamicViewFTI',
+        'Products.CMFPlone',
+        'Zope2',
+      ],
+      )
