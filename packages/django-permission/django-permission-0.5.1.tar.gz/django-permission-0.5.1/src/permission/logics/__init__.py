a368f09e668c2ec9fcf60ecd6594a6ac5915d55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:54 +0000

    Add package django-permission/0.5.1/django-permission-0.5.1.tar.gz

---
+++ packages/django-permission/django-permission-0.5.1.tar.gz/django-permission-0.5.1/src/permission/logics/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""
+Permission logic module
+"""
+__author__ = 'Alisue <lambdalisue@hashnote.net>'
+from permission.logics.base import PermissionLogic
+from permission.logics.author import AuthorPermissionLogic
+from permission.logics.collaborators import CollaboratorsPermissionLogic
