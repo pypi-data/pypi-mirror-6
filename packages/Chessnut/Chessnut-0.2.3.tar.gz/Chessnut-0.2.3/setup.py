f72c61341c9ac42b384307c92606ef6007c875c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package Chessnut/0.2.3/Chessnut-0.2.3.tar.gz

---
+++ packages/Chessnut/Chessnut-0.2.3.tar.gz/Chessnut-0.2.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Chessnut',
+    version='0.2.3',  # TODO: https://pythonhosted.org/setuptools/setuptools.html#id7
+    packages=['Chessnut'],
+
+    author="Chris Gearhart",
+    author_email="chris@gearley.com",
+    description="A basic chess model to imports/export FEN & finds moves.",
+    license="Unlicense",
+    keywords="chess",
+    url="https://github.com/cgearhart/Chessnut",
+)
