86f40bcb774d7a8653378a08b0808123b295fe5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:58 +0000

    Add package django-bakery/0.3.0/django-bakery-0.3.0.tar.gz

---
+++ packages/django-bakery/django-bakery-0.3.0.tar.gz/django-bakery-0.3.0/bakery/management/commands/buildserver.py
@@ -0,0 +1,19 @@
+from django.conf import settings, urls
+from django.conf.urls import patterns, url
+from django.core.management.commands import runserver
+
+
+class Command(runserver.Command):
+    help = "Starts a variation of Django's runserver designed to serve \
+the static files you've built."
+
+    def handle(self, *args, **kwds):
+        urls.urlpatterns = patterns(
+            "bakery.static_views",
+            url(r"^(.*)$", "serve", {
+                "document_root": settings.BUILD_DIR,
+                'show_indexes': True,
+                'default': 'index.html'
+                }),
+        )
+        runserver.Command.handle(self, *args, **kwds)
