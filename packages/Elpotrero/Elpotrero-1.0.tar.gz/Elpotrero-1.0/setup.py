797393312b9cb15207c1720ad2d62040e90ec81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:02 +0000

    Add package Elpotrero/1.0/Elpotrero-1.0.tar.gz

---
+++ packages/Elpotrero/Elpotrero-1.0.tar.gz/Elpotrero-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Elpotrero',
+    version='1.0',
+    packages=['elpotrero',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
