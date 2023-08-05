7952c53502d5a8d8e6905f672c719e4aa63c11ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:54 +0000

    Add package django-ptree/0.2.13/django-ptree-0.2.13.zip

---
+++ packages/django-ptree/django-ptree-0.2.13.zip/django-ptree-0.2.13/ptree/common.py
@@ -0,0 +1,59 @@
+import babel.numbers
+from django.conf import settings
+from decimal import Decimal
+import urllib
+import urlparse
+from django.utils.importlib import import_module
+import subprocess
+
+def add_params_to_url(url, params):
+    url_parts = list(urlparse.urlparse(url))
+    query = dict(urlparse.parse_qsl(url_parts[4]))
+    query.update(params)
+    url_parts[4] = urllib.urlencode(query)
+    return urlparse.urlunparse(url_parts)
+
+def id_label_name(id, label):
+    if label:
+        return '{} (label: {})'.format(id, label)
+    return '{}'.format(id)
+
+def currency(value):
+    """Takes in a number of cents (int) and returns a formatted currency amount.
+    """
+
+    if value == None:
+        return '?'
+    value_in_major_units = Decimal(value)/(10**settings.CURRENCY_DECIMAL_PLACES)
+    return babel.numbers.format_currency(value_in_major_units, settings.CURRENCY_CODE, locale=settings.CURRENCY_LOCALE)
+
+def create_match(MatchClass, treatment):
+    match = MatchClass(treatment = treatment,
+                       experiment = treatment.experiment,
+                       session = treatment.session)
+    # need to save it before you assign the participant.match ForeignKey
+    match.save()
+    return match
+
+def add_participant_to_existing_match(participant, match):
+    participant.index_among_participants_in_match = match.participants().count()
+    participant.match = match
+
+def assign_participant_to_match(MatchClass, participant):
+    if not participant.match:
+        match = participant.treatment.next_open_match() or create_match(MatchClass, participant.treatment)
+        add_participant_to_existing_match(participant, match)
+
+def is_experiment_app(app_label):
+    try:
+        models_module = import_module('{}.models'.format(app_label))
+    except ImportError:
+        return False
+    class_names = ['Participant', 'Match', 'Treatment', 'Experiment']
+    return all(hasattr(models_module, ClassName) for ClassName in class_names)
+
+def git_hash():
+    try:
+        return subprocess.check_output(['git','rev-parse','HEAD'])
+    except:
+        return None
---
+++ packages/django-ptree/django-ptree-0.2.13.zip/django-ptree-0.2.13/ptree/templatetags/ptree_dashboard.py
@@ -0,0 +1,44 @@
+from django.template import Library
+from ptree import common
+
+
+register = Library()
+
+
+EXPERIMENT_APP_MODELS_ORDER = ["Experiment", "Treatment", "Match", "Participant"]
+
+
+def _get_model_position(object_name):
+    try:
+        position = EXPERIMENT_APP_MODELS_ORDER.index(object_name)
+    except ValueError:
+        position = -1
+    return position
+
+
+def fix_experiment_app_models_order(models):
+    models = [model for model in models]
+    models.sort(lambda a, b: cmp(_get_model_position(a["object_name"]), _get_model_position(b["object_name"])))
+    return models
+
+
+def is_experiment_app(app):
+    return common.is_experiment_app(app["app_label"])
+
+
+def experiment_apps_only(apps):
+    return [app for app in apps if common.is_experiment_app(app["app_label"])]
+
+
+def non_experiment_apps(apps):
+    return [app for app in apps if not common.is_experiment_app(app["app_label"])]
+
+
+def app_name_format(app_name):
+    return app_name.replace("_", " ")
+
+
+register.filter('fix_experiment_app_models_order', fix_experiment_app_models_order)
+register.filter('experiment_apps_only', experiment_apps_only)
+register.filter('non_experiment_apps', non_experiment_apps)
+register.filter('app_name_format', app_name_format)
