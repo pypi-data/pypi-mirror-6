d92f1aa4200c02ca4aabb4a7cd6d0903a2c04076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:45 +0000

    Add package ordrin/1.0.2/ordrin-1.0.2.tar.gz

---
+++ packages/ordrin/ordrin-1.0.2.tar.gz/ordrin-1.0.2/setup.py
@@ -0,0 +1,34 @@
+import distribute_setup
+distribute_setup.use_setuptools()
+from setuptools import setup
+
+import sys
+if not (sys.version_info.major == 2 and sys.version_info.minor == 7):
+    print "This package only works with Python 2.7"
+    exit()
+
+setup(
+    name="ordrin",
+    version='1.0.2',
+    packages=['ordrin'],
+    package_dir={'ordrin':'ordrin'},
+    py_modules=['distribute_setup'],
+    description="Ordr.in API Client",
+    author="Ordr.in",
+    author_email="tech@ordr.in",
+    url="https://github.com/ordrin/api-py",
+    install_requires=['requests', 'jsonschema'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development",
+        "Topic :: Internet"
+    ],
+    package_data = {
+        'ordrin': ['*.json']
+    }
+)
