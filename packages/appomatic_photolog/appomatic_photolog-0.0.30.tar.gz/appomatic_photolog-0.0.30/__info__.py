ae674cfcb58062e2d373ef941d11760ceea7ca0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package appomatic_photolog/0.0.30/appomatic_photolog-0.0.30.tar.gz

---
+++ packages/appomatic_photolog/appomatic_photolog-0.0.30.tar.gz/appomatic_photolog-0.0.30/__info__.py
@@ -0,0 +1,10 @@
+version = "0.0.30"
+author = "RedHog (Egil Moeller)"
+author_email = "egil.moller@freecode.no"
+license = "GPL"
+url = "http://github.com/redhog/appomatic"
+name = "appomatic_photolog"
+description = "Photologue is a reusable Django application that provides powerful image management and manipulation functionality as well as a complete photo gallery solution."
+keywords = "appomatic photologue"
+url = "http://code.google.com/p/django-photologue/"
+install_requires = ['django-photologue']
