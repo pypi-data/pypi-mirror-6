81585bfffa4154739e6dae4eea9ac3d63ef3eba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:04 +0000

    Add package pyisbn/1.0.0/pyisbn-1.0.0.tar.bz2

---
+++ packages/pyisbn/pyisbn-1.0.0.tar.bz2/pyisbn-1.0.0/setup.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+# coding=utf-8
+"""setup.py - Setuptools tasks and config for pyisbn"""
+# Copyright © 2007-2013  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+try:
+    from email.utils import parseaddr
+except ImportError:  # Python2.4
+    from email.Utils import parseaddr  # NOQA
+
+from setuptools import setup
+
+import pyisbn
+
+
+author, author_email = parseaddr(pyisbn.__author__)
+
+paras = pyisbn.__doc__.split('\n\n')
+long_description = "\n\n".join([paras[1], paras[3]])
+
+setup(
+    name='pyisbn',
+    version=pyisbn.__version__,
+    description=pyisbn.__doc__.splitlines()[0][:-1],
+    long_description=long_description,
+    author=author,
+    author_email=author_email,
+    maintainer=author,
+    maintainer_email=author_email,
+    url="https://github.com/JNRowe/pyisbn/",
+    packages=['pyisbn', ],
+    license=pyisbn.__license__,
+    keywords='ISBN ISBN-10 ISBN-13 SBN',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Other/Nonlisted Topic',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+    ],
+)
---
+++ packages/pyisbn/pyisbn-1.0.0.tar.bz2/pyisbn-1.0.0/tests/test_isbn.py
@@ -0,0 +1,114 @@
+#
+# coding=utf-8
+"""test_isbn - Test Isbn class"""
+# Copyright © 2007-2013  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from unittest import (TestCase, skipIf)
+from sys import version_info
+
+from expecter import expect
+from nose2.tools import params
+
+from pyisbn import (CountryError, Isbn, SiteError)
+
+
+class TestIsbn(TestCase):
+    @params(
+        '9780521871723',
+        '3540009787',
+    )
+    def test___repr__(self, isbn):
+        expect(repr(Isbn(isbn))) == 'Isbn(%r)' % isbn
+
+    @params(
+        '9780521871723',
+        '978-052-187-1723',
+        '3540009787',
+    )
+    def test___str__(self, isbn):
+        expect(str(Isbn(isbn))) == 'ISBN %s' % isbn
+
+    @skipIf(version_info < (2, 6),
+            "format() not supported with this Python version")
+    @params(
+        ('9780521871723', '', 'ISBN 9780521871723'),
+        ('978-052-187-1723', 'urn', 'URN:ISBN:978-052-187-1723'),
+        ('3540009787', 'url',
+         'http://www.amazon.com/s?search-alias=stripbooks&field-isbn='
+         '3540009787'),
+        ('3540009787', 'url:amazon:uk',
+         'http://www.amazon.co.uk/s?search-alias=stripbooks&field-isbn='
+         '3540009787'),
+    )
+    def test___format__(self, isbn, format_spec, result):
+        expect(format(Isbn(isbn), format_spec)) == result
+
+    @params(
+        ('978-052-187-1723', '3'),
+        ('3540009787', '7'),
+        ('354000978', '7'),
+    )
+    def test_calculate_checksum(self, isbn, result):
+        expect(Isbn(isbn).calculate_checksum()) == result
+
+    @params(
+        ('0071148167', '9780071148160'),
+        ('9780071148160', '0071148167'),
+    )
+    def test_convert(self, isbn, result):
+        expect(Isbn(isbn).convert()) == result
+
+    @params(
+        ('978-052-187-1723', True),
+        ('978-052-187-1720', False),
+        ('3540009787', True),
+        ('354000978x', False),
+    )
+    def test_validate(self, isbn, result):
+        expect(Isbn(isbn).validate()) == result
+
+    @params(
+        ('us', 'http://www.amazon.com/s?search-alias=stripbooks&field-isbn=0071148167'),
+        ('uk', 'http://www.amazon.co.uk/s?search-alias=stripbooks&field-isbn=0071148167'),
+        ('de', 'http://www.amazon.de/s?search-alias=stripbooks&field-isbn=0071148167'),
+    )
+    def test_to_url(self, country, result):
+        expect(Isbn('0071148167').to_url(country=country)) == result
+
+    def test_to_url_invalid_country(self):
+        with expect.raises(CountryError, "zh"):
+            Isbn('0071148167').to_url(country='zh')
+
+    @params(
+        ('copac', 'http://copac.ac.uk/search?isn=0071148167'),
+        ('google', 'http://books.google.com/books?vid=isbn:0071148167'),
+        ('isbndb', 'http://isbndb.com/search/all?query=0071148167'),
+        ('worldcat', 'http://worldcat.org/isbn/0071148167'),
+        ('waterstones',
+         'http://www.waterstones.com/waterstonesweb/advancedSearch.do?buttonClicked=2&isbn=0071148167'),
+        ('whsmith',
+         'http://www.whsmith.co.uk/CatalogAndSearch/SearchWithinCategory.aspx?as_ISBN=0071148167'),
+    )
+    def test_to_url_site(self, site, result):
+        expect(Isbn('0071148167').to_url(site=site)) == result
+
+    def test_to_url_invalid_site(self):
+        with expect.raises(SiteError, "nosite"):
+            Isbn('0071148167').to_url(site='nosite')
+
+    def test_to_urn(self):
+        expect(Isbn('0071148167').to_urn()) == 'URN:ISBN:0071148167'
