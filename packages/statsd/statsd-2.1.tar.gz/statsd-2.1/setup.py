07de41d0d7ad203116e822d9cf49a33be2ba4abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package statsd/2.1/statsd-2.1.tar.gz

---
+++ packages/statsd/statsd-2.1.tar.gz/statsd-2.1/setup.py
@@ -0,0 +1,33 @@
+import os
+import re
+from setuptools import find_packages, setup
+
+
+setup(
+    name='statsd',
+    version='2.1',
+    description='A simple statsd client.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='james@mozilla.com',
+    url='https://github.com/jsocol/pystatsd',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
