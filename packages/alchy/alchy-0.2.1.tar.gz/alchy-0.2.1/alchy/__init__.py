255558d1198a4be113bd4acb5575466f55751d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:04 +0000

    Add package alchy/0.2.1/alchy-0.2.1.tar.gz

---
+++ packages/alchy/alchy-0.2.1.tar.gz/alchy-0.2.1/alchy/__init__.py
@@ -0,0 +1,10 @@
+
+from .model import ModelBase, make_declarative_base
+from .query import Query
+from .manager import Manager, ManagerBase
+
+__version__ = '0.2.1'
+__author__ = 'Derrick Gilland <dgilland@gmail.com>'
+__all__ = ['ModelBase', 'make_declarative_base', 'Query', 'Manager', 'ManagerBase']
+
+print locals()
