b90b21d80c1005136866c0345bc0fd9cd705f200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:39 +0000

    Add package pysummer/1.0/pysummer-1.0.tar.gz

---
+++ packages/pysummer/pysummer-1.0.tar.gz/pysummer-1.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysummer',
+      version='1.0',
+      description='Compute & verify hashes : MD5 and SHA family',
+      author='Stephane JEANNENOT',
+      author_email='stephane.jeannenot@gmail.com',
+      maintainer='Juno',
+      maintainer_email='juno.devel@gmail.com',
+      url='',
+      py_modules=['pysummer'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          ],
+     )
