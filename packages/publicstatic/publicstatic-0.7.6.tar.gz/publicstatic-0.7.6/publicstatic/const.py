e04cd7d2a5f4f4bd33db76ad4597e20a7544f811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:26 +0000

    Add package publicstatic/0.7.6/publicstatic-0.7.6.tar.gz

---
+++ packages/publicstatic/publicstatic-0.7.6.tar.gz/publicstatic-0.7.6/publicstatic/const.py
@@ -0,0 +1,316 @@
+# coding: utf-8
+
+# Site configuration file name
+CONF_NAME = 'conf.yaml'
+
+# Configuration file header
+CONF_HEADER = 'public-static configuration file'
+
+# standard name for template content block
+CONTENT_BLOCK = 'content'
+
+# default image number limitation for 'image ls' command output
+LS_NUM = 10
+
+# environment variable name to override 'verbose' configuration parameter
+ENV_VERBOSE = 'ps_verbose'
+
+# Program name and version info
+GENERATOR = 'public-static'
+
+# Program home page
+GENERATOR_URL = 'http://github.com/dreikanter/public-static'
+
+# Logger message timestamp format
+LOG_DATE_FORMAT = "%Y/%m/%d %H:%M:%S"
+
+# Logger message format
+LOG_FORMAT = "%(asctime)s %(levelname)s: %(message)s"
+
+# program name for CLI
+PROG = 'pub'
+
+# prototype for the site source directory, located within the package directory
+PROTO_DIR = 'prototype'
+
+# page prototype
+PROTO_PAGE = """title: {title}
+created: {created}
+
+# {title}
+
+This is a new page."""
+
+# blog post prototype
+PROTO_POST = """title: {title}
+created: {created}
+tags: unsorted
+
+This is a new blog post."""
+
+# sitemap file name
+SITEMAP = 'sitemap.xml'
+
+# default post name
+UNTITLED_POST = 'untitled-post'
+
+# parameters sequence for the configuration file
+EXPORTS = [
+    'author',
+    'author_url',
+    'default_tags',
+    'default_templates',
+    'deploy_cmd',
+    'id_disqus',
+    'editor_cmd',
+    'enable_search_form',
+    'id_addthis',
+    'id_google_analytics',
+    'image_max_height',
+    'image_max_width',
+    'images_location',
+    'images_path',
+    'less_cmd',
+    'menu',
+    'min_css',
+    'min_css_cmd',
+    'min_html',
+    'min_js',
+    'min_js_cmd',
+    'pluso_enabled',
+    'port',
+    'post_location',
+    'root_url',
+    'source_url',
+    'subtitle',
+    'title',
+    'verbose',
+]
+
+# configuration parameters
+DEFAULTS = {
+    'archive_location': {
+        'value': 'archive/index.html',
+        'desc': 'Blog archive page location',
+    },
+    'assets_path': {
+        'value': 'assets',
+        'desc': 'Web assets path inside website source dir (*.js, *.css, etc)',
+    },
+    'atom_location': {
+        'value': 'atom.xml',
+        'desc': 'Atom feed file name',
+    },
+    'author': {
+        'value': 'Anonymous',
+        'desc': 'Author name',
+    },
+    'author_url': {
+        'value': 'http://example.net',
+        'desc': 'Author home page',
+    },
+    'build_path': {
+        'value': 'build',
+        'desc': 'Build path for web content generator output',
+    },
+    'default_tags': {
+        'value': ['misc'],
+        'desc': 'A list of default tags to be added to a new post',
+    },
+    'default_templates': {
+        'value': False,
+        'desc': 'Use default templates for all pages',
+    },
+    'date_format': {
+        'value': "%B %d, %Y",
+        'desc': 'Page/post short date format for generated HTML',
+    },
+    'datetime_format': {
+        'value': "%B %d, %Y %H:%M",
+        'desc': 'Page/post full date and time format for generated HTML',
+    },
+    'deploy_cmd': {
+        'value': '',
+        'desc': 'Shell command for web content deployment',
+    },
+    'editor_cmd': {
+        'value': "$EDITOR \"\"{source}\"\"",
+        'desc': 'Shell command to open files in text editor. {source} will '
+                'be replaced with a file path to open',
+    },
+    'enable_search_form': {
+        'value': True,
+        'desc': 'Enable Google search form',
+    },
+    'id_addthis': {
+        'value': '',
+        'desc': 'AddThis.com identifier, e.g. ra-1234567890123456 (sharing '
+                'buttons code will be included if the value is not empty)',
+    },
+    'id_disqus': {
+        'value': '',
+        'desc': 'Site short name for Disqus comments (comments code '
+                'will be included if the value is not empty)',
+    },
+    'id_google_analytics': {
+        'value': '',
+        'desc': 'Google Analytics tracking ID, e.g. UA-12345678-9 (tracking '
+                'code will be included if the value is not empty)',
+    },
+    'image_max_height': {
+        'value': 0,
+        'desc': 'Maximum vrtical image size for previews (0 for no limit)',
+    },
+    'image_max_width': {
+        'value': 600,
+        'desc': 'Maximum horizontal image size for previews (0 for no limit)',
+    },
+    'images_location': {
+        'value': 'img',
+        'desc': 'Path to image files inside build directory',
+    },
+    'images_path': {
+        'value': 'images',
+        'desc': 'Image files path inside website source dir',
+    },
+    'index_page': {
+        'value': 'index.html',
+        'desc': 'File name for an index page',
+    },
+    'iso_datetime_format': {
+        'value': "%B %d, %Y",
+        'desc': 'Page/post short date format for generated HTML',
+    },
+    'humans_author_location': {
+        'value': '',
+        'desc': 'Site author location for humans.txt',
+    },
+    'humans_author_twitter': {
+        'value': '',
+        'desc': 'Site author twitter account for humans.txt',
+    },
+    'humans_doctype': {
+        'value': 'HTML5',
+        'desc': 'Site Doctype for humans.txt',
+    },
+    'humans_ide': {
+        'value': 'Netscape Composer 7',
+        'desc': 'IDE value for humans.txt',
+    },
+    'humans_language': {
+        'value': 'English',
+        'desc': 'Site Language for humans.txt',
+    },
+    'less_cmd': {
+        'value': "lessc --yui-compress {source} > {dest}",
+        'desc': 'Shell command for LESS compillation',
+    },
+    'log_backup_cnt': {
+        'value': 3,
+        'desc': 'Amount of log files to keep',
+    },
+    'log_file': {
+        'value': 'pub.log',
+        'desc': 'Log file name',
+    },
+    'log_max_size': {
+        'value': 1024 * 1024,
+        'desc': 'Maximum file size for log rotation (in bytes)',
+    },
+    'menu': {
+        'value': [
+            {'title': 'About', 'href': '/about.html'},
+            {'title': 'Archive', 'href': '/archive/'},
+        ],
+        'desc': 'Navigation menu items',
+    },
+    'min_css': {
+        'value': False,
+        'desc': 'Enable CSS minification',
+    },
+    'min_css_cmd': {
+        'value': "yuicompressor --type css -o {dest} {source}",
+        'desc': 'Shell command for CSS minification',
+    },
+    'min_html': {
+        'value': False,
+        'desc': 'Remove extra whitespace from HTML',
+    },
+    'min_js': {
+        'value': False,
+        'desc': 'Enable JavaScript minification',
+    },
+    'min_js_cmd': {
+        'value': "yuicompressor --type js --nomunge -o {dest} {source}",
+        'desc': 'Shell command for JavaScript minification',
+    },
+    'page_tpl': {
+        'value': 'page',
+        'desc': 'Template name for pages',
+    },
+    'pages_path': {
+        'value': 'pages',
+        'desc': 'Page files path inside website source dir',
+    },
+    'pluso_enabled': {
+        'value': False,
+        'desc': 'Enable pluso.ru sharing buttons',
+    },
+    'port': {
+        'value': 8000,
+        'desc': 'Port number for the local web server',
+    },
+    'post_at_root_url': {
+        'value': True,
+        'desc': 'Put a copy of the latest post to the website root',
+    },
+    'post_location': {
+        'value': '{year}/{month}/{day}/{name}.html',
+        'desc': 'Post destination path pattern',
+    },
+    'post_source_name': {
+        'value': '{year}{month}{day}-{name}.md',
+        'desc': 'Post source file name pattern',
+    },
+    'post_tpl': {
+        'value': 'post',
+        'desc': 'Template name for blog posts',
+    },
+    'posts_path': {
+        'value': 'posts',
+        'desc': 'Post files path inside website source dir',
+    },
+    'rel_root_url': {
+        'value': '/',
+        'desc': 'Relative root website URL',
+    },
+    'root_url': {
+        'value': 'http://example.com/',
+        'desc': 'Root website URL',
+    },
+    'source_url': {
+        'value': 'http://github.com/username/example.com',
+        'desc': 'Website source URL',
+    },
+    'subtitle': {
+        'value': '',
+        'desc': 'Site sublitle',
+    },
+    'tag_location': {
+        'value': 'tags/{tag}.html',
+        'desc': 'Tag file name pattern for tag page and URL generation',
+    },
+    'time_format': {
+        'value': ["%Y/%m/%d %H:%M:%S", "%Y/%m/%d %H:%M", "%Y/%m/%d"],
+        'desc': 'A list of possible date/time formats for the '
+                'post and page header fields',
+    },
+    'title': {
+        'value': 'Brand New Blog',
+        'desc': 'Site title',
+    },
+    'verbose': {
+        'value': True,
+        'desc': 'Enable verbose logging',
+    },
+}
---
+++ packages/publicstatic/publicstatic-0.7.6.tar.gz/publicstatic-0.7.6/publicstatic/helpers.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+
+"""General purpose helper functions"""
+
+import codecs
+from datetime import datetime
+import os
+import re
+import shutil
+import sys
+import time
+from publicstatic import conf
+
+RE_H1 = re.compile(r"^\s*#\s*(.*)\s*", re.I | re.M | re.U)
+
+
+def makedirs(dir_path):
+    """Creates directory if it not exists."""
+    if dir_path and not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
+        return True
+    return False
+
+
+def browse(url, delay):
+    """Opens specified @url with system default browser
+    after @delay seconds."""
+    time.sleep(delay)
+    from webbrowser import open_new_tab
+    open_new_tab(url)
+
+
+def parse_time(value, default=None):
+    """Converts string to datetime using the first of the preconfigured
+    time_format values that will work."""
+    if not value and default:
+        return default
+    for time_format in conf.get('time_format'):
+        try:
+            return datetime.strptime(value.strip(), time_format)
+        except ValueError:
+            pass
+    else:
+        raise Exception('bad date/time format')
+
+
+def check_build(path):
+    """Check if the web content was built and exit if it isn't."""
+    if not os.path.isdir(path):
+        sys.exit("web content directory not exists: '%s'" % path)
+
+
+def drop_build(path, create=False):
+    """Drops the build if it exists"""
+    if os.path.isdir(path):
+        shutil.rmtree(path, ignore_errors=True)
+    if create and not os.path.isdir(path):
+        os.makedirs(path)
+
+
+def get_h1(text):
+    """Extracts the first h1-header from markdown text."""
+    matches = RE_H1.search(text)
+    return matches.group(1) if matches else ''
+
+
+def copydir(source, dest, indent=0):
+    """Copy a directory structure overwriting existing files."""
+    for root, dirs, files in os.walk(source):
+        if not os.path.isdir(root):
+            os.makedirs(root)
+        for each_file in files:
+            rel_path = root.replace(source, '').lstrip(os.sep)
+            dest_dir = os.path.join(dest, rel_path)
+            dest_path = os.path.join(dest_dir, each_file)
+            if not os.path.isdir(dest_dir):
+                os.makedirs(dest_dir)
+            shutil.copyfile(os.path.join(root, each_file), dest_path)
+
+
+def walk(path, operation):
+    """Performs operation for each file in the specified path.
+
+    - Operation should take two arguments: the original path and
+      additional relative path to each file.
+    - Directory names starting with underscore will be ignored."""
+    visible = lambda name: not name.startswith('_')
+    for curdir, dirnames, curfiles in os.walk(path):
+        dirnames[:] = filter(visible, dirnames)
+        for nextfile in curfiles:
+            fullpath = os.path.join(curdir, nextfile)
+            relpath = fullpath[len(path):].strip(os.sep)
+            operation(path, relpath)
+
+
+def tag_url(tag):
+    """Returns relative URL to the specified tag page."""
+    return conf.get('rel_root_url') + conf.get('tag_location').format(tag=tag)
+
+
+def tag_path(tag):
+    """Returns full path to the specified tag page."""
+    file_name = conf.get('tag_location').format(tag=tag)
+    return os.path.join(conf.get('build_path'), file_name)
+
+
+def execute(command, source, dest=''):
+    """Executes a command with {source} and {dest} parameter replacements."""
+    os.system(os.path.expandvars(command.format(source=source, dest=dest)))
+
+
+def mergedicts(*args):
+    """Merge a set of dictionaries."""
+    result = {}
+    for d in args:
+        result.update(d)
+    return result
+
+
+def xsplit(sep, text, strip=False, drop_empty=False):
+    """Split a string."""
+    result = text.split(sep)
+    if strip:
+        result = [tag.strip() for tag in result]
+    if drop_empty:
+        result = filter(None, result)
+    return result
+
+
+def suffix(file_name, number):
+    """Insert a numeric suffix before the [file_name] extension,
+    if the [number] is greater than 0. Effective suffix value will
+    be [number] + 1."""
+    base, ext = os.path.splitext(file_name)
+    suffix = "-%d" % (number + 1) if number > 0 else ''
+    return ''.join([base, suffix, ext])
+
+
+def newfile(file_name, text=''):
+    """Create new or overwrite existing file, and write text."""
+    makedirs(os.path.dirname(file_name))
+    with codecs.open(file_name, mode='w', encoding='utf-8') as f:
+        f.write(text)
+
+
+def utime(path, value):
+    """Set access time and modification time for the specified file
+    using datetime value."""
+    ts = value.timestamp()
+    os.utime(path, (ts, ts))
+
+
+def ext(file_name):
+    """Returns a file name extension in lower case (with leading dot)."""
+    return os.path.splitext(file_name)[1].lower()
+
+
+def unindent(text):
+    """Remove multiline text indentation."""
+    return '\n'.join(map(str.strip, (str(text) or '').split('\n')))
