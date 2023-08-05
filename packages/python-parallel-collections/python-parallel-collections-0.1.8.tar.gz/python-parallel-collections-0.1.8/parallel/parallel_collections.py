136b7775e17e2f92a9b69a36aa5a4e662d1141e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:01 +0000

    Add package python-parallel-collections/0.1.8/python-parallel-collections-0.1.8.tar.gz

---
+++ packages/python-parallel-collections/python-parallel-collections-0.1.8.tar.gz/python-parallel-collections-0.1.8/parallel/parallel_collections.py
@@ -0,0 +1,102 @@
+from concurrent import futures
+from multiprocessing import Manager
+from itertools import chain, imap
+from UserList import UserList
+from UserDict import UserDict
+
+
+Pool = futures.ProcessPoolExecutor()
+
+class _Filter(object):
+    '''Helper for the filter methods, 
+    need to use class as closures cannot be pickled and sent to other processes'''
+    
+    def __init__(self, pred):
+        self.pred = pred
+        
+    def __call__(self, item):
+        if self.pred(item):
+            return item
+        
+            
+class _Reducer(object):
+    '''Helper for the reducer methods'''
+    
+    def __init__(self, func, init=None):
+        self.func = func
+        self.q = Manager().list([init,])
+        
+    def __call__(self, item):
+        init = self.func(self.q[0], item)
+        self.q[0] = init
+    
+    @property
+    def result(self):
+        return self.q[0]
+
+
+class ParallelSeq(object):
+        
+    def foreach(self, func):
+        self.data = self.__class__(self.pool.map(func, self))
+        return None
+    
+    def filter(self, pred):
+        _filter = _Filter(pred)
+        return self.__class__(i for i in self.pool.map(_filter, self, ) if i)
+        
+    def flatten(self):
+        '''this will differ based on the underlying data struct'''
+        raise(NotImplemented)
+        
+    def map(self, func):
+        return self.__class__(self.pool.map(func, self, ))
+        
+    def flatmap(self, func):
+        data = self.flatten()
+        return self.__class__(self.pool.map(func, data, ))
+        
+    def reduce(self, function, init=None):
+        _reducer = _Reducer(function, init)
+        for i in self.pool.map(_reducer, self, ):
+            pass
+        return _reducer.result
+    
+
+class ParallelList(UserList, ParallelSeq):
+    
+    def __init__(self, *args, **kwargs):
+        self.pool = Pool
+        super(ParallelList, self).__init__(*args, **kwargs)
+        
+    def __iter__(self):
+        return iter(self.data)
+        
+    def flatten(self):
+        '''if the list consists of several sequences, those will be chained in one'''
+        return ParallelList(chain(*self))
+        
+        
+class ParallelDict(UserDict, ParallelSeq):
+    
+    def __init__(self, *args, **kwargs):
+        self.pool = Pool
+        super(ParallelDict, self).__init__(*args, **kwargs)
+        
+    def __iter__(self):
+        for i in self.data.items():
+            yield i
+
+    def flatten(self):
+        '''if the values of the dict consists of several sequences, those will be chained in one'''
+        flat = []
+        for k,v in self:
+            try:
+                flat.append((k,list(chain(*v))))
+            except TypeError:
+                flat.append((k, v))
+        return ParallelDict(flat)
+        
+    
+        
+    
