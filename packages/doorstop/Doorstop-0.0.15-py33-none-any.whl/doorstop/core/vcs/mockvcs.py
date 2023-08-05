f697929bdd012b384994836388d0d9a57078bdb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:14 +0000

    Add package doorstop/0.0.15/Doorstop-0.0.15-py33-none-any.whl

---
+++ packages/doorstop/Doorstop-0.0.15-py33-none-any.whl/doorstop/core/vcs/mockvcs.py
@@ -0,0 +1,24 @@
+"""
+Plug-in module to simulate the storage of requirements in a repository.
+"""
+
+import logging
+
+from doorstop.core.vcs.base import BaseWorkingCopy
+
+
+class WorkingCopy(BaseWorkingCopy):  # pragma: no cover - integration test
+    """Simulated working copy."""
+
+    DIRECTORY = '.mockvcs'
+
+    def lock(self, path):
+        logging.info("simulated lock on: {}...".format(path))
+
+    def save(self, message=None):
+        logging.info("simulated save")
+
+    @property
+    def ignores(self):
+        """Get a list of glob expressions to ignore."""
+        return ("*env*", "*apidocs*", "*build*", "*dist*")
