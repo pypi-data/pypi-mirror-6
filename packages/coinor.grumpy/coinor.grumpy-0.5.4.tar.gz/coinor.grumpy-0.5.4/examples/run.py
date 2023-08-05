914748a8c82b59af7a46c0d707319017c5fd3458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:44 +0000

    Add package coinor.grumpy/0.5.4/coinor.grumpy-0.5.4.tar.gz

---
+++ packages/coinor.grumpy/coinor.grumpy-0.5.4.tar.gz/coinor.grumpy-0.5.4/examples/run.py
@@ -0,0 +1,28 @@
+from grumpy import BBTree
+import sys
+
+if __name__ == '__main__':
+    bt = BBTree()
+    bt.set_display_mode('pygame')
+    line_number = 0
+    file_ = open('p0201_GLPK.in', 'r')
+#    for line in sys.stdin:
+    for line in file_:
+        bt.ProcessLine(line)
+        print 'line', line_number, 'processed.'
+        line_number = line_number+1
+        if line_number%200 != 0:
+            continue
+#        imagefile = open('tree-'+str(line_number)+'.png','w')
+#        imagefile.write(bt.GenerateTreeImage())
+#        imagefile.close()
+        if bt.root is not None:
+#            gnuplot_image = bt.GenerateHistogram()
+            gnuplot_image = bt.GenerateTreeImage()
+#            gnuplot_image = bt.GenerateScatterplot()
+#            gnuplot_image = bt.GenerateIncumbentPath()
+#            gnuplot_image = bt.GenerateForecastImages()
+#            if gnuplot_image is not None:
+#                bt.display_image(gnuplot_image)
+            bt.display_all()
+
