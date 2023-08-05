67a42c0006129a3c6b56a9333b7e5b641c05bdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:54 +0000

    Add package django-simple-poll/0.1.1b1/django-simple-poll-0.1.1b1.tar.gz

---
+++ packages/django-simple-poll/django-simple-poll-0.1.1b1.tar.gz/django-simple-poll-0.1.1b1/poll/urls.py
@@ -0,0 +1,11 @@
+try:
+    from django.conf.urls import patterns, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, url
+
+
+urlpatterns = patterns('poll.views',
+    url(r'^vote/(?P<poll_pk>\d)/$', 'vote', name='poll_ajax_vote'),
+    url(r'^poll/(?P<poll_pk>\d)/$', 'poll', name='poll'),
+    url(r'^result/(?P<poll_pk>\d)/$', 'result', name='poll_result'),
+)
