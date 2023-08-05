032fb0088c446ad76bff47a66870aed72ba51555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:01 +0000

    Add package dotter/0.0.4/dotter-0.0.4.tar.gz

---
+++ packages/dotter/dotter-0.0.4.tar.gz/dotter-0.0.4/dotter/__init__.py
@@ -0,0 +1,13 @@
+"""
+dotter
+
+Copyright (c) 2013, Friedrich Paetzke (f.paetzke@gmail.com)
+All rights reserved.
+
+"""
+from .dotter import Dotter, RankType
+
+__version__ = '0.0.4'
+__author__ = 'Friedrich Paetzke'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2013 Friedrich Paetzke'
---
+++ packages/dotter/dotter-0.0.4.tar.gz/dotter-0.0.4/setup.py
@@ -0,0 +1,45 @@
+"""
+dotter
+
+Copyright (c) 2013, Friedrich Paetzke (f.paetzke@gmail.com)
+All rights reserved.
+
+"""
+import os
+
+from setuptools import setup, find_packages
+
+
+pathname = os.path.abspath(os.path.dirname(__file__))
+try:
+    from orgco import convert_rst
+
+    with open(os.path.join(pathname, 'README.org')) as org_file:
+        with open(os.path.join(pathname, 'README.rst'), 'w') as rst_file:
+            rst_file.write(convert_rst(org_file.read()))
+except:
+    print('Error converting .org')
+
+
+setup(name='dotter',
+      py_modules=['dotter'],
+      description='Dotter is a graphviz wrapper for Python 2 and 3',
+      long_description=(open('README.rst').read()),
+      version='0.0.4',
+      license='BSD',
+      author='Friedrich Paetzke',
+      author_email='f.paetzke@gmail.com',
+      url='https://github.com/paetzke/dotter',
+      packages=find_packages(exclude=['tests*']),
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities',
+      ])
