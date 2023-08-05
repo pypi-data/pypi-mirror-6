9281c659d1422d94ac9c38d8d2aa00971a588baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:23 +0000

    Add package w3c-validator/0.2.1/w3c-validator-0.2.1.tar.gz

---
+++ packages/w3c-validator/w3c-validator-0.2.1.tar.gz/w3c-validator-0.2.1/setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requires = ['requests>=2.2.0']
+
+setup(
+    name='w3c-validator',
+    version='0.2.1',
+    description='W3C panel for Django Debug Toolbar using requests.',
+    author='Albert O\'Connor',
+    author_email='iqc@albertoconnor.ca',
+    url='https://bitbucket.org/amjoconn/w3c_validator',
+    packages=['w3c_validator'],
+    package_dir={'w3c_validator': 'w3c_validator'},
+    package_data={'': ['LICENSE', 'README.md'], 'w3c_validator': ['templates/w3c_validator/*.html']},
+    install_requires=requires,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
