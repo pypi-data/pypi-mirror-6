b7de89c24eaf96f63192f42514ab2730ff44cc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package tgext.crud/0.6.5/tgext.crud-0.6.5.tar.gz

---
+++ packages/tgext.crud/tgext.crud-0.6.5.tar.gz/tgext.crud-0.6.5/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.6.5'
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.txt')).read()
+    CHANGES = open(os.path.join(here, 'docs/HISTORY.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+setup(name='tgext.crud',
+      version=version,
+      description="Crud Controller Extension for TG2",
+      long_description=README + "\n" +
+                       CHANGES,
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='turbogears2.extension, TG2, REST, crud, sprox',
+      author='Christopher Perkins',
+      author_email='chris@percious.com',
+      url='https://github.com/TurboGears/tgext.crud',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['tgext'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          "TurboGears2",
+          'sprox>=0.8.3',
+          # -*- Extra requirements: -*-
+      ],
+      test_suite='nose.collector',
+      tests_require=[
+          'nose',
+          'webtest',
+          'TurboGears2',
+          'jinja2',
+          'sqlalchemy',
+          'zope.sqlalchemy',
+          'transaction',
+          'tw2.core',
+          'tw2.forms'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
