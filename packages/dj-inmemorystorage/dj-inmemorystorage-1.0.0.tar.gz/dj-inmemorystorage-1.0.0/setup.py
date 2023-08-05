80e64e32997d34208f9812589f3e7177e67a7ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package dj-inmemorystorage/1.0.0/dj-inmemorystorage-1.0.0.tar.gz

---
+++ packages/dj-inmemorystorage/dj-inmemorystorage-1.0.0.tar.gz/dj-inmemorystorage-1.0.0/setup.py
@@ -0,0 +1,33 @@
+#-*- coding: utf-8 -*-
+
+from setuptools import setup
+
+requires = ['Django >= 1.4', 'six==1.4.1']
+tests_require = requires
+
+setup(
+    name="dj-inmemorystorage",
+    description="A non-persistent in-memory data storage backend for Django.",
+    version="1.0.0",
+    url="https://github.com/waveaccounting/django-inmemorystorage",
+    license=open('LICENSE').read(),
+    long_description=open('README.rst').read(),
+    author='Cody Soyland, Seán Hayes, Tore Birkeland, Nick Presta',
+    author_email='opensource@waveapps.com',
+    packages=[
+        'inmemorystorage',
+    ],
+    zip_safe=True,
+    install_requires=requires,
+    tests_require=tests_require,
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
