64036f8366475bee04b026854ba1641f2131ffc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:20 +0000

    Add package tastypie-mongodb-resource/0.0.8/tastypie-mongodb-resource-0.0.8.tar.gz

---
+++ packages/tastypie-mongodb-resource/tastypie-mongodb-resource-0.0.8.tar.gz/tastypie-mongodb-resource-0.0.8/README.rst
@@ -0,0 +1,51 @@
+-----------------------------
+MongoDB Resource for Tastypie
+-----------------------------
+
+Allows you to create delicious APIs for MongoDB.
+
+--------
+Settings
+--------
+
+::
+
+    MONGODB_HOST = None
+    MONGODB_PORT = None
+    MONGODB_DATABASE = "database_name"
+
+    # optional:
+    MONGODB_USERNAME = None
+    MONGODB_PASSWORD = None
+
+----------------
+Example of Usage
+----------------
+
+::
+
+    from tastypie import fields
+    from tastypie.authorization import Authorization
+
+    from tastypie_mongodb.resources import MongoDBResource, Document
+
+    class DocumentResource(MongoDBResource):
+
+        id = fields.CharField(attribute="_id")
+        title = fields.CharField(attribute="title", null=True)
+        entities = fields.ListField(attribute="entities", null=True)
+
+        class Meta:
+            resource_name = "documents"
+            list_allowed_methods = ["delete", "get", "post"]
+            authorization = Authorization()
+            object_class = Document
+            collection = "documents" # collection name
+
+Contributors
+############
+
+- Ferrix Hovi (<https://github.com/ferrix>)
+- Alper Kanat (<https://github.com/tunix>)
+
+
---
+++ packages/tastypie-mongodb-resource/tastypie-mongodb-resource-0.0.8.tar.gz/tastypie-mongodb-resource-0.0.8/setup.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+
+import os
+from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="tastypie-mongodb-resource",
+    version="0.0.8",
+    author="Fatih Erikli",
+    author_email="fatiherikli@gmail.com",
+    description=("Tastypie MongoDB Resource"),
+    keywords="tastypie rest api module resource mongodb",
+    url="https://github.com/fatiherikli/tastypie-mongodb-resource",
+    install_requires = [
+        'Django>=1.5',
+        'django-tastypie>=0.10',
+        'pymongo>=2.6',
+    ],
+    packages=[
+        'tastypie_mongodb',
+    ],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Internet",
+    ],
+    include_package_data=True,
+    zip_safe=False
+)
