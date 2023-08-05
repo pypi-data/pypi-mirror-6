7c107503c8a9be59b13ef92f9b2cc6aceff8cae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package django-pragmatic/0.7.4/django-pragmatic-0.7.4.tar.gz

---
+++ packages/django-pragmatic/django-pragmatic-0.7.4.tar.gz/django-pragmatic-0.7.4/pragmatic/static/js/grouped-checkboxes.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    // GroupedCheckboxSelectMultiple
+    $('.group').each(function() {
+        if ($('.panel-heading input[type=checkbox]', this).is(':checked')) {
+            $('.list-group', this).show();
+        } else {
+            $('.list-group', this).hide();
+        }
+
+        $('.panel-heading input[type=checkbox]', this).change(function() {
+            if ($(this).is(':checked')) {
+                $(this).closest('.group').find('.list-group').show();
+
+                $(this).closest('.group').find('.list-group input[type=checkbox]').each(function() {
+                    if ($(this).attr('on_open') == 'checked') {
+                        $(this).prop('checked', true).change();
+                    }
+                });
+            } else {
+                $(this).closest('.group').find('.list-group').hide();
+
+                $(this).closest('.group').find('.list-group input[type=checkbox]').each(function() {
+//                    if ($(this).attr('on_open') == 'checked') {
+                        $(this).prop('checked', false).change();
+//                    }
+                });
+            }
+        });
+
+        $('input[type=checkbox]', this).change(function() {
+            $('input[type=checkbox][value='+$(this).val()+']').prop('checked', $(this).prop('checked')).closest('div').toggleClass('ez-checked');
+        });
+    });
+});
