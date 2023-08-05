70c699dfb6d41cb5f595e6a595910a42971c8e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package holmes/0.4.16/holmes-0.4.16.tar.gz

---
+++ packages/holmes/holmes-0.4.16.tar.gz/holmes-0.4.16/holmes/facters/title.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from holmes.facters import Facter
+
+
+class TitleFacter(Facter):
+
+    @classmethod
+    def get_fact_definitions(cls):
+        return {
+            'page.title': {
+                'title': 'Page Title',
+                'description': lambda value: value
+            }
+        }
+
+    def get_facts(self):
+        titles = self.reviewer.current_html.cssselect('title')
+
+        if not titles:
+            return
+
+        self.review.data['page.title_count'] = len(titles)
+
+        self.add_fact(key='page.title', value=titles[0].text)
