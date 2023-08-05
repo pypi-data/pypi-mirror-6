647368b5e26d41407819f308cbf2efecbb593cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package alert-from-script/0.1.2/alert-from-script-0.1.2.tar.gz

---
+++ packages/alert-from-script/alert-from-script-0.1.2.tar.gz/alert-from-script-0.1.2/alert_from_script/__init__.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+import subprocess
+import tempfile
+
+import boto
+
+def main():
+    parser = argparse.ArgumentParser(description='Runs a script and sends the stdout and stderr to SNS in case exit != 0.')
+    
+    parser.add_argument('sns_topic', type=str, help='ARN of SNS topic to publish to.')
+    parser.add_argument('sns_subject', type=str, help='Subject used for SNS messages.')
+    parser.add_argument('script', type=str, help='The script to run. Includes all args to run.')
+    parser.add_argument('--aws-access-key-id',type=str, help='Optional AWS Access Key Id.  Will use environment variables and instance profiles if not set.')
+    parser.add_argument('--aws-secret-access-key',type=str, help='Optional AWS Access Key Id.  Will use environment variables and instance profiles if not set.')
+    args = parser.parse_args()
+    
+    if args.aws_access_key_id:
+        os.environ['AWS_ACCESS_KEY_ID'] = args.aws_access_key_id
+    if args.aws_secret_access_key:
+        os.environ['AWS_SECRET_ACCESS_KEY'] = args.aws_secret_access_key
+    
+    sns_conn = boto.connect_sns()
+    
+    stdout = tempfile.TemporaryFile()
+    stderr = tempfile.TemporaryFile()
+    
+    return_code = subprocess.call(args.script, shell=True, stdout=stdout, stderr=stderr)
+    
+    if return_code != 0:
+        stdout.seek(0)
+        stderr.seek(0)
+        
+        message = "COMMAND:\n%s\n\nSTDOUT:\n%s\n\nSTDERR:\n%s" % (args.script, stdout.read(), stderr.read())
+        
+        sns_conn.publish(topic=args.sns_topic, message=message, subject=args.sns_subject)
+    
+if __name__ == '__main__':
+    main()
---
+++ packages/alert-from-script/alert-from-script-0.1.2.tar.gz/alert-from-script-0.1.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import sys
+
+kwargs = {
+    'name': 'alert-from-script',
+    'version': '0.1.2',
+    'description': 'Generate SNS alerts from any command.',
+    'author': 'Brandon Adams',
+    'author_email': 'brandon.adams@me.com',
+    'url': 'https://github.com/adamsb6/alert_from_script',
+    'packages': ['alert_from_script'],
+    'install_requires': [
+        'boto>=2.12.0',
+    ],
+    'entry_points': {
+        'console_scripts': ['alert-from-script = alert_from_script:main'],
+    },
+}
+
+if sys.version_info < (2, 7):
+    kwargs['install_requires'].append('argparse==1.2.1')
+
+setup(**kwargs)
