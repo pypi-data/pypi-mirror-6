356ef84d20a9eceff32fce312c21a68bee0bea80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:22 +0000

    Add package alchemist/0.3.3/alchemist-0.3.3.tar.gz

---
+++ packages/alchemist/alchemist-0.3.3.tar.gz/alchemist-0.3.3/alchemist/conf/default_settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import, division
+from alchemist.conf import defer
+
+# Debugging.
+DEBUG = True
+
+# Default server configuration.
+SERVER_HOST = '::1'
+SERVER_PORT = 8000
+SERVER_DEBUG = defer('DEBUG')
+SERVER_RELOAD = defer('DEBUG')
+SERVER_THREADED = False
+SERVER_PROCESSES = 1
+
+# List of registered components.
+COMPONENTS = ['alchemist']
