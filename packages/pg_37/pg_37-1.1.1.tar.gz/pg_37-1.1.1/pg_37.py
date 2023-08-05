505c818cd34ae1976254308670ddea99a16a59cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:59 +0000

    Add package pg_37/1.1.1/pg_37-1.1.1.tar.gz

---
+++ packages/pg_37/pg_37-1.1.1.tar.gz/pg_37-1.1.1/pg_37.py
@@ -0,0 +1,17 @@
+"""
+Este é um método para tratamento de listas aninhadas!
+"""
+def print_lol(alista, nivel):
+    """
+    Este método recebe uma lista como parametro e faz todo o 
+    processamento
+    Agora este metodo alem da lista, recebe um parametro inteiro para medir
+    o recuo
+    """
+    for each in alista:
+        if isinstance (each, list):
+            print_lol(each, nivel+1)
+        else:
+            for espaco in range(nivel):
+                print ('\t', end='')
+            print (each)
