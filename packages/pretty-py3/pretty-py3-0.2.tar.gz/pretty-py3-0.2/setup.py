c4daed15918c59d38d603768b9b1cffda1aeeaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:36 +0000

    Add package pretty-py3/0.2/pretty-py3-0.2.tar.gz

---
+++ packages/pretty-py3/pretty-py3-0.2.tar.gz/pretty-py3-0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='pretty-py3',
+    author='Mark Grandi',
+    author_email='markgrandi@gmail.com',
+    version='0.2',
+    url='https://github.com/mgrandi/pretty-py3',
+    py_modules=['pretty'],
+    description='extensible pprint successor - python3 version',
+    long_description='A python 3 fork of the original pretty.py from http://dev.pocoo.org/hg/sandbox',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python'
+    ]
+)
