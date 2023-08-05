357e5c05bf1e8f7f2c517d4163540da7feb74f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package tohaml/1.0/tohaml-1.0.tar.gz

---
+++ packages/tohaml/tohaml-1.0.tar.gz/tohaml-1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+description = "Docs at http://github.com/boscoh/tohaml."
+
+setup(
+    name='tohaml',
+    version='1.0',
+    author='Bosco Ho',
+    author_email='boscoh@gmail.com',
+    url='http://github.com/boscoh/tohaml',
+    description='converts HTML to HAML for the HamlPy compiler',
+    long_description=description,
+    license='BSD',
+    install_requires=['beautifulsoup4'],
+    scripts=['tohaml'],
+    py_modules=['tohaml']
+)
