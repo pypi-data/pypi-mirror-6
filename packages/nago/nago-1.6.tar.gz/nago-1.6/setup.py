0e84c1d7800d97159ca07be273f3670c10ee7a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:40 +0000

    Add package nago/1.6/nago-1.6.tar.gz

---
+++ packages/nago/nago-1.6.tar.gz/nago-1.6/setup.py
@@ -0,0 +1,48 @@
+# setup.py ###
+#from distutils.core import setup
+from setuptools import find_packages, setup
+import os
+
+
+from nago import get_version
+
+NAME = "nago"
+VERSION = get_version()
+SHORT_DESC = "General purpose agent, mostly used with nagios"
+LONG_DESC = """ """
+
+datafiles = []
+
+datadirs = ['nago/protocols/httpserver/templates/', 'etc/nago']
+
+for i in datadirs:
+    for cur_dir, dirlist, filelist in os.walk(i):
+        datafiles.append(("/" + cur_dir, filelist))
+
+
+
+if __name__ == "__main__":
+    manpath = "share/man/man1/"
+    etcpath = "/etc/%s" % NAME
+    etcmodpath = "/etc/%s/modules" % NAME
+    initpath = "/etc/init.d/"
+    logpath = "/var/log/%s/" % NAME
+    varpath = "/var/lib/%s/" % NAME
+    rotpath = "/etc/logrotate.d"
+    datarootdir = "/usr/share/%s" % NAME
+    setup(
+        name='%s' % NAME,
+        version=VERSION,
+        author='Pall Sigurdsson',
+        description=SHORT_DESC,
+        long_description=LONG_DESC,
+        author_email='palli@opensource.is',
+        url='http://github.com/opinkerfi/nago',
+        license='GPL',
+        scripts=['scripts/nago'],
+        packages=find_packages(),
+        requires=['flask', 'simplejson', 'pynag', 'requests'],
+        include_package_data=True,
+        package_data={'nago/protocols/httpserver': ['nago/protocols/httpserver/templates/']},
+        #data_files=datafiles,
+    )
