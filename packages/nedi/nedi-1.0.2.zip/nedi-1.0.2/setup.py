16eb8525507c2a01fb3fac8e7fc984b2ee2cc1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package nedi/1.0.2/nedi-1.0.2.zip

---
+++ packages/nedi/nedi-1.0.2.zip/nedi-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nedi',
+    version = '1.0.2',
+    py_modules = ['nedi'],
+    author = 'Hoyoun Lee',
+    author_email = 'lhy332@gmail.com',
+    url = 'http://www.headfirstlab.com',
+    description = 'A simple and first code in Python',
+    )
