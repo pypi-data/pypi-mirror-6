cbd229d88f1308bd99e80e87f1fc6149cf30c769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package plicata/0.0.134/plicata-0.0.134.tar.gz

---
+++ packages/plicata/plicata-0.0.134.tar.gz/plicata-0.0.134/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plicata',
+    version='0.0.134',
+    description='Django Application for Book Publishers',
+    author='MiddleFork',
+    url='http://plicata.com',
+    author_email='walker@mfgis.com',
+    packages=find_packages(),
+    install_requires=[
+        "setuptools_git >= 0.3",
+        "django >= 1.6",
+        "mezzanine >= 3.0",
+    ],
+)
