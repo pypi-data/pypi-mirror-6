40ce03c177e6e5acfedddd5c34fdbf59f2c501a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:41 +0000

    Add package homekeeper/2.2.2/homekeeper-2.2.2.tar.gz

---
+++ packages/homekeeper/homekeeper-2.2.2.tar.gz/homekeeper-2.2.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    author='Min Huang',
+    author_email='min.huang@alumni.usc.edu',
+    description='Homekeeper can version your dotfiles with Git.',
+    long_description=open('README.txt').read(),
+    name='homekeeper',
+    package_dir={'': 'lib'},
+    py_modules=['homekeeper'],
+    scripts=['bin/homekeeper'],
+    test_suite='homekeeper_test',
+    version='2.2.2',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+    ],
+)
