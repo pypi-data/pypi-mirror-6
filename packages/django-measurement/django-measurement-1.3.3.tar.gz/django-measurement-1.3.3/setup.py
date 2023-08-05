9bc261461c5faf6e29d1e970e7c1ab6594900bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package django-measurement/1.3.3/django-measurement-1.3.3.tar.gz

---
+++ packages/django-measurement/django-measurement-1.3.3.tar.gz/django-measurement-1.3.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+requirements = []
+with open('requirements.txt', 'r') as in_:
+    requirements = in_.readlines()
+
+tests_require=['django']
+
+setup(
+    name='django-measurement',
+    version='1.3.3',
+    url='http://github.com/latestrevision/django-measurement/',
+    description='Convenient fields and classes for handling measurements',
+    author='Adam Coddington',
+    author_email='me@adamcoddington.net',
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='django_measurement.runtests.runtests',
+    install_requires=requirements,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=[
+        'django_measurement',
+        ],
+)
