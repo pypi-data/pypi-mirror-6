63ac008f2e3a6abd3adda6f76223a2a982cd5827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:54 +0000

    Add package django-planet/0.5/django-planet-0.5.tar.gz

---
+++ packages/django-planet/django-planet-0.5.tar.gz/django-planet-0.5/planet/tests/templatetags.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from mock import patch
+
+from django.test import TestCase
+
+
+#class GetLanguageVersionsENTest(TestCase):
+
+    #def setUp(self):
+        #self.obj = DummyModelFactory.create()
+
+    #@patch("django.utils.translation.get_language")
+    #def test_en_get_language_versions(self, translation_get_language_mock):
+        #translation_get_language_mock.return_value = "en"
+        #self.assertEquals([], get_language_versions(self.obj))
