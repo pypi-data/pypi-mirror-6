962679725b9a8db295a7b8bb89f4adf3520f8727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:09 +0000

    Add package cbagent/1.16.0/cbagent-1.16.0.tar.gz

---
+++ packages/cbagent/cbagent-1.16.0.tar.gz/cbagent-1.16.0/cbagent/settings.py
@@ -0,0 +1,49 @@
+import json
+import os
+from argparse import ArgumentParser
+
+from logger import logger
+
+
+class Settings(object):
+
+    DEFAULT = {
+        "cbmonitor_host_port": "127.0.0.1:8000",
+        "seriesly_host": "127.0.0.1",
+
+        "interval": 10,
+
+        "cluster": "default",
+        "master_node": "127.0.0.1",
+        "dest_master_node": "127.0.0.1",
+        "rest_username": "Administrator",
+        "rest_password": "password",
+        "ssh_username": "root",
+        "ssh_password": "couchbase",
+        "partitions": {},
+
+        "buckets": None,
+        "nodes": None
+    }
+
+    def __init__(self, options={}):
+        for option, value in dict(self.DEFAULT, **options).items():
+            setattr(self, option, value)
+
+    def read_cfg(self):
+        parser = ArgumentParser()
+        parser.add_argument("config", help="name of configuration file")
+        args = parser.parse_args()
+
+        if not os.path.isfile(args.config):
+            logger.interrupt("File doesn\'t exist: {}".format(args.config))
+
+        logger.info("Reading configuration file: {}".format(args.config))
+        with open(args.config) as fh:
+            try:
+                for option, value in json.load(fh).items():
+                    setattr(self, option, value)
+            except ValueError as e:
+                logger.interrupt("Error reading config: {}".format(e))
+            else:
+                logger.info("Configuration file successfully parsed")
---
+++ packages/cbagent/cbagent-1.16.0.tar.gz/cbagent-1.16.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+version = '1.16.0'
+
+setup(
+    name='cbagent',
+    version=version,
+    author='Couchbase',
+    license='Apache Software License',
+    packages=[
+        'cbagent',
+        'cbagent.cli_wrappers',
+        'cbagent.collectors',
+        'cbagent.collectors.libstats'
+    ],
+    entry_points={
+        'console_scripts': [
+            'at_collector = cbagent.cli_wrappers.at_collector:main',
+            'atop_collector = cbagent.cli_wrappers.atop_collector:main',
+            'latency_collector = cbagent.cli_wrappers.latency_collector:main',
+            'ns_collector = cbagent.cli_wrappers.ns_collector:main',
+            'ps_collector = cbagent.cli_wrappers.ps_collector:main',
+            'sgw_collector = cbagent.cli_wrappers.sgw_collector:main',
+            'xdcr_lag_collector = cbagent.cli_wrappers.xdcr_lag_collector:main',
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'couchbase==1.2.0',
+        'decorator',
+        'eventlet==0.12.1',
+        'fabric==1.8.0',
+        'logger',
+        'requests==2.1.0',
+        'seriesly',
+        'spring'
+    ],
+)
