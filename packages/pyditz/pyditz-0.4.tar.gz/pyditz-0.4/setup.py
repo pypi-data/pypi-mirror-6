3133faa9384626f1be68e3850e48ea6171983922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package pyditz/0.4/pyditz-0.4.tar.gz

---
+++ packages/pyditz/pyditz-0.4.tar.gz/pyditz-0.4/setup.py
@@ -0,0 +1,49 @@
+# Setup script for Ditz.
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+import ditz.version as info
+
+desc = """
+This package is intended to be a drop-in replacement for the Ditz
+distributed issue tracker.  It provides a command-line program, which acts
+(mostly) the same way as Ditz, and it adds several other nice things too.
+"""
+
+setup(name             = info.__title__,
+      author           = info.__author__,
+      author_email     = info.__email__,
+      description      = info.__doc__.strip(),
+      long_description = desc.strip(),
+      version          = info.__version__,
+      license          = info.__license__,
+      url              = info.__url__,
+      classifiers      = info.__classifiers__.strip().split("\n"),
+
+      packages = ["ditz"],
+      include_package_data = True,
+
+      setup_requires = [
+          'setuptools_hg',
+      ],
+
+      install_requires = [
+          'pyyaml >= 3.10',
+          'jinja2 >= 2.7',
+      ],
+
+      tests_require = [
+          'nose >= 1.3.0',
+          'coverage >= 3.6',
+          'mock >= 1.0.1',
+      ],
+
+      test_suite = 'nose.collector',
+
+      entry_points = {
+          'console_scripts': [
+              'pyditz = ditz.console:main'
+          ]
+      })
