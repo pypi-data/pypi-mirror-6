a652b366db63cd72c426f28181df8cd5b3120d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:07 +0000

    Add package isort/2.4.0/isort-2.4.0.tar.gz

---
+++ packages/isort/isort-2.4.0.tar.gz/isort-2.4.0/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+try:
+    from setuptools import setup
+    from setuptools.command.test import test as TestCommand
+
+    class PyTest(TestCommand):
+        extra_kwargs = {'tests_require': ['pytest', 'mock']}
+
+        def finalize_options(self):
+            TestCommand.finalize_options(self)
+            self.test_args = []
+            self.test_suite = True
+
+        def run_tests(self):
+            import pytest
+            sys.exit(pytest.main(self.test_args))
+
+except ImportError:
+    from distutils.core import setup, Command
+
+    class PyTest(Command):
+        extra_kwargs = {}
+        user_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            raise SystemExit(subprocess.call([sys.executable, 'runtests.py']))
+
+
+setup(name='isort',
+      version='2.4.0',
+      description='A Python utility / library to sort Python imports.',
+      author='Timothy Crosley',
+      author_email='timothy.crosley@gmail.com',
+      url='https://github.com/timothycrosley/isort',
+      download_url='https://github.com/timothycrosley/isort/archive/2.4.0.tar.gz',
+      license="MIT",
+      scripts=['scripts/isort'],
+      packages=['isort'],
+      requires=['pies', 'natsort'],
+      install_requires=['pies>=1.0.3', 'natsort>=3.0.0'],
+      cmdclass={'test': PyTest},
+      **PyTest.extra_kwargs)
