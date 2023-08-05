5d850f7239185b5efdcc92f2e84740d06e536577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:47 +0000

    Add package robotframework-archivelibrary/0.3.0/robotframework-archivelibrary-0.3.0.tar.gz

---
+++ packages/robotframework-archivelibrary/robotframework-archivelibrary-0.3.0.tar.gz/robotframework-archivelibrary-0.3.0/ArchiveLibrary/utils.py
@@ -0,0 +1,77 @@
+import os
+import zipfile
+import tarfile
+
+""" Originally from
+
+ http://code.activestate.com/recipes/252508/
+
+ heavily refactored since
+
+"""
+
+
+class Archive(object):
+
+    def _createstructure(self, file, dir):
+        self._makedirs(self._listdirs(file), dir)
+
+    def _makedirs(self, directories, basedir):
+        """ Create any directories that don't currently exist """
+        for dir in directories:
+            curdir = os.path.join(basedir, dir)
+            if not os.path.exists(curdir):
+                os.mkdir(curdir)
+
+
+class Unzip(Archive):
+    def extract(self, zfile, dest='.'):
+        dest = os.path.abspath(dest)
+        if not dest.endswith(':') and not os.path.exists(dest):
+            os.mkdir(dest)
+
+        zf = zipfile.ZipFile(zfile)
+
+        # create directory structure to house files
+        self._createstructure(zfile, dest)
+
+        # extract files to directory structure
+        for i, name in enumerate(zf.namelist()):
+            if not name.endswith('/'):
+                outfile = open(os.path.join(dest, name), 'wb')
+                outfile.write(zf.read(name))
+                outfile.flush()
+                outfile.close()
+
+    def _listdirs(self, zfile):
+        """ Grabs all the directories in the zip structure
+        This is necessary to create the structure before trying
+        to extract the file to it. """
+
+        zf = zipfile.ZipFile(zfile)
+
+        dirs = [name for name in zf.namelist() if name.endswith('/')]
+
+        return dirs
+
+
+class Untar(Archive):
+    def extract(self, tfile, dest="."):
+        if not dest.endswith(':') and not os.path.exists(dest):
+            os.mkdir(dest)
+
+        tff = tarfile.open(name=tfile)
+        tff.extractall(dest)
+
+    def _listdirs(self, tfile):
+        """ Grabs all the directories in the tar structure
+        This is necessary to create the structure before trying
+        to extract the file to it. """
+
+        tff = tarfile.open(name=tfile)
+        return [name for name in tff.getnames() if name.endswith('/')]
+
+
+if __name__ == "__main__":
+    ut = Untar()
+    ut.extract(r"/tmp/test.tar", r"/tmp/testout")
---
+++ packages/robotframework-archivelibrary/robotframework-archivelibrary-0.3.0.tar.gz/robotframework-archivelibrary-0.3.0/ArchiveLibrary/version.py
@@ -0,0 +1,2 @@
+# switching to semver
+VERSION = '0.3.0'
