eb7ad3786169add8801ea3c4a6a6632c14dc8bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:56 +0000

    Add package etcd/1.1.5/etcd-1.1.5.tar.gz

---
+++ packages/etcd/etcd-1.1.5.tar.gz/etcd-1.1.5/etcd/config.py
@@ -0,0 +1,11 @@
+DEFAULT_HOSTNAME = '127.0.0.1'
+"Default hostname for server."
+
+DEFAULT_PORT = 4001
+"Default port for server."
+
+DEFAULT_SCHEME = 'http'
+"Default scheme for server."
+
+HOST_FAIL_WAIT_S = 5
+"Number of seconds that must elapse before we're allowed to retry a host."
