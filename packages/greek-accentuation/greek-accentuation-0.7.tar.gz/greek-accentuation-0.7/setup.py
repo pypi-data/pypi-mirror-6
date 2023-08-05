ad10b5af44bf85c335ff84fd52c520592c473823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:28 +0000

    Add package greek-accentuation/0.7/greek-accentuation-0.7.tar.gz

---
+++ packages/greek-accentuation/greek-accentuation-0.7.tar.gz/greek-accentuation-0.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = "greek-accentuation",
+    version = "0.7",
+    description = "Python 3 library for accenting (and analyzing the accentuation of) Ancient Greek words",
+    license = "MIT",
+    url = "http://github.com/jtauber/greek-accentuation",
+    author = "James Tauber",
+    author_email = "jtauber@jtauber.com",
+    py_modules = ["characters", "syllabify", "accentuation"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+)
