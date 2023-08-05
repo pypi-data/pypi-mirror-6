851c2bc6a4735525bda5ead0ed1b7bd42183e33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package file_visitor/0.1.4/file_visitor-0.1.4.zip

---
+++ packages/file_visitor/file_visitor-0.1.4.zip/file_visitor-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+	name='file_visitor',
+	version='0.1.4',
+	py_modules=['visitor'],
+	author='sens',
+	author_email='hanyueqi123@163.com',
+	url='http://www.5dit.org',
+	description='A simple file search tools, you can easily sum all the files lines.',
+	
+	)
