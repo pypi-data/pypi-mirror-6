d1c2ab337119492bf9c8f9d5a361e4f5636ec253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:08 +0000

    Add package techies/0.1.1/techies-0.1.1.tar.gz

---
+++ packages/techies/techies-0.1.1.tar.gz/techies-0.1.1/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from techies import __version__
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    'techies'
+]
+
+with open('requirements.txt') as f:
+    requires = f.read().strip().splitlines()
+
+with open('LICENSE') as f:
+    license = f.read().strip()
+
+with open('README.rst') as f:
+    readme = f.read().strip()
+
+with open('CHANGES.rst') as f:
+    changes = f.read().strip()
+
+long_description = readme + '\n\n' + changes
+
+setup(
+    name='techies',
+    version=__version__,
+    description='Opinionated Python toolbox',
+    long_description=long_description,
+    author='Runzhou Li (Leo)',
+    author_email='runzhou.li@gmail.com',
+    url='https://github.com/woozyking/techies',
+    packages=packages,
+    package_data={'': ['LICENSE']},
+    package_dir={'techies': 'techies'},
+    include_package_data=True,
+    install_requires=requires,
+    license=license,
+    zip_safe=False,
+    classifiers=(
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3'
+    ),
+)
