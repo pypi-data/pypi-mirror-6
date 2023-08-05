304c112d143169da9098e163e8c37dc4983791bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:28 +0000

    Add package ztfy.skin/0.6.10/ztfy.skin-0.6.10.tar.gz

---
+++ packages/ztfy.skin/ztfy.skin-0.6.10.tar.gz/ztfy.skin-0.6.10/src/ztfy/skin/resources/js/i18n/fr.js
@@ -0,0 +1,24 @@
+(function($) {
+
+	$.ZTFY.I18n = {
+
+		INFO: "Information",
+		WARNING: "!! ATTENTION !!",
+
+		ERROR_OCCURED: "Une erreur s'est produite !",
+		ERRORS_OCCURED: "Des erreurs se sont produites !",
+
+		BAD_LOGIN_TITLE: "Erreur de connexion !",
+		BAD_LOGIN_MESSAGE: "Vos paramètres d'authentification n'ont pas permis d'ouvrir de session ; veuillez vérifier ces paramètres ou contacter l'administrateur du site.",
+
+		CONFIRM: "Confirmation",
+		CONFIRM_REMOVE: "La suppression de ce contenu ne pourra pas être annulée. Confirmez-vous ?",
+
+		NO_UPDATE: "Aucune modification n'a été appliquée.",
+		DATA_UPDATED: "Modifications enregistrées.",
+
+		MISSING_OVERLAY: "Le plug-in JQuery « overlay » est nécessaire ; veuillez inclure les ressources JQuery-tools dans votre page !"
+
+	}
+
+})(jQuery);
