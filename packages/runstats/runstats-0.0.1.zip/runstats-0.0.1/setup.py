c9ed5a34cc85199bc9ee980a3f0d171c74d1b7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:12 +0000

    Add package runstats/0.0.1/runstats-0.0.1.zip

---
+++ packages/runstats/runstats-0.0.1.zip/runstats-0.0.1/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+import runstats
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='runstats',
+    version=runstats.__version__,
+    description='Compute statistics and regression in one pass',
+    long_description=readme,
+    author='Grant Jenks',
+    author_email='contact@grantjenks.com',
+    url='http://www.grantjenks.com/blog/portfolio-post/python-runstats-module/',
+    py_modules=['runstats'],
+    package_data={'': ['LICENSE', 'README.rst']},
+    install_requires=[],
+    license=license,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
