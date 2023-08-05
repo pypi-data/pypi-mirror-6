40ba969748ac7ea964274156d8a054d9e9f16698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package infi.storagemodel/0.1.69/infi.storagemodel-0.1.69-py2.7.egg

---
+++ packages/infi.storagemodel/infi.storagemodel-0.1.69-py2.7.egg/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.69"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.69)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '0f9211a259715c15ada822b24cf21ce36f9ac23a'
+__git_head_subject__ = 'Finished release v0.1.69.'
+__git_head_message__ = "* release/v0.1.69:\n  Freezing dependencies\n  STORAGEMODEL-239 remove unused FileSystemOnPartition and FileSystemFactory\n  TRIVIAL linux initate_rescan: more logging\n  TRIVIAL handling defunct rescan processes\n  TRIVIAL rescan_scsi_bus: calculating scsi hosts to rescan inside the child process\n  TRIVIAL check_for_scsi_errors: logging AsiSCSIError without the traceback\n  HIP-811 creating aligned partitions\n  HIP-811 infinibox alignment\n  HIP-811 creating aligned partitions\n  TRIVIAL fixing typo in WindowsPartition.reize\n  STORAGEMODEL-238 reverting resize partitions on Linux as it doesn't work\n  buildout.cfg: adding infi.diskmanagement>=0.1.27 to requirements\n  buildout.cfg: remove infi.diskmanagement>0.1.26 from requirements\n  STORAGEMODEL-238 linux Filesystem.resize\n  STORAGEMODEL-238 linux Partition.resize\n  STORAGEMODEL-238 Filesystem.resize and Partition.size implemented on Windows\n  TRIVIAL fixing previous comit\n  TRIVIAL adding missing import\n  STORAGEMODEL-237 Filesystem.get_label and Filesystem.set_label\n  Unfreezing dependencies"
+__git_dirty_diff__ = ''
