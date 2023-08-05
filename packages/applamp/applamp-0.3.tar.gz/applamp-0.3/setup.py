7574cfc821be3ee94d2912c36afac09b1b5e2742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:37 +0000

    Add package applamp/0.3/applamp-0.3.tar.gz

---
+++ packages/applamp/applamp-0.3.tar.gz/applamp-0.3/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='applamp',
+    version='0.3',
+    description='Python API for controlling AppLamp LED lighting',
+    author='Ludwik Trammer',
+    author_email='ludwik@gmail.com',
+    url='https://github.com/ludwiktrammer/applamp',
+    packages=['applamp'],
+)
