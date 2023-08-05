f47f4c41af262f02d3c61787f10701ee6ef8485c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:36 +0000

    Add package PyMeetupJan/0.1/PyMeetupJan-0.1-py2.7.egg

---
+++ packages/PyMeetupJan/PyMeetupJan-0.1-py2.7.egg/module_a/setup_db.py
@@ -0,0 +1,6 @@
+import sys
+
+
+def initialize_database(argv=sys.argv):
+    print "Setting up the database"
+    return 0
---
+++ packages/PyMeetupJan/PyMeetupJan-0.1-py2.7.egg/module_a/tests/test_setup_db.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestSetupDB(unittest.TestCase):
+    def test_that_db_is_setup(self):
+        self.assertEqual(1, 1)
