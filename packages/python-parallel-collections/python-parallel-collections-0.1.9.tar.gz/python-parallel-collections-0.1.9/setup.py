b814b26bf7050c53c9230ab9b1a5296cff1d7ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:23 +0000

    Add package python-parallel-collections/0.1.9/python-parallel-collections-0.1.9.tar.gz

---
+++ packages/python-parallel-collections/python-parallel-collections-0.1.9.tar.gz/python-parallel-collections-0.1.9/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import parallel
+
+setup(
+    name='python-parallel-collections',
+    data_files=[('', ['requirements.txt', 'README.md', '.gitignore']),],
+    version='0.1.9',
+    packages=['parallel',],
+    description='parallel implementations of collections with support for map/reduce style operations',
+    long_description='''
+    In this package you'll find very simple parallel implementations of list and dict. 
+    The parallelism is achieved using the Python 2.7 backport of the concurrent.futures package.
+    If you can define your problem in terms of map/reduce/filter operations, it will run on several parallel Python processes on your machine, taking advantage of multiple cores. 
+    Otherwise these datastructures are equivalent to their non-parallel peers found in the standard library.
+    \n
+    Examples at http://gterzian.github.io/Python-Parallel-Collections/''',
+    author='Gregory Terzian',
+    author_email='gregory.terzian@gmail.com',
+    license='BSD License',
+    url='https://github.com/gterzian/Python-Parallel-Collections',
+    platforms=["any"],
+    requires=['futures',],
+    classifiers=[
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Distributed Computing',
+    ],
+)
