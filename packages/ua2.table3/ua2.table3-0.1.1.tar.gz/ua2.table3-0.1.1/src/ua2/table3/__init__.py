0fbdf11ce82c5bd8babb65d2d25c1e3ad00ed321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:09 +0000

    Add package ua2.table3/0.1.1/ua2.table3-0.1.1.tar.gz

---
+++ packages/ua2.table3/ua2.table3-0.1.1.tar.gz/ua2.table3-0.1.1/src/ua2/table3/__init__.py
@@ -0,0 +1,3 @@
+from .table import Table
+from .column import (Column, LabelColumn, HrefColumn,
+                     TemplateColumn, CheckboxColumn)
---
+++ packages/ua2.table3/ua2.table3-0.1.1.tar.gz/ua2.table3-0.1.1/src/ua2/table3/plugins/__init__.py
@@ -0,0 +1,7 @@
+from .sortable import SingleSortPlugin
+from .output import DjangoTemplatePlugin
+from .paginator import PaginatorPlugin
+from .quicksearch import QuickSearchORM
+from .actions import PostAction
+from .sortable import SingleSortPlugin
+from .filters import CategoryFilter
