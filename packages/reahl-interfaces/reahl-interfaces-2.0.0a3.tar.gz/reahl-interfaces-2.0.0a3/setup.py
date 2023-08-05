1ea844eec464a4466b2eec2751066ee8808f7c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:06 +0000

    Add package reahl-interfaces/2.0.0a3/reahl-interfaces-2.0.0a3.tar.gz

---
+++ packages/reahl-interfaces/reahl-interfaces-2.0.0a3.tar.gz/reahl-interfaces-2.0.0a3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    name='reahl-interfaces',
+    version=u'2.0.0a3',
+    description='Python abstract classes for important Reahl interfaces.',
+    long_description=u'Reahl is a web application framework that allows a Python programmer to work in terms of useful abstractions - using a single programming language. . This package contains interfaces for which different implementations can be supplied. These implementations are called from the core Reahl framework. ',
+    maintainer='Iwan Vosloo',
+    maintainer_email='iwan@reahl.org',
+    packages=['reahl'],
+    py_modules=['setup'],
+    include_package_data=False,
+    package_data={'': [u'*/LC_MESSAGES/*.mo']},
+    namespace_packages=['reahl'],
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[u'reahl-tofu>=2.0.0a3,<2.1', u'reahl-stubble>=2.0.0a3,<2.1'],
+    test_suite='tests',
+    entry_points={
+        'reahl.eggs': [
+            u'Egg = reahl.component.eggs:ReahlEgg'    ],
+                 },
+    extras_require={}
+)
