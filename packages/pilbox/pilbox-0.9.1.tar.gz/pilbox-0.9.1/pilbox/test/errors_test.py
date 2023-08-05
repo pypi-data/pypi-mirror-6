c48a7970cdffb7cbf983f6e7577db2afa69c6e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:10 +0000

    Add package pilbox/0.9.1/pilbox-0.9.1.tar.gz

---
+++ packages/pilbox/pilbox-0.9.1.tar.gz/pilbox-0.9.1/pilbox/test/errors_test.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, division, with_statement
+
+from tornado.test.util import unittest
+
+from pilbox.errors import *
+
+
+class ErrorsTest(unittest.TestCase):
+
+    def test_unique_error_codes(self):
+        errors = [SignatureError, ClientError, HostError, BackgroundError,
+                  DimensionsError, FilterError, FormatError, ModeError,
+                  PositionError, QualityError, UrlError, ImageFormatError,
+                  FetchError, DegreeError, OperationError, RectangleError]
+        codes = []
+        for error in errors:
+            code = str(error.get_code())
+            if code in codes:
+                self.fail("The error code, %s, is repeated" % str(code))
+            codes.append(code)
+
+    def test_base_not_implemented(self):
+        self.assertRaises(NotImplementedError, PilboxError.get_code)
