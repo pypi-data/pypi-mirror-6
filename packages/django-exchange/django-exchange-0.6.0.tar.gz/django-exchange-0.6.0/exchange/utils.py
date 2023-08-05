dc97a2ecadaa4a7c8fa6bb38434415a4081ccd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package django-exchange/0.6.0/django-exchange-0.6.0.tar.gz

---
+++ packages/django-exchange/django-exchange-0.6.0.tar.gz/django-exchange-0.6.0/exchange/utils.py
@@ -0,0 +1,21 @@
+def import_class(class_path):
+    """imports and returns given class string.
+
+    :param class_path: Class path as string
+    :type class_path: str
+
+    :returns: Class that has given path
+    :rtype: class
+
+    :Example:
+
+    >>> import_class('collections.OrderedDict').__name__
+    'OrderedDict'
+    """
+    try:
+        from django.utils.importlib import import_module
+        module_name = '.'.join(class_path.split(".")[:-1])
+        mod = import_module(module_name)
+        return getattr(mod, class_path.split(".")[-1])
+    except Exception, detail:
+        raise ImportError(detail)
