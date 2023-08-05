f0f4c2d3c29806b608e2b9e9a057cfce52bfc57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package pythomics/0.1.3/pythomics-0.1.3.tar.gz

---
+++ packages/pythomics/pythomics-0.1.3.tar.gz/pythomics-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pythomics',
+      version='0.1.3',
+      description='A multi-omic python package',
+      url='https://github.com/pandeylab/pythomics',
+      author='Chris Mitchell',
+      author_email='cmitch48@jhmi.edu',
+      license='GPL3',
+      packages=['pythomics','pythomics.parsers', 'pythomics.proteomics',
+                'pythomics.templates', 'pythomics.tests'],
+      scripts=['pythomics/scripts/fastadigest.py'],
+      zip_safe=False)
