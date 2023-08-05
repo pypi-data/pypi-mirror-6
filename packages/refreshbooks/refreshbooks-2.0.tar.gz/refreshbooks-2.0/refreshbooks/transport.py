7e0bde182a8cba756908ba850101b9e152ea6ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:12 +0000

    Add package refreshbooks/2.0/refreshbooks-2.0.tar.gz

---
+++ packages/refreshbooks/refreshbooks-2.0.tar.gz/refreshbooks-2.0/refreshbooks/transport.py
@@ -0,0 +1,72 @@
+import base64
+
+from refreshbooks import exceptions
+
+try:
+    from refreshbooks.optional import oauth as os
+    
+    OAuthAuthorization = os.OAuthAuthorization
+except ImportError:
+    def OAuthAuthorization(consumer, token, sig_method=None):
+        raise NotImplementedError('oauth support requires the "oauth" module.')
+
+try:
+    from refreshbooks.transports import use_requests as transport
+except ImportError:
+    try:
+        from refreshbooks.transports import use_httplib2 as transport
+    except ImportError:
+        import warnings
+        warnings.warn(
+            "Unable to load requests or httplib2 transports, falling back to urllib2. SSL cert verification disabled."
+        )
+        from refreshbooks.transports import use_urllib2 as transport
+
+class TokenAuthorization(object):
+    """Generates HTTP BASIC authentication headers obeying FreshBooks'
+    token-based auth scheme (token as username, password irrelevant).
+    
+        >>> auth = TokenAuthorization("monkey")
+        >>> auth()
+        {'Authorization': 'Basic bW9ua2V5Og=='}
+    
+    Prefer OAuthAuthorization, from refreshbooks.optional.oauth, for new
+    development.
+    """
+    def __init__(self, token):
+        try:
+            token = token.encode('US-ASCII')
+        except NameError:
+            # token already byte string.
+            pass
+        # See RFC 2617.
+        base64_user_pass = base64.b64encode(token + b':').decode('US-ASCII')
+        
+        self.headers = {
+            'Authorization': 'Basic %s' % (base64_user_pass, )
+        }
+    
+    def __call__(self):
+        return self.headers
+
+class UserAgentHeaders(object):
+    def __init__(self, base_headers_factory, user_agent):
+        self.base_headers_factory = base_headers_factory
+        self.user_agent = user_agent
+    
+    def __call__(self):
+        headers = self.base_headers_factory()
+        headers['User-Agent'] = self.user_agent
+        return headers
+
+class KeepAliveHeaders(object):
+    def __init__(self, base_headers_factory):
+        self.base_headers_factory = base_headers_factory
+    
+    def __call__(self):
+        headers = self.base_headers_factory()
+        headers['Connection'] = 'Keep-Alive'
+        return headers
+
+HttpTransport = transport.Transport
+TransportException = exceptions.TransportException
---
+++ packages/refreshbooks/refreshbooks-2.0.tar.gz/refreshbooks-2.0/setup.py
@@ -0,0 +1,49 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='refreshbooks',
+    version='2.0',
+    author='Owen Jacobson',
+    author_email='api@freshbooks.com',
+    url='http://github.com/freshbooks/refreshbooks',
+    download_url='http://pypi.python.org/pypi/refreshbooks/',
+    description='A client library for the FreshBooks API',
+    
+    long_description=open("README.rst").read(),
+    
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    
+    packages=[
+        'refreshbooks', 'refreshbooks.transports', 'refreshbooks.optional'
+    ],
+    
+    setup_requires=[
+        'setuptools-git',
+        'nose'
+    ],
+    tests_require=[
+        'nose',
+        'mock'
+    ],
+    install_requires=[
+        'lxml',
+    ],
+    extras_require={
+        'oauth': ['oauth'],
+        'httplib2': ['httplib2 >= 0.7.2'],
+        'requests': ['requests >= 0.11.1']
+    },
+    
+    test_suite = 'nose.collector'
+)
