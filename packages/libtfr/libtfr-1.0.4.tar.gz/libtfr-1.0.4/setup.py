044b701ec0a90181b301bc54edc8fe2815d09bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:53 +0000

    Add package libtfr/1.0.4/libtfr-1.0.4.tar.gz

---
+++ packages/libtfr/libtfr-1.0.4.tar.gz/libtfr-1.0.4/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# -*- mode: python -*-
+from setuptools import setup, find_packages
+from numpy.distutils.core import setup, Extension
+import os, sys
+
+sources = ['libtfr.c','tfr.c','mtm.c']
+ext_libs = []
+ext_libdirs = []
+ext_incl = []
+compiler_defines = []
+compiler_args = []
+package_data = []
+
+if hasattr(os, 'uname'):
+    system = os.uname()[0]
+else:
+    system = 'Windows'
+
+if system == 'Darwin':
+    ext_libs.extend(('lapack','fftw3'))
+    ext_incl.append('/opt/local/include')
+elif system == 'Linux':
+    ext_libs.extend(('lapack','fftw3'))
+elif system == 'Windows':
+    ext_incl.append('fftw3')
+    ext_libs.append('fftw3-3')
+    ext_libdirs.append('fftw3')
+    compiler_defines.append(('NO_LAPACK', None))
+    package_data.append(('', ['fftw3/libfftw3-3.dll']))
+
+cls_txt = """
+Development Status :: 5 - Production/Stable
+Intended Audience :: Science/Research
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Operating System :: Unix
+Operating System :: POSIX :: Linux
+Operating System :: MacOS :: MacOS X
+Natural Language :: English
+"""
+
+short_desc = "Calculates multi-taper windowed and time-frequency reassignment spectrograms"
+
+long_desc = """
+libtfr provides high-performance C and Python libraries for
+calculating multitaper time-frequency reassignment (TFR) spectrograms
+as well as conventional STFTs.  TFR spectrograms have enhanced
+signal-to-noise characteristics and can provide very precise spectral
+estimates under many conditions. The library requires FFTW for the underlying
+FFT transformations.
+"""
+
+setup(
+    name= 'libtfr',
+    version= "1.0.4",
+    py_modules= ['libtfr'],
+    ext_modules= [Extension('_libtfr',
+                             sources=sources,
+                             define_macros=compiler_defines,
+                             libraries=ext_libs,
+                             extra_compile_args=compiler_args,
+                             include_dirs=ext_incl,
+                             library_dirs=ext_libdirs)],
+
+    description= short_desc,
+    long_description= long_desc,
+    author= 'C Daniel Meliza',
+    author_email= '"dan" at the domain "meliza.org"',
+    maintainer= 'C Daniel Meliza',
+    maintainer_email= '"dan" at the domain "meliza.org"',
+    url= 'http://melizalab.github.com/libtfr',
+    download_url= 'https://github.com/downloads/melizalab/libtfr',
+    data_files= package_data,
+    zip_safe= False
+)
+
+
+# Variables:
+# End:
---
+++ packages/libtfr/libtfr-1.0.4.tar.gz/libtfr-1.0.4/test_tfr.c
@@ -0,0 +1,110 @@
+/*
+ * @file   test_tfr.c
+ * @author C Daniel Meliza <dmeliza@uchicago.edu>
+ * @date   Mon Mar  1 13:35:27 2010
+ *
+ * Test program for tfr library
+ *
+ * Copyright C Daniel Meliza 2010.  Licensed for use under GNU
+ * General Public License, Version 2.  See COPYING for details.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "tfr.h"
+
+int npoints = 17590;
+int N = 256;
+int Np = 201;
+double NW = 3.5;
+int step = 10;
+int k = 6;
+double tm = 6.0;
+
+void
+fmsin(double *val, int N, double fnormin, double fnormax, double period, double t0,
+      double fnorm0, double pm1)
+{
+        double fnormid, delta, phi;
+        int t;
+
+        fnormid = 0.5 * (fnormax+fnormin);
+        delta = 0.5 * (fnormax-fnormin);
+        phi = - copysign(1.0,pm1) * acos((fnorm0 - fnormid)/delta);
+
+        for (t = 0; t < N; t++) {
+                complex double phase = 2 * M_PI * fnormid * (t - t0) +
+                        delta * period * (sin(2 * M_PI * (t - t0) / period + phi) - sin(phi));
+                val[t] = creal(cexp(I * phase));
+        }
+}
+
+/* output a tab-delimited file */
+void
+write_file(char const * fn, double *buf, int nrow, int ncol)
+{
+        FILE *fp = fopen(fn, "wt");
+        for (int i = 0; i < nrow; ++i) {
+                for (int j = 0; j+1 < ncol; ++j) {
+                        fprintf(fp, "%3.4f\t", *buf);
+                        ++buf;
+                }
+                fprintf(fp, "%.6g\n", *buf);
+                ++buf;
+        }
+        fclose(fp);
+}
+
+
+int
+main(int argc, char **argv)
+{
+        mfft *mtmh;
+        double *sig, *psd, *specgram;
+        double sigpow;
+
+        printf("* Testing TFR library:\n");
+        printf("* N = %d\n", N);
+        printf("* shift = %d\n", step);
+        printf("* MTM NW = %3.2f\n", NW);
+        printf("* TFR Np = %d\n", Np);
+        printf("* TFR k = %d\n", k);
+        printf("* TFR tm = %3.2f\n", tm);
+
+        sig = (double*)malloc(17590 * sizeof(double));
+
+        fmsin(sig, npoints, 0.15, 0.45, 1024, 256./4, 0.3, -1);
+
+        printf("* Input signal to tfr_in.dat\n");
+        write_file("tfr_in.dat", sig, npoints, 1);
+
+        mtmh = mtm_init_dpss(N, NW, (int)(NW*2-1));
+
+        printf("* MTM PSD to tfr_out_psd\n");
+        psd = (double*)malloc(N * sizeof(double));
+        sigpow = mtfft(mtmh, sig+8300, N);
+        mtpower(mtmh, psd, sigpow);
+        write_file("tfr_out_psd.dat", psd, N, 1);
+        free(psd);
+
+	const int l = (npoints - Np + 1) / step;
+        printf("* MTM spectrogram to tfr_out_mtm\n");
+        specgram = (double*)calloc(l * (N/2+1), sizeof(double));
+        mtm_spec(mtmh, specgram, sig, npoints, step, 1);
+        write_file("tfr_out_mtm.dat", specgram, l, (N/2+1));
+        free(specgram);
+
+        mtm_destroy(mtmh);
+
+        printf("* TFR spectrogram to tfr_out_tfr\n");
+        mtmh = mtm_init_herm(N, Np, k, tm);
+        specgram = (double*)calloc(l * (N/2+1), sizeof(double));
+        tfr_spec(mtmh, specgram, sig, npoints, -1, step, 0.01, 5, 0, NULL);
+        write_file("tfr_out_tfr.dat", specgram, l, (N/2+1));
+        free(specgram);
+
+        mtm_destroy(mtmh);
+
+        free(sig);
+}
