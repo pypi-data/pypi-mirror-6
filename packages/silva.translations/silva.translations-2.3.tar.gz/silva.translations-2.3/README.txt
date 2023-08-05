92ef719b70078a3baddab2c226b773f3a99ac269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package silva.translations/2.3/silva.translations-2.3.tar.gz

---
+++ packages/silva.translations/silva.translations-2.3.tar.gz/silva.translations-2.3/README.txt
@@ -0,0 +1,8 @@
+==================
+silva.translations
+==================
+
+``silva.translations`` contains all translations for the `Silva CMS`_ in many languages.
+
+.. _Silva CMS: http://infrae.com/products/silva
+
---
+++ packages/silva.translations/silva.translations-2.3.tar.gz/silva.translations-2.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.3'
+
+setup(name='silva.translations',
+      version=version,
+      description="Translations files in many languages for the Silva CMS",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='silva translations',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='https://github.com/silvacms/silva.translations',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.i18nmessageid',
+          'zope.i18n',
+          ],
+      )
