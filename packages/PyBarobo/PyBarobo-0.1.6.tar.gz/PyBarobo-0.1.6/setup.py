b432160f9059407b2a7a6ebaa1dd86d78a423989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:08 +0000

    Add package PyBarobo/0.1.6/PyBarobo-0.1.6.tar.gz

---
+++ packages/PyBarobo/PyBarobo-0.1.6.tar.gz/PyBarobo-0.1.6/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PyBarobo',
+    version='0.1.6',
+    description='Native Python Barobo robotics control library',
+    author='David Ko',
+    author_email='david@barobo.com',
+    maintainer='David Ko',
+    maintainer_email='david@barobo.com',
+    url='http://www.barobo.com',
+    license='GPL',
+    platforms='any',
+    packages=['barobo']
+    #py_modules=['pybarobo']
+    )
