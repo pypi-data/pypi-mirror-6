6e861ec62e3e64e472265ffcc2fa9ede0085ccd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:24 +0000

    Add package blobuploader/1.0.1b/blobuploader-1.0.1b.tar.gz

---
+++ packages/blobuploader/blobuploader-1.0.1b.tar.gz/blobuploader-1.0.1b/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="blobuploader",
+      version="1.0.1b",
+      author="Mihai Tabara",
+      author_email="mtabara@mozilla.com",
+      url="https://github.com/catlee/blobber",
+      scripts = ["blobberc.py"],
+      license="MPL",
+      install_requires=["requests==1.2.3.",
+                        "docopt==0.6.1"],
+      description="Specific client for uploading blob files on Mozilla server",
+      packages=['blobuploader'],
+      package_dir={'blobuploader': 'blobuploader'},
+      package_data={'blobuploader': ['*.pem']},
+      include_package_data=True,
+      )
