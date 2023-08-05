d584889892266d3ac1175f6022bc8faae11be407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package ioflo/0.9.05/ioflo-0.9.05.tar.gz

---
+++ packages/ioflo/ioflo-0.9.05.tar.gz/ioflo-0.9.05/ioflo/base/__init__.py
@@ -0,0 +1,18 @@
+""" base package
+
+
+"""
+#print "\nPackage at %s" % __path__[0]
+
+__all__ = ['globaling', 'aiding', 'excepting', 'interfacing',
+           'registering', 'storing', 'skedding', 
+           'tasking', 'framing', 'logging', 'serving', 'monitoring',
+           'acting', 'poking', 'goaling', 'needing', 'traiting',
+           'fiating', 'wanting','completing','deeding', 'arbiting',
+           'housing', 'building',  'testing'] 
+
+
+for m in __all__:
+    exec "from . import %s" % m  #relative import
+    #print "Imported %s" % globals().get(m,'')
+
