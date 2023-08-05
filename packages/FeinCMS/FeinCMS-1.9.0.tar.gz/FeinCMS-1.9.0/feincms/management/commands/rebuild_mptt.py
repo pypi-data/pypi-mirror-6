074e3360caf2e9f4231d6104e539149daf9cca8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package FeinCMS/1.9.0/FeinCMS-1.9.0.tar.gz

---
+++ packages/FeinCMS/FeinCMS-1.9.0.tar.gz/FeinCMS-1.9.0/feincms/management/commands/rebuild_mptt.py
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------
+# coding=utf-8
+# ------------------------------------------------------------------------
+"""
+``rebuild_mptt``
+---------------------
+
+``rebuild_mptt`` rebuilds your mptt pointers. Only use in emergencies.
+"""
+from __future__ import print_function
+
+from django.core.management.base import NoArgsCommand
+
+from feincms.module.page.models import Page
+
+
+class Command(NoArgsCommand):
+    help = (
+        "Run this manually to rebuild your mptt pointers. Only use in"
+        " emergencies.")
+
+    def handle_noargs(self, **options):
+        print("Rebuilding MPTT pointers for Page")
+        Page._tree_manager.rebuild()
