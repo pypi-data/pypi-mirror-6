141ebbcd5c0a75afb93a0e5e21ec39a5b7acd189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:38 +0000

    Add package django-primitivegallery/0.0.6/django-primitivegallery-0.0.6.tar.gz

---
+++ packages/django-primitivegallery/django-primitivegallery-0.0.6.tar.gz/django-primitivegallery-0.0.6/primitivegallery/models.py
@@ -0,0 +1,220 @@
+import os
+import subprocess
+from datetime import datetime
+from django.conf import settings
+from django.db import models
+from os.path import basename, dirname, exists, isfile, join
+from PIL.ExifTags import TAGS
+from PIL.Image import open
+from django.utils import timezone
+
+VIDEO_EXTS = ['.mp4', '.webm', '.mov', '.ogv', '.flv', '.avi', '.3gp', '.mpg',
+              '.qt', '.3g2', '.wmv', '.m4v']
+
+
+class Image(models.Model):
+    path = models.CharField(max_length=500, unique=True)
+    datestamp = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(default=0)
+    datetaken = models.DateTimeField(null=True)
+
+    def name(self, size=None):
+        if size:
+            ret = '.' + size + '_' + basename(self.path)
+
+            if self.is_video():
+                ret += '.jpg'
+
+            return ret
+
+        return basename(self.path)
+
+    def dir(self):
+        return dirname(self.path)
+
+    def url(self, size=''):
+        file = self.name(size)
+        path = join(settings.PRIMITIVE_GALLERY['IMAGE_URL'], self.dir(), file)
+        if exists(self.local(size)):
+            return path
+        if self.status != 0:
+            self.status = 0
+            self.save()
+        if not exists(self.local()):
+            print self.path
+            self.delete()
+        return join(settings.STATIC_URL, 'primitivegallery/img/placeholder_' + size + '.png')
+
+    def local(self, size=''):
+        file = self.name(size)
+        return join(settings.PRIMITIVE_GALLERY['IMAGE_ROOT'], self.dir(), file)
+
+    def is_video(self):
+        ext = os.path.splitext(self.path)[1]
+        return ext in VIDEO_EXTS
+
+    def process(self):
+        if self.status > 0:
+            return
+
+        filepath = self.local()
+        mediumpath = self.local('medium')
+        smallpath = self.local('small')
+        thumbpath = self.local('thumb')
+
+        self.status = 1
+
+        self._autorot(filepath)
+
+        if not exists(mediumpath):
+            retcode = self._create_resized(filepath, mediumpath, '900x600')
+            if retcode > 0:
+                self.status = 2
+
+        if not exists(smallpath):
+            retcode = self._create_resized(mediumpath, smallpath, '480x320')
+            if retcode > 0:
+                self.status = 2
+
+        if not exists(thumbpath):
+            retcode = self._create_thumbnail(smallpath, thumbpath, '100x100')
+            if retcode > 0:
+                self.status = 2
+
+        try:
+            self.datetaken = self.exiftaken()
+        except (IOError, AttributeError):
+            pass
+        self.save()
+
+    def _autorot(self, infile):
+        return subprocess.call(["jhead", '-autorot', infile])
+
+    def _create_thumbnail(self, infile, outfile, size):
+        if self.is_video():
+            return self._create_videothumb(infile, outfile, size)
+
+        return subprocess.call([
+            'convert', infile,
+            '-thumbnail', size + '^',
+            '-gravity', 'center',
+            '-extent', size,
+            outfile
+        ])
+
+    def _create_resized(self, infile, outfile, size):
+        if self.is_video():
+            return self._create_videothumb(infile, outfile, size)
+
+        return subprocess.call([
+            'convert', infile,
+            '-resize', size,
+            '-unsharp', '1.0x1.0+0.5+0.10',
+            outfile
+        ])
+
+    def _create_videothumb(self, infile, outfile, size):
+        return subprocess.call([
+            'ffmpeg',
+            '-i', infile,
+            '-vframes', '1',
+            '-an',
+            '-s', size,
+            '-ss', '1',
+            outfile
+        ])
+
+    def taken(self):
+        if self.datetaken:
+            return self.datetaken
+        return timezone.now()
+
+    def exiftaken(self):
+        i = open(self.local())
+        info = i._getexif()
+        if info:
+            for tag, value in info.items():
+                decoded = TAGS.get(tag, tag)
+                if decoded == 'DateTimeOriginal':
+                    return datetime.strptime(value, '%Y:%m:%d %H:%M:%S')
+        return timezone.now()
+
+    def __unicode__(self):
+        return self.path
+
+
+class Directory:
+
+    def __init__(self, subfolder):
+        self.subfolder = subfolder
+
+    def get_image(self, localpath, path):
+        contained_files = os.listdir(localpath)
+        for c_file in contained_files:
+            c_localpath = join(localpath, c_file)
+            c_path = join(path, c_file)
+            if c_file[0] != '.' and isfile(c_localpath):
+                i, created = Image.objects.get_or_create(path=c_path)
+                if i.status == 1:
+                    return i
+            elif c_file[0] != '.':
+                sub = self.get_image(c_localpath, c_path)
+                if sub:
+                    return sub
+        return None
+
+    def count(self, localpath):
+        imgcount = 0
+        dircount = 0
+        contained_files = os.listdir(localpath)
+        for c_file in contained_files:
+            c_localpath = join(localpath, c_file)
+            if c_file[0] == '.':
+                continue
+            if isfile(c_localpath):
+                imgcount += 1
+            else:
+                dircount += 1
+        return (imgcount, dircount)
+
+    def list(self):
+        dir = join(settings.PRIMITIVE_GALLERY['IMAGE_ROOT'], self.subfolder)
+        list = os.listdir(dir)
+        ret = []
+        for file in list:
+            if file[0] == '.':
+                continue
+            localpath = join(dir, file)
+            path = join(self.subfolder, file)
+            if isfile(localpath):
+                i, created = Image.objects.get_or_create(path=path)
+                ret.append({'isfile': True,
+                            'name': i.name(),
+                            'url': i.url(),
+                            'medium': i.url('medium'),
+                            'small': i.url('small'),
+                            'thumbnail': i.url('thumb'),
+                            'taken': i.taken(),
+                            'dir': i.dir(),
+                            'id': i.pk,
+                            })
+            else:
+                name = file.replace('_', ' ')
+                folder = {'isfile': False,
+                          'name': name,
+                          'url': path,
+                          'taken': timezone.now(),
+                          }
+                i = self.get_image(localpath, path)
+                if i:
+                    folder['medium'] = i.url('medium')
+                    folder['small'] = i.url('small')
+                    folder['thumbnail'] = i.url('thumb')
+                    folder['taken'] = i.taken()
+
+                imgcount, dircount = self.count(localpath)
+                folder['imgcount'] = imgcount
+                folder['dircount'] = dircount
+                ret.append(folder)
+        ret.sort(key=lambda file: file['taken'])
+        return ret
---
+++ packages/django-primitivegallery/django-primitivegallery-0.0.6.tar.gz/django-primitivegallery-0.0.6/setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+def get_author(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__author__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+author = get_author('primitivegallery')
+version = get_version('primitivegallery')
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+LONG_DESCRIPTION = open('README.rst').read()
+
+setup(
+    name='django-primitivegallery',
+    version=version,
+    description="A simple image gallery.",
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='django,gallery,images',
+    author=author,
+    author_email='ludvigw@gmail.com',
+    license='MIT',
+    url='https://github.com/ludw/django-primitivegallery',
+    packages=get_packages('primitivegallery'),
+    include_package_data=True,
+    install_requires=[
+        'django>=1.5.0',
+    ],
+    zip_safe=False,
+)
