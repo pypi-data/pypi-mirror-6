06486f4949b85b98900c5b453f26b2b2869231bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package vertica-python/0.2.0/vertica-python-0.2.0.tar.gz

---
+++ packages/vertica-python/vertica-python-0.2.0.tar.gz/vertica-python-0.2.0/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import collections
+from setuptools import setup, find_packages
+
+from pip.req import parse_requirements
+
+dependency_links = []
+install_requires = []
+
+ReqOpts = collections.namedtuple('ReqOpts', ['skip_requirements_regex', 'default_vcs'])
+
+opts = ReqOpts(None, 'git')
+
+for ir in parse_requirements("requirements.txt", options=opts):
+    if ir is not None:
+        if ir.url is not None:
+            dependency_links.append(str(ir.url))
+        if ir.req is not None:
+            install_requires.append(str(ir.req))
+
+setup(
+    name='vertica-python',
+    version='0.2.0',
+    description='A native Python client for the Vertica database.',
+    author='Justin Berka, Alex Kim',
+    author_email='justin.berka@gmail.com, alex.kim@uber.com',
+    url='https://github.com/uber/vertica-python/',
+    keywords="database vertica",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Operating System :: OS Independent"
+    ]
+)
