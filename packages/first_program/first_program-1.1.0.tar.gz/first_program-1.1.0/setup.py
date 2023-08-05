8e367fb6654537b11ce63d6b7c89fe649b07d354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package first_program/1.1.0/first_program-1.1.0.tar.gz

---
+++ packages/first_program/first_program-1.1.0.tar.gz/first_program-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name           ='first_program',
+        version        ='1.1.0',
+        py_modules     =['first'],
+        author         ='marquespython',
+        author_email   ='luquinhasmarques09@gmail.com',
+        url            ='http://www.headfirstlabs.com',
+        description    ='A simple printer of nested list',
+    )
