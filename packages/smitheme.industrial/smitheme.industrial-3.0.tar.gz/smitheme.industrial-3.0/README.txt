33b5e937d0df85cf6b8453647f901002978942fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:42 +0000

    Add package smitheme.industrial/3.0/smitheme.industrial-3.0.tar.gz

---
+++ packages/smitheme.industrial/smitheme.industrial-3.0.tar.gz/smitheme.industrial-3.0/README.txt
@@ -0,0 +1,22 @@
+===================
+smitheme.industrial
+===================
+
+``smitheme.industrial`` is an alternative demo skin for the `Silva`_
+Management Interface in Silva 3.x.
+
+*This skin is not actively maintained and seems broken on some
+screens*.
+
+For more information on how to make your own skin, please check the
+`Silva developer documentation`_.
+
+Code repository
+===============
+
+You can find the code of this extension in Git:
+https://github.com/silvacms/smitheme.industrial
+
+.. _Silva: http://silvacms.org
+.. _Silva developer documentation: http://docs.silvacms.org/latest
+
---
+++ packages/smitheme.industrial/smitheme.industrial-3.0.tar.gz/smitheme.industrial-3.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012 Infrae. All rights reserved.x
+
+from setuptools import setup, find_packages
+import os
+
+version = '3.0'
+
+setup(name='smitheme.industrial',
+      version=version,
+      description="Alternate (original) SMI Skin for Silva 3.0",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='skin silva smi',
+      author='Infrae',
+      author_email='info@infrae.com',
+      url='https://github.com/silvacms/smitheme.industrial',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      namespace_packages=['smitheme'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'five.grok',
+          'js.jqueryui',
+          'megrok.pagetemplate',
+          'setuptools',
+          'silva.core.conf',
+          'silva.core.interfaces',
+          'silva.ui',
+          'zope.interface',
+          'zope.publisher',
+          ],
+      )
