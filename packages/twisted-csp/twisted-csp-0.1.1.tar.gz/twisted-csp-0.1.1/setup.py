ecf26dc0e6ca49ba6d869002e9d40432061b96c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:54 +0000

    Add package twisted-csp/0.1.1/twisted-csp-0.1.1.tar.gz

---
+++ packages/twisted-csp/twisted-csp-0.1.1.tar.gz/twisted-csp-0.1.1/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name="twisted-csp",
+      version="0.1.1",
+      description="Go-style channels for Twisted",
+      author="Nguyễn Tuấn Anh",
+      author_email="ubolonton@gmail.com",
+      url="https://github.com/ubolonton/twisted-csp",
+      long_description=open('README.md').read(),
+
+      install_requires=[
+          "twisted"
+      ]
+)
