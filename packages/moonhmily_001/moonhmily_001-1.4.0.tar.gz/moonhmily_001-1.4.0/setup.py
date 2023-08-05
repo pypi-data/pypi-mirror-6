85bad84075de82eeb65a9338875b4c03f8e9adbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:56 +0000

    Add package moonhmily_001/1.4.0/moonhmily_001-1.4.0.tar.gz

---
+++ packages/moonhmily_001/moonhmily_001-1.4.0.tar.gz/moonhmily_001-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup ( 
+	name		='moonhmily_001',
+	version		='1.4.0',
+	py_modules	=['nester'],
+	author		='yql',
+	author_email	='yangqinglin_0723@163.com',
+	url		='http://www.163.com',
+	description	='A simple printer',
+	)
