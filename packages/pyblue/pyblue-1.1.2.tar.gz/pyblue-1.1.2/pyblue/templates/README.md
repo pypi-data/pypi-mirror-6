c939f74a1a36b0785bf9f9204c42b60d59f4f847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:55 +0000

    Add package pyblue/1.1.2/pyblue-1.1.2.tar.gz

---
+++ packages/pyblue/pyblue-1.1.2.tar.gz/pyblue-1.1.2/pyblue/templates/README.md
@@ -0,0 +1,5 @@
+Templates
+---------
+
+This directory is automatically added to the template import paths.
+
---
+++ packages/pyblue/pyblue-1.1.2.tar.gz/pyblue-1.1.2/sites/demo/settings.py
@@ -0,0 +1,6 @@
+# optional module named settings.py
+
+data = [
+    dict(title="Publication Title 1", author="John Doe", url="#"),
+    dict(title="Publication Title 2", author="Jane Doe", url="#"),
+]
