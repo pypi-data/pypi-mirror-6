6ebf6fe6737888c678ec7c0c760837b8512e6d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package inlist/1.0.1/inlist-1.0.1.zip

---
+++ packages/inlist/inlist-1.0.1.zip/inlist-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'inlist', 
+        version      = '1.0.1', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['inlist'],
+        author       = 'huongnh',
+        author_email = 'huongnh1812@gmail.com',
+        url          = 'http://www.faccebook.com/huongnh',
+        description  = 'A simple printer of lists',
+     )
