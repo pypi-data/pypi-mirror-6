7675e260170e16b84bd772f22d5374fb035340da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:54 +0000

    Add package solrcloudpy/1.0.1/solrcloudpy-1.0.1.tar.gz

---
+++ packages/solrcloudpy/solrcloudpy-1.0.1.tar.gz/solrcloudpy-1.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+    
+setup(
+    name = "solrcloudpy",
+    version = "1.0.1",
+    author = 'Didier Deshommes',
+    author_email = 'dfdeshom@gmail.com',
+    packages=find_packages(exclude=['ez_setup']),
+    url='https://github.com/dfdeshom/solrcloudpy',
+    license='LICENSE.txt',
+    keywords='solr solrcloud',
+    description='python library for interacting with SolrCloud ',
+    long_description=long_description,
+    include_package_data=True,
+    platforms='any',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search'
+        ],
+        
+    install_requires = ['requests',]
+        )
