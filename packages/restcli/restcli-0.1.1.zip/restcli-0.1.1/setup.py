1c3be4f65662ec21fa07cccfccc46a84627d5608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:04 +0000

    Add package restcli/0.1.1/restcli-0.1.1.zip

---
+++ packages/restcli/restcli-0.1.1.zip/restcli-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import restcli
+
+setup(
+    name='restcli',
+    version=restcli.__version__,
+    packages=find_packages(exclude='test'),
+    entry_points={
+        'console_scripts': ['restcli = restcli.restcli:main']
+    },
+    install_requires=['requests>=2.0.0', 'Pygments>=1.6', 'colorama>=0.2.5'],
+    package_data={
+        'restcli': ['*.conf'],
+    },
+
+    author='jykntr',
+    author_email='jykntr@gmail.com',
+    description='Command line REST request tool',
+    license='MIT',
+    keywords='rest http',
+    url='https://github.com/jykntr/rest-cli-client',
+    download_url='https://github.com/jykntr/rest-cli-client/releases/tag/v' + restcli.__version__
+)
