04ec6a093ee366fedadd6d4824e349546ec4b4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:28 +0000

    Add package pytest-pyramid/0.1.1/pytest_pyramid-0.1.1.tar.gz

---
+++ packages/pytest-pyramid/pytest_pyramid-0.1.1.tar.gz/pytest_pyramid-0.1.1/README.rst
@@ -0,0 +1,43 @@
+pytest_pyramid
+==============
+
+.. image:: https://travis-ci.org/fizyk/pytest_pyramid.png?branch=master
+    :target: https://travis-ci.org/fizyk/pytest_pyramid
+    :alt: Tests for RandomWords
+
+.. image:: https://coveralls.io/repos/fizyk/pytest_pyramid/badge.png?branch=master
+    :target: https://coveralls.io/r/fizyk/pytest_pyramid?branch=master
+    :alt: Coverage Status
+
+.. image:: https://requires.io/github/fizyk/pytest_pyramid/requirements.png?branch=master
+   :target: https://requires.io/github/fizyk/pytest_pyramid/requirements/?branch=master
+   :alt: Requirements Status
+
+pytest_pyramid provides basic fixtures for testing pyramid applications with pytest test suite.
+
+By default, pytest_pyramid will create two fixtures: pyramid_config, which creates configurator based on config.ini file, and pyramid_app, which creates TestApp based on Configurator returned by pyramid_config.
+
+Command line options
+--------------------
+
+* **--pc** - pyramid configuration file based on which pytest_pyramid will create test app
+
+Documentation
+-------------
+
+http://pytest-pyramid.readthedocs.org/en/latest/
+
+TODO
+----
+
+This goal should make it in to 1.0 major release.
+
+#. provide a pyramid_proc fixture that will start pyramid app process using summon_process
+
+
+Tests
+-----
+
+To run tests run this command:
+
+`py.test --pc tests/pyramid.test.ini`
---
+++ packages/pytest-pyramid/pytest_pyramid-0.1.1.tar.gz/pytest_pyramid-0.1.1/pytest_pyramid/factories.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2013 by pytest_pyramid authors and contributors <see AUTHORS file>
+#
+# This module is part of pytest_pyramid and is released under
+# the MIT License (MIT): http://opensource.org/licenses/MIT
+
+import pytest
+from webtest import TestApp
+from pyramid.config import Configurator
+try:
+    from ConfigParser import ConfigParser
+except ImportError:  # py3
+    from configparser import ConfigParser
+
+
+def pyramid_config(settings=None, config_path=None):
+    '''
+    A pyramid_config fixture factory, used to create aditional fixtures returning pyramid's :class:`~pyramid.config.Configurator` object
+
+    :param dict settings: optional parameter delivering
+        settings for building a Configurator object.
+    :param str config_path: optional parameter delivering path to pyramid configuration file
+
+    :returns: configuration
+    :rtype: `pyramid.config.Configurator`
+
+    '''
+
+    @pytest.fixture
+    def pyramid_config(request):
+        app_settings = settings
+        if app_settings is None:
+            config_parser = ConfigParser()
+            if config_path is None:
+                config_file = request.config.getvalue('pyramid_config')
+                config_parser.read(config_file)
+            else:
+                config_parser.read(config_path)
+            app_settings = {}
+            for option, value in config_parser.items('app:main'):
+                app_settings[option] = value
+
+        return Configurator(settings=app_settings)
+
+    return pyramid_config
+
+
+def pyramid_app(config_fixture_name):
+    '''
+    Pyramid_app fixture factory. Creates a TestApp instance based on
+
+    :param str config_fixture_name: name of a fixture creating
+        :class:`pyramid.config.Configurator`
+
+    :returns: TestApp based on pyramid's COnfigurator object as returned from
+        fixture passed by  **config_fixture_name**
+    :rtype: webtest.app.TestApp
+    '''
+
+    @pytest.fixture
+    def pyramid_app(request):
+        config = request.getfuncargvalue(config_fixture_name)
+
+        app = TestApp(config.make_wsgi_app())
+
+        return app
+
+    return pyramid_app
