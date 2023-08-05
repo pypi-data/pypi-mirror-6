ecd6a7d9dd1414fb24f5df7fb1531bd3b55d5a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:27 +0000

    Add package base32-crockford/0.2.0/base32-crockford-0.2.0.tar.gz

---
+++ packages/base32-crockford/base32-crockford-0.2.0.tar.gz/base32-crockford-0.2.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import base32_crockford
+
+
+package_data = {
+    '': ['LICENSE', 'README.rst'],
+}
+
+setup(
+    name='base32-crockford',
+    version='0.2.0',
+    description=("A Python implementation of Douglas Crockford's "
+                 "base32 encoding scheme"),
+    long_description=base32_crockford.__doc__,
+    license='BSD',
+    author='Jason Bittel',
+    author_email='jason.bittel@gmail.com',
+    url='https://github.com/jbittel/base32-crockford',
+    download_url='https://github.com/jbittel/base32-crockford/downloads',
+    py_modules=['base32_crockford'],
+    package_data=package_data,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
