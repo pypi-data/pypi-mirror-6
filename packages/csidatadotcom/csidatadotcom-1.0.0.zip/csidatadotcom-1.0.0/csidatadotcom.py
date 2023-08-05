52688752edd7c7eb4488983bc73a9bf60af89fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package csidatadotcom/1.0.0/csidatadotcom-1.0.0.zip

---
+++ packages/csidatadotcom/csidatadotcom-1.0.0.zip/csidatadotcom-1.0.0/csidatadotcom.py
@@ -0,0 +1,32 @@
+# Created by James Darabi (mail@jdarabi.com)
+# License: GNU LGPL
+
+__version__ = '1.0.0'
+
+import requests
+import re
+
+def get_end_of_day(type, exchangeid, active_only = True):
+
+  payload = {'type': type, 'format': 'csv', 'exchangeid': exchangeid}
+  r = requests.get('http://www.csidata.com/factsheets.php', params = payload)
+
+  string = r.text
+
+  re.sub('\"', '', string)
+
+  line_list = re.split('\r\n', string.encode('utf-8'))
+  del line_list[-1]
+
+  header = re.split(',', line_list[0])
+  del line_list[0]
+
+  out = []
+
+  for line in line_list:
+    values = re.split(',', line)
+    data = dict(zip(header, values))
+    if (not active_only) or (active_only and '1' == data['IsActive']):
+      out.append(data)
+
+  return out
---
+++ packages/csidatadotcom/csidatadotcom-1.0.0.zip/csidatadotcom-1.0.0/setup.py
@@ -0,0 +1,23 @@
+# Created by James Darabi (mail@jdarabi.com)
+# License: GNU LGPL
+
+import os
+import sys
+
+from distutils.core import setup
+
+from csidatadotcom import __version__
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+  name = 'csidatadotcom',
+  version = __version__,
+  description = 'Retrieve end-of-day market data from www.csidata.com',
+  long_description = read('README.md'),
+  author = 'James Darabi',
+  author_email = 'mail@jdarabi.com',
+  url = 'https://bitbucket.org/jamesdarabi/csidatadotcom',
+  py_modules = ['csidatadotcom']
+)
