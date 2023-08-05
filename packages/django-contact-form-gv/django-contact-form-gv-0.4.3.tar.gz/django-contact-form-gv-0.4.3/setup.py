bc30cbf0d1dec7659d276412fe80b033d4d5c5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package django-contact-form-gv/0.4.3/django-contact-form-gv-0.4.3.tar.gz

---
+++ packages/django-contact-form-gv/django-contact-form-gv-0.4.3.tar.gz/django-contact-form-gv-0.4.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+REQUIREMENTS = (
+    'django>=1.4',
+)
+TEST_REQUIREMENTS = (
+    'mock',
+    'django-debug-toolbar',
+)
+
+from contact_form import VERSION
+
+setup(
+    name="django-contact-form-gv",
+    version=VERSION,
+    author="Aaron Madison",
+    description="Django Contact Form using class based views.",
+    long_description=open('README', 'r').read(),
+    url="https://github.com/madisona/django-contact-form",
+    packages=find_packages(exclude=["example*"]),
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    tests_require=TEST_REQUIREMENTS,
+    test_suite='runtests.runtests',
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+)
