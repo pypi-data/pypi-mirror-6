42d0e249def388c9fe84c47c48ed271869648271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package ella/3.0.11/ella-3.0.11.tar.gz

---
+++ packages/ella/ella-3.0.11.tar.gz/ella-3.0.11/ella/__init__.py
@@ -0,0 +1,4 @@
+VERSION = (3, 0, 11)
+
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
---
+++ packages/ella/ella-3.0.11.tar.gz/ella-3.0.11/test_ella/urls.py
@@ -0,0 +1,8 @@
+try:
+    from django.conf.urls import patterns, include
+except ImportError:
+    from django.conf.urls.defaults import patterns, include
+
+urlpatterns = patterns('',
+    (r'^', include('ella.core.urls')),
+)
