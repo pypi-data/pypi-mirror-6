eeba01984ae7a687b7b115bbea0b0a5e575701d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:44 +0000

    Add package django-review/1.1/django-review-1.1.tar.gz

---
+++ packages/django-review/django-review-1.1.tar.gz/django-review-1.1/review/tests/urls.py
@@ -0,0 +1,13 @@
+"""URLs to run the tests."""
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^umedia/', include('user_media.urls')),
+    url(r'^review/', include('review.urls')),
+)
