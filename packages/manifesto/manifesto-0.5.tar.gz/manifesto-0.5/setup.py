044132612aafd84347676d5ef45c20e6ff8dd4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:21 +0000

    Add package manifesto/0.5/manifesto-0.5.tar.gz

---
+++ packages/manifesto/manifesto-0.5.tar.gz/manifesto-0.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='manifesto',
+    version='0.5',
+    description='Pluggable cache manifest for Django.',
+    author='Timothee Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    url='https://github.com/cyberdelia/manifesto',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
