dd168b74d253cb508783a900f92c811d142c30dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:17 +0000

    Add package decorated/1.2.7/decorated-1.2.7.tar.gz

---
+++ packages/decorated/decorated-1.2.7.tar.gz/decorated-1.2.7/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='decorated',
+    version='1.2.7',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='Base components for easily writing decorators.',
+    install_requires=[
+        'six',
+    ],
+    packages=[
+        'decorated',
+        'decorated.base',
+        'decorated.decorators'
+    ],
+    url='https://github.com/CooledCoffee/decorated/',
+)
