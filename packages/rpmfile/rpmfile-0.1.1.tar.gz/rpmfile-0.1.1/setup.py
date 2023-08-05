c9998e1e2d56fb916324b5860894330feeed9088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:33 +0000

    Add package rpmfile/0.1.1/rpmfile-0.1.1.tar.gz

---
+++ packages/rpmfile/rpmfile-0.1.1.tar.gz/rpmfile-0.1.1/setup.py
@@ -0,0 +1,23 @@
+'''
+@author: sean
+'''
+
+from setuptools import setup, find_packages
+from os.path import isfile
+
+if isfile('README.md'):
+    long_description = open('README.md').read()
+else:  
+    long_description = '???'
+
+setup(
+    name='rpmfile',
+    description='Read rmp archive files',
+    version="0.1.1",
+    author='Sean Ross-Ross',
+    author_email='srossross@gmail.com',
+    url='https://github.com/srossross/rpmfile',
+    license='MIT',
+    long_description=long_description,
+    packages=find_packages(),
+)
