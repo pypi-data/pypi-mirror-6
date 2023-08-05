e7af6ff1499c55eec1e4846f322aea467ec5f712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:00 +0000

    Add package silva.batch/1.0/silva.batch-1.0.tar.gz

---
+++ packages/silva.batch/silva.batch-1.0.tar.gz/silva.batch-1.0/README.txt
@@ -0,0 +1,6 @@
+silva.batch
+===========
+
+``silva.batch`` let define pagination in Silva using `zeam.utils.batch
+<http://pypi.python.org/pypi/zeam.utils.batch>`_.
+
---
+++ packages/silva.batch/silva.batch-1.0.tar.gz/silva.batch-1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='silva.batch',
+      version=version,
+      description="Batch integration for Silva CMS",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Framework :: Zope2",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        ],
+      keywords='batch silva pagination',
+      author='Antonin Amand',
+      author_email='info@infrae.com',
+      url='https://github.com/silvacms/silva.batch',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['silva'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zeam.utils.batch'
+      ],
+      )
