5fdcd83f7f67d5d0812c213693c66b9b180dff95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:39 +0000

    Add package pnumpy/1.0/pnumpy-1.0.tar.gz

---
+++ packages/pnumpy/pnumpy-1.0.tar.gz/pnumpy-1.0/pnumpy/__init__.py
@@ -0,0 +1,8 @@
+"""
+pnumpy: Distributed array
+"""
+__all__ = ["pnDistArray", "pnGhostedDistArray", "pnMultiArrayIter", "pnCubeDecomp"]
+from pnDistArray import DistArray, daZeros, daOnes, daArray
+from pnGhostedDistArray import GhostedDistArray, ghZeros, ghOnes, ghArray
+from pnMultiArrayIter import MultiArrayIter 
+from pnCubeDecomp import CubeDecomp
