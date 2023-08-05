86a82029efd40e4e5c6ba9c34cf4f7b086064af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:02 +0000

    Add package tbpaste/0.2.1/tbpaste-0.2.1.tar.gz

---
+++ packages/tbpaste/tbpaste-0.2.1.tar.gz/tbpaste-0.2.1/setup.py
@@ -0,0 +1,98 @@
+import sys
+import re
+import subprocess
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def find_version(fname):
+    '''Attempts to find the version number in the file names fname.
+    Raises RuntimeError if not found.
+    '''
+    version = ''
+    with open(fname, 'r') as fp:
+        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            m = reg.match(line)
+            if m:
+                version = m.group(1)
+                break
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
+
+__version__ = find_version("tbpaste.py")
+
+PUBLISH_CMD = "python setup.py register sdist bdist_wheel upload"
+TEST_PUBLISH_CMD = 'python setup.py register -r test sdist bdist_wheel upload -r test'
+TEST_CMD = 'nosetests'
+
+if 'publish' in sys.argv:
+    try:
+        __import__('wheel')
+    except ImportError:
+        print("wheel required. Run `pip install wheel`.")
+        sys.exit(1)
+    status = subprocess.call(PUBLISH_CMD, shell=True)
+    sys.exit(status)
+
+if 'publish_test' in sys.argv:
+    try:
+        __import__('wheel')
+    except ImportError:
+        print("wheel required. Run `pip install wheel`.")
+        sys.exit(1)
+    status = subprocess.call(TEST_PUBLISH_CMD, shell=True)
+    sys.exit(status)
+
+if 'run_tests' in sys.argv:
+    try:
+        __import__('nose')
+    except ImportError:
+        print('nose required. Run `pip install nose`.')
+        sys.exit(1)
+
+    status = subprocess.call(TEST_CMD, shell=True)
+    sys.exit(status)
+
+def read(fname):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+
+setup(
+    name='tbpaste',
+    version=__version__,
+    description='Sentiment analysis, as easy as copy-and-paste',
+    long_description=read("README.rst"),
+    author='Steven Loria',
+    author_email='sloria1@gmail.com',
+    url='https://github.com/sloria/tbpaste',
+    py_modules=['tbpaste'],
+    install_requires=[
+        'docopt',
+        'clint',
+        'textblob>=0.8.2',
+        'xerox'
+    ],
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    test_suite='tests',
+    tests_require=['nose', 'xerox', 'textblob'],
+    entry_points={
+        'console_scripts': [
+            "tbpaste = tbpaste:main"
+        ]
+    },
+)
