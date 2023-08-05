1ebafb21ce68378b750c2d3fa41ac7ce187d8fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:55 +0000

    Add package django-authority/0.8/django-authority-0.8.tar.gz

---
+++ packages/django-authority/django-authority-0.8.tar.gz/django-authority-0.8/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-authority',
+    version='0.8',
+    description=(
+        "A Django app that provides generic per-object-permissions "
+        "for Django's auth app."
+    ),
+    long_description=read('README.rst'),
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    license='BSD',
+    url='https://github.com/jezdez/django-authority/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Framework :: Django',
+    ],
+    install_requires=['django'],
+    package_data = {
+        'authority': [
+            'fixtures/test.json',
+            'templates/authority/*.html',
+            'templates/admin/edit_inline/action_tabular.html',
+            'templates/admin/permission_change_form.html',
+        ]
+    },
+    zip_safe=False,
+)
