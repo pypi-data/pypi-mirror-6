d20a3b7b3f83640d2ddc914f47f962539c9816d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:25 +0000

    Add package jsinclude/1.0.2/jsinclude-1.0.2.zip

---
+++ packages/jsinclude/jsinclude-1.0.2.zip/jsinclude-1.0.2/jsinclude/templatetags/jsinclude.py
@@ -0,0 +1,18 @@
+from .jsincludenode import JSIncludeNode
+from django.template import TemplateSyntaxError, Library
+register = Library()
+
+@register.tag
+def jsinclude(parser, token):
+    # Separate all tag arguments.
+    contents = token.split_contents()
+
+    # Grab path to script.
+    try:
+        path = contents[1]
+    except IndexError:
+        raise TemplateSyntaxError('Missing path to script.')
+
+    # Grab any remaining arguments and return the Node.
+    args = contents[2:]
+    return JSIncludeNode(path, args)
