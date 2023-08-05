3bf35688b082f068b5c2bb572e7ce651ce61a262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:07 +0000

    Add package Quandl/1.8/Quandl-1.8.tar.gz

---
+++ packages/Quandl/Quandl-1.8.tar.gz/Quandl-1.8/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='Quandl',
+      version='1.8',
+      description = "Package for Quandl API access",
+      author = "Mark Hartney, Chris Stevens",
+      maintainer = 'Chris Stevens',
+      maintainer_email = "connect@quandl.com",
+      license = 'MIT',
+      packages=['Quandl'],
+      )
