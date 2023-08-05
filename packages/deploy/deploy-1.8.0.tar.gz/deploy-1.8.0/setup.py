aa060b6dd9574bebdcb943cb08bfc333789326e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:02 +0000

    Add package deploy/1.8.0/deploy-1.8.0.tar.gz

---
+++ packages/deploy/deploy-1.8.0.tar.gz/deploy-1.8.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "deploy",
+    version = "1.8.0",
+    url = 'https://github.com/ondrejsika/deploy',
+    license = 'MIT',
+    description = "Easy deploy Python WSGI apps",
+    author = 'Ondrej Sika',
+    author_email = 'ondrej@ondrejsika.com',
+    packages = find_packages(),
+    scripts = [
+        "deploy/bin/deploy",
+        "deploy/bin/deploy-startserver",
+        "deploy/bin/deploy-serverconf",
+        "deploy/bin/deploy-init",
+        "deploy/bin/deploy-restart",
+        "deploy/bin/deploy-remove",
+    ],
+    install_requires = ["pip", "pycoffee"],
+)
