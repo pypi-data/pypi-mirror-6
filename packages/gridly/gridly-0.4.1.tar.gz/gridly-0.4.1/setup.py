92969af7c08efa247a4febf519ca41e3fe4c43bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package gridly/0.4.1/gridly-0.4.1.tar.gz

---
+++ packages/gridly/gridly-0.4.1.tar.gz/gridly-0.4.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="gridly",
+    version="0.4.1",
+    packages=['gridly'],
+    test_suite='test',
+    platforms='any',
+    install_requires=[
+        'enum34'
+    ],
+
+    author="Nathan West",
+    description="A Python library for managing fixed-size 2D spaces",
+    license="LGPLv3",
+    url="https://github.com/Lucretiel/gridly",
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Games/Entertainment'
+    ]
+)
---
+++ packages/gridly/gridly-0.4.1.tar.gz/gridly-0.4.1/test/test_grid.py
@@ -0,0 +1,137 @@
+from unittest import TestCase
+from gridly import DenseGrid, SparseGrid, CompositeGrid
+
+class TestGenericGrid:
+    @classmethod
+    def setUpClass(cls):
+        cls.num_rows = 5
+        cls.num_columns = 7
+
+    def test_dimensions(self):
+        self.assertEqual(self.grid.num_rows, self.num_rows)
+        self.assertEqual(self.grid.num_columns, self.num_columns)
+
+    def test_out_of_range(self):
+        for row in (-1, self.num_rows):
+            for column in (-1, self.num_columns):
+                with self.assertRaises(IndexError):
+                    value = self.grid[row, column]
+
+    def test_row_iter_out_of_range(self):
+        with self.assertRaises(IndexError):
+            row = list(self.grid.row(self.num_rows+10))
+
+    def test_column_iter_out_of_range(self):
+        with(self.assertRaises(IndexError)):
+            column = list(self.grid.column(self.num_columns-10))
+
+class TestGenericConcreteGrid(TestGenericGrid):
+    def setUp(self):
+        self.grid = self.grid_type(
+            self.num_rows,
+            self.num_columns)
+
+    def test_empty_reads(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_columns):
+                self.assertIs(self.grid[row, column], None)
+
+    def test_random_sets(self):
+        locations = {
+            (1, 1),
+            (4, 0),
+            (2, 0),
+            (0, 1),
+            (4, 5),
+            (2, 5)
+        }
+
+        for location in locations:
+            self.grid[location] = 10
+
+        for r, row in enumerate(self.grid.rows()):
+            for c, cell in enumerate(row):
+                if (r, c) in locations:
+                    self.assertEqual(cell, 10)
+                else:
+                    self.assertIs(cell, None)
+
+    def test_all_sets_row(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_columns):
+                self.grid[row, column] = row
+
+        for r, row in enumerate(self.grid.rows()):
+            for cell in row:
+                self.assertEqual(cell, r)
+
+    def test_all_sets_column(self):
+        for row in range(self.num_rows):
+            for column in range(self.num_columns):
+                self.grid[row, column] = row
+
+        for column in self.grid.columns():
+            for r, cell in enumerate(column):
+                self.assertEqual(cell, r)
+
+    def test_reset(self):
+        locations = {
+            (1, 1),
+            (4, 0),
+            (2, 0),
+            (0, 1),
+            (4, 5),
+            (2, 5)
+        }
+
+        for row in range(self.num_rows):
+            for column in range(self.num_columns):
+                self.grid[row, column] = 10
+
+        for location in locations:
+            self.grid[location] = None
+
+        for r, row in enumerate(self.grid.rows()):
+            for c, cell in enumerate(row):
+                if (r, c) in locations:
+                    self.assertIs(cell, None)
+                else:
+                    self.assertEqual(cell, 10)
+
+class TestDenseGrid(TestGenericConcreteGrid, TestCase):
+    grid_type = DenseGrid
+
+class TestSparseGrid(TestGenericConcreteGrid, TestCase):
+    grid_type = SparseGrid
+
+class TestCompositeGrid(TestGenericGrid, TestCase):
+    def setUp(self):
+        self.grid1 = DenseGrid(self.num_rows, self.num_columns)
+        self.grid2 = SparseGrid(self.num_rows, self.num_columns)
+        self.grid = CompositeGrid(self.grid1, self.grid2)
+
+    def test_get_set(self):
+        self.grid[3, 4] = [1, 2]
+        self.assertEqual(list(self.grid[3, 4]), [1, 2])
+        self.assertEqual(self.grid1[3, 4], 1)
+        self.assertEqual(self.grid2[3, 4], 2)
+
+    def test_iter(self):
+        for row in self.grid.rows():
+            for cell in row:
+                cell[0] = 1
+                cell[1] = 2
+
+        for row in range(self.num_rows):
+            for column in range(self.num_columns):
+                self.assertEqual(list(self.grid[row, column]), [1, 2])
+                self.assertEqual(self.grid1[row, column], 1)
+                self.assertEqual(self.grid2[row, column], 2)
+
+                for layer, i in zip(self.grid[row, column], (1, 2)):
+                    self.assertEqual(layer, i)
+
+    def test_dimension_mismatch(self):
+        self.grid2 = SparseGrid(self.num_rows+1, self.num_columns+1)
+        with self.assertRaises(ValueError):
+            self.grid = CompositeGrid(self.grid1, self.grid2)
