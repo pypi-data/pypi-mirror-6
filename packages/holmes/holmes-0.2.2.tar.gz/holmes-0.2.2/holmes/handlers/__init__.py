e60014fa555359d63de6509c431c88bc47e06969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package holmes/0.2.2/holmes-0.2.2.tar.gz

---
+++ packages/holmes/holmes-0.2.2.tar.gz/holmes-0.2.2/holmes/handlers/__init__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+
+from ujson import dumps
+from tornado.web import RequestHandler
+
+
+class BaseHandler(RequestHandler):
+    def initialize(self, *args, **kw):
+        super(BaseHandler, self).initialize(*args, **kw)
+
+        self._session = None
+
+    def on_finish(self):
+        if self.application.config.COMMIT_ON_REQUEST_END:
+            try:
+                if self.get_status() > 399:
+                    logging.debug('ROLLING BACK TRANSACTION')
+                    self.db.rollback()
+                else:
+                    logging.debug('COMMITTING TRANSACTION')
+                    self.db.commit()
+                    self.application.event_bus.flush()
+            finally:
+                if self._session is not None:
+                    self._session.remove()
+                    self._session = None
+
+    def options(self):
+        self.set_header('Access-Control-Allow-Origin', self.application.config.ORIGIN)
+        self.set_header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        self.set_header('Access-Control-Allow-Headers', 'Accept, Content-Type')
+        self.set_status(200)
+        self.finish()
+
+    def set_default_headers(self):
+        self.set_header('Access-Control-Allow-Origin', self.application.config.ORIGIN)
+        self.set_header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+    def write_json(self, obj):
+        self.set_header("Content-Type", "application/json")
+        self.write(dumps(obj))
+
+    @property
+    def db(self):
+        if self._session is None:
+            self._session = self.application.get_sqlalchemy_session()
+
+        return self._session
