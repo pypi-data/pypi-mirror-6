3cda775dd71cd52161227555480b5f7121bc4944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package django-ipware/0.0.3/django-ipware-0.0.3.tar.gz

---
+++ packages/django-ipware/django-ipware-0.0.3.tar.gz/django-ipware-0.0.3/ipware/ip.py
@@ -0,0 +1,26 @@
+
+from utils import is_valid_ip
+from defaults import IPWARE_META_PRECEDENCE_LIST
+from defaults import IPWARE_NON_PUBLIC_IP_PREFIX
+
+
+def get_real_ip(request):
+    """
+    Best attempt to get client's real ip-address or returns None
+    """
+    for key in IPWARE_META_PRECEDENCE_LIST:
+        value = request.META.get(key, '')
+        if value.strip() != '':
+            ips = [ip.strip().lower() for ip in value.split(',')]
+            for ip_str in ips:
+                if ip_str and not ip_str.startswith(IPWARE_NON_PUBLIC_IP_PREFIX):
+                    if is_valid_ip(ip_str):
+                        return ip_str
+    return None
+
+def get_ip_address_from_request(request):
+    """
+    Backwards compatibility -- use get_real_ip() instead
+    """
+    return get_real_ip(request)
+
