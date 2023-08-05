94ffc05700332dd675c42d2b4a3dfc1a9ae3809b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:11 +0000

    Add package tiddlyweb/2.0.0/tiddlyweb-2.0.0.tar.gz

---
+++ packages/tiddlyweb/tiddlyweb-2.0.0.tar.gz/tiddlyweb-2.0.0/test/test_external_load.py
@@ -0,0 +1,24 @@
+"""
+Test handling of loading external stuff in twanager.
+"""
+
+from tiddlyweb.manage import _external_load
+from tiddlyweb.config import config as global_config
+
+config = {
+        'monkey': 'bar',
+        }
+
+
+def test_load_file():
+    args = _external_load(['twanager', '--load', 'test/test_external_load.py',
+        'foobar'], global_config)
+    assert args == ['twanager', 'foobar']
+    assert global_config['monkey'] == 'bar'
+
+
+def test_load_module():
+    args = _external_load(['twanager', '--load', 'test.test_external_load',
+        'foobar'], global_config)
+    assert args == ['twanager', 'foobar']
+    assert global_config['monkey'] == 'bar'
