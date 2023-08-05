ca8d3416949343cfbef018e2ef2a4cc536fc740b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:31 +0000

    Add package SmallSettings/0.2.1/SmallSettings-0.2.1.tar.gz

---
+++ packages/SmallSettings/SmallSettings-0.2.1.tar.gz/SmallSettings-0.2.1/setup.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if __name__ == '__main__':
+    setup(name='SmallSettings',
+          version='0.2.1',
+          author='Dominik "Socek" Długajczy',
+          author_email='msocek@gmail.com',
+          test_suite='smallsettings.tests.get_all_test_suite',
+          packages=find_packages(),
+          package_data={'smallsettings': ['README.md']},
+          long_description=read('README.md'),
+          description = "Simple settings functions for projects.",
+          )
