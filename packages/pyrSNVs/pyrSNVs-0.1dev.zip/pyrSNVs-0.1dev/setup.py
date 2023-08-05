20436de18e08b762f00f022305e3f31a57fa9f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:14 +0000

    Add package pyrSNVs/0.1dev/pyrSNVs-0.1dev.zip

---
+++ packages/pyrSNVs/pyrSNVs-0.1dev.zip/pyrSNVs-0.1dev/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyrSNVs',
+    packages = ['pyrSNVs'], # this must be the same as the name above
+    version = '0.1dev',
+    description = 'A Python package for detection of regulatory SNVs',
+    author = 'Guipeng Li',
+    author_email = 'guipenglee@gmail.com',
+    url = 'https://github.com/GuipengLi/pyrSNVs',   # use the URL to the github repo
+    download_url = 'https://github.com/GuipengLi/pyrSNVs/tarball/0.1dev', # I'll explain this in a second
+    keywords = ['detection', 'regulatory', 'SNVs'], # arbitrary keywords
+    classifiers = [],
+    long_description=open('README.md').read(),
+)
