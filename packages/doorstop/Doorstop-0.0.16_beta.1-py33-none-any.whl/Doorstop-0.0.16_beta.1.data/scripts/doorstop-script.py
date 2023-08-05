c1b08f2efdda158dd0850ddf38e2c84518fc8102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:56 +0000

    Add package doorstop/0.0.16-beta.1/Doorstop-0.0.16_beta.1-py33-none-any.whl

---
+++ packages/doorstop/Doorstop-0.0.16_beta.1-py33-none-any.whl/Doorstop-0.0.16_beta.1.data/scripts/doorstop-script.py
@@ -0,0 +1,10 @@
+#!python
+# EASY-INSTALL-ENTRY-SCRIPT: 'Doorstop==0.0.16-beta.1','console_scripts','doorstop'
+__requires__ = 'Doorstop==0.0.16-beta.1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Doorstop==0.0.16-beta.1', 'console_scripts', 'doorstop')()
+    )
