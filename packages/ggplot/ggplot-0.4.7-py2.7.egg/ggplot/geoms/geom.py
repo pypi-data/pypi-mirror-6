3c0ee86a96e619f6508770eeee0d4347f0e7b7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:16 +0000

    Add package ggplot/0.4.7/ggplot-0.4.7-py2.7.egg

---
+++ packages/ggplot/ggplot-0.4.7-py2.7.egg/ggplot/geoms/geom.py
@@ -0,0 +1,43 @@
+from copy import deepcopy
+from ggplot.components import aes
+from pandas import DataFrame
+
+__ALL__ = ["geom"]
+
+class geom(object):
+    """Base class of all Geoms"""
+    VALID_AES = []
+    data = None
+    aes = None
+    def __init__(self, *args, **kwargs):
+        # new dict for each geom
+        self.aes = {}
+        for arg in args:
+            if isinstance(arg, aes):
+                for k, v in arg.items():
+                    if k in self.VALID_AES:
+                        self.aes[k] = v
+            elif isinstance(arg, DataFrame):
+                self.data = arg
+            else:
+                raise Exception('Unknown argument of type "{0}".'.format(type(arg)))
+        if "data" in kwargs:
+            self.data = kwargs.pop("data")
+        if "mapping" in kwargs:
+            for k, v in kwargs.pop("mapping").items():
+                if k in self.VALID_AES:
+                    self.aes[k] = v
+        if "colour" in kwargs:
+            kwargs["color"] = kwargs["colour"]
+            del kwargs["colour"]
+
+        self.manual_aes = {}
+        for k, v in kwargs.items():
+            if k in self.VALID_AES:
+                self.manual_aes[k] = v
+
+    def __radd__(self, gg):
+        gg = deepcopy(gg)
+        gg.geoms.append(self)
+        return gg
+
---
+++ packages/ggplot/ggplot-0.4.7-py2.7.egg/ggplot/geoms/geom_point.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.colors import Normalize
+import numpy as np
+from copy import deepcopy
+from .geom import geom
+import numpy as np
+
+class geom_point(geom):
+    VALID_AES = ['x', 'y', 'size', 'color', 'alpha', 'shape', 'label', 'cmap',
+                 'position']
+
+    def plot_layer(self, layer):
+        layer = {k: v for k, v in layer.items() if k in self.VALID_AES}
+        layer.update(self.manual_aes)
+
+        if "size" in layer:
+            layer["s"] = layer["size"]
+            del layer["size"]
+
+        if "shape" in layer:
+            layer["marker"] = layer["shape"]
+            del layer["shape"]
+
+        # for some reason, scatter doesn't default to the same color styles
+        # as the axes.color_cycle
+        if "color" not in layer and "cmap" not in layer:
+            layer["color"] = mpl.rcParams.get("axes.color_cycle", ["#333333"])[0]
+        
+        if "position" in layer:
+            del layer["position"]
+            layer['x'] *= np.random.uniform(.9, 1.1, len(layer['x']))
+            layer['y'] *= np.random.uniform(.9, 1.1, len(layer['y']))
+
+        plt.scatter(**layer)
+
