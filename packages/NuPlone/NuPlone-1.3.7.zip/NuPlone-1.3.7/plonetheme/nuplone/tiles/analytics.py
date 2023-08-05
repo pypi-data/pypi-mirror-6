debc3ffe27fc8d72b892b6c1887221434a1d2955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package NuPlone/1.3.7/NuPlone-1.3.7.zip

---
+++ packages/NuPlone/NuPlone-1.3.7.zip/NuPlone-1.3.7/plonetheme/nuplone/tiles/analytics.py
@@ -0,0 +1,15 @@
+from AccessControl import getSecurityManager
+from plonetheme.nuplone.tiles.tile import AppConfigTile
+from plonetheme.nuplone.utils import SimpleLiteral
+
+class AnalyticsTile(AppConfigTile):
+    def __call__(self):
+        self.account=self.data.get("account", None)
+        if not self.account:
+            return ''
+
+        self.domain=self.data.get("domain", None)
+
+        user=getSecurityManager().getUser()
+        self.login=user.getUserName()
+        return SimpleLiteral(self.index())
