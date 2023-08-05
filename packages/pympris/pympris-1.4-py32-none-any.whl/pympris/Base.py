bac64c56c93a9f388769ec4ffd0d3f2f7bcaf6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package pympris/1.4/pympris-1.4-py32-none-any.whl

---
+++ packages/pympris/pympris-1.4-py32-none-any.whl/pympris/Base.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Copyright (c) Mikhail Mamrouski.
+# See LICENSE for details.
+
+"""
+This module provides a `Base` class used as a base class
+for implementing MPRIS2 interfaces.
+
+Decorator 'signal_wrapper' used by `Base` class
+to convert function's arguments from dbus type to python type.
+
+`BaseMeta` metaclass (inherited ExceptionMeta and ConverterMeta classes)
+to avoid returning or raising dbus types and exceptions.
+"""
+
+from functools import partial, wraps
+import types
+import dbus
+from .common import convert, converter, exception_wrapper
+
+IPROPERTIES = "org.freedesktop.DBus.Properties"
+
+
+def signal_wrapper(f):
+    """Decorator converts function's arguments from dbus types to python."""
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        args = map(convert, args)
+        kwds = {convert(k): convert(v) for k, v in kwds.items()}
+        return f(*args, **kwds)
+    return wrapper
+
+
+class ExceptionMeta(type):
+    """Metaclass wraps all class' functions and properties
+    in `exception_wrapper` decorator to avoid raising dbus exceptions
+    """
+    def __new__(cls, name, parents, dct):
+        fn = exception_wrapper
+        for attr_name in dct:
+            if isinstance(dct[attr_name], types.FunctionType):
+                dct[attr_name] = fn(dct[attr_name])
+            elif isinstance(dct[attr_name], property) and dct[attr_name].fget:
+                dct[attr_name] = property(
+                    fn(dct[attr_name].fget) if dct[attr_name].fget else None,
+                    fn(dct[attr_name].fset) if dct[attr_name].fset else None,
+                    fn(dct[attr_name].fdel) if dct[attr_name].fdel else None)
+
+        return super(ExceptionMeta, cls).__new__(cls, name, parents, dct)
+
+
+class ConverterMeta(type):
+    """Metaclass wraps all class' functions and properties
+    in `converter` decorator to avoid returning dbus types
+    """
+
+    def __new__(cls, name, parents, dct):
+        for attr_name in dct:
+            if isinstance(dct[attr_name], types.FunctionType):
+                dct[attr_name] = converter(dct[attr_name])
+            elif isinstance(dct[attr_name], property) and dct[attr_name].fget:
+                dct[attr_name] = property(converter(dct[attr_name].fget),
+                                          dct[attr_name].fset,
+                                          dct[attr_name].fdel
+                                          )
+
+        return super(ConverterMeta, cls).__new__(cls, name, parents, dct)
+
+
+class BaseMeta(ExceptionMeta, ConverterMeta):
+    pass
+
+BaseVersionFix = BaseMeta('BaseVersionFix', (object,), {})
+
+class Base(BaseVersionFix):
+
+    """Base class provides common functionality
+    for other classes which implement MPRIS2 interfaces"""
+
+    OBJ_PATH = "/org/mpris/MediaPlayer2"
+
+    def __init__(self, name, bus=None, private=False):
+        """Init inner attributes to work with dbus.
+        Parameters:
+            name - unique or well-known objects name
+            bus - bus object;
+                  new SessionBus() object will be created if value is None.
+            private - private connection (uses only if bus is None)
+
+        below described attributes will be created:
+            bus - Bus object from the functions argument or SessionBus()
+            name - objects name from the functions argument
+            proxy - DBUS proxy object
+            iface - DBUS interface (uses self.IFACE path to create it)
+            properties - DBUS interface to work with object's properties
+            get - function to receive property's value
+            set - function to set property's value
+        """
+        if not bus:
+            bus = dbus.SessionBus(private=private)
+        self.bus = bus
+        self.name = name
+        self.proxy = bus.get_object(name, self.OBJ_PATH)
+        self.iface = dbus.Interface(self.proxy, self.IFACE)
+        self.properties = dbus.Interface(self.proxy, IPROPERTIES)
+        self.get = partial(self.properties.Get, self.IFACE)
+        self.set = partial(self.properties.Set, self.IFACE)
+
+    def register_signal_handler(self, signal_name, handler_function):
+        """register `handler_function` to receive `signal_name`.
+
+        Uses class's dbus interface self.IFACE, objects name self.name
+            and objects path self.OBJ_PATH to match signal.
+        """
+        self.bus.add_signal_receiver(signal_wrapper(handler_function),
+                                     signal_name=signal_name,
+                                     dbus_interface=self.IFACE,
+                                     bus_name=self.name,
+                                     path=self.OBJ_PATH)
---
+++ packages/pympris/pympris-1.4-py32-none-any.whl/pympris/TrackList.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Copyright (c) Mikhail Mamrouski.
+# See LICENSE for details.
+
+"""
+This module provides a `TrackList` class
+wich implemented MPRIS2 TrackList interface:
+http://specifications.freedesktop.org/mpris-spec/latest/Track_List_Interface.html
+
+Usage:
+
+tl = TrackList('org.mpris.MediaPlayer2.vlc')
+print tl.Tracks
+tl.RemoveTrack(tl.Tracks[2])
+
+"""
+
+from .common import convert2dbus
+from .Base import Base
+
+
+class TrackList(Base):
+
+    """class implements methods and properties
+    to working with MPRIS2 TrackList interface
+    """
+
+    IFACE = "org.mpris.MediaPlayer2.TrackList"
+
+    def __init__(self, name, bus=None, private=False):
+        super(TrackList, self).__init__(name, bus, private)
+
+    def GetTracksMetadata(self, track_ids):
+        """Gets all the metadata available for a set of tracks.
+        Parameters:
+            track_ids - list of track ids
+
+        Returns:
+            Metadata of the set of tracks given as input.
+        """
+        return self.iface.GetTracksMetadata(convert2dbus(track_ids, 'ao'))
+
+    def AddTrack(self, uri, after_track, set_as_current):
+        """Adds a URI in the TrackList.
+        Parameters:
+            uri — The uri of the item to add.
+            after_track — The identifier of the track
+                          after which the new item should be inserted.
+            set_as_current - Whether the newly inserted track
+                             should be considered as the current track.
+        """
+        self.iface.AddTrack(uri,
+                            convert2dbus(after_track, 'o'),
+                            convert2dbus(set_as_current, 'b'))
+
+    def RemoveTrack(self, track_id):
+        """Removes an item from the TrackList.
+        Parameters:
+            track_id - Identifier of the track to be removed.
+        """
+        self.iface.RemoveTrack(convert2dbus(track_id, 'o'))
+
+    def GoTo(self, track_id):
+        """Skip to the specified TrackId.
+        Parameters:
+            track_id - Identifier of the track to skip to.
+        """
+        self.iface.GoTo(convert2dbus(track_id, 'o'))
+
+    @property
+    def Tracks(self):
+        """Returns an list which contains the identifier of each track
+        in the tracklist, in order."""
+        return self.get('Tracks')
+
+    @property
+    def CanEditTracks(self):
+        """If false, calling AddTrack or RemoveTrack will have no effect,
+        and may raise a NotSupported error."""
+        return self.get('CanEditTracks')
