8339063bccee34db4135fdad25968ecaec58e6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package dictregister/1.0.0/dictregister-1.0.0.tar.gz

---
+++ packages/dictregister/dictregister-1.0.0.tar.gz/dictregister-1.0.0/HISTORY.rst
@@ -0,0 +1,29 @@
+.. :changelog:
+
+History
+-------
+
+0.9.0 (2013-11-08)
+++++++++++++++++++
+
+* First implementation
+
+0.9.0.post1 (2013-11-08)
+++++++++++++++++++
+
+* Fixed PyPI classifiers
+
+0.9.1 (2013-11-14)
+++++++++++++++++++
+
+* Added a direct import of DictRegister in __init__.py (thanks to https://github.com/Jeff17Robbins)
+
+0.9.2 (2013-11-14)
+++++++++++++++++++
+
+* Fixed import in __init__.py for Python 3, now all tox tests pass
+
+1.0.0 (2013-12-04)
+++++++++++++++++++
+
+* Raise TypeError instead of ValueError if invalid object is passed (pull request from joshgeller)
---
+++ packages/dictregister/dictregister-1.0.0.tar.gz/dictregister-1.0.0/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from setuptools.command.test import test as TestCommand
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+setup(
+    name='dictregister',
+    version='1.0.0',
+    description='A searchable list of dictionaries',
+    long_description=readme + '\n\n' + history,
+    author='Leonardo Giordani',
+    author_email='giordani.leonardo@gmail.com',
+    url='https://github.com/lgiordani/dictregister',
+    packages=[
+        'dictregister',
+    ],
+    package_dir={'dictregister': 'dictregister'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='dictregister',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
+)
