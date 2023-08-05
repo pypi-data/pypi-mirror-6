c27d6f0e579c1db351e1e6fb6c00829cfe3d0003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:59 +0000

    Add package Zinc/0.1.4/Zinc-0.1.4.tar.gz

---
+++ packages/Zinc/Zinc-0.1.4.tar.gz/Zinc-0.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+README = os.path.join(os.path.dirname(__file__), 'README.txt')
+long_description = open(README).read() + 'nn'
+
+setup(
+    name='Zinc',
+    version='0.1.4',
+    author='John Wang',
+    author_email='john@zinc.io',
+    packages=['zinc'],
+    url='https://github.com/wangjohn/zinc_cli',
+    scripts=[],
+    license='LICENSE.txt',
+    description='Wrapper for Zinc ecommerce API (zinc.io)',
+    long_description=long_description,
+    install_requires=[
+        "requests >= 1.1.0"
+    ],
+)
