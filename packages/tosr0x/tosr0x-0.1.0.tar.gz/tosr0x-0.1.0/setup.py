f4d0c09f9681e418e38ce7d2538640a333dc42cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:05 +0000

    Add package tosr0x/0.1.0/tosr0x-0.1.0.tar.gz

---
+++ packages/tosr0x/tosr0x-0.1.0.tar.gz/tosr0x-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='tosr0x',
+    version='0.1.0',
+    py_modules=['tosr0x'],
+    description='An interface to the tinysine.com tosr0x line of USB-controlled relays.',
+    long_description=open('README.md').read()
+)
