01e758fd1abb1f737701452e0346c25b7d34457d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:59 +0000

    Add package django_sql_dashboards/0.0.4-3/django_sql_dashboards-0.0.4-3.tar.gz

---
+++ packages/django_sql_dashboards/django_sql_dashboards-0.0.4-3.tar.gz/django_sql_dashboards-0.0.4-3/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django_sql_dashboards',
+    version='0.0.4-3',
+    author=u'Guillaume Thomas',
+    description='',
+    long_description=open('README.txt').read(),
+    zip_safe=False,
+    install_requires=[
+       "Django==1.5",
+       "django-bootstrap3>=2.3.0",
+       "MySQL-python",
+       "South",
+       "django-filter"
+    ],
+    packages = find_packages(),
+    include_package_data=True,
+)
