633e2a1044d10ef9a35903f081956069fd96ab3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:12 +0000

    Add package py3quizlet2/0.1/py3quizlet2-0.1.tar.gz

---
+++ packages/py3quizlet2/py3quizlet2-0.1.tar.gz/py3quizlet2-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='py3quizlet2',
+      version='0.1',
+      py_modules=['py3quizlet2'],
+      url='https://github.com/hammer/py3quizlet2',
+      author='Jeff Hammerbacher',
+      authoer_email='jeff.hammerbacher@gmail.com'
+      )
