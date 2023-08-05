02f5f0d895567d4fbaf008700b104902a0745a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package bootstrap_2to3/0.1.1/bootstrap_2to3-0.1.1.tar.gz

---
+++ packages/bootstrap_2to3/bootstrap_2to3-0.1.1.tar.gz/bootstrap_2to3-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='bootstrap_2to3',
+    version='0.1.1',
+    author='Jayesh',
+    author_email='jayesh@jayeshv.info',
+    packages=['bootstrap_2to3'],
+    url='https://github.com/jayeshv/bootstrap_2to3',
+    license='BSD licence',
+    description='Script to migrate bootstrap 2 templates to bootstrap3.',
+    #long_description=open('README.md').read(),
+)
