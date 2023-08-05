3947f0473dc7ee0e3c34d332922f54acc6aa62fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package hack-exe/0.2/hack-exe-0.2.tar.gz

---
+++ packages/hack-exe/hack-exe-0.2.tar.gz/hack-exe-0.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='hack-exe',
+    version='0.2',
+    url='https://github.com/gardaud/hack-exe',
+    author='Guillaume Ardaud',
+    author_email='gardaud@acm.org',
+    description=('An animated terminal application that pretends to be hacking a website, just like in the movies.'),
+    license='MIT',
+    packages=['hack_exe'],
+    package_data={},
+    scripts=[
+        'bin/hack.exe'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Topic :: Artistic Software',
+    ],
+)
