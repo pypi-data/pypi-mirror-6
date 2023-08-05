3d2046c060d362c6e24030614e14b558a547025a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package django-sequere/0.1.6/django-sequere-0.1.6.tar.gz

---
+++ packages/django-sequere/django-sequere-0.1.6.tar.gz/django-sequere-0.1.6/sequere/tests/settings.py
@@ -0,0 +1,32 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+SITE_ID = 1
+DEBUG = True
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'sequere.backends.database',
+    'sequere.backends.redis',
+    'sequere.tests',
+    'sequere.contrib.user',
+    'sequere',
+]
+
+SECRET_KEY = 'blabla'
+
+ROOT_URLCONF = 'sequere.tests.urls'
+
+TEST_RUNNER = 'sequere.tests.runner.DjangoTestSuiteRunner'
