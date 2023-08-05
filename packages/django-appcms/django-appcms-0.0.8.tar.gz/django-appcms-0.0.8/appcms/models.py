0bbcbee05b5e845197652cf4f2ec6964db291fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:40 +0000

    Add package django-appcms/0.0.8/django-appcms-0.0.8.tar.gz

---
+++ packages/django-appcms/django-appcms-0.0.8.tar.gz/django-appcms-0.0.8/appcms/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from cms.models.fields import PlaceholderField
+
+
+class Placeholder(models.Model):
+    placeholder = PlaceholderField('appcms_placeholder')
+    name = models.CharField(max_length=300)
+
+    def __unicode__(self):
+        return self.name
