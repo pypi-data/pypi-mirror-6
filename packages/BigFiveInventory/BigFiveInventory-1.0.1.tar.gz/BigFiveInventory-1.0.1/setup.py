4640ab44b09f7a8b87eec13f6642a501dfc14cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:06 +0000

    Add package BigFiveInventory/1.0.1/BigFiveInventory-1.0.1.tar.gz

---
+++ packages/BigFiveInventory/BigFiveInventory-1.0.1.tar.gz/BigFiveInventory-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name        ='BigFiveInventory',
+       version     ='1.0.1',
+       py_module   =['Bing Five Inventory'],
+       author      ='xiaozhen',
+       author_email='sajc.35sc.xiaozhen.she@gmail.com',
+       url         ='nil',
+       description ='this is a simple quiz to test your personality using Big Five Inventory',
+       )
