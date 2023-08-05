e825e05fae4119d3e60e83db4561a846bf51128f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:51 +0000

    Add package django-styleguide/0.1.5/django-styleguide-0.1.5.tar.gz

---
+++ packages/django-styleguide/django-styleguide-0.1.5.tar.gz/django-styleguide-0.1.5/README.md
@@ -0,0 +1,17 @@
+django-styleguide
+=================
+
+[![Build Status](https://travis-ci.org/andrefarzat/django-styleguide.png?branch=master)](https://travis-ci.org/andrefarzat/django-styleguide)
+
+styleguide for django
+
+
+
+Executing the tests
+-------------------
+
+`python manage.py test`
+
+
+Uploading the egg
+-----------------
---
+++ packages/django-styleguide/django-styleguide-0.1.5.tar.gz/django-styleguide-0.1.5/styleguide/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('styleguide.views',
+    url(r'', 'index', name="styleguide.index"),
+    url(r'(?P<module_name>\w+)/$', 'module', name="styleguide.module"),
+    url(r'(?P<module_name>\w+)/(?P<component_name>\w+)', 'component', name="styleguide.component"),
+)
