1e0a04a913da08619df22a72ceaea02f8612a18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:49 +0000

    Add package tmuxp/0.1.0rc4/tmuxp-0.1.0rc4.tar.gz

---
+++ packages/tmuxp/tmuxp-0.1.0rc4.tar.gz/tmuxp-0.1.0rc4/tmuxp/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf8 - *-
+"""Manage tmux workspaces from JSON and YAML, pythonic API, shell completion.
+
+tmuxp
+~~~~~
+
+:copyright: Copyright 2013 Tony Narlock.
+:license: BSD, see LICENSE for details
+
+"""
+
+
+from __future__ import absolute_import, division, print_function, with_statement
+
+from .session import Session
+from .server import Server
+from .window import Window
+from .pane import Pane
+from .workspacebuilder import WorkspaceBuilder
+from . import config, util, cli
+
+import logging
+
+__version__ = '0.1.0rc4'
