102305c0d6dca871f211e0a8a60ce149f87625c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:26 +0000

    Add package django_kibrit/0.1.4/django_kibrit-0.1.4.tar.gz

---
+++ packages/django_kibrit/django_kibrit-0.1.4.tar.gz/django_kibrit-0.1.4/django_kibrit/__init__.py
@@ -0,0 +1,15 @@
+
+"""
+
+Kibrit
+======
+
+The module for watching SCM (git, hg, svn).
+Use it for control a static's versions.
+
+"""
+
+__version__ = '0.1.4'
+__project__ = __name__
+__author__ = "Sinan Midillili <sinan@hipo.biz>"
+__license__ = "BSD"
