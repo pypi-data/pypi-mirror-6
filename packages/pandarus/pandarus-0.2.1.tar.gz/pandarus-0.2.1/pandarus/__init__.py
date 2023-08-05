aab85823895d607bb81dc459cf118c49f99ff8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:34 +0000

    Add package pandarus/0.2.1/pandarus-0.2.1.tar.gz

---
+++ packages/pandarus/pandarus-0.2.1.tar.gz/pandarus-0.2.1/pandarus/__init__.py
@@ -0,0 +1,7 @@
+__version__ = (0, 2, 1)
+
+from .calculate import Pandarus
+from .maps import Map
+from .matching import matchstick, matchmaker
+from .projection import project
+from .raster import Raster
