0eac5d9d23144829789e1b0b067b4a38c0337115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:55 +0000

    Add package icerockettail/0.1/icerockettail-0.1.tar.gz

---
+++ packages/icerockettail/icerockettail-0.1.tar.gz/icerockettail-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='icerockettail',
+      version='0.1',
+      description='equivalent of tail -f on icerocket twitter search results',
+      author='Laurent Peuch',
+      #long_description='',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/psycojoker/icerockettail',
+      install_requires=['requests', 'argh', 'BeautifulSoup4'],
+      packages=[],
+      license= 'MIT',
+      scripts=['icerockettail'],
+      keywords='icerocket twitter tail',
+     )
