680dd094b8b33dcee6b9d9703b56092df6d86342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:10 +0000

    Add package wgety/1.2.2/wgety-1.2.2.tar.gz

---
+++ packages/wgety/wgety-1.2.2.tar.gz/wgety-1.2.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wgety',
+      version='1.2.2',
+      description='wgety for Python',
+      long_description='wgety for Python',
+      license='MIT',
+      author='Jong-Ha Ahn',
+      author_email='jongha.ahn@mrlatte.net',
+      url='https://github.com/jongha/python-wgety',
+      packages=['wgety'],
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Topic :: Internet :: WWW/HTTP',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+     )
