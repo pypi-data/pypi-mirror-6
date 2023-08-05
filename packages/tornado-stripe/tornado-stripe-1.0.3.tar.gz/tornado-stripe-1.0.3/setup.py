5fd986695820552beff4315e66fa2427b82213e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:19 +0000

    Add package tornado-stripe/1.0.3/tornado-stripe-1.0.3.tar.gz

---
+++ packages/tornado-stripe/tornado-stripe-1.0.3.tar.gz/tornado-stripe-1.0.3/setup.py
@@ -0,0 +1,27 @@
+import os, re
+from setuptools import setup, find_packages
+
+setup(
+    name = "tornado-stripe",
+    version = "1.0.3",
+    author = "Didip Kerabat",
+    author_email = "didipk@gmail.com",
+    description = ("Tornado client library for accessing Stripe API"),
+    license = "Apache Software License",
+    keywords = "tornado stripe api",
+    url = "https://github.com/didip/tornado-stripe",
+    packages=find_packages(exclude=['tests']),
+    package_data = {
+        # If any package contains *.txt or *.md files, include them
+        '': ['*.txt', '*.md'],
+    },
+    include_package_data=True,
+    install_requires = ["tornado>=2.4"],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+    ],
+)
