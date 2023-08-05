592851ba69a7b75dc268256f0c1c7cba953624ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:17 +0000

    Add package trytond_nereid_catalog/3.0.2.0/trytond_nereid_catalog-3.0.2.0.tar.gz

---
+++ packages/trytond_nereid_catalog/trytond_nereid_catalog-3.0.2.0.tar.gz/trytond_nereid_catalog-3.0.2.0/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''
+
+    nereid_catalog
+
+    :copyright: (c) 2010-2013 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: GPLv3, see LICENSE for more details
+
+'''
+from trytond.pool import Pool
+from product import (
+    Product, ProductsImageSet, ProductsRelated, ProductCategory,
+    WebSite, WebsiteCategory
+)
+
+
+def register():
+    Pool.register(
+        Product,
+        ProductsImageSet,
+        ProductsRelated,
+        ProductCategory,
+        WebSite,
+        WebsiteCategory,
+        module='nereid_catalog', type_='model')
