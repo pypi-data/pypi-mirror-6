9597c4062c3bc244a80c993a2519098732f67d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:42 +0000

    Add package holmes/0.6.2/holmes-0.6.2.tar.gz

---
+++ packages/holmes/holmes-0.6.2.tar.gz/holmes-0.6.2/holmes/handlers/page.py
@@ -0,0 +1,301 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from uuid import UUID
+import hashlib
+
+from ujson import loads, dumps
+from tornado import gen
+import tornado.httpclient
+import logging
+from sqlalchemy import or_
+
+from holmes.models import Page, Domain, Review
+from holmes.utils import get_domain_from_url
+from holmes.handlers import BaseHandler
+
+
+class PageHandler(BaseHandler):
+
+    @gen.coroutine
+    def post(self):
+        post_data = loads(self.request.body)
+        url = post_data['url']
+        score = float(post_data.get('score', self.application.config.DEFAULT_PAGE_SCORE))
+
+        domain_name, domain_url = get_domain_from_url(url)
+        if not domain_name:
+            self.set_status(400, 'Invalid url [%s]' % url)
+            self.write_json({
+                'reason': 'invalid_url',
+                'url': url
+            })
+            return
+
+        phost = self.application.config.HTTP_PROXY_HOST
+        pport = self.application.config.HTTP_PROXY_PORT
+
+        request = tornado.httpclient.HTTPRequest(
+            url=url,
+            proxy_host=phost,
+            proxy_port=pport
+        )
+
+        logging.info('Obtaining "%s" using proxy "%s:%s"...' % (url, phost, pport))
+
+        response = yield tornado.gen.Task(self.application.http_client.fetch, request)
+
+        if response.code > 399:
+            self.set_status(400, 'Invalid URL [%s]' % url)
+            self.write_json({
+                'reason': 'invalid_url',
+                'url': url
+            })
+            return
+
+        if response.effective_url != url:
+            self.set_status(400, 'Redirect URL [%s]' % url)
+            self.write_json({
+                'reason': 'redirect',
+                'url': url,
+                'effectiveUrl': response.effective_url
+            })
+            return
+
+        domains = self.db.query(Domain).filter(or_(
+            Domain.name == domain_name,
+            Domain.name == domain_name.rstrip('/'),
+            Domain.name == "%s/" % domain_name
+        )).all()
+
+        if not domains:
+            domain = None
+        else:
+            domain = domains[0]
+
+        if not domain:
+            url_hash = hashlib.sha512(domain_url).hexdigest()
+            domain = Domain(url=domain_url, url_hash=url_hash, name=domain_name)
+            self.db.add(domain)
+
+            self.application.event_bus.publish(dumps({
+                'type': 'new-domain',
+                'domainUrl': str(domain_url)
+            }))
+
+        pages = self.db.query(Page).filter(or_(
+            Page.url == url,
+            Page.url == url.rstrip('/'),
+            Page.url == "%s/" % url
+        )).all()
+
+        if pages:
+            page = pages[0]
+            page.score += score  # if page exists we need to increase page score
+            self.db.flush()
+        else:
+            page = None
+
+        if page:
+            self.write(str(page.uuid))
+            return
+
+        url_hash = hashlib.sha512(url).hexdigest()
+
+        page = Page(url=url, url_hash=url_hash, domain=domain, score=score)
+        self.db.add(page)
+        self.db.flush()
+
+        yield self.cache.increment_page_count(domain)
+        yield self.cache.increment_page_count()
+
+        self.application.event_bus.publish(dumps({
+            'type': 'new-page',
+            'pageUrl': str(url)
+        }))
+
+        self.write(str(page.uuid))
+
+    def get(self, uuid=''):
+        uuid = UUID(uuid)
+
+        page = Page.by_uuid(uuid, self.db)
+
+        if not page:
+            self.set_status(404, 'Page UUID [%s] not found' % uuid)
+            return
+
+        page_json = {
+            "uuid": str(page.uuid),
+            "url": page.url
+        }
+
+        self.write(page_json)
+
+
+class PageReviewsHandler(BaseHandler):
+
+    def get(self, uuid='', limit=10):
+        uuid = UUID(uuid)
+
+        page = Page.by_uuid(uuid, self.db)
+
+        if not page:
+            self.set_status(404, 'Page UUID [%s] not found' % uuid)
+            return
+
+        reviews = self.db.query(Review) \
+            .filter(Review.page == page) \
+            .filter(Review.is_complete == True) \
+            .order_by(Review.completed_date.desc())[:limit]
+
+        result = []
+        for review in reviews:
+            result.append({
+                'uuid': str(review.uuid),
+                'completedAt': review.completed_date,
+                'violationCount': review.violation_count
+            })
+
+        self.write_json(result)
+
+
+class PagesHandler(BaseHandler):
+
+    @gen.coroutine
+    def post(self):
+        urls = self.get_arguments('url')
+
+        if not urls:
+            self.set_status(200)
+            self.write('0')
+            return
+
+        all_domains = []
+        for url in urls:
+            domain_name, domain_url = get_domain_from_url(url.strip())
+            if not domain_name:
+                self.set_status(400, 'In the urls you posted there is an invalid URL: %s' % url)
+                return
+            all_domains.append((domain_name, domain_url))
+
+        domains = self.add_domains(all_domains)
+
+        existing_pages = self.db.query(Page).filter(Page.url.in_(urls)).all()
+        if not existing_pages:
+            existing_pages = []
+        existing_pages_dict = dict([(page.url, page) for page in existing_pages])
+
+        added_pages = []
+        for url in set(urls):
+            if url in existing_pages_dict:
+                continue
+
+            page_url = str(url)
+
+            url_hash = hashlib.sha512(page_url).hexdigest()
+
+            has_lock = yield self.cache.has_lock(url_hash)
+            if has_lock:
+                logging.debug('Lock found in page %s. Skipping...' % page_url)
+                continue
+
+            yield self.cache.lock_page(url_hash)
+
+            domain_name, domain_url = get_domain_from_url(url.strip())
+
+            logging.debug("Adding URL: %s" % url)
+
+            domain = domains[domain_url]
+            page = Page(url=page_url, url_hash=url_hash, domain=domain)
+            self.db.add(page)
+            self.db.flush()
+
+            yield self.cache.increment_page_count(domain)
+            added_pages.append(page)
+
+        if added_pages:
+            self.application.event_bus.publish(dumps({
+                'type': 'new-page',
+                'pageUrl': added_pages[0].url
+            }))
+
+        pages = list(added_pages)
+
+        self.write(str(len(pages)))
+
+    def add_domains(self, domains):
+        resulting_domains = {}
+
+        #existing_domains = self.db.query(Domain.id, Domain.url).filter(
+            #or_(
+                #Domain.url.in_([
+                    #domain[1] for domain in domains
+                #]),
+                #Domain.url.in_([
+                    #domain[1].rstrip('/') for domain in domains
+                #]),
+                #Domain.url.in_([
+                    #"%s/" % domain[1] for domain in domains
+                #])
+            #)
+        #).all()
+        #existing_domain_dict = dict([(domain[0], domain[1]) for domain in existing_domains])
+
+        for domain_name, domain_url in domains:
+            if domain_url in resulting_domains:
+                continue
+
+            domain = self.db.query(Domain).filter(or_(
+                Domain.url == domain_url,
+                Domain.url == domain_url.rstrip('/')
+            )).first()
+
+            if domain:
+                resulting_domains[domain_url] = domain
+                continue
+
+            key = domain_url.rstrip('/')
+            domain = self.db.query(Domain).filter(Domain.url == key).first()
+            if domain:
+                resulting_domains[domain_url] = domain
+                continue
+
+            key = ("%s/" % domain_url)
+            domain = self.db.query(Domain).filter(Domain.url == key).first()
+            if domain:
+                resulting_domains[domain_url] = domain
+                continue
+
+            resulting_domains[domain_url] = Domain(
+                name=domain_name,
+                url=domain_url,
+                url_hash=hashlib.sha512(domain_url).hexdigest())
+
+            self.db.add(resulting_domains[domain_url])
+
+            self.application.event_bus.publish(dumps({
+                'type': 'new-domain',
+                'domainUrl': domain_url
+            }))
+
+        self.db.flush()
+        return resulting_domains
+
+
+class PageViolationsPerDayHandler(BaseHandler):
+
+    def get(self, uuid):
+        page = Page.by_uuid(uuid, self.db)
+
+        if not page:
+            self.set_status(404, 'Page UUID [%s] not found' % uuid)
+            return
+
+        violations_per_day = page.get_violations_per_day(self.db)
+
+        page_json = {
+            "violations": violations_per_day
+        }
+
+        self.write_json(page_json)
---
+++ packages/holmes/holmes-0.6.2.tar.gz/holmes-0.6.2/holmes/reviewer.py
@@ -0,0 +1,298 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+from os.path import join
+import urlparse
+import inspect
+import email.utils as eut
+from datetime import datetime
+
+import codecs
+from box.util.rotunicode import RotUnicode
+codecs.register(RotUnicode.search_function)
+
+import requests
+from requests.exceptions import ConnectionError
+import lxml.html
+import logging
+from ujson import dumps
+
+from holmes.config import Config
+from holmes.facters import Facter
+from holmes.validators.base import Validator
+
+
+class InvalidReviewError(RuntimeError):
+    pass
+
+
+class ReviewDAO(object):
+    def __init__(self, page_uuid, page_url, last_modified=None, expires=None):
+        self.page_uuid = page_uuid
+        self.page_url = page_url
+        self.last_modified = last_modified
+        self.expires = expires
+        self.facts = {}
+        self.violations = []
+        self.data = {}
+        self._current = None
+
+    def add_fact(self, key, value):
+        self.facts[key] = {
+            'key': key,
+            'value': value
+        }
+
+    def add_violation(self, key, value, points):
+        self.violations.append({
+            'key': key,
+            'value': value,
+            'points': points
+        })
+
+    def to_dict(self):
+        return {
+            'page_uuid': self.page_uuid,
+            'page_url': self.page_url,
+            'facts': self.facts.values(),
+            'violations': self.violations,
+            'lastModified': self.last_modified,
+            'expires': self.expires
+        }
+
+
+class Reviewer(object):
+    def __init__(
+            self, api_url, page_uuid, page_url, page_score, config=None, validators=[], facters=[],
+            async_get=None, wait=None, wait_timeout=None):
+
+        self.api_url = api_url
+
+        self.page_uuid = page_uuid
+        self.page_url = page_url
+        self.page_score = page_score
+
+        self.review_dao = ReviewDAO(self.page_uuid, self.page_url)
+
+        assert isinstance(config, Config), 'config argument must be an instance of holmes.config.Config'
+        self.config = config
+
+        for facter in facters:
+            message = 'All facters must subclass holmes.facters.Facter (Error: %s)' % facter.__class__.__name__
+            assert inspect.isclass(facter), message
+            assert issubclass(facter, Facter), message
+
+        for validator in validators:
+            message = 'All validators must subclass holmes.validators.base.Validator (Error: %s)' % validator.__class__.__name__
+            assert inspect.isclass(validator), message
+            assert issubclass(validator, Validator), message
+
+        self.validators = validators
+        self.facters = facters
+
+        self.responses = {}
+        self.raw_responses = {}
+        self.status_codes = {}
+
+        self.proxies = None
+        if self.config.HTTP_PROXY_HOST is not None:
+            proxy = "%s:%s" % (self.config.HTTP_PROXY_HOST, self.config.HTTP_PROXY_PORT)
+            http_proxy = proxy
+            https_proxy = proxy
+
+            self.proxies = {
+                "http": http_proxy,
+                "https": https_proxy,
+            }
+
+        self.async_get_func = async_get
+        self._wait_for_async_requests = wait
+        self._wait_timeout = wait_timeout
+
+    def _async_get(self, url, handler, method='GET', **kw):
+        if self.async_get_func:
+            self.async_get_func(url, handler, method, **kw)
+
+    def _get(self, url):
+        #if self.proxies:
+            #logging.debug('Getting "%s" using proxy "%s"...' % (url, self.proxies.get('http', None)))
+            #return requests.get(url, proxies=self.proxies)
+
+        return requests.get(url)
+
+    def _post(self, url, data):
+        #if self.proxies:
+            #logging.debug('Posting to "%s" using proxy "%s"...' % (url, self.proxies.get('http', None)))
+            #return requests.post(url, data=data, proxies=self.proxies)
+
+        return requests.post(url, data=data)
+
+    def review(self):
+        self.load_content(self.content_loaded)
+        self.wait_for_async_requests()
+
+    def load_content(self, callback):
+        self._async_get(self.page_url, callback)
+
+    def content_loaded(self, url, response):
+        if response.status_code > 399:
+            if response.text:
+                text = response.text.decode('rotunicode')
+            else:
+                text = 'Empty response.text'
+
+            msg = "Could not load '%s' (%s) - %s!" % (url,
+                                                      response.status_code,
+                                                      text)
+            logging.error(msg)
+            return
+
+        logging.debug('Content for url %s loaded.' % url)
+
+        last_modified = None
+
+        if 'Last-Modified' in response.headers:
+            last_modified = datetime(*eut.parsedate(response.headers['Last-Modified'])[:6])
+
+        self.review_dao.last_modified = last_modified
+
+        expires = None
+        if 'Expires' in response.headers:
+            expires = datetime(*eut.parsedate(response.headers['Expires'])[:6])
+
+        self.review_dao.expires = expires
+
+        self._current = response
+
+        try:
+            self._current.html = lxml.html.fromstring(response.text)
+        except (lxml.etree.XMLSyntaxError, lxml.etree.ParserError):
+            self._current.html = None
+
+            # can't do this there, this is not a validator
+            #self.add_violation(
+                #key='invalid.content',
+                #title='Invalid Content',
+                #description='Fail to parse content from %s' % url,
+                #points=1000)
+
+        self.run_facters()
+        self.wait_for_async_requests()
+
+        self.run_validators()
+        self.wait_for_async_requests()
+
+        self.save_review()
+
+    @property
+    def current(self):
+        return self._current
+
+    @property
+    def current_html(self):
+        if not hasattr(self.current, 'html') or self.current.html is None:
+            return lxml.html.HtmlElement()
+        else:
+            return self.current.html
+
+    def run_facters(self):
+        for facter in self.facters:
+            logging.debug('---------- Started running facter %s ---------' % facter.__name__)
+            facter_instance = facter(self)
+            facter_instance.get_facts()
+
+    def run_validators(self):
+        for validator in self.validators:
+            logging.debug('---------- Started running validator %s ---------' % validator.__name__)
+            validator_instance = validator(self)
+            validator_instance.validate()
+
+    def get_url(self, url):
+        return join(self.api_url.rstrip('/'), url.lstrip('/'))
+
+    def increase_lambda_tax(self, tax):
+        post_url = self.get_url('/tax')
+        response = self._post(post_url, data={'tax': tax})
+
+        if response.status_code > 399:
+            logging.error('Failed to increase lambda tax by %.2f (Status: %s, Text: %s)' % (
+                tax,
+                response.status_code,
+                response.text
+            ))
+
+    def enqueue(self, urls):
+        if not urls:
+            return
+
+        #if isinstance(urls, basestring):
+            #post_url = self.get_url('/page')
+            #data = dumps({
+                #'url': urls,
+                #'origin_uuid': str(self.page_uuid)
+            #})
+            #error_message = "Could not enqueue page '" + urls + "'! Status Code: %d, Error: %s"
+
+            #response = self._post(post_url, data=data)
+
+            #if response.status_code > 399:
+                #logging.error(error_message % (
+                    #response.status_code,
+                    #response.text
+                #))
+        #else:
+        post_url = self.get_url('/page')
+        #post_url = self.get_url('/pages')
+
+        for url, score in urls:
+            data = dumps({
+                'url': url,
+                'score': score,
+                'origin_uuid': str(self.page_uuid)
+            })
+
+            error_message = "Could not enqueue page '" + url + "'! Status Code: %d, Error: %s"
+
+            response = self._post(post_url, data=data)
+
+            if response.status_code > 399:
+                logging.error(error_message % (
+                    response.status_code,
+                    response.text
+                ))
+
+    def add_fact(self, key, value):
+        self.review_dao.add_fact(key, value)
+
+    def add_violation(self, key, value, points):
+        self.review_dao.add_violation(key, value, points)
+
+    def save_review(self):
+        url = self.get_url('/page/%s/review/' % (self.page_uuid))
+
+        try:
+            data = dumps(self.review_dao.to_dict())
+            response = self._post(url, data={
+                'review': data
+            })
+        except ConnectionError:
+            err = sys.exc_info()[1]
+            logging.error("Could not save review! ConnectionError - %s (%s)" % (
+                url,
+                str(err)
+            ))
+            return
+
+        if response.status_code > 399:
+            logging.error("Could not save review! Status Code: %d, Error: %s" % (
+                response.status_code,
+                response.text
+            ))
+
+    def wait_for_async_requests(self):
+        self._wait_for_async_requests(self._wait_timeout)
+
+    def is_root(self):
+        result = urlparse.urlparse(self.page_url)
+        return '{0}://{1}'.format(result.scheme, result.netloc) == self.page_url.rstrip('/')
