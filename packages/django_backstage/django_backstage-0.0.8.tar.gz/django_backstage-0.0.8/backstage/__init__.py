3a03a7cca4948f58a2cf8f1c853fd958b5d9a659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package django_backstage/0.0.8/django_backstage-0.0.8.tar.gz

---
+++ packages/django_backstage/django_backstage-0.0.8.tar.gz/django_backstage-0.0.8/backstage/__init__.py
@@ -0,0 +1,2 @@
+from backstage.project import Project, new_project, use_project
+from backstage.site import Site
---
+++ packages/django_backstage/django_backstage-0.0.8.tar.gz/django_backstage-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_backstage',
+    version='0.0.8',
+    description='Django project and site deployment using uWSGI, nginx, etc.',
+    author='MiddleFork',
+    author_email='walker@mfgis.com',
+    packages=find_packages(),
+    install_requires=[
+        'django==1.5.5',
+        "setuptools_git >= 0.3",
+        "mezzanine",
+        "psycopg2",
+        "django-selectable", ],
+)
