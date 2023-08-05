35171f99712debd4dbada32dfdc29cd81bdbb9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:29 +0000

    Add package django-tsearch2/0.3.0/django-tsearch2-0.3.0.tar.gz

---
+++ packages/django-tsearch2/django-tsearch2-0.3.0.tar.gz/django-tsearch2-0.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name = "django-tsearch2",
+    version = "0.3.0",
+    url = "http://github.com/hcarvalhoalves/django-tsearch2",
+    description = "Postgresql's full text search support for Django",
+    author = "Henrique Carvalho Alves",
+    author_email = "hcarvalhoalves@gmail.com",
+    packages = [
+        'tsearch2',
+        'tsearch2.management',
+        'tsearch2.management.commands'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    zip_safe = False,
+)
