68ffe566f8686a8d89c6008bfba4d285614d0305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:09 +0000

    Add package django-admin-tools-stats/0.5.4/django-admin-tools-stats-0.5.4.tar.gz

---
+++ packages/django-admin-tools-stats/django-admin-tools-stats-0.5.4.tar.gz/django-admin-tools-stats-0.5.4/admin_tools_stats/__init__.py
@@ -0,0 +1,26 @@
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2014 Star2Billing S.L.
+#
+# The Initial Developer of the Original Code is
+# Arezqui Belaid <info@star2billing.com>
+#
+
+VERSION = (0, 5, 4, "final", 0)  # edit also docs/source/conf.py
+
+
+def get_version():
+    if VERSION[3] == "final":
+        return "%s.%s.%s" % (VERSION[0], VERSION[1], VERSION[2])
+    elif VERSION[3] == "dev":
+        if VERSION[2] == 0:
+            return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[3], VERSION[4])
+        return "%s.%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3], VERSION[4])
+    else:
+        return "%s.%s.%s%s" % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+
+
+__version__ = get_version()
