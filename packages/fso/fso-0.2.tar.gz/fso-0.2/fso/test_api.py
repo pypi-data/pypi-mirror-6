58a37de038e015e5fa562610211ec97fc2e0f06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:15 +0000

    Add package fso/0.2/fso-0.2.tar.gz

---
+++ packages/fso/fso-0.2.tar.gz/fso-0.2/fso/test_api.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# auth: metagriffin <mg.github@metagriffin.net>
+# date: 2013/10/20
+# copy: (C) Copyright 2013-EOT metagriffin -- see LICENSE.txt
+#------------------------------------------------------------------------------
+# This software is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#------------------------------------------------------------------------------
+
+import sys, os, unittest, tempfile, uuid, stat
+
+from . import api
+
+#------------------------------------------------------------------------------
+class TestApi(unittest.TestCase):
+
+  #----------------------------------------------------------------------------
+  def test_api_push(self):
+    fname = tempfile.mktemp(prefix='fso-test_api-unittest.')
+    tdata = 'random-unique-data:' + str(uuid.uuid4())
+    self.assertFalse(os.path.exists(fname))
+    with api.push() as overlay:
+      self.assertFalse(os.path.exists(fname))
+      with open(fname, 'wb') as fp:
+        fp.write(tdata)
+      self.assertTrue(os.path.exists(fname))
+      self.assertEqual(len(overlay.entries), 1)
+      entry = overlay.entries.values()[0]
+      self.assertEqual(entry.path, fname)
+      self.assertEqual(entry.mode, stat.S_IFREG)
+      self.assertEqual(entry.content, tdata)
+    self.assertFalse(os.path.exists(fname))
+
+  #----------------------------------------------------------------------------
+  def test_api_multipush(self):
+    fname1 = tempfile.mktemp(prefix='fso-test_api-unittest.1.')
+    fname2 = tempfile.mktemp(prefix='fso-test_api-unittest.2.')
+    tdata1 = 'random-unique-data.1:' + str(uuid.uuid4())
+    tdata2 = 'random-unique-data.2:' + str(uuid.uuid4())
+    self.assertFalse(os.path.exists(fname1))
+    self.assertFalse(os.path.exists(fname2))
+    with api.push() as overlay1:
+      self.assertFalse(os.path.exists(fname1))
+      self.assertFalse(os.path.exists(fname2))
+      with open(fname1, 'wb') as fp:
+        fp.write(tdata1)
+      self.assertTrue(os.path.exists(fname1))
+      self.assertFalse(os.path.exists(fname2))
+      with api.push() as overlay2:
+        self.assertTrue(os.path.exists(fname1))
+        self.assertFalse(os.path.exists(fname2))
+        with open(fname2, 'wb') as fp:
+          fp.write(tdata2)
+        self.assertTrue(os.path.exists(fname1))
+        self.assertTrue(os.path.exists(fname2))
+        self.assertEqual(overlay1.entries.keys(), [fname1])
+        self.assertEqual(overlay2.entries.keys(), [fname2])
+      self.assertTrue(os.path.exists(fname1))
+      self.assertFalse(os.path.exists(fname2))
+    self.assertFalse(os.path.exists(fname1))
+    self.assertFalse(os.path.exists(fname2))
+
+
+#------------------------------------------------------------------------------
+# end of $Id$
+#------------------------------------------------------------------------------
---
+++ packages/fso/fso-0.2.tar.gz/fso-0.2/setup.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# auth: metagriffin <mg.github@metagriffin.net>
+# date: 2013/10/19
+# copy: (C) Copyright 2013-EOT metagriffin -- see LICENSE.txt
+#------------------------------------------------------------------------------
+# This software is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#------------------------------------------------------------------------------
+
+import os, sys, setuptools
+from setuptools import setup, find_packages
+
+# require python 2.7+
+if sys.hexversion < 0x02070000:
+  raise RuntimeError('This package requires python 2.7 or better')
+
+heredir = os.path.abspath(os.path.dirname(__file__))
+def read(*parts, **kw):
+  try:    return open(os.path.join(heredir, *parts)).read()
+  except: return kw.get('default', '')
+
+test_dependencies = [
+  'nose                 >= 1.3.0',
+  'coverage             >= 3.5.3',
+  ]
+
+dependencies = [
+  'distribute           >= 0.6.24',
+  'six                  >= 1.4.1',
+  'aadict               >= 0.2.0',
+  'globre               >= 0.0.5',
+  'asset                >= 0.0.4',
+  'morph                >= 0.1.1',
+  ]
+
+entrypoints = {}
+
+classifiers = [
+  'Development Status :: 4 - Beta',
+  #'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'Programming Language :: Python',
+  'Operating System :: OS Independent',
+  'Natural Language :: English',
+  'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+  ]
+
+setup(
+  name                  = 'fso',
+  version               = read('VERSION.txt', default='0.0.1').strip(),
+  description           = 'File System Overlay - filesystem layering for unit testing',
+  long_description      = read('README.rst'),
+  classifiers           = classifiers,
+  author                = 'metagriffin',
+  author_email          = 'mg.pypi@uberdev.org',
+  url                   = 'http://github.com/metagriffin/fso',
+  keywords              = 'unit testing file system open read write layering protection',
+  packages              = find_packages(),
+  platforms             = ['any'],
+  include_package_data  = True,
+  zip_safe              = True,
+  install_requires      = dependencies,
+  tests_require         = test_dependencies,
+  test_suite            = 'fso',
+  entry_points          = entrypoints,
+  license               = 'GPLv3+',
+  )
+
+#------------------------------------------------------------------------------
+# end of $Id$
+#------------------------------------------------------------------------------
