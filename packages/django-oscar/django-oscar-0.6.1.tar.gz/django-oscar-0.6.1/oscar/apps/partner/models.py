f934579b08136714981bb001764ddff05c0f5c26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:55 +0000

    Add package django-oscar/0.6.1/django-oscar-0.6.1.tar.gz

---
+++ packages/django-oscar/django-oscar-0.6.1.tar.gz/django-oscar-0.6.1/oscar/apps/partner/models.py
@@ -0,0 +1,19 @@
+from oscar.apps.address.abstract_models import AbstractPartnerAddress
+from oscar.apps.partner.abstract_models import (
+    AbstractPartner, AbstractStockRecord, AbstractStockAlert)
+
+
+class Partner(AbstractPartner):
+    pass
+
+
+class PartnerAddress(AbstractPartnerAddress):
+    pass
+
+
+class StockRecord(AbstractStockRecord):
+    pass
+
+
+class StockAlert(AbstractStockAlert):
+    pass
