9193b05786afff6b1b1816a3a4ac76e9b207f7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package SizeFS/0.2.4/SizeFS-0.2.4.tar.gz

---
+++ packages/SizeFS/SizeFS-0.2.4.tar.gz/SizeFS-0.2.4/setup.py
@@ -0,0 +1,23 @@
+__author__ = 'mm'
+from distutils.core import setup
+
+setup(
+    name='SizeFS',
+    version='0.2.4',
+    author='Mark McArdle',
+    author_email='mark.mcardle@sohonet.com',
+    packages=['sizefs', 'tests'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/SizeFS/',
+    download_url='https://github.com/sohonetlabs/sizefs',
+    license='LICENSE.txt',
+    description='SizeFS is a mock filesystem for creating files of particular '
+                'sizes with specified contents.',
+    long_description=open('README.txt').read(),
+    keywords=['testing', 'files', 'size'],
+    install_requires=[
+        "fusepy>=2.0.2",
+        "Cython>=0.19.1",
+        "docopt==0.6.1"
+        ],
+)
