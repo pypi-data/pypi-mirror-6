d1a4167566d7edde223082f02dca825eddb2e685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:52 +0000

    Add package b/1.0.0/b-1.0.0.zip

---
+++ packages/b/b-1.0.0.zip/b-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'b',
+    version = '1.0.0',
+    py_modules = ['b'],
+    
+    author = 'yuchunyang',
+    author_email = '969844405@qq.com',
+    url = 'www.baidu.com',
+    description = 'ccccccccccc',
+    )
