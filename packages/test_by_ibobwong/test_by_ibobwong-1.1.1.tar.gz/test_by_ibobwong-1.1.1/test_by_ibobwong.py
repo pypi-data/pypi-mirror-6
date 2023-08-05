21e32c1584a9c193df5a36fde7c6d7c482e03eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:35 +0000

    Add package test_by_ibobwong/1.1.1/test_by_ibobwong-1.1.1.tar.gz

---
+++ packages/test_by_ibobwong/test_by_ibobwong-1.1.1.tar.gz/test_by_ibobwong-1.1.1/test_by_ibobwong.py
@@ -0,0 +1,27 @@
+'''Generate Random passwords with by-default Min 
+2 numeric, 2 alpha, 2 special symbols and length 8
+'''
+
+import random
+import string
+
+def getPassword(passwrd_len = 8, 
+            alpha_len = 2, 
+            numeric_len = 2,
+            special_len = 2):
+    '''    
+    http://code.activestate.com/recipes/578467/
+    '''
+    alpha   = string.ascii_letters
+    numeric = string.digits
+    special = string.punctuation
+    password = [random.choice(alpha) for x in range(alpha_len)]
+    password += [random.choice(numeric) for x in range(numeric_len)]
+    password += [random.choice(special) for x in range(special_len)]
+    length = passwrd_len - (alpha_len + numeric_len + special_len)
+    password += [random.choice(special + numeric + special) for x in range(length)]
+    random.shuffle(password)
+    return ''.join(password)
+
+def gethello():
+	print('hello')
