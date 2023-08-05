ab1af2647acaab9e71ef0bb4d9da0df84c08f8dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:47 +0000

    Add package pytemplates/0.1.3.1/pytemplates-0.1.3.1.tar.gz

---
+++ packages/pytemplates/pytemplates-0.1.3.1.tar.gz/pytemplates-0.1.3.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='pytemplates',
+    version='0.1.3.1',
+    description='Lightweight Python HTML Template Engine',
+    keywords=['Django', 'HTML', 'Template', 'Templating Python', 'DSL'],
+    author='Zeid Zabaneh',
+    author_email='zeid@ncf.ca',
+    url='http://bitbucket.com/zeid/pytemplates',
+    packages=['pytemplates'],
+)
