822a2f9a15933456eabed8b2ce8729d84d7cc694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:19 +0000

    Add package mohawk/0.0.2/mohawk-0.0.2.tar.gz

---
+++ packages/mohawk/mohawk-0.0.2.tar.gz/mohawk-0.0.2/mohawk/sender.py
@@ -0,0 +1,74 @@
+import logging
+
+from .base import HawkAuthority, Resource
+from .util import (calculate_mac,
+                   parse_authorization_header,
+                   validate_credentials)
+
+__all__ = ['Sender']
+log = logging.getLogger(__name__)
+
+
+class Sender(HawkAuthority):
+
+    def __init__(self, credentials,
+                 url,
+                 method,
+                 content='',
+                 content_type='text/plain',
+                 nonce=None,
+                 ext=None,
+                 app=None,
+                 dlg=None,
+                 seen_nonce=None,
+                 # For easier testing:
+                 _timestamp=None):
+
+        self.reconfigure(credentials)
+        self.request_header = None
+        self.seen_nonce = seen_nonce
+
+        log.debug('generating request header')
+        self.req_resource = Resource(url=url,
+                                     credentials=self.credentials,
+                                     ext=ext,
+                                     app=app,
+                                     dlg=dlg,
+                                     nonce=nonce,
+                                     method=method,
+                                     content=content,
+                                     timestamp=_timestamp,
+                                     content_type=content_type)
+
+        mac = calculate_mac('header', self.req_resource)
+        self.request_header = self._make_header(self.req_resource, mac)
+
+    def accept_response(self,
+                        response_header,
+                        content='',
+                        content_type='text/plain',
+                        **auth_kw):
+        log.debug('accepting response {header}'
+                  .format(header=response_header))
+
+        parsed_header = parse_authorization_header(response_header)
+
+        resource = Resource(ext=parsed_header.get('ext', None),
+                            nonce=parsed_header['nonce'],
+                            timestamp=parsed_header['ts'],
+                            content=content,
+                            content_type=content_type,
+                            # The following response attributes must match
+                            # our request:
+                            url=self.req_resource.url,
+                            method=self.req_resource.method,
+                            app=self.req_resource.app,
+                            dlg=self.req_resource.dlg,
+                            credentials=self.credentials,
+                            seen_nonce=self.seen_nonce)
+
+        self._authorize('response', parsed_header, resource, **auth_kw)
+
+    def reconfigure(self, credentials):
+        validate_credentials(credentials)
+        self.credentials = credentials
