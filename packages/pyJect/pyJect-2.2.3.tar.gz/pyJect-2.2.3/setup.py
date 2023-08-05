05dabc7d073b28b7422f3004d123a340c0fdf2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:47 +0000

    Add package pyJect/2.2.3/pyJect-2.2.3.tar.gz

---
+++ packages/pyJect/pyJect-2.2.3.tar.gz/pyJect-2.2.3/setup.py
@@ -0,0 +1,28 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+from versionup import get_version
+version = get_version()
+
+
+config = {
+	'name': 'pyJect',
+    'version': version,
+	'author': 'Kyle Roux',
+	'author_email': 'jstacoder@gmail.com',
+	'description': 'a command line tool to make and orginize projects',
+	'long_description': open('README','r').read(),
+	'packages': ['pyject'],
+    'py_modules': ['pyject'],
+    'requires' : ['versionup'],
+	'entry_points': {
+		'console_scripts': [
+			'pyject = pyject.main:main'
+			]
+		}
+	}
+
+setup(**config)
+
