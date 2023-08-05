f54243396afc8d48389df40e72119a018c97003a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:36 +0000

    Add package plicata/0.0.132/plicata-0.0.132.tar.gz

---
+++ packages/plicata/plicata-0.0.132.tar.gz/plicata-0.0.132/plicata/reviews/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from plicata.books.models import Book
+from mezzanine.core.models import Displayable, Ownable, RichText, Slugged
+from mezzanine.core.fields import FileField, RichTextField
+
+class Review(models.Model):
+    book = models.ForeignKey(Book)
+    visible = models.BooleanField(default=True)
+    sortorder = models.IntegerField(default=999)
+    reviewer = models.TextField(max_length=80)
+    entity = models.TextField(max_length=80)
+    citation = models.TextField(max_length=255)
+    body = RichTextField()
+
+class Quote(models.Model):
+    review = models.ForeignKey(Review)
+    visible = models.BooleanField(default=True)
+    sortorder = models.IntegerField(default=999)
+    quote = RichTextField()
