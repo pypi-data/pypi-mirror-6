4e55a6f28faa55994332dd4465e7d58a1cb39630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:13 +0000

    Add package js.leaflet/0.7.1/js.leaflet-0.7.1.zip

---
+++ packages/js.leaflet/js.leaflet-0.7.1.zip/js.leaflet-0.7.1/CHANGES.rst
@@ -0,0 +1,42 @@
+CHANGES
+*******
+
+0.7.1 (2014-01-06)
+==================
+
+- Upgraded Leaflet to v0.7.1.
+  [davidjb]
+
+
+0.6.3 (2013-07-18)
+==================
+
+- Upgraded Leaflet to v0.6.3.
+  [davidjb]
+
+
+0.6.2 (2013-07-03)
+==================
+
+- Upgraded Leaflet to v0.6.2.
+  [davidjb]
+- Use Fanstatic for minification of CSS. This library now requires Fanstatic
+  1.0a or above.
+  [davidjb]
+- Fix typo in library name.
+  [davidjb]
+- Add tox and Travis CI support.
+  [davidjb]
+
+
+0.5 (2013-02-04)
+================
+
+- Upgraded Leaflet to v0.5.
+  [davidjb]
+
+
+0.4.5 (2013-01-07)
+==================
+
+- Initial release.
---
+++ packages/js.leaflet/js.leaflet-0.7.1.zip/js.leaflet-0.7.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+# The version of the wrapped library is the starting point for the
+# version number of the python package.
+# In bugfix releases of the python package, add a '-' suffix and an
+# incrementing integer.
+# For example, a packaging bugfix release version 1.4.4 of the
+# js.jquery package would be version 1.4.4-1 .
+
+version = '0.7.1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('js', 'leaflet', 'test_leaflet.txt')
+    + '\n' +
+    read('CHANGES.rst'))
+
+setup(
+    name='js.leaflet',
+    version=version,
+    description="Fanstatic packaging of Leaflet",
+    long_description=long_description,
+    classifiers=[],
+    keywords='javascript mapping map leaflet js openlayers',
+    author='Fanstatic Developers',
+    author_email='fanstatic@googlegroups.com',
+    url='https://github.com/davidjb/js.leaflet',
+    license='BSD',
+    packages=find_packages(),namespace_packages=['js'],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=[
+        'setuptools-git',
+    ],
+    install_requires=[
+        'fanstatic>=1.0a',
+        'setuptools',
+        ],
+    entry_points={
+        'fanstatic.libraries': [
+            'leaflet = js.leaflet:library',
+            ],
+        },
+    )
