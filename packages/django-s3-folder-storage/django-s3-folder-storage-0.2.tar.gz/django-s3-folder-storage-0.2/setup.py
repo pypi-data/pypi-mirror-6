80406833cab563c3815cbb3e3c97c99f5423cf39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:56 +0000

    Add package django-s3-folder-storage/0.2/django-s3-folder-storage-0.2.tar.gz

---
+++ packages/django-s3-folder-storage/django-s3-folder-storage-0.2.tar.gz/django-s3-folder-storage-0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+# Utility function to read README file
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='django-s3-folder-storage',
+    version='0.2',
+    description="Quick extension of django-storages' S3BotoStorage to allow separate folders for uploaded and static media within an S3 bucket.",
+    author='Benjamin W Stookey',
+    author_email='ben.stookey@gmail.com',
+    url='https://github.com/jamstooks/django-s3-folder-storage',
+    long_description=read("README.md"),
+    packages=[
+        's3_folder_storage',
+        's3_folder_storage.tests'
+        ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+    ],
+    test_suite='tests.main',
+    install_requires=['django-storages', 'boto'],
+)
