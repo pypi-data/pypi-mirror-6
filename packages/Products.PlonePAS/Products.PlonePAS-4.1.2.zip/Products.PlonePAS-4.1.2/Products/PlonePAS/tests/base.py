c64a1592071a92ef99b2dd5981313e0bdd4a79e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package Products.PlonePAS/4.1.2/Products.PlonePAS-4.1.2.zip

---
+++ packages/Products.PlonePAS/Products.PlonePAS-4.1.2.zip/Products.PlonePAS-4.1.2/Products/PlonePAS/tests/base.py
@@ -0,0 +1,2 @@
+from plone.app.testing.bbb import PloneTestCase as TestCase
+from plone.app.testing.bbb import PloneTestCase as FunctionalTestCase
---
+++ packages/Products.PlonePAS/Products.PlonePAS-4.1.2.zip/Products.PlonePAS-4.1.2/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+
+version = '4.1.2'
+
+install_requires=[
+        'setuptools',
+        'plone.memoize',
+        'plone.session',
+        'plone.i18n',
+        'Products.CMFCore',
+        'Products.GenericSetup',
+        'Products.PluggableAuthService',
+        'Zope2 > 2.12.4',
+      ]
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    install_requires.append('ordereddict')
+
+setup(name='Products.PlonePAS',
+      version=version,
+      description="PlonePAS adapts the PluggableAuthService for use by Plone.",
+      long_description=open("README.txt").read() + "\n" + \
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+      ],
+      keywords='Zope CMF Plone PAS authentication',
+      author='Kapil Thangavelu, Wichert Akkerman',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/Products.PlonePAS',
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+        test=[
+            'plone.app.testing',
+            'plone.testing',
+            'Products.Archetypes',
+        ],
+        atstorage=[
+            'Products.Archetypes',
+        ]
+      ),
+      install_requires=install_requires,
+)
