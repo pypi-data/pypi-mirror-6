823409d03cfd757aacc20352e5b875c12a590ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package gipc/0.4.0/gipc-0.4.0.zip

---
+++ packages/gipc/gipc-0.4.0.zip/gipc-0.4.0/gipc/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012-2013 Jan-Philip Gehrcke. See LICENSE file for details.
+
+
+__version__ = '0.4.0'
+
+
+from gipc import pipe, start_process, GIPCError, GIPCClosed, GIPCLocked
