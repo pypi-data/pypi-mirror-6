dc1ba0b3b3b693172c6c380cf43a818c4d1d2d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package postgres/2.1.2/postgres-2.1.2.tar.gz

---
+++ packages/postgres/postgres-2.1.2.tar.gz/postgres-2.1.2/setup.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from setuptools import setup, find_packages
+
+setup( name='postgres'
+     , author='Gittip, LLC'
+     , description="postgres is a high-value abstraction over psycopg2."
+     , url='https://postgres-py.readthedocs.org'
+     , version='2.1.2'
+     , packages=find_packages()
+     , install_requires=['psycopg2 >= 2.5.0']
+     , classifiers=[
+         'Development Status :: 5 - Production/Stable',
+         'Intended Audience :: Developers',
+         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python :: 2',
+         'Programming Language :: Python :: 2.6',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.2',
+         'Programming Language :: Python :: 3.3',
+         'Programming Language :: SQL',
+         'Topic :: Database :: Front-Ends',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+       ],
+      )
