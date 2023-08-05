5eb71dba54864f8921b009c59b9bb79ecce5b5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:39 +0000

    Add package infi.recipe.application-packager/0.4.1/infi.recipe.application_packager-0.4.1.tar.gz

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.4.1.tar.gz/infi.recipe.application_packager-0.4.1/setup.py
@@ -0,0 +1,62 @@
+
+SETUP_INFO = dict(
+    name = 'infi.recipe.application_packager',
+    version = '0.4.1',
+    author = 'Guy Rozendorn',
+    author_email = 'guy@rzn.co.il',
+
+    url = 'https://github.com/Infinidat/infi.recipe.application_packager',
+    license = 'PSF',
+    description = """buildout recipe for packaging projects as applications""",
+    long_description = """buildout recipe for packaging projects are applications""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = ['Archive',
+'git-py',
+'infi.execute',
+'infi.pypi_manager',
+'infi.pyutils',
+'infi.recipe.buildout_logging',
+'infi.recipe.close_application',
+'infi.recipe.console_scripts',
+'infi.registry',
+'lxml',
+'setuptools',
+'zc.buildout'],
+    namespace_packages = ['infi', 'infi.recipe'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': ['postinst.in', 'signtool.exe', 'control.in', 'changelog.in', 'rules.in', 'prerm.in', 'template.wxs', 'ez_setup.txt', 'silent_launcher-x86.exe', 'rpmspec.in', 'md5sums.in', 'silent_launcher-x64.exe']},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = {
+        'console_scripts': [],
+        'gui_scripts': [],
+        'zc.buildout': [
+                        'default = infi.recipe.application_packager.auto:Recipe',
+                        'msi = infi.recipe.application_packager.msi:Recipe',
+                        'rpm = infi.recipe.application_packager.rpm:Recipe',
+                        'deb = infi.recipe.application_packager.deb:Recipe',
+                       ]
+        },
+    )
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
