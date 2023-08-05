253c11980cf9616a7ffce8f457afdeb428089f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package balanced/0.11.14/balanced-0.11.14.tar.gz

---
+++ packages/balanced/balanced-0.11.14.tar.gz/balanced-0.11.14/balanced/exc.py
@@ -0,0 +1,46 @@
+import requests
+
+
+class BalancedError(Exception):
+    pass
+
+
+class ResourceError(BalancedError):
+    pass
+
+
+class NoResultFound(BalancedError):
+    pass
+
+
+class MultipleResultsFound(BalancedError):
+    pass
+
+
+class HTTPError(BalancedError, requests.HTTPError):
+    """
+    Baseclass for all HTTP exceptions.
+    """
+    status_code = None
+
+
+class MoreInformationRequiredError(HTTPError):
+    redirect_uri = None
+
+
+class FundingInstrumentVerificationFailure(HTTPError):
+    pass
+
+
+class BankAccountVerificationFailure(FundingInstrumentVerificationFailure):
+    pass
+
+
+category_code_map = {
+    'bank-account-authentication-not-pending':
+    BankAccountVerificationFailure,
+    'bank-account-authentication-failed':
+    BankAccountVerificationFailure,
+    'bank-account-authentication-already-exists':
+    BankAccountVerificationFailure,
+}
