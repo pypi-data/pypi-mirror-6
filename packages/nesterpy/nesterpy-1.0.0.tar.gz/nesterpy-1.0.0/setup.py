8864541e7f3ef525dfb94195f1e9b3792c7296d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:39 +0000

    Add package nesterpy/1.0.0/nesterpy-1.0.0.tar.gz

---
+++ packages/nesterpy/nesterpy-1.0.0.tar.gz/nesterpy-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'nesterpy',
+    version = '1.0.0',
+    py_modules = ['nesterpy'],
+    author = 'hfpython',
+    author_email = 'hfpython@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nester lists',
+)
