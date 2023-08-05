f02cb24515f2ae5d94806915af91fdb355a52df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:16 +0000

    Add package libComXML/2.0.2/libComXML-2.0.2.tar.gz

---
+++ packages/libComXML/libComXML-2.0.2.tar.gz/libComXML-2.0.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='libComXML',
+    version='2.0.2',
+    url='http://git.gisce.lan/libComXML.git',
+    author='GISCE Enginyeria, SL',
+    author_email='devel@gisce.net',
+    packages=find_packages(),
+    install_requires=['lxml'],
+    license='None',
+    description='libComXML',
+    long_description=open('README.md').read(),
+)
