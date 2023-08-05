b990518ebce1f54f0a6b46f16b0cbb37e3e024ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package raygun4py/1.1.3/raygun4py-1.1.3.zip

---
+++ packages/raygun4py/raygun4py-1.1.3.zip/raygun4py-1.1.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='raygun4py',
+    version='1.1.3',
+    packages=['raygun4py'],
+    license='LICENSE',
+    url='http://raygun.io',
+    author='Mindscape',
+    author_email='contact@mindscape.co.nz',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "jsonpickle == 0.4.0"
+    ],
+)
