9eaa252a5fadcd4417bfa7bfb247ede94dc8417f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:37 +0000

    Add package killdupes/0.1.5/killdupes-0.1.5.tar.gz

---
+++ packages/killdupes/killdupes-0.1.5.tar.gz/killdupes-0.1.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import killdupes
+
+
+setup(
+    name='killdupes',
+    version=killdupes.__version__,
+    description='Kill duplicate files, finding partial files as well',
+    author='Martin Matusiak',
+    author_email='numerodix@gmail.com',
+    url='https://github.com/numerodix/killdupes',
+
+    packages=find_packages('.'),
+    package_dir={'': '.'},
+
+    # don't install as zipped egg
+    zip_safe=False,
+
+    scripts=[
+        'killdupes/killdupes.py',
+    ],
+
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
