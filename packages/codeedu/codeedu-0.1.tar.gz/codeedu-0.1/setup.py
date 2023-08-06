7ed9caa250b46ea6e69beb3fcf6d6f1a20b89b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package codeedu/0.1/codeedu-0.1.tar.gz

---
+++ packages/codeedu/codeedu-0.1.tar.gz/codeedu-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='codeedu',
+      version='0.1',
+      description='a simple tool to pick a random github repo',
+      url='https://github.com/jxieeducation/codeEDU',
+      author='Jason Xie',
+      author_email='jxieeducation@berkeley.edu',
+      license='BST',
+      packages=['codeEDU'],
+      zip_safe=False)
