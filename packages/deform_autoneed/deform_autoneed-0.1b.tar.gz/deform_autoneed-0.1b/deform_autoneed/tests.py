affbe00ec0b94495423a94a7d0a062765019f014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:30 +0000

    Add package deform_autoneed/0.1b/deform_autoneed-0.1b.tar.gz

---
+++ packages/deform_autoneed/deform_autoneed-0.1b.tar.gz/deform_autoneed-0.1b/deform_autoneed/tests.py
@@ -0,0 +1,83 @@
+from unittest import TestCase
+
+import deform
+import colander
+from fanstatic import Library
+from fanstatic import get_needed
+from fanstatic import init_needed
+from pkg_resources import resource_filename
+
+
+def _mk_reg():
+    from deform_autoneed import ResourceRegistry
+    return ResourceRegistry()
+
+def _clearFLib(*args):
+    from deform_autoneed import deform_autoneed_lib
+    #To clear any old registered resources - needs to be done on test start too for some reason.
+    deform_autoneed_lib.known_resources = {}
+
+def _mk_richtext_form():
+    class Schema(colander.Schema):
+        richtext = colander.SchemaNode(colander.String(),
+                                       widget = deform.widget.RichTextWidget())
+    return deform.Form(Schema())
+
+
+class ResourceRegistryTests(TestCase):
+    setUp = tearDown = _clearFLib
+
+    @property
+    def _cut(self):
+        from deform_autoneed import ResourceRegistry
+        return ResourceRegistry
+
+    def test_create_requirement_for_deform_1_style(self):
+        reg = self._cut(add_basics = False)
+        reg.create_requirement_for('something', 'css/beautify.css', depends = [])
+        self.assertIn('something', reg.requirements)
+        self.assertIn('css/beautify.css', reg.paths)
+ 
+    def test_create_requirement_for_deform_2_style(self):
+        reg = self._cut()
+        reg.create_requirement_for('something', 'deform:static/css/beautify.css', depends = [])
+        self.assertIn('something', reg.requirements)
+        self.assertIn('deform:static/css/beautify.css', reg.paths)
+ 
+    def test_create_requirement_for_other_lib(self):
+        reg = self._cut()
+        testing_fixture_dir = resource_filename('deform_autoneed', 'testing_fixture')
+        reg.libraries['other'] = Library('other', testing_fixture_dir)
+        reg.create_requirement_for('something', 'other:testing_fixture/dummy.js',
+                                   remove_leading = 'testing_fixture/', depends = ())
+        self.assertIn('other', reg.libraries)
+
+    def test_populate_resource_registry(self):
+        reg = self._cut()
+        reg.populate_from_resources()
+        self.assertIn('deform', reg.libraries)
+        self.assertIn('jquery.form', reg.requirements)
+
+
+class AutoNeedTests(TestCase):
+    tearDown = _clearFLib
+
+    def setUp(self):
+        _clearFLib()
+        self.reg = _mk_reg()
+        self.reg.populate_from_resources()
+        init_needed()
+
+    def tearDown(self):
+        _clearFLib()
+
+    @property
+    def _fut(self):
+        from deform_autoneed import auto_need
+        return auto_need
+
+    def test_auto_need(self):
+        form = _mk_richtext_form()
+        self._fut(form, reg = self.reg)
+        resources = get_needed().resources()
+        self.assertIn('deform.js', [x.filename for x in resources])
---
+++ packages/deform_autoneed/deform_autoneed-0.1b.tar.gz/deform_autoneed-0.1b/setup.py
@@ -0,0 +1,43 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+
+requires = [
+    'fanstatic',
+    'deform',
+    'colander',
+    ]
+
+setup(name='deform_autoneed',
+      version='0.1b',
+      description='Auto include resources in deform via Fanstatic.',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        ],
+      author='Robin Harms Oredsson',
+      author_email='robin@betahaus.net',
+      url='https://github.com/robinharms/deform_autoneed',
+      keywords='web colander deform fanstatic',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires = requires,
+      tests_require = requires,
+      test_suite = "deform_autoneed",
+      entry_points = """\
+      [fanstatic.libraries]
+      deform_autoneed_lib = deform_autoneed:deform_autoneed_lib
+      """,
+      )
