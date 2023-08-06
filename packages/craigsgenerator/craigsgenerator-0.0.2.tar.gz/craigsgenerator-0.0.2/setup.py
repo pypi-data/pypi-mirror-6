134c677da34eb9a88a8f5786a866bd0775755305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:46 +0000

    Add package craigsgenerator/0.0.2/craigsgenerator-0.0.2.tar.gz

---
+++ packages/craigsgenerator/craigsgenerator-0.0.2.tar.gz/craigsgenerator-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(name='craigsgenerator',
+    author='Thomas Levine',
+    author_email='_@thomaslevine.com',
+    description='Iterate through Craig\'s list',
+    long_description=long_description,
+    url='https://github.com/tlevine/craigsgenerator.git',
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    packages=['craigsgenerator'],
+
+    install_requires = ['nose','lxml'],
+
+    version='0.0.2',
+    license='AGPL'
+)
