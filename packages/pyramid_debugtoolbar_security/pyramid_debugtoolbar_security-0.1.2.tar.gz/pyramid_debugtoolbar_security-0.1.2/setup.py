acd2f6d31e4c64b2668360daaecd90b5f4f4a2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:57 +0000

    Add package pyramid_debugtoolbar_security/0.1.2/pyramid_debugtoolbar_security-0.1.2.tar.gz

---
+++ packages/pyramid_debugtoolbar_security/pyramid_debugtoolbar_security-0.1.2.tar.gz/pyramid_debugtoolbar_security-0.1.2/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+
+description = (
+    "security panel for pyramid_debugtoolbar (principals, context acl, "
+    "view permission)"
+)
+
+setup(
+    name='pyramid_debugtoolbar_security',
+    version='0.1.2',
+    description=description,
+    long_description=README,
+    classifiers=[
+        "Development Status :: 3 - Alpha",  #need write test and documentation
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        #"Programming Language :: Python :: 2",  #not tested yet.
+        #"Programming Language :: Python :: 2.6",  #not tested yet.
+        "Programming Language :: Python :: 2.7",
+        #"Programming Language :: Python :: 3",  #not tested yet.
+        #"Programming Language :: Python :: 3.3",  #not tested yet.
+        #"Programming Language :: Python :: 3.4",  #not tested yet.
+    ],
+    author='shimizukawa',
+    author_email='shimizukawa@gmail.com',
+    url='https://bitbucket.org/shimizukawa/pyramid_debugtoolbar_security',
+    license='Apache Software License',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pyramid',
+        'pyramid_debugtoolbar',
+    ],
+    zip_safe=False,
+)
