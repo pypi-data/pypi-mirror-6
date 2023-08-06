6c7c96be6b11056582815e5984ed49264766b4a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:09 +0000

    Add package TorrentMediaSearcher/1.0.2/TorrentMediaSearcher-1.0.2.zip

---
+++ packages/TorrentMediaSearcher/TorrentMediaSearcher-1.0.2.zip/TorrentMediaSearcher-1.0.2/README.txt
@@ -0,0 +1,52 @@
+TorrentMediaSearcher
+====================
+
+Fetches magnet links from various torrent providers.
+
+Allows specifying a movie or show name, season, episode and returns (if available) magnet links to the content of the requested quality.
+Torrents with the most seeds are prioritized. Works with threads and a function can be specified to send the results to.
+
+Features
+=============
+
+- Very easy to use
+
+- Returns a magnet link from the specified provider and lists seeds (if provider shows seeds)
+
+- Provides exceptions
+
+Torrent providers
+=============
+
+- EZTV (tv)
+
+- TorrentProject (tv/movies)
+
+- YIFY (movies)
+
+- More can be requested
+
+Usage
+=============
+
+For usage examples, see:
+
+https://github.com/Christilut/TorrentMediaSearcher/blob/master/torrentmediasearcher/examples/examples.py
+
+Installation
+=============
+
+Windows users can run the .exe installer
+
+Everyone can run:
+
+python setup.py install
+
+Dependencies
+=============
+
+- requests
+
+- beautifulsoup4
+
+- simplejson
---
+++ packages/TorrentMediaSearcher/TorrentMediaSearcher-1.0.2.zip/TorrentMediaSearcher-1.0.2/torrentmediasearcher/providers/eztv_api.py
@@ -0,0 +1,85 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+from base_api import BaseAPI, QualityNotFound, ShowNotFound, EpisodeNotFound
+
+
+class EZTVAPI(BaseAPI):
+
+    _URL = "http://eztv.it"
+
+    def _query_tvshow(self, show, season, episode, quality):
+        show_id = self._get_show_id(show=show)
+
+        result = self._get_magnet_tv(show_id=show_id, show=show, season=season, episode=episode, quality=quality)
+
+        if len(result) == 0:   # No quality of any kind was found, most likely the episode does not exist.
+            raise EpisodeNotFound('Could not find episode ' + str(episode) + ' of season ' + str(season) + ' of ' + show)
+
+        return result
+
+    def _query_movie(self, *args):
+        raise RuntimeError('Movies are not supported in the EZTV provider')
+
+    def _get_show_id(self, show):
+        # all strings are in lowercase
+        show = show.lower()
+        terms = show.split(' ')
+
+        try:
+            req = requests.get(self._URL, timeout=5)
+        except requests.ConnectionError:
+            raise LookupError('Could not reach host')
+
+        soup = BeautifulSoup(req.content, 'html.parser')
+
+        tv_shows = str(
+            soup('select', {'name': 'SearchString'})).split('</option>')
+        for tv_show in tv_shows:
+            tv_show = tv_show.lower()
+            if all(x in tv_show for x in terms):
+                show_id = re.search(r"\d+", tv_show).group()
+                break
+        else:
+            raise ShowNotFound()
+
+        return show_id
+
+
+    def _get_magnet_tv(self, show_id, show, season, episode, quality):
+
+        show_url = self._URL + '/shows/' + show_id + '/'
+
+        try:
+            req = requests.get(show_url, timeout=5)
+        except requests.ConnectionError:
+            raise LookupError('Could not reach host')
+
+        soup = BeautifulSoup(req.content, 'html.parser')
+
+        episodes = soup.find_all(class_='epinfo')
+
+        wanted_episode = None
+
+        for e in episodes:
+            if re.search(show, e.text, re.IGNORECASE) is None: continue     # Skip if text does not contain show name
+            if re.search(quality, e.text, re.IGNORECASE) is None: continue  # Skip if text does not contain wanted quality
+
+            for s in self._TV_INDEX_SPECIFIERS:
+                regex_result = re.search(s, e.text, re.IGNORECASE)
+                if  regex_result is not None:
+                    if int(regex_result.group(1)) == season and int(regex_result.group(2)) == episode:
+                        wanted_episode = e
+                        break
+
+            if wanted_episode is not None:
+                break
+
+        if wanted_episode is None:
+            raise QualityNotFound('Could not find anything matching the quality: ' + quality)
+
+        return {'magnet': wanted_episode.parent.next_sibling.next_sibling.find(class_='magnet').get('href'), 'seeds': 'unknown'}    # EZTV does not shows seeds
+
+
+
