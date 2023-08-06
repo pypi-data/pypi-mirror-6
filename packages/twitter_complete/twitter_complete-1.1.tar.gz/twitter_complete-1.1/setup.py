30228d8604522b57b467ff38fbcbeafb7e001187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:30 +0000

    Add package twitter_complete/1.1/twitter_complete-1.1.tar.gz

---
+++ packages/twitter_complete/twitter_complete-1.1.tar.gz/twitter_complete-1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+	name = 'twitter_complete',
+	packages = ['twitter_complete'],
+	version = '1.1',
+	description='Python based interface to the 1.1, aimed for beginners who just want to fetch data from twitter ',
+	author='rohit khatana',
+	author_email='rohitkhatana.khatana@gmail.com',
+     	url ='https://github.com/takirohit/twitter_complete',
+	classifiers = [
+	"Programming Language :: Python",
+	"Development Status :: 1 - Planning",
+	"License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+	"Environment :: Other Environment",
+	]
+)
