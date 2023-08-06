a3deb31b01259b059eb9e4ac39c486557a0c2cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:39 +0000

    Add package mingdao/0.1.1/mingdao-0.1.1.zip

---
+++ packages/mingdao/mingdao-0.1.1.zip/mingdao-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import distutils.core
+
+version = '0.1.1'
+
+distutils.core.setup(
+	name='mingdao',
+	version=version,
+	packages=['mingdao'],
+	author='Sin',
+	author_email='sin.zou@mindao.com',
+	url='http://github.com/ipy/api_python',
+	description='Python SDK for mingdao.com, provides Mingdao OAuth2 authorization and API wrapper.',
+	install_requires=[
+		'requests',
+	],
+	classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Bottle",
+    ],
+)
