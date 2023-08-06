65cd4eff75c42e53c21cf6a50fac6f89e713e421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:37 +0000

    Add package infi.storagemodel/0.2.7/infi.storagemodel-0.2.7-py2.7.egg

---
+++ packages/infi.storagemodel/infi.storagemodel-0.2.7-py2.7.egg/infi/storagemodel/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.7"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'd8a997c8f64c82c9949d660f615e27caa947496d'
+__git_head_subject__ = 'Finished release v0.2.7.'
+__git_head_message__ = '* release/v0.2.7:\n  TRIVIAL bringing back StorageModel.predicates, used by the QA infrastructure\n  STORAGEMODEL-260 rescan_scsi_bus: increasing the retry count to 10\n  STORAGEMODEL-260 rescan_scsi_bus: retry on POWER ON OCCURRED'
+__git_dirty_diff__ = ''
