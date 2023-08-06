1bf57c9712744e2501a75ecb0b67b62d348fcdaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:02 +0000

    Add package recoder/0.2.0/recoder-0.2.0.tar.gz

---
+++ packages/recoder/recoder-0.2.0.tar.gz/recoder-0.2.0/recoder/cyrillic/recoder.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import os
+from ..base_recoder import BaseRecoder
+
+__all__ = ['Recoder']
+
+
+class Recoder(BaseRecoder):
+    base_dir = os.path.dirname(__file__)
+    file_3grams = os.path.join(base_dir, '3grams.json')
+    file_plus_words = os.path.join(base_dir, 'plus_words.json')
+
+    codings = [
+        'utf-8',
+        'koi8-r',
+        'koi8-u',
+        'cp1251',
+        'cp1252',
+        'iso8859-1',
+        'iso8859-2',
+        'iso8859-5',
+        'iso8859-9',
+        'cp866',
+        'maccyrillic',
+        'gb2312'
+    ]
