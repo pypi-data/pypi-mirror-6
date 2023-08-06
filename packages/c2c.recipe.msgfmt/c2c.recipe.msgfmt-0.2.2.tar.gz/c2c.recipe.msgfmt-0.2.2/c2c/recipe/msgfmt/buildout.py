d2fb2bdd63e96f2caa105f0c405e052205ff5f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:52 +0000

    Add package c2c.recipe.msgfmt/0.2.2/c2c.recipe.msgfmt-0.2.2.tar.gz

---
+++ packages/c2c.recipe.msgfmt/c2c.recipe.msgfmt-0.2.2.tar.gz/c2c.recipe.msgfmt-0.2.2/c2c/recipe/msgfmt/buildout.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+from babel.messages.mofile import write_mo
+from babel.messages.pofile import read_po
+
+class CompileMo(object):
+    def __init__(self, buildout, name, options):
+        self.name = name
+        basedir = buildout['buildout']['directory']
+        self.podir = os.path.join(basedir, options.get('po-directory'))
+        self.followlinks = options.get('followlinks', False)
+
+    def install(self):
+        generated = []
+        for dirpath, _, filenames in os.walk(self.podir,
+                                             followlinks=self.followlinks):
+            for filename in filenames:
+                domain, ext = os.path.splitext(filename)
+                if ext == '.po':
+                    po = open(os.path.join(dirpath, filename))
+                    mo = open(os.path.join(dirpath, domain + '.mo'), 'wb')
+                    logging.getLogger(self.name).info(
+                        "compiling catalog '%s' to '%s'"%(po.name, mo.name))
+                    write_mo(mo, read_po(po))
+                    generated.append(mo.name)
+                    mo.close()
+
+        return generated
+
+    update = install
---
+++ packages/c2c.recipe.msgfmt/c2c.recipe.msgfmt-0.2.2.tar.gz/c2c.recipe.msgfmt-0.2.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'c2c.recipe.msgfmt',
+    version = '0.2.2',
+    license = 'MIT License',
+
+    author  = 'Frederic Junod',
+    author_email = 'frederic.junod@camptocamp.com',
+    url = 'https://github.com/camptocamp/c2c.recipe.msgfmt',
+
+    description = 'A buildout recipe to compile message catalog to binary format.',
+    long_description = open('README').read(),
+
+    classifiers = [
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License'
+    ],
+
+    zip_safe = False, 
+    install_requires = ['zc.buildout', 'Babel'],
+    packages = find_packages(exclude=['ez_setup']),
+    namespace_packages = ['c2c', 'c2c.recipe'],
+    entry_points = {'zc.buildout' : ['default = c2c.recipe.msgfmt.buildout:CompileMo']}
+)
