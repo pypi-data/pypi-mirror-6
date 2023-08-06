ef314e590c32de8a76f2c3c8ed6256803cdec127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:02 +0000

    Add package embedly_cards/0.2.0/embedly_cards-0.2.0.tar.gz

---
+++ packages/embedly_cards/embedly_cards-0.2.0.tar.gz/embedly_cards-0.2.0/embedly_cards/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'embedly-cards'
+__version__ = '0.2.0'
+__author__ = 'Josh Izaac'
+__credits__ = ["Josh Izaac", ]
+__maintainer__ = "Josh Izaac"
+__email__ = "josh@iza.ac"
+__status__ = "Stable"
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014'
+
+from embedly_cards.card_rst import register
+from embedly_cards.card_markdown import EmbedlyCardExtension
