f976129606d9579b09716f28740724360427ca84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:37 +0000

    Add package proso-geography-model/0.9.7/proso-geography-model-0.9.7.tar.gz

---
+++ packages/proso-geography-model/proso-geography-model-0.9.7.tar.gz/proso-geography-model-0.9.7/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+VERSION = '0.9.7'
+
+
+setup(
+    name='proso-geography-model',
+    version=VERSION,
+    description='Models library for PROSO Geography project',
+    author='Jan Papousek',
+    author_email='jan.papousek@gmail.com',
+    packages=['proso.geography', 'proso'],
+    license='Gnu GPL v3',
+    url='https://github.com/proso/geography-model-libs/',
+)
