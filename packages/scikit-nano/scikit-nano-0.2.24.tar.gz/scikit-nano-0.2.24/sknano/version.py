5d152c0e5fce65a80387482521fd04d5b9087f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:59 +0000

    Add package scikit-nano/0.2.24/scikit-nano-0.2.24.tar.gz

---
+++ packages/scikit-nano/scikit-nano-0.2.24.tar.gz/scikit-nano-0.2.24/sknano/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIKIT-NANO SETUP.PY
+short_version = '0.2.24'
+version = '0.2.24'
+full_version = '0.2.24'
+git_revision = '21db3ce61aa3ba3f8dfd1dd9aa98340bb8772467'
+release = True
+
+if not release:
+    version = full_version
