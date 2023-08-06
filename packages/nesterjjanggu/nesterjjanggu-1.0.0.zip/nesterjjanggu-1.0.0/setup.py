e3c2790c15ac9856606240c5662420e42512963e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:05 +0000

    Add package nesterjjanggu/1.0.0/nesterjjanggu-1.0.0.zip

---
+++ packages/nesterjjanggu/nesterjjanggu-1.0.0.zip/nesterjjanggu-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterjjanggu' ,
+    version = '1.0.0' ,
+    py_modules= ['nester'] ,
+    author = 'jjanggupython' ,
+    author_email = 'jjanggu1990@naver.com' ,
+    url = 'asdf',
+    description = 'asdf',
+    )
+
