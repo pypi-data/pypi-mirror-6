289e42c208072c0506a930b37cc312f93413d130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:24 +0000

    Add package GoldSaxCreateTablesGFinance/0.03/GoldSaxCreateTablesGFinance-0.03.zip

---
+++ packages/GoldSaxCreateTablesGFinance/GoldSaxCreateTablesGFinance-0.03.zip/GoldSaxCreateTablesGFinance-0.03/GoldSaxCreateTablesGFinance/goldsaxcreatetablesgfinance.py
@@ -0,0 +1,41 @@
+from GoldSaxGFinanceQuote import goldsaxgfinancequote
+import sqlite3 as lite
+import string
+import time
+import re
+print ("Market Starts.......")
+
+def pullprocess(ass):
+        sds = goldsaxgfinancequote.getquote(ass)
+        return sds
+def createtables(url,dbase):
+        f = pullprocess(url)
+        print ("Database ", dbase, " Tablespace check Started.........")
+        con = lite.connect(dbase)
+        with open('conf/symbolname.conf') as fille:
+            actionlist = fille.read().splitlines()
+        fille.close()
+        for fuck in f:
+            name = fuck[0]
+            for act in actionlist:
+                actor = act.split("=")        
+                if name==actor[0]:
+                        name = actor[1]
+            name = re.sub('[^a-zA-Z]+', '', name)
+            
+            stmt = "CREATE TABLE IF NOT EXISTS "+name+"table(ONNN TEXT, ATTT TEXT, PRIC REAL)"
+            cur = con.cursor()
+            try:
+                    cur.execute(stmt)
+            except lite.OperationalError:
+                    print(fuck[0],stmt)
+                    time.sleep(0.5)
+                    print("Database ", dbase, " Tablespace check Failed.........")
+            
+        con.commit()
+        con.close()
+        time.sleep(2)
+        print ("Database ", dbase, " Tablespace check finished.........")
+
+    
+    
