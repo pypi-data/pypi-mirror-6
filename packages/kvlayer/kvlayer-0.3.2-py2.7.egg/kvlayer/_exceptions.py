801a5415f46a529cd1c8307bf1fc1729ad8ed1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:37 +0000

    Add package kvlayer/0.3.2/kvlayer-0.3.2-py2.7.egg

---
+++ packages/kvlayer/kvlayer-0.3.2-py2.7.egg/kvlayer/_exceptions.py
@@ -0,0 +1,45 @@
+
+
+class KVLayerError(Exception):
+    pass
+
+
+class ConfigurationError(KVLayerError):
+    pass
+
+
+class ProgrammerError(KVLayerError):
+    pass
+
+
+class StorageClosed(KVLayerError):
+    pass
+
+
+class MissingID(KVLayerError):
+    """A kvlayer function was passed a nonexistent key.
+
+    If the function takes multiple keys or a key range, nothing matched
+    at all.
+
+    """
+    pass
+
+
+class DatabaseEmpty(KVLayerError):
+    pass
+
+
+class BadKey(KVLayerError):
+    """A key value passed to a kvlayer function was not of the correct form.
+
+    Keys must be tuples of a fixed length of :class:`uuid.UUID` objects.
+    The length of the tuple is specified in the initial call to
+    :meth:`kvlayer._abstract_storage.AbstractStorage.setup_namespace`.
+
+    """
+    pass
+
+class SerializationError(KVLayerError):
+    """Converting between an item and a serialized form failed."""
+    pass
