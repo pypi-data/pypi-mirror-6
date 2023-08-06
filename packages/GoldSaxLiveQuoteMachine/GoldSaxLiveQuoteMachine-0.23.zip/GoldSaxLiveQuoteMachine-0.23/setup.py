97ae3a9cc0a70858f061496929f886692887d0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package GoldSaxLiveQuoteMachine/0.23/GoldSaxLiveQuoteMachine-0.23.zip

---
+++ packages/GoldSaxLiveQuoteMachine/GoldSaxLiveQuoteMachine-0.23.zip/GoldSaxLiveQuoteMachine-0.23/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+        name = 'GoldSaxLiveQuoteMachine',
+        packages = ['GoldSaxLiveQuoteMachine'], # this must be the same as the name above
+        version = '0.23',
+        description = 'Orchestration Engine, parsing of FIXML, FPML, XML, Json, FIX messages realtime data for storage, analytics to the GoldSaxEngine-****Markets or any other engine. This can be used by any Trader, Market Maker, a Retail Investor, Instituitional Investor, Hedge Fund Managers, and Asset Managers.',
+        author = 'Vance King Saxbe. A',
+        author_email = 'vsaxbe@yahoo.com',
+        url = 'https://github.com/VanceKingSaxbeA/GoldSaxLiveQuoteMachine.git',   # use the URL to the github repo
+        download_url = 'https://github.com/VanceKingSaxbeA/GoldSaxLiveQuoteMachine/archive/master.zip', # I'll explain this in a second
+        keywords = ['FIX','FIXML','FPML','XML','JSON','AutoFetch Quotes', 'Automated Trading', 'Algorithmic Trading', 'Financial Markets', 'Hedge Funds', 'Asset Managers', 'investors'], # arbitrary keywords
+        classifiers = ['Development Status :: 5 - Production/Stable','Intended Audience :: Financial and Insurance Industry','Intended Audience :: Information Technology','Intended Audience :: Science/Research','License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)','Programming Language :: Python :: 3.3'],
+    )
