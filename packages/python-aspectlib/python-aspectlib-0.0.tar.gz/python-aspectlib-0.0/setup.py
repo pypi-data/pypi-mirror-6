05a94e7d05ddbc5f62f84836923f66debcb6e8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:22 +0000

    Add package python-aspectlib/0.0/python-aspectlib-0.0.tar.gz

---
+++ packages/python-aspectlib/python-aspectlib-0.0.tar.gz/python-aspectlib-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+    name='python-aspectlib',
+    version="0.0",
+    url='https://pypi.python.org/pypi/aspectlib/',
+    description='Aspect-Oriented Programming toolkit.',
+    long_description='''Use `aspectlib <https://pypi.python.org/pypi/aspectlib/>`_ instead.''',
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    platforms=['all'],
+    zip_safe=False,
+    author_email='contact@ionelmc.ro',
+    install_requires=['aspectlib'],
+)
