5009e79fd9b5538343c999c4148c96340ad2c3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:39 +0000

    Add package infi.run_as_root/0.1.2/infi.run_as_root-0.1.2-py2.7.egg

---
+++ packages/infi.run_as_root/infi.run_as_root-0.1.2-py2.7.egg/infi/run_as_root/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '222c109b5968d66b1429156b182084963fdd3a7f'
+__git_head_subject__ = 'Finished release v0.1.2.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
