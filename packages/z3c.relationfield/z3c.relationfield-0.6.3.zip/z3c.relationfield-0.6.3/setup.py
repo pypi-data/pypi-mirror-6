47e17f8a8e72aeff1d7a93f771f222b14f4bdb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:09 +0000

    Add package z3c.relationfield/0.6.3/z3c.relationfield-0.6.3.zip

---
+++ packages/z3c.relationfield/z3c.relationfield-0.6.3.zip/z3c.relationfield-0.6.3/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(*rnames)).read()
+
+long_description = (
+    read('src', 'z3c', 'relationfield', 'README.txt')
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Download\n'
+    '********\n'
+)
+
+setup(
+    name='z3c.relationfield',
+    version='0.6.3',
+    description="A relation field framework for Zope 3.",
+    long_description=long_description,
+    classifiers=[],
+    keywords='',
+    author='Martijn Faassen',
+    author_email='faassen@startifact.com',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'ZODB3',
+        'zope.app.intid',
+        'zope.app.container',
+        'z3c.objpath',
+        'zc.relation >= 1.0',
+    ],
+    extras_require={
+        'test': [
+            'zope.app.zcmlfiles',
+            'zope.app.testing',
+            'zope.app.keyreference',
+            'zope.securitypolicy',
+        ],
+        'xml': ['z3c.schema2xml >= 1.0',
+                'lxml'],
+    },
+    entry_points={},
+)
