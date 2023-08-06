b983c928ab34ec542228da8fba1b2ca3aba88818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:16 +0000

    Add package MD-ELM/0.42/MD-ELM-0.42.tar.gz

---
+++ packages/MD-ELM/MD-ELM-0.42.tar.gz/MD-ELM-0.42/mdelm/mdelm.py
@@ -0,0 +1,62 @@
+sc1# -*- coding: utf-8 -*-
+"""MD-ELM multi-class implementation.
+
+Momo's version, counts errors less than the original one.
+Created on Thu Feb  6 13:43:20 2014
+
+@author: akusoka1
+"""
+
+import numpy as np
+from numpy.linalg import inv
+from random import sample
+
+def MDELM(H, Y, k, flips, MSE_orig):
+    """Find probable mislabeled samples in a dataset.
+    
+    Mislabelled samples get higher scores.
+    
+    H - hidden layer output of an ELM
+    Y - desired outputs of an ELM
+    k - number of samples to relabel at each iteration
+    flips - number of flips to try
+    MSE_orig - MSE error threshold
+    """
+    N = H.shape[0]    
+
+    # pre-calculate parameters for reduced PRESS
+    C = inv(np.dot(H.T, H))
+    P = H.dot(C)
+    D = np.ones((N,)) - np.einsum('ij,ji->i', P, H.T)
+    D = D.reshape((-1,1))    
+
+    # create expandable array to store all scores
+    c = Y.shape[1]
+    score = np.zeros((N,c))
+
+    # make the swaps
+    Yorig = Y.copy()
+    for _ in xrange(flips):
+        idx_sw = np.array(sample(xrange(N), k))  # random samples to relabel
+        Y = Yorig.copy()
+    
+        # relabeling
+        for i in idx_sw:
+            c_candidate = np.where(Y[i] == 0)[0]
+            print 'ccand', c_candidate
+            Y[i] = 0
+            Y[i, c_candidate[np.random.randint(c-1)]] = 1        
+    
+        # reduced MSE    
+        W = C.dot(H.T.dot(Y))        
+        e = (Y - H.dot(W)) / D
+        MSE = np.mean(e**2)
+    
+        # saving successful flip
+        if MSE < MSE_orig:
+            c_sw = np.argmax(Y[idx_sw], 1)
+            score[idx_sw, c_sw] += 1
+    
+    return score
+
+
---
+++ packages/MD-ELM/MD-ELM-0.42.tar.gz/MD-ELM-0.42/setup.py
@@ -0,0 +1,17 @@
+'''
+Created on Mar 21, 2014
+
+@author: akusoka1
+'''
+
+from setuptools import setup, find_packages
+
+readme = open("README.txt").read()
+
+setup(name='MD-ELM',
+      version='0.42',
+      author='Anton Akusok',
+      author_email='akusok.a@gmail.com',
+      license='MIT',
+      description='Mislabeled samples detection with OP-ELM',
+      packages=find_packages())
