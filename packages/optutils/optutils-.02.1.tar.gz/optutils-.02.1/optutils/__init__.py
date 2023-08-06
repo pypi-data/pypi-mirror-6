cb9aef41a25528b6d5e681bbe4301b5bae51d503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:22 +0000

    Add package optutils/.02.1/optutils-.02.1.tar.gz

---
+++ packages/optutils/optutils-.02.1.tar.gz/optutils-.02.1/optutils/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Tim Henderson
+#Email: tim.tadh@hackthology.com
+#For licensing see the LICENSE file in the top level directory.
+
+from util import main, custom_log_main
+from lib import log, output, error_codes, add_code
+
