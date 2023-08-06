8c458ca1ac97e7d4e55aca5d3f7154dfb6e5c0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:55 +0000

    Add package eventsocket/0.1.4/eventsocket-0.1.4.tar.gz

---
+++ packages/eventsocket/eventsocket-0.1.4.tar.gz/eventsocket-0.1.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="eventsocket",
+    version="0.1.4",
+    description="Twisted protocol for the FreeSWITCH's Event Socket",
+    author="Alexandre Fiori",
+    url="http://github.com/fiorix/eventsocket",
+    py_modules=["eventsocket"],
+    install_requires=["twisted"],
+)
