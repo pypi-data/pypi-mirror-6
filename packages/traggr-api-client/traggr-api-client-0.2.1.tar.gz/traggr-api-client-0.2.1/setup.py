74f1917a4e6dde2d996dea47b3f9425fa8fd1e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:02 +0000

    Add package traggr-api-client/0.2.1/traggr-api-client-0.2.1.tar.gz

---
+++ packages/traggr-api-client/traggr-api-client-0.2.1.tar.gz/traggr-api-client-0.2.1/setup.py
@@ -0,0 +1,19 @@
+# from distutils.core import setup
+from setuptools import setup
+
+__version__ = '0.2.1'
+
+
+setup(name='traggr-api-client',
+      py_modules=['traggrcl'],
+      version=__version__,
+      description='API client for Test Results Aggregation system',
+      author='Vitaliy Yakoviv',
+      author_email='witalick@gmail.com',
+      url='https://github.com/witalick/traggr-api-client',
+      keywords=['api', 'testing'],
+      classifiers=[],
+      install_requires=['requests'])
+
+
+# EOF
