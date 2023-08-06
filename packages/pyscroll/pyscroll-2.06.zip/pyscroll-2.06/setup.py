e35f87409d92b6a2330d1cbc70dea207f3cc8b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:23 +0000

    Add package pyscroll/2.06/pyscroll-2.06.zip

---
+++ packages/pyscroll/pyscroll-2.06.zip/pyscroll-2.06/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+import os
+from setuptools import setup
+import pyscroll
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name="pyscroll",
+      version=pyscroll.__version__,
+      description=pyscroll.__description__,
+      author=pyscroll.__author__,
+      author_email=pyscroll.__author_email__,
+      packages=['pyscroll'],
+      install_requires=['pygame'],
+      license="LGPLv3",
+      long_description=read('README.md'),
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Games/Entertainment",
+          "Topic :: Multimedia :: Graphics",
+          "Topic :: Software Development :: Libraries :: pygame",
+      ],
+)
