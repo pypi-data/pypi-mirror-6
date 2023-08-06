e5bbc235a82cd4db6d0edfdf392e87d2db7c2ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:15 +0000

    Add package quickfind/0.1.5.2/quickfind-0.1.5.2.tar.gz

---
+++ packages/quickfind/quickfind-0.1.5.2.tar.gz/quickfind-0.1.5.2/README.md
@@ -0,0 +1,89 @@
+quickfind
+=========
+
+Fuzzy finder for the terminal.  `quickfind` can search files in the file system, ctags files, or anything that can pipe.
+
+Install
+-------
+
+To install from Pypi:
+
+    pip install quickfind
+
+To install the faster version with ctags from Pypi:
+
+    pip install quickfind[ctags,fsnix]
+
+To install from source, clone the repo and type:
+
+    python setup.py install
+
+ in the directory.  
+
+'sudo' might be needed, depending on permissions.
+
+To Use
+------
+
+_quickfind_ has two builtin forms: File search and CTags search, with File search as default.  
+
+After installation, a new executable is added to the path 'qf'.  To use: 
+
+    qf # for file search 
+
+    qf -c # To search tags in CTags file
+
+and start typing!  _quickfind_ can be configured to match against file name and/or path
+while selecting either files, directories, or both.  By default, it filters out files listed
+in a tree's .gitignore.
+
+Up and Down arrow keys selects which file to open.  Enter opens the current file with $EDITOR.
+If -s FILENAME is specified, quickfind writes the selected file to disk.
+
+Stdin
+-----
+_quickfind_ can search on stdin when provided a naked '-':
+    
+    find . -type f | qf -
+
+_quickfind_ can further be extended to execute custom commands after an item as been selected
+    # To kill a process
+    ps aux | qf - -f "kill -9 {2}"
+
+_quickfind_ similarly can output to stdout with the '-o' flag rather than exec-ing the 
+formatted command:
+    
+    # To view man a file
+    man $(man -k . | qf - -f "man {1}" -o)
+
+Commands
+--------
+By default, _quickfind_ will execute "$EDITOR {0}", where "{0}" represents the entire 
+selected record.  In "{N}", N represents the Nth piece split by the delimiter specified
+by -D.  By default, the delimiter is whitespace.  
+
+Tricks
+-----
+Add to your .bashrc:
+
+    bind '"\C-f": "qf -w\n"'
+
+to add ctrl+f as a qf hotkey.
+
+Add to your .bashrc:
+
+    function qcd() {
+        _OFILE=$(qf -d -o -f "{0}")
+        if [ -n $_OFILE ]; then
+            cd $_OFILE
+        fi
+        unset _OFILE
+    }
+
+to enable a new bash command, 'qcd', for quickly cd-ing to a directory.
+
+Add to your .bashrc:
+
+alias qkill="ps aux | qf - -f 'kill -f {2}'
+
+for easy process killing.
---
+++ packages/quickfind/quickfind-0.1.5.2.tar.gz/quickfind-0.1.5.2/quickfind/source/Util.py
@@ -0,0 +1,97 @@
+import os
+
+from quickfind.Searcher import CString, Ranker
+
+def truncate_front(line, length=70):
+    reduce_amt = len(line) - length
+    # If it already fits
+    if reduce_amt <= 0 or length <= 0:
+        return line 
+
+    reduce_amt += 3 # for the ellipsis
+    return '...'+line[reduce_amt:]
+
+def truncate_middle(line, length=70):
+    reduce_amt = len(line) - length
+
+    # If it already fits
+    if reduce_amt <= 0 or length <= 0:
+        return line 
+
+    reduce_amt += 3 # for the ellipsis
+
+    start = (len(line) / 2) - (reduce_amt / 2)
+    end = start + reduce_amt
+    return "%s...%s" % (line[:start], line[end:])
+
+def rec_dir_up(dir):
+    if os.path.isdir(dir):
+        while True:
+            yield dir 
+            newdir = os.path.split(dir)[0]
+            if newdir == dir: break
+            dir = newdir
+
+def highlight(v, query, color="green"):
+    i = v.lower().rfind(query.lower())
+    if query and i > -1:
+        left = v[:i]
+        lq = len(query)
+        highlight = v[i:i+lq]
+        right = v[i+lq:]
+        return [ left, CString(highlight, color), right ]
+
+    return [v]
+
+class StringRanker(Ranker):
+
+    ws_query = False
+    weight_f = lambda x: 0
+
+    def __init__(self, query):
+        self.qs = query.lower()
+        if self.ws_query:
+            self.qs = self.qs.split()
+        else:
+            self.qs = [self.qs]
+
+    def get_parts(self, item):
+        raise NotImplementedError()
+
+    def rank_part(self, q, part):
+        if q not in part:
+            return None
+
+        # Don't do more interesting ranking with one character
+        lq = len(q)
+        lp = len(part)
+        if lq == 1:
+            return lp
+
+        score = (lp) ** 0.5
+        score -= 1.0 if part.startswith(q) else 0.0
+        score -= 1.0 if part.endswith(q) else 0.0
+        return score
+
+    def rank(self, item):
+        part = self.get_part(item).lower()
+
+        agg_score = 0.0
+        for q in self.qs:
+            score = self.rank_part(q, part)
+            if score is None:
+                return None
+            agg_score += score
+        
+        return agg_score + self.weight_f(item)
+    
+    @staticmethod
+    def new(ws_query, weight_f=lambda *x: 0, **kwargs):
+        kwargs['ws_query'] = ws_query
+        kwargs['weight_f'] = weight_f
+        return type('StringRanker', (StringRanker,), kwargs)
+
+def simpleFormatter(item, query, dims):
+    v = truncate_middle(item, dims[0])
+    return highlight(v, query)
+
