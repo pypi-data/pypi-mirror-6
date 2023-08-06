f2eb2dea981013944e2aa81a482da8244b3973af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:00 +0000

    Add package pypi_agreement/1.0.0/pypi_agreement-1.0.0.tar.gz

---
+++ packages/pypi_agreement/pypi_agreement-1.0.0.tar.gz/pypi_agreement-1.0.0/pypi_agreement.py
@@ -0,0 +1,18 @@
+"""
+By registering to upload content to PyPI, you must agree and affirmatively acknowledge the following:
+
+1. Content is restricted to Python packages and related information only.
+
+2. Any content uploaded to PyPI is provided on a non-confidential basis.
+
+3. The PSF is free to use or disseminate any content that I upload on an unrestricted basis for any 
+purpose. In particular, the PSF and all other users of the web site are granted an irrevocable, 
+worldwide, royalty-free, nonexclusive license to reproduce, distribute, transmit, display, perform, 
+and publish the content, including in digital form.
+
+4. I represent and warrant that I have complied with all government regulations concerning the 
+transfer or export of any content I upload to PyPI. In particular, if I am subject to United States 
+law, I represent and warrant that I have obtained the proper governmental authorization for the export 
+of the content I upload. I further affirm that any content I provide is not intended for use by a 
+government end-user as defined in part 772 of the United States Export Administration Regulations.
+"""
---
+++ packages/pypi_agreement/pypi_agreement-1.0.0.tar.gz/pypi_agreement-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        = 'pypi_agreement',
+    version     = '1.0.0',
+    py_modules  = ['pypi_agreement'],
+    author      = 'Jeffery1st',
+    author_email= 'li.jeffery@gmail.com',
+    url         = 'http://www.jeffery1st.com',
+    description = 'The agreement of pypi register',
+    )
