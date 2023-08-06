9df80033e2758daa63796622786090bf26df5056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:38 +0000

    Add package isbntools/0.8.3/isbntools-0.8.3.tar.gz

---
+++ packages/isbntools/isbntools-0.8.3.tar.gz/isbntools-0.8.3/isbntools/metadata.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import wcat
+import googlebooks
+
+
+def query(isbn, service='wcat'):
+    """
+    Queries worldcat.org or Google Books (JSON API) for metadata
+    """
+    if service == 'wcat':
+        canonical = wcat.query(isbn)
+    elif service == 'goob' or service == 'googlebooks':
+        canonical = googlebooks.query(isbn)
+    else:
+        print(('Error:%s is not a recognized service!' % service))
+        return
+    if canonical:
+        return canonical
