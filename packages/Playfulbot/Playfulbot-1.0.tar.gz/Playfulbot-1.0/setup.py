742f1cdc311c6406a7178fd2ce5de82a1b496a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:58 +0000

    Add package Playfulbot/1.0/Playfulbot-1.0.tar.gz

---
+++ packages/Playfulbot/Playfulbot-1.0.tar.gz/Playfulbot-1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# 2014 Alex Silva <alexsilvaf28 at gmail.com>
+
+"""
+Usage:
+    python setup.py install
+"""
+
+# from ez_setup import use_setuptools
+
+# use_setuptools()
+
+from setuptools import setup
+
+setup(
+	name="Playfulbot",
+	description="Playfulbet auto-bet",
+	version="1.0",
+	author="Alex Silva",
+	url="https://github.com/Alexsays/Playfulbot",
+	license="GNU General Public License (GPLv2)",
+	scripts=['playfulbot.py'],
+	install_requires=[
+		"mechanize",
+		"beautifulsoup4"
+	]
+)
