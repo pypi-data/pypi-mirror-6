3843b0d938784b19ed18fd17c27f91d77bc9c986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:06 +0000

    Add package Naked/0.1.26/Naked-0.1.26.tar.gz

---
+++ packages/Naked/Naked-0.1.26.tar.gz/Naked-0.1.26/lib/Naked/scratchpad.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+from functools import wraps
+from Naked.toolshed.system import stderr, stderr_xnl, stdout, stdout_xnl, stdout_iter, stdout_iter_xnl
+
+def print_scratch(func):
+    @wraps(func)
+    def print_wrapper(*args, **kwargs):
+        print(func(*args, **kwargs))
+    return print_wrapper
+
+
+def run_scratchpad():
+    # from Naked.toolshed.file import FileReader
+    # from Naked.toolshed.types import XString, XUnicode
+
+    # r = FileReader('/Users/ces/Desktop/code/naked/tests/testfiles/unicode.txt')
+    # test1 = r.read_utf8()
+    # test2 = XUnicode(test1, {'a': 'b'})
+    # test3 = "Hey! It's Bengali ব য,and here is some more ২"
+    # print(unicode(test2))
+
+    stdout_iter_xnl(['test', 'this', 'string'])
+
+
+
+
+if __name__ == '__main__':
+    run_scratchpad()
