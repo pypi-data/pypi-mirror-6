cfe9b75de8a84ff20b400090f5235318b319fe2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:08 +0000

    Add package jenks/0.2/jenks-0.2.tar.gz

---
+++ packages/jenks/jenks-0.2.tar.gz/jenks-0.2/README.rst
@@ -0,0 +1,37 @@
+jenks
+=====
+
+A Jenkins command-line tool.
+
+* since jenkinsapi (one of the dependencies) is only python2 compatible, jenks is currently only python2 compatible.
+
+-------
+Install
+-------
+
+There's a few ways to install jenks.
+
+Through pip:
+
+    pip install jenks
+    pip install http://github.com/toumorokoshi/jenks/tarball/master
+
+Through `sprinter <http://sprinter.readthedocs.org/en/latest/>`_:
+
+    sprinter install https://raw.github.com/toumorokoshi/jenks/master/sprinter.cfg
+
+-----
+Using
+-----
+
+jenks looks for a .jenksrc file, recursively going up from the current working directory you're in.
+
+Most users are probably ok with just adding a .jenksrc to their user root.
+
+Example .jenksrc::
+
+    localhost: 
+      url: 'http://localhost:8080/'
+      jobs:
+        - foo
+        - bar
---
+++ packages/jenks/jenks-0.2.tar.gz/jenks-0.2/jenks/command.py
@@ -0,0 +1,67 @@
+"""
+A file to store jenks command classes
+"""
+import requests
+
+
+class Status(object):
+    """ Return the status of jobs """
+
+    argument = None
+    TEMPLATE = "{key}: {host}, {name} (last build #{number}) {status}"
+
+    @staticmethod
+    def act(job):
+        last_build = job.api_instance.get_last_build()
+
+        if job.api_instance.is_queued():
+            status = "queued"
+        else:
+            status = last_build.get_status() or "running..."
+
+        return Status.TEMPLATE.format(
+            key=job.key,
+            host=job.host,
+            name=job.name,
+            number=last_build.get_number(),
+            status=status
+        )
+
+
+class Console(object):
+    """ return the console output of jobs """
+
+    argument = "--console"
+
+    @staticmethod
+    def act(job):
+        return job.api_instance.get_last_build().get_console()
+
+
+class List(object):
+    """ list the information about the job without retrieving information online """
+
+    argument = "--list"
+    TEMPLATE = "{key} {host} {name}"
+
+    @staticmethod
+    def act(job):
+        return List.TEMPLATE.format(
+            key=job.key,
+            host=job.host,
+            name=job.name
+        )
+
+
+class Trigger(object):
+    """" trigger jobs """
+
+    argument = "--trigger"
+
+    @staticmethod
+    def act(job):
+        if job.api_instance.is_running():
+            return "{0}, {1} is already running".format(job.host, job.name)
+        else:
+            requests.get(job.api_instance.get_build_triggerurl())
+            return "triggering {0}, {1}...".format(job.host, job.name)
