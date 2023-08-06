075c2464d948d0515ffdd978e829180dcaa277f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:42 +0000

    Add package pyConditions/0.3.0/pyConditions-0.3.0.tar.gz

---
+++ packages/pyConditions/pyConditions-0.3.0.tar.gz/pyConditions-0.3.0/pyconditions/invariant.py
@@ -0,0 +1,64 @@
+from functools import wraps
+
+from .exceptions import PyCondition
+
+def wrap_method( func ):
+  @wraps( func )
+  def wrapper( *args, **kwargs ):
+    ret = func( *args, **kwargs )
+
+    for invariant in args[0].__invariant__:
+      invariant.assertInvariant( args[0] )
+
+    return ret
+
+  wrapper.is_wrapped = True
+
+  return wrapper
+
+class InvariantMeta( type, object ):
+  def __new__(cls, name, bases, attrs):
+    for a in attrs:
+      if( a != "__metaclass__" and hasattr( attrs[a], "__call__" ) ):
+        attrs[a] = wrap_method( attrs[a] )
+
+    return super( InvariantMeta, cls).__new__( cls, name, bases, attrs)
+
+class Invariant( object ):
+  def __init__( self ):
+    self.condition = lambda a: True
+
+  def __call__( self, klass ):
+    self.name = "%s.%s" % ( klass.__module__, klass.__name__ )
+    dct = dict( klass.__dict__ )
+
+    if( not hasattr( klass, "__invariant__" ) ):
+      dct["__invariant__"] = []
+    dct["__invariant__"].append( self )
+
+    t = InvariantMeta( klass.__name__, klass.__bases__, dct )
+
+    return t
+
+class NoopInvariant( Invariant ):
+  def __init__( self ):
+    super( NoopInvariant, self ).__init__()
+
+    self.condition = lambda a: True
+
+  def assertInvariant( self, s ):
+    return True
+
+class FieldsNotNone( Invariant ):
+  def __init__( self, fields ):
+    super( FieldsNotNone, self ).__init__()
+
+    self.fields = fields
+ 
+  def assertInvariant( self, s ):
+    for f in self.fields:
+      if( getattr( s, f ) == None ):
+        raise PyCondition( "Field \"%s\" was None when it should not have been in invariant \"%s\"" % ( f, self.name ) )
+
+    return True
+
---
+++ packages/pyConditions/pyConditions-0.3.0.tar.gz/pyConditions-0.3.0/pyconditions/test/test_Invariant.py
@@ -0,0 +1,52 @@
+import unittest
+
+from ..exceptions import PyCondition
+from ..invariant import Invariant, NoopInvariant, FieldsNotNone, InvariantMeta
+
+class TestInvariant( unittest.TestCase ):
+
+  def test_Invariant_properly_sets_metaclass_and_invariant_list( self ):
+    @NoopInvariant()
+    @NoopInvariant()
+    class Test( object ):
+      pass
+
+    self.assertTrue( len( Test.__invariant__ ) == 2 )
+
+  def test_Invariant_properly_wraps_each_method( self ):
+    @NoopInvariant()
+    class Test( object ):
+      def test( self ):
+        pass
+
+      def test2( self ):
+        pass
+
+      def testing( self, a, b ):
+        pass
+
+    t = Test()
+  
+    self.assertTrue( t.test.is_wrapped )
+    self.assertTrue( t.test2.is_wrapped )
+    self.assertTrue( t.testing.is_wrapped )
+
+  def test_FieldsNotNone( self ):
+    @FieldsNotNone( [ "test", "test2" ] )
+    class Test( object ):
+      def __init__( self ):
+        self.test = 1
+        self.test2 = 2
+
+      def test3( self ):
+        pass
+
+    t = Test()
+    t.test = None
+
+    self.assertRaises( PyCondition, t.test3 )
+
+    t.test = 1
+    t.test2 = None
+
+    self.assertRaises( PyCondition, t.test3 )
