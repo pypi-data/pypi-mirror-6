20b71a011504c945ac7f8d5706949e5fec56be9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:08 +0000

    Add package infi.nose_html_output/0.1.15/infi.nose_html_output-0.1.15.tar.gz

---
+++ packages/infi.nose_html_output/infi.nose_html_output-0.1.15.tar.gz/infi.nose_html_output-0.1.15/src/infi/nose_html_output/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.15"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.15)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'c2538d066091595d68c8ff0262bde09115378852'
+__git_head_subject__ = 'flake8 fixes'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
