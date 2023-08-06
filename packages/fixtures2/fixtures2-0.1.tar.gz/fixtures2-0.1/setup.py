4e87cdc1b39d6cece93b38000985325ed3ba6bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:04 +0000

    Add package fixtures2/0.1/fixtures2-0.1.tar.gz

---
+++ packages/fixtures2/fixtures2-0.1.tar.gz/fixtures2-0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='fixtures2',
+    version='0.1',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='Fixtures2 is an extension of the fixtures test framework.',
+    extras_require={
+        'mox': ['mox'],
+    },
+    install_requires=[
+        'fixtures',
+        'mox',
+    ],
+    packages=[
+        'fixtures2',
+    ],
+    url='https://github.com/CooledCoffee/fixtures2/',
+)
