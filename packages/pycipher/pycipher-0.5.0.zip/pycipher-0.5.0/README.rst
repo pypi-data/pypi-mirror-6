01ac6edb492f8452e03e77ce4805c1c65f8fb509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:24 +0000

    Add package pycipher/0.5.0/pycipher-0.5.0.zip

---
+++ packages/pycipher/pycipher-0.5.0.zip/pycipher-0.5.0/README.rst
@@ -0,0 +1,38 @@
+pycipher
+========
+
+Common classical ciphers implemented in Python.
+Documentation is available at http://pycipher.readthedocs.org/en/latest/ .
+
+Install
+-------
+
+::
+
+    pip install git+git://github.com/jameslyons/pycipher
+
+
+Example usage
+-------------
+
+::
+
+    >>> from pycipher import ADFGVX
+    >>> adfgvx = ADFGVX(key='PH0QG64MEA1YL2NOFDXKR3CVS5ZW7BJ9UTI8', keyword='GERMAN')
+    >>> adfgvx.encipher("Hello world!")
+    'FVFDAGXAFFFFGFAGADFG'
+    >>> adfgvx.decipher(_)
+    'HELLOWORLD'
+
+
+Feedback
+--------
+
+The code is hosted on GitHub: https://github.com/jameslyons/pycipher
+
+If you find any bugs make an issue or create a pull request.
+
+To run the test suite::
+
+    python setup.py test
+
---
+++ packages/pycipher/pycipher-0.5.0.zip/pycipher-0.5.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pycipher',
+    version='0.5.0',
+    description='Several simple cipher algorithms',
+    author='James Lyons',
+    author_email='james.lyons0@gmail.com',
+
+    packages=find_packages(exclude=['tests','tests.*']),
+    include_package_data=True,  # declarations in MANIFEST.in
+    url='https://github.com/jameslyons/pycipher',
+    test_suite='tests',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    long_description=open('README.rst', 'rt').read()
+)
