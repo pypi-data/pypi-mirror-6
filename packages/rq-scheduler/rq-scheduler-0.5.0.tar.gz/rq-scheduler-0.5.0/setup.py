b422c39e5910c587aec6edc4ddb36d891d9a1d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:25 +0000

    Add package rq-scheduler/0.5.0/rq-scheduler-0.5.0.tar.gz

---
+++ packages/rq-scheduler/rq-scheduler-0.5.0.tar.gz/rq-scheduler-0.5.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='rq-scheduler',
+    version='0.5.0',
+    author='Selwin Ong',
+    author_email='selwin.ong@gmail.com',
+    packages=['rq_scheduler'],
+    url='https://github.com/ui/rq-scheduler',
+    license='MIT',
+    description='Provides job scheduling capabilities to RQ (Redis Queue)',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    include_package_data=True,
+    entry_points='''\
+    [console_scripts]
+    rqscheduler = rq_scheduler.scripts.rqscheduler:main
+    ''',
+    package_data = { '': ['README.rst'] },
+    install_requires=['rq>=0.3.5'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
