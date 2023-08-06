bb37519409a4fd9ddd1779c728e1a78f89cedef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:06 +0000

    Add package akatsuki/0.1/akatsuki-0.1.tar.gz

---
+++ packages/akatsuki/akatsuki-0.1.tar.gz/akatsuki-0.1/akatsuki/bib2html.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from akatsuki.exporter import export_html
+from akatsuki.parser import load_bibtex_file
+from akatsuki.utils import sort_by_date
+
+
+def main(bibtex_file, html_file):
+    """Load BibTeX file and export to HTML file"""
+    entries = load_bibtex_file(bibtex_file)
+    entries = sort_by_date(entries, reverse=True)
+    export_html(html_file, entries)
