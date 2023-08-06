a372bd66ec917f7722eba47421b2e78c8e78639f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:57 +0000

    Add package NetflixRouletteAPI/2.0/NetflixRouletteAPI-2.0.zip

---
+++ packages/NetflixRouletteAPI/NetflixRouletteAPI-2.0.zip/NetflixRouletteAPI-2.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='NetflixRouletteAPI',
+    version='2.0',
+    author='Andrew Sampson',
+    author_email='andrew@codeusa523.org',
+    license='GNU GENERAL PUBLIC LICENSE V3.0',
+    url='http://netflixroulette.net/api/',
+    keywords='netflix roulette webapi wrapper',
+    description='The official python wrapper for the Netflix Roulette Web API',
+    download_url="http://www.netflixroulette.net/api/downloads/NetflixRouletteAPI-1.0.zip",
+    py_modules=["NetflixRoulette"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet'
+    ]
+)
