4559bad2d24d616bfbaff2b7b79f26731f354bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:17 +0000

    Add package dirwatch/1.0/dirwatch-1.0.tar.gz

---
+++ packages/dirwatch/dirwatch-1.0.tar.gz/dirwatch-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "dirwatch",
+    version = "1.0",
+    py_modules = ['dirwatch'],
+    author = "Sergey Kirillov",
+    author_email = "sergey.kirillov@gmail.com",
+    description = "Simple cross-platform file changes watcher",
+    url = "http://github.com/pistolero/dirwatch",
+)
