36656386dc10547f59dc165f1b8ffde489c89517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:27 +0000

    Add package flickrstock/0.2.2/flickrstock-0.2.2.tar.gz

---
+++ packages/flickrstock/flickrstock-0.2.2.tar.gz/flickrstock-0.2.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+setup(
+    name='flickrstock',
+    version='0.2.2',
+    description='Download stock photos from flickr',
+    long_description=long_description,
+    author='Omar Khan',
+    author_email='omar@omarkhan.me',
+    url='https://github.com/omarkhan/flickrstock',
+    py_modules=['flickrstock'],
+    scripts=['flickrstock'],
+    requires=['flickrapi'],
+)
