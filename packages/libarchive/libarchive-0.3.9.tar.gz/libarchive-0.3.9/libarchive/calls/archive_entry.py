d67fc4e8c2ffbada6c6364a2384c341cc89eaecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:08 +0000

    Add package libarchive/0.3.9/libarchive-0.3.9.tar.gz

---
+++ packages/libarchive/libarchive-0.3.9.tar.gz/libarchive-0.3.9/libarchive/calls/archive_entry.py
@@ -0,0 +1,28 @@
+from ctypes import *
+
+from libarchive.library import libarchive
+from libarchive.types.archive import *
+from libarchive.constants.archive import *
+
+def _check_zero_success(value):
+    if value != ARCHIVE_OK:
+        raise ValueError("Function returned failure: (%d)" % (value))
+    
+    return value
+
+c_archive_entry_pathname = libarchive.archive_entry_pathname
+c_archive_entry_pathname.argtypes = [c_void_p]
+c_archive_entry_pathname.restype = c_char_p
+
+c_archive_entry_new = libarchive.archive_entry_new
+c_archive_entry_new.argtypes = []
+c_archive_entry_new.restype = c_void_p
+
+c_archive_entry_sourcepath = libarchive.archive_entry_sourcepath
+c_archive_entry_sourcepath.argtypes = [c_void_p]
+c_archive_entry_sourcepath.restype = c_char_p
+
+c_archive_entry_free = libarchive.archive_entry_free
+c_archive_entry_free.argtypes = [c_void_p]
+c_archive_entry_free.restype = None
+
---
+++ packages/libarchive/libarchive-0.3.9.tar.gz/libarchive-0.3.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+def _pre_install():
+    print("Verifying that the library is accessible.")
+
+    try:
+        import libarchive.library
+    except OSError as e:
+        print("Library can not be loaded: %s" % (str(e)))
+        raise
+
+
+class _custom_install(install):
+    def run(self):
+        _pre_install()
+        install.run(self)
+
+description = "Python adapter for universal, libarchive-based archive access."
+
+setup(name='libarchive',
+      version='0.3.9',
+      description=description,
+      long_description="""""",
+      classifiers=[],
+      keywords='archive libarchive 7z tar bz2 zip gz',
+      author='Dustin Oprea',
+      author_email='myselfasunder@gmail.com',
+      url='https://github.com/dsoprea/PyEasyArchive',
+      license='GPL 2',
+      packages=find_packages(exclude=['dev']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      cmdclass={ 'install': _custom_install },
+)
