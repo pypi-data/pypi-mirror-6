4be27fd2961ac4dfd4c69afe1616c40c0fe1ab57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:54 +0000

    Add package Fibonacci_printer/1.1.0/Fibonacci_printer-1.1.0.zip

---
+++ packages/Fibonacci_printer/Fibonacci_printer-1.1.0.zip/Fibonacci_printer-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+    name= 'Fibonacci_printer',
+    version = '1.1.0',
+    py_modules = ['fiboseq'],
+    author = 'Sounak Mondal',
+    author_email = 'sounak.mdl@gmail.com',
+    url = 'www.google.com',
+    description = 'Prints fibonacci sequences upto a certain number of terms.',
+
+    )
