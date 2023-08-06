e9bd5fd1ae83c2577c09d6988227e973c62939fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:14 +0000

    Add package flanker/0.4.11/flanker-0.4.11.tar.gz

---
+++ packages/flanker/flanker-0.4.11.tar.gz/flanker-0.4.11/setup.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+
+import sys
+from setuptools import setup, find_packages
+
+
+setup(name='flanker',
+      version='0.4.11',
+      description='Mailgun Parsing Tools',
+      long_description=open('README.rst').read(),
+      classifiers=[],
+      keywords='',
+      author='Mailgun Inc.',
+      author_email='admin@mailgunhq.com',
+      url='http://mailgun.net',
+      license='Apache 2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'chardet>=1.0.1',
+          'dnsq>=1.1',
+          'expiringdict>=1.1',
+          'mock>=1.0.1',
+          'nose>=1.2.1',
+          'Paste>=1.7.5',
+          'redis>=2.7.1',
+          # IMPORTANT! Newer regex versions can be a lot slower for mime
+          # parsing (100x slower), keep that in mind when using newer versions
+          'regex>=0.1.20110315',
+      ],
+      )
