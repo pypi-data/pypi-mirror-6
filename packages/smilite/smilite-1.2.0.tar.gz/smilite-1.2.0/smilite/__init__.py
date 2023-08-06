c78a044ed72659fdb480ec5f9e1bc3674c3ba5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:02 +0000

    Add package smilite/1.2.0/smilite-1.2.0.tar.gz

---
+++ packages/smilite/smilite-1.2.0.tar.gz/smilite-1.2.0/smilite/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2014 Sebastian Raschka
+#
+# A small module to retrieve SMILE strings 
+# (Simplified molecular-input line-entry system) from the ZINC online
+# database (http://zinc.docking.org)
+
+from .smilite import get_zinc_smile
+from .smilite import generate_zincid_smile_csv
+from .smilite import simplify_smile
+from .smilite import check_duplicate_smiles
+
+__version__ = '1.2.0'
+
+
