e5ff5a491dbe5bc7ddeb4dda440471643fb89b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:40 +0000

    Add package shjson/0.2/shjson-0.2.tar.gz

---
+++ packages/shjson/shjson-0.2.tar.gz/shjson-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from distutils.core import Extension
+
+mod = Extension(
+    'shjson',
+    sources=['shjson.c'],
+    libraries=['yajl'],
+    extra_compile_args=["-O3"],
+    depends=["deque.c"]
+    )
+
+setup(
+    name="shjson",
+    version='0.2',
+    description='Stream based JSON parsing with a Python C-Extension around YAJL',
+    keywords='json',
+    author='Stephan Hofmockel',
+    author_email="Use the github issues",
+    url="https://github.com/stephan-hof/shjson",
+    packages=['tests'],
+    ext_modules = [mod]
+)
+
