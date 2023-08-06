d3bc83874f618ae667362a7eaba6b0995e8d40a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:45 +0000

    Add package aiozmq/0.1.2/aiozmq-0.1.2.tar.gz

---
+++ packages/aiozmq/aiozmq-0.1.2.tar.gz/aiozmq-0.1.2/aiozmq/__init__.py
@@ -0,0 +1,46 @@
+from collections import namedtuple
+import re
+import sys
+
+from .selector import ZmqSelector
+from .core import ZmqEventLoop, ZmqEventLoopPolicy
+from .interface import ZmqTransport, ZmqProtocol
+
+
+__all__ = ('ZmqSelector', 'ZmqEventLoop', 'ZmqEventLoopPolicy',
+           'ZmqTransport', 'ZmqProtocol',
+           'version_info', 'version')
+
+__version__ = '0.1.2'
+
+version = __version__ + ' , Python ' + sys.version
+
+
+VersionInfo = namedtuple('VersionInfo',
+                         'major minor micro releaselevel serial')
+
+
+def _parse_version(ver):
+    RE = (r'^(?P<major>\d+)\.(?P<minor>\d+)\.'
+          '(?P<micro>\d+)((?P<releaselevel>[a-z]+)(?P<serial>\d+)?)?$')
+    match = re.match(RE, ver)
+    try:
+        major = int(match.group('major'))
+        minor = int(match.group('minor'))
+        micro = int(match.group('micro'))
+        levels = {'rc': 'candidate',
+                  'a': 'alpha',
+                  'b': 'beta',
+                  None: 'final'}
+        releaselevel = levels[match.group('releaselevel')]
+        serial = int(match.group('serial')) if match.group('serial') else 0
+        return VersionInfo(major, minor, micro, releaselevel, serial)
+    except Exception:
+        raise ImportError("Invalid package version {}".format(ver))
+
+
+version_info = _parse_version(__version__)
+
+
+# make pyflakes happy
+(ZmqSelector, ZmqEventLoop, ZmqEventLoopPolicy, ZmqTransport, ZmqProtocol)
