a8f9f811cb5d5bd995f3161b04a2b0e6953707c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:49 +0000

    Add package metaweb/1.0.1/metaweb-1.0.1.tar.gz

---
+++ packages/metaweb/metaweb-1.0.1.tar.gz/metaweb-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='metaweb',
+    version='1.0.1',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='A web framework based on other web frameworks.',
+    extras_require={
+        'test': ['fixtures'],
+    },
+    install_requires=[
+        'decorated',
+        'loggingd',
+    ],
+    packages=[
+        'metaweb',
+        'metaweb.impls'
+    ],
+    url='https://github.com/CooledCoffee/metaweb/',
+)
