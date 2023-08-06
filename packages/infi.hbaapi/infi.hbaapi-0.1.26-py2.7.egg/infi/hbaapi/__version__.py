ae131dbe34e3905319971ddcd83678d475b1ac5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:26 +0000

    Add package infi.hbaapi/0.1.26/infi.hbaapi-0.1.26-py2.7.egg

---
+++ packages/infi.hbaapi/infi.hbaapi-0.1.26-py2.7.egg/infi/hbaapi/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.26"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.26)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '57d975fab3803180d63d83a780accf34cb67cd94'
+__git_head_subject__ = 'Finished release v0.1.26.'
+__git_head_message__ = '* release/v0.1.26:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements\n  update dependencies'
+__git_dirty_diff__ = ''
