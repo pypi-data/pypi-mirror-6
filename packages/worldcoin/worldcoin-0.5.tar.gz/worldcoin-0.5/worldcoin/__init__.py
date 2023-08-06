4fc22e399069a0f6d3e7b2b117afba422a86c978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:37 +0000

    Add package worldcoin/0.5/worldcoin-0.5.tar.gz

---
+++ packages/worldcoin/worldcoin-0.5.tar.gz/worldcoin-0.5/worldcoin/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#                        __    __           _     
+#   _      ______  _____/ /___/ /________  (_)___ 
+#  | | /| / / __ \/ ___/ / __  / ___/ __ \/ / __ \
+#  | |/ |/ / /_/ / /  / / /_/ / /__/ /_/ / / / / /
+#  |__/|__/\____/_/  /_/\__,_/\___/\____/_/_/ /_/
+
+
+__title__   = 'worldcoin'
+__version__ = '0.5'
+__author__  = "@c0ding"
+__repo__    = "https://github.com/c0ding/worldcoin-api"
+__license__ = "Apache v2.0 License"
+
+import worldcoin_utils
+from worldcoin_api import (
+	about, difficulty, hashrate, block_count, total_coins, block_hash,
+	transaction, address, to_btc, to_usd
+)
---
+++ packages/worldcoin/worldcoin-0.5.tar.gz/worldcoin-0.5/worldcoin/worldcoin_api.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import urllib
+from worldcoin_utils import exchange
+from worldcoin_utils import blockexplorer
+
+__title__   = 'worldcoin'
+__version__ = '0.5'
+__author__  = "@c0ding"
+__repo__    = "https://github.com/c0ding/worldcoin-api"
+__license__ = "Apache v2.0 License"
+
+
+def about():
+	return "{} v.{} is maintained by {} and available at {}.".format(__title__, __version__, __author__, __repo__)
+
+
+def difficulty():
+	"""Returns the current network difficulty."""
+
+	d = urllib.urlopen(blockexplorer('coindetails'))
+	d = json.loads(d.read())
+	return d[u'Difficulty']
+
+
+def hashrate():
+	"""Returns the current network hashrate."""
+	
+	d = urllib.urlopen(blockexplorer('coindetails'))
+	d = json.loads(d.read())
+	return d[u'HashRate']
+
+
+def block_count():
+	"""Returns the number of blocks in the longest block chain.
+	   Equivalent to Bitcoin's getblockcount.
+	"""
+
+	d = urllib.urlopen(blockexplorer('coindetails'))
+	d = json.loads(d.read())
+	return d[u'Blocks']
+
+
+def total_coins():
+	"""Returns the number of Worldcoin mined."""
+	
+	d = urllib.urlopen(blockexplorer('coindetails'))
+	d = json.loads(d.read())
+	return d[u'TotalCoins']
+
+
+def block_hash(PARAMETER=None):
+	"""Returns the block hash at a specific height.
+	   [PARAMETER] is optional, should be a block hash or height.
+	   [DEFAULT] equals to last block.
+	"""
+	if not PARAMETER:
+		d = urllib.urlopen(blockexplorer('block'))
+		d = json.loads(d.read())
+		return json.dumps(d, indent=4)
+	else:
+		d = urllib.urlopen(blockexplorer('block') + '/' + str(PARAMETER))
+		d = json.loads(d.read())
+		return json.dumps(d, indent=4)
+	
+
+def transaction(PARAMETER):
+	"""Returns details of a specific transaction.
+	   [PARAMETER] is required and should be a transaction hash.
+	"""
+
+	d = urllib.urlopen(blockexplorer('transaction') + '/' + str(PARAMETER))
+	d = json.loads(d.read())
+	return json.dumps(d, indent=4)
+
+
+def address(PARAMETER):
+	"""Returns details of a specific address.
+	   [PARAMETER] is required and should be an address hash.
+	"""
+
+	d = urllib.urlopen(blockexplorer('address') + '/' + str(PARAMETER))
+	d = json.loads(d.read())
+	return json.dumps(d, indent=4)
+
+
+def to_btc():
+	"""Returns array with trading pair object."""
+	
+	d = urllib.urlopen(exchange('wdc_btc'))
+	d = json.loads(d.read())
+	return json.dumps(d, indent=4)
+	
+	
+def to_usd():
+	"""Returns array with trading pair object."""
+	
+	d = urllib.urlopen(exchange('wdc_usd'))
+	d = json.loads(d.read())
+	return json.dumps(d, indent=4)
