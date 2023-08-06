e54ed821415f14b1ea7a058bdfd4d34f08dd3b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:49 +0000

    Add package RuSocSci/0.6.7/RuSocSci-0.6.7.zip

---
+++ packages/RuSocSci/RuSocSci-0.6.7.zip/RuSocSci-0.6.7/rusocsci/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2013 Wilbert van Ham, Radboud University Nijmegen
+# Distributed under the terms of the GNU General Public License (GPL).
+
+#version info for RuSocSci
+__version__='0.6.7'
+__license__='GNU GPLv3 (or more recent equivalent)'
+__author__='Wilbert van Ham'
+__author_email__='W.vanHam@socsci.ru.nl'
+__maintainer_email__='W.vanHam@socsci.ru.nl'
+__url__='http://www.socsci.ru.nl/~wilberth/python/hardware.html'
+
+
+
