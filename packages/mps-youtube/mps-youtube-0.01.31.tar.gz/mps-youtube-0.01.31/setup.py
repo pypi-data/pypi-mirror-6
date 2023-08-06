60e44f704d33e86c0ca217f63eecef8f2951c469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:49 +0000

    Add package mps-youtube/0.01.31/mps-youtube-0.01.31.tar.gz

---
+++ packages/mps-youtube/mps-youtube-0.01.31.tar.gz/mps-youtube-0.01.31/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+""" setup.py for mps-youtube.
+
+https://np1.github.com/mps-youtube
+
+"""
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name="mps-youtube",
+    version="0.01.31",
+    description="Terminal based YouTube jukebox with playlist management",
+    keywords=["video", "music", "audio", "youtube", "stream", "download"],
+    author="nagev",
+    author_email="np1nagev@gmail.com",
+    url="http://github.com/np1/mps-youtube",
+    download_url="https://github.com/np1/mps-youtube/tarball/master",
+    scripts=['mpsyt'],
+    install_requires=['Pafy'],
+    package_data={"": ["LICENSE", "README.rst", "README"]},
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+        "Topic :: Internet :: WWW/HTTP"],
+    long_description=open("README").read()
+)
