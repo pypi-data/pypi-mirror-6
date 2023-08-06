6be3e7b54867a77854baf4ab29890437f530d18f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:14 +0000

    Add package libarchive/0.3.7/libarchive-0.3.7.tar.gz

---
+++ packages/libarchive/libarchive-0.3.7.tar.gz/libarchive-0.3.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+def _pre_install():
+    print("Verifying that the library is accessible.")
+
+    try:
+        import libarchive.library
+    except OSError:
+        print("Library can not be loaded: %s" % (_LIB_FILEPATH))
+        raise
+
+
+class _custom_install(install):
+    def run(self):
+        _pre_install()
+        install.run(self)
+
+description = "Python adapter for universal, libarchive-based archive access."
+
+setup(name='libarchive',
+      version='0.3.7',
+      description=description,
+      long_description="""""",
+      classifiers=[],
+      keywords='archive libarchive 7z tar bz2 zip gz',
+      author='Dustin Oprea',
+      author_email='myselfasunder@gmail.com',
+      url='https://github.com/dsoprea/PyEasyArchive',
+      license='GPL 2',
+      packages=find_packages(exclude=['dev']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      cmdclass={ 'install': _custom_install },
+)
