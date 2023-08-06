59f5de93d2e6389eb0c5dd7a77866f6d872df78e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package ajenti/1.2.20.6/ajenti-1.2.20.6.tar.gz

---
+++ packages/ajenti/ajenti-1.2.20.6.tar.gz/ajenti-1.2.20.6/ajenti/plugins/custom_branding_ponvia/content/js/branding.m.js
@@ -0,0 +1,12 @@
+$(function () {
+    // Favicon
+    $('link[rel=icon]').attr('href', 'http://www.ponvia.com/wp-content/themes/ponvia/favicon.ico')
+    $('.licensebar').remove()
+
+    // Custom title
+    // $('title').text('?')
+
+    // Login page
+    $('.page.auth img.logo').attr('src', 'http://www.ponvia.com/wp-content/themes/ponvia/images/logo-new.jpg')
+    $('.page.auth img.logo').addClass('visible')
+});
