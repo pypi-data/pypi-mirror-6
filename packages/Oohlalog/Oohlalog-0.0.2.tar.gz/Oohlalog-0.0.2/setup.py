84bf0b1401d3249f7d7b15814260a4218767c298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:23 +0000

    Add package Oohlalog/0.0.2/Oohlalog-0.0.2.tar.gz

---
+++ packages/Oohlalog/Oohlalog-0.0.2.tar.gz/Oohlalog-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='Oohlalog',
+    version='0.0.2',
+    author='Andy Warner',
+    author_email='awarner@bertramcapital.com',
+    packages=['oohlalog'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Oohlalog/',
+    license='LICENSE.txt',
+    description='Useful remote logger api to be used with the Oohlalog web application.',
+    long_description=open('README.txt').read()
+
+)
