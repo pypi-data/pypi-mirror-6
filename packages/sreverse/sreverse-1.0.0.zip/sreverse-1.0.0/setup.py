c96d46ac99243cb29bdfa1131c65da0cfc869b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:21 +0000

    Add package sreverse/1.0.0/sreverse-1.0.0.zip

---
+++ packages/sreverse/sreverse-1.0.0.zip/sreverse-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'sreverse',
+    version = '1.0.0',
+    py_modules = ['sreverse'],
+    author = 'group1',
+    author_email = 'rnjsduddnr2@naver.com',
+    url = '-',
+    description = 'A simple str reverse',
+    )
