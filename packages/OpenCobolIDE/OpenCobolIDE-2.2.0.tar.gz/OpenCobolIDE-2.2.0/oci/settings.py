b6c89262bf97fc62dcda3d98f9bf078a502200d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:21 +0000

    Add package OpenCobolIDE/2.2.0/OpenCobolIDE-2.2.0.tar.gz

---
+++ packages/OpenCobolIDE/OpenCobolIDE-2.2.0.tar.gz/OpenCobolIDE-2.2.0/oci/settings.py
@@ -0,0 +1,277 @@
+# Copyright (c) <2013-2014> Colin Duquesnoy
+#
+# This file is part of OpenCobolIDE.
+#
+# OpenCobolIDE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# OpenCobolIDE is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# OpenCobolIDE. If not, see http://www.gnu.org/licenses/.
+"""
+Gives an easy and safe access to the app settings
+"""
+import os
+import sys
+import pyqode.core
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import QSettings
+
+
+DEFAULT_EDITOR_SETTINGS = """{
+    "General": {
+        "leftMarginPos": "7",
+        "rightMarginPos": "72",
+        "showWhiteSpaces": "False",
+        "tabLength": "4",
+        "useSpacesInsteadOfTab": "True"
+    },
+    "codeCompletion": {
+        "caseSensitive": "False",
+        "showTooltips": "True",
+        "triggerKey": "32",
+        "triggerKeys": "[46]",
+        "triggerLength": "1",
+        "triggerSymbols": "[]"
+    }
+}
+"""
+
+DEFAULT_EDITOR_STYLE = """{
+    "General": {
+        "background": "#f8f8f8",
+        "caretLineBackground": "#efefef",
+        "foldIndicatorBackground": "#c1d1b0",
+        "font": "monospace",
+        "fontSize": "10",
+        "foreground": "#000000",
+        "margin": "#FF0000",
+        "matchedBraceBackground": "#b4eeb4",
+        "matchedBraceForeground": "#ff0000",
+        "nativeFoldingIndicator": "True",
+        "pygmentsStyle": "default",
+        "searchOccurrenceBackground": "#ffff00",
+        "searchOccurrenceForeground": "#000000",
+        "selectionBackground": "#accd8a",
+        "selectionForeground": "#ffffff",
+        "whiteSpaceForeground": "#dddddd"
+    }
+}
+"""
+
+class Settings(object):
+
+    def __init__(self):
+        self._settings = QSettings("OpenCobolIDE", "OpenCobolIDE")
+
+    @property
+    def geometry(self):
+        v = self._settings.value("mainWindowGeometry")
+        if v:
+            return bytes(v)
+        return None
+
+    @geometry.setter
+    def geometry(self, geometry):
+        self._settings.setValue("mainWindowGeometry", geometry)
+
+    @property
+    def state(self):
+        v = self._settings.value("mainWindowState")
+        if v:
+            return bytes(v)
+        return None
+
+    @state.setter
+    def state(self, state):
+        self._settings.setValue("mainWindowState", state)
+
+    @property
+    def maximised(self):
+        return bool(int(self._settings.value("maximised", "0")))
+
+    @maximised.setter
+    def maximised(self, value):
+        self._settings.setValue("maximised", int(value))
+
+    @property
+    def fullscreen(self):
+        return bool(int(self._settings.value("fullscreen", "0")))
+
+    @fullscreen.setter
+    def fullscreen(self, value):
+        self._settings.setValue("fullscreen", int(value))
+
+    @property
+    def size(self):
+        return self._settings.value("size", QtCore.QSize(900, 700))
+
+    @property
+    def navigationPanelVisible(self):
+        return bool(int(self._settings.value("navigationPanelVisible", "1")))
+
+    @navigationPanelVisible.setter
+    def navigationPanelVisible(self, value):
+        return self._settings.setValue("navigationPanelVisible", int(value))
+
+    @property
+    def logPanelVisible(self):
+        return bool(int(self._settings.value("logPanelVisible", "0")))
+
+    @logPanelVisible.setter
+    def logPanelVisible(self, value):
+        return self._settings.setValue("logPanelVisible", int(value))
+
+    @size.setter
+    def size(self, value):
+        self._settings.setValue("size", value)
+
+    @property
+    def lastFilePath(self):
+        """
+        Returns the last used open/save path
+        """
+        default_value = ""
+        if sys.platform == "win32":
+            default_value = "c:\\"
+        return self._settings.value("lastUsedPath", default_value)
+
+    @lastFilePath.setter
+    def lastFilePath(self, path):
+        """
+        Sets the last used path (save or open path)
+
+        :param path: path string
+        :type path: str or unicode
+        """
+        assert isinstance(path, str) or isinstance(path, unicode)
+        self._settings.setValue("lastUsedPath", os.path.dirname(path))
+
+    @property
+    def show_line_numbers(self):
+        return bool(int(self._settings.value("showLineNumbers", True)))
+
+    @show_line_numbers.setter
+    def show_line_numbers(self, state):
+        self._settings.setValue("showLineNumbers", int(state))
+
+    @property
+    def show_whitespaces(self):
+        return bool(int(self._settings.value("showWhitespaces", False)))
+
+    @show_whitespaces.setter
+    def show_whitespaces(self, state):
+        self._settings.setValue("showWhitespaces", int(state))
+
+    @property
+    def enable_cc(self):
+        return bool(int(self._settings.value("enableCC", True)))
+
+    @enable_cc.setter
+    def enable_cc(self, state):
+        self._settings.setValue("enableCC", int(state))
+
+    @property
+    def useExternalShell(self):
+        return bool(int(self._settings.value("useExternalShell", False)))
+
+    @useExternalShell.setter
+    def useExternalShell(self, state):
+        self._settings.setValue("useExternalShell", int(state))
+
+    @property
+    def shellCommand(self):
+        return str(self._settings.value("shell", "gnome-terminal -e"))
+
+    @shellCommand.setter
+    def shellCommand(self, cmd):
+        self._settings.setValue("shell", cmd)
+
+    @property
+    def editorSettings(self):
+        json_data = self._settings.value("editorSettings",
+                                         DEFAULT_EDITOR_SETTINGS)
+        r = pyqode.core.PropertyRegistry()
+        r.load(json_data)
+        return r
+
+    @editorSettings.setter
+    def editorSettings(self, propertyRegistry):
+        self._settings.setValue("editorSettings", propertyRegistry.dump())
+
+    @property
+    def editorStyle(self):
+        json_data = self._settings.value("editorStyle",
+                                         DEFAULT_EDITOR_STYLE)
+        r = pyqode.core.PropertyRegistry()
+        r.load(json_data)
+        return r
+
+    @editorStyle.setter
+    def editorStyle(self, propertyRegistry):
+        self._settings.setValue("editorStyle", propertyRegistry.dump())
+
+    @property
+    def consoleBackground(self):
+        return QtGui.QColor(self._settings.value("consoleBackground",
+                                                 "#FFFFFF"))
+
+    @consoleBackground.setter
+    def consoleBackground(self, value):
+        if isinstance(value, QtGui.QColor):
+            value = value.name()
+        self._settings.setValue("consoleBackground", value)
+
+    @property
+    def consoleForeground(self):
+        return QtGui.QColor(self._settings.value("consoleForeground",
+                                                 "#404040"))
+
+    @consoleForeground.setter
+    def consoleForeground(self, value):
+        if isinstance(value, QtGui.QColor):
+            value = value.name()
+        self._settings.setValue("consoleForeground", value)
+
+    @property
+    def consoleUserInput(self):
+        return QtGui.QColor(self._settings.value("consoleUserInput",
+                                                 "#22AA22"))
+
+    @consoleUserInput.setter
+    def consoleUserInput(self, value):
+        if isinstance(value, QtGui.QColor):
+            value = value.name()
+        self._settings.setValue("consoleUserInput", value)
+
+    @property
+    def consoleAppOutput(self):
+        return QtGui.QColor(self._settings.value("consoleAppOutput",
+                                                 "#4040FF"))
+
+    @consoleAppOutput.setter
+    def consoleAppOutput(self, value):
+        if isinstance(value, QtGui.QColor):
+            value = value.name()
+        self._settings.setValue("consoleAppOutput", value)
+
+    @property
+    def homePageColorScheme(self):
+        return int(self._settings.value("homePageStyle", "0"))
+
+    @homePageColorScheme.setter
+    def homePageColorScheme(self, value):
+        self._settings.setValue("homePageStyle", str(value))
+
+    @property
+    def appStyle(self):
+        return int(self._settings.value("style", "0"))
+
+    @appStyle.setter
+    def appStyle(self, value):
+        self._settings.setValue("style", value)
---
+++ packages/OpenCobolIDE/OpenCobolIDE-2.2.0.tar.gz/OpenCobolIDE-2.2.0/setup.py
@@ -0,0 +1,93 @@
+# Copyright (c) <2013-2014> Colin Duquesnoy
+#
+# This file is part of OpenCobolIDE.
+#
+# OpenCobolIDE is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# OpenCobolIDE is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# OpenCobolIDE. If not, see http://www.gnu.org/licenses/.
+"""
+PCEF is code editor framework for PySide applications
+
+This is the setup script, install it as any python package.
+
+.. note:: You will need to install PySide and OpenCobol on your own
+"""
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+def read_version():
+    """
+    Reads the version without self importing
+    """
+    with open("oci/__init__.py") as f:
+        lines = f.read().splitlines()
+        for l in lines:
+            if "__version__" in l:
+                return l.split("=")[1].strip().replace('"', "")
+
+
+def run_as_root():
+    return os.getuid() == 0
+
+
+# get long description
+with open('README.rst', 'r') as readme:
+    long_desc = readme.read()
+
+
+# install requirements
+requirements = ['pygments>=1.6', 'pyqode.core>=1.3.0',
+                'pyqode.widgets>=1.2.0', 'qdarkstyle>=1.8']
+if sys.platform == "win32" and sys.version_info[0] == 3:
+    requirements += ['chardet2']
+else:
+    requirements += ['chardet']
+
+data_files = []
+if sys.platform == "linux" and run_as_root():
+    data_files.append(('/usr/share/applications', ['share/open-cobol-ide.desktop']))
+    data_files.append(('/usr/share/pixmaps', ['share/OpenCobolIDE.png']))
+
+
+setup(
+    name='OpenCobolIDE',
+    version=read_version(),
+    packages=find_packages(),
+    keywords=["Cobol; OpenCobol; IDE"],
+    package_dir={'oci': 'oci', "oci_designer_plugins": "oci_designer_plugins"},
+    data_files=data_files,
+    url='https://github.com/OpenCobolIDE/OpenCobolIDE',
+    license='GPL v3',
+    author='Colin Duquesnoy',
+    author_email='colin.duquesnoy@gmail.com',
+    description='A simple cobol IDE',
+    long_description=long_desc,
+    zip_safe=False,
+    install_requires=requirements,
+    entry_points={'gui_scripts': ['open-cobol-ide = oci.main:main'],
+                  'pyqode_plugins': [
+                      'oci_widgets = oci_designer_plugins.cobol_plugin']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications :: Qt',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later '
+        '(GPLv3+)',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Text Editors :: Integrated Development Environments (IDE)']
+)
