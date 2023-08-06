094a1a6ddb94ac5b23422a9156f06b40e6c3eb71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:02 +0000

    Add package PyTox/0.0.13/PyTox-0.0.13.tar.gz

---
+++ packages/PyTox/PyTox-0.0.13.tar.gz/PyTox-0.0.13/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+
+setup(
+    name="PyTox",
+    version="0.0.13",
+    description = 'Python binding for Tox the skype replacement',
+    author = 'Wei-Ning Huang (AZ)',
+    author_email = 'aitjcize@gmail.com',
+    url = 'http://github.com/aitjcize/PyTox',
+    license = 'GPL',
+    ext_modules=[
+        Extension("tox", [
+                "tox/tox.c",
+                "tox/core.c",
+                "tox/av.c",
+                "tox/util.c",
+       ],
+       extra_compile_args=['-g', '-Wno-declaration-after-statement'],
+       libraries=['toxcore', 'toxav'])
+    ]
+)
