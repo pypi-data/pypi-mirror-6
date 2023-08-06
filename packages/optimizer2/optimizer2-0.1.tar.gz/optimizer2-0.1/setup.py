86175acb7f523c5cd3636b352d19e1db7bb39fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:47 +0000

    Add package optimizer2/0.1/optimizer2-0.1.tar.gz

---
+++ packages/optimizer2/optimizer2-0.1.tar.gz/optimizer2-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='optimizer2',
+      version='0.1',
+      description='A command line optimization software',
+      author='Pavel Sountsov',
+      author_email='slabode@aim.com',
+      url='https://github.com/SiegeLord/Optimizer2',
+      scripts=['bin/optimizer'],
+      license='LICENSE',
+      packages=['optimizer2']
+     ) 
