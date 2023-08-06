409a91fcd0eb6787d90f8ac340e6ae126e474611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:04 +0000

    Add package fut14/0.0.11/fut14-0.0.11.tar.gz

---
+++ packages/fut14/fut14-0.0.11.tar.gz/fut14-0.0.11/fut14/config.py
@@ -0,0 +1,10 @@
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip,deflate,sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    #'Accept-Charset': 'utf-8, iso-8859-1, utf-16, *;q=0.1',
+    'Connection': 'keep-alive',
+    #'Keep-Alive': '300',
+    'DNT': '1',
+}
