5f56a4a998e76b6d6f71355b7d3247213b5840a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:40 +0000

    Add package muto-client/0.2.2/muto-client-0.2.2.tar.gz

---
+++ packages/muto-client/muto-client-0.2.2.tar.gz/muto-client-0.2.2/setup.py
@@ -0,0 +1,25 @@
+import os.path
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+
+setup(
+    name='muto-client',
+    version='0.2.2',
+    description='muto is a client/server system for cloud-based image manipulation in Django projects',
+    author='Philipp Bosch',
+    author_email='hello+muto-client@pb.io',
+    url='http://github.com/philippbosch/muto-client',
+    packages=find_packages(),
+    package_data={'': ['requirements.txt']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    test_suite='tests',
+    install_requires=[str(ir.req) for ir in parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'))]
+)
