1dd94dbb356ad3d920157faa6c1f0f6c580d4126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:35 +0000

    Add package myfirst/1.0.0/myfirst-1.0.0.zip

---
+++ packages/myfirst/myfirst-1.0.0.zip/myfirst-1.0.0/myfirst.py
@@ -0,0 +1,7 @@
+"""This is my first module!"""
+def print_me(this_list):
+    """this_list is any Python list."""
+    print("My birthday: ")
+    for each_item in this_list:
+        print(each_item)
+
