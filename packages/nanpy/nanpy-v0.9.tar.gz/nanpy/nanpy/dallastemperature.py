353a15d1f29986b9fffe87198af717d54d2d3eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:16 +0000

    Add package nanpy/0.9/nanpy-v0.9.tar.gz

---
+++ packages/nanpy/nanpy-v0.9.tar.gz/nanpy/nanpy/dallastemperature.py
@@ -0,0 +1,48 @@
+from nanpy.arduinoboard import ArduinoObject
+from nanpy.arduinoboard import (arduinoobjectmethod, arduinoclassmethod, returns)
+
+class DallasTemperature(ArduinoObject):
+
+    def __init__(self, pin):
+        ArduinoObject.__init__(self)
+        self.pin = pin
+        self.id = self.call('new', pin)
+
+    @returns(int)
+    @arduinoobjectmethod
+    def getDeviceCount(self):
+        pass
+
+    def getAddress(self, index):
+        val = self.call('getAddress')
+        if val == "1":
+            return val
+        return val.split(' ')
+            
+
+    @arduinoobjectmethod
+    def requestTemperatures(self, address = None):
+        pass
+
+    @returns(float)
+    @arduinoobjectmethod
+    def getTempC(self, address):
+        pass
+
+    @returns(float)
+    @arduinoobjectmethod
+    def getTempF(self, address):
+        pass
+
+    @classmethod
+    @returns(float)
+    @arduinoclassmethod
+    def toCelsius(cls, value):
+        pass
+
+    @classmethod
+    @returns(float)
+    @arduinoclassmethod
+    def toFahrenheit(cls, value):
+        pass
+
