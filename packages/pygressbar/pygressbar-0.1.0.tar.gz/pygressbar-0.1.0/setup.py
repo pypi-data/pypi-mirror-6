531ce30e1fab11dc7abc17374ee0a7d0c69693a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:00 +0000

    Add package pygressbar/0.1.0/pygressbar-0.1.0.tar.gz

---
+++ packages/pygressbar/pygressbar-0.1.0.tar.gz/pygressbar-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='pygressbar',
+    py_modules=['pygressbar'],
+    version='0.1.0',
+    description='Terminal progress bar for Python',
+    author='Andrew Shim',
+    author_email='andrewkshim92@gmail.com',
+    url='https://github.com/andrewkshim/pygressbar',
+    download_url='https://github.com/andrewkshim/pygressbar/tarball/1.0',
+    keywords=['terminal'],
+    license='MIT',
+    classifiers=[],
+)
