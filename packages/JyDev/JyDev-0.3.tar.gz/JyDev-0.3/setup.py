d343eb144fd57b386a056d429a6ad3b40a8ad97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:34 +0000

    Add package JyDev/0.3/JyDev-0.3.tar.gz

---
+++ packages/JyDev/JyDev-0.3.tar.gz/JyDev-0.3/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='JyDev',
+      version='0.3',
+      description='Jython Project Maker/Manager with pydev support',
+      author='Yoav Glazner',
+      author_email='yoavglazner@gmail.com',
+      url='https://github.com/yglazner/jy-dev',
+      packages=['jy_dev'],
+     )
