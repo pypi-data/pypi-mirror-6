3ee7ca7e5c9c63a976c022ac3a8c6b2eb0d4a9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:08 +0000

    Add package PreIngest/0.1/PreIngest-0.1.tar.gz

---
+++ packages/PreIngest/PreIngest-0.1.tar.gz/PreIngest-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PreIngest',
+    version='0.1',
+    packages=['preingest',],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
