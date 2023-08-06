0d9d1203888ba8844ca14d858c8cb84862123638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:15 +0000

    Add package s3cat/1.0.7/s3cat-1.0.7.zip

---
+++ packages/s3cat/s3cat-1.0.7.zip/s3cat-1.0.7/setup.py
@@ -0,0 +1,29 @@
+# to distribute:
+# python setup.py register sdist upload
+
+from setuptools import setup
+import sys
+import os
+
+try:
+    ## Remove 'MANIFEST' file to force
+    ## distutils to recreate it.
+    ## Only in "sdist" stage. Otherwise
+    ## it makes life difficult to packagers.
+    if sys.argv[1] == "sdist":
+        os.unlink("MANIFEST")
+except:
+    pass
+
+setup(
+    name="s3cat",
+    version="1.0.7",
+    scripts=['s3cat'],
+    author="Ron Reiter",
+    author_email="ron@crosswiselabs.com",
+    url="http://github.com/crosswise/s3cat",
+    license="MIT",
+    description="s3cat lets you fetch all files under a certain directory (either raw or gzipped) into stdout.",
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
+    install_requires=["boto"],
+)
