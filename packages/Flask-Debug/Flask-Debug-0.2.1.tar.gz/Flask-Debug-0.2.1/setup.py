28c1849b3ede1d46a92e60696fe85dfca6d40367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:14 +0000

    Add package Flask-Debug/0.2.1/Flask-Debug-0.2.1.tar.gz

---
+++ packages/Flask-Debug/Flask-Debug-0.2.1.tar.gz/Flask-Debug-0.2.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='Flask-Debug',
+    version='0.2.1',
+    description='Shows reflection/configuration to aid the development of '
+                'Flask applications.',
+    long_description=read('README.rst'),
+    include_package_data=True,
+    zip_safe=False,
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/flask-debug',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['flask'],
+)
