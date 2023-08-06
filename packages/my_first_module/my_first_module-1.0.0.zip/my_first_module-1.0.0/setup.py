a25e8dc754fd61159b9ce168253792e436fc8282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:46 +0000

    Add package my_first_module/1.0.0/my_first_module-1.0.0.zip

---
+++ packages/my_first_module/my_first_module-1.0.0.zip/my_first_module-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+	name = 'my_first_module' ,
+	version = '1.0.0' ,
+	py_modules = ['my_first_module'] ,
+	author = 'redo' ,
+	author_email = 'uonered@gmail.com' ,
+	description = 'distribution test' ,
+)
