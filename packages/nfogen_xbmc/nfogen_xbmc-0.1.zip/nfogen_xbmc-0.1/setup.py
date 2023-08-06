66c4a6249d1bb8e78d48eb459a03bc1db5900013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:21 +0000

    Add package nfogen_xbmc/0.1/nfogen_xbmc-0.1.zip

---
+++ packages/nfogen_xbmc/nfogen_xbmc-0.1.zip/nfogen_xbmc-0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+	name = 'nfogen_xbmc',
+	packages = ['nfogen_xbmc'],
+	version = '0.1',
+	description = 'Python module for generating XBMC NFO files',
+	url = 'https://github.com/nterupt/nfogen_xbmc',
+	author = 'Mark Eggert',
+	author_email = 'nteruptedservice@gmail.com',
+	license = 'GPLv3',
+	classifiers = [
+		"Programming Language :: Python :: 3.3", 
+		"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)", 
+		"Operating System :: OS Independent", 
+		"Development Status :: 2 - Pre-Alpha", 
+		"Intended Audience :: Developers", 
+		"Topic :: Utilities"
+		]
+	)
