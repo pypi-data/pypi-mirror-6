74b0cd12888aece86526d6e9ae79fc5866f60d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package bam2x/0.1.1/bam2x-0.1.1.tar.gz

---
+++ packages/bam2x/bam2x-0.1.1.tar.gz/bam2x-0.1.1/lib/bam2x/Run/__init__.py
@@ -0,0 +1,16 @@
+__all__=["getseq","query_bam","read","sort","cmpgene","getanno"]
+from . import *
+
+'''
+command lines
+'''
+commands= {
+    "getseq":getseq,
+    "getanno":getanno,
+    "query_bam":query_bam,
+    "read":read,
+    "sort":sort,
+    "cmpgene":cmpgene,
+}
+
+
