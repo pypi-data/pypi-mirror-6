71996da704053c779551ebd3636e000412c96f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:33 +0000

    Add package modlib/0.1.5/modlib-0.1.5.tar.gz

---
+++ packages/modlib/modlib-0.1.5.tar.gz/modlib-0.1.5/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='modlib',
+    version='0.1.5',
+    description='Dynamic module loader',
+    author='Les Aker',
+    author_email='me@lesaker.org',
+    url='https://github.com/akerl/modlib',
+    license='MIT License',
+    packages=['modlib'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
