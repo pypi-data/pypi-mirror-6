8ffdeb0b0c991d55c84301365c4923b8aa754aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:56 +0000

    Add package django-clever-pages/0.2.6/django-clever-pages-0.2.6.tar.gz

---
+++ packages/django-clever-pages/django-clever-pages-0.2.6.tar.gz/django-clever-pages-0.2.6/page/views.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import get_object_or_404
+from django.views.generic.detail import DetailView
+from .models import Page
+
+from meta.views import MetadataMixin
+
+
+class MetaTagsMexin(MetadataMixin):
+    """ Mixin для отображения meta тегов из батарейки django-meta """
+
+    def get_meta_description(self, context):
+        return self.page.meta_description
+
+    def get_meta_keywords(self, context):
+        keywords_str = self.page.meta_keywords
+        if keywords_str:
+            return [c.strip() for c in keywords_str.split(',')]
+
+    def get_meta_title(self, context):
+        return self.page.meta_title or self.page.name
+
+
+class HomePageView(MetaTagsMexin, DetailView):
+
+    model = Page
+    context_object_name = 'page'
+    template_name = 'page/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        return context
+
+    def get_object(self):
+        page = get_object_or_404(Page, slug='home')
+        self.page = page
+        return page
+
+
+class PageDetailView(MetaTagsMexin, DetailView):
+
+    model = Page
+    context_object_name = 'page'
+    template_name = 'page/default.html'
+
+    def get_object(self):
+        page = get_object_or_404(Page, path=self.request.path, active=1)
+        self.page = page
+        return page
+
+    def get_template_names(self):
+        return ["page/%s.html" % self.page.template]
