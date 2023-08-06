336d8637876a70221fc84a37e5a1880e79bc1589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:30 +0000

    Add package boost_regex/0.55/boost_regex-0.55.zip

---
+++ packages/boost_regex/boost_regex-0.55.zip/boost_regex-0.55/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+
+
+
+setup(
+    name='boost_regex',
+    version='0.55',
+    ext_modules=[
+        Extension('boost_regex',
+                  ['boost_regex.cpp'],
+                  include_dirs=['/usr/include', '\\usr\\include', '\\usr\\include\\boost', 'C:\\local\\boost_1_55_0'],
+                  extra_compile_args=['-std=c++0x'],
+                  library_dirs=['C:\\local\\boost_1_55_0\\lib64-msvc-9.0', '\\usr\\lib', '\\usr\\lib\\boost', '/usr/lib', '/usr/lib/boost'],
+                  extra_link_args=['-lboost_regex']
+        )
+    ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author='Bart Jellema',
+    author_email='bart@zeromail.com',
+    url='http://pypi.python.org/pypi/boost_regex/',
+    description='Very basic interface to the boost regex library.'
+)
