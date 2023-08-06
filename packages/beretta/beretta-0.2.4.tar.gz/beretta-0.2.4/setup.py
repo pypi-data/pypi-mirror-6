1210c6c788d421ef2e6c20602c061ce81bfdecb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:15 +0000

    Add package beretta/0.2.4/beretta-0.2.4.tar.gz

---
+++ packages/beretta/beretta-0.2.4.tar.gz/beretta-0.2.4/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+
+setup(
+  name = 'beretta',
+  author = 'beretta contributors',
+  author_email = 'github@require.pm',
+  url = 'https://github.com/dieselpoweredkitten/beretta',
+  version = '0.2.4',
+  description = 'BERT (de)serializer for your Pythons',
+  license = 'MIT',
+  ext_modules = [Extension('beretta', ['beretta.c'])],
+  classifiers = (
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+  ),
+  install_requires = (
+    'termformat>=0.1.2',
+  )
+)
