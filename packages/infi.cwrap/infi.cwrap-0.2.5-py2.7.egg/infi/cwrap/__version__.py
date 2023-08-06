3df61d3698bc67d036ae803cea3b530f4859959c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:25 +0000

    Add package infi.cwrap/0.2.5/infi.cwrap-0.2.5-py2.7.egg

---
+++ packages/infi.cwrap/infi.cwrap-0.2.5-py2.7.egg/infi/cwrap/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '6bd835605bcf504c5c50f1c120df1af371d75559'
+__git_head_subject__ = 'Finished release v0.2.5.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
