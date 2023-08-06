9c9fa43eb4a7fc22006abe0f96247cd81df0c50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:37 +0000

    Add package haii/1.0/haii-1.0.tar.gz

---
+++ packages/haii/haii-1.0.tar.gz/Madlib-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="Madlib",
+      version="1.0",
+      description="A madlib generator",
+      author="Steven de Jong",
+      py_module=['madlib']
+)
