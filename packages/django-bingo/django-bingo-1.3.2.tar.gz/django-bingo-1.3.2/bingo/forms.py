51562e62ac24828815e3573286d6822b5faca708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:19 +0000

    Add package django-bingo/1.3.2/django-bingo-1.3.2.tar.gz

---
+++ packages/django-bingo/django-bingo-1.3.2.tar.gz/django-bingo-1.3.2/bingo/forms.py
@@ -0,0 +1,79 @@
+from django import forms
+from django.utils.translation import pgettext, ugettext as _
+from django.conf import settings
+from django.contrib.auth.hashers import get_hasher
+
+from models import BingoBoard, is_starttime
+from times import is_starttime
+
+
+SALT = getattr(settings, "SALT", "hackme")
+GAME_START_TIMES = getattr(settings, "GAME_START_TIMES", None)
+
+GAME_START_DISABLED = getattr(settings, "GAME_START_DISABLED", False)
+
+
+class CreateForm(forms.Form):
+    password = forms.CharField(label=_(u"Password (optional)"),
+                               widget=forms.PasswordInput(),
+                               required=False)
+
+    def clean_password(self):
+        if self.cleaned_data['password']:
+            hasher = get_hasher(algorithm='sha1')
+            hashed_password = hasher.encode(
+                self.cleaned_data['password'], SALT)
+            return hashed_password
+        else:
+            return None
+
+    def clean(self):
+        if GAME_START_DISABLED:
+            raise forms.ValidationError(
+                _(u"Starting new games is disabled."))
+
+        if not is_starttime():
+            start, end = GAME_START_TIMES
+            start_time_str = "{0}:{1}".format(
+                str(start[0]).zfill(2),
+                str(start[1]).zfill(2))
+            end_time_str = "{0}:{1}".format(
+                str(end[0]).zfill(2),
+                str(end[1]).zfill(2))
+
+            raise forms.ValidationError(
+                _(u"Games can only be started between {0} and {1}.").format(
+                    start_time_str, end_time_str))
+
+        return super(CreateForm, self).clean()
+
+
+class ReclaimForm(forms.Form):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, data=None, game=None, *args, **kwargs):
+        self.game = game
+        super(ReclaimForm, self).__init__(data=data, *args, **kwargs)
+
+    def clean_password(self):
+        hasher = get_hasher(algorithm='sha1')
+        hashed_password = hasher.encode(self.cleaned_data['password'],
+                                        SALT)
+        if not self.game or self.game.is_expired():
+            raise forms.ValidationError(
+                _("The game is expired, please create a new board."))
+        bingo_boards = BingoBoard.objects.filter(game=self.game,
+                                                 password=hashed_password)
+        if bingo_boards.count() > 0:
+            # if two users have the same password,
+            # just return the first board.
+            self.cleaned_data['bingo_board'] = bingo_boards[0]
+        else:
+            raise forms.ValidationError(
+                _(u"No active board with this password."
+                    " Try again, or create a new one."))
+        return hashed_password
+
+
+class ChangeThemeForm(forms.Form):
+    theme = forms.CharField(required=False)
---
+++ packages/django-bingo/django-bingo-1.3.2.tar.gz/django-bingo-1.3.2/bingo/static/bingo/js/bingo.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+    // dummy, for when no ajax request was executed, yet
+    var ajaxRequest = {"abort": function(){}};
+
+    $("form.voteform").each(function(idx, obj){
+        $(obj).find("input[type=submit]").remove();
+        var vote_field = $("<input>").attr("name", "vote").attr("type", "hidden");
+        var veto_link = $("<a>").attr("href", "#").addClass("veto").text("[-]");
+
+        function ajax_submit(form){
+            var data = {};
+            form.find("input").each(function(idx, obj){
+                form_field = $(obj);
+                data[form_field.attr("name")] = form_field.val();
+            })
+            ajaxRequest.abort(); // abort any running ajax requests
+            ajaxRequest = $.ajax(ajax_vote_url + bingo_board + "/", {
+                "type": "post",
+                "dataType": "json",
+                "data": data,
+                "success":  update_numbers
+            });
+        }
+
+        function vote(form, what){
+            form.find("input[name=vote]").val(what);
+            ajax_submit(form);
+            var field_id = $(obj).find("input[name='field_id']").val();
+            var fields = $("[data-field-id=" + field_id + "]");
+            mark_field(fields, what);
+        }
+
+        veto_link.click(function(){
+            vote($(obj), "-");
+            return false;
+        });
+
+        // toggle up / neutral
+        $(obj).parent().click(function(){
+            // toggle
+            if($(obj).parent().hasClass("active")){
+                vote($(obj), "0");
+            } else {
+                vote($(obj), "+");
+            }
+            return false;
+        });
+
+        $(obj).append(vote_field);
+        $(obj).append(veto_link);
+    });
+
+    if(bingo_board == my_board){
+        $(".bingofield, .word").not(".middle").each(function(idx, obj){
+            $(obj).addClass("clickable");
+        });
+    }
+
+    function mark_field(obj, vote){
+        if(vote == "+") {
+            obj.removeClass("veto").addClass("active");
+        } else if (vote == "0" ){
+            obj.removeClass("veto").removeClass("active");
+        } else if (vote == "-" ){
+            obj.addClass("veto").removeClass("active");
+        }
+    }
+
+    function update_numbers(data) {
+        if(data != null) {
+            $("#num_active_users").text(data["num_active_users"]);
+            $("#num_users").text(data["num_users"]);
+            $("[data-field-id]").each(function(idx, obj){
+                var field_data = data[$(obj).attr("data-field-id")];
+                var vote = field_data[0];
+                var num_votes = field_data[1]
+
+                mark_field($(obj), vote);
+                $(obj).find("span.votes").text(num_votes)
+            });
+        } else {
+            ajaxRequest.abort(); // abort any running ajax requests
+            ajaxRequest = $.ajax(ajax_vote_url + bingo_board + "/", {
+                "type": "get",
+                "dataType": "json",
+                "success": update_numbers
+            });
+        }
+    }
+
+    setInterval(update_numbers, 10000)
+})
