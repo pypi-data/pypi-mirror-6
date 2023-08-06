3067d74ffdca47eeca62e2d9c34ce3512b3eb933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:47 +0000

    Add package logging_subprocess/0.0.1/logging_subprocess-0.0.1.tar.gz

---
+++ packages/logging_subprocess/logging_subprocess-0.0.1.tar.gz/logging_subprocess-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='logging_subprocess',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='maxvitek',
+    author_email='',
+    description=''
+)
