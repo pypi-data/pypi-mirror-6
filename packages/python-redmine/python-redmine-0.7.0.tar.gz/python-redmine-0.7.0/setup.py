cb6e1d1ff12f1a3af77ed2cdd6b1a37ff00c2af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:16 +0000

    Add package python-redmine/0.7.0/python-redmine-0.7.0.tar.gz

---
+++ packages/python-redmine/python-redmine-0.7.0.tar.gz/python-redmine-0.7.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+exec(open('redmine/version.py').read())
+
+setup(
+    name='python-redmine',
+    version=__version__,
+    packages=['redmine'],
+    url='https://github.com/maxtepkeev/python-redmine',
+    license=open('LICENSE').read(),
+    author='Max Tepkeev',
+    author_email='tepkeev@gmail.com',
+    description='Library for communicating with a Redmine project management application',
+    long_description=open('README.rst').read() + '\n\n' + open('CHANGELOG.rst').read(),
+    keywords='redmine',
+    install_requires=['requests >= 2.1.0'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
