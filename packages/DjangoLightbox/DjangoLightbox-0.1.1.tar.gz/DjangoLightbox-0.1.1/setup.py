58e881195dde92e5eecf7a75adf4398d7af59b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:42 +0000

    Add package DjangoLightbox/0.1.1/DjangoLightbox-0.1.1.tar.gz

---
+++ packages/DjangoLightbox/DjangoLightbox-0.1.1.tar.gz/DjangoLightbox-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='DjangoLightbox',
+    version='0.1.1',
+    author='Geert Dekkers',
+    author_email='geert@djangowebstudio.nl',
+    packages=['lightbox', ],
+    scripts=[],
+    url='http://pypi.python.org/pypi/DjangoLightbox/',
+    license='LICENSE.txt',
+    description='alternative for a full screen javascript lightbox for the django framework',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.5",
+    ],
+)
