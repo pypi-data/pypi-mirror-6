4298fe276742ce81baa5ca98cefde4a965ef4294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:41 +0000

    Add package dysl/0.1/dysl-0.1.tar.gz

---
+++ packages/dysl/dysl-0.1.tar.gz/dysl-0.1/README.md
@@ -0,0 +1,12 @@
+Do you speak London?
+====================
+
+Command line tool for naturla language identification, also known as langID. Currently supporting 3 languages only, English, French and Spanish.
+
+$ python dysl.py WRITE SOME TEXT HERE TO BE CLASSIFIED
+
+Contacts
+--------
+ 
++ Name: [Tarek Amr](http://tarekamr.appspot.com/)
++ Twitter: [@gr33ndata](https://twitter.com/gr33ndata)
---
+++ packages/dysl/dysl-0.1.tar.gz/dysl-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='dysl',
+      version='0.1',
+      description='Do you speak London?',
+      long_description=readme(),
+      url='https://github.com/gr33ndata/dysl',
+      author='Tarek Amr',
+      license='MIT',
+      packages=['dysl', 
+                'dysl.dyslib', 
+                'dysl.corpora', 
+                'dysl.corpora.corpuslib'],
+      include_package_data=True,
+      zip_safe=False)
