9672935b94cfd66ea185385c9f93b231df9af382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package robotframework-faker/0.1/robotframework-faker-0.1.tar.gz

---
+++ packages/robotframework-faker/robotframework-faker-0.1.tar.gz/robotframework-faker-0.1/setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+
+setup(
+    name='robotframework-faker',
+    packages=['robotframework-faker'],  # this must be the same as the name above
+    version='0.1',
+    description='Robot Framework wrapper for faker, a fake test data generator',
+    author='Guy Kisel',
+    author_email='guy.kisel@gmail.com',
+    url='https://github.com/guykisel/robotframework-faker',
+    keywords='robotframework testing test automation faker',  # arbitrary keywords
+    classifiers=[],
+    install_requires=['fake-factory', 'robotframework'],
+    long_description=open('README.md').read(),
+    license='LICENSE'
+)
