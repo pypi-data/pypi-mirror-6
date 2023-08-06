8969d3a8a219963e1fbbdb1602b10db3284c76f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:31 +0000

    Add package django-ptree/0.2.85/django-ptree-0.2.85.zip

---
+++ packages/django-ptree/django-ptree-0.2.85.zip/django-ptree-0.2.85/ptree/management/__init__.py
@@ -0,0 +1,49 @@
+from django.conf import settings
+from django.contrib.auth import models as auth_models
+from django.contrib.auth.management import create_superuser
+from django.db.models import signals
+
+from ptree.sessionlib.models import StubModel
+
+
+def create_default_superuser(app, created_models, verbosity, **kwargs):
+    """
+    Creates our default superuser.
+    """
+    username = settings.ADMIN_USERNAME
+    password = settings.ADMIN_PASSWORD
+    email = getattr(settings, 'ADMIN_EMAIL', '')
+    try:
+        auth_models.User.objects.get(username=username)
+    except auth_models.User.DoesNotExist:
+        s = '\n'.join(['Creating default superuser.',
+             'Username: {}'.format(username),
+             'Email: {}'.format(email)])
+        assert auth_models.User.objects.create_superuser(username, email, password)
+    else:
+        print 'Default superuser already exists.'
+
+if getattr(settings, 'CREATE_DEFAULT_SUPERUSER', False):
+    # From http://stackoverflow.com/questions/1466827/:
+    # Prevent interactive question about wanting a superuser created.
+    # (This code has to go in this otherwise empty "models" module
+    # so that it gets processed by the "syncdb" command during
+    # database creation.)
+    signals.post_syncdb.disconnect(
+        create_superuser,
+        sender=auth_models,
+        dispatch_uid='django.contrib.auth.management.create_superuser'
+    )
+
+    # Trigger default superuser creation.
+    signals.post_syncdb.connect(
+        create_default_superuser,
+        sender=auth_models,
+        dispatch_uid='common.models.create_testuser'
+    )
+
+def create_stub_model(sender, **kwargs):
+    if sender.__name__ == 'django.contrib.auth.models':
+        StubModel().save()
+
+signals.post_syncdb.connect(create_stub_model)
