c0ca9e2c7447e65f85b9c5ebcc10e7289250ef04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:09 +0000

    Add package GameReportCommon/1.1/GameReportCommon-1.1.zip

---
+++ packages/GameReportCommon/GameReportCommon-1.1.zip/GameReportCommon-1.1/README.txt
@@ -0,0 +1,10 @@
+===========
+GameReportCommon package
+===========
+
+    from  GameReportCommon.loger import Logger
+    from  GameReportCommon.config_manager import ConfigManager
+    from  GameReportCommon.worker import Worker
+
+
+
---
+++ packages/GameReportCommon/GameReportCommon-1.1.zip/GameReportCommon-1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='GameReportCommon',
+    version='1.1',
+    author='Sebastien Tolron',
+    author_email='sebastien@tolron',
+    packages=['GameReportCommon', ],
+    scripts=['bin/event.py'],
+    url='http://pypi.python.org/pypi/GameReportCommon/',
+    license='LICENSE.txt',
+    description='Common library for GameReport project',
+    long_description=open('README.txt').read(),
+)
+
