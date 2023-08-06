83f97672bdcf6c5259107082e98da3199fe121e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:03 +0000

    Add package datatank_py/0.4/datatank_py-0.4.tar.gz

---
+++ packages/datatank_py/datatank_py-0.4.tar.gz/datatank_py-0.4/setup.py
@@ -0,0 +1,19 @@
+#
+# http://peterdowns.com/posts/first-time-with-pypi.html
+#
+from setuptools import setup, find_packages
+setup(
+    name = "datatank_py",
+    version = "0.4",
+    packages = find_packages(),
+    
+    install_requires = ['numpy>1.0'],
+    zip_safe = False,
+    author = "Adam R. Maxwell",
+    author_email = "amaxwell@mac.com",
+    description = "Python modules for creating and modifying DataTank files",
+    license = "BSD New",
+    url = "http://github.com/amaxwell/datatank_py",
+    doc_url = "http://amaxwell.github.io/datatank_py",
+    download_url = "http://github.com/amaxwell/datatank_py/tarball/0.4",
+)
