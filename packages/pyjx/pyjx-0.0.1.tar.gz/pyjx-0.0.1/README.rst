8606b76e68e2d9a2cb6a1630944ef3a4b3d524b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:21 +0000

    Add package pyjx/0.0.1/pyjx-0.0.1.tar.gz

---
+++ packages/pyjx/pyjx-0.0.1.tar.gz/pyjx-0.0.1/README.rst
@@ -0,0 +1,8 @@
+.. image:: https://travis-ci.org/damoti/pyjx.png
+    :target: https://travis-ci.org/damoti/pyjx
+    :alt: Build Status
+
+Pyjx
+====
+
+Python to JavaScript translator and runtime.
---
+++ packages/pyjx/pyjx-0.0.1.tar.gz/pyjx-0.0.1/pyjd/__init__.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+import pyjswidgets
+sys.path += [os.path.dirname(pyjswidgets.__file__)]
+from pyjs.runners import RunnerManager
+
+#TODO: very ugly to self-import and setattr(self) ... remove ASAP!
+import pyjd
+
+
+pyjdversion = '0.9'
+
+
+_manager = RunnerManager()
+_manager.set_conf()
+for key, value in _manager._conf.iteritems():
+    setattr(pyjd, key, value)
+_manager.set_runner()
+
+
+#TODO: perm delete ASAP unless someone claims use; disable for now
+sys.path += [os.path.dirname(__file__)]
+
+
+add_setup_callback = _manager.add_setup_listener
+setup = _manager.setup
+run = _manager.run
