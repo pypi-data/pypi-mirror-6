d8ab10eeccd2f1fe6458212be1de6f958c19051e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:37 +0000

    Add package listoflist/1.1.0/listoflist-1.1.0.tar.gz

---
+++ packages/listoflist/listoflist-1.1.0.tar.gz/listoflist-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+			name = "listoflist",
+			version = "1.1.0",
+			py_modules = ["listoflist"],
+			author = "Allen",
+			author_email = "zhangjunnana05@gmail.com",
+			url = "",
+			description ="",
+
+	)
