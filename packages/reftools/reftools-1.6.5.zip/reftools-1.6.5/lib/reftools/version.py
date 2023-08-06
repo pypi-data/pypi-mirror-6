9365a910323fa2e91ae444ca949fec9840c1b2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:18 +0000

    Add package reftools/1.6.5/reftools-1.6.5.zip

---
+++ packages/reftools/reftools-1.6.5.zip/reftools-1.6.5/lib/reftools/version.py
@@ -0,0 +1,70 @@
+"""This is an automatically generated file created by stsci.distutils.hooks.version_setup_hook.
+Do not modify this file by hand.
+"""
+
+__all__ = ['__version__', '__vdate__', '__svn_revision__', '__svn_full_info__',
+           '__setup_datetime__']
+
+import datetime
+
+
+__version__ = '1.6.5'
+__vdate__ = 'unspecified'
+__svn_revision__ = '30214'
+__svn_full_info__ = 'Path: reftools\nURL: https://svn.stsci.edu/svn/ssb/stsci_python/reftools/tags/release_1.6.5\nRepository Root: https://svn.stsci.edu/svn/ssb/stsci_python\nRepository UUID: fe389314-cf27-0410-b35b-8c050e845b92\nRevision: 30214\nNode Kind: directory\nSchedule: normal\nLast Changed Author: cslocum\nLast Changed Rev: 30213\nLast Changed Date: 2014-03-11 10:20:26 -0400 (Tue, 11 Mar 2014)'
+__setup_datetime__ = datetime.datetime(2014, 3, 11, 11, 0, 49, 397284)
+
+
+def update_svn_info():
+    """Update the SVN info if running out of an SVN working copy."""
+
+    import os
+    import string
+    import subprocess
+
+    global __svn_revision__
+    global __svn_full_info__
+
+    # Wind up the module path until we find the root of the project
+    # containing setup.py
+    path = os.path.abspath(os.path.dirname(__file__))
+    dirname = os.path.dirname(path)
+    setup_py = os.path.join(path, 'setup.py')
+    while path != dirname and not os.path.exists(setup_py):
+        path = os.path.dirname(path)
+        dirname = os.path.dirname(path)
+        setup_py = os.path.join(path, 'setup.py')
+
+    try:
+        pipe = subprocess.Popen(['svnversion', path],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        if pipe.wait() == 0:
+            stdout = pipe.stdout.read().decode('latin1').strip()
+            if stdout and stdout[0] in string.digits:
+                __svn_revision__ = stdout
+    except OSError:
+        pass
+
+    try:
+        pipe = subprocess.Popen(['svn', 'info', path],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        if pipe.wait() == 0:
+            lines = []
+            for line in pipe.stdout.readlines():
+                line = line.decode('latin1').strip()
+                if not line:
+                    continue
+                lines.append(line)
+
+            if not lines:
+                __svn_full_info__ = 'unknown'
+            else:
+                __svn_full_info__ = '\n'.join(lines)
+    except OSError:
+        pass
+
+
+update_svn_info()
+del update_svn_info
