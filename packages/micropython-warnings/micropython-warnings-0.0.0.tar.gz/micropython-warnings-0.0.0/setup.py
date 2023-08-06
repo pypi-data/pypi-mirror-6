47b4ed91f00ea59e5c8700e37b362770b0c5a5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:51 +0000

    Add package micropython-warnings/0.0.0/micropython-warnings-0.0.0.tar.gz

---
+++ packages/micropython-warnings/micropython-warnings-0.0.0.tar.gz/micropython-warnings-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-warnings',
+      version='0.0.0',
+      description='Reimplementation of unittest package for MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='MicroPython Developers',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['warnings'])
