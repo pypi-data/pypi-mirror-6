29a55f4713e8cbf9fd5f7350620e6660f60ad41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:30 +0000

    Add package alf/0.4.1/alf-0.4.1.tar.gz

---
+++ packages/alf/alf-0.4.1.tar.gz/alf-0.4.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='alf',
+    version='0.4.1',
+    description="OAuth Client",
+    long_description=open('README.rst').read(),
+    keywords='oauth client client_credentials requests',
+    author=u'Globo.com',
+    author_email='timecore@corp.globo.com',
+    url='https://github.com/globocom/alf',
+    license='Proprietary',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        ],
+    packages=find_packages(
+        exclude=(
+            'tests',
+        ),
+    ),
+    include_package_data=True,
+    install_requires=[
+        'requests>=1.2.3',
+    ],
+)
