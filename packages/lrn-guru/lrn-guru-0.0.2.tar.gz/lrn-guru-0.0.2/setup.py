ed241ec4bc3756a94e2a5484360d8a8f207b0d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:04 +0000

    Add package lrn-guru/0.0.2/lrn-guru-0.0.2.tar.gz

---
+++ packages/lrn-guru/lrn-guru-0.0.2.tar.gz/lrn-guru-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='lrn-guru',
+    version='0.0.2',
+    author='Razzi Abuissa + Alex Brashear',
+    author_email='alexjbrashear@gmail.com',
+    packages=['lrn'],
+    scripts=['bin/lrn.py'],
+    url='http://pypi.python.org/pypi/TowelStuff/',
+    license='LICENSE.txt',
+    description='Useful towel-related stuff.',
+    long_description=open('README.md').read(),
+	install_requires=[],
+)
+
