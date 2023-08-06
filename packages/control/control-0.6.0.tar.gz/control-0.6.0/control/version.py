1b8a564152f06b93b3339b7c4d9439a3326b2c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:18 +0000

    Add package control/0.6.0/control-0.6.0.tar.gz

---
+++ packages/control/control-0.6.0.tar.gz/control-0.6.0/control/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CONTROL SETUP.PY
+short_version = '0.6.0'
+version = '0.6.0'
+full_version = '0.6.0'
+git_revision = 'e51de5a9823766fef505f90b77f68bd4682250a8'
+release = True
+
+if not release:
+    version = full_version
