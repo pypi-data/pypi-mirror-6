d84af13d192decbe9dc9548aecb0e69faff30a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:42 +0000

    Add package django-daguerre/2.0.0/django-daguerre-2.0.0.tar.gz

---
+++ packages/django-daguerre/django-daguerre-2.0.0.tar.gz/django-daguerre-2.0.0/daguerre/utils.py
@@ -0,0 +1,40 @@
+from hashlib import sha1
+
+from django.core.files.base import File
+from django.core.files.storage import default_storage
+from django.core.files.temp import NamedTemporaryFile
+from django.utils.encoding import smart_bytes
+import six
+
+
+#: Formats that we trust to be able to handle gracefully.
+KEEP_FORMATS = ('PNG', 'JPEG', 'GIF')
+#: Default format to convert other file types to.
+DEFAULT_FORMAT = 'PNG'
+
+
+def make_hash(*args, **kwargs):
+    start = kwargs.get('start', None)
+    stop = kwargs.get('stop', None)
+    step = kwargs.get('step', None)
+    return sha1(smart_bytes(u''.join([
+        six.text_type(arg) for arg in args])
+    )).hexdigest()[start:stop:step]
+
+
+def save_image(
+        image,
+        storage_path,
+        format=DEFAULT_FORMAT,
+        storage=default_storage):
+    """
+    Saves a PIL image file to the given storage_path using the given storage.
+    Returns the final storage path of the saved file.
+
+    """
+    if format not in KEEP_FORMATS:
+        format = DEFAULT_FORMAT
+
+    with NamedTemporaryFile() as temp:
+        image.save(temp, format=format)
+        return storage.save(storage_path, File(temp))
