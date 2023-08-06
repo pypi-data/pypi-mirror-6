9d03b57e3becd205af98ebb35463282c72f90b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:22 +0000

    Add package pyDOE/0.3.4/pyDOE-0.3.4.tar.gz

---
+++ packages/pyDOE/pyDOE-0.3.4.tar.gz/pyDOE-0.3.4/pyDOE/__init__.py
@@ -0,0 +1,21 @@
+"""
+================================================================================
+pyDOE: Design of Experiments for Python
+================================================================================
+
+Author: Abraham Lee
+Copyright: 2013
+
+"""
+# from __future__ import absolute_import
+
+__author__ = 'Abraham Lee'
+__version__ = '0.3.4'
+
+from pyDOE.doe_box_behnken import *
+from pyDOE.doe_composite import *
+from pyDOE.doe_factorial import *
+from pyDOE.doe_lhs import *
+from pyDOE.doe_fold import *
+from pyDOE.doe_plackett_burman import *
+    
