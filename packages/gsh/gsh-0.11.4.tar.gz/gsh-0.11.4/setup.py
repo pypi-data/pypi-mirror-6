5156a0a79373a5265e930ed09bc07e02a74fbdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:08 +0000

    Add package gsh/0.11.4/gsh-0.11.4.tar.gz

---
+++ packages/gsh/gsh-0.11.4.tar.gz/gsh-0.11.4/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+execfile('gsh/version.py')
+
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
+
+kwargs = {
+    "name": "gsh",
+    "version": str(__version__),
+    "packages": ["gsh"],
+    "package_data": {"gsh": [
+        "plugins/loaders/*.py",
+        "plugins/hooks/*.py",
+        "plugins/executors/*.py",
+    ]},
+    "scripts": ["bin/gsh"],
+    "description": "Pluggable Distributed SSH Command Executer.",
+    # PyPi, despite not parsing markdown, will prefer the README.md to the
+    # standard README. Explicitly read it here.
+    "long_description": open("README").read(),
+    "author": "Gary M. Josack",
+    "maintainer": "Gary M. Josack",
+    "author_email": "gary@byoteki.com",
+    "maintainer_email": "gary@byoteki.com",
+    "license": "MIT",
+    "install_requires": required,
+    "url": "https://github.com/gmjosack/gsh",
+    "download_url": "https://github.com/gmjosack/gsh/archive/master.tar.gz",
+    "classifiers": [
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+}
+
+setup(**kwargs)
