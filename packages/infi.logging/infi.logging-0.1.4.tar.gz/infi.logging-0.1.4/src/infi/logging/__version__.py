bd83e7706c4b3327ebd468373a66276447bdf2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:36 +0000

    Add package infi.logging/0.1.4/infi.logging-0.1.4.tar.gz

---
+++ packages/infi.logging/infi.logging-0.1.4.tar.gz/infi.logging-0.1.4/src/infi/logging/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.4"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '539568164ca69d57e664ad3f3f0b8667c2286a67'
+__git_head_subject__ = 'Finished release v0.1.4.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
