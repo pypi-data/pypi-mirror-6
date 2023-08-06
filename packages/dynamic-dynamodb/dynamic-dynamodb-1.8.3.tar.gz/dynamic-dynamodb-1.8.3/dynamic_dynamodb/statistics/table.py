2b0c8fce7f45005f72cb9730c86e8aec9362fe7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package dynamic-dynamodb/1.8.3/dynamic-dynamodb-1.8.3.tar.gz

---
+++ packages/dynamic-dynamodb/dynamic-dynamodb-1.8.3.tar.gz/dynamic-dynamodb-1.8.3/dynamic_dynamodb/statistics/table.py
@@ -0,0 +1,80 @@
+""" This module returns stats about the DynamoDB table """
+import math
+from datetime import datetime, timedelta
+
+from dynamic_dynamodb.core import dynamodb
+from dynamic_dynamodb.log_handler import LOGGER as logger
+from dynamic_dynamodb.core.cloudwatch import (
+    CLOUDWATCH_CONNECTION as cloudwatch_connection)
+
+
+def get_consumed_read_units_percent(table_name, time_frame=300):
+    """ Returns the number of consumed read units in percent
+
+    :type table_name: str
+    :param table_name: Name of the DynamoDB table
+    :type time_frame: int
+    :param time_frame: How many seconds to look at
+    :returns: int -- Number of consumed reads
+    """
+    metrics = cloudwatch_connection.get_metric_statistics(
+        period=time_frame,
+        start_time=datetime.utcnow()-timedelta(minutes=10, seconds=time_frame),
+        end_time=datetime.utcnow()-timedelta(minutes=10),
+        metric_name='ConsumedReadCapacityUnits',
+        namespace='AWS/DynamoDB',
+        statistics=['Sum'],
+        dimensions={'TableName': table_name},
+        unit='Count')
+
+    if metrics:
+        consumed_read_units = int(
+            math.ceil(float(metrics[0]['Sum'])/float(time_frame)))
+    else:
+        consumed_read_units = 0
+
+    consumed_read_units_percent = int(
+        math.ceil(
+            float(consumed_read_units) /
+            float(dynamodb.get_provisioned_table_read_units(table_name)) *
+            100))
+
+    logger.info('{0} - Consumed read units: {1:d}%'.format(
+        table_name, consumed_read_units_percent))
+    return consumed_read_units_percent
+
+
+def get_consumed_write_units_percent(table_name, time_frame=300):
+    """ Returns the number of consumed write units in percent
+
+    :type table_name: str
+    :param table_name: Name of the DynamoDB table
+    :type time_frame: int
+    :param time_frame: How many seconds to look at
+    :returns: int -- Number of consumed writes
+    """
+    metrics = cloudwatch_connection.get_metric_statistics(
+        period=time_frame,
+        start_time=datetime.utcnow()-timedelta(minutes=10, seconds=time_frame),
+        end_time=datetime.utcnow()-timedelta(minutes=10),
+        metric_name='ConsumedWriteCapacityUnits',
+        namespace='AWS/DynamoDB',
+        statistics=['Sum'],
+        dimensions={'TableName': table_name},
+        unit='Count')
+
+    if metrics:
+        consumed_write_units = int(
+            math.ceil(float(metrics[0]['Sum'])/float(time_frame)))
+    else:
+        consumed_write_units = 0
+
+    consumed_write_units_percent = int(
+        math.ceil(
+            float(consumed_write_units) /
+            float(dynamodb.get_provisioned_table_write_units(table_name)) *
+            100))
+
+    logger.info('{0} - Consumed write units: {1:d}%'.format(
+        table_name, consumed_write_units_percent))
+    return consumed_write_units_percent
