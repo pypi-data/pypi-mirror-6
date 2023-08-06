86235efbf5776575fb315ea70b6f8e2675965e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:16 +0000

    Add package gluish/0.1.7/gluish-0.1.7.tar.gz

---
+++ packages/gluish/gluish-0.1.7.tar.gz/gluish-0.1.7/gluish/parameter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Parameter add-ons
+=================
+
+Custom luigi parameters.
+
+"""
+
+import luigi
+
+class ILNParameter(luigi.Parameter):
+    """
+    Parse ILN (internal library number), so that the result is always in the
+    *4 char* format.
+    """
+    def parse(self, s):
+        return s.zfill(4)
