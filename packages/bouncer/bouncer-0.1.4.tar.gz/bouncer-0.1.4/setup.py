6a610d4d63f2dc05d2bf3677605f7669cf44e071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:51 +0000

    Add package bouncer/0.1.4/bouncer-0.1.4.tar.gz

---
+++ packages/bouncer/bouncer-0.1.4.tar.gz/bouncer-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+required_modules = []
+
+setup(name='bouncer',
+      version='0.1.4',
+      description='Simple Declarative Authentication based on Ryan Bates excellent cancan library',
+      url='http://github.com/jtushman/bouncer',
+      author='Jonathan Tushman',
+      author_email='jonathan@zefr.com',
+      install_requires=required_modules,
+      license='MIT',
+      packages=['bouncer'],
+      zip_safe=False)
