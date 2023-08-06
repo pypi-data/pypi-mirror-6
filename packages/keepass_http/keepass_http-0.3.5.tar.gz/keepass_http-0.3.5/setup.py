a289ad1f2ea09ccae52ce822a3438eb72ead2be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:40 +0000

    Add package keepass_http/0.3.5/keepass_http-0.3.5.tar.gz

---
+++ packages/keepass_http/keepass_http-0.3.5.tar.gz/keepass_http-0.3.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+with open("requirements/package.txt") as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open("README.rst") as readme_file:
+    long_description = readme_file.read()
+
+setup(name="keepass_http",
+      version="0.3.5",
+      description="Python Keepass HTTPD for ChromeIPass",
+      long_description=long_description,
+      author="Benjamin Hedrich",
+      author_email="kiwisauce@pagenotfound.de",
+      url="https://github.com/bhedrich/python-keepass-httpd/",
+      package_dir={"": "src"},
+      packages=find_packages("src/", exclude="tests"),
+      install_requires=requirements,
+      scripts=["bin/python-keepass-httpd"],
+      include_package_data=True,
+      zip_safe=False,
+)
