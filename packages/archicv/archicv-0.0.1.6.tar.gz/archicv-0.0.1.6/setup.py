bbb7993743f5930efc59f47be78787b6af7d617d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:30 +0000

    Add package archicv/0.0.1.6/archicv-0.0.1.6.tar.gz

---
+++ packages/archicv/archicv-0.0.1.6.tar.gz/archicv-0.0.1.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'archicv',
+      version = '0.0.1.6',
+      keywords = ('archicv', 'ArchiCV', 'archi', 'archiCV'),
+      url = 'https://github.com/zhangxiansheng/-archi',
+      description = 'OpenCV on Landscape Architecture',
+      license = 'MIT License',
+      install_requires = ['numpy>=1.0'],
+      
+      author = 'Zhang Yiwei',
+      author_email = 'Karl1991223@126.com',
+      
+      packages = find_packages(),
+      platforms = 'any',
+      )
