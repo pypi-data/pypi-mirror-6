423f7f0ba1de185af59274fb2621e7e6f4a9161b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:27 +0000

    Add package pypore/0.0.2.dev-20140423221759-f88cddd/pypore-0.0.2.dev-20140423221759-f88cddd.tar.gz

---
+++ packages/pypore/pypore-0.0.2.dev-20140423221759-f88cddd.tar.gz/pypore-0.0.2.dev-20140423221759-f88cddd/src/pypore/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '0.0.2'
+version = '0.0.2'
+full_version = '0.0.2.dev-20140423221759-f88cddd'
+git_revision = 'f88cddd62f9f3bd019d24ee40a4d09d300d355f5'
+release = False
+
+if not release:
+    version = full_version
