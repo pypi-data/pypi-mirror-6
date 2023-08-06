1131f2ad4523039c3183df0aaa194d3075082d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package nbastats/1.1.0/nbastats-1.1.0.zip

---
+++ packages/nbastats/nbastats-1.1.0.zip/nbastats-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'nbastats',
+	version = '1.1.0',
+	packages = ['nbastats'],
+	install_requires=['pandas>=0.13','requests>=2.2.1'],
+	author = 'Bradley Fay',
+	url = 'https://github.com/bradleyfay/nbadatacollection',
+	author_email = 'bradley.fay@gmail.com',
+	license = 'MIT'
+	)
