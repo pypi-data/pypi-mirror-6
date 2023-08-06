71a6db17a292e3eea04e39d71db9722e9b2f89f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:20 +0000

    Add package cms-draggable-files/0.1.1/cms-draggable-files-0.1.1.tar.gz

---
+++ packages/cms-draggable-files/cms-draggable-files-0.1.1.tar.gz/cms-draggable-files-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='cms-draggable-files',
+    version='0.1.1',
+    author='Jelena Kutalovskaja',
+    author_email='jelena.kutalovskaja@gmail.com',
+    packages=['cms_draggable_files'],
+    scripts=[''],
+    url='http://pypi.python.org/pypi/cms-draggable-files',
+    license='LICENSE.txt',
+    description='CMS placeholder can recieve dragged files and add to plugin by files extensions.',
+    long_description=open('README.txt').read(),
+)
