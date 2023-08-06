6567c6e305116feeec5c310cbc46ee7fec4ba629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:23 +0000

    Add package sseclient/0.0.8/sseclient-0.0.8.tar.gz

---
+++ packages/sseclient/sseclient-0.0.8.tar.gz/sseclient-0.0.8/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys
+from setuptools import setup
+
+pytest_runner = ['pytest-runner'] if 'ptr' in sys.argv else []
+
+setup(
+    name='sseclient',
+    version='0.0.8',
+    author='Brent Tubbs',
+    author_email='brent.tubbs@gmail.com',
+    py_modules=['sseclient'],
+    install_requires=['requests>=2.0.0', 'six'],
+    tests_require=['pytest', 'mock'],
+    setup_requires=[] + pytest_runner,
+    description=(
+        'Python client library for reading Server Sent Event streams.'),
+    long_description=open('README.rst').read(),
+    url='http://bits.btubbs.com/sseclient',
+)
