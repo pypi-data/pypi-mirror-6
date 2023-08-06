f479dac46dbbeb0576b9c2f352f3fa95813e6cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:08 +0000

    Add package hellosign-python-sdk/0.1/hellosign-python-sdk-0.1.tar.gz

---
+++ packages/hellosign-python-sdk/hellosign-python-sdk-0.1.tar.gz/hellosign-python-sdk-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='hellosign-python-sdk',
+      version='0.1',
+      description='An API wrapper written in Python to use with HelloSign\'s API (http://www.hellosign.com)',
+      url='http://github.com/storborg/hellosign-python-sdk',
+      author='Minh Danh',
+      author_email='minhdanh@siliconstraits.vn',
+      license='MIT',
+      packages=['hellosign-python-sdk'],
+      zip_safe=False)
