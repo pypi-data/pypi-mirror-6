dfb545d2a687d50fee8c726139b3985efd32e486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:16 +0000

    Add package ElePy/0.0.1/ElePy-0.0.1.tar.gz

---
+++ packages/ElePy/ElePy-0.0.1.tar.gz/ElePy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ElePy',
+    version='0.0.1',
+    url='https://github.com/joedborg/ElePy',
+    scripts=['CoPing/coping'],
+    license='GPLv2',
+    author='Joe Borg',
+    author_email='cyborg101010@gmail.com',
+    description='A Cisco style ping tool'
+)
