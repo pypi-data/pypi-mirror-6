215c8e882b7540729fcec4d61ac73d240a6e6d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:10 +0000

    Add package kotoba/3.0.1/kotoba-3.0.1.tar.gz

---
+++ packages/kotoba/kotoba-3.0.1.tar.gz/Kotoba-3.0.1/kotoba/exception.py
@@ -0,0 +1,14 @@
+class InvalidDataSourceError(Exception):
+    ''' Exception: The data source invalid '''
+
+class InvalidSelectorError(Exception):
+    ''' Exception: The selector invalid '''
+
+class InvalidInputError(Exception):
+    ''' Exception: Invalid input '''
+
+class FileNotFoundError(Exception):
+    ''' Exception: File not found '''
+
+class LexicalError(Exception):
+    ''' Exception: Selector Syntax Error '''
