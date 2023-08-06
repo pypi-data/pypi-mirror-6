ce8c4bdafb88c5113c9613ae6aea3bf594063a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:42 +0000

    Add package nostrain/0.1.1/nostrain-0.1.1.tar.gz

---
+++ packages/nostrain/nostrain-0.1.1.tar.gz/nostrain-0.1.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='nostrain',
+      version='0.1.1',
+      description="Themes for sphinx and sublime text.",
+      author='Gregory Vincic',
+      author_email='g@7de.se',
+      url='https://github.com/gregoryv/nostrain',
+      license='GPLv3',
+      packages=['nostrain'],
+      include_package_data=True,
+      install_requires=[]
+      )
