78a3e43505c644465767259a561fd278508b3aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:42 +0000

    Add package csvorm/0.1.1dev/csvorm-0.1.1dev.tar.gz

---
+++ packages/csvorm/csvorm-0.1.1dev.tar.gz/csvorm-0.1.1dev/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1.1'
+long_description = 'This allows to use the CSV as ORM'
+try:
+    with open('README') as fp:
+        long_description = fp.read()
+except:
+    pass
+
+setup(name='csvorm',
+      version=version,
+      description="orm for csv",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Natural Language :: Japanese',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='csv',
+      author='TakesxiSximada',
+      url='https://bitbucket.org/takesxi_sximada/csvorm',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
