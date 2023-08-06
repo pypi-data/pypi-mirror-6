da3c3cdaecafc504a22dc405f0ff53f5338437e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:05 +0000

    Add package asveloper_nester/1.4.0/asveloper_nester-1.4.0.tar.gz

---
+++ packages/asveloper_nester/asveloper_nester-1.4.0.tar.gz/asveloper_nester-1.4.0/asveloper_nester.py
@@ -0,0 +1,11 @@
+def print_list(the_list,indent=False,level=0):
+        for my_list in the_list:
+                if isinstance(my_list,list):
+                        print_list(my_list,indent,level+1)
+
+                else:
+                        if indent:
+                                for tab_space in range(level):
+                                        print('\t',end='')
+                        print(my_list)
+
---
+++ packages/asveloper_nester/asveloper_nester-1.4.0.tar.gz/asveloper_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'asveloper_nester',
+	version 	= '1.4.0',
+	py_modules 	= ['asveloper_nester'],
+	author 		= 'Arslan Nasir',
+	author_email 	= 'arslan@asveloper.com',
+	url 		= 'http://www.asveloper.com',
+	description 	= 'Nester allow you to print lists either nested list exists or not'
+)
