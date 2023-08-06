6947841713af672a0700520fdb60cd93e5920b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:09 +0000

    Add package pocket-change/0.0.3/pocket-change-0.0.3.zip

---
+++ packages/pocket-change/pocket-change-0.0.3.zip/pocket-change-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='pocket-change',
+      version='0.0.3',
+      author='Silas Ray',
+      author_email='silas.ray@nytimes.com',
+      license='Apache2.0',
+      url='http://pocket-change.readthedocs.org/',
+      description='The web front end for Sneeze and Pocket',
+      long_description='The web front end for the Sneeze nose plugin test result reporting tool.',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Information Technology',
+                   'Topic :: Software Development :: Quality Assurance',
+                   'Topic :: Software Development :: Testing'],
+      packages=find_packages(),
+      install_requires=['sneeze-pocket',
+                        'Flask',
+                        'Flask-RESTful',
+                        'Flask-SQLAlchemy',
+                        'passlib'])
