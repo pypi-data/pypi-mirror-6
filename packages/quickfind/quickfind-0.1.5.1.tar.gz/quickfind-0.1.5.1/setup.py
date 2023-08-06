df6be1392f6692526848b2e5eb80a305ff279f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:15 +0000

    Add package quickfind/0.1.5.1/quickfind-0.1.5.1.tar.gz

---
+++ packages/quickfind/quickfind-0.1.5.1.tar.gz/quickfind-0.1.5.1/setup.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='quickfind',
+        version="0.1.5.1",
+      description='Fuzzy find for the terminal',
+      url="https://github.com/Refefer/quickfind",
+      packages=['quickfind', 'quickfind/source'],
+      license="LICENSE",
+      scripts=['qf'],
+      author='Andrew Stanton',
+      author_email='Andrew Stanton',
+      extras_require={
+          "ctags": 'python-ctags',
+          "fsnix": 'fsnix==0.2'
+      },
+      classifiers=[
+       "License :: OSI Approved :: Apache Software License",
+       "Programming Language :: Python :: 2.7",
+       "Programming Language :: Python :: 3.2",
+       "Operating System :: OS Independent"
+      ])  
