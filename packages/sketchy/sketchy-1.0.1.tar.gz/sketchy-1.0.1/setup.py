8c482bd02e7b722c83378e70950ab6537fbd02bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:55 +0000

    Add package sketchy/1.0.1/sketchy-1.0.1.tar.gz

---
+++ packages/sketchy/sketchy-1.0.1.tar.gz/sketchy-1.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup	(
+		name = 'sketchy',
+		version = '1.0.1',
+		#py_modules = ['sketch.py'],
+		author_email = 'pramitghsh@gmail.com',
+		author = 'pramitghsh',
+	)
