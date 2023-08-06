fc3ab22c5f10984ec2b033c97778eb0f31a334bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:42 +0000

    Add package PyPrind/2.1.0/PyPrind-2.1.0.tar.gz

---
+++ packages/PyPrind/PyPrind-2.1.0.tar.gz/PyPrind-2.1.0/pyprind/prog_class.py
@@ -0,0 +1,41 @@
+import time
+import sys
+import os
+
+
+class Prog():
+    def __init__(self, iterations, track_time, stream):
+        self.cnt = 0
+        self.max_iter = iterations
+        self.track = track_time
+        self.start = time.clock()
+        self.stream = stream
+        self._check_stream()
+
+    def _check_stream(self):
+        if self.stream == 1 and os.isatty(sys.stdout.fileno()):
+            self._stream_out = sys.stdout.write
+            self._stream_flush = sys.stdout.flush
+        elif self.stream == 2 and os.isatty(sys.stderr.fileno()):
+            self._stream_out = sys.stderr.write
+            self._stream_flush = sys.stderr.flush
+        elif self.stream is not None and hasattr(self.stream, 'write'):
+            self._stream_out = self.stream.write
+            self._stream_flush = self.stream.flush
+        else:
+            print('Warning: No valid output stream.')
+            self._stream_out = self._no_stream
+            self._stream_flush = self._no_stream
+
+    def _elapsed(self):
+        return time.clock() - self.start
+
+    def _calc_eta(self):
+        elapsed = self._elapsed()
+        if self.cnt == 0 or elapsed < 0.001:
+            return None
+        rate = float(self.cnt) / elapsed
+        return int((float(self.max_iter) - float(self.cnt)) / rate)
+
+    def _no_stream(self, text=None):
+        pass
---
+++ packages/PyPrind/PyPrind-2.1.0.tar.gz/PyPrind-2.1.0/pyprind/progpercent.py
@@ -0,0 +1,53 @@
+# Sebastian Raschka 2014
+#
+# Progress Percentage class to instantiate a percentage indicator object
+# that is printed to the standard output screen to visualize the
+# progress in a iterative Python procedure
+
+import sys
+import time
+from pyprind.prog_class import Prog
+
+
+class ProgPercent(Prog):
+    """Initializes a percentage indicator object that allows visuzalization
+       of an iterational computation in the standard output screen. 
+
+    Keyword Arguments:
+        iterations (int): number of iterations of the computation
+        width (int): width of the progress bar in characters
+        track_time (bool): prints elapsed time and estimated time left
+        stream: takes 1 for stdout, 2 for stderr, or given stream object
+
+    """
+    def __init__(self, iterations, track_time=True, stream=2):
+        Prog.__init__(self, iterations, track_time, stream)
+        self.perc = 0
+        self.max_iter = float(self.max_iter) # accommodation for Python 2.x users
+        self._print_update()
+
+    def _calc_percent(self):
+        """Calculates the rel. progress in percent and rounds it to an integer."""
+        return round(self.cnt/self.max_iter * 100)
+
+    def _print_update(self):
+        """Prints formatted integer percentage and tracked time to the screen."""
+        self._stream_out('\r[%3d %%]' % (self.perc))
+        if self.track:
+            self._stream_out('   elapsed: %.3f sec' % self._elapsed())
+            if self._calc_eta():
+                self._stream_out(' | eta: %.3f sec' % self._calc_eta())
+            self._stream_flush()
+
+    def update(self):
+        """Updates the percentage indicator in every iteration of the task."""
+        self.cnt += 1
+        next_perc = self._calc_percent()
+        if next_perc > self.perc:
+            self.perc = next_perc
+            self._print_update()
+            self._stream_flush()
+        if self.cnt == self.max_iter:
+            if self.track:
+                self._stream_out(' | eta: 0.000 sec')
+            self._stream_out('\n') 
