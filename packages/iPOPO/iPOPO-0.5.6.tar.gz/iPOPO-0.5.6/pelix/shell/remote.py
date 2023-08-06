e31b7480c623976c7b045bdad810a2aacdbd2af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:18 +0000

    Add package iPOPO/0.5.6/iPOPO-0.5.6.tar.gz

---
+++ packages/iPOPO/iPOPO-0.5.6.tar.gz/iPOPO-0.5.6/pelix/shell/remote.py
@@ -0,0 +1,387 @@
+#!/usr/bin/env python
+# -- Content-Encoding: UTF-8 --
+"""
+iPOPO remote shell
+
+Provides a remote interface for the Pelix shell that can be accessed using
+telnet or netcat.
+
+:author: Thomas Calmant
+:copyright: Copyright 2014, isandlaTech
+:license: Apache License 2.0
+:version: 0.1.3
+:status: Beta
+
+..
+
+    Copyright 2014 isandlaTech
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+# Module version
+__version_info__ = (0, 1, 3)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
+# ------------------------------------------------------------------------------
+
+# iPOPO decorators
+from pelix.ipopo.decorators import ComponentFactory, Requires, Property, \
+    Validate, Invalidate, Provides
+
+# Shell constants
+import pelix.shell
+
+# Standard library
+from select import select
+import logging
+import threading
+import socket
+
+try:
+    # Python 3
+    import socketserver
+
+except ImportError:
+    # Python 2
+    import SocketServer as socketserver
+
+# ------------------------------------------------------------------------------
+
+_logger = logging.getLogger(__name__)
+
+# ------------------------------------------------------------------------------
+
+class SharedBoolean(object):
+    """
+    Shared boolean between objects / threads
+    """
+    def __init__(self, value=False):
+        """
+        Set up members
+        """
+        self._lock = threading.Lock()
+        self._value = value
+
+
+    def get_value(self):
+        """
+        Retrieves the boolean value
+        """
+        with self._lock:
+            return self._value
+
+
+    def set_value(self, value):
+        """
+        Sets the boolean value
+        """
+        with self._lock:
+            self._value = value
+
+# ------------------------------------------------------------------------------
+
+class RemoteConsole(socketserver.StreamRequestHandler):
+    """
+    Handles incoming connections and redirect network stream to the Pelix shell
+    """
+    def __init__(self, shell_svc, active_flag, *args):
+        """
+        Sets up members
+
+        :param shell_svc: The underlying Pelix shell service
+        :param active_flag: Common flag for stopping the client communication
+        """
+        self._shell = shell_svc
+        self._active = active_flag
+        socketserver.StreamRequestHandler.__init__(self, *args)
+
+
+    def send(self, data):
+        """
+        Tries to send data to the client.
+
+        :param data: Data to be sent
+        :return: True if the data was sent, False on error
+        """
+        if data is not None:
+            data = data.encode("UTF-8")
+
+        try:
+            self.wfile.write(data)
+            self.wfile.flush()
+            return True
+
+        except IOError:
+            # An error occurred, mask it
+            # -> This allows to handle the command even if the client has been
+            # disconnect (i.e. "echo stop 0 | nc localhost 9000")
+            return False
+
+
+    def handle(self):
+        """
+        Handles a TCP client
+        """
+        _logger.info("RemoteConsole client connected: [%s]:%d",
+                     self.client_address[0], self.client_address[1])
+
+        # Print the banner
+        ps1 = self._shell.get_ps1()
+        self.send(self._shell.get_banner())
+        self.send(ps1)
+
+        try:
+            while self._active.get_value():
+                # Wait for data
+                rlist = select([self.rfile], [], [], .5)[0]
+                if not rlist:
+                    # Nothing to do (poll timed out)
+                    continue
+
+                data = self.rfile.readline()
+                if not data:
+                    # End of stream (client gone)
+                    break
+
+                # Strip the line
+                line = data.strip()
+                if not data:
+                    # Empty line
+                    continue
+
+                # Execute it
+                try:
+                    self._shell.handle_line(self.rfile, self.wfile, line)
+
+                except KeyboardInterrupt:
+                    # Stop there on interruption
+                    self.send("\nInterruption received.")
+                    return
+
+                except IOError as ex:
+                    # I/O errors are fatal
+                    _logger.exception("Error communicating with a client: %s",
+                                      ex)
+                    break
+
+                except Exception as ex:
+                    # Other exceptions are not important
+                    import traceback
+                    self.send("\nError during last command: %s\n" % ex)
+                    self.send(traceback.format_exc())
+
+                # Print the prompt
+                self.send(ps1)
+
+        finally:
+            _logger.info("RemoteConsole client gone: [%s]:%d",
+                         self.client_address[0], self.client_address[1])
+
+            # Be polite
+            self.send("\nSession closed. Good bye.\n")
+            self.finish()
+
+# ------------------------------------------------------------------------------
+
+class ThreadingTCPServerFamily(socketserver.ThreadingTCPServer):
+    """
+    Threaded TCP Server handling different address families
+    """
+    def __init__(self, server_address, request_handler_class):
+        """
+        Sets up the TCP server. Doesn't bind nor activate it.
+        """
+        # Determine the address family
+        addr_info = socket.getaddrinfo(server_address[0], server_address[1],
+                                       0, 0, socket.SOL_TCP)
+
+        # Change the address family before the socket is created
+        # Get the family of the first possibility
+        self.address_family = addr_info[0][0]
+
+        # Call the super constructor
+        socketserver.ThreadingTCPServer.__init__(self, server_address,
+                                                 request_handler_class,
+                                                 False)
+
+        if self.address_family == socket.AF_INET6:
+            # Explicitly ask to be accessible both by IPv4 and IPv6
+            # Some versions of Python don't have V6ONLY.
+            # On Linux, IPC6_V6ONLY = 26
+            opt_ipv6_only = getattr(socket, "IPV6_V6ONLY", 26)
+
+            try:
+                self.socket.setsockopt(socket.IPPROTO_IPV6, opt_ipv6_only, 0)
+
+            except socket.error as ex:
+                # Log the error
+                _logger.exception("Couldn't set IP double stack flag: %s", ex)
+
+
+def _create_server(shell, server_address, port):
+    """
+    Creates the TCP console on the given address and port
+
+    :param shell: The remote shell handler
+    :param server_address: Server bound address
+    :param port: Server port
+    :return: server thread, TCP server object
+    """
+    # Set up the request handler creator
+    active_flag = SharedBoolean(True)
+    request_handler = lambda *args: RemoteConsole(shell, active_flag, *args)
+
+    # Set up the server
+    server = ThreadingTCPServerFamily((server_address, port), request_handler)
+
+    # Set flags
+    server.daemon_threads = True
+    server.allow_reuse_address = True
+
+    # Activate the server
+    server.server_bind()
+    server.server_activate()
+
+    # Serve clients
+    server_thread = threading.Thread(target=server.serve_forever,
+                                     name="RemoteShell-{0}".format(port))
+    server_thread.daemon = True
+    server_thread.start()
+
+    return (server_thread, server, active_flag)
+
+# ------------------------------------------------------------------------------
+
+@ComponentFactory(pelix.shell.FACTORY_REMOTE_SHELL)
+@Provides(pelix.shell.SERVICE_SHELL_REMOTE)
+@Requires("_shell", pelix.shell.SERVICE_SHELL)
+@Property("_address", "pelix.shell.address", "localhost")
+@Property("_port", "pelix.shell.port", 9000)
+class IPopoRemoteShell(object):
+    """
+    The iPOPO Remote Shell, based on the Pelix Shell
+    """
+    def __init__(self):
+        """
+        Sets up the component
+        """
+        # Component shell
+        self._shell = None
+        self._address = None
+        self._port = 0
+
+        # Internals
+        self._thread = None
+        self._server = None
+        self._server_flag = None
+
+
+    def get_access(self):
+        """
+        Implementation of the remote shell specification
+
+        :return: A (host, port) tuple
+        """
+        return (self._address, self._port)
+
+
+    def get_banner(self):
+        """
+        Retrieves the shell banner
+
+        :return: The shell banner
+        """
+        line = '-' * 72
+        shell_banner = self._shell.get_banner()
+
+        return "{lines}\n{shell_banner}\niPOPO Remote Shell\n{lines}\n" \
+            .format(lines=line, shell_banner=shell_banner)
+
+
+    def get_ps1(self):
+        """
+        Returns the shell prompt
+
+        :return: The shell prompt
+        """
+        return self._shell.get_ps1()
+
+
+    def handle_line(self, rfile, wfile, line):
+        """
+        Handles the command line.
+
+        **Does not catch exceptions !**
+
+        :param rfile: Input file-like object
+        :param wfile: Output file-like object
+        :param line: The command line
+        :return: The execution result (True on success, else False)
+        """
+        return self._shell.execute(line, rfile, wfile)
+
+
+    @Validate
+    def validate(self, context):
+        """
+        Component validation
+        """
+        if not self._address:
+            # Local host by default
+            self._address = "localhost"
+
+        try:
+            self._port = int(self._port)
+            if self._port < 0 or self._port > 65535:
+                # Invalid port value
+                self._port = 0
+
+        except (ValueError, TypeError):
+            # Invalid port string: use a random port
+            self._port = 0
+
+        # Start the TCP server
+        self._thread, self._server, self._server_flag = \
+                                _create_server(self, self._address, self._port)
+
+        # Property update (if port was 0)
+        self._port = self._server.socket.getsockname()[1]
+
+        _logger.info("RemoteShell validated on port: %d", self._port)
+
+
+    @Invalidate
+    def invalidate(self, context):
+        """
+        Component invalidation
+        """
+        # Stop the clients loops
+        self._server_flag.set_value(False)
+
+        # Shutdown the server
+        self._server.shutdown()
+        self._thread.join(2)
+
+        # Close the server socket (ignore errors)
+        self._server.server_close()
+
+        # Clean up
+        self._thread = None
+        self._server = None
+        self._server_flag = None
+
+        _logger.info("RemoteShell gone from port: %d", self._port)
---
+++ packages/iPOPO/iPOPO-0.5.6.tar.gz/iPOPO-0.5.6/pelix/threadpool.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python
+# -- Content-Encoding: UTF-8 --
+"""
+Pelix Utilities: Task pool
+
+:author: Thomas Calmant
+:copyright: Copyright 2014, isandlaTech
+:license: Apache License 2.0
+:version: 0.5.6
+:status: Beta
+
+..
+
+    Copyright 2014 isandlaTech
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+# Documentation strings format
+__docformat__ = "restructuredtext en"
+
+# Module version
+__version_info__ = (0, 5, 6)
+__version__ = ".".join(str(x) for x in __version_info__)
+
+# ------------------------------------------------------------------------------
+
+# Standard library
+import logging
+import threading
+
+try:
+    # Python 3
+    import queue
+
+except ImportError:
+    # Python 2
+    import Queue as queue
+
+# ------------------------------------------------------------------------------
+
+class FutureResult(object):
+    """
+    An object to wait for the result of a threaded execution
+    """
+    def __init__(self):
+        """
+        Sets up the FutureResult object
+        """
+        self._done_event = threading.Event()
+        self._result = None
+
+
+    def execute(self, method, args, kwargs):
+        """
+        Execute the given method and stores its result.
+        The result is considered "done" even if the method raises an exception
+
+        :param method: The method to execute
+        :param args: Method positional arguments
+        :param kwargs: Method keyword arguments
+        :raise Exception: The exception raised by the method
+        """
+        # Normalize arguments
+        if args is None:
+            args = []
+
+        if kwargs is None:
+            kwargs = {}
+
+        try:
+            # Call the method
+            self._result = method(*args, **kwargs)
+
+        finally:
+            # Mark the action as executed
+            self._done_event.set()
+
+
+    def done(self):
+        """
+        Returns True if the job has finished, else False
+        """
+        return self._done_event.is_set()
+
+
+    def result(self, timeout=None):
+        """
+        Waits up to timeout for the result the threaded job.
+        Returns immediately the result if the job has already been done.
+
+        :param timeout: The maximum time to wait for a result (in seconds)
+        :raise OSError: The timeout raised before the job finished
+        """
+        if self._done_event.wait(timeout) or self._done_event.is_set():
+            return self._result
+
+        raise OSError("Timeout raised")
+
+# ------------------------------------------------------------------------------
+
+class ThreadPool(object):
+    """
+    Executes the tasks stored in a FIFO in a thread pool
+    """
+    def __init__(self, nb_threads, queue_size=0, timeout=5, logname=None):
+        """
+        Sets up the task executor
+
+        :param nb_threads: Size of the thread pool
+        :param queue_size: Size of the task queue (0 for infinite)
+        :param timeout: Queue timeout (in seconds)
+        :param logname: Name of the logger
+        :raise ValueError: Invalid number of threads
+        """
+        # Validate parameters
+        if type(nb_threads) is not int or nb_threads < 1:
+            raise ValueError("Invalid pool size: {0}".format(nb_threads))
+
+        # The logger
+        self.__name = logname or __name__
+        self._logger = logging.getLogger(self.__name)
+
+        # The loop control event
+        self._done_event = threading.Event()
+        self._done_event.set()
+
+        # The task queue
+        try:
+            queue_size = int(queue_size)
+
+        except (TypeError, ValueError):
+            # Not a valid integer
+            queue_size = 0
+
+        self._queue = queue.Queue(queue_size)
+        self._timeout = timeout
+        self.__lock = threading.Lock()
+
+        # The thread pool
+        self._nb_threads = nb_threads
+        self._threads = []
+
+
+    def start(self):
+        """
+        Starts the thread pool. Does nothing if the pool is already started.
+        """
+        if not self._done_event.is_set():
+            # Stop event not set: we're running
+            return
+
+        # Clear the stop event
+        self._done_event.clear()
+
+        # Create the threads
+        i = 0
+        while i < self._nb_threads:
+            i += 1
+            thread = threading.Thread(target=self.__run,
+                                      name="{0}-{1}".format(self.__name, i))
+            self._threads.append(thread)
+
+        # Start'em
+        for thread in self._threads:
+            thread.start()
+
+
+    def stop(self):
+        """
+        Stops the thread pool. Does nothing if the pool is already stopped.
+        """
+        if self._done_event.is_set():
+            # Stop event set: we're stopped
+            return
+
+        # Set the stop event
+        self._done_event.set()
+
+        with self.__lock:
+            # Add something in the queue (to unlock the join())
+            try:
+                for _ in self._threads:
+                    self._queue.put(self._done_event, True, self._timeout)
+
+            except queue.Full:
+                # There is already something in the queue
+                pass
+
+            # Join threads
+            for thread in self._threads:
+                thread.join()
+
+        # Clear storage
+        del self._threads[:]
+        self.clear()
+
+
+    def enqueue(self, method, *args, **kwargs):
+        """
+        Enqueues a task in the pool
+
+        :param method: Method to call
+        :return: A FutureResult object, to get the result of the task
+        :raise ValueError: Invalid method
+        :raise Full: The task queue is full
+        """
+        if not hasattr(method, '__call__'):
+            raise ValueError("{0} has no __call__ member." \
+                             .format(method.__name__))
+
+        # Prepare the future result object
+        future = FutureResult()
+
+        # Use a lock, as we might be "resetting" the queue
+        with self.__lock:
+            # Add the task to the queue
+            self._queue.put((method, args, kwargs, future), True, self._timeout)
+
+        return future
+
+
+    def clear(self):
+        """
+        Empties the current queue content.
+        Returns once the queue have been emptied.
+        """
+        with self.__lock:
+            # Empty the current queue
+            try:
+                while True:
+                    self._queue.get_nowait()
+                    self._queue.task_done()
+
+            except queue.Empty:
+                # Queue is now empty
+                pass
+
+            # Wait for the tasks currently executed
+            self.join()
+
+
+    def join(self, timeout=None):
+        """
+        Waits for all the tasks to be executed
+
+        :param timeout: Maximum time to wait (in seconds)
+        :return: True if the queue has been emptied, else False
+        """
+        if self._queue.empty():
+            # Nothing to wait for...
+            return True
+
+        elif timeout is None:
+            # Use the original join
+            self._queue.join()
+            return True
+
+        else:
+            # Wait for the condition
+            with self._queue.all_tasks_done:
+                self._queue.all_tasks_done.wait(timeout)
+                return self._queue.empty()
+
+
+    def __run(self):
+        """
+        The main loop
+        """
+        while not self._done_event.is_set():
+            try:
+                # Wait for an action (blocking)
+                task = self._queue.get(True, self._timeout)
+                if task is self._done_event:
+                    # Stop event in the queue: get out
+                    return
+
+            except queue.Empty:
+                # Nothing to do
+                pass
+
+            else:
+                # Extract elements
+                method, args, kwargs, future = task
+                try:
+                    # Call the method
+                    future.execute(method, args, kwargs)
+
+                except Exception as ex:
+                    self._logger.exception("Error executing %s: %s",
+                                           method.__name__, ex)
+
+                finally:
+                    # Mark the action as executed
+                    self._queue.task_done()
