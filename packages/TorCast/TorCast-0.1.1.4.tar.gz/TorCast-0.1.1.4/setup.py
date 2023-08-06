b5705e825a84461960b9648305c38b41a76adbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:13 +0000

    Add package TorCast/0.1.1.4/TorCast-0.1.1.4.tar.gz

---
+++ packages/TorCast/TorCast-0.1.1.4.tar.gz/TorCast-0.1.1.4/setup.py
@@ -0,0 +1,17 @@
+#coding=utf8
+from setuptools import setup
+
+__author__ = 'alex'
+
+setup(
+    name='TorCast',
+    version='0.1.1.4',
+    packages=['TorCast'],
+    author='Alexander.Li',
+    author_email='superpowerlee@gmail.com',
+    license='LGPL',
+    install_requires=["tornado>=2.4.1",],
+    description="Broadcast messages to all tornado process subcribed on Redis asynchronously Or Block on Redis Queue asynchronously",
+    keywords ='tornado asynchronous redis message',
+    url="https://github.com/ipconfiger/TorCast"
+)
