170a19df629f8b147d71650444e5760544d4bd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:18 +0000

    Add package pypsdier/0.1.1/Pypsdier-0.1.1.tar.gz

---
+++ packages/pypsdier/Pypsdier-0.1.1.tar.gz/Pypsdier-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Pypsdier',
+    version='0.1.1',
+    author='Sebastian Flores',
+    author_email='sebastiandres@gmail.com',
+    packages=['pypsdier'],
+    scripts=[],
+    url='http://www.bitbucket.com/sebastiandres/pypsdier',
+    license='BSD',
+    description='Solver for general Reaction-Diffusion equations.',
+    long_description=open('README.txt').read(),
+)
