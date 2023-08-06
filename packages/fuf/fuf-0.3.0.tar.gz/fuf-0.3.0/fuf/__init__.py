7542def47697d192badfeb0654572198200bfb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:06 +0000

    Add package fuf/0.3.0/fuf-0.3.0.tar.gz

---
+++ packages/fuf/fuf-0.3.0.tar.gz/fuf-0.3.0/fuf/__init__.py
@@ -0,0 +1,7 @@
+from .wrapper import wrapper, fdup
+from .pat import (
+        OverloadSet, Overload,
+        _, Anything, lt, le, gt, ge, eq, ne, Between,
+        Or, And, Not, Is, Exists
+)
+from .action import ActionSet
---
+++ packages/fuf/fuf-0.3.0.tar.gz/fuf-0.3.0/fuf/wrapper.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+Action Set class and demonstration
+Matt Soucy
+
+The main purpose of this file is to demonstrate some of the fun things
+that can be done with functions in Python.
+
+Particularly interesting are:
+- `fdup`: Duplicates a function perfectly down to the signature
+   that is stored internally and printed with using help()
+- `wrapper`: Wrapper wrapper - apply as a decorator to a decorator to produce
+   a decorator that generates perfect-forwarded functions
+
+Ideas taken from:
+- http://numericalrecipes.wordpress.com/2009/05/25/signature-preserving-function-decorators/
+"""
+import inspect  # Used to create our duplicate
+from functools import update_wrapper  # Convenience to update the metadata
+from .interop import *
+
+
+def wrapper(_wrap_):
+    '''Wrap a decorator with support for the perfect wrapper decorator'''
+    def wrapper(_func_):
+        '''Create a perfect wrapper (including signature) around a function'''
+        # convert bar(f)(*args, **kwargs)
+        # into    f(*args, **kwargs)
+        src = r'def _newfunc{0}: return _wrap_(func){0}'.format(
+            inspect.formatargspec(*inspect.getargspec(_func_))
+        )
+        evaldict = {'_wrap_': _wrap_, 'func': _func_}
+        exec_(src, evaldict)
+        ret = evaldict['_newfunc']
+        update_wrapper(ret, _func_)
+        ret.__wrapped__ = _func_
+        return ret
+    return wrapper
+
+fdup = wrapper((lambda func:(lambda *_a, **_kw: func(*_a, **_kw))))
