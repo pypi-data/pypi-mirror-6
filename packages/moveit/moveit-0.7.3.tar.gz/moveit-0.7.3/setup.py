5c10cee1e712b406be40b0fc0ccc530a043de2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:59 +0000

    Add package moveit/0.7.3/moveit-0.7.3.tar.gz

---
+++ packages/moveit/moveit-0.7.3.tar.gz/moveit-0.7.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+setup(
+    name='moveit',
+    version='0.7.3',
+    description='ns_server master events analyzer',
+    author='Pavel Paulau',
+    author_email='pavel.paulau@gmail.com',
+    packages=[
+        'moveit',
+    ],
+    entry_points={
+        'console_scripts': [
+            'flow = moveit.flow:main',
+            'moveit = moveit.moveit:main',
+        ]
+    },
+    install_requires=[
+        'svgwrite',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
