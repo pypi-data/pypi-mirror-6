e0ce401139be853dd76e93fdf3a302a9f3b3af10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:00 +0000

    Add package lensfunpy/0.11.0/lensfunpy-0.11.0.tar.gz

---
+++ packages/lensfunpy/lensfunpy-0.11.0.tar.gz/lensfunpy-0.11.0/test/tests.py
@@ -0,0 +1,72 @@
+from __future__ import division
+
+import numpy as np
+import lensfun
+import gc
+
+# the following string were taken from the lensfun xml files
+camMaker = 'NIKON CORPORATION'
+camModel = 'NIKON D3S'
+lensMaker = 'Nikon'
+lensModel = 'Nikkor 28mm f/2.8D AF'
+
+def testDatabaseLoading():
+    db = lensfun.Database()
+       
+    cams = db.findCameras(camMaker, camModel)
+    assert len(cams) == 1
+    cam = cams[0]
+    assert cam.Maker.lower() == camMaker.lower()
+    assert cam.Model.lower() == camModel.lower()
+    
+    lenses = db.findLenses(cam, lensMaker, lensModel)
+    assert len(lenses) == 1
+    lens = lenses[0]
+    assert lens.Maker.lower() == lensMaker.lower()
+    assert lens.Model.lower() == lensModel.lower()
+    
+def testModifier():
+    db = lensfun.Database()
+    cam = db.findCameras(camMaker, camModel)[0]
+    lens = db.findLenses(cam, lensMaker, lensModel)[0]
+    
+    focalLength = 28.0
+    aperture = 1.4
+    distance = 10
+    width = 4256
+    height = 2832
+    
+    mod = lensfun.Modifier(lens, cam.CropFactor, width, height)
+    mod.initialize(focalLength, aperture, distance)
+    
+    undistCoords = mod.applyGeometryDistortion()
+    assert undistCoords.shape[0] == height and undistCoords.shape[1] == width
+    
+    # check if coordinates were actually transformed
+    y, x = np.mgrid[0:undistCoords.shape[0], 0:undistCoords.shape[1]]
+    coords = np.dstack((x,y))
+    assert np.any(undistCoords != coords)
+    
+    undistCoords = mod.applySubpixelDistortion()
+    assert undistCoords.shape[0] == height and undistCoords.shape[1] == width
+    assert np.any(undistCoords[:,:,0] != coords)
+    
+    undistCoords = mod.applySubpixelGeometryDistortion()
+    assert undistCoords.shape[0] == height and undistCoords.shape[1] == width
+    assert np.any(undistCoords[:,:,0] != coords)
+    
+def testDeallocationBug():
+    db = lensfun.Database()
+    cam = db.findCameras(camMaker, camModel)[0]
+    lens = db.findLenses(cam, lensMaker, lensModel)[0]
+    
+    # By garbage collecting the database object, its queried objects
+    # were deallocated as well, which is not what we want.
+    # Now, all queried objects hold a reference to the Database object
+    # they came from. This way, the Database object is only deallocated
+    # when all queried objects were garbage collected.
+    del db
+    gc.collect()
+    
+    assert cam.Maker.lower() == camMaker.lower()
+    assert lens.Maker.lower() == lensMaker.lower()
