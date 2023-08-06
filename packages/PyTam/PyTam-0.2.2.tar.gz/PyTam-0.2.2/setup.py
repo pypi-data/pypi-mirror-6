d258853be75fa0792bef5c63e4e047f1c0cd29b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:01 +0000

    Add package PyTam/0.2.2/PyTam-0.2.2.tar.gz

---
+++ packages/PyTam/PyTam-0.2.2.tar.gz/PyTam-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='PyTam',
+    author="Camilo A. Ospina A.",
+    author_email='camilo.ospinaa@gmail.com',
+    version='0.2.2',
+    packages=['pytam',],
+    license='MIT license',
+    platforms=['Linux','Mac','Windows'],
+    long_description=open('README.txt').read(),
+    description="Python Tables Manager (PyTam) is a module that allows managing data as a DBMS in Tables.",
+)
