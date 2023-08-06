60bcb86c798d63e9bf35bd5d3dac62dd192b01b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:35 +0000

    Add package jos/0.1.2/jos-0.1.2.tar.gz

---
+++ packages/jos/jos-0.1.2.tar.gz/jos-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+from jos import __version__
+
+
+with open('README.rst') as long_description_file:
+    long_description = long_description_file.read()
+
+
+setup(
+    name='jos',
+    version=__version__,
+    url='https://github.com/verycb/jos',
+    author='VeryCB',
+    author_email='imcaibin@gmail.com',
+    description='Jingdong API Client',
+    long_description=long_description,
+    license='MIT',
+    keywords = ('jingdong', 'jos'),
+    packages = find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.7",
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    install_requires=[
+        "requests >= 1.0",
+    ],
+)
