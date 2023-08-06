6e8364fae6833debbe3980cf14ce4514c992e12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:00 +0000

    Add package docker-sync/1.0.0/docker-sync-1.0.0.tar.gz

---
+++ packages/docker-sync/docker-sync-1.0.0.tar.gz/docker-sync-1.0.0/docker_sync/lib/Image.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+class Image(object):
+    def __init__(self, _id):
+        super(Image, self).__init__()
+
+        self._id = _id
+        self._tags = set()
+        self.entrypoint = None
+        self.command = None
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def tags(self):
+        return self._tags
+    
+    @tags.setter
+    def tags(self, tags):
+        self._tags = set(tags)
+    
+    def __str__(self):
+        return "<Image %s [%r]>" % (self.id, self.tags)
+    
+    __repr__ = __str__
