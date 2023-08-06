bed076dbca269a352b5e415a92643636c63d0a4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:51 +0000

    Add package Chinese-RFID-Access-Control-Library/0.0.3/Chinese-RFID-Access-Control-Library-0.0.3.zip

---
+++ packages/Chinese-RFID-Access-Control-Library/Chinese-RFID-Access-Control-Library-0.0.3.zip/Chinese-RFID-Access-Control-Library-0.0.3/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='Chinese-RFID-Access-Control-Library',
+    version='0.0.3',
+    description='A library for interfacing with one of the most common RFID Access Control System sold in China.',
+    long_description=readme,
+    author='Paul Brown',
+    author_email='paul90brown@gmail.com',
+    url='https://github.com/pawl/Chinese-RFID-Access-Control-Library',
+    license=license,
+	download_url = ['https://github.com/pawl/Chinese-RFID-Access-Control-Library/tarball/master#egg=package-0.0.3'],
+	keywords = ['rfid', 'access control'],
+    py_modules = ['rfid']
+)
+
