d3f84192da0d8eea5ec0a7a211904bf4e509a590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:48 +0000

    Add package cloudcast/0.0.4/cloudcast-0.0.4.tar.gz

---
+++ packages/cloudcast/cloudcast-0.0.4.tar.gz/cloudcast-0.0.4/setup.py
@@ -0,0 +1,32 @@
+'''
+@author: David Losada Carballo <david@tuxpiper.com>
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "cloudcast",
+    version = "0.0.4",
+    packages = find_packages(),
+    package_data = {
+        # Script files that contain initial bootstrap sequences
+        'cloudcast.iscm': ['scripts/*']
+    },
+
+    description = ("Easy and powerful stack templates for AWS CloudFormation"),
+    author = "David Losada Carballo",
+    author_email = "david@tuxpiper.com",
+    license = 'MIT',
+    keywords = "aws internet cloud cloudformation deployment automation",
+    long_description = open('README.md').read(),
+    url = "http://github.com/tuxpiper/cloudcast",
+    zip_safe = False,
+    classifiers=[
+       "Development Status :: 1 - Planning",
+       "Topic :: System",
+       "Environment :: Console",
+       "Intended Audience :: System Administrators",
+       "License :: OSI Approved :: MIT License",
+       "Programming Language :: Python"
+    ], 
+)
