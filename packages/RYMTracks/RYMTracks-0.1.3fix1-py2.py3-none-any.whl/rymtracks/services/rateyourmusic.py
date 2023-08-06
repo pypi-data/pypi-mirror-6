e2acbb6bf2e56a69c62c56275294e06b1a60869c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:32 +0000

    Add package RYMTracks/0.1.3fix1/RYMTracks-0.1.3fix1-py2.py3-none-any.whl

---
+++ packages/RYMTracks/RYMTracks-0.1.3fix1-py2.py3-none-any.whl/rymtracks/services/rateyourmusic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains Service implementation of RateYourMusic.
+http://rateyourmusic.com
+"""
+
+
+from . import SchemaOrgService
+
+
+##############################################################################
+
+
+class RateYourMusic(SchemaOrgService):
+    """
+    Implementation of Service which is intended to parse RateYourMusic.
+    Yes, because I can.
+    """
+    pass
