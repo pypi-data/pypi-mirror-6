b7bea1b6a60dbc7d7c2a956dde8d3381fd3b47f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:08 +0000

    Add package arnold/0.3.6/arnold-0.3.6.tar.gz

---
+++ packages/arnold/arnold-0.3.6.tar.gz/arnold-0.3.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+install_requires = ['peewee', 'termcolor']
+
+try:
+    import importlib
+except ImportError:
+    install_requires.append('importlib')
+
+setup(
+    name='arnold',
+    version='0.3.6',
+    description='Simple migrations for python ORMs',
+    long_description='',
+    keywords='python, peewee, migrations',
+    author='Cameron A. Stitt',
+    author_email='cameron@castitt.com',
+    url='https://github.com/cam-stitt/arnold',
+    license='BSD',
+    packages=['arnold'],
+    zip_safe=False,
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
