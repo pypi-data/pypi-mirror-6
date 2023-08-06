d551a3a0ce1488dd5a146b4c23a09c80acd07634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:28 +0000

    Add package hgcanttype/1.0.1/hgcanttype-1.0.1.tar.gz

---
+++ packages/hgcanttype/hgcanttype-1.0.1.tar.gz/hgcanttype-1.0.1/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+requires = []
+try:
+    import mercurial
+except ImportError:
+    requires.append('mercurial')
+
+setup(
+    name='hgcanttype',
+    version='1.0.1',
+    author='Nathan Hoad',
+    author_email='nathan@getoffmalawn.com',
+    maintainer='Nathan Hoad',
+    maintainer_email='nathan@getoffmalawn.com',
+    url='http://bitbucket.org/getoffmalawn/hgcanttype',
+    description='',
+    long_description=open('README').read(),
+    keywords='hg mercurial',
+    license='GPLv2+',
+    py_modules=['hgcanttype'],
+    install_requires=requires,
+)
+
