3f9baec5a55612351bf9c40d86e16e81a37d37ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:22 +0000

    Add package infi.git_mirror/0.0.8/infi.git_mirror-0.0.8.tar.gz

---
+++ packages/infi.git_mirror/infi.git_mirror-0.0.8.tar.gz/infi.git_mirror-0.0.8/src/infi/git_mirror/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.8"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.0.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '0877a332a13f8ca574a0a83826e150591ea0e0cc'
+__git_head_subject__ = 'Finished release v0.0.8.'
+__git_head_message__ = '* release/v0.0.8:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements'
+__git_dirty_diff__ = ''
