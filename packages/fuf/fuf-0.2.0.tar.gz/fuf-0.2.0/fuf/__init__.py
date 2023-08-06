9b42157d852d0c8d215b7916dcfb9e2846945b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:24 +0000

    Add package fuf/0.2.0/fuf-0.2.0.tar.gz

---
+++ packages/fuf/fuf-0.2.0.tar.gz/fuf-0.2.0/fuf/__init__.py
@@ -0,0 +1,7 @@
+from .wrapper import wrapper, fdup
+from .pat import (
+        OverloadSet, Overload,
+        _, Anything, lt, le, gt, ge, eq, ne, between,
+        Or, And, Not, Is, Exists
+)
+from .action import ActionSet
