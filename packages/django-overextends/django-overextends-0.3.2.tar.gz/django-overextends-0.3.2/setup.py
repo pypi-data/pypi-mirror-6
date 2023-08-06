3c51e72e7a204b543cc12e74d4708c6d27cce748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:45 +0000

    Add package django-overextends/0.3.2/django-overextends-0.3.2.tar.gz

---
+++ packages/django-overextends/django-overextends-0.3.2.tar.gz/django-overextends-0.3.2/setup.py
@@ -0,0 +1,36 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "django-overextends",
+    version = __import__("overextends").__version__,
+    author = "Stephen McDonald",
+    author_email = "stephen.mc@gmail.com",
+    description = ("A Django reusable app providing the ability to use "
+                   "circular template inheritance."),
+    long_description = open("README.rst").read(),
+    url = "http://github.com/stephenmcd/django-overextends",
+    zip_safe = False,
+    include_package_data = True,
+    packages = find_packages(),
+    install_requires = [
+        "sphinx-me >= 0.1.2",
+        "django >= 1.4",
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: Site Management",
+    ]
+)
