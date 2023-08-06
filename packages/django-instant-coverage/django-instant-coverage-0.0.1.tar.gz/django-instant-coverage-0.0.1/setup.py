6641d9fe58824f234f10607f391f0fcd3da8cb8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:34 +0000

    Add package django-instant-coverage/0.0.1/django-instant-coverage-0.0.1.tar.gz

---
+++ packages/django-instant-coverage/django-instant-coverage-0.0.1.tar.gz/django-instant-coverage-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "instant_coverage.tests.settings")
+
+setup(
+    name='django-instant-coverage',
+    description='Better-than-nothing testing for Django',
+    url='https://github.com/colons/instant-coverage',
+    version='0.0.1',
+    platforms=['any'],
+    packages=['instant_coverage'],
+    install_requires=[
+        'Django',
+        'mock',
+    ],
+    tests_require=['nose'],
+    test_suite='nose.collector',
+)
