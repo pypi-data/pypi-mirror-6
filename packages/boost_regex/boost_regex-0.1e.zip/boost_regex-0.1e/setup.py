a9c03804e9c0bd24925dfc79e3acdc8552e0d783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:35 +0000

    Add package boost_regex/0.1e/boost_regex-0.1e.zip

---
+++ packages/boost_regex/boost_regex-0.1e.zip/boost_regex-0.1e/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Extension
+
+
+
+setup(
+    name='boost_regex',
+    version='0.1e',
+    ext_modules=[
+        Extension('boost_regex',
+                  ['boost_regex.cpp'],
+                  include_dirs=['/usr/include', '\\usr\\include', 'C:\\local\\boost_1_55_0'],
+                  library_dirs=['C:\\local\\boost_1_55_0\\lib64-msvc-9.0', '\\usr\\lib']
+        )
+    ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author='Bart Jellema',
+    author_email='bart@zeromail.com',
+    url='http://pypi.python.org/pypi/boost_regex/',
+    description='Very basic interface to the boost regex library.'
+)
