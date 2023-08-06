e0ed24784aa4e77b47593aa03facea96d533609b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:29 +0000

    Add package bugzilla2fedmsg/0.1.2/bugzilla2fedmsg-0.1.2.tar.gz

---
+++ packages/bugzilla2fedmsg/bugzilla2fedmsg-0.1.2.tar.gz/bugzilla2fedmsg-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='bugzilla2fedmsg',
+    version='0.1.2',
+    description='Consume BZ messages over STOMP and republish to fedmsg',
+    author='Ralph Bean',
+    author_email='rbean@redhat.com',
+    url='https://github.com/fedora-infra/bugzilla2fedmsg',
+    install_requires=[
+        "fedmsg",
+        "python-bugzilla",
+        "moksha.hub",
+        "stomper",
+    ],
+    packages=[],
+    py_modules=['bugzilla2fedmsg'],
+    entry_points="""
+    [moksha.consumer]
+    bugzilla2fedmsg = bugzilla2fedmsg:BugzillaConsumer
+    """,
+)
