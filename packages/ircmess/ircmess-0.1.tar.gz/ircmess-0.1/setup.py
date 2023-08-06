79d5a802cce28100fbd1771ab6d7a8396ac0ee58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:11 +0000

    Add package ircmess/0.1/ircmess-0.1.tar.gz

---
+++ packages/ircmess/ircmess-0.1.tar.gz/ircmess-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ircmess',
+      version='0.1',
+      description='Simple and fast IRC line parser',
+      url='https://github.com/lyska/ircmess',
+      author='Sam Dodrill',
+      author_email='shadow.h511@gmail.com',
+      license='Zlib',
+      packages=['ircmess'],
+      zip_safe=False)
+
