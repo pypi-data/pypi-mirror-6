4b3fe9a6f84b6b0ed01fe2744ce1ad08749eab15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:33 +0000

    Add package conflib/0.1.6/conflib-0.1.6.tar.gz

---
+++ packages/conflib/conflib-0.1.6.tar.gz/conflib-0.1.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='conflib',
+    version='0.1.6',
+    description='Manage configuration hierarchies',
+    author='Les Aker',
+    author_email='me@lesaker.org',
+    url='https://github.com/akerl/conflib',
+    license='MIT License',
+    packages=['conflib'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
