16f021d4efb83c2069409448e2038c085f4f9e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:09 +0000

    Add package ipython-cluster-helper/0.2.18/ipython-cluster-helper-0.2.18.tar.gz

---
+++ packages/ipython-cluster-helper/ipython-cluster-helper-0.2.18.tar.gz/ipython-cluster-helper-0.2.18/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name = "ipython-cluster-helper",
+      version = "0.2.18",
+      author = "Rory Kirchner",
+      author_email = "rory.kirchner@gmail.com",
+      description = "Simplify IPython cluster start up and use for multiple schedulers.",
+      license = "MIT",
+      zip_safe=False,
+      url = "https://github.com/roryk/ipython-cluster-helper",
+      packages = find_packages(),
+      install_requires = [
+          "pyzmq >= 2.1.11",
+          "ipython >= 1.1.0"])
