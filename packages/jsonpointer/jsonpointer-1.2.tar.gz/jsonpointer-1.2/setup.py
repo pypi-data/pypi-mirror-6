f2fbd692234662ad79071c8156e169509ddb8c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:26 +0000

    Add package jsonpointer/1.2/jsonpointer-1.2.tar.gz

---
+++ packages/jsonpointer/jsonpointer-1.2.tar.gz/jsonpointer-1.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import re
+import os.path
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(dirname, 'jsonpointer.py')
+src = open(filename).read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", src))
+docstrings = re.findall('"""(.*)"""', src)
+
+PACKAGE = 'jsonpointer'
+
+MODULES = (
+        'jsonpointer',
+)
+
+AUTHOR_EMAIL = metadata['author']
+VERSION = metadata['version']
+WEBSITE = metadata['website']
+LICENSE = metadata['license']
+DESCRIPTION = docstrings[0]
+
+# Extract name and e-mail ("Firstname Lastname <mail@example.org>")
+AUTHOR, EMAIL = re.match(r'(.*) <(.*)>', AUTHOR_EMAIL).groups()
+
+setup(name=PACKAGE,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=EMAIL,
+      license=LICENSE,
+      url=WEBSITE,
+      py_modules=MODULES,
+      scripts=['bin/jsonpointer'],
+      entry_points = {
+        'console_scripts': [
+            'jsonpointer = jsonpointer:main',
+      ]},
+)
