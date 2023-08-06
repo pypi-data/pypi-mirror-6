ffb20f3730166544676426a8328472f8d0ee2dd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:30 +0000

    Add package bernoulli/0.1.4/bernoulli-0.1.4.tar.gz

---
+++ packages/bernoulli/bernoulli-0.1.4.tar.gz/bernoulli-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='bernoulli',
+      version='0.1.4',
+      description='A Python API for Bernoulli',
+      url='https://github.com/bernoulli-metrics/bernoulli-python',
+      author='Joe Gasiorek',
+      author_email='joe.gasiorek@gmail.com',
+      license='MIT',
+      packages=['bernoulli'],
+      install_requires=[
+          'requests',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False
+      )
