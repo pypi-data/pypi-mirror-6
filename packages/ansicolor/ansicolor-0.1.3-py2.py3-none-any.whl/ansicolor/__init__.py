a2562f014a33404b1e52082443aab054d2a6db7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:30 +0000

    Add package ansicolor/0.1.3/ansicolor-0.1.3-py2.py3-none-any.whl

---
+++ packages/ansicolor/ansicolor-0.1.3-py2.py3-none-any.whl/ansicolor/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from ansicolor.ansicolor import *  # noqa
+
+
+__major_version__ = "0.1"
+__release__ = "3"
+__version__ = "%s.%s" % (__major_version__, __release__)
