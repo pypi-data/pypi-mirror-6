c869a1dd223dc40d99d6544cc838487b5c317ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package plone.app.contenttypes/1.1b1/plone.app.contenttypes-1.1b1.zip

---
+++ packages/plone.app.contenttypes/plone.app.contenttypes-1.1b1.zip/plone.app.contenttypes-1.1b1/plone/app/contenttypes/migration/vocabularies.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+from zope.interface import implements
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import IVocabularyFactory
+
+from Products.CMFCore.utils import getToolByName
+from plone.app.contenttypes.migration.utils import ATCT_LIST
+from plone.app.contenttypes.migration.utils import isSchemaExtended
+from .. import _
+
+
+def get_terms(context, counter, ext_dict, show_extended):
+    results = []
+    for k, v in counter.iteritems():
+        if not show_extended:
+            if k not in ext_dict:
+                display = u"{0} ({1})".format(context.translate(_(k)), v)
+                term = SimpleVocabulary.createTerm(k, k, display)
+                results.append(term)
+        else:
+            if k in ext_dict:
+                ext = str(ext_dict[k]['fields']).\
+                    replace("[", "").replace("]", "")
+                display = u"{0} ({1}) - extended fields: {2}".\
+                    format(context.translate(_(k)), v, ext)
+                term = SimpleVocabulary.createTerm(k, k, display)
+                results.append(term)
+    results.sort(key=lambda x: x.title)
+    return results
+
+
+def count(brains):
+    counter = {}
+    for i in brains:
+        pt = i.portal_type
+        if "Blob" in i.meta_type:
+            if pt == "File":
+                pt = "BlobFile"
+            else:
+                pt = "BlobImage"
+        if not counter.get(pt):
+            counter[pt] = 0
+        counter[pt] += 1
+    return counter
+
+
+def results(context, show_extended=False):
+    """Helper method to create the vocabularies used below.
+    """
+    ext_dict = {}
+    ifaces = []
+    for k, v in ATCT_LIST.items():
+        iface = v['iface'].__identifier__
+        extendend_fields = isSchemaExtended(v['iface'])
+        expected = v['extended_fields']
+        is_extended = len(extendend_fields) > len(expected)
+        if is_extended and show_extended:
+            ifaces.append(iface)
+            ext_dict[k] = {}
+            if expected:
+                extendend_fields.remove(expected[0])
+            ext_dict[k]['fields'] = extendend_fields
+
+        elif not show_extended and not is_extended:
+            ifaces.append(iface)
+    catalog = getToolByName(context, "portal_catalog")
+    brains = catalog.search({'object_provides': ifaces})
+
+    counter = count(brains)
+
+    return SimpleVocabulary(get_terms(context,
+                                      counter,
+                                      ext_dict,
+                                      show_extended))
+
+
+class ATCTypesVocabulary(object):
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        """Return a vocabulary with standard content types
+        and, for each one, the number of occurrences.
+        """
+        return results(context, show_extended=False)
+
+
+class ExtendedTypesVocabulary(object):
+    implements(IVocabularyFactory)
+
+    def __call__(self, context):
+        """Return a vocabulary with all extended types
+        and for each the number of occurences and a list of the
+        extended fields.
+        """
+        return results(context, show_extended=True)
---
+++ packages/plone.app.contenttypes/plone.app.contenttypes-1.1b1.zip/plone.app.contenttypes-1.1b1/plone/app/contenttypes/upgrades.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+from plone.dexterity.interfaces import IDexterityFTI
+from zope.component import queryUtility
+
+
+def update_fti(context):
+    """ Schema-files moved into their own folder after 1.0b1
+    """
+    # Document
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Document'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:document.xml"
+    # File
+    fti = queryUtility(
+        IDexterityFTI,
+        name='File'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:file.xml"
+    # Folder
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Folder'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:folder.xml"
+    # Image
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Image'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:image.xml"
+    # Link
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Link'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:link.xml"
+    # News Item
+    fti = queryUtility(
+        IDexterityFTI,
+        name='News Item'
+    )
+    fti.model_file = "plone.app.contenttypes.schema:news_item.xml"
+
+
+def enable_collection_behavior(context):
+    """Enable collection behavior on Collection.
+    """
+    # Document
+    fti = queryUtility(
+        IDexterityFTI,
+        name='Collection'
+    )
+    behavior = 'plone.app.contenttypes.behaviors.collection.ICollection'
+    if behavior in fti.behaviors:
+        return
+    new = list(fti.behaviors)
+    new.append(behavior)
+    fti.behaviors = tuple(new)
+    if fti.schema == 'plone.app.contenttypes.interfaces.ICollection':
+        fti.schema = None
