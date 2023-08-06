79d6bc4a75875c98c8a5f9898d5f71ecc9cf3f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:17 +0000

    Add package django-ptree/0.2.57/django-ptree-0.2.57.zip

---
+++ packages/django-ptree/django-ptree-0.2.57.zip/django-ptree-0.2.57/ptree/management/commands/create_session.py
@@ -0,0 +1,27 @@
+from django.utils.importlib import import_module
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+import os.path
+
+def directory_name(path):
+    return os.path.basename(os.path.normpath(path))
+
+class Command(BaseCommand):
+    help = "pTree: Create a session."
+    args = '[name]'
+
+    def handle(self, *args, **options):
+        print 'Creating session...'
+        if len(args) > 1:
+            raise CommandError("Wrong number of arguments (expecting '{}')".format(self.args))
+
+        if len(args) == 1:
+            name = args[0]
+        else:
+            name = None
+
+
+        session_module = import_module('{}.{}'.format(directory_name(settings.BASE_DIR), 'session'))
+        session_module.create(name)
+
+
