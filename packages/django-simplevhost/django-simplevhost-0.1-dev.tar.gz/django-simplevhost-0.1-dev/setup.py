81a431082858a6b66a12b14a14c188dc962ddcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:37 +0000

    Add package django-simplevhost/0.1-dev/django-simplevhost-0.1-dev.tar.gz

---
+++ packages/django-simplevhost/django-simplevhost-0.1-dev.tar.gz/django-simplevhost-0.1-dev/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='django-simplevhost',
+    version='0.1-dev',
+    packages=['simplevhost'],
+    url='https://bitbucket.org/russellhay/django-simplevhost',
+    author='Russell Hay',
+    author_email='me@russellhay.com',
+)
