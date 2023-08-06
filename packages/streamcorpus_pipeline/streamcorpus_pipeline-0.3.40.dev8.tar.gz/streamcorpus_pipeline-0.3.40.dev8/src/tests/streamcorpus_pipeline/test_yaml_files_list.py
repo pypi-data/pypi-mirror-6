1ce1909660b7913110bbb6800a8ff6f81866b448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:27 +0000

    Add package streamcorpus_pipeline/0.3.40.dev8/streamcorpus_pipeline-0.3.40.dev8.tar.gz

---
+++ packages/streamcorpus_pipeline/streamcorpus_pipeline-0.3.40.dev8.tar.gz/streamcorpus_pipeline-0.3.40.dev8/src/tests/streamcorpus_pipeline/test_yaml_files_list.py
@@ -0,0 +1,20 @@
+# import time
+# import errno
+import os
+from streamcorpus_pipeline._yaml_files_list import yaml_files_list
+
+def test_parse_file():
+    yfl = yaml_files_list({})
+    input_file = os.path.join(os.path.dirname(__file__), '../../../data/john-smith/ground-truth.yaml')
+
+    for si in yfl(input_file):
+        assert si.stream_id
+        assert si.body.raw
+
+def test_parse_mentions():
+    raw_mentions = ['John Smith', {'name': 'John Smith'}, {'ip_address': '10.0.0.1'}]
+    yfl = yaml_files_list({})
+    mentions = yfl._parse_mentions(raw_mentions)
+
+    assert len(mentions) == len(raw_mentions)
+    assert mentions == [('name', 'John Smith'), ('name', 'John Smith'), ('ip_address', '10.0.0.1')]
