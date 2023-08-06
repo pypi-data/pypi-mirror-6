27c7951935bc9945cd0bee73c53c7f6ddef1f9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:34 +0000

    Add package iac-protocol/0.2/iac-protocol-0.2.tar.gz

---
+++ packages/iac-protocol/iac-protocol-0.2.tar.gz/iac-protocol-0.2/iac/modify_interfaces.py
@@ -0,0 +1,16 @@
+import os
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+def modify(application_name, uncomment=True):
+    new_interfaces = []
+    with open(os.path.join(_ROOT, 'interfaces.py'),'r') as f:
+        for line in f:
+            if application_name in line and uncomment:
+                new_interfaces.append(line.replace('#', ''))
+            elif application_name in line and not uncomment:
+                new_interfaces.append("#" + line)
+            else:
+                new_interfaces.append(line)
+    with open(os.path.join(_ROOT, 'interfaces.py'),'w') as f:
+        for line in new_interfaces:
+            f.write(line)
