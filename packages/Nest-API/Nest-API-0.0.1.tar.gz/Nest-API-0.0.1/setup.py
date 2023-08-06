c937a6470846a5708b1c5d093115a3d462054cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:55 +0000

    Add package Nest-API/0.0.1/Nest-API-0.0.1.tar.gz

---
+++ packages/Nest-API/Nest-API-0.0.1.tar.gz/Nest-API-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Nest-API',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='maxvitek',
+    author_email='',
+    description=''
+)
