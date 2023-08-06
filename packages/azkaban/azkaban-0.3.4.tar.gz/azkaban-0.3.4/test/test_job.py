2efdbd92111a5f2df320978376d4b07aedfb814c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:02 +0000

    Add package azkaban/0.3.4/azkaban-0.3.4.tar.gz

---
+++ packages/azkaban/azkaban-0.3.4.tar.gz/azkaban-0.3.4/test/test_job.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""Test Azkaban job module."""
+
+from azkaban.job import *
+from azkaban.project import Project
+from azkaban.util import AzkabanError, temppath
+from nose.tools import eq_, ok_, raises, nottest
+
+
+class TestJob(object):
+
+  def test_generate_simple(self):
+    job = Job({'a': 1, 'b': {'c': 2, 'd': 3}})
+    with temppath() as path:
+      job.build(path)
+      with open(path) as reader:
+        eq_(reader.read(), 'a=1\nb.c=2\nb.d=3\n')
+
+  def test_generate_with_defaults(self):
+    defaults = {'b': {'d': 4}, 'e': 5}
+    job = Job(defaults, {'a': 1, 'b': {'c': 2, 'd': 3}})
+    with temppath() as path:
+      job.build(path)
+      with open(path) as reader:
+        eq_(reader.read(), 'a=1\nb.c=2\nb.d=3\ne=5\n')
+
+  def test_generate_with_dependencies(self):
+    foo = Job()
+    bar = Job({'a': 3})
+    job = Job({'a': 2, 'dependencies': 'bar,foo'})
+    with temppath() as path:
+      job.build(path)
+      with open(path) as reader:
+        eq_(reader.read(), 'a=2\ndependencies=bar,foo\n')
