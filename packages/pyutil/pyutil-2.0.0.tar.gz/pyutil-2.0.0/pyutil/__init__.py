f6fa3b8d6f5723e6c5c4269696e3df8fc5000377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:40 +0000

    Add package pyutil/2.0.0/pyutil-2.0.0.tar.gz

---
+++ packages/pyutil/pyutil-2.0.0.tar.gz/pyutil-2.0.0/pyutil/__init__.py
@@ -0,0 +1,9 @@
+﻿# -*- coding: utf-8-with-signature-unix; fill-column: 77 -*-
+# -*- indent-tabs-mode: nil -*-
+"""
+Library of useful Python functions and classes.
+"""
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
