fa61e689244fa1b67c074d20ee674f9658a16b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:44 +0000

    Add package python-ted/0.0.1/python-ted-0.0.1.tar.gz

---
+++ packages/python-ted/python-ted-0.0.1.tar.gz/python-ted-0.0.1/setup.py
@@ -0,0 +1,80 @@
+name = 'python-ted'
+install_requires = ['requests', 'icalendar']
+
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+# Get the long description from the relevant file
+with codecs.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="python-ted",
+    version=find_version('ted', '__init__.py'),
+    description="Python library for the University of Edinburgh's T@Ed Timetabling Service",
+    long_description=long_description,
+    license='MPL 2.0',
+
+    # The project URL.
+    url='https://github.com/jeffbr13/python-ted',
+
+    # Author details
+    author='Ben Jeffrey',
+    author_email='mail@benjeffrey.com',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='calendar edinburgh timetable',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages.
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+
+    # Run-time dependencies.
+    install_requires = ['icalendar', 'lxml', 'requests'],
+)
