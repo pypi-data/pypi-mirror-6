948f98a7cfebd43d03832782dbfbdd88a8a4d4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:29 +0000

    Add package tictactoe/0.0.3/tictactoe-0.0.3.tar.gz

---
+++ packages/tictactoe/tictactoe-0.0.3.tar.gz/tictactoe-0.0.3/README.rst
@@ -0,0 +1,4 @@
+tic-tac-toe-tictactoe
+=====================
+
+Front-end of Ultimate Tic Tac Toe Challenge
---
+++ packages/tictactoe/tictactoe-0.0.3.tar.gz/tictactoe-0.0.3/setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+REQS_FILE = open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+install_reqs = [r.strip() for r in REQS_FILE]
+
+if sys.version_info < (3, 3):
+    install_reqs.append("mock==1.0.1")
+
+setup(
+    name='tictactoe',
+    version='0.0.3',
+    author="Motiejus Jakštys",
+    author_email="desired.mta@gmail.com",
+    description="Ultimate Tic-Tac-Toe tictactoe front-end",
+    long_description=open('README.rst').read(),
+    url="https://github.com/Motiejus/tictactoe",
+    packages=find_packages(),
+    install_requires=install_reqs,
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            "tictactoe = tictactoe.manage:main"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Education",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
