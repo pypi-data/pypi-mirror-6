8d80368e09e2b8f131aec27ccfbc75964f4f8e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:23 +0000

    Add package GeoAlchemy2/0.2.4/GeoAlchemy2-0.2.4.tar.gz

---
+++ packages/GeoAlchemy2/GeoAlchemy2-0.2.4.tar.gz/GeoAlchemy2-0.2.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+version = '0.2.4'
+
+setup(
+    name='GeoAlchemy2',
+    version=version,
+    description="Using SQLAlchemy with Spatial Databases",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Plugins",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+    keywords='geo gis sqlalchemy orm',
+    author='Eric Lemoine',
+    author_email='eric.lemoine@gmail.com',
+    url='http://geoalchemy.org/',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests', "doc"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'SQLAlchemy>=0.8',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
