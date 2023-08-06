ceaef20e5c933304e946bca176a25d338f5ddee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:57 +0000

    Add package ramyaathletelist/1.1.0/ramyaathletelist-1.1.0.zip

---
+++ packages/ramyaathletelist/ramyaathletelist-1.1.0.zip/ramyaathletelist-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         = 'ramyaathletelist',
+    version      = '1.1.0',
+    py_modules   = ['ramyaathletelist'],
+    author       = 'RamyaK',
+    author_email = 'ramyakrshnakolla@gmail.com',
+    description  = 'Athlete List',
+    )
