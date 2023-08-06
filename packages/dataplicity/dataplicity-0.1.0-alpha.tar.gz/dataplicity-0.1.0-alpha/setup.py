b20977152859c8f52e35ecaee55a452ff8078f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:35 +0000

    Add package dataplicity/0.1.0-alpha/dataplicity-0.1.0-alpha.tar.gz

---
+++ packages/dataplicity/dataplicity-0.1.0-alpha.tar.gz/dataplicity-0.1.0-alpha/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+from dataplicity import __version__ as VERSION
+
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python',
+]
+
+with open('README.txt') as f:
+    long_desc = f.read()
+
+setup(name='dataplicity',
+      version=VERSION,
+      description="Platform for connected devices",
+      long_description=long_desc,
+      author='Adixein',
+      author_email='support@dataplicity.com',
+      url='http://www.dataplicity.com',
+      platforms=['any'],
+      packages=['dataplicity',
+                'dataplicity.app',
+                'dataplicity.app.subcommands',
+                'dataplicity.client',
+                'dataplicity.tasks'
+                ],
+      classifiers=classifiers,
+      scripts=["dataplicity/app/dataplicity"],
+      install_requires=['python-daemon',
+                        'fs']
+      )
