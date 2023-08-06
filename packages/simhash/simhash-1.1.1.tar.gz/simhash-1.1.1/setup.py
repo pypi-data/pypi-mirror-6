4ff24c5e6c2a5492678a225d56acefa027243aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:25 +0000

    Add package simhash/1.1.1/simhash-1.1.1.tar.gz

---
+++ packages/simhash/simhash-1.1.1.tar.gz/simhash-1.1.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'simhash',
+    version = '1.1.1',
+    keywords = ('simhash'),
+    description = 'A Python implementation of Simhash Algorithm',
+    long_description = open('README.md').read(),
+    license = 'MIT License',
+
+    url = 'http://liangsun.org/posts/a-python-implementation-of-simhash-algorithm/',
+    author = 'Liang Sun',
+    author_email = 'i@liangsun.org',
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
