d4a922d23fda5c21762694699fde59e35dc22c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:27 +0000

    Add package sous-chef/0.8.3/sous-chef-0.8.3.tar.gz

---
+++ packages/sous-chef/sous-chef-0.8.3.tar.gz/sous-chef-0.8.3/sous_chef/chef.py
@@ -0,0 +1,13 @@
+"""A wrapper around PyChef to deal with configuration"""
+
+from __future__ import absolute_import
+
+import chef
+
+
+class FlaskChefAPI(chef.ChefAPI):
+    @classmethod
+    def configure(cls, app):
+        keys = ['CHEF_URL', 'CHEF_KEY', 'CHEF_CLIENT']
+        args = [app.config[k] for k in keys]
+        return cls(*args) if all(args) else chef.autoconfigure()
