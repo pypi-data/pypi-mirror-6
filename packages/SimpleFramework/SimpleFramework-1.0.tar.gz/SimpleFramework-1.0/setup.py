a21954bdb94417ee76f74976e01416b55b588437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package SimpleFramework/1.0/SimpleFramework-1.0.tar.gz

---
+++ packages/SimpleFramework/SimpleFramework-1.0.tar.gz/SimpleFramework-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='SimpleFramework',
+      version='1.0',
+      description='Simple Web Framework by Simplendi',
+      author='Simplendi',
+      author_email='info@simplendi.com',
+      url='https://github.com/Simplendi/SimpleFramework',
+      packages=['framework'],
+      )
