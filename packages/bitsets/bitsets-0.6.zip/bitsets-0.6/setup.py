9fc4dd087fb6b0cf99a24f30aded608edcdbdd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:55 +0000

    Add package bitsets/0.6/bitsets-0.6.zip

---
+++ packages/bitsets/bitsets-0.6.zip/bitsets-0.6/setup.py
@@ -0,0 +1,29 @@
+# setup.py
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='bitsets',
+    version='0.6',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Ordered subsets over a predefined domain',
+    keywords='ordered set finite immutable bit string array map field',
+    license='MIT',
+    url='http://github.com/xflr6/bitsets',
+    packages=['bitsets'],
+    platforms='any',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
