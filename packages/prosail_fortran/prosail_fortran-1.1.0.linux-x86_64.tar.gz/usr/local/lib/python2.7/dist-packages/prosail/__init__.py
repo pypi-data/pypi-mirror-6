71d7e5fca343009f86448d6b06847a683c7985d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:33 +0000

    Add package prosail_fortran/1.1.0/prosail_fortran-1.1.0.linux-x86_64.tar.gz

---
+++ packages/prosail_fortran/prosail_fortran-1.1.0.linux-x86_64.tar.gz/usr/local/lib/python2.7/dist-packages/prosail/__init__.py
@@ -0,0 +1,23 @@
+import numpy as np
+from prosail_fortran import run_prosail, prospect_5b
+
+def trans_prosail ( N, cab, car, cbrown, cw, cm, lai, lidfa, lidfb, rsoil, psoil, \
+        hspot, tts, tto, psi, typelidf):
+    """A version of PROSAIL that uses transformed parameters to quasi-linearise
+    the   model. See http://dx.doi.org/10.1016/j.rse.2011.12.027"""
+    # Define the constants
+    slai = -2.0
+    skab = -100.0
+    skar = -100.0
+    skw =  -1./50.
+    skm =  -1./100.
+    # Transform the parameters to real units
+    xlai = slai * np.log ( lai )
+    xkab = skab * np.log ( cab )
+    xkar = skar * np.log ( car )
+    xkw = skw * np.log ( cw )
+    xdm = skm * np.log ( cm )
+    # Run the PROSAIL model
+    retval = run_prosail ( N, xkab, xkar, cbrown, xkw, xdm, xlai, \
+            lidfa, lidfb, rsoil, psoil, hspot, tts, tto, psi, typelidf )
+    return retval
