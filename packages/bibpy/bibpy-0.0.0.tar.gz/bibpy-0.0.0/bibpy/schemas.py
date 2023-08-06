ab9b2f9668c94e938c0f3592e81e0a99491e21b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:52 +0000

    Add package bibpy/0.0.0/bibpy-0.0.0.tar.gz

---
+++ packages/bibpy/bibpy-0.0.0.tar.gz/bibpy-0.0.0/bibpy/schemas.py
@@ -0,0 +1,21 @@
+ACS = {
+    'article': {
+        'author': [],
+        'title': [],
+        'volume': [],
+        'issued': [],
+        'volume': [],
+        'page': [],
+        'url': [],
+    },
+    'book': {
+        'author': [],
+        'title': [],
+        'issued': []
+    },
+    'inproceedings': {
+        'author': [],
+        'title': [],
+        'issued': []
+    }
+}
