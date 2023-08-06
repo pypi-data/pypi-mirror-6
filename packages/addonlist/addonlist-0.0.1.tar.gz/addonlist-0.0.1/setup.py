adcbf4d61ae816dbdcd231e51e55041584601489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:18 +0000

    Add package addonlist/0.0.1/addonlist-0.0.1.tar.gz

---
+++ packages/addonlist/addonlist-0.0.1.tar.gz/addonlist-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'addonlist',
+    packages = ['addonlist'], # this must be the same as the name above
+    version = '0.0.1',
+    description = 'AddonList Addons package placeholder',
+    author = 'Ben Pellow',
+    author_email = 'benjamin.pellow@gmail.com',
+    url = 'https://github.com/addonlist/addons-pypi',   # use the URL to the github repo
+    download_url = 'https://github.com/addonlist/addons-pypi/tarball/0.0.1', # download tar package, could be binary
+    keywords = ['addonlist', 'addons', 'startup', 'email', 'analytics'], # keywords
+    classifiers = ["Development Status :: 1 - Planning"],
+)
