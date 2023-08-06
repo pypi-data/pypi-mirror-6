6158e57d10fbb426690d456e3b79280fea8e63b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:12 +0000

    Add package django-thermostat/2.0.10/django-thermostat-2.0.10.tar.gz

---
+++ packages/django-thermostat/django-thermostat-2.0.10.tar.gz/django-thermostat-2.0.10/django_thermostat/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from models import *
+
+class ThermometerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Thermometer, ThermometerAdmin)
+
+class RuleAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Rule, RuleAdmin)
+
+
+class DayAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Day, DayAdmin)
+
+
+class TimeRangeAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(TimeRange, TimeRangeAdmin)
+
