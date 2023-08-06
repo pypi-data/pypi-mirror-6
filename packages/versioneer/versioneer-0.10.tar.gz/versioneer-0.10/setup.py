fa6ed6fcf86ec99f8b0ba35d60453e6315d558f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:06 +0000

    Add package versioneer/0.10/versioneer-0.10.tar.gz

---
+++ packages/versioneer/versioneer-0.10.tar.gz/versioneer-0.10/setup.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+
+import os, base64, tempfile
+from distutils.core import setup, Command
+from distutils.command.build_scripts import build_scripts
+
+LONG="""
+Versioneer is a tool to automatically update version strings (in setup.py and
+the conventional 'from PROJECT import _version' pattern) by asking your
+version-control system about the current tree.
+"""
+
+# as nice as it'd be to versioneer ourselves, that sounds messy.
+VERSION = "0.10"
+
+def get(fn):
+    return open(fn, "r").read()
+def unquote(s):
+    return s.replace("%", "%%")
+def ver(s):
+    return s.replace("@VERSIONEER-VERSION@", VERSION)
+def readme(s):
+    return s.replace("@README@", get("README.md"))
+
+def generate_versioneer():
+    vcs = "git"
+    if vcs not in ("git",):
+        raise ValueError("Unhandled revision-control system '%s'" % vcs)
+    out = []
+    out.append(readme(ver(get("src/header.py"))))
+    out.append('VCS = "%s"\n' % vcs)
+    out.append("\n\n")
+    for line in open("src/%s/long-version.py" % vcs, "r").readlines():
+        if line.startswith("#### START"):
+            out.append("LONG_VERSION_PY = '''\n")
+        elif line.startswith("#### SUBPROCESS_HELPER"):
+            out.append(unquote(get("src/subprocess_helper.py")))
+        elif line.startswith("#### MIDDLE"):
+            out.append(unquote(get("src/%s/middle.py" % vcs)))
+        elif line.startswith("#### PARENTDIR"):
+            out.append(unquote(get("src/parentdir.py")))
+        elif line.startswith("#### END"):
+            out.append("'''\n")
+        else:
+            out.append(ver(line))
+    out.append(get("src/subprocess_helper.py"))
+    out.append(get("src/%s/middle.py" % vcs))
+    out.append(get("src/parentdir.py"))
+    out.append(get("src/%s/install.py" % vcs))
+    out.append(ver(get("src/trailer.py")))
+    return ("".join(out)).encode("utf-8")
+
+
+class make_versioneer(Command):
+    description = "create standalone versioneer.py"
+    user_options = []
+    boolean_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        with open("versioneer.py", "w") as f:
+            f.write(generate_versioneer().decode("utf8"))
+        return 0
+
+class my_build_scripts(build_scripts):
+    def run(self):
+        v = generate_versioneer()
+        v_b64 = base64.b64encode(v).decode("ascii")
+        lines = [v_b64[i:i+60] for i in range(0, len(v_b64), 60)]
+        v_b64 = "\n".join(lines)+"\n"
+
+        with open("src/installer.py") as f:
+            s = f.read()
+        s = ver(s.replace("@VERSIONEER-INSTALLER@", v_b64))
+
+        tempdir = tempfile.mkdtemp()
+        installer = os.path.join(tempdir, "versioneer-installer")
+        with open(installer, "w") as f:
+            f.write(s)
+
+        self.scripts = [installer]
+        rc = build_scripts.run(self)
+        os.unlink(installer)
+        os.rmdir(tempdir)
+        return rc
+
+setup(
+    name = "versioneer",
+    license = "public domain",
+    version = VERSION,
+    description = "Easy VCS-based management of project version strings",
+    author = "Brian Warner",
+    author_email = "warner-versioneer@lothar.com",
+    url = "https://github.com/warner/python-versioneer",
+    # "fake" is replaced with versioneer-installer in build_scripts. We need
+    # a non-empty list to provoke "setup.py build" into making scripts,
+    # otherwise it skips that step.
+    scripts = ["fake"],
+    long_description = LONG,
+    cmdclass = { "build_scripts": my_build_scripts,
+                 "make_versioneer": make_versioneer,
+                 },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        ],
+    )
---
+++ packages/versioneer/versioneer-0.10.tar.gz/versioneer-0.10/src/git/middle.py
@@ -0,0 +1,96 @@
+
+import sys
+import re
+import os.path
+
+def get_expanded_variables(versionfile_abs):
+    # the code embedded in _version.py can just fetch the value of these
+    # variables. When used from setup.py, we don't want to import
+    # _version.py, so we do it with a regexp instead. This function is not
+    # used from _version.py.
+    variables = {}
+    try:
+        f = open(versionfile_abs,"r")
+        for line in f.readlines():
+            if line.strip().startswith("git_refnames ="):
+                mo = re.search(r'=\s*"(.*)"', line)
+                if mo:
+                    variables["refnames"] = mo.group(1)
+            if line.strip().startswith("git_full ="):
+                mo = re.search(r'=\s*"(.*)"', line)
+                if mo:
+                    variables["full"] = mo.group(1)
+        f.close()
+    except EnvironmentError:
+        pass
+    return variables
+
+def versions_from_expanded_variables(variables, tag_prefix, verbose=False):
+    refnames = variables["refnames"].strip()
+    if refnames.startswith("$Format"):
+        if verbose:
+            print("variables are unexpanded, not using")
+        return {} # unexpanded, so not in an unpacked git-archive tarball
+    refs = set([r.strip() for r in refnames.strip("()").split(",")])
+    # starting in git-1.8.3, tags are listed as "tag: foo-1.0" instead of
+    # just "foo-1.0". If we see a "tag: " prefix, prefer those.
+    TAG = "tag: "
+    tags = set([r[len(TAG):] for r in refs if r.startswith(TAG)])
+    if not tags:
+        # Either we're using git < 1.8.3, or there really are no tags. We use
+        # a heuristic: assume all version tags have a digit. The old git %d
+        # expansion behaves like git log --decorate=short and strips out the
+        # refs/heads/ and refs/tags/ prefixes that would let us distinguish
+        # between branches and tags. By ignoring refnames without digits, we
+        # filter out many common branch names like "release" and
+        # "stabilization", as well as "HEAD" and "master".
+        tags = set([r for r in refs if re.search(r'\d', r)])
+        if verbose:
+            print("discarding '%s', no digits" % ",".join(refs-tags))
+    if verbose:
+        print("likely tags: %s" % ",".join(sorted(tags)))
+    for ref in sorted(tags):
+        # sorting will prefer e.g. "2.0" over "2.0rc1"
+        if ref.startswith(tag_prefix):
+            r = ref[len(tag_prefix):]
+            if verbose:
+                print("picking %s" % r)
+            return { "version": r,
+                     "full": variables["full"].strip() }
+    # no suitable tags, so we use the full revision id
+    if verbose:
+        print("no suitable tags, using full revision id")
+    return { "version": variables["full"].strip(),
+             "full": variables["full"].strip() }
+
+def versions_from_vcs(tag_prefix, root, verbose=False):
+    # this runs 'git' from the root of the source tree. This only gets called
+    # if the git-archive 'subst' variables were *not* expanded, and
+    # _version.py hasn't already been rewritten with a short version string,
+    # meaning we're inside a checked out source tree.
+
+    if not os.path.exists(os.path.join(root, ".git")):
+        if verbose:
+            print("no .git in %s" % root)
+        return {}
+
+    GITS = ["git"]
+    if sys.platform == "win32":
+        GITS = ["git.cmd", "git.exe"]
+    stdout = run_command(GITS, ["describe", "--tags", "--dirty", "--always"],
+                         cwd=root)
+    if stdout is None:
+        return {}
+    if not stdout.startswith(tag_prefix):
+        if verbose:
+            print("tag '%s' doesn't start with prefix '%s'" % (stdout, tag_prefix))
+        return {}
+    tag = stdout[len(tag_prefix):]
+    stdout = run_command(GITS, ["rev-parse", "HEAD"], cwd=root)
+    if stdout is None:
+        return {}
+    full = stdout.strip()
+    if tag.endswith("-dirty"):
+        full += "-dirty"
+    return {"version": tag, "full": full}
+
