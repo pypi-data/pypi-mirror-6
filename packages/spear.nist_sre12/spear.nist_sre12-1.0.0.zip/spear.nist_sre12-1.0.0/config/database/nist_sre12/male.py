9a41d1eb90febcda7561b2156f7ee0ee70e6f03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:49 +0000

    Add package spear.nist_sre12/1.0.0/spear.nist_sre12-1.0.0.zip

---
+++ packages/spear.nist_sre12/spear.nist_sre12-1.0.0.zip/spear.nist_sre12-1.0.0/config/database/nist_sre12/male.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import xbob.db.nist_sre12
+
+# 0/ The database to use
+name = 'sre12'
+# put the full path to the database filelist
+db = xbob.db.nist_sre12.Database()
+protocol = 'male'
+
+# put the full path to the folder that contains the audio files
+wav_input_dir = "/idiap/temp/ekhoury/NIST_DATA/"
+wav_input_ext = ".sph"
+
