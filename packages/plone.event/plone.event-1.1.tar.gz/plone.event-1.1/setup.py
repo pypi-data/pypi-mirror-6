95d8be6d38a91f6be12b3b611b6617284570be2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:15 +0000

    Add package plone.event/1.1/plone.event-1.1.tar.gz

---
+++ packages/plone.event/plone.event-1.1.tar.gz/plone.event-1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '1.1'
+
+
+setup(
+    name='plone.event',
+    version=version,
+    description="Event and calendaring related tools not bound to Plone",
+    long_description=open("README.rst").read() + "\n" +
+           open(("CHANGES.rst")).read(),
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+    ],
+    keywords='Plone calendar calendaring event recurring',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/plone/plone.event',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'python-dateutil',
+        'pytz',
+        'zope.component',
+        'zope.interface',
+    ],
+    extras_require={
+        'test': [
+            'DateTime',
+            'mock',
+            'unittest2',
+            'zope.configuration',
+        ],
+    },
+)
