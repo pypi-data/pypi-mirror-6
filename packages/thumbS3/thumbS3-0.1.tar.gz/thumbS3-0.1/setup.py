daebfee9fa578daca1e27eec053506866e3cfe94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:12 +0000

    Add package thumbS3/0.1/thumbS3-0.1.tar.gz

---
+++ packages/thumbS3/thumbS3-0.1.tar.gz/thumbS3-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'thumbS3',
+    packages = ['thumbS3'], 
+    version = '0.1',
+    description = 'Python module and command-line tool for creating thumbs on Amazon S3',
+    author = 'Gianluca Tiepolo',
+    author_email = 'tiepolo.gian@gmail.com',
+    url = 'https://github.com/tiepologian/thumbS3',  
+    keywords = ['s3', 'thumbs', 'aws'], 
+    classifiers = []
+)
