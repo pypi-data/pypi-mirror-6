ab5430b4398d73a4e5423038b0936b03c53d0afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:29 +0000

    Add package zopyx.together/0.1.1/zopyx.together-0.1.1.zip

---
+++ packages/zopyx.together/zopyx.together-0.1.1.zip/zopyx.together-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.1'
+
+setup(name='zopyx.together',
+      version=version,
+      description="Plone integration with together.js",
+      long_description=open("README.rst").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='Plone TogetherJS',
+      author='Andreas Jung',
+      author_email='info@zopyx.com',
+      url='http://github.com/zopyx/zopyx.together',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zopyx', 'zopyx.together'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
