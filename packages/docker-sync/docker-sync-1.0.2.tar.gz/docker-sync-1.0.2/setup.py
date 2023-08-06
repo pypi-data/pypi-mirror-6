353edd6668f275eb00b57e8d8188c524ccdd90da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:56 +0000

    Add package docker-sync/1.0.2/docker-sync-1.0.2.tar.gz

---
+++ packages/docker-sync/docker-sync-1.0.2.tar.gz/docker-sync-1.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-sync",
+    version="1.0.2",
+    description="Configuration management for Docker containers",
+    long_description=open("README.md").read(),
+    author="Brian Lalor",
+    author_email="brian@autosportlabs.com",
+    url="http://github.com/autosportlabs/docker-sync",
+    packages=find_packages(),
+    install_requires=[
+        "PyYAML >= 3.10, < 4.0",
+        "argparse >= 1.1",
+        "docker-py >= 0.3.0, < 0.4.0",
+        "setuptools",
+    ],
+    entry_points={
+        "console_scripts": [
+            "docker-sync = docker_sync.cli:sync",
+        ],
+    },
+)
