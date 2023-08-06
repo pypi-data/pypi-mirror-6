b578e68f9062eb09dc8a82ec363eef50fe5aeec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:21 +0000

    Add package substitute/1.1.2/substitute-1.1.2.zip

---
+++ packages/substitute/substitute-1.1.2.zip/substitute-1.1.2/test/test_verify_calls_with_arguments.py
@@ -0,0 +1,72 @@
+from nose.tools import *
+from substitute import *
+from substitute.complaint import *
+expect_that = assert_true
+
+# arg = Argument
+# kwarg = Keyword Argument
+
+def test_expect_received_data():
+    '''Substitute - Verify (args): Method was called with data'''
+    component = Substitute()
+    component.method('hello')
+    expect_that(component.method.received('hello'))
+
+def test_expect_received_type():
+    '''Substitute - Verify (args): Method was called with type'''
+    component = Substitute()
+    component.method('hello')
+    expect_that(component.method.received_any(str))
+
+@raises(MissingCallComplaint)
+def test_was_never_called_fails():
+    '''Substitute - Verify (args): Method was never called (data)'''
+    component = Substitute()
+    expect_that(component.method.received('hello'))
+
+@raises(MissingCallComplaint)
+def text_expect_was_never_called_fails_2():
+    '''Substitute - Verify (args): Method was never called (type)'''
+    component = Substitute()
+    expect_that(component.method.received_any(str))
+
+@raises(UnexpectedKeywordComplaint)
+def test_expect_received_unexpected_keyword_fails():
+    '''Substitute - Verify (args): Method was called with unexpected kwarg'''
+    component = Substitute()
+    component.method(name='hello')
+    expect_that(component.method.received('hello'))
+
+@raises(ArgumentMissmatchComplaint)
+def test_expect_received_wrong_data_fails():
+    '''Substitute - Verify (args): Method was called with wrong data'''
+    component = Substitute()
+    component.method('hello, honey')
+    expect_that(component.method.received('hello'))
+
+@raises(MissingArgumentComplaint)
+def test_expect_received_no_type_fails():
+    component = Substitute()
+    component.method()
+    expect_that(component.method.received_any(str))
+
+@raises(WrongArgumentTypeComplaint)
+def test_expect_received_wrong_type_fails():
+    '''Substitute - Verify (args): Method was called with wrong type'''
+    component = Substitute()
+    component.method(5)
+    expect_that(component.method.received_any(str))
+
+@raises(WrongArgumentTypeComplaint)
+def test_expect_received_wrong_signature_fails():
+    '''Substitute - Verify (args): Method was called with wrong signature'''
+    component = Substitute()
+    component.method(5, 'hello', {})
+    expect_that(component.method.received_any(str, dict))
+
+@raises(MissingArgumentComplaint)
+def test_expect_received_no_argument_fails():
+    '''Substitute - Verify (args): Method was called without arguments'''
+    component = Substitute()
+    component.method()
+    expect_that(component.method.received('hello'))
