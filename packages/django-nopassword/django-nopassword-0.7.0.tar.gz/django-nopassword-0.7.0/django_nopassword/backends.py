c6c9ff4c0df68e2f3d105ad9fac2ddf7643f3f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:43 +0000

    Add package django-nopassword/0.7.0/django-nopassword-0.7.0.tar.gz

---
+++ packages/django-nopassword/django-nopassword-0.7.0.tar.gz/django-nopassword-0.7.0/django_nopassword/backends.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+from django.conf import settings
+from django.core.exceptions import FieldError
+
+from django_nopassword.utils import User
+from django_nopassword.models import LoginCode
+
+
+class EmailBackend:
+
+    supports_inactive_user = True
+
+    def authenticate(self, code=None, **credentials):
+        try:
+            user = User.objects.get(**credentials)
+            if not user.is_active:
+                return None
+
+            if code is None:
+                return LoginCode.create_code_for_user(user)
+            else:
+                timestamp = datetime.now() - timedelta(seconds=getattr(settings, 'NOPASSWORD_LOGIN_CODE_TIMEOUT', 900))
+                login_code = LoginCode.objects.get(user=user, code=code, timestamp__gt=timestamp)
+                user = login_code.user
+                user.code = login_code
+                login_code.delete()
+                return user
+        except (TypeError, User.DoesNotExist, LoginCode.DoesNotExist, FieldError):
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
