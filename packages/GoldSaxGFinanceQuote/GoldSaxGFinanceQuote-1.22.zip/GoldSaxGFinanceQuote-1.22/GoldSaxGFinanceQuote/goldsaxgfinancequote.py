eaa7171f18a13ccf9a413c0cc2aa7163a28033a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:31 +0000

    Add package GoldSaxGFinanceQuote/1.22/GoldSaxGFinanceQuote-1.22.zip

---
+++ packages/GoldSaxGFinanceQuote/GoldSaxGFinanceQuote-1.22.zip/GoldSaxGFinanceQuote-1.22/GoldSaxGFinanceQuote/goldsaxgfinancequote.py
@@ -0,0 +1,36 @@
+import urllib3
+import string
+import re
+from time import localtime, strftime
+    
+def getquote(url):
+            http = urllib3.PoolManager()
+            try:
+                contents = []
+                r = http.request('GET', url)
+                r.release_conn()
+            except urllib3.exceptions.MaxRetryError:
+                return contents
+            except ConnectionResetError:
+                return contents
+            f = r.data.decode("latin-1")
+            a = f.split('"t" : "')
+            tstamp = strftime("%H:%M:%S", localtime())
+            count = 0
+            
+            for ass in a:
+                if count > 0:
+                    v = ass.split('"\n,"e" : "')
+                    h = ass.split(',"l" : "')
+                    j = h[1].split('"\n,"l_')
+                    #filename = strftime("%Y-%m-%d", localtime())+"_"+v[0]+".csv"
+                    #text_file = open(filename, "a")
+                    #text_file.write(tstamp+", "+j[0]+"\n")
+                    #text_file.close()
+                    try:
+                        contents.append([v[0],strftime("%Y-%m-%d"),tstamp,float(re.sub('[^0-9.]+', '', j[0]))])
+                    except ValueError:
+                        pass
+                count = count+1
+            return contents
+
