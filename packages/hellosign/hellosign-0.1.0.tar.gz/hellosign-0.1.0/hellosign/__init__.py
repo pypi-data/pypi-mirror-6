13e75e1b03e7aa0f90f14da76cd82a0ec9471fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:35 +0000

    Add package hellosign/0.1.0/hellosign-0.1.0.tar.gz

---
+++ packages/hellosign/hellosign-0.1.0.tar.gz/hellosign-0.1.0/hellosign/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .api import BaseApiClient
+from .hellosign import (HelloSign,
+                        HelloSignSignature,
+                        HelloSignEmbeddedDocumentSignature,
+                        HelloSignEmbeddedDocumentSigningUrl)
+from .hello_objects import HelloSigner, HelloDoc
