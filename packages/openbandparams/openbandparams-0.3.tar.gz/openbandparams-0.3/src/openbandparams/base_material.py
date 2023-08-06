1fe21d026aefd847659b7f2deaf83ce077f989ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:35 +0000

    Add package openbandparams/0.3/openbandparams-0.3.tar.gz

---
+++ packages/openbandparams/openbandparams-0.3.tar.gz/openbandparams-0.3/src/openbandparams/base_material.py
@@ -0,0 +1,41 @@
+#
+#   Copyright (c) 2013-2014, Scott J Maddox
+#
+#   This file is part of openbandparams.
+#
+#   openbandparams is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   openbandparams is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with openbandparams.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+# std lib imports
+import logging; log = logging.getLogger(__name__)
+
+# third party imports
+
+# local imports
+
+class Base(object):
+    @classmethod
+    def _get_T(cls, kwargs):
+        '''
+        Returns kwargs['T'], kwargs['temp'], kwargs['temperature'], or 300.
+        '''
+        for k in ['T', 'temp', 'temperature']:
+            if k in kwargs:
+                return kwargs[k]
+        else:
+            return 300 # K
+
+class AlloyBase(Base):
+    pass
