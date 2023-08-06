d76911efc3651dae687e71a1ec0c96e18363f088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:52 +0000

    Add package MDTools/0.0.2/MDTools-0.0.2.tar.gz

---
+++ packages/MDTools/MDTools-0.0.2.tar.gz/MDTools-0.0.2/README.txt
@@ -0,0 +1,5 @@
+MDTools
+----------
+
+A [NEON](http://www.neoninc.org) created module for creating and writing generic xml metadata.  This module allows users to read in a flat file structure and create an XML metadata tree.  Alternatively nodes can be added with different classes allowing the module to be used as a backbone for Metadata specific standard modules.  A full manual can be found at http://neondps.github.io/MDTools
+
---
+++ packages/MDTools/MDTools-0.0.2.tar.gz/MDTools-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "MDTools",
+    version = "0.0.2",
+    author = "Edmund Hart",
+    author_email = "emh@emhart.info",
+    packages=['mdtools'],
+    description = ("A module to create XML metadata"),
+    license = "BSD",
+    keywords = "XML metadata",
+    url = "http://github.com/neondps/MDTools",
+    long_description=read('README.txt'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+          'lxml',
+      ],
+
+)
