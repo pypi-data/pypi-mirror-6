8961375d19eabdfec38d32c598e65fc1d792cf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:38 +0000

    Add package solar-theme/1.3.1/solar-theme-1.3.1.tar.gz

---
+++ packages/solar-theme/solar-theme-1.3.1.tar.gz/solar-theme-1.3.1/README.rst
@@ -0,0 +1,26 @@
+Solar
+=====
+Solar is an attempt to create a theme for the Python Sphinx documentation
+generator based on the `Solarized <http://ethanschoonover.com/solarized>`_
+color scheme.
+
+Preview and Documentation
+-------------------------
+http://vkvn.github.io/solar
+
+How to use
+----------
+#. Extract the archive.
+#. Modify ``conf.py`` of an existing Sphinx project or create new project
+   using ``sphinx-quickstart``.
+#. Change the ``html_theme`` parameter to ``solar``.
+#. Change the ``html_theme_path`` to the location containing the extracted
+   archive.
+
+----
+
+Free software: BSD license
+
+Modified from the default Sphinx theme (sphinxdoc)
+
+Background "Subtle dots" pattern from http://subtlepatterns.com.
---
+++ packages/solar-theme/solar-theme-1.3.1.tar.gz/solar-theme-1.3.1/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='solar-theme',
+    version='1.3.1',
+    description='Theme for Python Sphinx',
+    long_description=readme + '\n\n' + history,
+    author='Vimalkumar Velayudhan',
+    author_email='vimalkumarvelayudhan@gmail.com',
+    url='https://github.com/vkvn/solar-theme',
+    packages=[
+        'solar_theme',
+    ],
+    package_dir={'solar_theme': 'solar_theme'},
+    package_data={'solar_theme': ['theme.conf', 'static/subtle_dots.png', '*.html',
+                            'static/*.css']},
+    install_requires=[
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='solar theme',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+    test_suite='tests',
+)
