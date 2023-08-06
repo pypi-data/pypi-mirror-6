1a7f2eb05a1e547828c475b32ce43bc77724c91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:03 +0000

    Add package monsql/0.1/monsql-0.1.zip

---
+++ packages/monsql/monsql-0.1.zip/monsql-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='monsql',
+      version='0.1',
+      packages = find_packages(),
+      author='firstprayer',
+      author_email='zhangty10@gmail.com',
+      url='https://github.com/firstprayer/python-mysql-wrapper.git',
+      # package_dir={'dbwrapper': 'dbwrapper'},
+)
