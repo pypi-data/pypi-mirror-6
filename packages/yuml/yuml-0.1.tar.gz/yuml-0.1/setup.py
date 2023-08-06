ea0350f934fd7047af9454015032d68a6661adcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:36 +0000

    Add package yuml/0.1/yuml-0.1.tar.gz

---
+++ packages/yuml/yuml-0.1.tar.gz/yuml-0.1/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='yuml',
+    description='A command-line client for the yUML service',
+    author='Wander Nauta',
+    author_email='info@wandernauta.nl',
+    version='0.1',
+    scripts=['yuml'],
+    url='https://github.com/wandernauta/yuml/',
+    download_url = 'https://github.com/wandernauta/yuml/tarball/master',
+    license='COPYING',
+    long_description=open('README.rst').read(),
+
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+    ],
+)
