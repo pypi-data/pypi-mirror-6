b67cb3986701fcc71dd596f1d2d968590451ce97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:41 +0000

    Add package import-stackmob-to-apiOmat/1.0/import-stackmob-to-apiOmat-1.0.tar.gz

---
+++ packages/import-stackmob-to-apiOmat/import-stackmob-to-apiOmat-1.0.tar.gz/import-stackmob-to-apiOmat-1.0/setup.py
@@ -0,0 +1,14 @@
+'''
+Created on 29.04.2013
+
+@author: phimi
+'''
+from setuptools import setup, find_packages
+setup(
+    name = "import-stackmob-to-apiOmat",
+    description = "Import your data from Stackmob to apiOmat",
+    url = "https://github.com/apinaut/apiOmat-import-stackmob",
+    version = "1.0",
+    scripts=['ImportStackmobData/import-stackmob-to-apiOmat'],
+    packages = find_packages(),
+)
