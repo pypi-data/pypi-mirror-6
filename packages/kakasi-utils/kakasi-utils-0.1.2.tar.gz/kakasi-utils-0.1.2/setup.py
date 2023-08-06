d1d526bc0b8161f86dae78c67f3345d5fd899df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:06 +0000

    Add package kakasi-utils/0.1.2/kakasi-utils-0.1.2.tar.gz

---
+++ packages/kakasi-utils/kakasi-utils-0.1.2.tar.gz/kakasi-utils-0.1.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+
+setup(name='kakasi-utils',
+      version='0.1.2',
+      description='KAKASI Utilities',
+      author='Yusuke Miyazaki',
+      author_email='miyazaki.dev@gmail.com',
+      url='https://github.com/403JFW/kakasi-utils',
+      packages=['kakasi_utils'],
+      scripts=['scripts/pykanwa', 'scripts/google2kakasi'],
+      test_suite='tests',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: MacOS',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Topic :: Utilities'
+      ])
