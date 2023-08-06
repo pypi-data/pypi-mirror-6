32aa27e0e870a896a68e42c5be124867dab60939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:52 +0000

    Add package ghdiff/0.2/ghdiff-0.2.tar.gz

---
+++ packages/ghdiff/ghdiff-0.2.tar.gz/ghdiff-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="ghdiff",
+    version="0.2",
+    author="Patrick Strawderman",
+    author_email="patrick@kilink.net",
+    url="http://github/kilink/ghdiff",
+    license="MIT",
+    package_data={"": ["*.py", "*.txt", "*.css"]},
+    include_package_data=True,
+    package_dir={"": "src"},
+    py_modules=["ghdiff"],
+    tests_require=["zope.testrunner"],
+    install_requires=["six", "chardet"],
+    test_suite="tests.test_suite",
+    entry_points={
+        'console_scripts': [
+            "ghdiff = ghdiff:main"
+        ]
+    },
+)
