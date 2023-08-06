58e8d55bea460c9ecc21c7ac9ce046af062730a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:17 +0000

    Add package xortool/0.95/xortool-0.95.tar.gz

---
+++ packages/xortool/xortool-0.95.tar.gz/xortool-0.95/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='xortool',
+      version='0.95',
+      description='Tool for xor cipher analysis',
+
+      author='hellman',
+      author_email='hellman1908@gmail.com',
+      license="MIT",
+
+      url='https://github.com/hellman/xortool',
+      packages=['xortool'],
+      scripts=["xortool/xortool", "xortool/xortool-xor"]
+      )
