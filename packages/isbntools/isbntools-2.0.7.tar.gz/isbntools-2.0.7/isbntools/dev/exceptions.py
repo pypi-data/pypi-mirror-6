da483141edbffd95fa531e93143be3a1d8da5f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:35 +0000

    Add package isbntools/2.0.7/isbntools-2.0.7.tar.gz

---
+++ packages/isbntools/isbntools-2.0.7.tar.gz/isbntools-2.0.7/isbntools/dev/exceptions.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Exceptions for isbntools.dev
+
+The classes in isbntools.dev should use the exceptions below.
+"""
+
+
+class ISBNToolsDevException(Exception):
+    """ Base class for isbntools.dev exceptions
+
+    This exception should not be raised directly,
+    only subclasses of this exception should be used!
+    """
+
+    def __init__(self, msg=None):
+        if msg:
+            self.message = '%s (%s)' % (self.message, msg)
+
+    def __str__(self):
+        return getattr(self, 'message', '')       # pragma: no cover
+
+
+class WSHTTPError(ISBNToolsDevException):
+    """ Exception raised for HTTP related errors
+    """
+    message = "an HTTP error has ocurred"
+
+
+class WSURLError(ISBNToolsDevException):
+    """ Exception raised for URL related errors
+    """
+    message = "an URL error has ocurred"
+
+
+class WQDataNotFoundError(ISBNToolsDevException):
+    """ Exception raised when there is no target data from the service
+    """
+    message = "the target data was not found"
+
+
+class WQServiceIsDownError(ISBNToolsDevException):
+    """ Exception raised when the service is not available
+    """
+    message = "the service is down (try later)"
+
+
+class WPDataWrongShapeError(ISBNToolsDevException):
+    """ Exception raised when the data hasn't the expected format
+    """
+    message = "the data hasn't the expected format"
+
+
+class WPNotValidMetadataError(ISBNToolsDevException):
+    """ Exception raised when the metadata hasn't the expected format
+    """
+    message = "the metadata hasn't the expected format"
---
+++ packages/isbntools/isbntools-2.0.7.tar.gz/isbntools-2.0.7/isbntools/dev/googlebooks.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+from .webquery import WEBQuery
+from .data import stdmeta
+from .exceptions import WPDataWrongShapeError
+
+UA = 'isbntools (gzip)'
+SERVICE_URL = 'https://www.googleapis.com/books/v1/volumes?q=isbn+%s&fields='\
+    'items/volumeInfo(title,authors,publisher,publishedDate,language)'\
+    '&maxResults=1'
+
+logger = logging.getLogger(__name__)
+
+
+class GOOBQuery(WEBQuery):
+    """
+    Queries the Google Books (JSON API v1) for metadata
+    """
+
+    def __init__(self, isbn):
+        """
+        Initializer & call webservice & handle errors
+        """
+        self.isbn = isbn
+        WEBQuery.__init__(self, SERVICE_URL % isbn, UA)
+        # lets us go with the default raw data_checker
+        WEBQuery.check_data(self)
+
+    def records(self):
+        """
+        Classifies (canonically) the parsed data
+        """
+        # this service uses JSON, so stay with the default parser
+        data = WEBQuery.parse_data(self)
+        try:
+            # put the selected data in records
+            records = data['items'][0]['volumeInfo']
+        except:             # pragma: no cover
+            try:
+                extra = data['stat']
+                logger.debug('WPDataWrongShapeError for % with data %s' %
+                             (self.isbn, extra))
+            except:
+                pass
+            raise WPDataWrongShapeError(self.isbn)
+
+        # canonical:
+        # -> ISBN-13, Title, Authors, Publisher, Year, Language
+        canonical = {}
+        canonical['ISBN-13'] = unicode(self.isbn)
+        canonical['Title'] = records['title'].replace(' :', ':')
+        canonical['Authors'] = records.get('authors', [])
+        canonical['Publisher'] = records.get('publisher', '')
+        if 'publishedDate' in records and len(records['publishedDate']) >= 4:
+            canonical['Year'] = records['publishedDate'][0:4]
+        else:         # pragma: no cover
+            canonical['Year'] = ''
+        canonical['Language'] = records['language']
+        # call stdmeta for extra cleanning and validation
+        return stdmeta(canonical)
+
+
+def query(isbn):
+    """
+    Function API to the class
+    """
+    q = GOOBQuery(isbn)
+    return q.records()
