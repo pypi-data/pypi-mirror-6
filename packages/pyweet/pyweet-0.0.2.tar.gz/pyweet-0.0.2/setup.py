c526d0af6d3a777ffaebf0c6de136c2eb7b0e4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package pyweet/0.0.2/pyweet-0.0.2.tar.gz

---
+++ packages/pyweet/pyweet-0.0.2.tar.gz/pyweet-0.0.2/setup.py
@@ -0,0 +1,29 @@
+"""Pyweet's setup/installer."""
+
+
+from setuptools import setup
+
+
+setup(
+    name="pyweet",
+    version="0.0.2",
+    author="Adam Talsma",
+    author_email="adam@talsma.ca",
+    package_dir={"pyweet": "src"},
+    packages=["pyweet"],
+    install_requires=["twitter"],
+    scripts=["bin/pyweet"],
+    url="https://github.com/a-tal/pyweet",
+    description="Twitter command line util",
+    long_description="Yet another Twitter command line utility.",
+    download_url="https://github.com/a-tal/pyweet",
+    license="BSD",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+    ],
+)
---
+++ packages/pyweet/pyweet-0.0.2.tar.gz/pyweet-0.0.2/src/__init__.py
@@ -0,0 +1,4 @@
+"""Pyweet as a package. Export the main cmd line entry point."""
+
+
+from pyweet import main
