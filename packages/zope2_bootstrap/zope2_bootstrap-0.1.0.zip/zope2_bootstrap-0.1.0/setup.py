a2e41f2c747a9e1193dec303e020ff894160475f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:37 +0000

    Add package zope2_bootstrap/0.1.0/zope2_bootstrap-0.1.0.zip

---
+++ packages/zope2_bootstrap/zope2_bootstrap-0.1.0.zip/zope2_bootstrap-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages
+from setuptools import setup
+import os
+
+
+VERSION = '0.1.0'
+
+
+setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+    description="ZMI Bootstrapped",
+    keywords='Bootstrap Plone Zope',
+    include_package_data=True,
+    install_requires=[
+        'collective.monkeypatcher',
+    ],
+    license='GPL',
+    long_description=(
+        open('README.rst').read() + '\n' +
+        open('CHANGES.rst').read()),
+    name='zope2_bootstrap',
+    packages=find_packages(),
+    test_suite='tests.TestCase',
+    url='https://github.com/aclark4life/zope2_bootstrap',
+    version=VERSION,
+    zip_safe=False,
+)
