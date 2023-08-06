faeef418cb82d901c7fccf473d3d4e1b9d64ff76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:54 +0000

    Add package Knowingness/0.5.0/Knowingness-0.5.0.tar.gz

---
+++ packages/Knowingness/Knowingness-0.5.0.tar.gz/Knowingness-0.5.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Knowingness',
+    version='0.5.0',
+    author='Michael Stitt',
+    author_email='michaelstitt10@hotmail.com',
+    packages=['knowingness', 'knowingness.social'],
+    license='LICENSE.txt',
+    description='Social Network Analytics',
+    long_description=open('README.txt').read(),
+    install_requires='requests',
+)
+
