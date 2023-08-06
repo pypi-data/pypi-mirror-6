e7d000c043b6575b614aac462ce2954ab3315078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:14 +0000

    Add package objbrowser/0.9.4/objbrowser-0.9.4.tar.gz

---
+++ packages/objbrowser/objbrowser-0.9.4.tar.gz/objbrowser-0.9.4/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from objbrowser.objectbrowser import PROGRAM_VERSION
+
+setup(name = 'objbrowser',
+    version = PROGRAM_VERSION, 
+    description = 'Python object browser implemented in Qt.',
+    long_description = open('README.txt').read(),
+    url = 'https://github.com/titusjan/objbrowser',    
+    author = "Pepijn Kenter", 
+    author_email = "titusjan@gmail.com", 
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Adaptive Technologies',
+        'Topic :: Software Development',
+        'Topic :: Utilities'],    
+    packages = ['objbrowser'],
+    install_requires = ['PySide >= 1.1.2'])
+
