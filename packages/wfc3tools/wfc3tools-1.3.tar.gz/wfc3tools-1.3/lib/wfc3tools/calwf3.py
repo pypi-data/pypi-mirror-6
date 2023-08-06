17edef229f7aa2bbd5c82857ee8b79de790565a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:17 +0000

    Add package wfc3tools/1.3/wfc3tools-1.3.tar.gz

---
+++ packages/wfc3tools/wfc3tools-1.3.tar.gz/wfc3tools-1.3/lib/wfc3tools/calwf3.py
@@ -0,0 +1,91 @@
+"""
+CALWF3 calibrates UVIS and IR images for WFC3
+"""
+
+#get the auto update version for the call to teal help
+from .version import *
+
+# STDLIB
+import os.path
+import subprocess
+
+#STSCI
+from stsci.tools import parseinput
+try:
+    from stsci.tools import teal
+except:
+    teal = None
+    
+__taskname__ = "calwf3"
+__vdate__ = "03-Jan-2013"
+
+
+def calwf3(input, printtime=False, save_tmp=False,
+           verbose=False, debug=False ):
+
+    """ Call the calwf3.e executable """
+
+    
+    call_list = ['calwf3.e']
+
+    if printtime:
+        call_list.append('-t')
+
+    if save_tmp:
+        call_list.append('-s')
+
+    if verbose:
+        call_list.append('-v')
+
+    if debug:
+        call_list.append('-d')
+
+
+    if not os.path.exists(input):
+        raise IOError('Input file not found: ' + input)
+
+    call_list.append(input)
+
+    subprocess.call(call_list)
+
+def help(file=None):
+    helpstr = getHelpAsString(docstring=True)
+    if file is None:
+        print helpstr
+    else:
+        if os.path.exists(file): os.remove(file)
+        f = open(file,mode='w')
+        f.write(helpstr)
+        f.close()
+    
+
+def getHelpAsString(docstring=False):
+    """Return documentation on the 'wf3ir' function. Required by TEAL."""
+
+    install_dir = os.path.dirname(__file__)
+    htmlfile = os.path.join(install_dir, 'htmlhelp', __taskname__ + '.html')
+    helpfile = os.path.join(install_dir, __taskname__ + '.help')
+    if docstring or (not docstring and not os.path.exists(htmlfile)):
+        helpString = ' '.join([__taskname__, 'Version', __version__,
+                               ' updated on ', __vdate__]) + '\n\n'
+        if os.path.exists(helpfile):
+            helpString += teal.getHelpFileAsString(__taskname__, __file__)
+    else:
+        helpString = 'file://' + htmlfile
+
+    return helpString
+        
+
+def run(configobj=None):
+    """
+    TEAL interface for the 'calwf3' function.
+
+    """
+    calwf3(configobj['input'],
+           printtime=configobj['printtime'],
+           save_tmp=configobj['save_tmp'],
+           verbose=configobj['verbose'],
+           debug=configobj['debug'])
+
+
+calwf3.__doc__ = getHelpAsString(docstring=True)
---
+++ packages/wfc3tools/wfc3tools-1.3.tar.gz/wfc3tools-1.3/lib/wfc3tools/htmlhelp/_sources/index.txt
@@ -0,0 +1,43 @@
+.. wfc3tools documentation master file, created by
+   sphinx-quickstart on Wed Dec 12 16:53:43 2012.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+    
+
+Welcome to the wfc3tools package documentation!
+===============================================
+.. image:: images/wfc_logo.png
+    :align: right
+
+See the team website for more information:  http://www.stsci.edu/hst/wfc3/
+
+The current WFC3 Data Handbook can be found at  http://www.stsci.edu/hst/wfc3/documents/handbooks/currentDHB/ .  
+
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   calwf3 <calwf3.rst>
+   wf32d <wf32d.rst>
+   wf3ccd <wf3ccd.rst>
+   wf3ir <wf3ir.rst>
+   wf3rej <wf3rej.rst>
+   Running Astrodriz <runastrodriz.rst>
+   sampinfo <sampinfo.rst>
+   pstat <pstat.rst>
+   pstack <pstack.rst>
+   sub2full <sub2full.rst>
+   embedsub <embedsub.rst>
+   Software History <history.rst>
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
