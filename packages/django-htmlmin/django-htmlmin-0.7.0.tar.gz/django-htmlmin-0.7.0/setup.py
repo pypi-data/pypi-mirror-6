95d572dea7d52e54054dfd77b7dae6b32dc0d531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:56 +0000

    Add package django-htmlmin/0.7.0/django-htmlmin-0.7.0.tar.gz

---
+++ packages/django-htmlmin/django-htmlmin-0.7.0.tar.gz/django-htmlmin-0.7.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2013 django-htmlmin authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from setuptools import setup, find_packages
+from htmlmin import __version__
+
+README = open('README.rst').read()
+
+setup(
+    name='django-htmlmin',
+    version=__version__,
+    description='html minify for django',
+    long_description=README,
+    author='CobraTeam',
+    author_email='andrewsmedina@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='nose.collector',
+    install_requires=['argparse', 'beautifulsoup4', 'html5lib'],
+    tests_require=['nose', 'coverage', 'django'],
+    entry_points={
+        'console_scripts': [
+            'pyminify = htmlmin.commands:main',
+        ],
+    },
+)
