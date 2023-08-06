2db340209c199818a63dbc2c96edd5abcf276fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:43 +0000

    Add package boost_regex/0.5/boost_regex-0.5.zip

---
+++ packages/boost_regex/boost_regex-0.5.zip/boost_regex-0.5/boost_regex.cpp
@@ -0,0 +1,214 @@
+#include <boost/regex.hpp>
+#include "Python.h"
+#include <structmember.h>
+
+typedef struct {
+	PyObject_HEAD
+	boost::wregex *rx;
+} BoostRegexObject;
+
+static int BoostRegex_init(BoostRegexObject *self, PyObject *args, PyObject *kwds)
+{
+	Py_UNICODE *pattern;
+	int *len;
+	static char *kwlist[] = {"pattern", NULL};
+	if (!PyArg_ParseTupleAndKeywords(args, kwds, "|u#", kwlist, &pattern, &len)) {
+		return -1;
+	}
+
+  	self->rx = new boost::wregex(pattern, boost::regex_constants::normal | boost::regex_constants::no_except);
+	return 0;
+};
+
+static void BoostRegex_dealloc(BoostRegexObject *self)
+{
+    delete self->rx;
+    Py_TYPE(self)->tp_free((PyObject *)self);
+};
+
+static PyMemberDef BoostRegex_members[] = {
+	{NULL}  /* Sentinel */
+};
+
+static PyObject *BoostRegex_match(BoostRegexObject *self, PyObject *args)
+{
+	Py_UNICODE *text;
+	bool match;
+
+	if (!PyArg_ParseTuple(args, "u", &text))
+	    return NULL;
+
+    if (self->rx->empty()) {
+        PyErr_SetString(PyExc_ValueError, "Regular expression invalid");
+        return NULL;
+    };
+
+    try {
+        match = boost::regex_match(text, *self->rx);
+        if (match)
+        {
+            Py_INCREF(Py_True);
+            return Py_True;
+        }
+        else
+        {
+            Py_INCREF(Py_False);
+            return Py_False;
+        };
+    }
+    catch (...) {
+        PyErr_SetString(PyExc_RuntimeError, "Matching failed");
+        return NULL;
+    };
+};
+
+static PyObject *BoostRegex_search(BoostRegexObject *self, PyObject *args)
+{
+	Py_UNICODE *text;
+	bool match;
+
+	if (!PyArg_ParseTuple(args, "u", &text))
+	    return NULL;
+
+    if (self->rx->empty()) {
+        PyErr_SetString(PyExc_ValueError, "Regular expression invalid");
+        return NULL;
+    };
+
+    try {
+        match = boost::regex_search(text, *self->rx);
+        if (match)
+        {
+            Py_INCREF(Py_True);
+            return Py_True;
+        }
+        else
+        {
+            Py_INCREF(Py_False);
+            return Py_False;
+        };
+    }
+    catch (...) {
+        PyErr_SetString(PyExc_RuntimeError, "Searching failed");
+        return NULL;
+    };
+};
+
+static PyMethodDef BoostRegex_methods[] = {
+	{"match", (PyCFunction)BoostRegex_match, METH_VARARGS, "Full regex match."},
+	{"search", (PyCFunction)BoostRegex_search, METH_VARARGS, "Regex search."},
+	{NULL}
+};
+
+static char BoostRegex_doc[] =
+	"Boost regular expression.";
+
+static PyTypeObject BoostRegexObjectType = {
+	PyObject_HEAD_INIT(NULL)
+	0,				/* ob_size           */
+	"boost_regex.BoostRegex",			/* tp_name           */
+	sizeof(BoostRegexObject),		/* tp_basicsize      */
+	0,				/* tp_itemsize       */
+	(destructor)BoostRegex_dealloc,				/* tp_dealloc        */
+	0,				/* tp_print          */
+	0,				/* tp_getattr        */
+	0,				/* tp_setattr        */
+	0,				/* tp_compare        */
+	0,				/* tp_repr           */
+	0,				/* tp_as_number      */
+	0,				/* tp_as_sequence    */
+	0,				/* tp_as_mapping     */
+	0,				/* tp_hash           */
+	0,				/* tp_call           */
+	0,				/* tp_str            */
+	0,				/* tp_getattro       */
+	0,				/* tp_setattro       */
+	0,				/* tp_as_buffer      */
+	Py_TPFLAGS_DEFAULT,		/* tp_flags          */
+	BoostRegex_doc,			/* tp_doc            */
+	0,				/* tp_traverse       */
+	0,				/* tp_clear          */
+	0,				/* tp_richcompare    */
+	0,				/* tp_weaklistoffset */
+	0,				/* tp_iter           */
+	0,				/* tp_iternext       */
+	BoostRegex_methods,	   		/* tp_methods        */
+	BoostRegex_members,			/* tp_members        */
+	0,				/* tp_getset         */
+	0,				/* tp_base           */
+	0,				/* tp_dict           */
+	0,				/* tp_descr_get      */
+	0,				/* tp_descr_set      */
+	0,				/* tp_dictoffset     */
+	(initproc)BoostRegex_init,		/* tp_init           */
+    PyType_GenericAlloc,                        /* tp_alloc */
+    PyType_GenericNew,                          /* tp_new */
+    PyObject_Del,                            /* tp_free */
+};
+
+static PyObject* match(PyObject *self, PyObject *args)
+{
+	int ok;
+	const char *text;
+	const char *expr;
+	bool match;
+	ok = PyArg_ParseTuple(args, "ss", &expr, &text);
+
+	if (ok) {
+	    try {
+	        using namespace boost;
+
+            regex rx(expr);
+
+            try {
+                match = regex_match(text, rx);
+
+                if (match)
+                {
+                    Py_INCREF(Py_True);
+                    return Py_True;
+                }
+                else
+                {
+                    Py_INCREF(Py_False);
+                    return Py_False;
+                };
+            }
+            catch (...) {
+                PyErr_SetString(PyExc_RuntimeError, "Matching failed");
+                return NULL;
+            };
+	    }
+	    catch (...) {
+	        PyErr_SetString(PyExc_ValueError, "Regular expression invalid");
+	        return NULL;
+	    };
+	};
+
+	return NULL;
+}
+
+static PyMethodDef boost_regex_methods[] = {
+	{"match", match, METH_VARARGS, "match(pattern, text)"},
+	{NULL, NULL}
+};
+
+PyMODINIT_FUNC
+initboost_regex(void)
+{
+	PyObject* m;
+
+	if (PyType_Ready(&BoostRegexObjectType) < 0)
+		return;
+
+	m = Py_InitModule("boost_regex", boost_regex_methods);
+	if (m == NULL)
+		return;
+
+	Py_INCREF(&BoostRegexObjectType);
+	PyModule_AddObject(m, "BoostRegex", (PyObject *)&BoostRegexObjectType);
+}
+
+
+
+
---
+++ packages/boost_regex/boost_regex-0.5.zip/boost_regex-0.5/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup, Extension
+
+
+
+setup(
+    name='boost_regex',
+    version='0.5',
+    ext_modules=[
+        Extension('boost_regex',
+                  ['boost_regex.cpp'],
+                  include_dirs=['/usr/include', '\\usr\\include', '\\usr\\include\\boost', 'C:\\local\\boost_1_55_0'],
+                  library_dirs=['C:\\local\\boost_1_55_0\\lib64-msvc-9.0', '\\usr\\lib', '\\usr\\lib\\boost', '/usr/lib', '/usr/lib/boost'],
+                  extra_link_args=['-lboost_regex']
+        )
+    ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    author='Bart Jellema',
+    author_email='bart@zeromail.com',
+    url='http://pypi.python.org/pypi/boost_regex/',
+    description='Very basic interface to the boost regex library.'
+)
