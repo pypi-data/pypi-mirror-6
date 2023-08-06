20120e2362ca84f3f27e0cb2ae5827c0e3dd31ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:30 +0000

    Add package pyoauth2-je/0.0.1/pyoauth2-je-0.0.1.tar.gz

---
+++ packages/pyoauth2-je/pyoauth2-je-0.0.1.tar.gz/pyoauth2-je-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pyoauth2-je',
+    version='0.0.1',
+    author='GraphEffect, Inc.',
+    author_email='nate@grapheffect.com',
+    packages=['pyoauth2', 'pyoauth2.tests'],
+    scripts=[],
+    url='https://github.com/GraphEffect/pyoauth2',
+    license='LICENSE.txt',
+    description='OAuth 2.0 compliant client and server library.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "requests >= 0.14",
+        "PyCrypto >= 2.6"
+    ]
+)
