831daf7cc0423295f4b9f682db0fcdda264dac36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:03 +0000

    Add package chunker/1.1/chunker-1.1.tar.gz

---
+++ packages/chunker/chunker-1.1.tar.gz/chunker-1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='chunker',
+    version='1.1',
+    description='Easy Chunk-Based File Structure Parser',
+    author='Archangel_SDY',
+    author_email='Archangel.SDY@gmail.com',
+    url='http://archangelsdy.github.io/chunker/',
+    packages=['chunker']
+)
