e9779f14af0e4d8e0bec84a39f8352357981bcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:43 +0000

    Add package ike/0.0.3/ike-0.0.3.tar.gz

---
+++ packages/ike/ike-0.0.3.tar.gz/ike-0.0.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+setup(name='ike',
+      version='0.0.3',
+      description='Minimalistic Internet Key Exchange protocol v2 (RFC 5996) library',
+      long_description=readme(),
+      author='Kimmo Parviainen-Jalanko',
+      author_email='k@77.fi',
+      url='http://github.com/kimvais/pyike/',
+      packages=['ike']
+     )
