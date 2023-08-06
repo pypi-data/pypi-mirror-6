63de858d5f0ec43156d4d16f3ec41671aa3adeda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:23 +0000

    Add package gsocketpool/0.1.3/gsocketpool-0.1.3.tar.gz

---
+++ packages/gsocketpool/gsocketpool-0.1.3.tar.gz/gsocketpool-0.1.3/gsocketpool/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from connection import Connection
+from connection import TcpConnection
+from pool import Pool
+from exceptions import ConnectionNotFoundError, PoolExhaustedError
