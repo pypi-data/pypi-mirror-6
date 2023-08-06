982bccda24278cc93b9bd3f5bc43e8acfdd4cdbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:30 +0000

    Add package django-content-bbcode/0.0.1/django-content-bbcode-0.0.1.tar.gz

---
+++ packages/django-content-bbcode/django-content-bbcode-0.0.1.tar.gz/django-content-bbcode-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from os import path
+from setuptools import setup, find_packages
+
+
+def read(name):
+    return open(path.join(path.dirname(__file__), name)).read()
+
+setup(
+    name='django-content-bbcode',
+    version='0.0.1',
+    description='Advanced BBCode alike tags parser',
+    long_description=read('README.md'),
+    author='Piotr Malinski',
+    author_email='riklaunim@gmail.com',
+    url='https://github.com/riklaunim/django-content-bbcode',
+    packages=find_packages(exclude=["content_bbcoode_demo"]),
+    install_requires=[
+        'Django',
+    ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
