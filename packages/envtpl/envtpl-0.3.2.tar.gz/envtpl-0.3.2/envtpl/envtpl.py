8e7dca36820836208cbd93002196f0f70fafdf19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:23 +0000

    Add package envtpl/0.3.2/envtpl-0.3.2.tar.gz

---
+++ packages/envtpl/envtpl-0.3.2.tar.gz/envtpl-0.3.2/envtpl/envtpl.py
@@ -0,0 +1,122 @@
+'''
+envtpl - jinja2 template rendering with shell environment variables
+Copyright (C) 2014  Andreas Jansson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import os
+import sys
+import re
+import argparse
+import jinja2
+
+EXTENSION = '.tpl'
+REGEX = re.compile(r'\{\{ *(?P<name>[^| ]+)(?: *\| *(?P<default>[^ \}]*))? *\}\}')
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='jinja2 template rendering with shell environment variables'
+    )
+    parser.add_argument('input_file', 
+                        nargs='?', help='Input filename. Defaults to stdin.'
+    )
+    parser.add_argument('-o', '--output-file', 
+                        help='Output filename. If none is given, and the input file ends '
+                        'with "%s", the output filename is the same as the input '
+                        'filename, sans the %s extension. Otherwise, defaults to stdout.' %
+                        (EXTENSION, EXTENSION)
+    )
+    parser.add_argument('--allow-missing', action='store_true',
+                        help='Allow missing variables. By default, envtpl will die with exit '
+                        'code 1 if an environment variable is missing'
+    )
+    parser.add_argument('--keep-template', action='store_true',
+                        help='Keep original template file. By default, envtpl will delete '
+                        'the template file'
+    )
+    args = parser.parse_args()
+
+    variables = os.environ
+    try:
+        process_file(args.input_file, args.output_file, variables, not args.allow_missing, not args.keep_template)
+    except (Fatal, IOError), e:
+        sys.stderr.write('Error: %s\n' % str(e))
+        sys.exit(1)
+
+    sys.exit(0)
+
+def process_file(input_filename, output_filename, variables, die_on_missing_variable, remove_template):
+    if not input_filename and not remove_template:
+        raise Fatal('--keep-template only makes sense if you specify an input file')
+
+    if input_filename and not output_filename:
+        if not input_filename.endswith(EXTENSION):
+            raise Fatal('If no output filename is given, input filename must end in %s' % EXTENSION)
+        output_filename = input_filename[:-len(EXTENSION)]
+        if not output_filename:
+            raise Fatal('Output filename is empty')
+
+    if input_filename:
+        with open(input_filename, 'r') as f:
+            source = f.read()
+    else:
+        source = sys.stdin.read()
+
+    output = render(source, variables, die_on_missing_variable)
+
+    if output_filename and output_filename != '-':
+        with open(output_filename, 'w') as f:
+            f.write(output)
+    else:
+        sys.stdout.write(output)
+
+    if input_filename and remove_template:
+        os.unlink(input_filename)
+
+def render(source, variables, die_on_missing_variable):
+    if die_on_missing_variable:
+        undefined = jinja2.StrictUndefined
+    else:
+        undefined = jinja2.Undefined
+
+    try:
+        template = jinja2.Template(source, undefined=undefined)
+    except jinja2.TemplateSyntaxError, e:
+        raise Fatal('Syntax error on line %d: %s' % (e.lineno, e.message))
+
+    template.globals['environment'] = get_environment
+
+    try:
+        output = template.render(**variables)
+    except jinja2.UndefinedError, e:
+        raise Fatal(e)
+
+    # jinja2 cuts the last newline
+    if source.split('\n')[-1] == '' and output.split('\n')[-1] != '':
+        output += '\n'
+
+    return output
+
+@jinja2.contextfunction
+def get_environment(context, prefix=''):
+    for key, value in sorted(context.items()):
+        if not callable(value) and key.startswith(prefix):
+            yield key[len(prefix):], value
+
+class Fatal(Exception):
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ packages/envtpl/envtpl-0.3.2.tar.gz/envtpl-0.3.2/setup.py
@@ -0,0 +1,125 @@
+from setuptools import setup
+
+long_description = '''
+envtpl
+======
+
+_Render jinja2 templates on the command line with shell environment variables_
+
+Installation
+------------
+
+    pip install envtpl
+
+How-to
+------
+
+Say you have a configuration file called whatever.conf that looks like this
+
+    foo = 123
+    bar = "abc"
+
+You can use envtpl to set `foo` and `bar` from the command line by creating a file called whatever.conf.tpl
+
+    foo = {{ FOO }}
+    bar = "{{ BAR }}"
+
+If you run
+
+    FOO=123 BAR=abc envtpl < whatever.conf.tpl > whatever.conf
+
+you'll get back the original whatever.conf.
+
+You can also specify default values
+
+    foo = {{ FOO | default(123) }}
+    bar = "{{ BAR | default("abc") }}"
+
+Running
+
+    FOO=456 envtpl < whatever.conf.tpl > whatever.conf
+
+will generate
+
+    foo = 456
+    bar = "abc"
+
+This is all standard [Jinja2 syntax](http://jinja.pocoo.org/docs/templates/), so you can do things like
+
+    {% if BAZ is defined %}
+    foo = 123
+    {% else %}
+    foo = 456
+    {% endif %}
+    bar = "abc"
+
+If an environment variable is missing, envtpl will throw an error
+
+    $ echo '{{ FOO }} {{ BAR }}' | FOO=123 envtpl
+    Error: 'BAR' is undefined
+
+You can change this behaviour to insert empty strings instead by passing the `--allow-missing` flag.
+
+Instead of reading from stdin and writing to stdout, you can pass the input filename as an optional positional argument,
+and set the output filename with the `--output-file` (`-o`) argument.
+
+    envtpl -o whatever.conf  whatever.conf.tpl
+
+As a convenience, if you don't specify an output filename and the input filename ends with `.tpl`, the output filename will be the input filename without the `.tpl` extension, i.e.
+
+    envtpl whatever.conf.tpl
+    # is equivalent to
+    envtpl -o whatever.conf whatever.conf.tpl
+
+By default, envtpl will **delete** the input template file. You can keep it by passing the `--keep-template` flag.
+
+There's a special `environment(prefix='')` function that you can use as a kind of wildcard variable. If you have `hello.tpl`
+
+    hello = {{ FOO }}
+    {% for key, value in environment('MY_') %}{{ key }} = {{ value }}
+    {% endfor %}
+
+and compile it using
+
+    FOO=world MY_baz=qux MY_foo=bar envtpl hello.tpl
+
+You end up with
+
+    hello = world
+    baz = qux
+    foo = bar
+
+What's the point?
+-----------------
+
+I use this script quite a lot in Docker images. Usually I'll have the CMD execute some file, like /bin/start_container, that sets up the runtime configuration for the container by inserting environment variables into config files before starting the main process. A redis example could look like this
+
+    #!/bin/bash
+    # start_container
+
+    envtpl /etc/redis.conf.tpl
+
+    redis-server
+
+This is the use case I've optimised for, so that's why envtpl by default will delete the original template file.
+'''
+
+setup(
+    name='envtpl',
+    version='0.3.2',
+    packages=['envtpl'],
+    entry_points={
+        'console_scripts': ['envtpl = envtpl.envtpl:main']
+    },
+    install_requires=[
+        'argparse>=1.0',
+        'Jinja2>=2.7',
+    ],
+    author='Andreas Jansson',
+    author_email='andreas@jansson.me.uk',
+    description=('Render jinja2 templates on the command line using shell environment variables'),
+    license='GPL v3',
+    keywords='template environment variables parameter substitution shell jinja2 docker',
+    long_description=long_description,
+    url='https://github.com/andreasjansson/envtpl',
+)
