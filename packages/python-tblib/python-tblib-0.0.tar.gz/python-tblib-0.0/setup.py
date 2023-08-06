579d6de0e9810abc09a8e8c31baac347ff9293f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:22 +0000

    Add package python-tblib/0.0/python-tblib-0.0.tar.gz

---
+++ packages/python-tblib/python-tblib-0.0.tar.gz/python-tblib-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+    name='python-tblib',
+    version="0.0",
+    url='https://pypi.python.org/pypi/tblib/',
+    description='Traceback fiddling library.',
+    long_description='''Use `tblib <https://pypi.python.org/pypi/tblib/>`_ instead.''',
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    platforms=['all'],
+    zip_safe=False,
+    author_email='contact@ionelmc.ro',
+    install_requires=['tblib'],
+)
