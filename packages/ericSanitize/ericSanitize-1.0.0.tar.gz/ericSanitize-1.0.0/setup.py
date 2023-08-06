995d9320aaf043f8466595b9e572e5d7200276ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:27 +0000

    Add package ericSanitize/1.0.0/ericSanitize-1.0.0.tar.gz

---
+++ packages/ericSanitize/ericSanitize-1.0.0.tar.gz/ericSanitize-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'ericSanitize',
+	version = '1.0.0',
+	py_modules = ['ericSanitize'],
+	author = 'Eric Tang',
+	author_email = 'tangcongyuan@gmail.com',
+	url = 'none',
+	description = 'Process the string to replace any dashes or colons found with a period and returns the sanitized string',
+     )
