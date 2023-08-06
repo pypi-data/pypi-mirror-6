c5de2c526c639060c640a6ec8680a17e8713a23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:53 +0000

    Add package gocept.amqprun/0.12.1/gocept.amqprun-0.12.1.zip

---
+++ packages/gocept.amqprun/gocept.amqprun-0.12.1.zip/gocept.amqprun-0.12.1/setup.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gocept.amqprun',
+    version='0.12.1',
+    author='gocept <cz at gocept dot com>',
+    author_email='cz@gocept.com',
+    url='https://code.gocept.com/hg/public/gocept.amqprun',
+    description="""\
+gocept.amqprun helps you writing and running AMQP consumers, and sending AMQP
+messages. It currently only supports AMQP 0-8 and integrates with the Zope Tool
+Kit (ZTK) so you can use adapters, utilities and all the buzz.
+""",
+    long_description=(
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development'
+    ],
+    license='ZPL',
+    namespace_packages=['gocept'],
+    install_requires=[
+        'ZConfig',
+        'pika < 0.9',
+        'setuptools',
+        'transaction',
+        'zope.component[zcml]',
+        'zope.configuration',
+        'zope.event',
+        'zope.interface',
+        'zope.schema',
+    ],
+    extras_require=dict(
+        test=[
+            'amqplib',
+            'gocept.testing',
+            'mock>=0.7.0',
+            'plone.testing',
+            'tcpwatch',
+            'zope.testing',
+            ],
+        writefiles=[
+            'zope.xmlpickle',
+            ],
+        readfiles=[
+            'gocept.filestore',
+            ],
+        security=[
+            'zope.security>=4.0.0dev',
+           ]),
+    entry_points=dict(console_scripts=[
+        'server = gocept.amqprun.main:main',
+    ]),
+)
