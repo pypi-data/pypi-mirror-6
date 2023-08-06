f436cf53f0a4bc8d30c5b4ccfba8d8a0658489fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:49 +0000

    Add package hix/1.0.0/hix-1.0.0.zip

---
+++ packages/hix/hix-1.0.0.zip/hix-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='hix',
+	version='1.0.0',
+	py_modules=['hix'],
+	author='camark',
+	url='http://feisty2007.github.io',
+	description='a simple python print program'
+	)
