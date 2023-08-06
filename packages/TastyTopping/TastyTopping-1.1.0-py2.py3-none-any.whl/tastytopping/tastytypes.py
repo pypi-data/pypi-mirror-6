de04e60afab9eec9711db2c3287c52f16cfc0f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:09 +0000

    Add package TastyTopping/1.1.0/TastyTopping-1.1.0-py2.py3-none-any.whl

---
+++ packages/TastyTopping/TastyTopping-1.1.0-py2.py3-none-any.whl/tastytopping/tastytypes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+.. module: tastytypes
+    :platform: Unix, Windows
+    :synopsis: Common definitions for tastypie constants.
+
+.. moduleauthor:: Christian Boelsen <christian.boelsen@hds.com>
+"""
+
+
+RELATED = 'related'
+DATETIME = 'datetime'
+DATETIME_FORMAT1 = "%Y-%m-%dT%H:%M:%S.%f"
+DATETIME_FORMAT2 = "%Y-%m-%dT%H:%M:%S"
+TO_ONE = 'to_one'
+TO_MANY = 'to_many'
