2ab16cc6e3bc09ffedecc401093e4791279a8e70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:35 +0000

    Add package infi.wmpio/0.1.18/infi.wmpio-0.1.18.tar.gz

---
+++ packages/infi.wmpio/infi.wmpio-0.1.18.tar.gz/infi.wmpio-0.1.18/src/infi/wmpio/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.18"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.18)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '180064335186a21a165538dd27e09ecd1e41d21c'
+__git_head_subject__ = 'HIP-915 fixing 2008-only stuff'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
