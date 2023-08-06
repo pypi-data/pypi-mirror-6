07aeeeb6cd62eb24b2aecb800856dd3e82c937c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:22 +0000

    Add package django-dictpatterns/0.2/django-dictpatterns-0.2.tar.gz

---
+++ packages/django-dictpatterns/django-dictpatterns-0.2.tar.gz/django-dictpatterns-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='django-dictpatterns',
+      version='0.2',
+      description='This makes your application be able to write simple urlpatterns with dictionary for Django',
+      author='luyikei',
+      author_email='luyikei.qmltu@gmail.com',
+      url='https://github.com/luyikei/django-dictpatterns',
+      download_url='https://github.com/luyikei/django-dictpatterns/archive/0.2.zip',
+      package_dir={'dictpatterns': 'dictpatterns'},
+      packages=find_packages(),
+      include_package_data=True,
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Utilities'],
+)
