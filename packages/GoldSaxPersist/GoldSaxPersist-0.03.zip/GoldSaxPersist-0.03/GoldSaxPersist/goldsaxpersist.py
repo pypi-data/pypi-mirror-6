bb3aff8b7b3124693b276c49742ae773141ba899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package GoldSaxPersist/0.03/GoldSaxPersist-0.03.zip

---
+++ packages/GoldSaxPersist/GoldSaxPersist-0.03.zip/GoldSaxPersist-0.03/GoldSaxPersist/goldsaxpersist.py
@@ -0,0 +1,17 @@
+import sqlite3 as lite
+def push(loc,dbs,stt):
+      try:
+        loc.acquire()
+        con = lite.connect(dbs)                                   
+        con.executescript(stt)
+        con.commit()
+        con.close()
+        loc.release()
+        print(dbs, len(stt)," pushed")
+        return 0
+      except lite.OperationalError:
+        con.commit()
+        con.close()
+        loc.release()
+        print(dbs, "script error", len(stt))
+        return 0
