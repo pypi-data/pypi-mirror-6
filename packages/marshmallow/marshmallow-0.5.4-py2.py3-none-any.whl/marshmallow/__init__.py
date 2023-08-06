28f3323920378321628a8a7c5f87f6673ff174d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:16 +0000

    Add package marshmallow/0.5.4/marshmallow-0.5.4-py2.py3-none-any.whl

---
+++ packages/marshmallow/marshmallow-0.5.4-py2.py3-none-any.whl/marshmallow/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+__version__ = '0.5.4'
+__author__ = 'Steven Loria'
+__license__ = "MIT"
+
+from marshmallow.serializer import Serializer
+
+from marshmallow.utils import pprint
