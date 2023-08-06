7441e428a7503c5013786fac73d7d3462af88065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:28 +0000

    Add package lrn/0.0.5/lrn-0.0.5.tar.gz

---
+++ packages/lrn/lrn-0.0.5.tar.gz/lrn-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='lrn',
+    version='0.0.5',
+    author='Razzi Abuissa + Alex Brashear + Peter Yin',
+    author_email='py.peteryin@gmail.com',
+    packages=['lrn'],
+    scripts=['lrn/lrn'],
+)
+
