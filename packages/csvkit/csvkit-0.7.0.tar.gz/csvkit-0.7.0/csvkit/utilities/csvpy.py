fd7db2ef157e95fd48774e5fc61da5007c0b7139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:41 +0000

    Add package csvkit/0.7.0/csvkit-0.7.0.tar.gz

---
+++ packages/csvkit/csvkit-0.7.0.tar.gz/csvkit-0.7.0/csvkit/utilities/csvpy.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from csvkit import CSVKitReader, CSVKitDictReader
+from csvkit.cli import CSVFileType, CSVKitUtility 
+
+class CSVPy(CSVKitUtility):
+    description = 'Load a CSV file into a CSVKitReader object and then drops into a Python shell.'
+    override_flags = ['l', 'f', 'zero', 'H']
+
+    def add_arguments(self):
+        self.argparser.add_argument('file', metavar="FILE", type=CSVFileType(),
+            help='The CSV file to operate on.')
+        self.argparser.add_argument('--dict', dest='as_dict', action='store_true',
+            help='Use CSVKitDictReader instead of CSVKitReader.')
+
+    def main(self):
+        # Attempt reading filename, will cause lazy loader to access file and raise error if it does not exist
+        filename = self.args.file.name
+
+        if self.args.as_dict:
+            reader_class = CSVKitDictReader
+        else:
+            reader_class = CSVKitReader
+
+        reader = reader_class(self.args.file, **self.reader_kwargs)
+        
+        welcome_message = 'Welcome! "%s" has been loaded in a %s object named "reader".' % (filename, reader_class.__name__)
+
+        try:
+            from IPython.frontend.terminal.embed import InteractiveShellEmbed
+            ipy = InteractiveShellEmbed(banner1=welcome_message)
+            ipy()
+        except ImportError:
+            import code
+            code.interact(welcome_message, local={ 'reader': reader })        
+
+def launch_new_instance():
+    utility = CSVPy()
+    utility.main()
+    
+if __name__ == "__main__":
+    launch_new_instance()
+
---
+++ packages/csvkit/csvkit-0.7.0.tar.gz/csvkit-0.7.0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='csvkit',
+    version='0.7.0',
+    description='A library of utilities for working with CSV, the king of tabular file formats.',
+    long_description=open('README').read(),
+    author='Christopher Groskopf',
+    author_email='staringmonkey@gmail.com',
+    url='http://blog.apps.chicagotribune.com/',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+    ],
+    packages=[
+        'csvkit',
+        'csvkit.convert',
+        'csvkit.utilities'
+    ],
+    entry_points ={
+        'console_scripts': [
+            'csvclean = csvkit.utilities.csvclean:launch_new_instance',
+            'csvcut = csvkit.utilities.csvcut:launch_new_instance',
+            'csvgrep = csvkit.utilities.csvgrep:launch_new_instance',
+            'csvjoin = csvkit.utilities.csvjoin:launch_new_instance',
+            'csvjson = csvkit.utilities.csvjson:launch_new_instance',
+            'csvlook = csvkit.utilities.csvlook:launch_new_instance',
+            'csvpy = csvkit.utilities.csvpy:launch_new_instance',
+            'csvsort = csvkit.utilities.csvsort:launch_new_instance',
+            'csvsql = csvkit.utilities.csvsql:launch_new_instance',
+            'csvstack = csvkit.utilities.csvstack:launch_new_instance',
+            'csvstat = csvkit.utilities.csvstat:launch_new_instance',
+            'in2csv = csvkit.utilities.in2csv:launch_new_instance',
+            'sql2csv = csvkit.utilities.sql2csv:launch_new_instance'
+        ]
+    },
+    install_requires = [
+        'argparse>=1.2.1',
+        'xlrd>=0.9.2',
+        'python-dateutil>=1.5',
+        'SQLAlchemy>=0.9.3',
+        'openpyxl>=1.8.4',
+        'dbf>=0.95.004'
+    ]
+)
