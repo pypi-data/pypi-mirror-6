3a73065f839771be3075224ea586bd6f1eb12241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:11 +0000

    Add package infi.pkgmgr/0.1.6/infi.pkgmgr-0.1.6.tar.gz

---
+++ packages/infi.pkgmgr/infi.pkgmgr-0.1.6.tar.gz/infi.pkgmgr-0.1.6/src/infi/pkgmgr/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.6"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '7f20985d57cc702c405a69da27aea89b21cded4e'
+__git_head_subject__ = 'Finished release v0.1.6.'
+__git_head_message__ = '* release/v0.1.6:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements\n  update dependencies'
+__git_dirty_diff__ = ''
