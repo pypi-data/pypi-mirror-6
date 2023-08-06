f2b535227f828dd6effd1e1953bc1b512156edcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:00 +0000

    Add package django-geo/0.4.3/django-geo-0.4.3.tar.gz

---
+++ packages/django-geo/django-geo-0.4.3.tar.gz/django-geo-0.4.3/geo/admin.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+from django.contrib.admin.options import TabularInline
+from django.contrib.admin.util import unquote
+from django.core.urlresolvers import reverse
+from geo.forms import CountryForm, AreaForm, administrativeareatypeform_factory_for_country, LocationForm
+from geo.models import Country, Location, AdministrativeArea, AdministrativeAreaType, Currency
+from geo.templatetags.geo import flag
+from django.contrib.admin import ModelAdmin, site, TabularInline
+
+
+def tabular_factory(model, fields=None, inline=None, form=None, **kwargs):
+    """ factory for TabularInline
+
+    >>> class MD(IModelAdmin):
+    ...     inlines = [tabular_factory(Permission)]
+    """
+    attrs = {'model': model, 'fields': fields}
+    read_only = kwargs.pop('read_only', False)
+    if read_only:
+        attrs['readonly_fields'] = fields
+        attrs['can_delete'] = False
+
+    Inline = inline or TabularInline
+    name = "%sInLine" % model.__class__.__name__
+    if form:
+        attrs['form'] = form
+    attrs.update(kwargs)
+    Tab = type(name, (Inline,), attrs)
+    return Tab
+
+
+class ICurrency(ModelAdmin):
+    search_fields = ('name', 'code')
+    list_display = ('name', 'code', 'symbol', 'used_by')
+    inlines = [tabular_factory(Country, fields=['name'], read_only=True)]
+
+    def used_by(self, o):
+        return ', '.join(['<a href="%s">%s</>' % (reverse('admin:geo_country_change', args=[c.pk]), c.name) for c in
+                          Country.objects.filter(currency=o)])
+
+    used_by.allow_tags = True
+
+
+class AdministrativeAreaInline(TabularInline):
+    model = AdministrativeArea
+
+    def get_formset(self, request, obj=None, **kwargs):
+        self.form = administrativeareatypeform_factory_for_country(obj)
+        return super(AdministrativeAreaInline, self).get_formset(request, obj, **kwargs)
+
+
+class ICountry(ModelAdmin):
+    form = CountryForm
+    search_fields = ('name', )
+    list_display = ('name', 'continent', 'region', 'iso_code', 'iso3_code', 'currency', 'capital', 'flag')
+    list_filter = ('continent', 'region', )
+    cell_filter = ('continent', 'region', 'currency')
+    fieldsets = [(None, {'fields': (('name', 'fullname'),
+                                    ('iso_code', 'iso3_code', 'num_code'),
+                                    ('region', 'continent', 'currency'),
+                                    )})]
+    inlines = (tabular_factory(Location, exclude=('description',)),
+               AdministrativeAreaInline,
+               tabular_factory(AdministrativeAreaType), )
+
+    def flag(self, o):
+        return flag(o)
+
+    flag.allow_tags = True
+
+    def capital(self, o):
+        c = o.location_set.get_or_none(is_capital=True)
+        if c:
+            admin_url = reverse('admin:geo_location_change', args=[c.pk])
+            return "<a href='%s'>%s</a>" % (admin_url, c.name)
+        return c
+
+    capital.allow_tags = True
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        obj = self.get_object(request, unquote(object_id))
+        context = {'nodes': obj.areas.all()}
+        context.update(extra_context or {})
+        return super(ICountry, self).change_view(request, object_id, form_url, context)
+
+
+class ILocation(ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('name', 'country', 'area', 'is_administrative', 'is_capital')
+    list_display_rel_links = cell_filter = ('country', 'area', 'is_administrative', 'is_capital')
+    list_filter = ('is_administrative', 'is_capital')
+    form = LocationForm
+
+def rebuild_tree(modeladmin, request, queryset):
+    modeladmin.model.objects.rebuild()
+rebuild_tree.short_description = "Rebuild MPTT table structure"
+
+class IArea(ModelAdmin):
+    form = AreaForm
+    search_fields = ('name', )
+    list_display = ('name', 'parent', 'country', 'type', 'code')
+    list_display_rel_links = cell_filter = ('country', 'type', 'code')
+    list_filter = ('type', 'country')
+    inlines = (tabular_factory(Location),)
+    actions = [rebuild_tree,]
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        obj = self.get_object(request, unquote(object_id))
+        context = {'nodes': obj.areas.all()}
+        context.update(extra_context or {})
+        return super(IArea, self).change_view(request, object_id, form_url, context)
+
+
+
+
+class IAreaType(ModelAdmin):
+    search_fields = ('name', )
+    list_display = ('name', 'parent', 'country')
+    list_display_rel_links = cell_filter = ('country', )
+    list_filter = ('country', )
+    inlines = (tabular_factory(AdministrativeArea),)
+    actions = [rebuild_tree,]
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        obj = self.get_object(request, unquote(object_id))
+        context = {'nodes': obj.children.all()}
+        context.update(extra_context or {})
+        return super(IAreaType, self).change_view(request, object_id, form_url, context)
+
+    def rebuild_tree(self, request, queryset):
+        self.model.objects.rebuild()
+    rebuild_tree.short_description = "Rebuild MPTT table structure"
+
+
+reg = ((Country, ICountry), (Location, ILocation), (AdministrativeArea, IArea),
+       (AdministrativeAreaType, IAreaType), (Currency, ICurrency), )
+
+for model, model_admin in reg:
+    site.register(model, model_admin)
---
+++ packages/django-geo/django-geo-0.4.3.tar.gz/django-geo-0.4.3/geo/models.py
@@ -0,0 +1,309 @@
+# -*- coding: utf-8 -*-
+'''
+Created on May 7, 2010
+
+@author: sax
+'''
+from uuidfield import UUIDField
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.core.validators import MinLengthValidator, RegexValidator
+from django.db import models
+from django.db.models.manager import Manager
+from django.utils.translation import ugettext_lazy as _
+import logging
+from mptt.managers import TreeManager
+from mptt.models import MPTTModel, TreeForeignKey
+
+logger = logging.getLogger("geo")
+
+
+class CurrencyManager(Manager):
+    use_for_related_fields = True
+
+    def get_by_natural_key(self, uuid):
+        return self.get(uuid=uuid)
+
+
+class Currency(models.Model):
+    uuid = UUIDField(auto=True, blank=False, version=1, help_text=_('unique id'))
+    code = models.CharField(max_length=5, unique=True, help_text="ISO 4217 code")
+    name = models.CharField(max_length=30)
+    symbol = models.CharField(max_length=5, blank=True, null=True)
+    objects = CurrencyManager()
+
+    class Meta:
+        app_label = 'geo'
+        ordering = ['code', ]
+
+    def __unicode__(self):
+        return unicode("%s (%s)" % (self.code, self.name))
+
+    def natural_key(self):
+        return (self.uuid.hex, )
+
+
+CONTINENTS = (
+    ('AF', _('Africa')),
+    ('AN', _('Antartica')),
+    ('AS', _('Asia')),
+    ('EU', _('Europe')),
+    ('NA', _('North America')),
+    ('OC', _('Oceania')),
+    ('SA', _('South America')),
+)
+
+REGIONS = zip(range(1, 5), ('Africa', 'Americas', 'Asia', 'Middle East'))
+
+
+class CountryManager(Manager):
+    use_for_related_fields = True
+
+    def _by_continent(self, continent):
+        return self.get_query_set().filter(continent=continent)
+
+    def asia(self):
+        return self._by_continent('AS')
+
+    def europa(self):
+        return self._by_continent('EU')
+
+    def north_america(self):
+        return self._by_continent('NA')
+
+    def south_america(self):
+        return self._by_continent('SA')
+
+    def oceania(self):
+        return self._by_continent('OC')
+
+    def antartica(self):
+        return self._by_continent('AN')
+
+    def get_by_natural_key(self, iso_code):
+        return self.get(iso_code=iso_code)
+
+
+class Country(models.Model):
+    """ Model for the country of origin.
+    """
+    iso_code = models.CharField(max_length=2, unique=True, blank=False, null=False, db_index=True, default=None,
+                                help_text='ISO 3166-1 alpha 2', validators=[MinLengthValidator(2)])
+    iso3_code = models.CharField(max_length=3, unique=True, blank=False, null=False, db_index=True, default=None,
+                                 help_text='ISO 3166-1 alpha 3', validators=[MinLengthValidator(3)])
+    num_code = models.CharField(max_length=3, unique=True, blank=False, null=False, default=None,
+                                help_text='ISO 3166-1 numeric', validators=[RegexValidator('\d\d\d')])
+    uuid = UUIDField(auto=True, blank=False, version=1, help_text=_('unique id'))
+    name = models.CharField(max_length=100, db_index=True, default=None)
+    fullname = models.CharField(max_length=100, db_index=True, default=None)
+    region = models.IntegerField(choices=REGIONS, blank=True, null=True)
+    continent = models.CharField(choices=CONTINENTS, max_length=2)
+    currency = models.ForeignKey(Currency, blank=True, null=True)
+
+    fullname.alphabetic_filter = True
+    objects = CountryManager()
+
+    class Meta:
+        app_label = 'geo'
+        verbose_name_plural = _('Countries')
+        ordering = ['name']
+
+    def __unicode__(self):
+        return unicode(self.fullname)
+
+    def clean(self):
+        super(Country, self).clean()
+        self.iso_code = self.iso_code.upper()
+        self.iso3_code = self.iso3_code.upper()
+
+    def natural_key(self):
+        return (self.iso_code, )
+    natural_key.dependencies = ['geo.currency']
+
+    def __contains__(self, item):
+        if hasattr(item, 'country'):
+            return item.country.iso_code == self.iso_code
+
+    def cities(self):
+        return self.location_set.cities()
+
+
+class AdministrativeAreaTypeManager(TreeManager):
+    use_for_related_fields = True
+
+    def get_by_natural_key(self, uuid):
+        return self.get(uuid=uuid)
+
+
+class AdministrativeAreaType(MPTTModel):
+    uuid = UUIDField(auto=True, blank=False, version=1, help_text=_('unique id'))
+    name = models.CharField(_('Name'), max_length=100, db_index=True)
+    country = models.ForeignKey(Country)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+    objects = AdministrativeAreaTypeManager()
+
+    class Meta:
+        verbose_name = _("Administrative Area Type")
+        verbose_name_plural = _("Administrative Area Types")
+        app_label = 'geo'
+        ordering = ['name']
+        order_with_respect_to = 'country'
+        unique_together = (('country', 'name'),)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+    def __contains__(self, item):
+        if isinstance(item, AdministrativeAreaType) and item.is_child_node():
+            return item.is_descendant_of(self)
+
+    def natural_key(self):
+        return (self.uuid.hex, )
+    natural_key.dependencies = ['geo.country']
+
+    def clean(self):
+        if self.parent == self:
+            raise ValidationError(_('`%s` cannot contains same type') % self.parent)
+        super(AdministrativeAreaType, self).clean()
+
+    def get_or_create(self, **kwargs):
+        return AdministrativeArea.objects.get_or_create(country=self.country, type=self, **kwargs)
+
+
+class AdministrativeAreaManager(TreeManager):
+    use_for_related_fields = True
+
+    def get_by_natural_key(self, uuid):
+        return self.get(uuid=uuid)
+
+
+class AdministrativeArea(MPTTModel):
+    """ Administrative areas that can contains other AdministrativeArea and/or Location.
+    """
+
+    uuid = UUIDField(auto=True, blank=False, version=1, help_text=_('unique id'))
+    name = models.CharField(_('Name'), max_length=255, db_index=True)
+    code = models.CharField(_('Code'), max_length=10, blank=True, null=True, db_index=True, help_text='ISO 3166-2 code')
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='areas')
+    country = models.ForeignKey(Country, related_name='areas')
+    type = models.ForeignKey(AdministrativeAreaType)
+    objects = AdministrativeAreaManager()
+
+    class Meta:
+        verbose_name = _("Administrative Area")
+        verbose_name_plural = _("Administrative Areas")
+        unique_together = (('name', 'country', 'type'),)
+        app_label = 'geo'
+        ordering = ['name']
+        order_with_respect_to = 'country'
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+    def natural_key(self):
+        return (self.uuid.hex, )
+    natural_key.dependencies = ['geo.administrativeareatype', 'geo.country']
+
+    def clean(self):
+        if self.parent == self:
+            raise ValidationError(_('`%s` cannot contains self') % self)
+        if self.parent and self.parent.type == self.type:
+            raise ValidationError(_('`%s` cannot contains same type') % self.parent.type)
+        if (self.pk and self.parent) and self.parent in self:
+            raise ValidationError(_('`%s` cannot contains `%s`') % (self, self.parent))
+        super(AdministrativeArea, self).clean()
+
+    def save(self, *args, **kwargs):
+        if not self.country_id:
+            self.country = self.parent.country
+        self.clean()
+        super(AdministrativeArea, self).save(*args, **kwargs)
+
+    def __contains__(self, item):
+        if isinstance(item, AdministrativeArea) and item.is_child_node():
+            return item.is_descendant_of(self)
+        elif isinstance(item, Location) and item.area:
+            return item.area.is_descendant_of(self)
+
+
+class LocationTypeManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_by_natural_key(self, uuid):
+        return self.get(uuid=uuid)
+
+
+class LocationType(models.Model):
+    """Type of the location (city, village, place, locality, neighbourhood, etc.)
+    This is not intended to contain anything inside it.
+    """
+    uuid = UUIDField(auto=True, blank=False, version=1, help_text=_('unique id'))
+    description = models.CharField(unique=True, max_length=100)
+    objects = LocationTypeManager()
+
+    class Meta:
+        verbose_name_plural = _('Location Types')
+        verbose_name = _('Location Type')
+        app_label = 'geo'
+
+    def natural_key(self):
+        return (self.uuid.hex, )
+
+
+class LocationManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_or_none(self, *args, **kwargs):
+        try:
+            return self.get(*args, **kwargs)
+        except ObjectDoesNotExist:
+            return None
+
+    def get_by_natural_key(self, uuid):
+        return self.get(uuid=uuid)
+
+
+class Location(models.Model):
+    """Administrative location (city, place everything with a name and Lat/Lng that
+    is not intended to contain anything; use Areas for that).
+    """
+    NONE = 0
+    COUNTRY = 10
+    EXACT = 20
+
+    ACCURACY = (
+        (NONE, _('None')),
+        (COUNTRY, _('Country')),
+        (EXACT, _('Exact')))
+
+    country = models.ForeignKey(Country, db_index=True)
+    area = models.ForeignKey(AdministrativeArea, db_index=True, blank=True, null=True)
+    type = models.ForeignKey(LocationType, blank=True, null=True)
+    is_capital = models.BooleanField(default=False, help_text="True if is the capital of `country`")
+    is_administrative = models.BooleanField(default=False, help_text="True if is administrative for `area`")
+    uuid = UUIDField(auto=True, blank=False, version=1, help_text=_('unique id'))
+    name = models.CharField(_('Name'), max_length=255, db_index=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    lat = models.DecimalField(max_digits=18, decimal_places=12, blank=True, null=True)
+    lng = models.DecimalField(max_digits=18, decimal_places=12, blank=True, null=True)
+    acc = models.IntegerField(choices=ACCURACY, default=NONE, blank=True, null=True, help_text="Define the level of accuracy of lat/lng infos")
+    objects = LocationManager()
+
+    class Meta:
+        verbose_name_plural = _('Locations')
+        verbose_name = _('Location')
+        app_label = 'geo'
+        ordering = ('name', 'country', )
+        order_with_respect_to = 'country'
+        unique_together = (('area', 'name'), )
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+    def natural_key(self):
+        return (self.uuid.hex, )
+    natural_key.dependencies = ['geo.administrativearea', 'geo.country', 'geo.locationtype']
+
+    def clean(self):
+        if self.area and self.area.country != self.country:
+            raise ValidationError('Selected area not in selected country')
+        super(Location, self).clean()
