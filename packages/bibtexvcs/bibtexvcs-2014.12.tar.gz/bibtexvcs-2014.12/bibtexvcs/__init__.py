fc5cda21e77b249759cd55011f19c75a0c0e8300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:06 +0000

    Add package bibtexvcs/2014.12/bibtexvcs-2014.12.tar.gz

---
+++ packages/bibtexvcs/bibtexvcs-2014.12.tar.gz/bibtexvcs-2014.12/bibtexvcs/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2014 Michael Helmling
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation
+
+"""BibTeX VCS main package."""
+from __future__ import division, print_function, unicode_literals
+import sys
+
+__version__ = '2014.12'
+
+
+def pypiVersion():
+    """Return the current version of this package on PyPI."""
+    if sys.version_info.major == 2:
+        import urllib2
+        from urllib2 import URLError
+        urlopen = urllib2.urlopen
+    else:
+        import urllib.request
+        from urllib.error import URLError
+        urlopen = urllib.request.urlopen
+    try:
+        with urlopen('https://pypi.python.org/pypi/bibtexvcs/json') as f:
+            data = f.read().decode()
+    except URLError as e:
+        return None
+    import json
+    decoded = json.loads(data)
+    return decoded['info']['version']
