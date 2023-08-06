ca72dc3b9535ac80912ad78d821a23facf6c6a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:08 +0000

    Add package tempodb-pandas/0.1.1/tempodb-pandas-0.1.1.tar.gz

---
+++ packages/tempodb-pandas/tempodb-pandas-0.1.1.tar.gz/tempodb-pandas-0.1.1/setup.py
@@ -0,0 +1,26 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': "Pandas functions for converting between Tempo data types",
+    'author': 'Parnell Springmeyer',
+    'url': 'https://github.com/ixmatus/tempodb-pandas',
+    'download_url': 'https://github.com/ixmatus/tempodb-pandas',
+    'author_email': 'parnell@ixmat.us',
+    'version': '0.1.1',
+    'install_requires': [
+        'nose',
+        'numpy >= 1.8.1',
+        'pandas >= 0.13.1',
+        'tempodb >= 0.5.0',
+        'six'
+    ],
+    'packages': ['tempodb_pandas'],
+    'scripts': [],
+    'name': 'tempodb-pandas'
+}
+
+setup(**config)
