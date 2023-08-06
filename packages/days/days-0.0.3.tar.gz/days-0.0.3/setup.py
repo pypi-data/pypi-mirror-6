4e45e18eae35739976e091708fea7b4a2c444704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:29 +0000

    Add package days/0.0.3/days-0.0.3.tar.gz

---
+++ packages/days/days-0.0.3.tar.gz/days-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup	
+
+setup(
+	name='days',
+	version='0.0.3',
+	description='shortcuts to get today, yesterday & tommorrow',
+	author='jarvys',
+	author_email='yuhan534@126.com',
+	url='https://github.com/jarvys/days',
+	install_requires=[],
+	py_modules=['days'],
+	scripts=[],
+	keywords=['date', 'today', 'yesterday', 'tommorrow']
+)
+
