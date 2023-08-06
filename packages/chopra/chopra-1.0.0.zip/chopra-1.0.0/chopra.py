1cce0a8b93ebc13a5057d548a4b47780121f7d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:42 +0000

    Add package chopra/1.0.0/chopra-1.0.0.zip

---
+++ packages/chopra/chopra-1.0.0.zip/chopra-1.0.0/chopra.py
@@ -0,0 +1,10 @@
+'''
+Created on Mar 26, 2014
+
+@author: Sanketh
+'''
+
+if __name__ == '__main__':
+    pass
+
+print('hello chopra ji')
---
+++ packages/chopra/chopra-1.0.0.zip/chopra-1.0.0/setup.py
@@ -0,0 +1,15 @@
+'''
+Created on Mar 26, 2014
+
+@author: Sanketh
+'''
+from distutils.core import setup
+
+setup(name='chopra',
+      version='1.0.0',
+      py_modules =['chopra'],
+      author ='sanketh',
+      author_email = 'sankethshetty007@gamil.com',
+      url='',
+      description =''
+      )
