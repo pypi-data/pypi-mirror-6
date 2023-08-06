7182f3010962a3a4b39a7ed1957c3f91d5f096b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:34 +0000

    Add package monty/0.1.3/monty-0.1.3-py2.7.egg

---
+++ packages/monty/monty-0.1.3-py2.7.egg/monty/__init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Shyue Ping Ong'
+__copyright__ = 'Copyright 2014, The Materials Virtual Lab'
+__version__ = '0.1.3'
+__maintainer__ = 'Shyue Ping Ong'
+__email__ = 'ongsp@ucsd.edu'
+__date__ = '2/13/14'
+
