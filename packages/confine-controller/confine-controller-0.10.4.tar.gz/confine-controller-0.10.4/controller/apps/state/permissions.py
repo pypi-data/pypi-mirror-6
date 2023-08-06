f9ea937feda2aedd8bf5a8cd41225bfe589d23a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:22 +0000

    Add package confine-controller/0.10.4/confine-controller-0.10.4.tar.gz

---
+++ packages/confine-controller/confine-controller-0.10.4.tar.gz/confine-controller-0.10.4/controller/apps/state/permissions.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+from permissions import ReadOnlyPermission
+
+from .models import State, StateHistory
+
+
+class StatePermission(ReadOnlyPermission):
+    """ Allow delete because this is a node related object """
+    def delete(self, obj, cls, user):
+        if obj is None:
+            return user.has_roles(('admin', 'technician'))
+        return obj.node.group.has_roles(user, roles=['admin', 'technician'])
+
+
+State.has_permission = StatePermission()
+StateHistory.has_permission = StatePermission()
