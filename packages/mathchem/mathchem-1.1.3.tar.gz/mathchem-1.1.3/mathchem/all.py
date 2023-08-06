a4ea91c226d4e0c39d9faebe7654dc8fd836d601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:16 +0000

    Add package mathchem/1.1.3/mathchem-1.1.3.tar.gz

---
+++ packages/mathchem/mathchem-1.1.3.tar.gz/mathchem-1.1.3/mathchem/all.py
@@ -0,0 +1,9 @@
+#*****************************************************************************
+#       Copyright (C) 2011 Alexander Vasilyev <hamster3d@gmail.com>
+#
+#              Distributed under the terms of the MIT License
+#*****************************************************************************
+
+
+from mathchem import *
+from utilities import *
