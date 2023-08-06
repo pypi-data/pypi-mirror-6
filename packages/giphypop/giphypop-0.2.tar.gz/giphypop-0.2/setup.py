7cbd09e29eb0fe2cf0a53263b28ecab0a018faeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:19 +0000

    Add package giphypop/0.2/giphypop-0.2.tar.gz

---
+++ packages/giphypop/giphypop-0.2.tar.gz/giphypop-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='giphypop',
+      version='0.2',
+      description=("Python wrapper for Giphy API"),
+      long_description=open('README.rst').read(),
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+      keywords='python giphy api',
+      author='Shaun Duncan',
+      author_email='shaun.duncan@gmail.com',
+      url='http://www.github.com/shaunduncan/giphypop/',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['requests'],
+      py_modules=['giphypop'],
+      )
