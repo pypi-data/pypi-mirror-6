0640c9200eeca4a2b2a64f3b4850914649e90b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:50 +0000

    Add package pyread/0.8.3/pyread-0.8.3.tar.gz

---
+++ packages/pyread/pyread-0.8.3.tar.gz/pyread-0.8.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pyread',
+      version='0.8.3',
+      description='Automatically guessing file properties and loading data.',
+      url='https://www.linkedin.com/profile/view?id=190745232',
+      author='Pascal van Kooten',
+      author_email='kootenpv@gmail.com',
+      license='GPL',
+      packages=['pyread'],
+      install_requires=[
+          'numpy',
+          'pandas'
+      ],
+      zip_safe=False)
