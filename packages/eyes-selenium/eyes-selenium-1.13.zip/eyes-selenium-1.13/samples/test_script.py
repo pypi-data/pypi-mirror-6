d41d6183b5e6e8c28de799eff46cba323863b210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:13 +0000

    Add package eyes-selenium/1.13/eyes-selenium-1.13.zip

---
+++ packages/eyes-selenium/eyes-selenium-1.13.zip/eyes-selenium-1.13/samples/test_script.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+import time
+from applitools import logger
+from applitools.logger import StdoutLogger
+from applitools.eyes import Eyes
+
+# import os
+#os.environ['HTTPS_PROXY'] = "http://localhost:8888"
+
+Eyes.api_key = 'YOUR_API_KEY'
+driver = webdriver.Firefox()
+
+logger.set_logger(StdoutLogger())
+eyes = Eyes()
+try:
+    eyes.open(driver, "Python app", "applitools", {'width': 800, 'height': 600})
+    driver.get('http://www.applitools.com')
+    eyes.check_window("initial")
+
+    pricing_element = driver.find_element_by_css_selector("li.pricing a")
+    eyes.check_region_by_element(pricing_element, "pricing page")
+
+    pricing_element.click()
+    eyes.check_window("pricing page")
+
+    driver.find_element_by_css_selector("li.contact-us a").click()
+    eyes.check_window("contact us page")
+    driver.find_element_by_class_name("input-name").send_keys("my name is what?")
+    eyes.check_window("name input")
+    eyes.close()
+finally:
+    driver.quit()
+    eyes.abort_if_not_closed()
