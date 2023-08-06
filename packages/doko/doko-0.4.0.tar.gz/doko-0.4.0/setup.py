806f86c0b951f76ae774613ad565753e57bff994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:42 +0000

    Add package doko/0.4.0/doko-0.4.0.tar.gz

---
+++ packages/doko/doko-0.4.0.tar.gz/doko-0.4.0/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+#  setup.py
+#  doko
+#
+
+"""
+Package information for doko package.
+"""
+
+from setuptools import setup
+
+VERSION = '0.4.0'
+
+requires = [
+    'PyYAML==3.11',
+    'geoip2>=0.5.1',
+    'requests>=2.2.1',
+]
+
+corelocation_requires = [
+    'pyobjc>=2.4',
+    'pyobjc-core>=2.4',
+    'pyobjc-framework-CoreLocation>=2.4',
+]
+
+setup(
+    name='doko',
+    description="Detect your current location.",
+    long_description=open('README.rst').read(),
+    url="http://github.com/larsyencken/doko/",
+    version=VERSION,
+    author="Lars Yencken",
+    author_email="lars@yencken.org",
+    license="ISC",
+    packages=[
+        'doko',
+    ],
+    entry_points={
+        'console_scripts': [
+            'doko = doko:main',
+            'doko-landmark = doko.landmark:main',
+        ],
+    },
+    install_requires=requires,
+    extras_require={
+        'corelocation': corelocation_requires,
+    }
+)
