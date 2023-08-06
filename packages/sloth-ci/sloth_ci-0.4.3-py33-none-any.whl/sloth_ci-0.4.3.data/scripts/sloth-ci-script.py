d2c9374488622c4e7906538b3e82b30dc25cc5f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:23 +0000

    Add package sloth-ci/0.4.3/sloth_ci-0.4.3-py33-none-any.whl

---
+++ packages/sloth-ci/sloth_ci-0.4.3-py33-none-any.whl/sloth_ci-0.4.3.data/scripts/sloth-ci-script.py
@@ -0,0 +1,10 @@
+#!python
+# EASY-INSTALL-ENTRY-SCRIPT: 'sloth-ci==0.4.3','console_scripts','sloth-ci'
+__requires__ = 'sloth-ci==0.4.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('sloth-ci==0.4.3', 'console_scripts', 'sloth-ci')()
+    )
