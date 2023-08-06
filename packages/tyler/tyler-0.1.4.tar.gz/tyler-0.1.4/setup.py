b1657a832217775288547af8c02516bb1189e7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:44 +0000

    Add package tyler/0.1.4/tyler-0.1.4.tar.gz

---
+++ packages/tyler/tyler-0.1.4.tar.gz/tyler-0.1.4/setup.py
@@ -0,0 +1,23 @@
+'''setup.py for tyler'''
+from setuptools import setup, find_packages
+from tyler.version import __VERSION__
+
+setup(
+    name                 = 'tyler',
+    version              = __VERSION__,
+    description          = 'Python webservice for tiling static maps',
+    author               = 'Ben Cardy',
+    author_email         = 'benbacardi@gmail.com',
+    packages             = find_packages(),
+    install_requires     = ['django', 'requests', 'Pillow',],
+    include_package_data = True,
+    classifiers          = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
