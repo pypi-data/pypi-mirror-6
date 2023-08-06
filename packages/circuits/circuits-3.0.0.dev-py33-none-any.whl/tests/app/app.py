c17e1f8e23af122f0d4c254911e77d8baf878477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:47 +0000

    Add package circuits/3.0.0.dev/circuits-3.0.0.dev-py33-none-any.whl

---
+++ packages/circuits/circuits-3.0.0.dev-py33-none-any.whl/tests/app/app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from coverage import coverage
+    HAS_COVERAGE = True
+except ImportError:
+    HAS_COVERAGE = False
+
+from circuits import Component
+from circuits.app import Daemon
+
+
+class App(Component):
+
+    def __init__(self, pidfile):
+        super(App, self).__init__()
+
+        Daemon(pidfile).register(self)
+
+    def prepare_unregister(self, *args):
+        return
+
+
+def main():
+    if HAS_COVERAGE:
+        _coverage = coverage(data_suffix=True)
+        _coverage.start()
+
+    pidfile = os.path.abspath(sys.argv[1])
+    app = App(pidfile)
+    app.run()
+
+    if HAS_COVERAGE:
+        _coverage.stop()
+        _coverage.save()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/circuits/circuits-3.0.0.dev-py33-none-any.whl/tests/app/test_daemon.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import pytest
+if pytest.PLATFORM == "win32":
+    pytest.skip("Unsupported Platform")
+
+import os
+import sys
+import errno
+from time import sleep
+from signal import SIGTERM
+from subprocess import Popen
+
+from tests.app import app
+
+
+def test(tmpdir):
+    tmpdir.ensure("app.pid")
+    pid_path = tmpdir.join("app.pid")
+
+    args = [sys.executable, app.__file__, str(pid_path)]
+    Popen(args, env={'PYTHONPATH': ':'.join(sys.path)}).wait()
+
+    sleep(1)
+
+    assert pid_path.check(exists=True, file=True)
+
+    pid = None
+    with pid_path.open() as f:
+        pid = int(f.read().strip())
+
+    assert isinstance(pid, int)
+    assert pid > 0
+
+    os.kill(pid, SIGTERM)
+    try:
+        os.waitpid(pid, os.WTERMSIG(0))
+    except OSError as e:
+        assert e.args[0] == errno.ECHILD
