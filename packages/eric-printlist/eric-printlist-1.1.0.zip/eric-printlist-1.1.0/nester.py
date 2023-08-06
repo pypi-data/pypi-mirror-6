06c2fa4905353de509bfb3f10df883b74af99de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:32 +0000

    Add package eric-printlist/1.1.0/eric-printlist-1.1.0.zip

---
+++ packages/eric-printlist/eric-printlist-1.1.0.zip/eric-printlist-1.1.0/nester.py
@@ -0,0 +1,14 @@
+'''
+nester.py
+打印一个list，list中可以包含其他list。
+'''
+
+def print_lol(the_list, level=0):
+    '''print_lol函数打印list。'''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for num in range(level):
+                print('\t', end="")
+            print(each_item)
---
+++ packages/eric-printlist/eric-printlist-1.1.0.zip/eric-printlist-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'eric-printlist',
+    version         = '1.1.0',
+    py_modules      = ['nester'],
+    author          = 'eric he',
+    author_email    = 'baijun.he@163.com',
+    url             = 'http://www.hebaijun.com',
+    description     = '打印list的模块'
+    )
