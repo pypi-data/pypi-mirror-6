a0af506a23110df89f88a72f513b3393f8fb2136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:30 +0000

    Add package shiori/0.2.1/shiori-0.2.1.tar.gz

---
+++ packages/shiori/shiori-0.2.1.tar.gz/shiori-0.2.1/docs/HISTORY.rst
@@ -0,0 +1,19 @@
+History
+-------
+
+0.2.1 (2014-02-16)
+^^^^^^^^^^^^^^^^^^
+
+* Remove django-notification from dependency
+
+0.2.0 (2014-02-16)
+^^^^^^^^^^^^^^^^^^
+
+* Added feed generator
+* Fixed some bugs related views
+
+0.1.0 (2014-02-08)
+^^^^^^^^^^^^^^^^^^
+
+* First release
+
---
+++ packages/shiori/shiori-0.2.1.tar.gz/shiori-0.2.1/setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+    Copyright (C) 2013, 2014 Kouhei Maeda <mkouhei@palmtb.net>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import sys
+from setuptools import setup
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: "
+    "GNU General Public License v3 or later (GPLv3+)",
+    "Programming Language :: Python",
+    "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+]
+
+long_description = \
+    open(os.path.join("docs", "README.rst")).read() + \
+    open(os.path.join("docs", "HISTORY.rst")).read() + \
+    open(os.path.join("docs", "TODO.rst")).read()
+
+requires = ['setuptools',
+            'django',
+            'djangorestframework',
+            'django-shortuuidfield',
+            'python-openid',
+            'django-openid-auth']
+
+setup(name='shiori',
+      version='0.2.1',
+      description='bookmarking tool based on Web UI and JSON REST API',
+      long_description=long_description,
+      author='Kouhei Maeda',
+      author_email='mkouhei@palmtb.net',
+      url='https://github.com/mkouhei/shiori',
+      license='GNU General Public License version 3',
+      classifiers=classifiers,
+      packages=['shiori'],
+      data_files=[],
+      install_requires=requires,
+      include_package_data=True,
+      extras_require=dict(test=['pytest', 'pep8'],),
+      test_suite='tests.runtest',
+      tests_require=['pytest', 'pep8'])
