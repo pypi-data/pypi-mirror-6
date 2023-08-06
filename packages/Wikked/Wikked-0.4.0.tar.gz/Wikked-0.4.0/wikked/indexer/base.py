c45b10ab8867534d5765d5b019bcef2aefee5ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:19 +0000

    Add package Wikked/0.4.0/Wikked-0.4.0.tar.gz

---
+++ packages/Wikked/Wikked-0.4.0.tar.gz/Wikked-0.4.0/wikked/indexer/base.py
@@ -0,0 +1,34 @@
+
+
+class HitResult(object):
+    def __init__(self, url, title, hl_text=None):
+        self.url = url
+        self.title = title
+        self.hl_text = hl_text
+
+
+class WikiIndex(object):
+    def __init__(self):
+        pass
+
+    def start(self, wiki):
+        pass
+
+    def init(self, wiki):
+        pass
+
+    def postInit(self):
+        pass
+
+    def reset(self, pages):
+        pass
+
+    def updatePage(self, page):
+        pass
+
+    def updateAll(self, pages):
+        pass
+
+    def search(self, query):
+        raise NotImplementedError()
+
