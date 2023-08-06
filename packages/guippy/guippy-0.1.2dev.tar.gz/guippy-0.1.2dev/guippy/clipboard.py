9352354636831c08d52ade6c8c17b2449e26b52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:30 +0000

    Add package guippy/0.1.2dev/guippy-0.1.2dev.tar.gz

---
+++ packages/guippy/guippy-0.1.2dev.tar.gz/guippy-0.1.2dev/guippy/clipboard.py
@@ -0,0 +1,73 @@
+#-*- coding: utf-8 -*-
+"""For clipboard.
+"""
+import ctypes
+from .api import (
+    HWND,
+    GlobalLock,
+    OpenClipboard,
+    GetClipboardData,
+    CF_UNICODETEXT,
+    GlobalUnlock,
+    CloseClipboard,
+    GlobalAlloc,
+    GMEM_DDESHARE,
+    GHND,
+    wcscpy_s,
+    EmptyClipboard,
+    SetClipboardData,
+    CF_TEXT,
+    )
+from .decorator import interval
+
+
+def _get_unicode_as_globaldata(data):
+    org_restype = GlobalLock.restype
+    hdata = None
+    try:
+        length = len(data)
+        hdata = GlobalAlloc(GMEM_DDESHARE|GHND, length+1)
+        ptr = GlobalLock(hdata)
+        try:
+            wcscpy_s(ctypes.c_wchar_p(ptr), length, data)
+        finally:
+            GlobalUnlock(hdata)
+    finally:
+        GlobalLock.restype = org_restype
+    return hdata
+
+class Clipboard(object):
+    """Control clipboard."""
+
+    @staticmethod
+    def set(data):
+        """Copy a data to clipboard."""
+        hdata = _get_unicode_as_globaldata(data)
+        OpenClipboard(None)
+        try:
+            EmptyClipboard()
+            SetClipboardData(CF_TEXT, hdata)
+        finally:
+            CloseClipboard()
+
+    @staticmethod
+    def get(hwnd=None):
+        """Get a clipboard data."""
+        res = None
+        if hwnd is None:
+            hwnd = HWND(0)
+
+        org_restype = GlobalLock.restype
+
+        OpenClipboard(hwnd)
+        try:
+            hmem = GetClipboardData(CF_UNICODETEXT)
+            GlobalLock.restype = ctypes.c_wchar_p
+            try:
+                res = GlobalLock(ctypes.c_int(hmem)) # return unicode text
+            finally:
+                GlobalUnlock(hmem)
+        finally:
+            GlobalLock.restype = org_restype
+            CloseClipboard()
+        return  res
