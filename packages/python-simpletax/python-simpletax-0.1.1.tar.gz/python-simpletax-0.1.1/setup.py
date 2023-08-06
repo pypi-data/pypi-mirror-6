80a34fc4b9cbb898bc181a698e10fe4a48fffc7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:45 +0000

    Add package python-simpletax/0.1.1/python-simpletax-0.1.1.tar.gz

---
+++ packages/python-simpletax/python-simpletax-0.1.1.tar.gz/python-simpletax-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='python-simpletax',
+    version='0.1.1',
+    author='Celso Pinto',
+    author_email='celso@gosimpletax.com',
+    packages=['simpletax'],
+    url='http://github.org/leftfield/python-simpletax/',
+    license='LICENSE.txt',
+    description='Python API client to gosimpletax.com',
+)
