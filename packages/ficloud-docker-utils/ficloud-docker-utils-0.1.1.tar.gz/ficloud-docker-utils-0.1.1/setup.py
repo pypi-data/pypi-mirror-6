d77f17ac3251d9f24ddd73a8a404bc1e827568b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:40 +0000

    Add package ficloud-docker-utils/0.1.1/ficloud-docker-utils-0.1.1.tar.gz

---
+++ packages/ficloud-docker-utils/ficloud-docker-utils-0.1.1.tar.gz/ficloud-docker-utils-0.1.1/setup.py
@@ -0,0 +1,16 @@
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ficloud-docker-utils',
+    version='0.1.1',
+    packages=find_packages(exclude=("test.*",)),
+
+    url='https://github.com/pywizard/ficloud-docker-utils',
+    license='MIT',
+    author='Aleksandr Rudakov',
+    author_email='ribozz@gmail.com',
+    description='Tool that implements chef/puppet -like approach for server management.',
+    long_description=''
+)
