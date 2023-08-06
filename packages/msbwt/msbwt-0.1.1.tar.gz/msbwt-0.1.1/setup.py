2f6a90d9e3847dea4018f06e16ebd491c5ef394e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package msbwt/0.1.1/msbwt-0.1.1.tar.gz

---
+++ packages/msbwt/msbwt-0.1.1.tar.gz/msbwt-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='msbwt',
+      version='0.1.1',
+      description='Allows for merging and querying of multi-string BWTs',
+      url='http://code.google.com/p/suspenders',
+      author='James Holt',
+      author_email='holtjma@cs.unc.edu',
+      license='MIT',
+      packages=['src/MUS'],
+      install_requires=['pysam'],
+      scripts=['bin/msbwt'],
+      zip_safe=False)
