03ec595443f46cc15bbfa6338ec1806ca28c211c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:32 +0000

    Add package lrn/0.0.9/lrn-0.0.9.tar.gz

---
+++ packages/lrn/lrn-0.0.9.tar.gz/lrn-0.0.9/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lrn',
+    version='0.0.9',
+    author='Razzi Abuissa + Alex Brashear + Peter Yin',
+    author_email='py.peteryin@gmail.com',
+    url='https://github.com/lrn-guru',
+    packages=['lrn'],
+    scripts=['lrn/lrn'],
+)
+
