d9b8d03ab055814dadd2a8e937ec06688e68476c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:16 +0000

    Add package infi.wmpio/0.1.20/infi.wmpio-0.1.20-py2.7.egg

---
+++ packages/infi.wmpio/infi.wmpio-0.1.20-py2.7.egg/infi/wmpio/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.20"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.20)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '40c66005e5bec1784f3cb1dbc076972aecd8af10'
+__git_head_subject__ = 'Finished release v0.1.20.'
+__git_head_message__ = '* release/v0.1.20:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements\n  update dependencies'
+__git_dirty_diff__ = ''
