78a75b638a6169ca1e7482f332bab09be4f692b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:14 +0000

    Add package simple-models/0.1.0/simple-models-0.1.0.tar.gz

---
+++ packages/simple-models/simple-models-0.1.0.tar.gz/simple-models-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='simple-models',
+    version='0.1.0',
+    packages=['simplemodels'],
+    url='https://github.com/prawn-cake/simple-models.git',
+    license='MIT',
+    author='prawn-cake',
+    author_email='me@multichannel.net',
+    description='Dict-like simple models for structure your any internal models. Useful for API or for any data which prefer to be structured '
+)
