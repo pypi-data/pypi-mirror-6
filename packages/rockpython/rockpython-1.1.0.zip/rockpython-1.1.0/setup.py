ea2893166b8f9071026611d8c23379c8a41d4fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:34 +0000

    Add package rockpython/1.1.0/rockpython-1.1.0.zip

---
+++ packages/rockpython/rockpython-1.1.0.zip/rockpython-1.1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'rockpython',
+    version = '1.1.0',
+    py_modules = 'rockpython.py',
+    author = 'Sounak',
+    author_email = 'sounak.mdl@gmail.com',
+    url ='www.google.com',
+    description = 'This is an aggressive but small code',
+
+    )
