446bbec5f5a81f24d35286c0a792aa6fd030bfa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:31 +0000

    Add package infi.docopt_completion/0.2.1/infi.docopt_completion-0.2.1.tar.gz

---
+++ packages/infi.docopt_completion/infi.docopt_completion-0.2.1.tar.gz/infi.docopt_completion-0.2.1/src/infi/docopt_completion/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.1"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.2.1)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '98064557513d81ba5650451b0ff21d55d1c24c16'
+__git_head_subject__ = 'Finished release v0.2.1.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
