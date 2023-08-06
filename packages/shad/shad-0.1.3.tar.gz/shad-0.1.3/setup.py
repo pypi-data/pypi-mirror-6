a9d89c55e1de4d4ff37e215a419caa87e6ec4b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:45 +0000

    Add package shad/0.1.3/shad-0.1.3.tar.gz

---
+++ packages/shad/shad-0.1.3.tar.gz/shad-0.1.3/setup.py
@@ -0,0 +1,41 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+packages = [
+    'shad',
+]
+
+requires = ['requests>=1.2.0']
+
+setup(
+    name='shad',
+    version='0.1.3',
+    description='Generic restful api -> Python function call adaptor',
+    long_description=read('README.md'),
+    author='Albert O\'Connor',
+    author_email='info@albertoconnor.ca',
+    url='https://bitbucket.org/amjoconn/shad',
+    packages=packages,
+    package_data={'': ['README.md', 'LICENSE', 'NOTICE']},
+    package_dir={},
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT',
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
