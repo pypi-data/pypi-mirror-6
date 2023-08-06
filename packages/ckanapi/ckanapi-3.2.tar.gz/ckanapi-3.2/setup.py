7e38ade07d8213c66b3dfd3f0555b9b0b4d3a721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package ckanapi/3.2/ckanapi-3.2.tar.gz

---
+++ packages/ckanapi/ckanapi-3.2.tar.gz/ckanapi-3.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='ckanapi',
+    version='3.2',
+    description=
+        'A command line interface and Python module for '
+        'accessing the CKAN Action API',
+    license='MIT',
+    author='Ian Ward',
+    author_email='ian@excess.org',
+    url='https://github.com/ckan/ckanapi',
+    packages=[
+        'ckanapi',
+        'ckanapi.tests',
+        'ckanapi.tests.mock',
+        'ckanapi.cli',
+        ],
+    test_suite='ckanapi.tests',
+    zip_safe=False,
+    entry_points = """
+        [console_scripts]
+        ckanapi=ckanapi.cli.main:main
+
+        [paste.paster_command]
+        ckanapi=ckanapi.cli.paster:CKANAPICommand
+        """
+    )
