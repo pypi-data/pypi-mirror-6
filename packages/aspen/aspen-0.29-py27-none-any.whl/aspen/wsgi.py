4a5a0ef0818f24d82ddabb47b73f4f5e9dcd280c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:27 +0000

    Add package aspen/0.29/aspen-0.29-py27-none-any.whl

---
+++ packages/aspen/aspen-0.29-py27-none-any.whl/aspen/wsgi.py
@@ -0,0 +1,16 @@
+"""Provide a WSGI callable.
+
+It would be a little nicer if this was at aspen:wsgi instead of
+aspen.wsgi:website, but then Website would be instantiated even if we don't
+want it to be. Here, it's only instantiated when someone passes this to
+gunicorn, spawning, etc.
+
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from aspen.website import Website
+
+website = Website([])
