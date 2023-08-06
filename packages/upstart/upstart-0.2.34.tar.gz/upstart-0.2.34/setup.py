9a19ce99b14dc7673aa8962b256cb01ef7d4c87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:47 +0000

    Add package upstart/0.2.34/upstart-0.2.34.tar.gz

---
+++ packages/upstart/upstart-0.2.34.tar.gz/upstart-0.2.34/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+
+long_description=\
+"An intuitive library interface to Upstart for service and job management. "\
+"Requires the python-dbus Ubuntu package or equivalent."
+
+setup(name='upstart',
+      version='0.2.34',#versioneer.get_version(),
+      description="Upstart-based service management.",
+      long_description=long_description,
+      classifiers=[],
+      keywords='upstart dbus',
+      author='Dustin Oprea',
+      author_email='myselfasunder@gmail.com',
+      url='https://github.com/dsoprea/PythonUpstart',
+      license='GPL 2',
+      packages=find_packages(exclude=['tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[],
+      entry_points="",
+#      cmdclass=_VLazyCmdClass(v)
+#      cmdclass=versioneer.get_cmdclass(),
+)
