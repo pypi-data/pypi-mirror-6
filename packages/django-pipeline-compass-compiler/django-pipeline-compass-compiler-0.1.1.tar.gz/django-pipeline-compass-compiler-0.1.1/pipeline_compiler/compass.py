aec7efff8a05f962be1483b14594d6c527423abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package django-pipeline-compass-compiler/0.1.1/django-pipeline-compass-compiler-0.1.1.tar.gz

---
+++ packages/django-pipeline-compass-compiler/django-pipeline-compass-compiler-0.1.1.tar.gz/django-pipeline-compass-compiler-0.1.1/pipeline_compiler/compass.py
@@ -0,0 +1,22 @@
+from os.path import dirname
+from django.conf import settings
+
+from pipeline.compilers import SubProcessCompiler
+
+
+class CompassCompiler(SubProcessCompiler):
+    output_extension = 'css'
+
+    def match_file(self, filename):
+        return filename.endswith(('.scss', '.sass'))
+
+    def compile_file(self, infile, outfile, outdated=False, force=False):
+        if outdated:
+            command = "%s compile --boring --sass-dir=%s --css-dir=%s %s %s" % (
+                getattr(settings, 'PIPELINE_COMPASS_BINARY', '/usr/bin/env compass'),
+                dirname(infile),
+                dirname(outfile),
+                getattr(settings, 'PIPELINE_COMPASS_ARGUMENTS', ''),
+                infile
+            )
+            return self.execute_command(command, cwd=dirname(infile))
---
+++ packages/django-pipeline-compass-compiler/django-pipeline-compass-compiler-0.1.1.tar.gz/django-pipeline-compass-compiler-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+description = """
+Compiler plugin to use Django Asset Pipeline package with Compass
+"""
+
+setup(
+    name='django-pipeline-compass-compiler',
+    version='0.1.1',
+    description=description,
+    long_description=open('README.md').read(),
+    author='Javi Velasco',
+    author_email='javier.velasco86@gmail.com',
+    url='https://github.com/javivelasco/django-pipeline-compass-compiler',
+    license='MIT License',
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ]
+)
