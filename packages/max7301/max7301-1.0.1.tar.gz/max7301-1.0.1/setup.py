0c09ac457de8502180b6d0fdb5d29041a881ebbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:17 +0000

    Add package max7301/1.0.1/max7301-1.0.1.tar.gz

---
+++ packages/max7301/max7301-1.0.1.tar.gz/max7301-1.0.1/setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='max7301',
+    version='1.0.1',
+    description='MAX7301 driver',
+    author='Tobias Schneider',
+    author_email='schneider@xtort.eu',
+    url='https://github.com/muccc/py-max7301',
+    packages=['max7301'],
+    long_description=open('README').read(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 2",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='max7301',
+    license='GPLv3+',
+    install_requires=['spidev']
+)
