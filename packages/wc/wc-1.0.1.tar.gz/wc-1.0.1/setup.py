a3b513a64a0433b1a0e4ed1a05481d16ff83e467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:12 +0000

    Add package wc/1.0.1/wc-1.0.1.tar.gz

---
+++ packages/wc/wc-1.0.1.tar.gz/wc-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = "wc",
+	version = "1.0.1",
+	py_modules = ["wc"],
+	author = "weviloju",
+	author_email = "weviloju@hotmail.com",
+	description = "Um simples modulo que troca o papel de parede do sistema operacional Linux, tudo utilizando Python",
+)
