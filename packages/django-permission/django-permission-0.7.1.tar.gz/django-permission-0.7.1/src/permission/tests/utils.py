c05080c252d88951e249ca727c24a94c7f3aaf93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:39 +0000

    Add package django-permission/0.7.1/django-permission-0.7.1.tar.gz

---
+++ packages/django-permission/django-permission-0.7.1.tar.gz/django-permission-0.7.1/src/permission/tests/utils.py
@@ -0,0 +1,57 @@
+# coding=utf-8
+"""
+"""
+__author__ = 'Alisue <lambdalisue@hashnote.net>'
+
+
+def create_user(username, **kwargs):
+    from django.contrib.auth.models import User
+    user = User.objects.create_user(
+            username=username,
+            email="%s@test.com" % username,
+            password="password",
+        )
+    # attribute assignment
+    for key, value in kwargs.items():
+        user.__dict__[key] = value
+    user.save()
+    return user
+
+def create_anonymous(**kwargs):
+    from django.contrib.auth.models import AnonymousUser
+    return AnonymousUser(**kwargs)
+
+def create_group(name, user=None):
+    from django.contrib.auth.models import Group
+    group = Group.objects.create(
+            name=name,
+        )
+    if user is not None:
+        user.groups.add(group)
+        user.save()
+    group.save()
+    return group
+
+def create_article(title, user=None):
+    import datetime
+    from permission.tests.models import Article
+    user = user or create_user(str(datetime.datetime.now()))
+    article = Article.objects.create(
+            title=title,
+            content=title*20,
+            author=user)
+    article.save()
+    return article
+
+def create_permission(name, model=None):
+    from django.contrib.auth.models import Permission
+    from django.contrib.contenttypes.models import ContentType
+    from permission.tests.models import Article
+    model = model or Article
+    ct = ContentType.objects.get_for_model(model)
+    permission = Permission.objects.create(
+            name=name, codename=name,
+            content_type=ct
+        )
+    permission.save()
+    return permission
