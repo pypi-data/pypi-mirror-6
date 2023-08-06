a2a9ba4a3735aed42387c1f612ac5f19fd4d4521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:57 +0000

    Add package Greeting/1.0.1/Greeting-1.0.1.zip

---
+++ packages/Greeting/Greeting-1.0.1.zip/Greeting-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'Greeting',
+    version = '1.0.1',
+    py_modules = ['Greeting'],
+    author = 'Kentaxer',
+    author_email = 'kentaxer@163.com',
+    url = 'http://www.kentaxer.com',
+    description = 'Say hello to someone',
+    )
+
+    
