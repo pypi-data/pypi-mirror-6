914796e022ba0fd81bde26621ed4e8cdda88194e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:21 +0000

    Add package django-vies/1.0.2/django-vies-1.0.2.tar.gz

---
+++ packages/django-vies/django-vies-1.0.2.tar.gz/django-vies-1.0.2/vies/models.py
@@ -0,0 +1,32 @@
+from warnings import warn
+from django.db.models import CharField
+
+from vies import fields
+
+
+class VATINField(CharField):
+
+    description = "A VIES VAT field."
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 14
+        super(VATINField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': fields.VATINField}
+        defaults.update(kwargs)
+        return super(VATINField, self).formfield(**defaults)
+
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^vies\.models"])
+except ImportError:
+    pass
+
+
+class VIESField(VATINField):
+    """Deprecated in favor of VATINField"""
+    def __init__(self, *args, **kwargs):
+        warn(DeprecationWarning, '%(class)s has been deprecated in favor of VATINField')
+        super(VIESField, self).__init__(*args, **kwargs)
