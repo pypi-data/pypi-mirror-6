bc7089da1073ea3d2152b85271e45149f7a86978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:26 +0000

    Add package curtsies/0.0.26/curtsies-0.0.26.tar.gz

---
+++ packages/curtsies/curtsies-0.0.26.tar.gz/curtsies-0.0.26/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='curtsies',
+      version='0.0.26',
+      description='Curses-like terminal wrapper, with colored strings!',
+      url='https://github.com/thomasballinger/curtsies',
+      author='Thomas Ballinger',
+      author_email='thomasballinger@gmail.com',
+      license='MIT',
+      packages=['curtsies'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          ],
+      zip_safe=False)
