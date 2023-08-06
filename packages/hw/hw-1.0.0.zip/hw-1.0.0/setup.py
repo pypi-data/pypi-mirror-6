5fb9457dadf6ced2699ec3bae042e120128ce528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:52 +0000

    Add package hw/1.0.0/hw-1.0.0.zip

---
+++ packages/hw/hw-1.0.0.zip/hw-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'hw',
+	version = '1.0.0',
+	py_modules = ['hw'],
+	author = 'yaoyin',
+	author_email = '514270761@qq.com',
+	url = 'http://user.qzone.qq.com/514270761',
+	description = 'A simple printer of helloworld!',
+	)
