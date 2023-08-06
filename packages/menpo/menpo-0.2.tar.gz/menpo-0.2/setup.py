7047576e2cfe65153d53273bf9b58166c1d0f13e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:59 +0000

    Add package menpo/0.2/menpo-0.2.tar.gz

---
+++ packages/menpo/menpo-0.2.tar.gz/menpo-0.2/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+import numpy as np
+
+
+# ---- C/C++ EXTENSIONS ---- #
+cython_modules = ["menpo/geodesics/kirsanov.pyx",
+                  "menpo/shape/mesh/cpptrimesh.pyx",
+                  "menpo/shape/mesh/normals.pyx",
+                  "menpo/interpolation/cinterp.pyx",
+                  "menpo/transform/piecewiseaffine/fastpwa.pyx",
+                  "menpo/features/cppimagewindowiterator.pyx"]
+
+cython_exts = cythonize(cython_modules, quiet=True)
+
+setup(name='menpo',
+      version='0.2',
+      description='iBUG Facial Modelling Toolkit',
+      author='James Booth',
+      author_email='james.booth08@imperial.ac.uk',
+      include_dirs=[np.get_include()],
+      ext_modules=cython_exts,
+      packages=find_packages(),
+      install_requires=[# Core
+                        'numpy>=1.8.0',
+                        'scipy>=0.12.0',
+                        'Cython>=0.20.1',  # req on OS X Mavericks
+
+                        # Image
+                        'Pillow>=2.0.0',
+                        'scikit-image>=0.8.2',
+
+                        # ML
+                        'scikit-learn>=0.14.1',
+
+                        # 3D import
+                        'menpo-pyvrml97==2.3.0a4',
+                        'cyassimp>=0.1.3',
+
+                        # Rasterization
+                        'cyrasterize>=0.1.4',
+
+                        # Visualization
+                        'matplotlib>=1.2.1',
+
+                        # Need to decide if this is really needed
+                        'decorator>=3.4.0',
+
+                        # Docs and testing
+                        'Sphinx>=1.2.2',
+                        'numpydoc>=0.4',
+                        'nose>=1.3.0'],
+      extras_require={'3d': 'mayavi>=4.3.0'}
+      )
