be22398539101b087c2b1e329819181540632181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:36 +0000

    Add package infi.winapi.file_version_information/0.0.5/infi.winapi.file_version_information-0.0.5.tar.gz

---
+++ packages/infi.winapi.file_version_information/infi.winapi.file_version_information-0.0.5.tar.gz/infi.winapi.file_version_information-0.0.5/src/infi/winapi/file_version_information/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.0.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '3e3305bf662ecb1f8fcdcd9567ca34437745e508'
+__git_head_subject__ = 'Finished release v0.0.5.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
