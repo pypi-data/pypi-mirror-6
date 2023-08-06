cc0acdd0f466a028054a14c59798b6951cf4dd5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:43 +0000

    Add package sphinxcontrib-robotframework/0.4.3/sphinxcontrib-robotframework-0.4.3.zip

---
+++ packages/sphinxcontrib-robotframework/sphinxcontrib-robotframework-0.4.3.zip/sphinxcontrib-robotframework-0.4.3/README.rst
@@ -0,0 +1,7 @@
+Sphinx-extension for Robot Framework
+====================================
+
+*sphinxcontrib-robotframework* is a Sphinx-extensions, which executes
+embedded Robot Framework -tests during Sphinx-documentation compilation.
+
+`Read the docs for more detailed information. <https://sphinxcontrib-robotframework.readthedocs.org/>`_
---
+++ packages/sphinxcontrib-robotframework/sphinxcontrib-robotframework-0.4.3.zip/sphinxcontrib-robotframework-0.4.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sphinxcontrib-robotframework',
+    version='0.4.3',
+    description='Robot Framework plugin for Sphinx',
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    url='http://github.com/vivekkodu/sphinxcontrib_robotframework',
+    author='Vivek',
+    author_email='1990vivekkumarverma@gmail.com',
+    license='GPL',
+    py_modules=[
+        'sphinxcontrib_robotframework'
+    ],
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Sphinx',
+        'robotframework',
+        'Pygments',
+    ],
+    extras_require={'docs': [
+        'robotframework',
+        'robotframework-selenium2library',
+        'robotframework-selenium2screenshots[Pillow]',
+    ]}
+)
