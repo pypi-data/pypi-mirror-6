d926bf6142d34db49e87729686ff0ca696cefd18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:10 +0000

    Add package google_news_crawler/0.3.6/google_news_crawler-0.3.6.tar.gz

---
+++ packages/google_news_crawler/google_news_crawler-0.3.6.tar.gz/google_news_crawler-0.3.6/google_news_crawler/__init__.py
@@ -0,0 +1,14 @@
+"""Google News Crawler
+===================
+
+A utility to fetch news articles from Google News.
+
+GNC retrieves the latest items from the Google News feeds and stores
+them in ElasticSearch or on disk.
+
+Written by Isaac Sijaranamual, copyright 2013 University of
+Amsterdam/ILPS, licensed under the Apache License, Version 2.0.
+
+"""
+
+__version__ = '0.3.6'
