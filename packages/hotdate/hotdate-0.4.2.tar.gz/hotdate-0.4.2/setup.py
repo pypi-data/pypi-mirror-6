0f509dbf8505066cc2b8ccc7779712068810db77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:44 +0000

    Add package hotdate/0.4.2/hotdate-0.4.2.tar.gz

---
+++ packages/hotdate/hotdate-0.4.2.tar.gz/hotdate-0.4.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='hotdate',
+    version='0.4.2',
+    author='Samuel "mansam" Lucidi',
+    author_email="mansam@csh.rit.edu",
+    packages=['hotdate'],
+    url='http://pypi.python.org/pypi/hotdate/',
+    license='LICENSE',
+    install_requires=[
+    	'six',
+    	'python-dateutil'
+    ],
+    description='Intuitive date formatting.',
+    long_description=open('README.md').read()
+)
