762f77299c544fc50c46602017567fe957d16986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:54 +0000

    Add package hanga/0.6/hanga-0.6.tar.gz

---
+++ packages/hanga/hanga-0.6.tar.gz/hanga-0.6/hanga/__init__.py
@@ -0,0 +1,12 @@
+"""
+Hanga
+=====
+
+Library for managing request on https://hanga.io/
+"""
+
+__version__ = "0.6"
+__all__ = ["HangaAPI", "HangaException"]
+
+
+from hanga.api import HangaAPI, HangaException
