601eee8b54063b02df5ac9f35bbfa1fcf10587ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package reutil/0.1.1/reutil-0.1.1.tar.gz

---
+++ packages/reutil/reutil-0.1.1.tar.gz/reutil-0.1.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='reutil',
+      version='0.1.1',
+      description="Utilities for working with multiple regular expressions",
+      author='Gregory Vincic',
+      author_email='g@7de.se',
+      url='https://github.com/gregoryv/reutil',
+      license='GPLv3',
+      packages=['reutil'],
+      include_package_data=True,
+      install_requires=[]
+      )
