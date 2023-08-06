82b05e3e54f2f4fe688f4a9e5efac35b854deb30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:06 +0000

    Add package azurepython3/1.7.4/azurepython3-1.7.4.zip

---
+++ packages/azurepython3/azurepython3-1.7.4.zip/azurepython3-1.7.4/azurepython3/djangostorage.py
@@ -0,0 +1,86 @@
+"""
+This module implements a custom Django storage based on the BlobService.
+"""
+import os
+from tempfile import SpooledTemporaryFile
+from django.core.files import File
+from requests import HTTPError
+from azurepython3.blobservice import BlobService
+
+try:
+    from django.core.files.storage import Storage
+    from django.conf import settings
+except ImportError:
+    class Storage:
+        pass
+
+    class Settings:
+        CUSTOM_STORAGE_OPTIONS = {}
+
+    settings = Settings()
+
+class AzureStorage(Storage):
+
+    def __init__(self, container = None, account_name = None, account_key = None):
+        """
+        Creates a new AzureStorage. The container is not automatically created and therefore must already exist.
+        """
+        if container is None:
+            if hasattr(settings, 'AZURE_DEFAULT_CONTAINER'):
+                self.container = settings.AZURE_DEFAULT_CONTAINER
+            else:
+                self.container = "$root"
+        else:
+            self.container = container
+
+        if account_name and account_key:
+            self.service = BlobService(account_name, account_key)
+        else:
+            self.service = BlobService(settings.AZURE_ACCOUNT_NAME, settings.AZURE_ACCOUNT_KEY)
+
+    def _transform_name(self, name):
+        return name.replace("\\", "/")
+
+    def _open(self, name, mode = 'rb') -> File:
+        name = self._transform_name(name)
+        content = self.service.get_blob_content(self.container, name)
+        file = SpooledTemporaryFile()
+        file.write(content)
+        file.seek(0) # explicitly reset to allow reading from the beginning afterwards as-is
+
+        return File(file)
+
+    def _save(self, name, content):
+        name = self._transform_name(name)
+        content.open(mode='rb')
+        data = bytearray(content.read())
+        self.service.create_blob(self.container, name, data)
+        return name
+
+    def delete(self, name):
+        name = self._transform_name(name)
+        self.service.delete_blob(self.container, name)
+        return name
+
+    def exists(self, name):
+        name = self._transform_name(name)
+        return self.service.blob_exists(self.container, name)
+
+    def listdir(self, path = None):
+        path = self._transform_name(path)
+        blobs = self.service.list_blobs(self.container, prefix = path)
+        paths = [os.path.split(blob.name) for blob in blobs]
+        dirs = [path[0] for path in paths]
+        files = [path[1] for path in paths]
+        return (dirs, files)
+
+    def size(self, name):
+        name = self._transform_name(name)
+        blob = self.service.get_blob(self.container, name, with_content=False)
+        return blob.content_length() if blob != None else 0
+
+    def url(self, name):
+        name = self._transform_name(name)
+        return self.service.get_blob_url(self.container, name)
+
+
---
+++ packages/azurepython3/azurepython3-1.7.4.zip/azurepython3-1.7.4/azurepython3/service.py
@@ -0,0 +1,74 @@
+from datetime import datetime
+import requests
+from requests.adapters import HTTPAdapter
+from azurepython3.auth import SharedKeyAuthentication
+
+USE_SSL = True
+
+try:
+    import ssl
+except ImportError:
+    USE_SSL = False
+
+class AzureService:
+
+    timeout = None
+    retry = True
+
+    def __init__(self, account_name, account_key):
+        self.account_name = account_name
+        self.account_key = account_key
+        self.auth = SharedKeyAuthentication(account_name, account_key)
+
+    def get_host(self, protocol=None):
+        if protocol is None:
+            protocol = 'https' if USE_SSL else 'http'
+        return "%s://%s.blob.core.windows.net" % (protocol, self.account_name)
+
+    def get_url(self, query = '/', protocol=None):
+        return self.get_host(protocol) + query
+
+    def _headers(self):
+        return {
+            'x-ms-version': '2011-08-18',
+            'x-ms-date': datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'),
+            'Content-Type': 'application/octet-stream Charset=UTF-8'
+        }
+
+    def _params(self):
+        return {
+            'timeout': self.timeout
+        }
+
+    def _request(self, method, uri, headers = None, params = None, content = None):
+        if content is None:
+            content = dict()
+
+        # filter empty headers
+        if headers != None:
+            headers = { key: value for key, value in headers.items() if value != None }
+
+        headers = dict(self._headers(), **headers) if headers else self._headers()
+        params = dict(self._params(), **params) if params else self._params()
+        req = requests.Request(method, self.get_url(uri), data=content, headers=headers, params=params)
+
+        # Give content length for modifying requests
+        if method.lower() in ['put', 'post', 'merge', 'delete'] and not content:
+            req.headers['Content-Length'] = '0'
+
+        # Generate and append Authorization signature to request headers
+        self.auth.authenticate(req, len(content))
+        request = req.prepare()
+
+        session = requests.session()
+        session.mount('http://', HTTPAdapter(max_retries=5))
+        session.mount('https://', HTTPAdapter(max_retries=5))
+
+        response = session.send(request)
+        response.encoding = 'utf-8-sig'
+
+        # raise underlying HTTPError if something goes wrong
+        if response.status_code >= 300:
+            response.raise_for_status()
+
+        return response
