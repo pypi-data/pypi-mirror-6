90f205961acfa7081bf45f0ae26d6d189c7cdb50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:02 +0000

    Add package uwaterlooapi/0.3.0/uwaterlooapi-0.3.0.tar.gz

---
+++ packages/uwaterlooapi/uwaterlooapi-0.3.0.tar.gz/uwaterlooapi-0.3.0/setup.py
@@ -0,0 +1,36 @@
+import os
+
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+packages = [
+    'uwaterlooapi',
+]
+
+requires = ['shad>=0.1.4']
+
+setup(
+    name='uwaterlooapi',
+    version='0.3.0',
+    description='Thin library wrapper for the University of Waterloo Open Data API.',
+    long_description='See http://api.uwaterloo.ca/ for a descripton of the API.',
+    author='Albert O\'Connor',
+    author_email='info@albertoconnor.ca',
+    url='https://bitbucket.org/amjoconn/uwaterlooapi',
+    packages=packages,
+    package_data={'': ['LICENSE', 'NOTICE']},
+    package_dir={},
+    include_package_data=True,
+    install_requires=requires,
+    license='MIT',
+    zip_safe=False,
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+        "Operating System :: OS Independent",
+    ),
+
+)
