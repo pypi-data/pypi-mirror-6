a6e7be05cb544d1f27e5a208ec2fadce40f472dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:06 +0000

    Add package haha/100.0.0/haha-100.0.0.tar.gz

---
+++ packages/haha/haha-100.0.0.tar.gz/haha-100.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name='haha',
+        version='100.0.0',
+        py_modules=['haha'],
+        author='zhaimingyu',
+        author_email='25839401@163.com',
+        url='www.baidu.com',
+        description='you are a bird',
+     )
