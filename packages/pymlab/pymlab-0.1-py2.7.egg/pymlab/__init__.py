293bd4d8c8ff634aa33fc604c86087423f128914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:11 +0000

    Add package pymlab/0.1/pymlab-0.1-py2.7.egg

---
+++ packages/pymlab/pymlab-0.1-py2.7.egg/pymlab/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""pymlab module.
+
+MLAB I2C bus modules binding.
+"""
+
+
+__version__ = "0.1"
+
+
+#from pymlab import config
+
+
+#main = config.main
+
+
+if __name__ == "__main__":
+	print __doc__
+    #main()
