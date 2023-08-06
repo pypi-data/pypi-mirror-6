65ddb283e9b25d2d1ef82d5e176ef459e92c8bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:26 +0000

    Add package edeposit.amqp/1.1.6/edeposit.amqp-1.1.6.tar.gz

---
+++ packages/edeposit.amqp/edeposit.amqp-1.1.6.tar.gz/edeposit.amqp-1.1.6/CHANGES.rst
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+1.1.6
+-----
+    - Documentation is now even for settings.py's attributes.
+    - User defined JSON configuration is now supported.
+
+1.1.5
+-----
+    - Added autogeneration of documentation to the package generator (setup.py).
+
+1.1.0
+-----
+    - Project released at PYPI
+
+1.0 (unreleased)
+----------------
+    - alephdaemon is working correctly, other classes are in release state too
---
+++ packages/edeposit.amqp/edeposit.amqp-1.1.6.tar.gz/edeposit.amqp-1.1.6/setup.py
@@ -0,0 +1,82 @@
+import os
+import os.path
+import shutil
+
+from setuptools import setup, find_packages
+from distutils.command.sdist import sdist
+
+
+version = '1.1.6'
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    open('CHANGES.rst').read()
+])
+
+
+class BuildSphinx(sdist):
+    """
+    Generates sphinx documentation, puts it into html_docs/, packs it to
+    package and removes unused directory.
+    """
+    def run(self):
+        d = os.path.abspath('.')
+        DOCS = d + "/" + "docs"
+        DOCS_IN = DOCS + "/build/html"
+        DOCS_OUT = d + "/html_docs"
+
+        if not self.dry_run:
+            print "Generating the documentation .."
+
+            os.chdir(DOCS)
+            os.system("make clean")
+            os.system("make html")
+
+            if os.path.exists(DOCS_OUT):
+                shutil.rmtree(DOCS_OUT)
+
+            shutil.copytree(DOCS_IN, DOCS_OUT)
+
+            global DOCS_GENERATED
+            DOCS_GENERATED = True
+
+            os.chdir(d)
+
+        sdist.run(self)
+
+        if os.path.exists(DOCS_OUT):
+            shutil.rmtree(DOCS_OUT)
+
+
+setup(
+    name='edeposit.amqp',
+    version=version,
+    description="E-Deposit's AMQP definitions and common classes/patterns.",
+    long_description=long_description,
+    url='https://github.com/jstavel/edeposit.amqp/',
+
+    author='Edeposit team',
+    author_email='edeposit@email.cz',
+
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    license='GPL2+',
+
+    packages=find_packages(exclude=['ez_setup']),
+
+    namespace_packages=['edeposit'],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='edeposit.amqp.tests',
+    install_requires=[
+        'setuptools',
+        "python-daemon>=1.5.5",
+        "pika>=0.9.13",
+        "edeposit.amqp.aleph>=1.2.3"
+    ],
+
+    cmdclass={'sdist': BuildSphinx}
+)
