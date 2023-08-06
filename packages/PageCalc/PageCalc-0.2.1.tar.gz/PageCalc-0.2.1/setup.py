0ff9801b6e2be4a56b10c8d7e89962399286915e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:48 +0000

    Add package PageCalc/0.2.1/PageCalc-0.2.1.tar.gz

---
+++ packages/PageCalc/PageCalc-0.2.1.tar.gz/PageCalc-0.2.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name             = 'PageCalc',
+  version          = '0.2.1',
+  author           = 'saaj',
+  author_email     = 'mail@saaj.me',
+  packages         = ['pagecalc'],
+  package_data     = {'pagecalc' : ['example/*']},  
+  url              = 'http://code.google.com/p/pagecalc/',
+  license          = 'LGPL',
+  description      = 'Python pagination calculator',
+  long_description = open('README.txt').read(),
+  platforms        = ['Any'],
+  classifiers      = [
+    'Topic :: Utilities',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Intended Audience :: Developers'
+  ]
+)
