4b4cbe8c9d9331d37cc6eac9c4a0d7747b1d1c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:59 +0000

    Add package pydouban/0.1.2/pydouban-0.1.2.tar.gz

---
+++ packages/pydouban/pydouban-0.1.2.tar.gz/pydouban-0.1.2/setup.py
@@ -0,0 +1,16 @@
+#encoding:utf-8
+
+from setuptools import setup
+
+setup(name='pydouban',  
+      author='TY',  
+      author_email='tianyu0915@gmail.com',  
+      version='0.1.2',  
+      description='A simple python libary for douban.com that contains some basic functions',  
+      keywords ='douban,libary',
+      url='http://github.com/tianyu0915/pydouban',  
+      packages=['pydouban'],  
+      package_data={'': ['LICENSE']},
+      install_requires = ['requests==0.13.1','lxml>=2.3.4'],
+
+)  
