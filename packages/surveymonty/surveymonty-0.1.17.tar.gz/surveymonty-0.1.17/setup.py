63b2c6a09c8755652eb287c3e3d83e497b62f83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:58 +0000

    Add package surveymonty/0.1.17/surveymonty-0.1.17.tar.gz

---
+++ packages/surveymonty/surveymonty-0.1.17.tar.gz/surveymonty-0.1.17/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='surveymonty',
+    py_modules=['surveymonty'],
+    version='0.1.17',
+    description='Python wrapper for SurveyMonkey API',
+    author='Andrew Shim',
+    author_email='andrewkshim92@gmail.com',
+    url='https://github.com/andrewkshim/SurveyMonty',
+    download_url='https://github.com/andrewkshim/SurveyMonty/tarball/1.0',
+    keywords=['SurveyMonkey'],
+    license='MIT',
+    classifiers=[],
+)
