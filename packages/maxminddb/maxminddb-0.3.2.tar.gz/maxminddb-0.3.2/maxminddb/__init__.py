15bb431c57f6599999638d957f0cc8b3ddb9d480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:09 +0000

    Add package maxminddb/0.3.2/maxminddb-0.3.2.tar.gz

---
+++ packages/maxminddb/maxminddb-0.3.2.tar.gz/maxminddb-0.3.2/maxminddb/__init__.py
@@ -0,0 +1,17 @@
+# pylint:disable=C0111
+import os
+
+try:
+    if os.environ.get('MAXMINDDB_PURE_PYTHON'):
+        raise ImportError()
+    from maxminddb.extension import Reader, InvalidDatabaseError
+except ImportError:
+    from maxminddb.decoder import InvalidDatabaseError
+    from maxminddb.reader import Reader
+
+
+__title__ = 'maxminddb'
+__version__ = '0.3.2'
+__author__ = 'Gregory Oschwald'
+__license__ = 'Apache License, Version 2.0'
+__copyright__ = 'Copyright 2014 Maxmind, Inc.'
