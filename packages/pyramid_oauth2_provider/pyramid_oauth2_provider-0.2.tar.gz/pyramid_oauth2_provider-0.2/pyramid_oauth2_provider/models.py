67f6adbf8ee498ffc648164cce21d34cc349c9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:33 +0000

    Add package pyramid_oauth2_provider/0.2/pyramid_oauth2_provider-0.2.tar.gz

---
+++ packages/pyramid_oauth2_provider/pyramid_oauth2_provider-0.2.tar.gz/pyramid_oauth2_provider-0.2/pyramid_oauth2_provider/models.py
@@ -0,0 +1,162 @@
+#
+# Copyright (c) Elliot Peele <elliot@bentlogic.net>
+#
+# This program is distributed under the terms of the MIT License as found
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warrenty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the MIT License for full details.
+#
+
+import time
+from datetime import datetime
+
+from sqlalchemy import Column
+from sqlalchemy import ForeignKey
+
+from sqlalchemy import String
+from sqlalchemy import Integer
+from sqlalchemy import Boolean
+from sqlalchemy import DateTime
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import backref
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+from .generators import gen_token
+from .generators import gen_client_id
+from .generators import gen_client_secret
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
+
+class Oauth2Client(Base):
+    __tablename__ = 'oauth2_provider_clients'
+    id = Column(Integer, primary_key=True)
+    client_id = Column(String(64), unique=True, nullable=False)
+    client_secret = Column(String(64), unique=True, nullable=False)
+    revoked = Column(Boolean, default=False)
+    revocation_date = Column(DateTime)
+
+    def __init__(self):
+        self.client_id = gen_client_id()
+        self.client_secret = gen_client_secret()
+
+    def revoke(self):
+        self.revoked = True
+        self.revocation_date = datetime.utcnow()
+
+    def isRevoked(self):
+        return self.revoked
+
+
+class Oauth2RedirectUri(Base):
+    __tablename__ = 'oauth2_provider_redirect_uris'
+    id = Column(Integer, primary_key=True)
+    uri = Column(String(256), unique=True, nullable=False)
+
+    client_id = Column(Integer, ForeignKey(Oauth2Client.id))
+    client = relationship(Oauth2Client, backref=backref('redirect_uris'))
+
+    def __init__(self, client, uri):
+        self.client = client
+        self.uri = uri
+
+
+class Oauth2Code(Base):
+    __tablename__ = 'oauth2_provider_codes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    authcode = Column(String(64), unique=True, nullable=False)
+    expires_in = Column(Integer, nullable=False, default=10*60)
+
+    revoked = Column(Boolean, default=False)
+    revocation_date = Column(DateTime)
+
+    creation_date = Column(DateTime, default=datetime.utcnow)
+
+    client_id = Column(Integer, ForeignKey(Oauth2Client.id))
+    client = relationship(Oauth2Client, backref=backref('authcode'))
+
+    def __init__(self, client, user_id):
+        self.client = client
+        self.user_id = user_id
+
+        self.authcode = gen_token(self.client)
+
+    def revoke(self):
+        self.revoked = True
+        self.revocation_date = datetime.utcnow()
+
+    def isRevoked(self):
+        expiry = time.mktime(self.create_date.timetuple()) + self.expires_in
+        if datetime.frometimestamp(expiry) < datetime.utcnow():
+            self.revoke()
+        return self.revoked
+
+
+class Oauth2Token(Base):
+    __tablename__ = 'oauth2_provider_tokens'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+    access_token = Column(String(64), unique=True, nullable=False)
+    refresh_token = Column(String(64), unique=True, nullable=False)
+    expires_in = Column(Integer, nullable=False, default=60*60)
+
+    revoked = Column(Boolean, default=False)
+    revocation_date = Column(DateTime)
+
+    creation_date = Column(DateTime, default=datetime.utcnow)
+
+    client_id = Column(Integer, ForeignKey(Oauth2Client.id))
+    client = relationship(Oauth2Client, backref=backref('tokens'))
+
+    def __init__(self, client, user_id):
+        self.client = client
+        self.user_id = user_id
+
+        self.access_token = gen_token(self.client)
+        self.refresh_token = gen_token(self.client)
+
+    def revoke(self):
+        self.revoked = True
+        self.revocation_date = datetime.utcnow()
+
+    def isRevoked(self):
+        expiry = time.mktime(self.creation_date.timetuple()) + self.expires_in
+        if datetime.fromtimestamp(expiry) < datetime.utcnow():
+            self.revoke()
+        return self.revoked
+
+    def refresh(self):
+        """
+        Generate a new token for this client.
+        """
+
+        cls = self.__class__
+        self.revoke()
+        return cls(self.client, self.user_id)
+
+    def asJSON(self, **kwargs):
+        token = {
+            'access_token': self.access_token,
+            'refresh_token': self.refresh_token,
+            'user_id': self.user_id,
+            'expires_in': self.expires_in,
+        }
+        kwargs.update(token)
+        return kwargs
+
+
+def initialize_sql(engine, settings):
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
---
+++ packages/pyramid_oauth2_provider/pyramid_oauth2_provider-0.2.tar.gz/pyramid_oauth2_provider-0.2/pyramid_oauth2_provider/util.py
@@ -0,0 +1,62 @@
+#
+# Copyright (c) Elliot Peele <elliot@bentlogic.net>
+#
+# This program is distributed under the terms of the MIT License as found
+# in a file called LICENSE. If it is not present, the license
+# is always available at http://www.opensource.org/licenses/mit-license.php.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warrenty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the MIT License for full details.
+#
+
+import base64
+import logging
+
+from pyramid.threadlocal import get_current_registry
+
+log = logging.getLogger('pyramid_oauth2_provider.util')
+
+def oauth2_settings(key=None, default=None):
+    settings = get_current_registry().settings
+
+    if key:
+        value = settings.get('oauth2_provider.%s' % key, default)
+        if value == 'true':
+            return True
+        elif value == 'false':
+            return False
+        else:
+            return value
+    else:
+        return dict((x.split('.', 1)[1], y) for x, y in settings.iteritems()
+            if x.startswith('oauth2_provider.'))
+
+def getClientCredentials(request):
+    if 'Authorization' in request.headers:
+        auth = request.headers.get('Authorization')
+    elif 'authorization' in request.headers:
+        auth = request.headers.get('authorization')
+    else:
+        log.debug('no authorization header found')
+        return False
+
+    if (not auth.lower().startswith('bearer') and
+        not auth.lower().startswith('basic')):
+        log.debug('authorization header not of type bearer or basic: %s'
+            % auth.lower())
+        return False
+
+    parts = auth.split()
+    if len(parts) != 2:
+        return False
+
+    token_type = parts[0].lower()
+    token = base64.b64decode(parts[1])
+
+    if token_type == 'basic':
+        client_id, client_secret = token.split(':')
+        request.client_id = client_id
+        request.client_secret = client_secret
+
+    return token_type, token
