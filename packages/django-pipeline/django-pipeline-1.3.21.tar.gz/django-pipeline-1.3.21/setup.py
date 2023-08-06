eda2a1c07eeebcc85a74c5b4437046ae8b15aff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:51 +0000

    Add package django-pipeline/1.3.21/django-pipeline-1.3.21.tar.gz

---
+++ packages/django-pipeline/django-pipeline-1.3.21.tar.gz/django-pipeline-1.3.21/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-pipeline',
+    version='1.3.21',
+    description='Pipeline is an asset packaging library for Django.',
+    long_description=open('README.rst').read() + '\n\n' +
+        open('HISTORY.rst').read(),
+    author='Timothée Peignier',
+    author_email='timothee.peignier@tryphon.org',
+    url='https://github.com/cyberdelia/django-pipeline',
+    license='MIT',
+    packages=find_packages(exclude=['tests', 'tests.tests']),
+    zip_safe=False,
+    install_requires=[
+        'futures>=2.1.3',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ]
+)
