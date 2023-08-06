1d6c69abefd36a0a221df41583945b125ca52ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:24 +0000

    Add package PyMunkTMX/0.5.3/PyMunkTMX-0.5.3.tar.gz

---
+++ packages/PyMunkTMX/PyMunkTMX-0.5.3.tar.gz/PyMunkTMX-0.5.3/pymunktmx/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "William Kevin Manire <williamkmanire@gmail.com>"
+__author_email__ = "williamkmanire@gmail.com"
+__version__ = "0.5.3"
+__description__ = "Load shapes from Tiled maps as pymunk objects."
+
+
+__all__ = ["load_shapes"]
+
+
+from .shapeloader import load_shapes
