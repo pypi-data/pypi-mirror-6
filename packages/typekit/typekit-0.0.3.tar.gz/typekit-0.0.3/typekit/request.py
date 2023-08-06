99546f58ba49b038242d31559bd952c695bbf4d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:24 +0000

    Add package typekit/0.0.3/typekit-0.0.3.tar.gz

---
+++ packages/typekit/typekit-0.0.3.tar.gz/typekit-0.0.3/typekit/request.py
@@ -0,0 +1,29 @@
+import urllib
+
+try:
+	import requests
+except ImportError:
+	pass
+
+from _version import __version__
+
+
+def make_request(method, url, params=None):
+
+	user_agent = 'Python Typekit API Wrapper v{}'.format(__version__)
+	headers = { 'User-Agent' : user_agent }
+
+	if params is not None:
+		data = urllib.urlencode(params, True)
+
+	if method == 'GET':
+		res = requests.get(url, headers=headers)
+
+	elif method == 'POST':
+		res = requests.post(url, data=data, headers=headers)
+
+	elif method == 'DELETE':
+		res = requests.delete(url, data=data, headers=headers)
+
+	return res.json()
+
