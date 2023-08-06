d3f92e2bff699e22b3d7986c75947498975b83fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:47 +0000

    Add package lp-show-my-bugs/0.0.1/lp-show-my-bugs-0.0.1.tar.gz

---
+++ packages/lp-show-my-bugs/lp-show-my-bugs-0.0.1.tar.gz/lp-show-my-bugs-0.0.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jorge Niedbalski R. <jnr@pyrosome.org>'
+
+import os
+from setuptools import setup, find_packages
+
+dependencies = [ "launchpadlib", "tabulate" ]
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="lp-show-my-bugs",
+    version="0.0.1",
+    author="Jorge Niedbalski R.",
+    author_email="jnr@metaklass.org",
+    description="",
+    install_requires=dependencies,
+    packages=find_packages(),
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+    ],
+    license="BSD",
+    entry_points={
+	'console_scripts' : [
+	'lp-show-my-bugs = lp_show_my_bugs.cli:main'
+	]
+    }
+)
