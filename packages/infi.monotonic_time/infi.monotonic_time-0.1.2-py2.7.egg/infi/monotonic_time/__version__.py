ccdd1f7803663587685f42ce984b0177b51b31c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:07 +0000

    Add package infi.monotonic_time/0.1.2/infi.monotonic_time-0.1.2-py2.7.egg

---
+++ packages/infi.monotonic_time/infi.monotonic_time-0.1.2-py2.7.egg/infi/monotonic_time/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = 'develop'
+__git_remote_tracking_branch__ = 'origin/develop'
+__git_remote_url__ = 'git@infinigit.infinidat.com:/host/monotonic-time.git'
+__git_head_hash__ = '61b477771642ae0027789ed4c60d9efca415266d'
+__git_head_subject__ = 'Finished release v0.1.2.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
