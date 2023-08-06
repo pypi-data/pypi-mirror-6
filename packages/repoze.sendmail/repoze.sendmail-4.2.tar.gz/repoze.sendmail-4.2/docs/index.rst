f2b11a66bb0c1ea33b4bd9cc46b7db344dd76352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:52 +0000

    Add package repoze.sendmail/4.2/repoze.sendmail-4.2.tar.gz

---
+++ packages/repoze.sendmail/repoze.sendmail-4.2.tar.gz/repoze.sendmail-4.2/docs/index.rst
@@ -0,0 +1,19 @@
+:mod:`repoze.sendmail` documentation
+====================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   narr
+   changes
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
---
+++ packages/repoze.sendmail/repoze.sendmail-4.2.tar.gz/repoze.sendmail-4.2/setup.py
@@ -0,0 +1,65 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from setuptools import setup, find_packages
+
+testing_extras = ['nose', 'coverage']
+docs_extras = ['Sphinx', 'repoze.sphinx.autointerface']
+
+requires = ['setuptools',
+            'zope.interface>=3.6.0',
+            'transaction']
+
+with open('README.rst') as f:
+    README = f.read()
+
+with open('CHANGES.rst') as f:
+    CHANGES = f.read()
+
+setup(name='repoze.sendmail',
+      version = '4.2',
+      url='http://www.repoze.org',
+      license='ZPL 2.1',
+      description='Repoze Sendmail',
+      author='Chris Rossi',
+      author_email='repoze-dev@lists.repoze.org',
+      long_description='\n\n'.join([README, CHANGES]),
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        ],
+      packages=find_packages(),
+      namespace_packages=['repoze',],
+      tests_require = requires,
+      install_requires=requires,
+      test_suite="repoze.sendmail",
+      include_package_data = True,
+      zip_safe = False,
+      entry_points = """
+          [console_scripts]
+          qp = repoze.sendmail.queue:run_console
+          """,
+      extras_require = {
+        'testing': requires + testing_extras,
+        'docs': requires + docs_extras,
+      },
+)
