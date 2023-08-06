e13f0440c29937b20839530be25d77b6275ae510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:16 +0000

    Add package doorstop/0.2/Doorstop-0.2.tar.gz

---
+++ packages/doorstop/Doorstop-0.2.tar.gz/Doorstop-0.2/doorstop/__init__.py
@@ -0,0 +1,14 @@
+"""Package for doorstop."""
+
+__project__ = 'Doorstop'
+__version__ = '0.2'
+
+CLI = 'doorstop'
+GUI = 'doorstop-gui'
+VERSION = __project__ + '-' + __version__
+
+try:
+    from doorstop.common import DoorstopError, DoorstopWarning, DoorstopInfo
+    from doorstop.core import Item, Document, Tree, build, report
+except ImportError:  # pragma: no cover, manual test
+    pass
