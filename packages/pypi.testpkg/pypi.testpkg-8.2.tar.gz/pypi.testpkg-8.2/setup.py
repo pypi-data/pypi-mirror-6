81ed31cde2f43f4b50a4fc677090e3a5df05a632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:59 +0000

    Add package pypi.testpkg/8.2/pypi.testpkg-8.2.tar.gz

---
+++ packages/pypi.testpkg/pypi.testpkg-8.2.tar.gz/pypi.testpkg-8.2/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name="pypi.testpkg",
+    author="",
+    version="8.2",
+    packages=["testpkg"],
+    install_requires=["requests"],
+    #requires=('one', 'two', 'three'),
+)
