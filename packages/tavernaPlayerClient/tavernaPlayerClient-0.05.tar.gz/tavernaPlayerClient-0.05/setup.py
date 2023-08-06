61949e6c1ce4cbd173c8e26cfcd71ae9c7ff8259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:59 +0000

    Add package tavernaPlayerClient/0.05/tavernaPlayerClient-0.05.tar.gz

---
+++ packages/tavernaPlayerClient/tavernaPlayerClient-0.05.tar.gz/tavernaPlayerClient-0.05/setup.py
@@ -0,0 +1,27 @@
+#  Taverna Player Client for running Taverna Workflows from IPython Notebook.
+#-----------------------------------------------------------------------------
+#  Copyright (c) 2014, University of Manchester <support@mygrid.org.uk>
+#
+# 
+
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+setup(name='tavernaPlayerClient',
+      version='0.05',
+      description='Taverna Player Client for running Taverna Workflows from IPython Notebook',
+      author='Alan R. Williams, Youri Lammers, Ross Mounce, Aleksandra Pawlik',
+      author_email='support@mygrid.org.uk',
+      url='http://www.mygrid.org.uk',   
+      packages=['tavernaplayerclient'],
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",       
+      ],
+     )
