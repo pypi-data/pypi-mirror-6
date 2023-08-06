d63bcff2f62507b0882deaf67e868b6c62e9f76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:24 +0000

    Add package espressocup/0.0.2/espressocup-0.0.2.tar.gz

---
+++ packages/espressocup/espressocup-0.0.2.tar.gz/espressocup-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+__version__ = '0.0.2'
+
+setup(
+    name = 'espressocup',
+    py_modules = ['espressocup'],
+    version = __version__,
+    description = 'A gevent-intended very very basic flask-like WSGI server',
+    long_description=open('README.rst','r').read(),
+    author = 'Daniel Fairhead',
+    author_email = 'danthedeckie@gmail.com',
+    url = 'https://bitbucket.org/dfairhead/espressocup',
+    download_url = 'https://bitbucket.org/dfairhead/espressocup/get/' + __version__ + '.zip',
+    keywords = ['WSGI'],
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Programming Language :: Python :: 2.7',
+                  ],
+    )
