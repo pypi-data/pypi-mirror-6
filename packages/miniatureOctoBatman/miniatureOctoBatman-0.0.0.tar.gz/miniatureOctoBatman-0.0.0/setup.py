67a982e905467213c91d62b9aa6ffc8517966711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:20 +0000

    Add package miniatureOctoBatman/0.0.0/miniatureOctoBatman-0.0.0.tar.gz

---
+++ packages/miniatureOctoBatman/miniatureOctoBatman-0.0.0.tar.gz/miniatureOctoBatman-0.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='miniatureOctoBatman',
+        packages=find_packages(),
+        namespace_packages=['miniatureOctoBatman']
+        )
