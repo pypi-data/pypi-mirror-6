d1951f778390fe502983d23f86b2166dcf5bac9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:52 +0000

    Add package fjd/0.1.36/fjd-0.1.36.tar.gz

---
+++ packages/fjd/fjd-0.1.36.tar.gz/fjd-0.1.36/fjd/dispatcher.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import time
+import signal
+
+from fjd import CoreProcess
+from fjd import Recruiter
+from fjd.utils import ensure_wdir
+
+
+class Dispatcher(CoreProcess):
+    '''
+    Directory and File-based work dispatcher.
+    Expects jobs to be put in directory `jobqueue` and workers to
+    annouce themselves in directory `workerqueue`. Will then assign jobs 
+    to workers by moving jobs to the `jobpod` directory. 
+    '''
+
+    def __init__(self, interval=.1, project=None, end_on_empty_queue=True):
+        if not project:
+            project = 'default'
+        self.wdir = ensure_wdir(project)
+        self.start_up()
+
+        print('[fjd-dispatcher] Started on project "{}"'.format(project))
+
+        def signal_handler(signal, frame):
+            ''' gently exiting, e.g. when CTRL-C was pressed.  '''
+            sys.stdout.write('\n[fjd-dispatcher] Received Exit signal. Exiting ...\n')
+            print('[fjd-dispatcher] Should I fire all workers in project {}? [Y|n]'\
+                        .format(project))
+            if raw_input().lower() in ["", "y"]:
+                Recruiter(project=project).fire()
+            sys.exit(0)
+        signal.signal(signal.SIGINT, signal_handler)
+
+        do_work = True
+        while do_work:
+            time.sleep(interval)
+            jq = os.listdir('{}/jobqueue'.format(self.wdir))
+            jp = os.listdir('{}/jobpod'.format(self.wdir))
+            wq = os.listdir('{}/workerqueue'.format(self.wdir))
+            self.sort_jobqueue(jq)
+            if len(jq) > 0:  # more jobs waiting for workers
+                sys.stdout.write("\r[fjd-dispatcher] {} job(s) waiting in the queue. Currently {} worker(s) are free...  "\
+                       .format(len(jq), len(wq)))
+                sys.stdout.flush()
+                for _ in range(min(len(jq), len(wq))):
+                    worker = wq.pop()
+                    job = jq.pop()
+                    os.rename('{wdir}/jobqueue/{j}'.format(wdir=self.wdir, j=job),
+                              '{wdir}/jobpod/{w}'.format(wdir=self.wdir, w=worker))
+                    os.remove('{wdir}/workerqueue/{w}'.format(wdir=self.wdir, w=worker))
+            elif len(jp) > 0:  # some jobs are still running
+                sys.stdout.write("\r[fjd-dispatcher] Queue is empty. Waiting for remaining {} job(s) to finish ...        ".format(len(jp)))
+                sys.stdout.flush()
+            else:  # all jobs are done
+                sys.stdout.write("\r[fjd-dispatcher] Queue is empty and all jobs have finished.                          ")
+                sys.stdout.flush()
+                if end_on_empty_queue:
+                    sys.stdout.write("\n")
+                    Recruiter(project=project).fire()
+                    do_work = False
+        self.wrap_up()
+
+    def sort_jobqueue(self, jobqueue):
+        '''
+        Sort jobs - overwrite to enable dynamic queueing.
+        Only an idea as of now, see issue #13
+        '''
+        pass
