5627f6d8b7c0d146f91848e85ceaebb5d81db68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:06 +0000

    Add package roowifi/0.0.1/roowifi-0.0.1.tar.gz

---
+++ packages/roowifi/roowifi-0.0.1.tar.gz/roowifi-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='roowifi',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='maxvitek',
+    author_email='',
+    description='',
+    requires=[
+        'requests',
+        'argparse_actions'
+    ]
+)
