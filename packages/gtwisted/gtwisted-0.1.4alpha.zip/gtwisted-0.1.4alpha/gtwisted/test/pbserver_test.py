edf4ba0f258786351f27cc8b36966fb7a2fa9194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:18 +0000

    Add package gtwisted/0.1.4alpha/gtwisted-0.1.4alpha.zip

---
+++ packages/gtwisted/gtwisted-0.1.4alpha.zip/gtwisted-0.1.4alpha/gtwisted/test/pbserver_test.py
@@ -0,0 +1,29 @@
+#coding:utf8
+'''
+Created on 2014年2月22日
+
+@author:  lan (www.9miao.com)
+'''
+import sys
+from gtwisted.core import reactor
+from gtwisted.core.rpc import PBServerProtocl,PBServerFactory
+from gtwisted.utils import log
+import gevent
+
+
+class MyPBServerProtocl(PBServerProtocl):
+    
+    def remote_getResult(self,a,b):
+        print a,b
+        remoteobj=self.getRootObject()
+        print "11111111111"
+        print "result:",remoteobj.callRemoteForResult('printok')
+        return a+b
+        
+class MyPBServerFactory(PBServerFactory):
+    
+    protocol = MyPBServerProtocl
+    
+reactor.listenTCP(9090, MyPBServerFactory())
+log.startLogging(sys.stdout)
+reactor.run()
