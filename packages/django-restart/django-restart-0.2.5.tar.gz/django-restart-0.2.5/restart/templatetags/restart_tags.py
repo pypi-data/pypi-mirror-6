862e9c761922addae5f2c0b1dae4b4e38b9bb1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package django-restart/0.2.5/django-restart-0.2.5.tar.gz

---
+++ packages/django-restart/django-restart-0.2.5.tar.gz/django-restart-0.2.5/restart/templatetags/restart_tags.py
@@ -0,0 +1,30 @@
+﻿# -*- encoding: utf-8 -*-
+from django import template
+from django.template import defaultfilters
+from django.utils.translation import pgettext, ungettext, ugettext as _
+
+from classytags.core import Tag, Options
+from classytags.arguments import Argument
+from datetime import date, datetime
+
+register = template.Library()
+
+class RestartStatus(Tag):
+    name = 'restart_status'
+
+    def render_tag(self, context):
+        status = True
+        context['restart_status'] = status
+        return ''
+
+register.tag(RestartStatus)
+        
+# The code below is taken from django.contrib.humanize.naturaltime to ensure
+# compatibility with older versions of Django
+@register.filter
+def naturaltime(value):
+    try:
+        from django.contrib.humanize.templatetags.humanize import naturaltime as djangonaturaltime
+        return djangonaturaltime(value)
+    except:
+        return u"%s" % value
