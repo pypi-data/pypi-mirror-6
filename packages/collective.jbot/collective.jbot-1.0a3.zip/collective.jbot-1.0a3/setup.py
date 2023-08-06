fc3e513369ea0a5675f1ec86003727e440570440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package collective.jbot/1.0a3/collective.jbot-1.0a3.zip

---
+++ packages/collective.jbot/collective.jbot-1.0a3.zip/collective.jbot-1.0a3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a3'
+
+setup(name='collective.jbot',
+      version=version,
+      description="",
+      long_description="%s\n%s" % (
+          open("README.rst").read(),
+          open(os.path.join("docs", "HISTORY.txt")).read()
+      ),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+          "Framework :: Plone",
+          "Programming Language :: Python",
+      ],
+      keywords='plone templates customizations',
+      author='Nathan Van Gheem',
+      author_email='vangheem@gmail.com',
+      url='https://github.com/collective/collective.jbot',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.jbot',
+          'five.globalrequest',
+          'plone.app.theming>=1.1',
+          'plone.resource'
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
