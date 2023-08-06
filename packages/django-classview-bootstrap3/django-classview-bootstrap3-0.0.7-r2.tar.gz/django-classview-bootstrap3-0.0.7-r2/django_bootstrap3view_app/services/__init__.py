830ee759068a2fd9ec02adc7262abaaa1563f04f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:24 +0000

    Add package django-classview-bootstrap3/0.0.7-r2/django-classview-bootstrap3-0.0.7-r2.tar.gz

---
+++ packages/django-classview-bootstrap3/django-classview-bootstrap3-0.0.7-r2.tar.gz/django-classview-bootstrap3-0.0.7-r2/django_bootstrap3view_app/services/__init__.py
@@ -0,0 +1,5 @@
+"""Base service class for services for Django Bootstrap 3 view project.
+
+.. moduleauthor:: Ernesto Bossi <bossi.ernestog@gmail.com>
+
+"""
---
+++ packages/django-classview-bootstrap3/django-classview-bootstrap3-0.0.7-r2.tar.gz/django-classview-bootstrap3-0.0.7-r2/django_bootstrap3view_app/views/__init__.py
@@ -0,0 +1,5 @@
+"""Base class fot view for the Django Bootstrap 3 view project.
+
+.. moduleauthor:: Ernesto Bossi <bossi.ernestog@gmail.com>
+
+"""
