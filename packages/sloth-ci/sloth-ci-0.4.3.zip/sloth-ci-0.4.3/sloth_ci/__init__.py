aff0a9d608867f5d6149670d7f3d42edf4bfdcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:23 +0000

    Add package sloth-ci/0.4.3/sloth-ci-0.4.3.zip

---
+++ packages/sloth-ci/sloth-ci-0.4.3.zip/sloth-ci-0.4.3/sloth_ci/__init__.py
@@ -0,0 +1,16 @@
+"""
+********
+Sloth CI
+********
+
+**Sloth CI** is a lightweight CI script.
+
+The repo is at `bitbucket.org/moigagoo/sloth-ci <https://bitbucket.org/moigagoo/sloth-ci>`_.
+"""
+
+__title__ = 'sloth-ci'
+__version__ = '0.4.3'
+__author__ = 'Konstantin Molchanov'
+__license__ = 'MIT'
+
+from .sloth import Sloth
