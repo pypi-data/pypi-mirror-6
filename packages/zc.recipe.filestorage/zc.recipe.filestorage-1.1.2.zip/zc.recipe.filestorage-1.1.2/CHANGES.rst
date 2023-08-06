a2a18781d8ac1dc8139e2306d6569cc7482fb25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:53 +0000

    Add package zc.recipe.filestorage/1.1.2/zc.recipe.filestorage-1.1.2.zip

---
+++ packages/zc.recipe.filestorage/zc.recipe.filestorage-1.1.2.zip/zc.recipe.filestorage-1.1.2/CHANGES.rst
@@ -0,0 +1,31 @@
+=======
+CHANGES
+=======
+
+1.1.2 (2014-02-21)
+------------------
+
+- Fixed: packaging bug that caused 'pip install zc.recipe.filestorage' to fail
+  with an error about missing README.txt
+
+1.1.1 (2014-02-16)
+------------------
+
+- Fixed: packaging bug that caused a test failure in
+  a test runner that didn't use buildout to run setup.py.
+
+1.1.0 (2014-02-14)
+------------------
+
+- Python 3 compatibility
+
+- Using Python's ``doctest`` module instead of deprecated
+  ``zope.testing.doctest``.
+
+- Removed 'shared-blob-dir' from blobstorage section.
+
+
+1.0.0 (2007-11-03)
+------------------
+
+- Initial release.
---
+++ packages/zc.recipe.filestorage/zc.recipe.filestorage-1.1.2.zip/zc.recipe.filestorage-1.1.2/zc/recipe/filestorage/__init__.py
@@ -0,0 +1,76 @@
+##############################################################################
+#
+# Copyright (c) 2006, 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import logging, os
+
+
+class Recipe:
+
+    def __init__(self, buildout, name, options):
+        self.name, self.options = name, options
+
+        path = options.get('path')
+        if path is None:
+            path = os.path.join(buildout['buildout']['parts-directory'],
+                                self.name, 'Data.fs')
+            self.make_part = True
+        else:
+            path = os.path.join(buildout['buildout']['directory'], path)
+            if not os.path.exists(path):
+                logging.getLogger('zc.recipe.filestorage').error(
+                    "%s does not exixt", path)
+            self.make_part = False
+        options['path'] = path
+
+        blob_dir = options.get('blob-dir', '')
+        if not blob_dir:
+            template = plain_template
+        else:
+            template = blob_template
+            blob_dir = os.path.join(buildout['buildout']['directory'],
+                                    blob_dir)
+        options['blob-dir'] = blob_dir
+
+        options['zconfig'] = template % dict(
+            path=path,
+            blob_dir=blob_dir)
+
+    def install(self):
+        if self.make_part:
+            part = os.path.dirname(self.options['path'])
+            if not os.path.exists(part):
+                os.mkdir(part)
+        return ()
+
+    def update(self):
+        self.install()
+
+plain_template = """\
+<zodb>
+  <filestorage>
+    path %(path)s
+  </filestorage>
+</zodb>
+"""
+
+blob_template = """\
+<zodb>
+  <blobstorage>
+    blob-dir %(blob_dir)s
+    <filestorage>
+      path %(path)s
+    </filestorage>
+  </blobstorage>
+</zodb>
+"""
