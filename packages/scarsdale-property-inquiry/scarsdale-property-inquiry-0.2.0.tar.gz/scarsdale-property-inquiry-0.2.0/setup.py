18bfe475dc7ef3f6f886f60a45d3d2a9db8b4d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:49 +0000

    Add package scarsdale-property-inquiry/0.2.0/scarsdale-property-inquiry-0.2.0.tar.gz

---
+++ packages/scarsdale-property-inquiry/scarsdale-property-inquiry-0.2.0.tar.gz/scarsdale-property-inquiry-0.2.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='scarsdale-property-inquiry',
+    author='Thomas Levine',
+    author_email='_@thomaslevine.com',
+    description='Inquire about Scarsdale properties',
+    url='https://github.com/tlevine/scarsdale-property-inquiry.git',
+    classifiers=[
+        'Intended Audience :: Developers',
+    ],
+    packages=['scarsdale_property_inquiry'],
+    scripts=['bin/scarsdale-property-inquiry'],
+    install_requires = ['requests','randua','lxml','pickle_warehouse','randomsleep','dataset'],
+    tests_require = ['nose'],
+    version='0.2.0',
+    license='AGPL',
+)
