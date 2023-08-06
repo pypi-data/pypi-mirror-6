6bc7b0e5925341de7a4fd43034c57b55a5931ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:05 +0000

    Add package pyramid-jinja2/2.0/pyramid_jinja2-2.0.zip

---
+++ packages/pyramid-jinja2/pyramid_jinja2-2.0.zip/pyramid_jinja2-2.0/pyramid_jinja2/tests/base.py
@@ -0,0 +1,15 @@
+from pyramid import testing
+
+
+class Base(object):
+    def setUp(self):
+        self.request = testing.DummyRequest()
+        self.config = testing.setUp(request=self.request)
+        self.request.registry = self.config.registry
+        import os
+        here = os.path.abspath(os.path.dirname(__file__))
+        self.templates_dir = os.path.join(here, 'templates')
+
+    def tearDown(self):
+        testing.tearDown()
+        del self.config
