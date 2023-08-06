12a8b2bffbed399af92458c730d49317cdc286ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package magnetpy/0.0.1/magnetpy-0.0.1.tar.gz

---
+++ packages/magnetpy/magnetpy-0.0.1.tar.gz/magnetpy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='magnetpy',
+        version='0.0.1',
+        description='very simple and half-assed magnet url parser written in python.',
+        author='JarekPS',
+        author_email='jot@smiejczak.net',
+        url='https://github.com/jarekps/magnetpy',
+        packages=['magnet'],
+        py_modules=['magnet'],
+        license='New BSD License',
+        keywords='magnet bittorent urlparser url torrent')
+
