59fecc8a92b0e4e9d4bc56fcb1800136dc3472b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package django-urlmagic/0.3.5/django-urlmagic-0.3.5.tar.gz

---
+++ packages/django-urlmagic/django-urlmagic-0.3.5.tar.gz/django-urlmagic-0.3.5/urlmagic/utils.py
@@ -0,0 +1,12 @@
+from django.utils.text import slugify
+
+
+def model_names(model):
+    names = {}
+    names["model_system"] = unicode(model._meta.module_name)
+    names["model_singular"] = unicode(model._meta.verbose_name)
+    names["model_singular_slug"] = unicode(slugify(names["model_singular"]))
+    names["model_plural"] = unicode(model._meta.verbose_name_plural)
+    names["model_plural_slug"] = unicode(slugify(names["model_plural"]))
+    names["model_module"] = unicode(model.__module__.split(".")[-2])
+    return names
