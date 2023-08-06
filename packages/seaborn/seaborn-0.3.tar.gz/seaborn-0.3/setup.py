cc652c2bc9a84379ca17855693eb14f8c6c8b2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:26 +0000

    Add package seaborn/0.3/seaborn-0.3.tar.gz

---
+++ packages/seaborn/seaborn-0.3.tar.gz/seaborn-0.3/setup.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2012-2014 Michael Waskom <mwaskom@stanford.edu>
+
+DESCRIPTION = "Seaborn: statistical data visualization"
+DISTNAME = 'seaborn'
+MAINTAINER = 'Michael Waskom'
+MAINTAINER_EMAIL = 'mwaskom@stanford.edu'
+URL = 'http://stanford.edu/~mwaskom/software/seaborn/'
+LICENSE = 'BSD (3-clause)'
+DOWNLOAD_URL = 'https://github.com/mwaskom/seaborn/'
+VERSION = '0.3'
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    import os
+    if os.path.exists('MANIFEST'):
+        os.remove('MANIFEST')
+
+    setup(name=DISTNAME,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        license=LICENSE,
+        url=URL,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        packages=['seaborn', 'seaborn.external', 'seaborn.tests'],
+        classifiers=[
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.3',
+                     'License :: OSI Approved :: BSD License',
+                     'Topic :: Scientific/Engineering :: Visualization',
+                     'Topic :: Multimedia :: Graphics',
+                     'Operating System :: POSIX',
+                     'Operating System :: Unix',
+                     'Operating System :: MacOS'],
+        install_requires=["pandas"],
+          )
