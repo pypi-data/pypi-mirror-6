c55725b4d7bd5ad488eff6d19afa8cf552bdbd3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:34 +0000

    Add package AndrewList/1.0.0/AndrewList-1.0.0.zip

---
+++ packages/AndrewList/AndrewList-1.0.0.zip/AndrewList-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name				= 'AndrewList',
+	version			= '1.0.0',
+	py_modules	= ['AndrewList'],
+	author				= 'andrew',
+	author_email	= 'andrew2767@gmail.com',
+	url					= 'http://www.eye2web.co.kr',
+	description		= 'A test printer method of nested list'
+)
