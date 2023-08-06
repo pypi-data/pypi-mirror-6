76ebbaa8ab861623ef585f4dad7a4c184ac23acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package bernoulli_django/0.1.6/bernoulli_django-0.1.6.tar.gz

---
+++ packages/bernoulli_django/bernoulli_django-0.1.6.tar.gz/bernoulli_django-0.1.6/bernoulli_django/__init__.py
@@ -0,0 +1,10 @@
+from bernoulli import record_goal_attained
+from django.conf import settings
+
+def record_variant_goal(request, experiment_id):
+    user_id = request.user.id
+
+    if request.user.is_anonymous():
+        user_id = request.session.get('bernoulli_id', None)
+
+    return record_goal_attained(settings.BERNOULLI_CLIENT_ID, experiment_id, user_id)
