142535a7b2d249fa87faf8a9b508938ebfb42814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:09 +0000

    Add package infi.recipe.application-packager/0.4.5/infi.recipe.application_packager-0.4.5-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.4.5-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.5"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.4.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '50eb5fc1cecc6a8ff03f046ccc0b45ef10565f53'
+__git_head_subject__ = 'bumping expected dependencies in CompilerTestCase.test_get_packages_to_install'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
