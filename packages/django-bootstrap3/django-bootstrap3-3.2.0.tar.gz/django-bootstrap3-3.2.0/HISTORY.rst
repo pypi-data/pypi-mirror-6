95b5106a40b3eada2fe5dbd8700bd5790bdff085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:05 +0000

    Add package django-bootstrap3/3.2.0/django-bootstrap3-3.2.0.tar.gz

---
+++ packages/django-bootstrap3/django-bootstrap3-3.2.0.tar.gz/django-bootstrap3-3.2.0/HISTORY.rst
@@ -0,0 +1,41 @@
+.. :changelog:
+
+History
+-------
+
+3.2.0 (2014-03-11)
+++++++++++++++++++
+
+* support for addons
+
+
+3.1.0 (2014-03-03)
+++++++++++++++++++
+
+* improve compatibility with Django < 1.5
+
+
+3.0.0 (2014-02-28)
+++++++++++++++++++
+
+* added support for themes (fix issue #74)
+* show inline form errors in field title (fix issue #81)
+* fixed bugs in demo application
+* update to newest Bootstrap (fix issue #83)
+
+
+2.6.0 (2014-02-20)
+++++++++++++++++++
+
+* new setting `set_required` to control setting of HTML `required` attribute (fix issue #76)
+
+
+2.5.6 (2014-01-23)
+++++++++++++++++++
+
+* project refactored
+* added skeleton for creating documentation (fix issue #30)
+* fixed FileField issues
+
+
+
---
+++ packages/django-bootstrap3/django-bootstrap3-3.2.0.tar.gz/django-bootstrap3-3.2.0/demo/demo/forms.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django import forms
+from django.forms.extras import SelectDateWidget
+
+
+RADIO_CHOICES = (
+    ('1', 'Radio 1'),
+    ('2', 'Radio 2'),
+)
+
+MEDIA_CHOICES = (
+    ('Audio', (
+        ('vinyl', 'Vinyl'),
+        ('cd', 'CD'),
+    )
+    ),
+    ('Video', (
+        ('vhs', 'VHS Tape'),
+        ('dvd', 'DVD'),
+    )
+    ),
+    ('unknown', 'Unknown'),
+)
+
+
+class ContactForm(forms.Form):
+    date = forms.DateField(widget=SelectDateWidget)
+    subject = forms.CharField(max_length=100, help_text='Maximum 100 chars.')
+    message = forms.CharField(required=False, help_text='<i>my_help_text</i>')
+    sender = forms.EmailField()
+    secret = forms.CharField(initial=42, widget=forms.HiddenInput)
+    cc_myself = forms.BooleanField(required=False, help_text='You will get a copy in your mailbox.')
+    select1 = forms.ChoiceField(choices=RADIO_CHOICES)
+    select2 = forms.MultipleChoiceField(
+        choices=RADIO_CHOICES,
+        help_text='Check as many as you like.',
+    )
+    select3 = forms.ChoiceField(choices=MEDIA_CHOICES)
+    select4 = forms.MultipleChoiceField(
+        choices=MEDIA_CHOICES,
+        help_text='Check as many as you like.',
+    )
+    category1 = forms.ChoiceField(choices=RADIO_CHOICES, widget=forms.RadioSelect)
+    category2 = forms.MultipleChoiceField(
+        choices=RADIO_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+        help_text='Check as many as you like.',
+    )
+    category3 = forms.ChoiceField(widget=forms.RadioSelect, choices=MEDIA_CHOICES)
+    category4 = forms.MultipleChoiceField(
+        choices=MEDIA_CHOICES,
+        widget=forms.CheckboxSelectMultiple,
+        help_text='Check as many as you like.',
+    )
+
+    def clean(self):
+        cleaned_data = super(ContactForm, self).clean()
+        raise forms.ValidationError("This error was added to show the non field errors styling.")
+        return cleaned_data
+
+
+class FilesForm(forms.Form):
+    text1 = forms.CharField()
+    file1 = forms.FileField()
+    file2 = forms.FileField(required=False)
+    file3 = forms.FileField(widget=forms.ClearableFileInput)
+    file4 = forms.FileField(required=False, widget=forms.ClearableFileInput)
