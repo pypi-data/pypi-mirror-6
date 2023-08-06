da2f5ee2c89cb2638b0368d284c6bc5276ab851a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package riemann-client/3.0.3/riemann_client-3.0.3-py27-none-any.whl

---
+++ packages/riemann-client/riemann_client-3.0.3-py27-none-any.whl/riemann_client/transport.py
@@ -0,0 +1,87 @@
+"""Riemann TCP and UDP transports"""
+
+from __future__ import absolute_import
+
+import abc
+import socket
+import struct
+
+import riemann_client.riemann_pb2
+
+
+def socket_recvall(socket, length, bufsize=4096):
+    """Recives bytes from a socket until the buffer is the requested length"""
+    data = ""
+    while len(data) < length:
+        data += socket.recv(bufsize)
+    return data
+
+
+class RiemannError(Exception):
+    """Error class for errors recived from the Riemann server"""
+    pass
+
+
+class Transport(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, host='localhost', port=5555):
+        self.host = host
+        self.port = port
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.disconnect()
+
+    @property
+    def address(self):
+        return self.host, self.port
+
+    @abc.abstractmethod
+    def connect(self):
+        pass
+
+    @abc.abstractmethod
+    def disconnect(self):
+        pass
+
+    @abc.abstractmethod
+    def send(self):
+        pass
+
+
+class UDPTransport(Transport):
+    def connect(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def disconnect(self):
+        self.socket.close()
+
+    def send(self, message):
+        self.socket.sendto(message.SerializeToString(), self.address)
+        return NotImplemented
+
+
+class TCPTransport(Transport):
+    def connect(self):
+        self.socket = socket.create_connection(self.address)
+        self.socket.setblocking(True)
+
+    def disconnect(self):
+        self.socket.close()
+
+    def send(self, message):
+        message = message.SerializeToString()
+        self.socket.sendall(struct.pack('!I', len(message)) + message)
+
+        length = struct.unpack('!I', self.socket.recv(4))[0]
+        response = riemann_client.riemann_pb2.Msg()
+        response.ParseFromString(socket_recvall(self.socket, length))
+
+        if not response.ok:
+            raise RiemannError(response.error)
+
+        return response
