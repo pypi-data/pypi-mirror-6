4e44343e6631ba1ed6fb14dd58f5bcb7fe098bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:45 +0000

    Add package site-auditor/1.4.1/site-auditor-1.4.1.zip

---
+++ packages/site-auditor/site-auditor-1.4.1.zip/site-auditor-1.4.1/site_auditor/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+__author__ = 'ST LEON'
+__title__ = 'site-auditor'
+__version__ = '1.4.1'
+__license__ = 'GPLv3'
+__copyright__ = 'Copyright 2014 ST LEON'
+
+from .site_auditor import SiteAuditor
