4342cc43e0bc78e9b3f2e7533f10675d72642973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:44 +0000

    Add package infi.multipathtools/0.1.23/infi.multipathtools-0.1.23.tar.gz

---
+++ packages/infi.multipathtools/infi.multipathtools-0.1.23.tar.gz/infi.multipathtools-0.1.23/src/infi/multipathtools/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.23"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.23)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '804418547e9c6f2c518c08d16c98482b236db612'
+__git_head_subject__ = "skip test that doesn't work"
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
