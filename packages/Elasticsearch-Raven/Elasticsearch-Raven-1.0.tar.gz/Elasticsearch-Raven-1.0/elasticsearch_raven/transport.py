cfb92f3b40b847cd282a294fbb3a4c59e3d7097b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:43 +0000

    Add package Elasticsearch-Raven/1.0/Elasticsearch-Raven-1.0.tar.gz

---
+++ packages/Elasticsearch-Raven/Elasticsearch-Raven-1.0.tar.gz/Elasticsearch-Raven-1.0/elasticsearch_raven/transport.py
@@ -0,0 +1,101 @@
+import base64
+import collections
+import contextlib
+import datetime
+import json
+import logging
+import re
+import time
+import zlib
+
+import elasticsearch
+
+from elasticsearch_raven import exceptions
+from elasticsearch_raven.postfix import postfix_encoded_data
+
+
+class SentryMessage(collections.namedtuple('SentryMessage',
+                                           ['headers', 'body'])):
+    @classmethod
+    def create_from_udp(cls, data):
+        try:
+            byte_headers, data = data.split(b'\n\n')
+        except ValueError:
+            raise exceptions.DamagedSentryMessageError
+        headers = cls.parse_headers(str(byte_headers.decode('utf-8')))
+        data = base64.b64decode(data)
+        return cls(headers, data)
+
+    @classmethod
+    def create_from_http(cls, raw_headers, data):
+        headers = cls.parse_headers(raw_headers)
+        data = base64.b64decode(data)
+        return cls(headers, data)
+
+    @staticmethod
+    def parse_headers(raw_headers):
+        match = re.search(r'sentry_key=(?P<sentry_key>[^, =]+), sentry_secret='
+                          r'(?P<sentry_secret>[^, =]+)$', raw_headers)
+        if match:
+            return match.groupdict()
+        else:
+            raise exceptions.BadSentryMessageHeaderError
+
+    def decode_body(self):
+        try:
+            return json.loads(zlib.decompress(self.body).decode('utf-8'))
+        except (zlib.error, ValueError):
+            raise exceptions.DamagedSentryMessageBodyError
+
+
+class ElasticsearchTransport:
+    def __init__(self, host, use_ssl=False):
+        self._host = host
+        self._use_ssl = use_ssl
+
+    def send(self, message):
+        message_body = message.decode_body()
+        postfix_encoded_data(message_body)
+        dated_index = message_body['project'].format(datetime.datetime.now())
+        http_auth = '{}:{}'.format(message.headers['sentry_key'],
+                                   message.headers['sentry_secret'])
+        connection = elasticsearch.Elasticsearch(hosts=[self._host],
+                                                 http_auth=http_auth,
+                                                 use_ssl=self._use_ssl)
+        for retry in retry_loop(15 * 60, delay=1, back_off=1.5):
+            try:
+                with logger_level_to_error('elasticsearch'):
+                    connection.index(body=message_body, index=dated_index,
+                                     doc_type='raven-log')
+            except elasticsearch.exceptions.ConnectionError as e:
+                retry(e)
+
+
+def retry_loop(timeout, delay, back_off=1.0):
+    start_time = time.time()
+    exceptions = []
+
+    def retry(exception):
+        exceptions.append(exception)
+
+    while True:
+        yield retry
+        if not exceptions:
+            return
+        if time.time() - start_time > timeout:
+            break
+        else:
+            exceptions.clear()
+        time.sleep(delay)
+        delay *= back_off
+
+    raise exceptions[0]
+
+
+@contextlib.contextmanager
+def logger_level_to_error(logger_name):
+    logger = logging.getLogger(logger_name)
+    level = logger.level
+    logger.setLevel(logging.ERROR)
+    yield
+    logger.setLevel(level)
---
+++ packages/Elasticsearch-Raven/Elasticsearch-Raven-1.0.tar.gz/Elasticsearch-Raven-1.0/elasticsearch_raven/udp_server.py
@@ -0,0 +1,139 @@
+import argparse
+import datetime
+import socket
+import sys
+import time
+import threading
+
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+import elasticsearch
+
+from elasticsearch_raven import configuration
+from elasticsearch_raven.transport import ElasticsearchTransport
+from elasticsearch_raven.transport import SentryMessage
+
+
+def run_server():
+    args = _parse_args()
+    try:
+        sock = get_socket(args.ip, args.port)
+    except socket.gaierror:
+        sys.stdout.write('Wrong hostname.\n')
+        sys.exit(1)
+    else:
+        transport = ElasticsearchTransport(configuration['host'],
+                                           configuration['use_ssl'])
+        pending_logs = queue.Queue(configuration['queue_maxsize'])
+        exception_queue = queue.Queue()
+        _run_server(sock, pending_logs, exception_queue, transport, args.debug)
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Udp proxy server for raven')
+    parser.add_argument('ip')
+    parser.add_argument('port', type=int)
+    parser.add_argument('--debug', dest='debug', action='store_const',
+                        const=True, default=False,
+                        help='Print debug logs to stdout')
+    return parser.parse_args()
+
+
+def get_socket(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, int(port)))
+    return sock
+
+
+def _run_server(sock, pending_logs, exception_queue, transport, debug=False):
+    handler = get_handler(sock, pending_logs, exception_queue, debug=debug)
+    sender = get_sender(transport, pending_logs, exception_queue)
+    handler.start()
+    sender.start()
+    try:
+        raise exception_queue.get()
+    except KeyboardInterrupt:
+        sock.close()
+        try:
+            while pending_logs.unfinished_tasks:
+                try:
+                    raise exception_queue.get(timeout=1)
+                except queue.Empty:
+                    pass
+        except KeyboardInterrupt:
+            pass
+
+
+def get_handler(sock, pending_logs, exception_queue, debug=False):
+    def _handle():
+        try:
+            while True:
+                data, address = sock.recvfrom(65535)
+                message = SentryMessage.create_from_udp(data)
+                pending_logs.put(message)
+                if debug:
+                    sys.stdout.write('{host}:{port} [{date}]\n'.format(
+                        host=address[0], port=address[1],
+                        date=datetime.datetime.now()))
+        except Exception as e:
+            sock.close()
+            pending_logs.join()
+            exception_queue.put(e)
+
+    handler = threading.Thread(target=_handle)
+    handler.daemon = True
+    return handler
+
+
+def get_sender(transport, pending_logs, exception_queue):
+
+    def _send():
+        try:
+            while True:
+                _send_message(transport, pending_logs)
+        except Exception as e:
+            exception_queue.put(e)
+
+    sender = threading.Thread(target=_send)
+    sender.daemon = True
+    return sender
+
+
+def _send_message(transport, pending_logs):
+    message = pending_logs.get()
+    try:
+        for retry in retry_loop(15 * 60, delay=1, back_off=1.5):
+            try:
+                transport.send(message)
+            except elasticsearch.exceptions.ConnectionError as e:
+                retry(e)
+    except elasticsearch.exceptions.TransportError as e:
+        connection = elasticsearch.Elasticsearch(
+            hosts=[configuration['host']],
+            use_ssl=configuration['use_ssl'],
+            http_auth=configuration['error_http_auth'])
+        body = {'message': str(message), 'error': str(e)}
+        connection.index(index='elasticsearch-raven-error', body=body,
+                         doc_type='elasticsearch-raven-log')
+    pending_logs.task_done()
+
+
+def retry_loop(timeout, delay, back_off=1.0):
+    start_time = time.time()
+    exceptions = set()
+
+    def retry(exception):
+        exceptions.add(exception)
+    yield retry
+    while time.time() - start_time <= timeout:
+        if not exceptions:
+            return
+        time.sleep(delay)
+        delay *= back_off
+        exceptions.clear()
+        yield retry
+
+    raise exceptions.pop()
