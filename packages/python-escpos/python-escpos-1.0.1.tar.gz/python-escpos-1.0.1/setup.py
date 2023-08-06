9f077397c63dbcfcdadce0e287859549d70d0a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:14 +0000

    Add package python-escpos/1.0.1/python-escpos-1.0.1.tar.gz

---
+++ packages/python-escpos/python-escpos-1.0.1.tar.gz/python-escpos-1.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name='python-escpos',
+    version='1.0.1',
+    url='http://code.google.com/p/python-escpos',
+    download_url='https://github.com/mosquito/python-escpos/archive/master.zip',
+    description='Python library to manipulate ESC/POS Printers',
+    license='GNU GPL v3',
+    long_description=open('README').read(),
+    author='Manuel F Martinez',
+    author_email='manpaz@bashlinux.com',
+    platforms=['linux'],
+    packages=[
+        'escpos',
+    ],
+    package_data={'': ['COPYING']},
+    install_requires=[
+        'pyusb',
+        'Pillow>=2.0',
+        'qrcode>=4.0',
+    ],
+)
