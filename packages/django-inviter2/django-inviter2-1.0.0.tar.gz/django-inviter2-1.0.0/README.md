f64f051e96819bdd6cecc2f04389571a0cd0c293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:46 +0000

    Add package django-inviter2/1.0.0/django-inviter2-1.0.0.tar.gz

---
+++ packages/django-inviter2/django-inviter2-1.0.0.tar.gz/django-inviter2-1.0.0/README.md
@@ -0,0 +1,24 @@
+django-inviter2
+===============
+
+[![Build Status](https://travis-ci.org/mjschultz/django-inviter2.png)](https://travis-ci.org/mjschultz/django-inviter2)
+[![Coverage Status](https://coveralls.io/repos/mjschultz/django-inviter2/badge.png?branch=master)](https://coveralls.io/r/mjschultz/django-inviter2?branch=master)
+
+Let your users invite new users.
+
+Useful if you want your webapp to be in a private beta mode or for only permitting access to known users.
+
+Version Support
+---------------
+
+- Django 1.4, 1.5, and 1.6 (requires Python 2.6 or 2.7)
+- Django 1.5 and 1.6 (requires Python 2.6, 2.7, 3.2, or 3.3)
+
+History
+-------
+
+This is a fork of [django-inviter](http://www.caffeinehit.com/work/code/django-inviter).
+
+
+Documentation is available via [Read the Docs](http://django-inviter2.readthedocs.org/).
+
---
+++ packages/django-inviter2/django-inviter2-1.0.0.tar.gz/django-inviter2-1.0.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import inviter2
+
+setup(
+    name='django-inviter2',
+    version=inviter2.__version__,
+    description='Simple email invitations for your Django app',
+    long_description=open('README.md').read(),
+    author='Michael J Schultz',
+    author_email='mjschultz@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Framework :: Django',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        #'Programming Language :: Python :: 3.4',
+    ],
+    install_requires=[
+        "shortuuid >= 0.1",
+        "six",
+        "Django >= 1.4",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
