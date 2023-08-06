b50acd4aeab68c49a8eebaa59febd867267f672b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:50 +0000

    Add package mysql-statsd/0.1.1/mysql-statsd-0.1.1.tar.gz

---
+++ packages/mysql-statsd/mysql-statsd-0.1.1.tar.gz/mysql-statsd-0.1.1/mysql_statsd/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jasper Capel'
+__email__ = 'jasper.capel@spilgames.com'
+__version__ = '0.1.1'
+__all__ = ['mysql_statsd']
+from mysql_statsd import MysqlStatsd 
