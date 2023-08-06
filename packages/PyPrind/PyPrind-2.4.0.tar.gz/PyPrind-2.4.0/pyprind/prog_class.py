947d9a8549e5b24e082478ef2ce1ea70bece6be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:30 +0000

    Add package PyPrind/2.4.0/PyPrind-2.4.0.tar.gz

---
+++ packages/PyPrind/PyPrind-2.4.0.tar.gz/PyPrind-2.4.0/pyprind/prog_class.py
@@ -0,0 +1,73 @@
+import time
+import sys
+import os
+
+class Prog():
+    def __init__(self, iterations, track_time, stream, title):
+        """ Initializes tracking object. """
+        self.cnt = 0
+        self.title = title
+        self.max_iter = float(iterations) # to support Python 2.x
+        self.track = track_time
+        self.start = time.time()
+        self.end = 0.0
+        self.stream = stream
+        self._stream_out = self._no_stream
+        self._stream_flush = self._no_stream
+        self._check_stream()
+        self._print_title()
+
+
+    def _check_stream(self):
+        """ Determines which output stream (stdout, stderr, or custom) to use. """
+        if self.stream == 1 and os.isatty(sys.stdout.fileno()):
+            self._stream_out = sys.stdout.write
+            self._stream_flush = sys.stdout.flush
+        elif self.stream == 2 and os.isatty(sys.stderr.fileno()):
+            self._stream_out = sys.stderr.write
+            self._stream_flush = sys.stderr.flush
+        elif self.stream is not None and hasattr(self.stream, 'write'):
+            self._stream_out = self.stream.write
+            self._stream_flush = self.stream.flush
+        else:
+            print('Warning: No valid output stream.')
+
+    def _elapsed(self):
+        """ Returns elapsed time at update. """
+        return time.time() - self.start
+
+    def _calc_eta(self):
+        """ Calculates estimated time left until completion. """
+        elapsed = self._elapsed()
+        if self.cnt == 0 or elapsed < 0.001:
+            return None
+        rate = float(self.cnt) / elapsed
+        return (float(self.max_iter) - float(self.cnt)) / rate
+
+    def _calc_percent(self):
+        """Calculates the rel. progress in percent with 2 decimal points."""
+        return round(self.cnt / self.max_iter * 100, 2)
+
+    def _no_stream(self, text=None):
+        """ Called when no valid output stream is available. """
+        pass
+
+    def _finish(self):
+        """ Determines if maximum number of iterations (seed) is reached. """
+        if self.cnt == self.max_iter:
+            self.end = self._elapsed()
+            if self.track:
+                self._stream_out('\nTotal time elapsed: {:.3f} sec'.format(self.end))
+            self._stream_out('\n')
+
+    def _print_title(self):
+        """ Prints tracking title at initialization. """
+        if self.title:
+            self._stream_out('{}\n'.format(self.title))
+            self._stream_flush()
+
+    def __repr__(self):
+        return 'Title: {}\nTotal time elapsed: {:.3f} sec'.format(self.title, self.end)
+
+    def __str__(self):
+        return self.__repr__()
---
+++ packages/PyPrind/PyPrind-2.4.0.tar.gz/PyPrind-2.4.0/pyprind/progpercent.py
@@ -0,0 +1,53 @@
+# Sebastian Raschka 2014
+#
+# Progress Percentage class to instantiate a percentage indicator object
+# that is printed to the standard output screen to visualize the
+# progress in a iterative Python procedure
+
+from pyprind.prog_class import Prog
+
+
+class ProgPercent(Prog):
+    """
+    Initializes a percentage indicator object that allows visuzalization
+    of an iterational computation in the standard output screen. 
+
+    Keyword Arguments:
+        iterations (int): number of iterations of the computation
+        width (int): width of the progress bar in characters
+        track_time (bool): prints elapsed time and estimated time left
+        stream: takes 1 for stdout, 2 for stderr, or given stream object
+        title (str): A title for the percent indicator
+
+    """
+    def __init__(self, iterations, track_time=True, stream=2, title=''):
+        Prog.__init__(self, iterations, track_time, stream, title)
+        self.perc = 0
+        self._print_update()
+
+    def _print_update(self):
+        """Prints formatted integer percentage and tracked time to the screen."""
+        self._stream_out('\r[%3d %%]' % (self.perc))
+        if self.track:
+            self._stream_out(' elapsed[sec]: {:.3f}'.format(self._elapsed()))
+            if self._calc_eta():
+                self._stream_out(' | ETA[sec]: {:.3f} '.format(self._calc_eta()))  
+            self._stream_flush()
+
+    def update(self, iterations=1):
+        """
+        Updates the progress bar in every iteration of the task.
+
+        Keyword arguments:
+            iterations (int): default argument can be changed to integer values
+                >=1 in order to update the progress indicators more than once 
+                per iteration.
+
+        """
+        self.cnt += iterations
+        next_perc = self._calc_percent()
+        if next_perc > self.perc:
+            self.perc = next_perc
+            self._print_update()
+            self._stream_flush()
+        self._finish()
