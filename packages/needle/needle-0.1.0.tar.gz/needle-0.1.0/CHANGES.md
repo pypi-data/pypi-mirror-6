8bcadedf652d6441c648d330b441f969d98aaece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:30 +0000

    Add package needle/0.1.0/needle-0.1.0.tar.gz

---
+++ packages/needle/needle-0.1.0.tar.gz/needle-0.1.0/CHANGES.md
@@ -0,0 +1,25 @@
+Change log
+==========
+
+0.1.0 (2014-02-20)
+------------------
+
+ - Add `set_viewport_size()` method to `NeedleTestCase`
+ - Calculate the dimensions of elements more accurately with jQuery
+ - Only load jQuery if it hasn't already been loaded
+
+Thanks @jphalip!
+
+0.0.2 (2013-10-24)
+------------------
+
+ - Allow needle to be used with custom web driver
+ - Replace PIL with pillow
+
+Thanks @treyhunner!
+
+0.0.1 (2013-05-07)
+------------------
+
+Initial release.
+
---
+++ packages/needle/needle-0.1.0.tar.gz/needle-0.1.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import re
+import os
+import codecs
+
+
+# Borrowed from
+# https://github.com/jezdez/django_compressor/blob/develop/setup.py
+def read(*parts):
+    return codecs.open(os.path.join(os.path.dirname(__file__), *parts)).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='needle',
+    version=find_version("needle", "__init__.py"),
+    description='Automated testing for your CSS.',
+    author='Ben Firshman',
+    author_email='ben@firshman.co.uk',
+    url='https://github.com/bfirsh/needle',
+    packages=find_packages(exclude=['scripts', 'tests']),
+    package_data={'needle': ['js/*']},
+    test_suite='nose.collector',
+    entry_points = {
+        'nose.plugins.0.10': [
+            'needle-capture = needle.plugin:NeedleCapturePlugin'
+        ]
+    },
+    install_requires=[
+        'nose>=1.0.0',
+        'selenium>=2,<3',
+        'unittest2>=0.5.1',
+        'pillow',
+    ],
+)
+
