2339871c7d468eca1a161d18005c6bd7bda67cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:14 +0000

    Add package pyulogin/0.0.1/pyulogin-0.0.1-py2.7.egg

---
+++ packages/pyulogin/pyulogin-0.0.1-py2.7.egg/pyulogin/ulogin_manager.py
@@ -0,0 +1,40 @@
+'''
+Copyright (c)2014 Gurov Dmitri 
+
+See the file license.txt for copying permission.
+
+This file contains class of UloginManager object,which return UloginUser with information about user
+UloginManager execute query to ulogin service and processing data from service. 
+   
+'''
+#!/usr/bin/env python
+import json
+import urllib
+import urllib2
+from user import UloginUser
+import ulogin
+
+class UloginManager:
+	def __init__(self,request):
+		server = request.environ['HTTP_HOST']
+		token = request.form['token']
+		
+		self.ulogdata = ({'token': token, 'host': server})
+		self.setUserData()
+
+	def setUserData(self):
+		self.ulogin = urllib2.urlopen(ulogin.url+'?'+self.ulogdata)
+		self.ulogin = self.ulogin.read()
+		decoder = json.JSONDecoder()
+		self.ulogin = decoder.decode()
+		if 'error' in self.ulogin:
+			if 'host is not' in ulogin['error']:
+					raise  ulogin.UloginError['host is not XXX']
+			else:
+				raise ulogin.UloginError[ulogin['error']]
+			 
+
+	def getUser(self):
+		return UloginUser(self.ulogin)
+
+
