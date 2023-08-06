46e869f8fd8f450e656d9661abb492897ef147c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:10 +0000

    Add package common/0.1.1/common-0.1.1.tar.gz

---
+++ packages/common/common-0.1.1.tar.gz/common-0.1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='common',
+      version='0.1.1',
+      description='Common tools and data structures implemented in pure python.',
+      author='Marcel Hellkamp',
+      author_email='marc@gsites.de',
+      url='https://pypi.python.org/pypi/common',
+      packages=['common', 'common.term'],
+)
