21fa23c706505a4b97973983469c9c500a27720c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:20 +0000

    Add package trytond_nereid_cart_b2c/3.0.3.0dev1/trytond_nereid_cart_b2c-3.0.3.0dev1.tar.gz

---
+++ packages/trytond_nereid_cart_b2c/trytond_nereid_cart_b2c-3.0.3.0dev1.tar.gz/trytond_nereid_cart_b2c-3.0.3.0dev1/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+'''
+    nereid_cart.
+
+    Cart
+
+    :copyright: (c) 2010-2013 by Openlabs Technologies & Consulting (P) Ltd.
+    :license: GPLv3, see LICENSE for more details
+'''
+from trytond.pool import Pool
+
+from product import Product
+from sale import Sale
+from cart import Cart
+from website import Website
+
+
+def register():
+    Pool.register(
+        Product,
+        Sale,
+        Cart,
+        Website,
+        type_="model", module="nereid_cart_b2c"
+    )
