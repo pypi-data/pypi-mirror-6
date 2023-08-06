3e532517aa273d7d8337544028c9bc5e32817241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:31 +0000

    Add package django-sslify/0.2.3/django-sslify-0.2.3.tar.gz

---
+++ packages/django-sslify/django-sslify-0.2.3.tar.gz/django-sslify-0.2.3/sslify/tests.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponsePermanentRedirect
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from sslify.middleware import SSLifyMiddleware as _SSLifyMiddleware
+
+
+class SSLifyMiddlware(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_perma_redirects_http_to_https(self):
+        request = self.factory.get('/woot/')
+        self.assertTrue(request.build_absolute_uri().startswith('http://'))
+
+        middleware = _SSLifyMiddleware()
+        request = middleware.process_request(request)
+
+        self.assertIsInstance(request, HttpResponsePermanentRedirect)
+        self.assertTrue(request['Location'].startswith('https://'))
+
+    def test_disable_for_tests(self):
+        """If disabled, we get a 404"""
+        with self.settings(SSLIFY_DISABLE=True):
+            request = self.client.get('/woot/')
+            self.assertEqual(404, request.status_code)
+
+    def tearDown(self):
+        del self.factory
