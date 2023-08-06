2291f41e62f4b092a0c6f21270ed95e82b00d82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:39 +0000

    Add package infi.run-as/0.1.4/infi.run_as-0.1.4.tar.gz

---
+++ packages/infi.run-as/infi.run_as-0.1.4.tar.gz/infi.run_as-0.1.4/src/infi/run_as/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.4"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b2fca7952bdf650f4c6d4a738c1f6543e7cb23d8'
+__git_head_subject__ = 'Finished release v0.1.4.'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
