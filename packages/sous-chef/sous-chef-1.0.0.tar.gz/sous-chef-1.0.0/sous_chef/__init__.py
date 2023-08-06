3e2b45563fe6d18a352239a0a05969ee3c2bde95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:44 +0000

    Add package sous-chef/1.0.0/sous-chef-1.0.0.tar.gz

---
+++ packages/sous-chef/sous-chef-1.0.0.tar.gz/sous-chef-1.0.0/sous_chef/__init__.py
@@ -0,0 +1,10 @@
+"""sous-chef - A small webapp for viewing and searching Chef nodes"""
+
+from __future__ import absolute_import
+
+__version__ = '1.0.0'
+__author__ = 'Sam Clements <sam.clements@datasift.com>'
+__all__ = ['create_app', 'create_debug_app']
+
+
+from sous_chef.app import create_app, create_debug_app
