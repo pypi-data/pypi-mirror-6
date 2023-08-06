940acd379f71493bcc6f58656986fe5ab590e4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:44 +0000

    Add package supermann/1.8.1/supermann-1.8.1-py27-none-any.whl

---
+++ packages/supermann/supermann-1.8.1-py27-none-any.whl/supermann/__init__.py
@@ -0,0 +1,9 @@
+"""A Supervisor event listener for Riemann"""
+
+from __future__ import absolute_import, unicode_literals
+
+from supermann.core import Supermann
+
+__version__ = '1.7.2'
+__author__ = 'Sam Clements <sam.clements@datasift.com>'
+__all__ = ['Supermann']
