9c8a77719e6e05991add8808a8fb8f4db6a17460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:12 +0000

    Add package secretsharing/0.1.3/secretsharing-0.1.3.tar.gz

---
+++ packages/secretsharing/secretsharing-0.1.3.tar.gz/secretsharing-0.1.3/secretsharing/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    Secret Sharing
+    ~~~~~
+
+    :copyright: (c) 2014 by Halfmoon Labs
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1.3'
+
+from .shamir import Secret
