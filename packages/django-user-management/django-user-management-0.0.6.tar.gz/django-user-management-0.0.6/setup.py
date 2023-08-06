805a7557c1bb9b4ab9980f2699b5809ec140db2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:10 +0000

    Add package django-user-management/0.0.6/django-user-management-0.0.6.tar.gz

---
+++ packages/django-user-management/django-user-management-0.0.6.tar.gz/django-user-management-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+version = '0.0.6'
+
+
+install_requires = (
+    'djangorestframework>=2.3.12,<3',
+    'incuna_mail>=0.1.1',
+)
+
+setup(
+    name='django-user-management',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description='',
+    long_description='',
+    author='incuna',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/django-user-management/',
+    install_requires=install_requires,
+    zip_safe=False,
+)
