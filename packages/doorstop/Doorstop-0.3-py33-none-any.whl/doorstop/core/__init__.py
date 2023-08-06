24c68f6eb92039660e1e2ffc81acba97e2ae1b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:22 +0000

    Add package doorstop/0.3/Doorstop-0.3-py33-none-any.whl

---
+++ packages/doorstop/Doorstop-0.3-py33-none-any.whl/doorstop/core/__init__.py
@@ -0,0 +1,5 @@
+"""Core package for Doorstop."""
+
+from doorstop.core.item import Item
+from doorstop.core.document import Document
+from doorstop.core.tree import Tree, build, find_document, find_item
