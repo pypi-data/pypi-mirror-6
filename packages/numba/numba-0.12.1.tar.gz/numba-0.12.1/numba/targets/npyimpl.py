edffead9306d1fc5c8c04c731ddefdc947f3d344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:27 +0000

    Add package numba/0.12.1/numba-0.12.1.tar.gz

---
+++ packages/numba/numba-0.12.1.tar.gz/numba-0.12.1/numba/targets/npyimpl.py
@@ -0,0 +1,298 @@
+from __future__ import print_function, division, absolute_import
+import numpy
+import math
+from llvm.core import Constant, Type
+from numba.utils import PYVERSION
+from numba import typing, types, cgutils
+from numba.targets.imputils import implement
+from numba import numpy_support
+
+functions = []
+
+
+def register(f):
+    functions.append(f)
+    return f
+
+
+def numpy_unary_ufunc(funckey, asfloat=False):
+    def impl(context, builder, sig, args):
+        [tyinp, tyout] = sig.args
+        [inp, out] = args
+        ndim = tyinp.ndim
+
+        iary = context.make_array(tyinp)(context, builder, inp)
+        oary = context.make_array(tyout)(context, builder, out)
+
+        if asfloat:
+            sig = typing.signature(types.float64, types.float64)
+        else:
+            sig = typing.signature(tyout.dtype, tyinp.dtype)
+
+        fnwork = context.get_function(funckey, sig)
+        intpty = context.get_value_type(types.intp)
+
+        # TODO handle differing shape by mimicking broadcasting
+        shape = cgutils.unpack_tuple(builder, iary.shape, ndim)
+        with cgutils.loop_nest(builder, shape, intp=intpty) as indices:
+            pi = cgutils.get_item_pointer(builder, tyinp, iary, indices)
+            po = cgutils.get_item_pointer(builder, tyout, oary, indices)
+
+            ival = builder.load(pi)
+            if asfloat:
+                dval = context.cast(builder, ival, tyinp.dtype, types.float64)
+                dres = fnwork(builder, [dval])
+                res = context.cast(builder, dres, types.float64, tyout.dtype)
+            elif tyinp.dtype != tyout.dtype:
+                tempres = fnwork(builder, [ival])
+                res = context.cast(builder, tempres, tyinp.dtype, tyout.dtype)
+            else:
+                res = fnwork(builder, [ival])
+            builder.store(res, po)
+
+        return out
+    return impl
+
+
+@register
+@implement(numpy.absolute, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_absolute(context, builder, sig, args):
+    imp = numpy_unary_ufunc(types.abs_type)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.absolute, types.float64)
+def numpy_absolute_scalar(context, builder, sig, args):
+    imp = context.get_function(math.fabs, sig)
+    return imp(builder, args)
+
+
+@register
+@implement(numpy.exp, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_exp(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.exp, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.exp, types.float64)
+def numpy_exp_scalar(context, builder, sig, args):
+    imp = context.get_function(math.exp, sig)
+    return imp(builder, args)
+
+
+@register
+@implement(numpy.sin, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_sin(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.sin, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.sin, types.float64)
+def numpy_sin_scalar(context, builder, sig, args):
+    imp = context.get_function(math.sin, sig)
+    return imp(builder, args)
+
+
+@register
+@implement(numpy.cos, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_cos(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.cos, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.cos, types.float64)
+def numpy_cos_scalar(context, builder, sig, args):
+    imp = context.get_function(math.cos, sig)
+    return imp(builder, args)
+
+
+@register
+@implement(numpy.tan, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_tan(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.tan, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.tan, types.float64)
+def numpy_tan_scalar(context, builder, sig, args):
+    imp = context.get_function(math.tan, sig)
+    return imp(builder, args)
+
+
+@register
+@implement(numpy.sqrt, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_sqrt(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.sqrt, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.negative, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_negative(context, builder, sig, args):
+    imp = numpy_unary_ufunc(types.neg_type)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.sqrt, types.float64)
+def numpy_sqrt_scalar(context, builder, sig, args):
+    imp = context.get_function(math.sqrt, sig)
+    return imp(builder, args)
+
+@register
+@implement(numpy.floor, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_floor(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.floor, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.ceil, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_ceil(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.ceil, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.trunc, types.Kind(types.Array), types.Kind(types.Array))
+def numpy_trunc(context, builder, sig, args):
+    imp = numpy_unary_ufunc(math.trunc, asfloat=True)
+    return imp(context, builder, sig, args)
+
+
+def numpy_binary_ufunc(core, divbyzero=False):
+    def impl(context, builder, sig, args):
+        [tyvx, tywy, tyout] = sig.args
+        [vx, wy, out] = args
+        assert tyvx.dtype == tywy.dtype
+        ndim = tyvx.ndim
+
+        xary = context.make_array(tyvx)(context, builder, vx)
+        yary = context.make_array(tywy)(context, builder, wy)
+        oary = context.make_array(tyout)(context, builder, out)
+
+        intpty = context.get_value_type(types.intp)
+
+        # TODO handle differing shape by mimicking broadcasting
+        loopshape = cgutils.unpack_tuple(builder, xary.shape, ndim)
+
+        xyo_shape = [cgutils.unpack_tuple(builder, ary.shape, ndim)
+                     for ary in (xary, yary, oary)]
+        xyo_strides = [cgutils.unpack_tuple(builder, ary.strides, ndim)
+                       for ary in (xary, yary, oary)]
+        xyo_data = [ary.data for ary in (xary, yary, oary)]
+        xyo_layout = [ty.layout for ty in (tyvx, tywy, tyout)]
+
+        with cgutils.loop_nest(builder, loopshape, intp=intpty) as indices:
+            [px, py, po] = [cgutils.get_item_pointer2(builder,
+                                                      data=data, shape=shape,
+                                                      strides=strides,
+                                                      layout=layout,
+                                                      inds=indices)
+                            for data, shape, strides, layout
+                            in zip(xyo_data, xyo_shape, xyo_strides,
+                                   xyo_layout)]
+
+            x = builder.load(px)
+            y = builder.load(py)
+            if divbyzero:
+                # Handle division
+                iszero = cgutils.is_scalar_zero(builder, y)
+                with cgutils.ifelse(builder, iszero, expect=False) as (then,
+                                                                       orelse):
+                    with then:
+                        # Divide by zero
+                        if tyout.dtype in types.real_domain:
+                            # If x is float and is 0 also, return Nan; else
+                            # return Inf
+                            outltype = context.get_data_type(tyout.dtype)
+                            shouldretnan = cgutils.is_scalar_zero(builder, x)
+                            nan = Constant.real(outltype, float("nan"))
+                            inf = Constant.real(outltype, float("inf"))
+                            res = builder.select(shouldretnan, nan, inf)
+                        elif (tyout.dtype in types.signed_domain and
+                                not numpy_support.int_divbyzero_returns_zero):
+                            res = Constant.int(y.type, 0x1 << (y.type.width-1))
+                        else:
+                            res = Constant.null(y.type)
+
+                        assert res.type == po.type.pointee, \
+                                        (str(res.type), str(po.type.pointee))
+                        builder.store(res, po)
+                    with orelse:
+                        # Normal
+                        res = core(builder, (x, y))
+                        assert res.type == po.type.pointee, \
+                                        (str(res.type), str(po.type.pointee))
+                        builder.store(res, po)
+            else:
+                # Handle other operations
+                res = core(builder, (x, y))
+                assert res.type == po.type.pointee, (res.type,
+                                                     po.type.pointee)
+                builder.store(res, po)
+
+        return out
+    return impl
+
+
+@register
+@implement(numpy.add, types.Kind(types.Array), types.Kind(types.Array),
+           types.Kind(types.Array))
+def numpy_add(context, builder, sig, args):
+    dtype = sig.args[0].dtype
+    coresig = typing.signature(types.Any, dtype, dtype)
+    core = context.get_function("+", coresig)
+    imp = numpy_binary_ufunc(core)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.subtract, types.Kind(types.Array), types.Kind(types.Array),
+           types.Kind(types.Array))
+def numpy_sub(context, builder, sig, args):
+    dtype = sig.args[0].dtype
+    coresig = typing.signature(types.Any, dtype, dtype)
+    core = context.get_function("-", coresig)
+    imp = numpy_binary_ufunc(core)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.multiply, types.Kind(types.Array), types.Kind(types.Array),
+           types.Kind(types.Array))
+def numpy_multiply(context, builder, sig, args):
+    dtype = sig.args[0].dtype
+    coresig = typing.signature(types.Any, dtype, dtype)
+    core = context.get_function("*", coresig)
+    imp = numpy_binary_ufunc(core)
+    return imp(context, builder, sig, args)
+
+
+@register
+@implement(numpy.divide, types.Kind(types.Array), types.Kind(types.Array),
+           types.Kind(types.Array))
+def numpy_divide(context, builder, sig, args):
+    dtype = sig.args[0].dtype
+    odtype = sig.return_type.dtype
+    isig = typing.signature(odtype, dtype, dtype)
+    if dtype in types.signed_domain:
+        if PYVERSION >= (3, 0):
+            real_div_impl = context.get_function("/", isig)
+            imp = numpy_binary_ufunc(real_div_impl, divbyzero=True)
+        else:
+            int_sdiv_impl = context.get_function("/?", isig)
+            imp = numpy_binary_ufunc(int_sdiv_impl, divbyzero=True)
+    elif dtype in types.unsigned_domain:
+        int_udiv_impl = context.get_function("/?", isig)
+        imp = numpy_binary_ufunc(int_udiv_impl, divbyzero=True)
+    else:
+        real_div_impl = context.get_function("/?", isig)
+        imp = numpy_binary_ufunc(real_div_impl, divbyzero=True)
+
+    return imp(context, builder, sig, args)
---
+++ packages/numba/numba-0.12.1.tar.gz/numba-0.12.1/numba/typing/npydecl.py
@@ -0,0 +1,151 @@
+import numpy
+from numba import types
+from numba.typing.templates import (AttributeTemplate, AbstractTemplate,
+                                    builtin_global, builtin, signature)
+
+
+@builtin
+class NumpyModuleAttribute(AttributeTemplate):
+    key = types.Module(numpy)
+
+    def resolve_absolute(self, mod):
+        return types.Function(Numpy_absolute)
+
+    def resolve_exp(self, mod):
+        return types.Function(Numpy_exp)
+
+    def resolve_sqrt(self, mod):
+        return types.Function(Numpy_sqrt)
+
+    def resolve_sin(self, mod):
+        return types.Function(Numpy_sin)
+
+    def resolve_cos(self, mod):
+        return types.Function(Numpy_cos)
+
+    def resolve_tan(self, mod):
+        return types.Function(Numpy_tan)
+
+    def resolve_add(self, mod):
+        return types.Function(Numpy_add)
+
+    def resolve_subtract(self, mod):
+        return types.Function(Numpy_subtract)
+
+    def resolve_multiply(self, mod):
+        return types.Function(Numpy_multiply)
+
+    def resolve_divide(self, mod):
+        return types.Function(Numpy_divide)
+
+    def resolve_negative(self, mod):
+        return types.Function(Numpy_negative)
+
+    def resolve_floor(self, mod):
+        return types.Function(Numpy_floor)
+
+    def resolve_ceil(self, mod):
+        return types.Function(Numpy_ceil)
+
+    def resolve_trunc(self, mod):
+        return types.Function(Numpy_trunc)
+
+class Numpy_unary_ufunc(AbstractTemplate):
+    def generic(self, args, kws):
+        assert not kws
+        nargs = len(args)
+        if nargs == 2:
+            [inp, out] = args
+            if isinstance(inp, types.Array) and isinstance(out, types.Array):
+                return signature(out, inp, out)
+        elif nargs == 1:
+            [inp] = args
+            if inp in types.number_domain:
+                return signature(types.float64, types.float64)
+
+
+class Numpy_sqrt(Numpy_unary_ufunc):
+    key = numpy.sqrt
+
+
+class Numpy_absolute(Numpy_unary_ufunc):
+    key = numpy.absolute
+
+
+class Numpy_sin(Numpy_unary_ufunc):
+    key = numpy.sin
+
+
+class Numpy_cos(Numpy_unary_ufunc):
+    key = numpy.cos
+
+
+class Numpy_tan(Numpy_unary_ufunc):
+    key = numpy.tan
+
+
+class Numpy_exp(Numpy_unary_ufunc):
+    key = numpy.exp
+
+
+class Numpy_negative(Numpy_unary_ufunc):
+    key = numpy.negative
+
+
+class Numpy_floor(Numpy_unary_ufunc):
+    key = numpy.floor
+
+
+class Numpy_ceil(Numpy_unary_ufunc):
+    key = numpy.ceil
+
+
+class Numpy_trunc(Numpy_unary_ufunc):
+    key = numpy.trunc
+
+
+class Numpy_binary_ufunc(AbstractTemplate):
+    def generic(self, args, kws):
+        assert not kws
+        [vx, wy, out] = args
+        if (isinstance(vx, types.Array) and isinstance(wy, types.Array) and
+                isinstance(out, types.Array)):
+            if vx.dtype != wy.dtype:
+                # TODO handle differing dtypes
+                return
+            return signature(out, vx, wy, out)
+
+
+class Numpy_add(Numpy_binary_ufunc):
+    key = numpy.add
+
+
+class Numpy_subtract(Numpy_binary_ufunc):
+    key = numpy.subtract
+
+
+class Numpy_multiply(Numpy_binary_ufunc):
+    key = numpy.multiply
+
+
+class Numpy_divide(Numpy_binary_ufunc):
+    key = numpy.divide
+
+
+builtin_global(numpy, types.Module(numpy))
+builtin_global(numpy.absolute, types.Function(Numpy_absolute))
+builtin_global(numpy.exp, types.Function(Numpy_exp))
+builtin_global(numpy.sin, types.Function(Numpy_sin))
+builtin_global(numpy.cos, types.Function(Numpy_cos))
+builtin_global(numpy.tan, types.Function(Numpy_tan))
+builtin_global(numpy.add, types.Function(Numpy_add))
+builtin_global(numpy.subtract, types.Function(Numpy_subtract))
+builtin_global(numpy.multiply, types.Function(Numpy_multiply))
+builtin_global(numpy.divide, types.Function(Numpy_divide))
+builtin_global(numpy.sqrt, types.Function(Numpy_sqrt))
+builtin_global(numpy.negative, types.Function(Numpy_negative))
+builtin_global(numpy.floor, types.Function(Numpy_floor))
+builtin_global(numpy.ceil, types.Function(Numpy_ceil))
+builtin_global(numpy.trunc, types.Function(Numpy_trunc))
+
+
