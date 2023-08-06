d97892fb89260914649527e7ba1e94f5c56041bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package soundcloud-syncer/0.2.2/soundcloud-syncer-0.2.2.tar.gz

---
+++ packages/soundcloud-syncer/soundcloud-syncer-0.2.2.tar.gz/soundcloud-syncer-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name="soundcloud-syncer",
+    version="0.2.2",
+    description="Synchronize user's favorites tracks from soundcloud'",
+    author="Sliim",
+    author_email="sliim@mailoo.org",
+    url="https://github.com/sliim/soundcloud-syncer",
+    license="GPLv3",
+    scripts=["scripts/sc-syncer", "scripts/sc-tagger"],
+    packages=["ssyncer"],
+    install_requires=[
+        "stagger",
+        "python-dateutil"
+    ],
+    tests_require=[
+        "mock",
+        "nose",
+        "pep8",
+        "coverage"
+    ]
+)
