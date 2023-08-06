39fc910a304777958ade7fe03732e57489714c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:32 +0000

    Add package infi.mountoolinux/0.1.11/infi.mountoolinux-0.1.11.tar.gz

---
+++ packages/infi.mountoolinux/infi.mountoolinux-0.1.11.tar.gz/infi.mountoolinux-0.1.11/src/infi/mountoolinux/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.11"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(detached from v0.1.11)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '66dfd31942666ed74b474163b2c06d75146ae71e'
+__git_head_subject__ = 'change bunch->munch and add dependency'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
