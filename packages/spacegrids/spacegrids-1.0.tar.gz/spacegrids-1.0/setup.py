718e3eee06b58a79d8e3c2b18f2e444494ec7aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:19 +0000

    Add package spacegrids/1.0/spacegrids-1.0.tar.gz

---
+++ packages/spacegrids/spacegrids-1.0.tar.gz/spacegrids-1.0/setup.py
@@ -0,0 +1,27 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+        long_description = file.read()
+
+setup(name='spacegrids',
+      version='1.0',
+      author='Willem Sijp',
+      author_email='w.sijp@unsw.edu.au',
+      description='numpy array with grids and associated operations',
+      keywords=('climate data','grid data','data on grids'),
+      packages = find_packages(exclude="tests"),
+      package_data = {
+	  "dimarray": ['README.rst']
+	  },
+      long_description=long_description,
+      url='https://github.com/willo12/spacegrids',
+      license = "BSD"
+#      install_requires = ["numpy>=1.7"]
+#      extras_require = {
+#	  "ncio": ["netCDF4>=1.0.6"],
+#	  "pandas": ["pandas>=0.8.0"],
+#	  "plotting": ["pandas>=0.8.0","matplotlib>=1.2.1"],
+#	  "interp2d": ["basemap>=1.06"],
+#	  }
+      )
