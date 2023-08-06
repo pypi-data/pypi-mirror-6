79e110c03830c7c4fa8d7fe25ca80afde8627872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:59 +0000

    Add package print_movies/1.0.0/print_movies-1.0.0.zip

---
+++ packages/print_movies/print_movies-1.0.0.zip/print_movies-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name		= 'print_movies',
+	version		= '1.0.0',
+	py_modules	= ['print_movies'],
+	author		= 'hfpython',
+	author_email	= 'subhasis.tcs08@gmail.com',
+	url 		= 'http://wordpress.subhamukh2010/blogs',
+	description	= 'This is a module to test the recusrion in python'
+        )
+	
