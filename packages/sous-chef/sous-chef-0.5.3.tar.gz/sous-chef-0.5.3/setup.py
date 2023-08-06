4d9d9179b04f7311a904bd2401db4c3cabd589d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:53 +0000

    Add package sous-chef/0.5.3/sous-chef-0.5.3.tar.gz

---
+++ packages/sous-chef/sous-chef-0.5.3.tar.gz/sous-chef-0.5.3/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+setuptools.setup(
+    name="sous-chef",
+    version="0.5.3",
+    url="https://github.com/datasift/sous-chef",
+
+    author="Sam Clements",
+    author_email="sam.clements@datasift.com",
+
+    description="A small webapp for viewing and searching Chef nodes",
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=[
+        'flask==0.10.1',
+        'pychef==0.2.3'
+    ],
+
+    extras_require={
+        'debug': [
+            'flask-debugtoolbar>=0.9.0'
+        ],
+        'deploy': [
+            'gunicorn>=18.0'
+        ]
+    },
+
+    entry_points={
+        'console_scripts': [
+            'sous-chef = sous_chef:main',
+            'sous-chef-debug = sous_chef:debug'
+        ]
+    },
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
