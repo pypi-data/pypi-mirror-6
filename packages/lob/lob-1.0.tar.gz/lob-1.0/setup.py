6c06e58f10853201c62a91e1a4df2489477adeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:12 +0000

    Add package lob/1.0/lob-1.0.tar.gz

---
+++ packages/lob/lob-1.0.tar.gz/lob-1.0/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+        name = 'lob',
+        version = '1.0',
+        author = 'Lob',
+        author_email = 'support@lob.com',
+        packages = ['lob'],
+        url = 'https://pypi.python.org/pypi/lob',
+        license = 'MIT',
+        install_requires = [
+            "requests"
+            ],
+        summary = 'A wrapper for the lob.com API.',
+        description = 'Lob python bindings'
+    )
