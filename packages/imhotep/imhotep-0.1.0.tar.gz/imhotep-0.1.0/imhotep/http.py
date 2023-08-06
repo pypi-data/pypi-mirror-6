c61cc2b40d507def91f3a3ecc3addd72244d3712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:48 +0000

    Add package imhotep/0.1.0/imhotep-0.1.0.tar.gz

---
+++ packages/imhotep/imhotep-0.1.0.tar.gz/imhotep-0.1.0/imhotep/http.py
@@ -0,0 +1,39 @@
+import json
+import logging
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+log = logging.getLogger(__name__)
+
+
+class NoGithubCredentials(Exception):
+    pass
+
+
+class GithubRequester(object):
+    """
+    Object used for issuing authenticated API calls to GitHub.
+    """
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def get_auth(self):
+        return HTTPBasicAuth(self.username, self.password)
+
+    def get(self, url):
+        log.debug("Fetching %s", url)
+        return requests.get(url, auth=self.get_auth())
+
+    def delete(self, url):
+        log.debug("Deleting %s", url)
+        return requests.delete(url, auth=self.get_auth())
+
+    def post(self, url, payload):
+        log.debug("Posting %s to %s", payload, url)
+        return requests.post(
+            url, data=json.dumps(payload),
+            auth=self.get_auth())
