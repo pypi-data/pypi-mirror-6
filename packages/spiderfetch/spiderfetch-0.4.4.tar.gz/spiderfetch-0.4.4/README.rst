06e8158766dd35fd50c5fcc585bbabd085a537c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:40 +0000

    Add package spiderfetch/0.4.4/spiderfetch-0.4.4.tar.gz

---
+++ packages/spiderfetch/spiderfetch-0.4.4.tar.gz/spiderfetch-0.4.4/README.rst
@@ -0,0 +1,21 @@
+spiderfetch
+===========
+
+
+Install
+-------
+
+.. code:: bash
+
+    pip install spiderfetch
+    spiderfetch
+
+
+Run from checkout
+-----------------
+
+.. code:: bash
+
+    python -m spiderfetch.fetch
+    python -m spiderfetch.spider
+    python -m spiderfetch.spiderfetch
---
+++ packages/spiderfetch/spiderfetch-0.4.4.tar.gz/spiderfetch-0.4.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import spiderfetch
+
+
+setup(
+    name='spiderfetch',
+    version=spiderfetch.__version__,
+    description='Web spider and fetcher',
+    author='Martin Matusiak',
+    author_email='numerodix@gmail.com',
+    url='https://github.com/numerodix/spiderfetch',
+
+    packages=find_packages('.'),
+    package_dir = {'': '.'},
+
+    # don't install as zipped egg
+    zip_safe=False,
+
+    entry_points={
+        "console_scripts": [
+            "spiderfetch = spiderfetch.spiderfetch:run_script",
+        ]
+    },
+)
