9c321d539282354093461afda0121a5acf0759d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:19 +0000

    Add package y_w/0.1.1/y_w-0.1.1.zip

---
+++ packages/y_w/y_w-0.1.1.zip/y_w-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='y_w',
+      version='0.1.1',
+      description='YW stats package',
+      url='http://github.com/y_w',
+      author='Yue Weng',
+      author_email='wengyue31@hotmail.com',
+      license='SJSU',
+      packages=['y_w'],
+      zip_safe=False)
---
+++ packages/y_w/y_w-0.1.1.zip/y_w-0.1.1/y_w/__init__.py
@@ -0,0 +1,6 @@
+from numpy import array as Array
+from pandas import Series, DataFrame, Panel
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
