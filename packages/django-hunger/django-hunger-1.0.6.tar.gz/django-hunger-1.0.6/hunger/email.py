ac717e785662bd96e17029e97d6f92d37dc5af8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:45 +0000

    Add package django-hunger/1.0.6/django-hunger-1.0.6.tar.gz

---
+++ packages/django-hunger/django-hunger-1.0.6.tar.gz/django-hunger-1.0.6/hunger/email.py
@@ -0,0 +1,84 @@
+import os.path
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+from hunger.utils import setting
+
+try:
+    from templated_email import send_templated_mail
+    templated_email_available = True
+except ImportError:
+    templated_email_available = False
+
+def beta_confirm(email, **kwargs):
+    """
+    Send out email confirming that they requested an invite.
+    """
+
+    templates_folder = setting('BETA_EMAIL_TEMPLATES_DIR', 'hunger')
+    templates_folder = os.path.join(templates_folder, '')
+    from_email = setting('EMAIL_HOST_USER')
+    if templates_folder == 'hunger':
+        file_extension = 'email'
+    else:
+        file_extension = None
+
+    context_dict = kwargs.copy()
+    if templated_email_available:
+        send_templated_mail(
+            template_dir=templates_folder,
+            template_name='beta_confirm',
+            file_extension=file_extension,
+            from_email=from_email,
+            recipient_list=[email],
+            context=context_dict,
+        )
+    else:
+        plaintext = get_template(os.path.join(templates_folder, 'beta_confirm.txt'))
+        html = get_template(os.path.join(templates_folder, 'beta_confirm.html'))
+        subject, to = 'You requested an invite!', email
+        text_content = plaintext.render(Context())
+        html_content = html.render(Context())
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to],
+                                     headers={'From': 'Mailer <%s>' % from_email})
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+def beta_invite(email, code, **kwargs):
+    """
+    Email for sending out the invitation code to the user.
+    Invitation code is added to the context, so it can be rendered with standard
+    django template engine.
+    """
+    context_dict = kwargs.copy()
+    context_dict.setdefault('code', code)
+    context = Context(context_dict)
+
+    templates_folder = setting('BETA_EMAIL_TEMPLATES_DIR', 'hunger')
+    templates_folder = os.path.join(templates_folder, '')
+    from_email = setting('EMAIL_HOST_USER')
+    if templates_folder == 'hunger':
+        file_extension = 'email'
+    else:
+        file_extension = None
+
+    if templated_email_available:
+        send_templated_mail(
+            template_dir=templates_folder,
+            template_name='beta_invite',
+            file_extension=file_extension,
+            from_email=from_email,
+            recipient_list=[email],
+            context=context_dict,
+        )
+    else:
+        plaintext = get_template(os.path.join(templates_folder, 'beta_invite.txt'))
+        html = get_template(os.path.join(templates_folder, 'beta_invite.html'))
+
+        subject, to = "Here is your invite", email
+        text_content = plaintext.render(context)
+        html_content = html.render(context)
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to],
+                                     headers={'From': 'Mailer <%s>' % from_email})
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
---
+++ packages/django-hunger/django-hunger-1.0.6.tar.gz/django-hunger-1.0.6/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+import hunger
+
+DESCRIPTION = "A Django app to manage a private beta phase."
+
+try:
+    LONG_DESCRIPTION = open('README.rst').read()
+except:
+    pass
+
+version_str = '%d.%d.%d' % (hunger.VERSION[0], hunger.VERSION[1], hunger.VERSION[2])
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+tests_require = [
+    'Django>=1.3',
+]
+
+setup(
+    name='django-hunger',
+    version=version_str,
+    packages=[
+        'hunger',
+        'hunger.contrib',
+    ],
+    author='Joshua Karjala-Svenden',
+    author_email='joshua@fluxuries.com',
+    url='https://github.com/joshuakarjala/django-hunger/',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+    install_requires=[''],
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='runtests.runtests',
+    include_package_data=True,
+    package_data={'hunger': ['templates/hunger/*',
+                             'templates/templated_email/hunger/*',]},
+    )
