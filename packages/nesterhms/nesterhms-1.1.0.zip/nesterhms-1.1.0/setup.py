f60da9113a274708322e7a291199224dff280f9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:45 +0000

    Add package nesterhms/1.1.0/nesterhms-1.1.0.zip

---
+++ packages/nesterhms/nesterhms-1.1.0.zip/nesterhms-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'nesterhms',
+        version = '1.1.0',
+        py_mouules = 'nesterhms',
+        author = 'hms',
+        author_email = 'hms@vvv.com',
+        url = 'www.dasdas.com',
+        description = 'Test',
+        
+    )
