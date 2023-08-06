e6c4f478667a2a0200ad7927b8b05e294a999248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:21 +0000

    Add package funniest123/0.1/funniest123-0.1.tar.gz

---
+++ packages/funniest123/funniest123-0.1.tar.gz/funniest123-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest123',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest123'],
+      zip_safe=False)
