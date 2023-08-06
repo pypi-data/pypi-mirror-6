61f78df0fd803f62937ce6bff745aeb02c423505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:19 +0000

    Add package exofrills/0.1.11/exofrills-0.1.11.tar.gz

---
+++ packages/exofrills/exofrills-0.1.11.tar.gz/exofrills-0.1.11/setup.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+import sys
+try:
+    from setuptools import setup
+    have_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    have_setuptools = False
+
+if sys.version_info[0] < 3:
+    sys.exit("ERROR: xo requires Python 3.")
+
+VERSION = "0.1"
+
+setup_kwargs = {
+    "version": VERSION + '.11',
+    "description": 'exofrills text editor',
+    "author": 'Anthony Scopatz',
+    "author_email": 'scopatz@gmail.com',
+    "url": 'http://exofrills.org/',
+    "download_url": "https://github.com/scopatz/xo/zipball/" + VERSION,
+    "classifiers": [
+        "License :: OSI Approved",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+        "Topic :: Text Editors",
+        ],
+    "zip_safe": False,
+    "data_files": [("", ['license', 'readme.rst']),],
+    }
+
+if have_setuptools:
+    setup_kwargs['install_requires'] = [
+        'Pygments >= 1.6',
+        'urwid >= 1.1.1',
+        ]
+
+if __name__ == '__main__':
+    setup(
+        name='exofrills',
+        py_modules=['xo'],
+        entry_points={'console_scripts': ['xo = xo:main',],},
+        long_description=open('readme.rst').read(),
+        **setup_kwargs
+        )
