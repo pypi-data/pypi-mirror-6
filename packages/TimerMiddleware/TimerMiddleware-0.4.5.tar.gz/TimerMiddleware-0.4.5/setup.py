7507bdf5103be387df4f18af2240315ed93b23c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:04 +0000

    Add package TimerMiddleware/0.4.5/TimerMiddleware-0.4.5.tar.gz

---
+++ packages/TimerMiddleware/TimerMiddleware-0.4.5.tar.gz/TimerMiddleware-0.4.5/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+from distutils.core import setup
+
+setup(name='TimerMiddleware',
+      version='0.4.5',
+      url='http://sourceforge.net/p/timermiddleware',
+      packages=['timermiddleware'],
+      requires=['webob'],
+      )
