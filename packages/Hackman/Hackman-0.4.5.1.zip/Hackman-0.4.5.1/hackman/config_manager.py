44ba3a923cc6b88e3ab7803792b6fb52dd2c7281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:05 +0000

    Add package Hackman/0.4.5.1/Hackman-0.4.5.1.zip

---
+++ packages/Hackman/Hackman-0.4.5.1.zip/Hackman-0.4.5.1/hackman/config_manager.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+import ConfigParser
+import  os , sys
+
+class ConfigManager(object):
+
+    instance = None
+
+    def __init__(self):
+        self.config = ConfigParser.SafeConfigParser()		
+        parent_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+        print parent_path+'/properties.cfg'
+        self.config.read(parent_path+'/properties.cfg')
+
+    @staticmethod
+    def get_instance():
+        if ConfigManager.instance is None:
+            ConfigManager.instance = ConfigManager()
+        return ConfigManager.instance.config
