817a17234a393c9b38a53365d63b3c57c1800d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package GoldSaxGetHighLow/0.03/GoldSaxGetHighLow-0.03.zip

---
+++ packages/GoldSaxGetHighLow/GoldSaxGetHighLow-0.03.zip/GoldSaxGetHighLow-0.03/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+        name = 'GoldSaxGetHighLow',
+        packages = ['GoldSaxGetHighLow'], # this must be the same as the name above
+        version = '0.03',
+        description = 'Auto retrieve historical data for analytics to the GoldSaxEngine-****Markets. This can be used by any Trader, Market Maker, a Retail Investor, Instituitional Investor, Hedge Fund Managers, and Asset Managers.',
+        author = 'Vance King Saxbe. A',
+        author_email = 'vsaxbe@yahoo.com',
+        url = 'https://github.com/VanceKingSaxbeA/GoldSaxGetHighLow.git',   # use the URL to the github repo
+        download_url = 'https://github.com/VanceKingSaxbeA/GoldSaxGetHighLow/archive/master.zip', # I'll explain this in a second
+        keywords = ['Autocreate Tables', 'Automated Trading', 'Algorithmic Trading', 'Financial Markets', 'Hedge Funds', 'Asset Managers', 'investors'], # arbitrary keywords
+        classifiers = ['Development Status :: 5 - Production/Stable','Intended Audience :: Financial and Insurance Industry','Intended Audience :: Information Technology','Intended Audience :: Science/Research','License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)','Programming Language :: Python :: 3.3'],
+    )
