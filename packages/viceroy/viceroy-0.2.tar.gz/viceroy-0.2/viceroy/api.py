8aa74e3f3649a081a21b6d85381e627b62204526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:54 +0000

    Add package viceroy/0.2/viceroy-0.2.tar.gz

---
+++ packages/viceroy/viceroy-0.2.tar.gz/viceroy-0.2/viceroy/api.py
@@ -0,0 +1,45 @@
+import contextlib
+import warnings
+import unittest
+
+from .server import Server
+from .server import QUnitServer
+from .server import JasmineServer
+
+
+class ViceroyTestCase(unittest.TestCase):
+    server_class = Server
+
+    def get_driver(self):
+        from selenium.webdriver.firefox import webdriver
+        return webdriver.WebDriver()
+
+    @contextlib.contextmanager
+    def run_server(self, javascript):
+        httpd = self.server_class(javascript)
+        try:
+            httpd.run_async()
+            yield httpd
+        finally:
+            httpd.stop()
+
+    def assertInBrowser(self, javascript):
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', ResourceWarning)
+            driver = self.get_driver()
+            try:
+                with self.run_server(javascript) as httpd:
+                    driver.get('http://localhost:{}/'.format(httpd.port))
+                    result = httpd.wait()
+            finally:
+                driver.quit()
+            if result.failed:
+                self.fail(result.message)
+
+
+class QUnitTestCase(ViceroyTestCase):
+    server_class = QUnitServer
+
+
+class JasmineTestCase(ViceroyTestCase):
+    server_class = JasmineServer
---
+++ packages/viceroy/viceroy-0.2.tar.gz/viceroy-0.2/viceroy/tests.py
@@ -0,0 +1,54 @@
+import unittest
+
+from viceroy.api import JasmineTestCase
+from viceroy.api import QUnitTestCase
+from viceroy.api import ViceroyTestCase
+
+
+class DummyTests(ViceroyTestCase):
+    @unittest.expectedFailure
+    def test_fail(self):
+        self.assertInBrowser("""
+        viceroy_notify(true, 'test message');
+        """)
+
+    def test_success(self):
+        self.assertInBrowser("""
+        viceroy_notify(false, 'test message');
+        """)
+
+
+class QunitTests(QUnitTestCase):
+    @unittest.expectedFailure
+    def test_fail(self):
+        self.assertInBrowser("""
+        test( "hello fail", function() {
+          deepEqual( 1 , "1", "Failed!" );
+        });
+        """)
+
+    def test_success(self):
+        self.assertInBrowser("""
+        test( "hello success", function() {
+          equal( 1 , "1", "Success!" );
+        });
+        """)
+
+
+class JasmineTests(JasmineTestCase):
+    suite_name = "A suite"
+
+    def test_spec_success(self):
+        self.assertInBrowser("""
+        it("contains spec with an expectation", function() {
+            expect(true).toBe(true);
+        });
+        """)
+
+    @unittest.expectedFailure
+    def test_spec_fail(self):
+        self.assertInBrowser("""
+        it("contains spec with an failing expectation", function() {
+            expect(true).toBe(false);
+        });
+        """)
