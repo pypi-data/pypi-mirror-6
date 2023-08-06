2f855b091526b5a462d9febfd20ccb94a4da7701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:25 +0000

    Add package collective.csv2dict/1.1/collective.csv2dict-1.1.zip

---
+++ packages/collective.csv2dict/collective.csv2dict-1.1.zip/collective.csv2dict-1.1/CHANGES.rst
@@ -0,0 +1,23 @@
+Changelog
+=========
+
+
+1.1 (2014-04-11)
+----------------
+
+- Optionally allow ignoring extra columns.  To use this: initialize
+  the reader with ``ignore_extra_columns=True``.
+  [maurits]
+
+- Add ``formatting`` method to readers.  It currently returns the
+  delimiter, the dialect instance, the encoding and the expected
+  number of columns.  You can use this to give a hint in an upload
+  form.
+  [maurits]
+
+
+1.0 (2012-06-21)
+----------------
+
+- Initial release
+  [maurits]
---
+++ packages/collective.csv2dict/collective.csv2dict-1.1.zip/collective.csv2dict-1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+version = '1.1'
+
+setup(name='collective.csv2dict',
+      version=version,
+      description="Turn a csv into a dictionary with a predefined schema.",
+      long_description=open("README.rst").read() + "\n" +
+      open("CHANGES.rst").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      keywords='',
+      author='Maurits van Rees',
+      author_email='m.van.rees@zestsoftware.nl',
+      url='https://github.com/collective/collective.csv2dict',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      )
