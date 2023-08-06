80f44edf3ad185a6f29da90839670e17010aa0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:42 +0000

    Add package django-nested-formset/0.1.1/django-nested-formset-0.1.1.tar.gz

---
+++ packages/django-nested-formset/django-nested-formset-0.1.1.tar.gz/django-nested-formset-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+
+version = '0.1.1'
+
+
+setup(name='django-nested-formset',
+      description='Nest Django formsets for multi-level hierarchical editing',
+      author='Nathan Yergler',
+      author_email='nathan@yergler.net',
+      version=version,
+      long_description=README,
+      classifiers=[
+          'License :: OSI Approved :: BSD License',
+          'Development Status :: 4 - Beta',
+          'Framework :: Django',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      keywords='',
+      url='https://github.com/nyergler/nested-formset',
+      license='BSD',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'Django>=1.5',
+          'django-discover-runner',
+          'rebar',
+      ],
+)
---
+++ packages/django-nested-formset/django-nested-formset-0.1.1.tar.gz/django-nested-formset-0.1.1/src/nested_formset/tests/test_instantiation.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+
+from django.forms.models import BaseInlineFormSet, inlineformset_factory
+from nested_formset.tests import models as test_models
+
+from nested_formset import nestedformset_factory
+
+
+class InstantiationTests(TestCase):
+
+    def setUp(self):
+
+        child_formset = inlineformset_factory(test_models.Building, test_models.Tenant)
+        self.formset_class = nestedformset_factory(
+            test_models.Block,
+            test_models.Building,
+            nested_formset=child_formset,
+        )
+
+    def test_takes_parent_discovers_children(self):
+
+        block = test_models.Block.objects.create()
+
+        for i in range(10):
+            test_models.Building.objects.create(block=block)
+
+        formset = self.formset_class(instance=block)
+
+        self.assertEqual(
+            len(formset.forms), 10 + formset.extra
+        )
+
+    def test_child_form_contains_nested_formset(self):
+
+        block = test_models.Block.objects.create()
+        test_models.Building.objects.create(block=block)
+
+        formset = self.formset_class(instance=block)
+
+        self.assertTrue(
+            isinstance(formset.forms[0].nested, BaseInlineFormSet)
+        )
+
+    def test_nested_has_derived_prefix(self):
+
+        block = test_models.Block.objects.create()
+        test_models.Building.objects.create(block=block)
+
+        formset = self.formset_class(instance=block)
+
+        # first level forms have a "normal" formset prefix
+        self.assertEqual(formset.forms[0].prefix, 'building_set-0')
+
+        # second level inherit from their parent
+        self.assertEqual(
+            formset.forms[0].nested.forms[0].prefix,
+            'building_set-0-tenant_set-0',
+        )
+
+    def test_empty_form_is_not_bound(self):
+
+        block = test_models.Block.objects.create()
+        test_models.Building.objects.create(block=block)
+
+        formset = self.formset_class(instance=block)
+
+        self.assertFalse(formset.empty_form.is_bound)
+        self.assertFalse(formset.forms[0].nested.empty_form.is_bound)
