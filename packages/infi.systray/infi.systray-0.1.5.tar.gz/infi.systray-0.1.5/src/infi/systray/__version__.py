e1f8d2c1a59e2299c270fa28b8adc6c519c545f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:14 +0000

    Add package infi.systray/0.1.5/infi.systray-0.1.5.tar.gz

---
+++ packages/infi.systray/infi.systray-0.1.5.tar.gz/infi.systray-0.1.5/src/infi/systray/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.5"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '9e60ec54b52c910e4dec124ec82603acb5fe1416'
+__git_head_subject__ = 'Finished release v0.1.5.'
+__git_head_message__ = '* release/v0.1.5:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements\n  update dependencies'
+__git_dirty_diff__ = ''
