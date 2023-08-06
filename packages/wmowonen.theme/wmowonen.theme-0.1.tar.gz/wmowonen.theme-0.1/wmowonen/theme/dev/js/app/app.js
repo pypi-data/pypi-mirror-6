bb3b514d0122d05d3f54115325d06d1782f4674d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:58 +0000

    Add package wmowonen.theme/0.1/wmowonen.theme-0.1.tar.gz

---
+++ packages/wmowonen.theme/wmowonen.theme-0.1.tar.gz/wmowonen.theme-0.1/wmowonen/theme/dev/js/app/app.js
@@ -0,0 +1,14 @@
+$(function() {
+  
+  $('.navbar-form .form-control').addClass('hidden-lg hidden-md');
+  
+  $('.navbar-form button').click(function( event ) {
+    if( $('.navbar-form .form-control').is(':hidden') ) {
+      event.preventDefault();
+    }
+    
+    $('.navbar-form .form-control').removeClass('hidden-lg hidden-md');
+    
+  });
+
+});
