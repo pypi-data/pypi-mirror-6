dc0a5b87973d1d14e174aa886e474f5cf98cf62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:35 +0000

    Add package webassets-iife/0.0.1/webassets-iife-0.0.1.tar.gz

---
+++ packages/webassets-iife/webassets-iife-0.0.1.tar.gz/webassets-iife-0.0.1/webassets_iife/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+
+"""
+IIFE filter for webassets. It wraps a JS bundle in an IIFE, thus preventing
+global leaks.
+"""
+
+from webassets.filter import Filter, register_filter
+
+__version__ = '0.0.1'
+__all__ = ('IIFE',)
+
+
+class IIFE(Filter):
+    name = 'iife'
+    max_debug_level = None
+
+    def output(self, _in, out, **kw):
+        out.write(';!function(){')
+        out.write(_in.read())
+        out.write('}();')
+
+register_filter(IIFE)
