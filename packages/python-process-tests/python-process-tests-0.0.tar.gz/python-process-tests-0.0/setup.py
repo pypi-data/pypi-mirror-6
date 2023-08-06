97eb103b3d341d907a274deb39394f10ac20891d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:22 +0000

    Add package python-process-tests/0.0/python-process-tests-0.0.tar.gz

---
+++ packages/python-process-tests/python-process-tests-0.0.tar.gz/python-process-tests-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+    name='python-process-tests',
+    version="0.0",
+    url='https://pypi.python.org/pypi/process-tests/',
+    description='UNKNOWN',
+    long_description='''Use `process-tests <https://pypi.python.org/pypi/process-tests/>`_ instead.''',
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    platforms=['all'],
+    zip_safe=False,
+    author_email='contact@ionelmc.ro',
+    install_requires=['process-tests'],
+)
