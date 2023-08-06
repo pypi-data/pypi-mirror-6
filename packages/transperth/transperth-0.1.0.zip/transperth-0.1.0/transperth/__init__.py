766c747f88e4c355a09826f90f954858e5f75af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:40 +0000

    Add package transperth/0.1.0/transperth-0.1.0.zip

---
+++ packages/transperth/transperth-0.1.0.zip/transperth-0.1.0/transperth/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.0'
+
+_BASE = '://www.transperth.wa.gov.au/'
+
+BASE_HTTPS = 'https' + _BASE
+BASE_HTTP = 'http' + _BASE
---
+++ packages/transperth/transperth-0.1.0.zip/transperth-0.1.0/transperth/exceptions.py
@@ -0,0 +1,53 @@
+class TransperthException(Exception):
+    """
+    Base exception for all exceptions thrown by this library
+    """
+
+
+class InvalidStopNumber(TransperthException):
+    """
+    Thrown when the provided stop number is not a five digit number
+    """
+
+
+class InvalidDirection(TransperthException):
+    """
+    Thrown when the provided direction is neither to nor from
+    """
+
+
+class InvalidStep(TransperthException):
+    """
+    Thrown when a step is not one of 'bus', 'walk', or 'train'
+    """
+
+
+class NoFareData(TransperthException):
+    """
+    Thrown when transperth does not provide data from which we
+    can assertain the fare for a route
+    """
+
+
+class NotLoggedIn(TransperthException):
+    """
+    Raised when the library detects that the session has expired
+    """
+
+
+class LoginFailed(NotLoggedIn):
+    """
+    Thrown when a login attempt fails
+    """
+
+
+class BadStationError(TransperthException):
+    """
+    Thrown when the library encounters a bad station name
+    """
+
+
+class IncompleteTrip(TransperthException):
+    """
+    Thrown when a trip is missing its conclusion
+    """
