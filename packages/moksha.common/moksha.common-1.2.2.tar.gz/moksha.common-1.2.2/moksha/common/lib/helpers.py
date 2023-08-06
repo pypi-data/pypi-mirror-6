e2ebc2fcb145c3ebb3cccb539d00a2a33f9d68da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:22 +0000

    Add package moksha.common/1.2.2/moksha.common-1.2.2.tar.gz

---
+++ packages/moksha.common/moksha.common-1.2.2.tar.gz/moksha.common-1.2.2/moksha/common/lib/helpers.py
@@ -0,0 +1,88 @@
+from __future__ import print_function
+# This file is part of Moksha.
+# Copyright (C) 2008-2010  Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import moksha.common.utils
+import moksha.common.config
+import re
+import os
+import logging
+import warnings
+
+import six.moves.configparser as configparser
+
+log = logging.getLogger(__name__)
+scrub_filter = re.compile('[^_a-zA-Z0-9-]')
+
+
+def get_moksha_config_path():
+    """
+    :returns: The path to Moksha's configuration file.
+    """
+    for config_path in ('.', '/etc/moksha/', __file__ + '/../../../'):
+        for config_file in ('production.ini', 'development.ini'):
+            cfg = os.path.join(os.path.abspath(config_path), config_file)
+            if os.path.isfile(cfg):
+                return cfg
+
+    log.warn('No moksha configuration file found, make sure the '
+             'controlling app is fully configured')
+
+    return None
+
+
+def get_moksha_dev_config():
+    fname = 'development.ini'
+    cfgs = [
+        os.path.join(os.path.abspath(__file__ + '/../../../'), fname),
+        os.path.join(os.path.abspath(__file__ + '/../../../../'), fname),
+        os.path.join(os.getcwd(), fname),
+        '/etc/moksha/%s' % fname,
+    ]
+    for cfg in cfgs:
+        if os.path.isfile(cfg):
+            return cfg
+    log.warn("Cannot find configuration in %r" % cfgs)
+
+
+def get_moksha_appconfig():
+    """ Return the appconfig of Moksha """
+    return appconfig('config:' + get_moksha_config_path())
+
+
+def appconfig(config_path):
+    """ Our own reimplementation of paste.deploy.appconfig """
+
+    if config_path.startswith('config:'):
+        config_path = config_path[7:]
+
+    here = os.path.abspath(os.path.dirname(config_path))
+    parser = moksha.common.config.EnvironmentConfigParser({"here": here})
+    parser.read(filenames=[config_path])
+    try:
+        return dict(parser.items('app:main'))
+    except configparser.NoSectionError:
+        for section in parser.sections():
+            if section.startswith('app:'):
+                print("Using %r" % section)
+                return dict(parser.items(section))
+
+        raise configparser.NoSectionError("Couldn't find app: section.")
+
+
+def create_app_engine(app, config):
+    """ Create a new SQLAlchemy engine for a given app """
+    from sqlalchemy import create_engine
+    return create_engine(config.get('app_db', 'sqlite:///%s.db' % app))
---
+++ packages/moksha.common/moksha.common-1.2.2.tar.gz/moksha.common-1.2.2/setup.py
@@ -0,0 +1,70 @@
+# This file is part of Moksha.
+# Copyright (C) 2008-2010  Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup, find_packages
+import sys
+
+# This is required (oddly) to fix a python 2.7 bug with nose tests.
+try:
+    import multiprocessing
+    import logging
+except Exception:
+    pass
+
+tests_require = [
+    'nose',
+    'mock',
+]
+
+if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
+    tests_require.extend([
+        'unittest2',
+    ])
+
+
+setup(
+    name='moksha.common',
+    version='1.2.2',
+    description='Common components for Moksha',
+    author='Luke Macken, John (J5) Palmieri, Mairin Duffy, and Ralph Bean',
+    author_email='',
+    url='http://moksha.fedorahosted.org',
+    install_requires=[
+        "six",
+        "decorator",
+        "pytz",
+        "kitchen",
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+    ],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    test_suite='nose.collector',
+    namespace_packages=['moksha'],
+    tests_require=tests_require,
+    entry_points="""
+    [console_scripts]
+    moksha = moksha.common.commands.cli:main
+    """,
+)
