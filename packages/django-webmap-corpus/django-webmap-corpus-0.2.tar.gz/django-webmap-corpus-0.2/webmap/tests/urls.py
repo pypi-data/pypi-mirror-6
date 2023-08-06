8918e08def07c9e181edf608cea20de134d307bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:32 +0000

    Add package django-webmap-corpus/0.2/django-webmap-corpus-0.2.tar.gz

---
+++ packages/django-webmap-corpus/django-webmap-corpus-0.2.tar.gz/django-webmap-corpus-0.2/webmap/tests/urls.py
@@ -0,0 +1,18 @@
+"""URLs to run the tests."""
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^webmap/', include('webmap.urls')),
+)
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
