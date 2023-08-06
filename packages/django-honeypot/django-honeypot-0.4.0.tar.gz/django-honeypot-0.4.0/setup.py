94a17123c0d67a3719f9ef48202623f4a56d862c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:21 +0000

    Add package django-honeypot/0.4.0/django-honeypot-0.4.0.tar.gz

---
+++ packages/django-honeypot/django-honeypot-0.4.0.tar.gz/django-honeypot-0.4.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-honeypot',
+    version="0.4.0",
+    package_dir={'honeypot': 'honeypot'},
+    packages=['honeypot', 'honeypot.templatetags'],
+    description='Django honeypot field utilities',
+    author='James Turk',
+    author_email='jturk@sunlightfoundation.com',
+    license='BSD License',
+    url='http://github.com/sunlightlabs/django-honeypot/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools', 'six'],
+)
