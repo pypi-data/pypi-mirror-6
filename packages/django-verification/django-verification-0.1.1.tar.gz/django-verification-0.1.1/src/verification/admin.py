0d3c5ae0a326d8142289daf545e39051d3ffc43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package django-verification/0.1.1/django-verification-0.1.1.tar.gz

---
+++ packages/django-verification/django-verification-0.1.1.tar.gz/django-verification-0.1.1/src/verification/admin.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from .models import Key, KeyGroup
+
+class KeyAdmin(admin.ModelAdmin):
+    model = Key
+    list_display = ('key', 'group', 'pub_date', 'claimed_by', 'expires')
+    list_filter = ('group', 'claimed_by')
+    date_hierarchy = 'pub_date'
+
+class KeyGroupAdmin(admin.ModelAdmin):
+    model = KeyGroup
+    list_display = ('name', 'ttl', 'has_fact')
+
+admin.site.register(Key, KeyAdmin)
+admin.site.register(KeyGroup, KeyGroupAdmin)
