c75ac361c00b0113f297abb31f49370f6b5a9e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:14 +0000

    Add package infi.sysv_service/0.1.8/infi.sysv_service-0.1.8-py2.7.egg

---
+++ packages/infi.sysv_service/infi.sysv_service-0.1.8-py2.7.egg/infi/sysv_service/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.8"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'e9a31ac8d1f813696472285ca738d50b5249b2a3'
+__git_head_subject__ = 'Finished release v0.1.8.'
+__git_head_message__ = '* release/v0.1.8:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements'
+__git_dirty_diff__ = ''
