3fba970a136cdb2598e3340c6711c7e02ba4d27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:28 +0000

    Add package twentytab-tcked/0.1/twentytab-tcked-0.1.tar.gz

---
+++ packages/twentytab-tcked/twentytab-tcked-0.1.tar.gz/twentytab-tcked-0.1/tcked/fields.py
@@ -0,0 +1,41 @@
+"""
+It contains RichTextField. It's a TextField with CKEditor widget in his form
+"""
+from django.db import models
+from django import forms
+from . import conf
+
+from tcked.widgets import CKEditorWidget
+
+
+class RichTextField(models.TextField):
+    """
+    Field that construct the textarea field with CKEditor widget.
+    """
+    def __init__(self, *args, **kwargs):
+        self.config = kwargs.pop("config", None)
+        super(RichTextField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {
+            'form_class': RichTextFormField,
+            'config': self.config
+        }
+        defaults.update(kwargs)
+        return super(RichTextField, self).formfield(**defaults)
+
+
+class RichTextFormField(forms.fields.Field):
+    """
+    FormField for RichTextField
+    """
+    def __init__(self, config=None, *args, **kwargs):
+        kwargs.update({'widget': CKEditorWidget(config=config)})
+        super(RichTextFormField, self).__init__(*args, **kwargs)
+
+# Fix field for South
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^tcked\.fields\.RichTextField"])
+except ImportError:
+    pass
