12a628a223a4ff5cd542bddddc29ab4921205dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:21 +0000

    Add package formbar/0.6.5/formbar-0.6.5.tar.gz

---
+++ packages/formbar/formbar-0.6.5.tar.gz/formbar-0.6.5/formbar/helpers.py
@@ -0,0 +1,39 @@
+import os
+from mako.lookup import TemplateLookup
+from formbar import template_dir
+
+template_lookup = TemplateLookup(directories=[template_dir])
+
+
+def get_css():
+    """Returns the content of the formbar CSS file
+    :returns: String with css
+    """
+    files = ['datepicker3.css', 'formbar.css']
+    css = []
+    values = {}
+    for filename in files:
+        template = template_lookup.get_template(filename)
+        css.append(template.render(**values))
+    return "".join(css)
+
+
+def get_js(eval_url="undefined"):
+    """Returns the content of the formbar JS file
+
+    :eval_url: External URL for rule evaluation. If defined this URL is
+    called to evaluate client side rules with a AJAX request. The rule
+    to evaluate is provided in a GET request in the "rule" paramenter.
+    The return Value is a JSON response with success attribute set
+    depending on the result of the evaluation and the error message in
+    the data attribute in case the evaluation fails.
+    :returns: String with js
+
+    """
+    files = ['bootstrap-datepicker.js', 'formbar.js']
+    js = []
+    values = {'eval_url': eval_url}
+    for filename in files:
+        template = template_lookup.get_template(filename)
+        js.append(template.render(**values))
+    return "".join(js)
---
+++ packages/formbar/formbar-0.6.5.tar.gz/formbar-0.6.5/formbar/templates/label.mako
@@ -0,0 +1,13 @@
+<label class="control-label" for="${field.name}">
+  % if field.number:
+    <sup>(${field.number})</sup>
+  % endif
+  % if field.label:
+    ${_(field.label)}
+  % endif
+  % if field.is_required():
+    <span data-toggle="tooltip" data-original-title="Required field" class="formbar-tooltip glyphicon glyphicon-asterisk"></span>
+  % elif field.is_desired():
+    <span data-toggle="tooltip" data-original-title="Desired field" class="formbar-tooltip glyphicon glyphicon-star"></span>
+  % endif
+</label>
