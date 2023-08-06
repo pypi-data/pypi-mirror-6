d0adda195d240d2d10d45e38749b400cec7d59b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:21 +0000

    Add package vipr-data/0.5/vipr-data-0.5.tar.gz

---
+++ packages/vipr-data/vipr-data-0.5.tar.gz/vipr-data-0.5/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import os
+
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='vipr-data',
+    version='0.5',
+    include_package_data=True,
+    description='EMC ViPR Data Services SDK for Python',
+    author='EMC',
+    author_email='ViPR.Data.Services.SDK@emc.com',
+    license='BSD',
+    keywords='emc vipr dataservices sdk',
+    url='https://community.emc.com/community/edn/vipr-data-services',
+    packages=find_packages(),
+    long_description=read('README'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: BSD License'
+    ],
+    install_requires=[
+        'requests'
+    ]
+)
