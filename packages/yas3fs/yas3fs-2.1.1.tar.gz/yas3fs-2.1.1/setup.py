fe6705010676b8aaf168f05fec26d60f5998a7be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package yas3fs/2.1.1/yas3fs-2.1.1.tar.gz

---
+++ packages/yas3fs/yas3fs-2.1.1.tar.gz/yas3fs-2.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import sys
+
+# Versions of Python pre-2.7 require argparse separately. 2.7+ and 3+ all
+# include this as the replacement for optparse.
+if sys.version_info[:2] < (2, 7):
+    install_requires.append("argparse")
+
+setup(
+    name='yas3fs',
+    version='2.1.1',
+    description='YAS3FS (Yet Another S3-backed File System) is a Filesystem in Userspace (FUSE) interface to Amazon S3.',
+    packages=['yas3fs'],
+    author='Danilo Poccia',
+    author_email='dpoccia@gmail.com',
+    install_requires=['boto>=2.25.0', 'fusepy>=2.0.2'],
+    entry_points = { 'console_scripts': ['yas3fs = yas3fs:main'] },
+    )
