c5380f6614d694be8660cf52c97e70126b8b0b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:33 +0000

    Add package python-redmine/0.6.2/python-redmine-0.6.2.tar.gz

---
+++ packages/python-redmine/python-redmine-0.6.2.tar.gz/python-redmine-0.6.2/redmine/utilities.py
@@ -0,0 +1,22 @@
+import sys
+from string import Formatter
+
+
+def to_string(string):
+    """Converts unicode to utf-8 if on Python 2, leaves as it is if on Python 3"""
+    return string.encode('utf-8') if sys.version_info[0] < 3 else string
+
+
+class MemorizeFormatter(Formatter):
+    """Memorizes all arguments, used during string formatting"""
+    def __init__(self):
+        self.used_kwargs = {}
+        self.unused_kwargs = {}
+
+    def check_unused_args(self, used_args, args, kwargs):
+        for item in used_args:
+            if item in kwargs:
+                self.used_kwargs[item] = kwargs[item]
+                del kwargs[item]
+
+        self.unused_kwargs = kwargs
