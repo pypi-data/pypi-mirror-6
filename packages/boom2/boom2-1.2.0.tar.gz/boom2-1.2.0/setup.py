f7c44848eaf6afc749aa84152e512212f82b6b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:05 +0000

    Add package boom2/1.2.0/boom2-1.2.0.tar.gz

---
+++ packages/boom2/boom2-1.2.0.tar.gz/boom2-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils. core import setup
+
+setup(
+    name = 'boom2' ,
+    version = '1.2.0' ,
+    py_modules = ['hello'] ,
+    author = 'tester' ,
+    author_email = '' ,
+    url = '',
+    description = '',
+    )
