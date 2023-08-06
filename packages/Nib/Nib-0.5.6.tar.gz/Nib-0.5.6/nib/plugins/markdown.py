69ef00d2eadb9f7cd52205f8f2ce157ddfb7677d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:47 +0000

    Add package Nib/0.5.6/Nib-0.5.6.tar.gz

---
+++ packages/Nib/Nib-0.5.6.tar.gz/Nib-0.5.6/nib/plugins/markdown.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import markdown
+from nib import Processor, markup
+
+@markup(['.md', '.mkdn'])
+class MarkdownProcessor(Processor):
+    def __init__(self, options):
+        self.options = options
+        self.markdown = markdown.Markdown(output_format='html5')
+
+    def document(self, document):
+        document.short = self.markdown.convert(document.short)
+        document.content = self.markdown.convert(document.content)
+        document.extension = '.html'
+        return document
