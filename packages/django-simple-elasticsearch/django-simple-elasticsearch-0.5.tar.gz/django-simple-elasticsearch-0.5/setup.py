ea59a70c4a1c671120b15a3e5c534c9ed6623846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:29 +0000

    Add package django-simple-elasticsearch/0.5/django-simple-elasticsearch-0.5.tar.gz

---
+++ packages/django-simple-elasticsearch/django-simple-elasticsearch-0.5.tar.gz/django-simple-elasticsearch-0.5/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    description_files = [
+        'README.md',
+        'HISTORY.md',
+        'AUTHORS.md',
+        'CONTRIBUTING.md'
+    ]
+
+    long_description = u''
+    for f in description_files:
+        try:
+            long_description += pypandoc.convert(f, 'rst') + '\n\n'
+        except IOError:
+            pass
+except ImportError:
+    long_description = u'View `django-simple-elasticsearch documentation on Github  <https://github.com/jaddison/django-simple-elasticsearch>`_.'
+
+
+setup(
+    name = 'django-simple-elasticsearch',
+    version='0.5',
+    description = 'Simple Django Elasticsearch indexing integration.',
+    long_description=long_description,
+    url='http://github.com/jaddison/django-simple-elasticsearch',
+    license='BSD',
+    author='James Addison',
+    author_email='code@scottisheyes.com',
+    packages=find_packages(exclude=['tests*']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP :: WSGI'
+    ]
+)
