afbd5bf72e11748c32439cb3233d8b5b1d999817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:56 +0000

    Add package pychemy/0.1/pychemy-0.1.tar.gz

---
+++ packages/pychemy/pychemy-0.1.tar.gz/pychemy-0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pychemy',
+      version='0.1',
+      description='Helpers for handling chemical formulas in Python',
+      author='Mostly adopted from work of Christoph Gohlke, by Benjie Chen',
+      author_email='benjie@alum.mit.edu',
+      packages=["pychemy"],
+      package_dir={"": "."},
+     )
