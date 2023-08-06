5be424ab5caad91dbe1fa87f84eaec105af8e3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:40 +0000

    Add package Flask-Bootstrap3/3.1.1.0/Flask-Bootstrap3-3.1.1.0.tar.gz

---
+++ packages/Flask-Bootstrap3/Flask-Bootstrap3-3.1.1.0.tar.gz/Flask-Bootstrap3-3.1.1.0/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-Bootstrap3',
+    version='3.1.1.0',
+    author="Ryan Olson",
+    author_email="rmolson@gmail.com",
+    url="https://github.com/ryanolson/flask-bootstrap3",
+    license='BSD',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.8'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
