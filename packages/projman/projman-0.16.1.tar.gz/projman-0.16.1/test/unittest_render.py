267220ffeb4c688ab013eed657f7bf0f0df6c2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:23 +0000

    Add package projman/0.16.1/projman-0.16.1.tar.gz

---
+++ packages/projman/projman-0.16.1.tar.gz/projman-0.16.1/test/unittest_render.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2000-2013 LOGILAB S.A. (Paris, FRANCE).
+# http://www.logilab.fr/ -- mailto:contact@logilab.fr
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+"""
+"""
+
+import os.path as osp
+import tempfile, shutil
+
+from logilab.common import testlib
+from projman.renderers import PILHandler, GanttRenderer, ResourcesRenderer
+
+from projman.test import DATADIR
+from projman.readers import ProjectXMLReader
+
+CONFIG = testlib.AttrObject(del_ended=False, del_empty=False,
+                            timestep=1, depth=0, selected_resource=None,
+                            view_begin=None, view_end=None,
+                            project_file=osp.join(DATADIR,"full_scheduled_projman.xml"),
+                            task_root=None,
+                            )
+
+if __name__ == '__main__':
+    testlib.unittest_main()
