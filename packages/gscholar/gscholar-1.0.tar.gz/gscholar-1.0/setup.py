9de4a7e80fe7cf87a71e04a133fe3c3773c42a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:04 +0000

    Add package gscholar/1.0/gscholar-1.0.tar.gz

---
+++ packages/gscholar/gscholar-1.0.tar.gz/gscholar-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+
+setup(name='gscholar',
+      version='1.0',
+      description='Python library to query Google Scholar.',
+      author='Bastian Venthur',
+      author_email='mail@venthur.de',
+      url='http://github.com/venthur/gscholar',
+      packages=['gscholar'],
+)
