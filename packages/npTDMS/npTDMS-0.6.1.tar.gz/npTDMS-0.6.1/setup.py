99927df19bd0e76ba0802582493418b9e179c7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:46 +0000

    Add package npTDMS/0.6.1/npTDMS-0.6.1.tar.gz

---
+++ packages/npTDMS/npTDMS-0.6.1.tar.gz/npTDMS-0.6.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+  name = 'npTDMS',
+  version = '0.6.1',
+  description = ("Cross-platform, NumPy based module for reading "
+    "TDMS files produced by LabView."),
+  author = 'Adam Reeve',
+  author_email = 'adreeve@gmail.com',
+  url = 'https://github.com/adamreeve/npTDMS',
+  packages = ['nptdms', 'nptdms.test'],
+  long_description=open('README.rst').read(),
+  license = 'LGPL',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Scientific/Engineering',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+  ],
+  install_requires = ['numpy'],
+  entry_points = """
+  [console_scripts]
+  tdmsinfo=nptdms.tdmsinfo:main
+  """
+)
