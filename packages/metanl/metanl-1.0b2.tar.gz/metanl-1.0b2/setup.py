b8e0130b5ef37f60c22d534da41bfb674425f8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:27 +0000

    Add package metanl/1.0b2/metanl-1.0b2.tar.gz

---
+++ packages/metanl/metanl-1.0b2.tar.gz/metanl-1.0b2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+version_str = '1.0b2'
+from setuptools import setup
+import sys
+
+if sys.version_info.major == 2:
+    nltk_version = 'nltk'
+else:
+    nltk_version = 'nltk >= 3.0a'
+
+classifiers=[
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: MacOS',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Programming Language :: C',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development',
+    'Topic :: Text Processing :: Linguistic',]
+
+import os
+README_contents = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+doclines = README_contents.split("\n")
+
+setup(
+    name="metanl",
+    version=version_str,
+    maintainer='Luminoso Technologies, Inc.',
+    maintainer_email='dev@luminoso.com',
+    url='http://github.com/commonsense/metanl/',
+    license = "MIT",
+    platforms = ["any"],
+    description = doclines[0],
+    classifiers = classifiers,
+    long_description = "\n".join(doclines[2:]),
+    packages=['metanl'],
+    package_data = {'metanl': ['data/freeling/*.cfg', 'data/freeling/*.dat']},
+    install_requires=[nltk_version, 'ftfy >= 3'],
+)
