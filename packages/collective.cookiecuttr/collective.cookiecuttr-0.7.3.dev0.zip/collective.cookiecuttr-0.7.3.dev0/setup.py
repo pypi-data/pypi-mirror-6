5f7c0575d8de7b99474ef77076c9c640826d16d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:01 +0000

    Add package collective.cookiecuttr/0.7.3.dev0/collective.cookiecuttr-0.7.3.dev0.zip

---
+++ packages/collective.cookiecuttr/collective.cookiecuttr-0.7.3.dev0.zip/collective.cookiecuttr-0.7.3.dev0/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.7.3.dev0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='collective.cookiecuttr',
+      version=version,
+      description="Integration package for cookiecuttr, european guidelines",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Franklin Kingma',
+      author_email='franklin@fourdigits.nl',
+      url='http://github.com/fourdigits/collective.cookiecuttr',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.registry',
+          'plone.app.layout',
+          'plone.browserlayer',
+          'collective.z3cform.datagridfield'
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+              'mocker',
+              'plone.mocktestcase',
+          ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
