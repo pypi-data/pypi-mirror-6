b2dd310c683c8840866f67e5ffd9fc1dcb45121e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:43 +0000

    Add package value_objects/0.1.0/value_objects-0.1.0.tar.gz

---
+++ packages/value_objects/value_objects-0.1.0.tar.gz/value_objects-0.1.0/value_objects/ValueObject.py
@@ -0,0 +1,119 @@
+
+from inspect import getargspec
+from itertools import izip
+
+from once import once
+
+# ==============================================================================
+# ValueObject
+# ==============================================================================
+
+class ValueObject( object ):
+
+  def __new__( klass, *args, **kwargs ):
+
+    initClass( klass )
+
+    instance = object.__new__( klass, *args, **kwargs )
+
+    instance.__dict__.update( dict( zip( klass.valueObjectFieldNames, args ) + kwargs.items() ) )
+
+    return instance
+
+  # ============================================================================
+  # fields
+  # ============================================================================
+
+  @once
+  def valueObjectFieldValues( self ):
+    return tuple( getattr( self, name ) for name in self.valueObjectFieldNames )
+
+  @property
+  def valueObjectFieldCount( self ):
+    return len( self.valueObjectFieldNames )
+
+  @property
+  def valueObjectFieldPairs( self ):
+    return izip( self.valueObjectFieldNames, self.valueObjectFieldValues )
+
+  # ============================================================================
+  # str and repr
+  # ============================================================================
+
+  @property
+  def valueObjectPairReprs( self ):
+    for name, value in self.valueObjectFieldPairs:
+      yield '%s=%s' % ( name, repr( value ) )
+
+  @property
+  def valueObjectPairStrs( self ):
+    for name, value in self.valueObjectFieldPairs:
+      yield '%s=%s' % ( name, str( value ) )
+
+  def __repr__( self ):
+    arcs = ', '.join( self.valueObjectPairReprs )
+    return '%s{%s}' % ( self.__class__.__name__, arcs )
+
+  def __str__( self ):
+    arcs = ', '.join( self.valueObjectPairStrs )
+    return '%s{%s}' % ( self.__class__.__name__, arcs )
+
+  # ============================================================================
+  # hash
+  # ============================================================================
+
+  def __hash__( self ):
+    return self.valueObjectHash
+
+  @once
+  def valueObjectHash( self ):
+    return hash( self.valueObjectFieldValues )
+
+  # ============================================================================
+  # equals
+  # ============================================================================
+
+  def __eq__( self, other ):
+    if( self.__class__ != other.__class__ ):
+      return False
+
+    return self.valueObjectFieldValues == other.valueObjectFieldValues
+
+  def __ne__( self, other ):
+    return not self.__eq__( other )
+
+# ==============================================================================
+# helpers
+# ==============================================================================
+
+def initClass( klass ):
+
+  fieldNames, defaultValues = parseFieldNamesAndDefaultValues( klass.__init__ )
+
+  setattr( klass, 'valueObjectFieldNames', fieldNames )
+
+  # defaultValues implemented as class attributes
+  for name, defaultValue in izip( reversed( fieldNames ), reversed( defaultValues ) ):
+    setattr( klass, name, defaultValue )
+
+def parseFieldNamesAndDefaultValues( init ):
+
+  fieldNames, args, kwargs, defaultValues = getargspec( init )
+
+  # remove self
+  fieldNames = tuple( fieldNames[1:] )
+
+  if args:
+      raise ValueError( 'ValueObject: `*args` are not allowed in __init__' )
+
+  if kwargs:
+      raise ValueError( 'ValueObject: `**kwargs` are not allowed in __init__' )
+
+  if not all( type( name ) is str for name in fieldNames ):
+      raise ValueError( 'ValueObject: parameter unpacking is not allowed in __init__' )
+
+  defaultValues = () if not defaultValues else defaultValues
+
+  return fieldNames, defaultValues
+
+
---
+++ packages/value_objects/value_objects-0.1.0.tar.gz/value_objects-0.1.0/value_objects/frozendict.py
@@ -0,0 +1,40 @@
+
+# http://stevewedig.com/2014/01/27/a-few-python-tricks/
+#
+# http://legacy.python.org/dev/peps/pep-0416/
+# http://code.activestate.com/recipes/414283-frozen-dictionaries/
+# from http://code.activestate.com/recipes/414283/
+# https://github.com/slezica/python-frozendict
+
+from once import once
+
+def blocked( *a, **kw ):
+  raise AttributeError( 'A frozendict cannot be modified.' )
+ 
+class frozendict( dict ):
+  '''
+  An immutable and hashable dictionary
+  '''
+
+  def __hash__( s ):
+    return hash( s.sorted_items )
+
+  @once
+  def sorted_items( s ):
+    return tuple(
+        sorted(
+          s.iteritems()
+        )
+      )
+  
+  __delitem__ = blocked
+  __setitem__ = blocked
+  clear = blocked
+  pop = blocked
+  popitem = blocked
+  setdefault = blocked 
+  update = blocked
+  
+  def __repr__( s ):
+    return "frozendict(%s)" % dict.__repr__( s )
+    
