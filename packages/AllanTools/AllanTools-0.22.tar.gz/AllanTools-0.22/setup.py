1d4d46481b2c146c74d11999e64c2dbb56ec762b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:55 +0000

    Add package AllanTools/0.22/AllanTools-0.22.tar.gz

---
+++ packages/AllanTools/AllanTools-0.22.tar.gz/AllanTools-0.22/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AllanTools',
+      version='0.22',
+      description='Allan deviation and relates time/frequency statistics',
+      author='Anders Wallin',
+      author_email='anders.e.e.wallin@gmail.com',
+      url='https://github.com/aewallin/allantools',
+      license='GPLv3+',
+      packages=['allantools',],
+      long_description ='Given phase or fractional frequency data this package calculates: ' \
+                        'Allan deviation, overlapping Allan deviation, modified Allan deviation, ' \
+                        'Hadamard deviation, overlapping Hadamard deviation, time deviation, ' \
+                        'total deviation, MTIE, TIE-rms'
+     )
