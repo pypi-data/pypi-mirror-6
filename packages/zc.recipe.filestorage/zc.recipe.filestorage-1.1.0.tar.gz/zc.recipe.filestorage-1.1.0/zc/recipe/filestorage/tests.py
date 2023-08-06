27868a04b52124c0f41f8f7a5b5052f5a6119a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:18 +0000

    Add package zc.recipe.filestorage/1.1.0/zc.recipe.filestorage-1.1.0.tar.gz

---
+++ packages/zc.recipe.filestorage/zc.recipe.filestorage-1.1.0.tar.gz/zc.recipe.filestorage-1.1.0/zc/recipe/filestorage/tests.py
@@ -0,0 +1,29 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import os, re, unittest
+import pkg_resources
+import doctest
+from zope.testing import renormalizing
+
+def test_suite():
+    return doctest.DocFileSuite(
+        "README.rst",
+        checker=renormalizing.RENormalizing([
+            (re.compile(r'\S+%(sep)s\w+%(sep)s\w+.fs'
+                        % dict(sep=re.escape(os.path.sep))),
+             r'/tmp/data/Data.fs'),
+            (re.compile(r'\S+sample-(\w+)'), r'/sample-\1'),
+            ]),
+        )
