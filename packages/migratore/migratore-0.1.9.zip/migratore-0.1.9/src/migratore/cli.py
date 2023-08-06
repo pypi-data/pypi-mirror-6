f09d1955ac2129d1344bec836b39cc29ce768356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:38 +0000

    Add package migratore/0.1.9/migratore-0.1.9.zip

---
+++ packages/migratore/migratore-0.1.9.zip/migratore-0.1.9/src/migratore/cli.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import info
+import migration
+
+def run_help():
+    print "%s %s (%s)" % (info.NAME, info.VERSION, info.AUTHOR)
+    print ""
+    print "The most commonly used migratore commands are:"
+    print "  version         Prints the current version of migratore"
+    print "  list            Lists the executed migrations on the current database"
+    print "  errors          Lists the various errors from migration of the database"
+    print "  trace [id]      Prints the traceback for the error execution with the provided id"
+    print "  upgrade [path]  Executes the pending migrations using the defined directory or current"
+    print "  generate [path] Generates a new migration file into the target path"
+
+def run_version():
+    print "%s %s" % (info.NAME, info.VERSION)
+
+def run_list():
+    migration.Migration.list()
+
+def run_errors():
+    migration.Migration.errors()
+
+def run_trace(id):
+    migration.Migration.trace(id)
+
+def run_upgrade(path = None):
+    migration.Migration.upgrade(path)
+
+def run_generate(path = None):
+    migration.Migration.generate(path)
