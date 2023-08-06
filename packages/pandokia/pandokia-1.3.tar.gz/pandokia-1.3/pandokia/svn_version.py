84afaa18b06771ab34212cb8be4641036be2d3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:58 +0000

    Add package pandokia/1.3/pandokia-1.3.tar.gz

---
+++ packages/pandokia/pandokia-1.3.tar.gz/pandokia-1.3/pandokia/svn_version.py
@@ -0,0 +1,15 @@
+__svn_version__ = '1219'
+
+__full_svn_info__ = '''
+Path: .
+URL: https://svn.stsci.edu/svn/ssb/etal/pandokia/trunk
+Repository Root: https://svn.stsci.edu/svn/ssb/etal
+Repository UUID: d34015c8-bcbb-4646-8ac8-8ba5febf221d
+Revision: 1219
+Node Kind: directory
+Schedule: normal
+Last Changed Author: sienkiew
+Last Changed Rev: 1219
+Last Changed Date: 2014-04-03 16:02:19 -0400 (Thu, 03 Apr 2014)
+'''
+
