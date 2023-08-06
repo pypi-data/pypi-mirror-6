92afd21cc6dc80077eadfad56f20d946e6c062e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:23 +0000

    Add package geograpy/0.2.2/geograpy-0.2.2.tar.gz

---
+++ packages/geograpy/geograpy-0.2.2.tar.gz/geograpy-0.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='geograpy',
+      version='0.2.2',
+      description='Extract place names and meta data from text or URLs',
+      url='https://github.com/ushahidi/geograpy',
+      download_url ='https://github.com/ushahidi/geograpy/tarball/0.2.2',
+      author='Jonathon Morgan',
+      author_email='jonathon@ushahidi.com',
+      license='MIT',
+      packages=['geograpy'],
+      install_requires=[
+            'numpy',
+            'nltk',
+            'newspaper',
+            'jellyfish',
+            'pycountry'
+      ],
+      scripts=['geograpy/bin/geograpy-nltk'],
+      data_files=[('./geograpy/data', ['geograpy/data/GeoLite2-City-Locations.csv', 
+            'geograpy/data/ISO3166ErrorDictionary.csv'])],
+      zip_safe=False)
