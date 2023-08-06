99b15688949154c5ae493920927623b58669b565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:58 +0000

    Add package ndrive/0.0.4/ndrive-0.0.4.tar.gz

---
+++ packages/ndrive/ndrive-0.0.4.tar.gz/ndrive-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='ndrive',
+    packages=['ndrive'],
+    version='0.0.4',
+    description='python wrapper for Naver Ndrive',
+    long_description='',
+    license='MIT License',
+    author='Kim Tae Hoon',
+    author_email='carpedm20@gmail.com',
+    url='https://github.com/carpedm20/ndrive',
+    keywords=['Ndrive','ndrive'],
+    classifiers=[],
+    package_data={'ndrive': ['*.js']},
+    install_requires=[
+        'rsa',
+        'requests',
+        'python-magic',
+        'clint',
+        'simplejson',
+        'python-spidermonkey',
+    ]
+)
