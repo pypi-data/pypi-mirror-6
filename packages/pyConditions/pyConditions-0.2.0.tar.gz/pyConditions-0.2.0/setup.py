fabd07231765ef4806cfb3910681ffadcaf69f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:10 +0000

    Add package pyConditions/0.2.0/pyConditions-0.2.0.tar.gz

---
+++ packages/pyConditions/pyConditions-0.2.0.tar.gz/pyConditions-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pyConditions',
+    version='0.2.0',
+    author='Sean Reed',
+    author_email='streed@mail.roanoke.edu',
+    packages=['pyconditions', 'pyconditions.test'],
+    url='http://github.com/streed/pyConditions',
+    license='LICENSE.txt',
+    description='Commenting sucks so let your code do it for you with Guava like preconditions that also actually do something.',
+    long_description=open('README.rst').read(),
+)
