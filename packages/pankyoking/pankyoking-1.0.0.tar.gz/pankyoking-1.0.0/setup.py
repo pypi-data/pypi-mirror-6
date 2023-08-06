7c9003601e9d5155e90dfa54fcb2af349388d60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package pankyoking/1.0.0/pankyoking-1.0.0.tar.gz

---
+++ packages/pankyoking/pankyoking-1.0.0.tar.gz/pankyoking-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name	= 'pankyoking',
+	version = '1.0.0',
+	py_modules = ['pankyoking'],
+	author = 'hihihi0110',
+	author_email = 'hihihi0110@gmail.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of instead lists',
+      )
