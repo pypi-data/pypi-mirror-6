db769cd48872f7a7e61e297f2c4b9365dff8ca55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:32 +0000

    Add package taskplot/0.1.1/taskplot-0.1.1.tar.gz

---
+++ packages/taskplot/taskplot-0.1.1.tar.gz/taskplot-0.1.1/CHANGES.rst
@@ -0,0 +1,11 @@
+ChangeLog
+=========
+
+0.1.1 (2014-02-25)
+------------------
+- NEW: Add examples to README.rst
+
+0.1 (2014-02-25)
+----------------
+- NEW: Print summary of daily effort as well as cumulative effort.
+- NEW: Plot graph using Matplotlib.
---
+++ packages/taskplot/taskplot-0.1.1.tar.gz/taskplot-0.1.1/setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2014 Susam Pal
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#   1. Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in
+#      the documentation and/or other materials provided with the
+#      distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+"""TaskPlot setup script."""
+
+
+from distutils.core import setup
+import os
+from src import taskplot
+
+_description = taskplot.__doc__.strip().split('\n', 1)[0]
+_long_description = '\n\n'.join(open(f).read() for f in
+                               ('README.rst', 'CHANGES.rst'))
+
+if os.name == 'nt':
+    scripts = ['scripts/taskplot.cmd']
+else:
+    scripts = ['scripts/taskplot']
+
+setup(name='taskplot',
+      version=taskplot.__version__,
+      description=_description,
+      long_description=_long_description,
+      author='Susam Pal',
+      author_email='susam@susam.in',
+      url='https://github.com/susam/taskplot',
+      download_url='https://pypi.python.org/pypi/taskplot',
+      py_modules=['taskplot'],
+      scripts=scripts,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+      ],
+      license='Simplified BSD License',
+      keywords=['task', 'effort', 'time', 'progress', 'graph', 'plot'],
+      platforms=['Any'],
+      package_dir={'': 'src'})
