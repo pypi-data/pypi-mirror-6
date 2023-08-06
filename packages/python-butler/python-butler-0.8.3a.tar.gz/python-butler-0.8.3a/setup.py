efb42bde43d2edb1649eb7170f1a2e48974e5397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:50 +0000

    Add package python-butler/0.8.3a/python-butler-0.8.3a.tar.gz

---
+++ packages/python-butler/python-butler-0.8.3a.tar.gz/python-butler-0.8.3a/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-butler',
+      version='0.8.3a',
+      description='Python Dictionaries and Lists on Steroids',
+      url='http://github.com/atmb4u/butler',
+      author='Anoop Thomas Mathew',
+      author_email='atmb4u@gmail.com',
+      license='BSD',
+      packages=['butler'],
+      zip_safe=False)
