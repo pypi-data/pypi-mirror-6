3f17cc87b09ec86a846b08c0aae54cd80b79dcad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:46 +0000

    Add package django-user-management/0.2.0/django-user-management-0.2.0.tar.gz

---
+++ packages/django-user-management/django-user-management-0.2.0.tar.gz/django-user-management-0.2.0/user_management/models/tests/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+from ..mixins import VerifyEmailMixin, AvatarMixin
+
+
+class User(AvatarMixin, VerifyEmailMixin, PermissionsMixin, AbstractBaseUser):
+    pass
