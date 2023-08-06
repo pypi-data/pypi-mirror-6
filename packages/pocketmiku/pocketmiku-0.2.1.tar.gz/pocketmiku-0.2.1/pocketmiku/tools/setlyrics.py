bcb08efffd9cf1379cc7c0a20f65989cb0931d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:47 +0000

    Add package pocketmiku/0.2.1/pocketmiku-0.2.1.tar.gz

---
+++ packages/pocketmiku/pocketmiku-0.2.1.tar.gz/pocketmiku-0.2.1/pocketmiku/tools/setlyrics.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+
+from pocketmiku.notes import stringToNotes
+from pocketmiku.control import notesToMidiString
+from pocketmiku.amidi import send
+
+if __name__ == "__main__":
+    print u"Setting lyrics to %s" % sys.argv[1].decode('utf-8')
+    values = stringToNotes(sys.argv[1].decode('utf-8'))
+    print notesToMidiString(values)
+    send(notesToMidiString(values))
+    print "%d note(s)" % len(values)
+
