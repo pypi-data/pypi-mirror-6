dfd051009a0079a82c638570303464360f54ac99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:21 +0000

    Add package redset/0.4.2/redset-0.4.2.tar.gz

---
+++ packages/redset/redset-0.4.2.tar.gz/redset-0.4.2/redset/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+redset
+---------
+
+Simple redis-backed, distributed sorted sets.
+
+"""
+
+__version__ = '0.4.2'
+
+from redset.sets import *
+import serializers
---
+++ packages/redset/redset-0.4.2.tar.gz/redset-0.4.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+import redset
+
+setup(
+    name='redset',
+    version=redset.__version__,
+    author='thekantian, jamesob',
+    author_email='zach@percolate.com, jamesob@percolate.com',
+    packages=['redset'],
+    url='https://github.com/percolate/redset',
+    license='see LICENSE',
+    description='Simple, distributed sorted sets with redis',
+    long_description=open('README.rst').read(),
+    tests_require=[
+        'redis',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
