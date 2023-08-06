83d72031cf8d324ee144a841caab698c3c2eb6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:31 +0000

    Add package ca-certs-locater/1.0/ca-certs-locater-1.0.tar.gz

---
+++ packages/ca-certs-locater/ca-certs-locater-1.0.tar.gz/ca-certs-locater-1.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = 'ca-certs-locater',
+    version = '1.0',
+    author = 'AR',
+    author_email = 'aleiphoenix@gmail.com',
+    packages = ['ca_certs_locater'],
+    description = 'a ca-certs file locater for httplib2',
+    license = 'MIT',
+    url = 'https://github.com/aleiphoenix/ca-certs-locater',
+)
