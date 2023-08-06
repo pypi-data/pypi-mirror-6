aedee4ef22acc2e156c7a8f9ce23aede2b83be9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:10 +0000

    Add package tengs-cli/0.0.6/tengs-cli-0.0.6.tar.gz

---
+++ packages/tengs-cli/tengs-cli-0.0.6.tar.gz/tengs-cli-0.0.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tengs-cli',
+    description="cli for tengs.ru",
+    version='0.0.6',
+    # packages=['towelstuff',],
+    license='MIT',
+    author="Kirill Mokevnin",
+    author_email="mokevnin@gmail.com",
+    url="http://tengs.ru",
+    long_description=open('README.txt').read(),
+    packages=find_packages(),
+    install_requires=[
+        'pyyaml',
+        'requests'
+    ],
+    entry_points = {
+        'console_scripts': ['tengs=tengs_cli.cli:main'],
+    }
+)
