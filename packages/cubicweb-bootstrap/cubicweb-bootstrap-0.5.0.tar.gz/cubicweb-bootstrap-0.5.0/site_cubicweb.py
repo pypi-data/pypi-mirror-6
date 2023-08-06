5806694a8b71b581e4ad7b838d7666e52777703a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:37 +0000

    Add package cubicweb-bootstrap/0.5.0/cubicweb-bootstrap-0.5.0.tar.gz

---
+++ packages/cubicweb-bootstrap/cubicweb-bootstrap-0.5.0.tar.gz/cubicweb-bootstrap-0.5.0/site_cubicweb.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""this is where you could register procedures for instance
+
+:organization: Logilab
+:copyright: 2013 LOGILAB S.A. (Paris, FRANCE), license is LGPL.
+:contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
+"""
+
+__docformat__ = "restructuredtext en"
+from logilab.common.configuration import REQUIRED
+
+options = (
+    ('cw_compatibility',
+      {'type' : 'yn',
+      'default': True,
+      'help': 'specifies if cw_combatibility css must be activated (needed in production)',
+      'group': 'bootrstrap', 'level': 0,
+      }),
+)
