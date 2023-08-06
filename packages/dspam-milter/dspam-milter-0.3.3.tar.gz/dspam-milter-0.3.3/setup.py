262d9165888c6b526212cef96f54e54955350ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:45 +0000

    Add package dspam-milter/0.3.3/dspam-milter-0.3.3.tar.gz

---
+++ packages/dspam-milter/dspam-milter-0.3.3.tar.gz/dspam-milter-0.3.3/setup.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2014, Tom Hendrikx
+# All rights reserved.
+#
+# See LICENSE for the license.
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import sys
+
+# http://pytest.org/dev/goodpractises.html#integration-with-setuptools-test-commands
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name = 'dspam-milter',
+    version = '0.3.3',
+    packages = ['dspam'],
+    include_package_data = True,
+    entry_points = {
+        'console_scripts': [
+            'dspam-milter = dspam.milter:main',
+        ]
+    },
+    install_requires = ['pymilter'],
+    zip_safe = True,
+    tests_require=['pytest', 'flexmock'],
+    cmdclass = {'test': PyTest},
+)
