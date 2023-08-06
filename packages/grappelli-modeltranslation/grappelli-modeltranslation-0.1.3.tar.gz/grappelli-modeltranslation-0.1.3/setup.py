ee4cbc41c874a174c11b6377a9934cc6f12a6bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:57 +0000

    Add package grappelli-modeltranslation/0.1.3/grappelli-modeltranslation-0.1.3.tar.gz

---
+++ packages/grappelli-modeltranslation/grappelli-modeltranslation-0.1.3.tar.gz/grappelli-modeltranslation-0.1.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from distutils.core import setup
+import os
+import sys
+
+
+appname = 'grappelli_modeltranslation'
+version = __import__(appname).__version__
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name='grappelli-modeltranslation',
+    version=version,
+    description="A small compatibility layer between grappelli and " \
+                "django-modeltranslation",
+    author='Jacob Magnusson',
+    author_email='m@jacobian.se',
+    url='https://github.com/jmagnusson/grappelli-modeltranslation',
+    license='New BSD License',
+    platforms=['any'],
+    packages=[appname],
+    package_data={appname: ['static/{0}/*/*'.format(appname)]},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
