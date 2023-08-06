880684417fd54a7e8dcedfa700d8701a51bffdf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:48 +0000

    Add package sous-chef/0.3.13/sous-chef-0.3.13.tar.gz

---
+++ packages/sous-chef/sous-chef-0.3.13.tar.gz/sous-chef-0.3.13/sous_chef/app.py
@@ -0,0 +1,46 @@
+"""The create_app() function"""
+
+import chef
+import flask
+
+import sous_chef.blueprints
+
+__all__ = ['create_app']
+
+
+def configure_chef():
+    flask.current_app.chef = chef.autoconfigure()
+    flask.current_app.chef.set_default()
+    flask.current_app.chef_environments = sorted(chef.Environment.list())
+
+
+def create_app():
+    app = flask.Flask('sous_chef')
+
+    # Load configuration from defaults and an optional config file
+    app.config.from_object('sous_chef.defaults')
+    app.config.from_envvar('SOUS_CHEF_SETTINGS', silent=True)
+
+    # Configure chef before the first request
+    app.before_first_request(configure_chef)
+
+    # Register blueprints - currently only the user interface
+    app.register_blueprint(sous_chef.blueprints.ui)
+
+    return app
+
+
+def create_debug_app():
+    app = create_app()
+    app.config['DEBUG'] = True
+
+    # Install the Flask debug toolbar extension, if the package is availible
+    try:
+        from flask_debugtoolbar import DebugToolbarExtension
+    except ImportError:
+        pass
+    else:
+        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+        DebugToolbarExtension(app)
+
+    return app
