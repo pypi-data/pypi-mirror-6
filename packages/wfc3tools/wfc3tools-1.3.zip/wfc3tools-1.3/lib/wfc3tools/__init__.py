50a1450344cfa746393421e87b056402b36ab1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:17 +0000

    Add package wfc3tools/1.3/wfc3tools-1.3.zip

---
+++ packages/wfc3tools/wfc3tools-1.3.zip/wfc3tools-1.3/lib/wfc3tools/__init__.py
@@ -0,0 +1,28 @@
+"""The wfc3tools package holds Python tasks useful for analyzing WFC3 data.
+
+These tasks include:
+
+Utility and library functions used by these tasks are also included in this
+module.
+
+
+"""
+from .version import *
+
+import calwf3
+import wf32d
+import wf3ccd
+import wf3ir
+import wf3rej
+import runastrodriz
+import sampinfo
+import pstack
+import pstat
+import sub2full
+import embedsub
+
+# These lines allow TEAL to print out the names of TEAL-enabled tasks
+# upon importing this package.
+import os
+from stsci.tools import teal
+teal.print_tasknames(__name__, os.path.dirname(__file__))
