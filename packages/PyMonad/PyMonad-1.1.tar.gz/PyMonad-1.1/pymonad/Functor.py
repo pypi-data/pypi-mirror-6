f9d64f78284a1fa3e466187a0a9a4132602fda4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:25 +0000

    Add package PyMonad/1.1/PyMonad-1.1.tar.gz

---
+++ packages/PyMonad/PyMonad-1.1.tar.gz/PyMonad-1.1/pymonad/Functor.py
@@ -0,0 +1,29 @@
+# --------------------------------------------------------
+# (c) Copyright 2014 by Jason DeLaat. 
+# Licensed under BSD 3-clause licence.
+# --------------------------------------------------------
+
+from pymonad.Container import *
+
+class Functor(Container):
+	""" Represents a type of values which can be "mapped over." """
+
+	def __init__(self, value):
+		""" Stores 'value' as the contents of the Functor. """
+		super(Functor, self).__init__(value)
+
+	def fmap(self, function): 
+		""" Applies 'function' to the contents of the functor and returns a new functor value. """
+		raise NotImplementedError("'fmap' not defined.")
+	
+	def __rmul__(self, aFunction):
+		""" 
+		The 'fmap' operator.
+		The following are equivalent:
+
+			aFunctor.fmap(aFunction)
+			aFunction * aFunctor
+
+		"""
+		
+		return self.fmap(aFunction)
