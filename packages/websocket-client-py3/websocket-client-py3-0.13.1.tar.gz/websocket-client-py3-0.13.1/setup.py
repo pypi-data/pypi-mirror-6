ad455a0094579417b421a9e84e775b01bd934c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:21 +0000

    Add package websocket-client-py3/0.13.1/websocket-client-py3-0.13.1.tar.gz

---
+++ packages/websocket-client-py3/websocket-client-py3-0.13.1.tar.gz/websocket-client-py3-0.13.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+VERSION = "0.13.1"
+
+
+setup(
+    name="websocket-client-py3",
+    version=VERSION,
+    description="WebSocket client for python3. hybi13 is supported.",
+    long_description=open("README.rst").read(),
+    author="liris",
+    author_email="liris.pp@gmail.com",
+    license="LGPL",
+    url="https://github.com/liris/websocket-client",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+    keywords='websockets',
+    scripts=["bin/wsdump.py"],
+    # install_requires=['backports.ssl_match_hostname'], # for python2
+    packages=["tests", "websocket"],
+    package_data={
+        'tests': ['data/*.txt'],
+        'websocket': ["cacert.pem"]
+    },
+)
