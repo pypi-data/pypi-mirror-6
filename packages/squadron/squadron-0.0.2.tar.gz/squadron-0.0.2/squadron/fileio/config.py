11a15b971365b3a6a93b3f8b633c6a7a5df4ee85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:53 +0000

    Add package squadron/0.0.2/squadron-0.0.2.tar.gz

---
+++ packages/squadron/squadron-0.0.2.tar.gz/squadron-0.0.2/squadron/fileio/config.py
@@ -0,0 +1,57 @@
+from ConfigParser import SafeConfigParser, NoSectionError
+import socket
+import os
+from ..log import log
+from ..exceptions import UserException
+
+def CONFIG_DEFAULTS():
+    return {
+        'polltime':'30',
+        'keydir':'/etc/squadron/keydir',
+        'nodename':socket.getfqdn(),
+        'statedir':'/var/squadron',
+        'send_status': 'false'
+    }
+
+#This is not a func so that we can change it in a test
+CONFIG_PATHS = ['/etc/squadron/config',
+        '/usr/local/etc/squadron/config',
+        os.path.expanduser('~/.squadron/config'),
+        ]
+
+def CONFIG_SECTIONS():
+    return ['squadron', 'status', 'daemon']
+
+def parse_config(config_file = None, defaults = CONFIG_DEFAULTS()):
+    """
+    Parses a given config file using SafeConfigParser. If the specified
+    config_file is None, it searches in the usual places. Returns a
+    dictionary of config keys to their values.
+
+    Keyword arguments:
+        defaults -- the default global values for the config
+        config_file -- the configuration file to read config from. If
+            None, searches for system-wide configuration and from
+            the local user's configuration.
+    """
+    parser = SafeConfigParser()
+    
+    if config_file is None:
+        # Try defaults
+        parsed_files = parser.read(CONFIG_PATHS)
+        log.debug("Using log files: " + str(parsed_files))
+    else:
+        with open(config_file) as cfile:
+            parser.readfp(cfile, config_file)
+
+    if parser.sections():
+        log.debug("Original section squadron: " + str(parser.items('squadron')))
+        result = defaults.copy()
+        for section in CONFIG_SECTIONS():
+            try:
+                result.update(parser.items(section))
+            except NoSectionError:
+                pass
+        return result
+    else:
+        raise UserException('No config file could be loaded. Make sure at least one of these exists and can be parsed: ' + str(CONFIG_PATHS))
---
+++ packages/squadron/squadron-0.0.2.tar.gz/squadron-0.0.2/squadron/libraries/apt/__init__.py
@@ -0,0 +1,28 @@
+import wrap_apt
+
+def schema():
+    """
+    This returns
+    """
+    return { 'title': 'apt schema',
+            'type': 'string'
+            }
+
+
+def verify(inputhashes):
+    """
+    """ 
+    failed = []
+    for package in inputhashes:
+        if(wrap_apt.check_package_is_installed(package) == False):
+            failed.append(package)
+    return failed
+
+def apply(inputhashes, log, **kwargs):
+    failed = []
+    for package in inputhashes:
+        if(wrap_apt.install_package(package) == False):
+            failed.append(package)
+    return failed
+
+
