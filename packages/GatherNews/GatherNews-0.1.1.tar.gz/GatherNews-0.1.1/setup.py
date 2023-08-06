e545b3c33b27da407d077d4b5f7d38d15eed39dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:50 +0000

    Add package GatherNews/0.1.1/GatherNews-0.1.1.tar.gz

---
+++ packages/GatherNews/GatherNews-0.1.1.tar.gz/GatherNews-0.1.1/setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+descr = "Gathers unstructured News data into a SQLite3 db"
+
+from distutils.core import setup
+
+setup(
+    name='GatherNews',
+    version='0.1.1',
+    maintainer='Tyler Brown',
+    maintainer_email='tylers.pile@gmail.com',
+    packages=['gathernews', 'gathernews.tests'],
+    scripts=['examples/ex_gRSS.py'],
+    url='http://pypi.python.org/pypi/GatherNews/',
+    license='doc/LICENSE.txt',
+    description= descr,
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "argparse>=1.2.1",
+        "feedparser>=5.1.3",
+        "nose>=1.3.0",
+        "simpleflake>=0.1.5",
+        "wsgiref>=0.1.2",
+    ],
+)
