6d6b38d4e7d695eca9fb33994db63fb131f2b812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:06 +0000

    Add package tailon/0.2.2/tailon-0.2.2.tar.gz

---
+++ packages/tailon/tailon-0.2.2.tar.gz/tailon-0.2.2/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from tailon import version
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+classifiers = (
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'License :: OSI Approved :: BSD License',
+    'Intended Audience :: Developers',
+    'Operating System :: POSIX :: Linux',
+)
+
+kw = {
+    'name'             : 'tailon',
+    'version'          : version,
+    'description'      : 'Webapp for looking at and searching through log files',
+    'long_description' : open(join(abspath(dirname(__file__)), 'README.rst')).read(),
+    'author'           : 'Georgi Valkov',
+    'author_email'     : 'georgi.t.valkov@gmail.com',
+    'license'          : 'Revised BSD License',
+    'url'              : 'https://github.com/gvalkov/tailon',
+    'keywords'         : 'log monitoring tail',
+    'classifiers'      : classifiers,
+    'packages'         : ['tailon'],
+    'install_requires' : [
+        'tornado>=3.2.0',
+        'sockjs-tornado==1.0.0',
+        'PyYAML>=3.10'],
+    'entry_points'     : {'console_scripts': ['tailon = tailon.main:main']},
+    'zip_safe'         : False,
+    'package_data'     : {
+        'tailon' : ['../assets/js/vendor/*',
+                    '../assets/js/main.js',
+                    '../assets/css/*',
+                    '../assets/favicon.ico',
+                    '../assets/fonts/*',
+                    '../templates/*',
+                    ]
+        },
+    'include_package_data': False,
+}
+
+if __name__ == '__main__':
+    setup(**kw)
