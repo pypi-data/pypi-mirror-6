13a9124d5b4f2a343ef80b03f45abfc85cb5d6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:13 +0000

    Add package site-auditor/1.0.3/site-auditor-1.0.3.zip

---
+++ packages/site-auditor/site-auditor-1.0.3.zip/site-auditor-1.0.3/site_auditor/gui.py
@@ -0,0 +1,46 @@
+from tkinter import *
+root = Tk()
+#root.iconbitmap(default='transparent.ico')
+root.title('Пример приложения')
+root.geometry('500x400+300+200') # ширина=500, высота=400, x=300, y=200
+root.resizable(True, False) # размер окна может быть изменен только по горизонтали
+
+def donothing():
+	filewin = Toplevel(root)
+	button = Button(filewin, text="Do nothing button")
+	button.pack()
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_command(label="Save as...", command=donothing)
+filemenu.add_command(label="Close", command=donothing)
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo", command=donothing)
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut", command=donothing)
+editmenu.add_command(label="Copy", command=donothing)
+editmenu.add_command(label="Paste", command=donothing)
+editmenu.add_command(label="Delete", command=donothing)
+editmenu.add_command(label="Select All", command=donothing)
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
+
+
+
+root.mainloop()
---
+++ packages/site-auditor/site-auditor-1.0.3.zip/site-auditor-1.0.3/site_auditor/oexceptions.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+class SiteException(Exception):
+	def __str__(self):
+		return 'Длина домена не должна превышать ' \
+				'255 символов или быть меньше 4!'
