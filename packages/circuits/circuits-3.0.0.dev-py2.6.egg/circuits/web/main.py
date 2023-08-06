2af28842731e5cc1fcb7f4422b33e5aee43af4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:47 +0000

    Add package circuits/3.0.0.dev/circuits-3.0.0.dev-py2.6.egg

---
+++ packages/circuits/circuits-3.0.0.dev-py2.6.egg/circuits/web/main.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Main
+
+circutis.web Web Server and Testing Tool.
+"""
+
+import os
+from optparse import OptionParser
+from wsgiref.validate import validator
+from wsgiref.simple_server import make_server
+
+try:
+    import hotshot
+    import hotshot.stats
+except ImportError:
+    hostshot = None
+
+try:
+    import psyco
+except:
+    psyco = None  # NOQA
+
+from circuits.core.pollers import Select
+from circuits.tools import inspect, graph
+from circuits import Component, Manager, Debugger
+from circuits import __version__ as systemVersion
+
+from circuits.web.wsgi import Application
+from circuits.web import BaseServer, Controller, Logger, Server, Static
+
+try:
+    from circuits.core.pollers import Poll
+except ImportError:
+    Poll = None  # NOQA
+
+try:
+    from circuits.core.pollers import EPoll
+except ImportError:
+    EPoll = None  # NOQA
+
+
+USAGE = "%prog [options] [docroot]"
+VERSION = "%prog v" + systemVersion
+
+
+def parse_options():
+    parser = OptionParser(usage=USAGE, version=VERSION)
+
+    parser.add_option(
+        "-b", "--bind",
+        action="store", type="string", default="0.0.0.0:8000", dest="bind",
+        help="Bind to address:[port]"
+    )
+
+    parser.add_option(
+        "-l", "--logging",
+        action="store_true", default=False, dest="logging",
+        help="Enable access logs"
+    )
+
+    if psyco is not None:
+        parser.add_option(
+            "-j", "--jit",
+            action="store_true", default=False, dest="jit",
+            help="Use python HIT (psyco)"
+        )
+
+    parser.add_option(
+        "-m", "--multiprocessing",
+        action="store", type="int", default=0, dest="mp",
+        help="Specify no. of processes to start (multiprocessing)"
+    )
+
+    parser.add_option(
+        "-t", "--type",
+        action="store", type="string", default="select", dest="type",
+        help="Specify type of poller to use"
+    )
+
+    parser.add_option(
+        "-s", "--server",
+        action="store", type="string", default="server", dest="server",
+        help="Specify server to use"
+    )
+
+    parser.add_option(
+        "-p", "--profile",
+        action="store_true", default=False, dest="profile",
+        help="Enable execution profiling support"
+    )
+
+    parser.add_option(
+        "-d", "--debug",
+        action="store_true", default=False, dest="debug",
+        help="Enable debug mode"
+    )
+
+    parser.add_option(
+        "-v", "--validate",
+        action="store_true", default=False, dest="validate",
+        help="Enable WSGI validation mode"
+    )
+
+    opts, args = parser.parse_args()
+
+    return opts, args
+
+
+class HelloWorld(Component):
+
+    channel = "web"
+
+    def request(self, request, response):
+        return "Hello World!"
+
+
+class Root(Controller):
+
+    def hello(self):
+        return "Hello World!"
+
+
+def main():
+    opts, args = parse_options()
+
+    if psyco and opts.jit:
+        psyco.full()
+
+    if ":" in opts.bind:
+        address, port = opts.bind.split(":")
+        port = int(port)
+    else:
+        address, port = opts.bind, 8000
+
+    bind = (address, port)
+
+    if opts.validate:
+        application = (Application() + Root())
+        app = validator(application)
+
+        httpd = make_server(address, port, app)
+        httpd.serve_forever()
+
+        raise SystemExit(0)
+
+    manager = Manager()
+
+    if opts.debug:
+        manager += Debugger()
+
+    poller = opts.type.lower()
+    if poller == "poll":
+        if Poll is None:
+            print("No poll support available - defaulting to Select...")
+            Poller = Select
+        else:
+            Poller = Poll
+    elif poller == "epoll":
+        if EPoll is None:
+            print("No epoll support available - defaulting to Select...")
+            Poller = Select
+        else:
+            Poller = EPoll
+    else:
+        Poller = Select
+
+    Poller().register(manager)
+
+    if opts.server.lower() == "base":
+        BaseServer(bind).register(manager)
+        HelloWorld().register(manager)
+    else:
+        Server(bind).register(manager)
+        Root().register(manager)
+
+    docroot = os.getcwd() if not args else args[0]
+
+    Static(docroot=docroot, dirlisting=True).register(manager)
+
+    if opts.logging:
+        Logger().register(manager)
+
+    if opts.profile:
+        if hotshot:
+            profiler = hotshot.Profile(".profile")
+            profiler.start()
+
+    if opts.debug:
+        print(graph(manager, name="circuits.web"))
+        print()
+        print(inspect(manager))
+
+    for i in range(opts.mp):
+        manager.start(process=True)
+
+    manager.run()
+
+    if opts.profile and hotshot:
+        profiler.stop()
+        profiler.close()
+
+        stats = hotshot.stats.load(".profile")
+        stats.strip_dirs()
+        stats.sort_stats("time", "calls")
+        stats.print_stats(20)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/circuits/circuits-3.0.0.dev-py2.6.egg/tests/web/test_websockets.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from circuits import Component
+from circuits.net.sockets import write
+from circuits.web.servers import Server
+from circuits.web.controllers import Controller
+from circuits.web.websockets import WebSocketClient, WebSocketsDispatcher
+
+
+class Echo(Component):
+
+    channel = "wsserver"
+
+    def read(self, sock, data):
+        self.fire(write(sock, "Received: " + data))
+
+
+class Root(Controller):
+
+    def index(self):
+        return "Hello World!"
+
+
+class Client(Component):
+    channel = "ws"
+
+    def init(self, *args, **kwargs):
+        self.response = None
+
+    def read(self, data):
+        self.response = data
+
+
+def test(manager, watcher):
+    server = Server(("localhost", 8123)).register(manager)
+    watcher.wait("ready")
+    from circuits import Debugger
+    Debugger().register(manager)
+    Echo().register(server)
+    Root().register(server)
+    watcher.wait("registered", channel="wsserver")
+    watcher.clear()
+    WebSocketsDispatcher("/websocket").register(server)
+    watcher.wait("registered", channel="web")
+    WebSocketClient("ws://localhost:8123/websocket").register(manager)
+    watcher.wait("connected", channel="wsclient")
+
+    client = Client().register(manager)
+    client.fire(write("Hello!"), "ws")
+    watcher.wait("read", channel="ws")
+    assert client.response == "Received: Hello!"
+
+    client.unregister()
+    watcher.wait("unregistered")
+    watcher.clear()
+
+    server.unregister()
+    watcher.wait("unregistered")
