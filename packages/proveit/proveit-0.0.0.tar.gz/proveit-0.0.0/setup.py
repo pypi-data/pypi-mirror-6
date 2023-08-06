c27b485dd92dc9cf2d4530e3519a04cafe53522b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:07 +0000

    Add package proveit/0.0.0/proveit-0.0.0.tar.gz

---
+++ packages/proveit/proveit-0.0.0.tar.gz/proveit-0.0.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    author='Nick Williamson',
+    author_email='nick@nickw.info',
+    url = 'https://github.com/ConceptPending/proveit',
+    description="Implementation of gmaxwell's suggestion of proving account balances",
+    name='proveit',
+    packages=['proveit'],
+    version='0.0.0'
+)
