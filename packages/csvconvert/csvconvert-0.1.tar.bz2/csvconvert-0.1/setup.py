101a6aa2bc6c8c51d6aa76134f08259674c86f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:08 +0000

    Add package csvconvert/0.1/csvconvert-0.1.tar.bz2

---
+++ packages/csvconvert/csvconvert-0.1.tar.bz2/csvconvert-0.1/setup.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='csvconvert',
+    version='0.1',
+    description='A tool to convert between various csv variants',
+    autor='Szymon Pyżalski',
+    author_email='szymon@pythonista.net',
+    py_modules=['csvconvert'],
+    entry_points="""[console_scripts]
+    csvconvert = csvconvert:convert"""
+)
