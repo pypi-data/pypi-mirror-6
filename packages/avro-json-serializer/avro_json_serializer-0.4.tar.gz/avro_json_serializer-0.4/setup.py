1bfedec40dd4b456cadd9e6e56dd9891455a9707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:26 +0000

    Add package avro-json-serializer/0.4/avro_json_serializer-0.4.tar.gz

---
+++ packages/avro-json-serializer/avro_json_serializer-0.4.tar.gz/avro_json_serializer-0.4/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+# (c) [2014] LinkedIn Corp. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License"); 
+# you may not use this file except in compliance with the License. 
+# You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+ 
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+import setuptools
+
+setuptools.setup(name='avro_json_serializer',
+      version='0.4',
+      description='Avro Json Serializer',
+      author='Roman Inozemtsev',
+      author_email='rinozemtsev@linkedin.com',
+      packages = ['avro_json_serializer'],
+      license = 'Apache 2.0'
+     )
