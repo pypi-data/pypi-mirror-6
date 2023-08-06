0dda3694135685cf99d6db3e7e26825b2b1a8b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:56 +0000

    Add package ibm_db_py3/2.0.5/ibm_db_py3-2.0.5.tar.gz

---
+++ packages/ibm_db_py3/ibm_db_py3-2.0.5.tar.gz/ibm_db_py3-2.0.5/config.py
@@ -0,0 +1,12 @@
+test_dir	=	'tests'		# Location of testsuite file (relative to current directory) (Don't change this.)
+
+database	=	'TEST'	# Database to connect to. Please use an empty database for best results.
+user		=	'db2'	# User ID to connect with
+password	=	'22'	# Password for given User ID
+hostname	=	'localhost'	# Hostname
+port		=	50000		# Port Number
+
+auth_user	=	'auth_user'	# Authentic user of Database
+auth_pass	=	'auth_pass'	# Password for Authentic user
+tc_user		=	'tc_user'	# Trusted user
+tc_pass		=	'tc_pass'	# Password to trusted user
