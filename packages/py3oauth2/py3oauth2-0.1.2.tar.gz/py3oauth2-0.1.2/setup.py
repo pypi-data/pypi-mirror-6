aabe948ca1a409fca29ec76aae9b615c7b775e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:54 +0000

    Add package py3oauth2/0.1.2/py3oauth2-0.1.2.tar.gz

---
+++ packages/py3oauth2/py3oauth2-0.1.2.tar.gz/py3oauth2-0.1.2/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.dirname(__file__)
+requires = []
+tests_require = [
+    'nose',
+    'coverage'
+]
+
+try:
+    import enum
+except ImportError:
+    requires.append('enum34')
+
+
+def _read(name):
+    try:
+        return open(os.path.join(here, name)).read()
+    except:
+        return ""
+readme = _read("README.md")
+license = _read("LICENSE.md")
+
+setup(
+    name='py3oauth2',
+    version='0.1.2',
+    test_suite='py3oauth2',
+    author='Kohei YOSHIDA',
+    author_email='kohei.yoshida@gehirn.co.jp',
+    description='OAuth 2.0 library for Python 3.',
+    long_description=readme,
+    license=license,
+    url='https://github.com/GehirnInc/py3oauth2',
+    packages=find_packages(),
+    install_requires=requires,
+    tests_require=tests_require,
+)
