53a2f6be27d05116cfc391d0ec18f8b363a54cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:54 +0000

    Add package SlickLogHandler/0.2.9/SlickLogHandler-0.2.9.tar.gz

---
+++ packages/SlickLogHandler/SlickLogHandler-0.2.9.tar.gz/SlickLogHandler-0.2.9/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='SlickLogHandler',
+    version='0.2.9',
+    author='Torindo Nesci',
+    author_email='torindo.nesci@slicklog.com',
+    packages=['slicklog'],
+    scripts=[],
+    url='http://slicklog.com/',
+    license='LICENSE.txt',
+    description='SlickLog.com Python Handler.',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
