5684c10e81e31904f4f736d7370bf044bf2a0f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:06 +0000

    Add package qmpy/0.4.9a/qmpy-0.4.9a.tar.gz

---
+++ packages/qmpy/qmpy-0.4.9a.tar.gz/qmpy-0.4.9a/qmpy/configuration/__init__.py
@@ -0,0 +1,2 @@
+import ConfigParser
+from qmpy.computing.resources import Host, Account, Project, Allocation, User
---
+++ packages/qmpy/qmpy-0.4.9a.tar.gz/qmpy-0.4.9a/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qmpy',
+    version='0.4.9a',
+    author='S. Kirklin',
+    author_email='scott.kirklin@gmail.com',
+    packages=find_packages(),
+    scripts=['bin/oqmd', 'bin/qmpy'],
+    url='http://pypi.python.org/pypi/qmpy',
+    license='LICENSE.txt',
+    description='Suite of computational materials science tools',
+    long_description=open('README.md').read(),
+    package_data = {'': ['*.yml', '*.txt', 'legacy.dat', '*.cfg', '*.rst']},
+    install_requires=[
+        "Django >= 1.5",
+        "PuLP",
+        "numpy >= 1.6.4",
+        "scipy >= 0.12.0",
+        "MySQL-python",
+        "matplotlib",
+        "networkx",
+        "pytest",
+        "python-memcached",
+        "python-ase",
+        "django-extensions",
+        "elementtree",
+        "pyparsing<=1.9.9",
+        "PyCifRW==3.6.2",
+        "PyYAML"
+    ],
+)
