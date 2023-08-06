cfd088bdd2fbb405da14d050dd46c78e61ad572f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package graphterm/0.51.0/graphterm-0.51.0.tar.gz

---
+++ packages/graphterm/graphterm-0.51.0.tar.gz/graphterm-0.51.0/graphterm/about.py
@@ -0,0 +1,9 @@
+version = "0.51.0"
+
+min_version = "0.51"
+
+description = "GraphTerm: A Graphical Terminal Interface"
+
+authors = ["R. Saravanan <sarava@sarava.net>",]
+
+url = "http://code.mindmeldr.com/graphterm"
