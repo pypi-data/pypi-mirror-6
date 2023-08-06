f9e3f992b1e3bf868d124443917fb881c15bc6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:57 +0000

    Add package Greeting/1.0.0/Greeting-1.0.0.zip

---
+++ packages/Greeting/Greeting-1.0.0.zip/Greeting-1.0.0/Greeting.py
@@ -0,0 +1,6 @@
+'''This module is for testing pypi'''
+
+def SayHello(name):
+    '''Say hello to someone'''
+    print("Hello" + name)
+    
---
+++ packages/Greeting/Greeting-1.0.0.zip/Greeting-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'Greeting',
+    version = '1.0.0',
+    py_modules = ['Greeting'],
+    author = 'kenny',
+    author_email = 'i_wangyuyue@hotmail.com',
+    url = 'http://www.wangyuyue.com',
+    description = 'Say hello to someone',
+    )
+
+    
