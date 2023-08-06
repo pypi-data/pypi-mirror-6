a38939d5d35564154c1a2828736c3b9b4e97aeb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:19 +0000

    Add package hellosign-python-sdk/0.3.6/hellosign-python-sdk-0.3.6.tar.gz

---
+++ packages/hellosign-python-sdk/hellosign-python-sdk-0.3.6.tar.gz/hellosign-python-sdk-0.3.6/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__),
+                                'hellosign'))
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='hellosign-python-sdk',
+      version='0.3.6',
+      description="An API wrapper written in Python to interact with \
+            HelloSign's API (http://www.hellosign.com)",
+      long_description=readme(),
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+      ],
+      keywords='hellosign python sdk',
+      url='https://github.com/minhdanh/hellosign-python-sdk',
+      author='Minh Danh',
+      author_email='minhdanh@siliconstraits.vn',
+      license='MIT',
+      packages=[
+        'hellosign',
+        'hellosign.utils',
+        'hellosign.resource',
+        ],
+      install_requires=[
+      'requests'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
