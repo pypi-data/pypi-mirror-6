374f805554c76519604e47d8b927eecbf4931437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:04 +0000

    Add package gpiocrust/0.9.0/gpiocrust-0.9.0.tar.gz

---
+++ packages/gpiocrust/gpiocrust-0.9.0.tar.gz/gpiocrust-0.9.0/gpiocrust/__init__.py
@@ -0,0 +1,14 @@
+"""
+Object oriented wrapper around RPi.GPIO. A work in progress.
+
+(zourtney, August 2013)
+"""
+
+try:
+  import RPi.GPIO
+  from raspberry_pi import *
+except:
+  print '--------------------------------------------------------------------'
+  print ' WARNING: RPi.GPIO library not found. Falling back to mock objects. '
+  print '--------------------------------------------------------------------'
+  from gpio_mock import *
---
+++ packages/gpiocrust/gpiocrust-0.9.0.tar.gz/gpiocrust-0.9.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='gpiocrust',
+      packages=['gpiocrust'],
+      version='0.9.0',
+      description='A pythonic wrapper around RPi.GPIO',
+      author='zourtney',
+      author_email='zourtney@gmail.com',
+      url='https://github.com/zourtney/gpiocrust'
+     )
