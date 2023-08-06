9488b89fb8eed758f77abc2516a577563eb4afe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:44 +0000

    Add package marked/0.9.0/marked-0.9.0.tar.gz

---
+++ packages/marked/marked-0.9.0.tar.gz/marked-0.9.0/setup.py
@@ -0,0 +1,21 @@
+"""Installer for marked
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='marked',
+    description='Library/utility that converts HTML to markdown text',
+    long_description=open('README.rst').read(),
+    provides=['marked'],
+    version='0.9.0',
+    author='Wes Mason',
+    author_email='wes@1stvamp.org',
+    url='https://github.com/1stvamp/marked.py',
+    install_requires=[
+        'beautifulsoup4 >= 4.3',
+        'markgen >= 0.9.6'
+    ],
+    packages=find_packages(exclude=['marked_tests']),
+    license='BSD'
+)
