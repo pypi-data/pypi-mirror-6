f11f80752b3c5f20314f34771b46ce4fdeaaf42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:11 +0000

    Add package infi.nose_html_output/0.1.16/infi.nose_html_output-0.1.16-py2.7.egg

---
+++ packages/infi.nose_html_output/infi.nose_html_output-0.1.16-py2.7.egg/infi/nose_html_output/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.16"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.16)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '178343549eaa2acff9f118827bf69ef47a040991'
+__git_head_subject__ = 'Finished release v0.1.16.'
+__git_head_message__ = '* release/v0.1.16:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements\n  update dependencies'
+__git_dirty_diff__ = ''
