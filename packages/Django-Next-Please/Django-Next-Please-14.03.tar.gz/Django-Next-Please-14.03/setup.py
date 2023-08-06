3f35f66b8022cb9a7fa77e24737e8b5b8d2895da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:14 +0000

    Add package Django-Next-Please/14.03/Django-Next-Please-14.03.tar.gz

---
+++ packages/Django-Next-Please/Django-Next-Please-14.03.tar.gz/Django-Next-Please-14.03/setup.py
@@ -0,0 +1,15 @@
+import sys
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Django-Next-Please",
+    version = '14.03',
+    description = "Simple Pagination Decorator for Django.",
+    url = "https://github.com/pizzapanther/Django-Next-Please",
+    author = "Paul Bailey",
+    author_email = "paul.m.bailey@gmail.com",
+    license = "BSD",
+    packages = ['NextPlease'],
+    include_package_data = True,
+)
