0f610e964294a747d4b47b8e7d2ee653e2fc87aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:04 +0000

    Add package thealot-excuse/0.1.3/thealot-excuse-0.1.3.tar.gz

---
+++ packages/thealot-excuse/thealot-excuse-0.1.3.tar.gz/thealot-excuse-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(name='thealot-excuse',
+      version='0.1.3',
+      author='Edvin "nCrazed" Malinovskis',
+      author_email='edvin.malinovskis@gmail.com',
+      url='https://github.com/nCrazed/ExcusePlugin',
+      packages=['thealot.plugins'],
+      namespace_packages=['thealot'],
+      install_requires=[
+          'thealot',
+          'sqlalchemy',
+          ]
+      )
