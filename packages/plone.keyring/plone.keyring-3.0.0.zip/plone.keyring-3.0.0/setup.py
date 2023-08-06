fba82eb9441bf53ba017bca744283f2cddcaed06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:51 +0000

    Add package plone.keyring/3.0.0/plone.keyring-3.0.0.zip

---
+++ packages/plone.keyring/plone.keyring-3.0.0.zip/plone.keyring-3.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '3.0.0'
+
+setup(name='plone.keyring',
+      version=version,
+      description="Manage secrets",
+      long_description=open("README.txt").read() + "\n" + \
+                       open("CHANGES.txt").read(),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://pypi.python.org/pypi/plone.keyring',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'ZODB3',
+          'zope.container',
+          'zope.interface',
+          'zope.location',
+      ],
+      )
