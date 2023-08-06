70e72ece8e14d65dd53a4a4515f7239637367752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:53 +0000

    Add package google_play/0.1.1/google_play-0.1.1.tar.gz

---
+++ packages/google_play/google_play-0.1.1.tar.gz/google_play-0.1.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='google_play',
+    author='Igor Skrynkovskyy',
+    author_email='skrynkovskyy@gmail.com',
+    description='Google Play app fetcher',
+    license="MIT",
+    url='https://github.com/h2rd/google_play',
+    version='0.1.1',
+    packages=['google_play'],
+    test_suite='tests',
+    install_requires=(
+        'grab==0.4.13',
+        'lxml==3.3.1',
+        'requests==2.2.1'
+    )
+)
