1912c64e046de7d93d0a507855d81222ac843de7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:03 +0000

    Add package divisor_g2/1.0.0/divisor_g2-1.0.0.zip

---
+++ packages/divisor_g2/divisor_g2-1.0.0.zip/divisor_g2-1.0.0/divisor.py
@@ -0,0 +1,11 @@
+def divisor(a):
+
+    print('입력한 수의 약수는 =')
+
+    for i in range(1, a+1):
+
+        if (a%i==0):
+
+            print(i)
+
+    print('입니다.')
---
+++ packages/divisor_g2/divisor_g2-1.0.0.zip/divisor_g2-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'divisor_g2',
+    version = '1.0.0',
+    py_modules = ['divisor'],
+    author = 'group2',
+    author_email = 'jsh001111@daum.com',
+    url = '-',
+    description = 'Seek divisor',
+    )
