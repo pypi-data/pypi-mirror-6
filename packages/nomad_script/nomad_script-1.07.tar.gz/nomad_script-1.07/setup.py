13afd6109da7b137cf96c8bdc3c6271318dd290e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:57 +0000

    Add package nomad_script/1.07/nomad_script-1.07.tar.gz

---
+++ packages/nomad_script/nomad_script-1.07.tar.gz/nomad_script-1.07/setup.py
@@ -0,0 +1,38 @@
+from __future__ import with_statement
+
+import os
+from setuptools import setup, find_packages
+
+version = '1.07'
+
+install_requires = [
+	'setuptools'
+]
+
+description = 'A python script in order to manage a blackbox with nomad.'
+current_dir = os.path.dirname(__file__)
+try:
+	long_description = open(os.path.join(cur_dir, 'README.rst')).read()
+except:
+	long_description = description
+
+setup(
+	name = 'nomad_script',
+	version = version,
+	description = description,
+	url = 'https://github.com/vlnk/nomad-script',
+	author = 'Valentin Laurent',
+	author_email = 'vlnk@mail.com',
+	license = 'MIT',
+	packages = find_packages(),
+    install_requires = install_requires,
+    entry_points='''
+    	[console_scripts]
+    	nomad-script = script.commands:main
+    ''',
+	classifiers=[
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ])
