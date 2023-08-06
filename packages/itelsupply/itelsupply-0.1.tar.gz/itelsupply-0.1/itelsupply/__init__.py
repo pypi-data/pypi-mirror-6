953c2e1a924a86306b388f26846ce814fa2bfbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:33 +0000

    Add package itelsupply/0.1/itelsupply-0.1.tar.gz

---
+++ packages/itelsupply/itelsupply-0.1.tar.gz/itelsupply-0.1/itelsupply/__init__.py
@@ -0,0 +1,14 @@
+"""An ITelSupply API wrapper for Python
+
+.. moduleauthor:: Mark Allan B Meriales <mark.meriales@gmail.com>
+
+"""
+
+
+__version__ = "0.1"
+__app_name__ = "itelsupply"
+__description__ = "Python API client for "
+__author__ = "Mark Allan B Meriales"
+__author_email__ = "mark.meriales@gmail.com"
+__app_url__ = "https://github.com/makmac213/"
+__download_url__ = "https://github.com/makmac213/python-itelsupply"
---
+++ packages/itelsupply/itelsupply-0.1.tar.gz/itelsupply-0.1/setup.py
@@ -0,0 +1,24 @@
+import itelsupply
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name=itelsupply.__app_name__,
+    version=itelsupply.__version__,
+    description=itelsupply.__description__,
+    author=itelsupply.__author__,
+    author_email=itelsupply.__author_email__,
+    packages=['itelsupply'],
+    url=itelsupply.__app_url__,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'License :: Freeware',
+    ),
+    download_url=itelsupply.__download_url__,
+)
