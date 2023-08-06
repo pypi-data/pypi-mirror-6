d51721c935e868d98467af72b15714cefbf3506e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:49 +0000

    Add package Fosbury/0.9.6/Fosbury-0.9.6.tar.gz

---
+++ packages/Fosbury/Fosbury-0.9.6.tar.gz/Fosbury-0.9.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='Fosbury',
+      version='0.9.6',
+      author='Willem Spruijt',
+      author_email='willem@fosbury.co',
+      description='Python client for Fosbury (http://fosbury.co)',
+      packages=find_packages(),
+      license='Apache License 2.0')
