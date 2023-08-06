0e6fbea05e89a37ed3ba9cd8c28a9904509f8e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:23 +0000

    Add package django-object-log/0.7.1/django-object-log-0.7.1.tar.gz

---
+++ packages/django-object-log/django-object-log-0.7.1.tar.gz/django-object-log-0.7.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+long_desc = open('README').read()
+
+setup(name='django-object-log',
+      version="0.7.1",
+      description='A method for logging user actions on models',
+      long_description=long_desc,
+      author='Peter Krenesky',
+      author_email='peter@osuosl.org',
+      maintainer="Corbin Simpson",
+      maintainer_email="simpsoco@osuosl.org",
+      url='http://code.osuosl.org/projects/django-object-log',
+      packages=['object_log'],
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          'Framework :: Django',
+          ],
+      )
