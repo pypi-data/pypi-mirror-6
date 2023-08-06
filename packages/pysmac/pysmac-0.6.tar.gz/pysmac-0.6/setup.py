cee306e14e6d2f5486152d3be80e4613105946d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:09 +0000

    Add package pysmac/0.6/pysmac-0.6.tar.gz

---
+++ packages/pysmac/pysmac-0.6.tar.gz/pysmac-0.6/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+"""
+    for the long description, convert:
+        https://coderwall.com/p/qawuyq
+    or manually here:
+        http://johnmacfarlane.net/pandoc/try/
+"""
+
+def check_java_exists():
+    from subprocess import call
+    import os
+    try:
+        devnull = open(os.devnull, 'w')
+        call("java", stdout=devnull, stderr=devnull)
+    except:
+        error_msg = """
+        Java not found!
+
+        pysmac needs java in order to work. You can download java from:
+        http://java.com/getjava
+        """
+        raise RuntimeError(error_msg)
+
+check_java_exists()
+
+setup(
+    name = "pysmac",
+    version = "0.6",
+    packages = find_packages(),
+    install_requires = ['numpy', 'docutils>=0.3', 'setuptools'],
+    author = "Tobias Domhan (python wrapper). Frank Hutter, Holger Hoos, Kevin Leyton-Brown, Kevin Murphy and Steve Ramage (SMAC)",
+    author_email = "domhant@informatik.uni-freiburg.de",
+    description = "python interface to the hyperparameter optimization tool SMAC.",
+    include_package_data = True,
+    test_suite = 'pysmac.test',
+    keywords = "hyperparameter parameter optimization hyperopt bayesian smac global",
+    license = "SMAC is free for academic & non-commercial usage. Please contact Frank Hutter(fh@informatik.uni-freiburg.de) to discuss obtaining a license for commercial purposes.",
+    url = "http://www.cs.ubc.ca/labs/beta/Projects/SMAC/"
+)
