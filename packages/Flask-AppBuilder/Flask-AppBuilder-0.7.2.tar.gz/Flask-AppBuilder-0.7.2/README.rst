c489a714b06bcc75de1d4b59eab29908bcf43d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:25 +0000

    Add package Flask-AppBuilder/0.7.2/Flask-AppBuilder-0.7.2.tar.gz

---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-0.7.2.tar.gz/Flask-AppBuilder-0.7.2/README.rst
@@ -0,0 +1,124 @@
+Flask App Builder
+=================
+
+Simple and rapid application builder framework, built on top of `Flask <http://flask.pocoo.org/>`_.
+includes detailed security, auto form generation, google charts and much more.
+
+Take a look at installation, quick how to tutorials, API reference etc: `Documentation <http://flask-appbuilder.readthedocs.org/en/latest/>`_
+
+Package Version
+---------------
+
+*New 0.7.N* with bug fixes, and new features. If your already using F.A.B. read carefully the `migration <http://flask-appbuilder.readthedocs.org/en/latest/versionmigration.html>`_ procedures.
+
+Also read the `Versions <http://flask-appbuilder.readthedocs.org/en/latest/versions.html>`_ for further detail on what changed.
+
+Fixes, Bugs and contributions
+-----------------------------
+
+You're welcome to report bugs, propose new features, or even better contribute to this project.
+
+`Issues, bugs and new features <https://github.com/dpgaspar/Flask-AppBuilder/issues/new>`_
+
+`Contribute <https://github.com/dpgaspar/Flask-AppBuilder/fork>`_
+
+Includes:
+---------
+
+  - Security
+      - Auto permissions lookup, based on exposed methods. It will grant all permissions to the Admin Role.
+      - Inserts on the Database all the detailed permissions possible on your application.
+      - Public (no authentication needed) and Private permissions.
+      - Role based permissions.
+      - Authentication based on OpenID, Database and LDAP.
+  - Views and Widgets
+      - Auto menu generator.
+      - Various view widgets: lists, master-detail, list of thumbnails etc
+      - Select2, Datepicker, DateTimePicker
+      - Menu with icons
+      - Google charts with automatic group by.
+  - Forms
+      - Auto Create, Remove, Add, Edit and Show from Database Models
+      - Labels and descriptions for each field.
+      - Automatic base validators from model definition.
+      - Custom validators, extra fields, custom filters for related dropdown lists.
+      - Image and File support for upload and database field association. It will handle everything for you.
+      - Field sets for Form's (Django style).
+  - i18n
+      - Support for multi-language via Babel
+  - Bootstrap 3.0.3 CSS and js, with Select2 and DatePicker
+  - Font-Awesome icons, for menu icons and actions.
+
+
+Some pictures
+-------------
+
+Login page (with AUTH_DB) 
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/login_db.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/login_db.png
+    
+
+Login page (with AUTH_OID)
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/login_oid.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/login_oid.png
+
+
+Security 
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/security.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/security.png
+
+
+Lists:
+
+List contacts example 
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/contact_list.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/contact_list.png
+
+
+`List Group example with search 
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/group_list.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/group_list.png
+
+
+Charts:
+
+Group by pie chart 
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/chart.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/chart.png
+
+Group by month chart 
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/chart_time1.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/chart_time1.png
+
+Group by year chart
+
+.. image:: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/chart_time2.png
+    :width: 480px
+    :target: https://raw.github.com/dpgaspar/flask-AppBuilder/master/images/chart_time2.png
+
+
+Depends on:
+-----------
+
+- flask
+- flask-sqlalchemy
+- flask-login
+- flask-openid
+- flask-wtform
+- flask-Babel
+
+
---
+++ packages/Flask-AppBuilder/Flask-AppBuilder-0.7.2.tar.gz/Flask-AppBuilder-0.7.2/flask_appbuilder/charts/views.py
@@ -0,0 +1,227 @@
+import logging
+import jsontools
+from flask import render_template
+from flask.ext.babelpkg import lazy_gettext
+from widgets import ChartWidget, DirectChartWidget, MultipleChartWidget
+from ..widgets import SearchWidget
+from ..security.decorators import has_access
+from ..baseviews import BaseModelView, expose
+from ..urltools import *
+
+log = logging.getLogger(__name__)
+
+
+class BaseChartView(BaseModelView):
+    """
+        This is the base class for all chart views. 
+        Use ChartView or TimeChartView, override their properties and these
+        to customise your charts
+    """
+
+    chart_template = 'appbuilder/general/charts/chart.html'
+    """ The chart template, override to implement your own """
+    chart_widget = ChartWidget
+    """ Chart widget override to implement your own """
+    search_widget = SearchWidget
+    """ Search widget override to implement your own """
+
+    chart_title = 'Chart'
+    """ A title to be displayed on the chart """
+    group_by_label = lazy_gettext('Group by')
+    """ The label that is displayed for the chart selection """
+
+    default_view = 'chart'
+
+    chart_type = 'PieChart'
+    """ The chart type PieChart, ColumnChart, LineChart """
+    chart_3d = 'true'
+    """ Will display in 3D? """
+    width = 400
+    """ The width """
+    height = '400px'
+
+    group_bys = {}
+    """ New for 0.6.4, on test, don't use yet """
+
+
+    def _get_chart_widget(self, value_columns=None, widgets=None, **args):
+        widgets = widgets or dict()
+        widgets['chart'] = self.chart_widget(route_base=self.route_base,
+                                             chart_title=self.chart_title,
+                                             chart_type=self.chart_type,
+                                             chart_3d=self.chart_3d,
+                                             value_columns=value_columns, **args)
+        return widgets
+
+
+class BaseSimpleGroupByChartView(BaseChartView):
+    group_by_columns = []
+    """ A list of columns to be possibly grouped by, this list must be filled """
+
+    def __init__(self, **kwargs):
+        if not self.group_by_columns:
+            raise Exception('Base Chart View property <group_by_columns> must not be empty')
+        else:
+            super(BaseChartView, self).__init__(**kwargs)
+
+
+class BaseSimpleDirectChartView(BaseChartView):
+    direct_columns = []
+    """
+        Make chart using the column on the dict
+        chart_columns = {'chart label 1':('X column','Y1 Column','Y2 Column, ...),
+                        'chart label 2': ('X Column','Y1 Column',...),...}
+    """
+
+    def __init__(self, **kwargs):
+        if not self.direct_columns:
+            raise Exception('Base Chart View property <direct_columns> must not be empty')
+        else:
+            super(BaseChartView, self).__init__(**kwargs)
+
+
+    def get_group_by_columns(self):
+        """
+            returns the keys from direct_columns
+            Used in template, so that user can choose from options
+        """
+        return self.direct_columns.keys()
+
+
+class ChartView(BaseSimpleGroupByChartView):
+    """
+        Provides a simple (and hopefully nice) way to draw charts on your application.
+
+        This will show Google Charts based on group by of your tables.                
+    """
+
+    @expose('/chart/<group_by>')
+    @expose('/chart/')
+    @has_access
+    def chart(self, group_by=''):
+        form = self.search_form.refresh()
+        get_filter_args(self._filters)
+
+        group_by = group_by or self.group_by_columns[0]
+        value_columns = self.datamodel.query_simple_group(group_by, filters=self._filters)
+
+        widgets = self._get_chart_widget(value_columns=value_columns)
+        widgets = self._get_search_widget(form=form, widgets=widgets)
+        return render_template(self.chart_template, route_base=self.route_base,
+                               title=self.chart_title,
+                               label_columns=self.label_columns,
+                               group_by_columns=self.group_by_columns,
+                               group_by_label=self.group_by_label,
+                               height=self.height,
+                               widgets=widgets,
+                               baseapp=self.baseapp)
+
+
+class TimeChartView(BaseSimpleGroupByChartView):
+    """
+        Provides a simple way to draw some time charts on your application.
+
+        This will show Google Charts based on count and group by month and year for your tables.
+    """
+
+    chart_template = 'appbuilder/general/charts/chart_time.html'
+    chart_type = 'ColumnChart'
+
+    @expose('/chart/<group_by>/<period>')
+    @expose('/chart/')
+    @has_access
+    def chart(self, group_by='', period=''):
+        form = self.search_form.refresh()
+        get_filter_args(self._filters)
+
+        group_by = group_by or self.group_by_columns[0]
+
+        if period == 'month' or not period:
+            value_columns = self.datamodel.query_month_group(group_by, filters=self._filters)
+        elif period == 'year':
+            value_columns = self.datamodel.query_year_group(group_by, filters=self._filters)
+        widgets = self._get_chart_widget(value_columns=value_columns)
+        widgets = self._get_search_widget(form=form, widgets=widgets)
+        return render_template(self.chart_template, route_base=self.route_base,
+                               title=self.chart_title,
+                               label_columns=self.label_columns,
+                               group_by_columns=self.group_by_columns,
+                               group_by_label=self.group_by_label,
+                               height=self.height,
+                               widgets=widgets,
+                               baseapp=self.baseapp)
+
+
+class DirectChartView(BaseSimpleDirectChartView):
+    """
+        This class is responsible for displaying a Google chart with
+        direct model values. Chart widget uses json.
+        No group by is processed, example::
+
+            class StatsChartView(DirectChartView):
+                datamodel = SQLAModel(Stats)
+                chart_title = lazy_gettext('Statistics')
+                direct_columns = {'Some Stats': ('stat1', 'col1', 'col2'),
+                                  'Other Stats': ('stat2', 'col3')}
+
+    """
+    chart_type = 'ColumnChart'
+
+    chart_widget = DirectChartWidget
+
+    @expose('/chart/<group_by>')
+    @expose('/chart/')
+    @has_access
+    def chart(self, group_by=''):
+        form = self.search_form.refresh()
+        get_filter_args(self._filters)
+
+        direct_key = group_by or self.direct_columns.keys()[0]
+        direct = self.direct_columns.get(direct_key)
+
+        if self.base_order:
+            order_column, order_direction = self.base_order
+        else:
+            order_column, order_direction = '', ''
+        count, lst = self.datamodel.query(filters=self._filters,
+                                          order_column=order_column,
+                                          order_direction=order_direction)
+        value_columns = self.datamodel.get_values(lst, list(direct))
+        value_columns = jsontools.dict_to_json(direct[0], direct[1:], self.label_columns, value_columns)
+
+        widgets = self._get_chart_widget(value_columns=value_columns)
+        widgets = self._get_search_widget(form=form, widgets=widgets)
+        return render_template(self.chart_template, route_base=self.route_base,
+                               title=self.chart_title,
+                               label_columns=self.label_columns,
+                               group_by_columns=self.get_group_by_columns(),
+                               group_by_label=self.group_by_label,
+                               height=self.height,
+                               widgets=widgets,
+                               baseapp=self.baseapp)
+
+
+class MultipleChartView(BaseChartView):
+    chart_template = 'appbuilder/general/charts/chart.html'
+    chart_type = 'ColumnChart'
+
+    chart_widget = MultipleChartWidget
+
+    @expose('/chart/')
+    @has_access
+    def chart(self):
+        form = self.search_form.refresh()
+        get_filter_args(self._filters)
+
+        value_columns = self.datamodel.query_group(self.group_bys[0], filters=self._filters)
+
+        widgets = self._get_chart_widget(value_columns=value_columns)
+        widgets = self._get_search_widget(form=form, widgets=widgets)
+        return render_template(self.chart_template, route_base=self.route_base,
+                               title=self.chart_title,
+                               label_columns=self.label_columns,
+                               group_by_columns=self.group_by_columns,
+                               group_by_label=self.group_by_label,
+                               height=self.height,
+                               widgets=widgets,
+                               baseapp=self.baseapp)
