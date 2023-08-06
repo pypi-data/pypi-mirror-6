6faa81c6470e2a8b1fdc1854e97cdf0594bee990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:18 +0000

    Add package ovhapi/2.1/ovhapi-2.1.tar.gz

---
+++ packages/ovhapi/ovhapi-2.1.tar.gz/ovhapi-2.1/ovhapi/__init__.py
@@ -0,0 +1,1 @@
+from OvhApi import Api, OVH_API_EU, OVH_API_CA
---
+++ packages/ovhapi/ovhapi-2.1.tar.gz/ovhapi-2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "ovhapi",
+    version = "2.1",
+    description='Python module to manage OVH API',
+    maintainer='Guilhem Lettron',
+    maintainer_email='guilhem.lettron@optiflows.com',
+    url='https://github.com/optiflows/OvhApi',   
+    packages = find_packages(),
+)
