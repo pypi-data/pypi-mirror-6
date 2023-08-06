d41d1d5380d910bf44b63cb3c46220c70553b5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:40 +0000

    Add package lazyconf/0.5.2/lazyconf-0.5.2.tar.gz

---
+++ packages/lazyconf/lazyconf-0.5.2.tar.gz/lazyconf-0.5.2/setup.py
@@ -0,0 +1,23 @@
+import sys
+from setuptools import setup, find_packages
+import lazyconf
+
+l = 'lazyconf/'
+s = 'schema/'
+
+setup(
+    name='lazyconf',
+    version='0.5.2',
+    author='Fareed Dudhia',
+    author_email='fareeddudhia@gmail.com',
+    packages=find_packages(),
+    package_data={"lazyconf": ['schema/' 'lazyconf/schema/*.json',]},
+    include_package_data=True,
+    py_modules=['lazyconf.lazyconf','lazyconf.console'],
+    entry_points={
+        'console_scripts': ['lazyconf = lazyconf.console:console',]},
+    url='https://www.github.com/fmd/lazyconf',
+    license='LICENSE.rst',
+    description='Insultingly simple configuration for Python 2.7 applications.',
+    long_description=open('README.rst').read(),
+)
