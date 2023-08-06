5647569a409f53e11a03553300438e189d3a8d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:55 +0000

    Add package i3pystatus/3.28/i3pystatus-3.28.tar.gz

---
+++ packages/i3pystatus/i3pystatus-3.28.tar.gz/i3pystatus-3.28/i3pystatus/parcel.py
@@ -0,0 +1,109 @@
+
+from urllib.request import urlopen
+import webbrowser
+
+import lxml.html
+from lxml.cssselect import CSSSelector
+
+from i3pystatus import IntervalModule
+from i3pystatus.core.util import internet, require
+
+
+class TrackerAPI:
+
+    def __init__(self, idcode):
+        pass
+
+    def status(self):
+        return {}
+
+
+"""class DHL(TrackerAPI):
+    URL = "http://nolp.dhl.de/nextt-online-public/set_identcodes.do?lang=en&idc={idcode}"
+
+    def __init__(self, idcode):
+        self.idcode = idcode
+        self.url = self.URL.format(idcode=self.idcode)
+
+        error_selector = CSSSelector("#set_identcodes .error")
+        self.error = lambda page: len(error_selector(page)) >= 1
+        self.progress_selector = CSSSelector(
+            ".greyprogressbar > span, .greenprogressbar > span")
+        self.last_status_selector = CSSSelector(".events .eventList tr")
+        self.intrarow_status_selector = CSSSelector("td.status div")
+
+    def status(self):
+        ret = {}
+        with urlopen(self.url) as page:
+            page = lxml.html.fromstring(page.read())
+            if self.error(page):
+                ret["progress"] = ret["status"] = "n/a"
+            else:
+                ret["progress"] = self.progress_selector(page)[0].text.strip()
+                last_row = self.last_status_selector(page)[-1]
+                ret["status"] = self.intrarow_status_selector(
+                    last_row)[0].text.strip()
+        return ret
+
+    def get_url(self):
+        return self.url
+"""
+
+class UPS(TrackerAPI):
+    URL = "http://wwwapps.ups.com/WebTracking/processRequest?HTMLVersion=5.0&Requester=NES&AgreeToTermsAndConditions=yes&loc=en_US&tracknum={idcode}"
+
+    def __init__(self, idcode):
+        self.idcode = idcode
+        self.url = self.URL.format(idcode=self.idcode)
+
+        error_selector = CSSSelector(".secBody .error")
+        self.error = lambda page: len(error_selector(page)) >= 1
+        self.status_selector = CSSSelector("#tt_spStatus")
+        self.progress_selector = CSSSelector(".pkgProgress div")
+
+    def status(self):
+        ret = {}
+        with urlopen(self.url) as page:
+            page = lxml.html.fromstring(page.read())
+            if self.error(page):
+                ret["progress"] = ret["status"] = "n/a"
+            else:
+                ret["status"] = self.status_selector(page)[0].text.strip()
+                progress_cls = int(
+                    int(self.progress_selector(page)[0].get("class").strip("staus")) / 5 * 100)
+                ret["progress"] = progress_cls
+        return ret
+
+    def get_url(self):
+        return self.url
+
+
+class ParcelTracker(IntervalModule):
+    """
+    Used to track parcel/shipments.
+    """
+    interval = 20
+
+    settings = (
+        ("instance", "Tracker instance, for example ``parcel.UPS('your_id_code')``"),
+        "format",
+        "name",
+    )
+    required = ("instance",)
+
+    format = "{name}:{progress}"
+
+    @require(internet)
+    def run(self):
+        fdict = {
+            "name": self.name,
+        }
+        fdict.update(self.instance.status())
+
+        self.output = {
+            "full_text": self.format.format(**fdict).strip(),
+            "instance": self.name,
+        }
+
+    def on_leftclick(self):
+        webbrowser.open_new_tab(self.instance.get_url())
