21a2751b8acc4c17628398e5f2749fa5efa2c7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:21 +0000

    Add package brownfox/0.1.1/brownfox-0.1.1.tar.gz

---
+++ packages/brownfox/brownfox-0.1.1.tar.gz/brownfox-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'brownfox',
+    version = '0.1.1',
+    packages = find_packages(),
+
+    scripts = ['brownfox.py'],
+
+    author = 'introom',
+    author_email = "i@introo.me",
+    description = "The quick brown fox switches directories amazingly fast",
+    keywords = "shell bookmark directory",
+    classifiers = [
+        'Topic :: Terminals',
+    ],
+    entry_points={
+        'console_scripts': [
+            'brownfox=brownfox:main',
+            ]
+    }
+)
+
