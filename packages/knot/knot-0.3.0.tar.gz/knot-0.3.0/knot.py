0f0b31a07b47e2b5db94abec0a915db4efaa7df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:52 +0000

    Add package knot/0.3.0/knot-0.3.0.tar.gz

---
+++ packages/knot/knot-0.3.0.tar.gz/knot-0.3.0/knot.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+
+"""
+knot
+~~~~
+
+Knot is a simple dependency container for Python.
+
+:copyright: (c) 2014 by Jaap Verloop.
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from functools import update_wrapper
+
+
+def factory(container, name=None):
+    """A decorator to register a factory on the container.
+    For more information see :meth:`Container.add_factory`.
+    """
+    def register(factory):
+        container.add_factory(factory, name)
+        return factory
+
+    return register
+
+
+def service(container, name=None):
+    """A decorator to register a service on a container.
+    For more information see :meth:`Container.add_service`.
+    """
+    def register(service):
+        container.add_service(service, name)
+        return service
+
+    return register
+
+
+def provider(container, cache, name=None):
+    """A decorator to register a provider on a container.
+    For more information see :meth:`Container.add_provider`.
+    """
+    def register(provider):
+        container.add_provider(provider, cache, name)
+        return provider
+
+    return register
+
+
+class FunctionCache(object):
+    """The :class:`FunctionCache` object wraps a function and ensures it's
+    called only once by caching the return value.
+    """
+
+    def __init__(self, f):
+        """Redirects function calls to :meth:`__call__` and updates itself to
+        look like the real function."""
+        self._f = f
+        self._cache = None
+        update_wrapper(self, f)
+
+    def __call__(self, *args, **kwargs):
+        """Returns the return value of the real function. First it will attempt
+        to return from cache before calling the real function.
+        """
+        if not self.cached:
+            self._cache = self._f(*args, **kwargs)
+        return self._cache
+
+    @property
+    def cached(self):
+        """Indicates whether a return value is cached."""
+        return self._cache is not None
+
+
+class Container(dict):
+    """The :class:`Container` object acts as a central registry for providers
+    and configuration settings.
+
+    This class is implemented as a subclass of ``dict``. All standard
+    dictionary methods are available without any modifications.
+    """
+
+    def __call__(self, *args):
+        """A shortcut method for convenience.
+        For more information see :meth:`Container.provide`.
+        """
+        return self.provide(*args)
+
+    def provide(self, name, default=None):
+        """Gets the value registered with ``name`` and determines whether the
+        value is a provider or a configuration setting. The ``default`` value
+        is returned if ``name`` is unknown.
+
+        The registered value is interpreted as a provider if it's callable. The
+        provider is called with a single argument, the current
+        :class:`Container` object. Returns the return value of a provider or
+        the value itself in case the value is not callable.
+
+        :param name:
+            The name of the provider or configuration setting.
+        :param default:
+            The default value to return if name is unknown.
+        """
+        rv = super(Container, self).get(name)
+        return rv(self) if callable(rv) else rv or default
+
+    def add_factory(self, factory, name=None):
+        """Registers a factory on the container. A factory is a provider with
+        the ``cache`` argument set to ``False``.
+        For more information see :meth:`add_provider`.
+        """
+        self.add_provider(factory, False, name)
+
+    def add_service(self, service, name=None):
+        """Registers a service on the container. A service is a provider with
+        the ``cache`` argument set to ``False``.
+        For more information see :meth:`add_provider`.
+        """
+        self.add_provider(service, True, name)
+
+    def add_provider(self, provider, cache, name=None):
+        """Registers a provider on the container.
+
+        :param provider:
+            Anything that's callable and expects exactly one argument, the
+            :class:`Container` object.
+        :param cache:
+            Whether to cache the return value of the provider.
+        :param name:
+            Alternative name of the provider.
+            Default: name of the callable.
+        """
+        register_as = name or provider.__name__
+        self[register_as] = FunctionCache(provider) if cache else provider
+
+    def is_cached(self, name):
+        """Determines if the return value is cached. Always returns false if
+        the registered value is not an instance of :class:`FunctionCache`.
+
+        :param name:
+            The name of the provider.
+        """
+        try:
+            cached = super(Container, self).get(name).cached
+        except AttributeError:
+            cached = False
+
+        return cached
---
+++ packages/knot/knot-0.3.0.tar.gz/knot-0.3.0/test_knot.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from mock import MagicMock
+from knot import Container, factory, service, provider
+
+
+class TestContainer(unittest.TestCase):
+    def test_returns_return_value_provider(self):
+        c = Container()
+
+        def foo(container):
+            return 'bar'
+
+        c.add_provider(foo, False)
+
+        self.assertEqual(c.provide('foo'), 'bar')
+
+    def test_returns_value(self):
+        c = Container({'value': 'foobar'})
+
+        self.assertEqual(c.provide('value'), 'foobar')
+
+    def test_returns_default(self):
+        c = Container()
+
+        self.assertEqual(c.provide('foo', 'bar'), 'bar')
+
+    def test_forwards_to_provide(self):
+        c = Container()
+        c.provide = MagicMock()
+
+        c.provide('foo', 'bar', 'default')
+        c.provide.assert_called_with('foo', 'bar', 'default')
+
+    def test_caches_return_value_provider(self):
+        c = Container()
+
+        def foobar(container):
+            return {}
+
+        c.add_provider(foobar, True)
+
+        dict1 = c.provide('foobar')
+        dict2 = c.provide('foobar')
+
+        assert isinstance(dict1, dict)
+        assert isinstance(dict2, dict)
+        assert dict1 is dict2
+
+    def test_uses_alternative_name(self):
+        c = Container()
+
+        def foobar(container):
+            return 'foobar'
+
+        c.add_provider(foobar, False, 'alternative')
+
+        self.assertEqual(c.provide('alternative'), 'foobar')
+
+    def test_registers_factory(self):
+        c = Container()
+        c.add_provider = MagicMock()
+
+        def foo(container):
+            pass
+
+        c.add_factory(foo)
+        c.add_provider.assert_called_with(foo, False, None)
+
+        c.add_factory(foo, 'alternative')
+        c.add_provider.assert_called_with(foo, False, 'alternative')
+
+    def test_registers_service(self):
+        c = Container()
+        c.add_provider = MagicMock()
+
+        def foo(container):
+            pass
+
+        c.add_service(foo)
+        c.add_provider.assert_called_with(foo, True, None)
+
+        c.add_service(foo, 'alternative')
+        c.add_provider.assert_called_with(foo, True, 'alternative')
+
+    def test_registers_factory_with_decorator(self):
+        c = Container()
+        c.add_factory = MagicMock()
+
+        @factory(c)
+        def foo(container):
+            pass
+
+        c.add_factory.assert_called_with(foo, None)
+
+        @factory(c, 'alternative')
+        def bar(container):
+            pass
+
+        c.add_factory.assert_called_with(bar, 'alternative')
+
+    def test_registers_service_with_decorator(self):
+        c = Container()
+        c.add_service = MagicMock()
+
+        @service(c)
+        def foo(container):
+            pass
+
+        c.add_service.assert_called_with(foo, None)
+
+        @service(c, 'alternative')
+        def bar(container):
+            pass
+
+        c.add_service.assert_called_with(bar, 'alternative')
+
+    def test_registers_provider_with_decorator(self):
+        c = Container()
+        c.add_provider = MagicMock()
+
+        @provider(c, False)
+        def foo(container):
+            pass
+
+        c.add_provider.assert_called_with(foo, False, None)
+
+        @provider(c, True, 'alternative')
+        def bar(container):
+            pass
+
+        c.add_provider.assert_called_with(bar, True, 'alternative')
+
+    def test_returns_whether_return_value_provider_is_cached(self):
+        c = Container()
+
+        def foobar(container):
+            return {}
+
+        c.add_provider(foobar, True)
+
+        self.assertFalse(c.is_cached('foobar'))
+        c.provide('foobar')
+        self.assertTrue(c.is_cached('foobar'))
+
+        def foobaz(container):
+            return {}
+
+        c.add_provider(foobaz, False)
+
+        self.assertFalse(c.is_cached('foobaz'))
+        c.provide('foobaz')
+        self.assertFalse(c.is_cached('foobaz'))
+
+
+if __name__ == '__main__':
+    unittest.main()
