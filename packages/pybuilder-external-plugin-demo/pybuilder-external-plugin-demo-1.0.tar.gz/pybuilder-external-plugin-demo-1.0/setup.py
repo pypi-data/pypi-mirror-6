84a023f36118eeb4947f3265c47bcb0974214e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:43 +0000

    Add package pybuilder-external-plugin-demo/1.0/pybuilder-external-plugin-demo-1.0.tar.gz

---
+++ packages/pybuilder-external-plugin-demo/pybuilder-external-plugin-demo-1.0.tar.gz/pybuilder-external-plugin-demo-1.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+          name = 'pybuilder-external-plugin-demo',
+          version = '1.0',
+          description = '''''',
+          long_description = '''''',
+          author = "",
+          author_email = "",
+          license = '',
+          url = '',
+          scripts = [],
+          packages = ['pybuilder_external_plugin_demo'],
+          classifiers = ['Development Status :: 3 - Alpha', 'Programming Language :: Python'],
+             #  data files
+             # package data
+          
+          
+          zip_safe=True
+    )
