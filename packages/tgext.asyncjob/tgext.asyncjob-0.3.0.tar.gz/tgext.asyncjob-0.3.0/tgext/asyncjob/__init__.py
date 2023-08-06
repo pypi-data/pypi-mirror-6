82e594790ea267f470d80cac6461d9cd99f6c63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:54 +0000

    Add package tgext.asyncjob/0.3.0/tgext.asyncjob-0.3.0.tar.gz

---
+++ packages/tgext.asyncjob/tgext.asyncjob-0.3.0.tar.gz/tgext.asyncjob-0.3.0/tgext/asyncjob/__init__.py
@@ -0,0 +1,20 @@
+from .tghooks import start_async_worker
+from .worker import asyncjob_running_uid
+from .progress import asyncjob_set_progress, asyncjob_get_progress
+from .queue import asyncjob_perform
+from .sqlahelpers import asyncjob_timed_query
+from tg.configuration import milestones
+
+import logging
+log = logging.getLogger('tgext.asyncjob')
+
+def plugme(configurator, options=None):
+    if options is None:
+        options = {}
+
+    log.info('Setting up tgext.asyncjob...')
+
+    milestones.environment_loaded.register(lambda: start_async_worker(options.get('app_globals'),
+                                                                      options.get('progress_tracker')))
+
+    return dict(appid='tgext.asyncjob')
