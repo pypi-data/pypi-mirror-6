3ea69644c236e09f00c2ffa2d08e24f244ef9da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:20 +0000

    Add package nester_ljj/1.2.0/nester_ljj-1.2.0.tar.gz

---
+++ packages/nester_ljj/nester_ljj-1.2.0.tar.gz/nester_ljj-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            ="nester_ljj",
+        version         ="1.2.0",
+        py_modules  = ['nester_ljj'],
+        author          = 'ljunjiang',
+        author_email     = 'ljunjiang@gmail.com',
+        url             = 'www.jinxin.com',
+        description     ='a printer of nester for list',
+        )
