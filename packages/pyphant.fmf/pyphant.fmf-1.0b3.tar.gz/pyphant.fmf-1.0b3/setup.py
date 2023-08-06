ab5c1825548a8f046466c44d77accd158b47dac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package pyphant.fmf/1.0b3/pyphant.fmf-1.0b3.tar.gz

---
+++ packages/pyphant.fmf/pyphant.fmf-1.0b3.tar.gz/pyphant.fmf-1.0b3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pyphant Full Meta File Toolbox
+Visit http://pyphant.sourceforge.net for more information.
+"""
+
+
+VERSION = '1.0b3'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyphant.fmf",
+    version=VERSION,
+    author="Alexander Held, Andreas W. Liehr, Rolf Wuerdemann, " + \
+        "Klaus Zimmermann",
+    author_email='alexander.held@fmf.uni-freiburg.de',
+    maintainer='Alexander Held',
+    maintainer_email='alexander.held@fmf.uni-freiburg.de',
+    license="BSD",
+    description=__doc__,
+    install_requires=[
+        'pyphant>=1.0b3',
+        'wxPython',
+        ],
+    packages=find_packages(),
+    entry_points="""
+    [pyphant.workers]
+    myeentry = fmfile
+    [pyphant.visualizers]
+    pil.image = fmfile.FMFWriter:FMFWriter
+    """,
+    include_package_data=True,
+    test_suite='fmfile.tests'
+    )
