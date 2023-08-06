8cd06585e932d2e0c1762f4d06054d68c499f205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:12 +0000

    Add package infi.recipe.application-packager/0.4.6/infi.recipe.application_packager-0.4.6-py2.7.egg

---
+++ packages/infi.recipe.application-packager/infi.recipe.application_packager-0.4.6-py2.7.egg/infi/recipe/application_packager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.6"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.4.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '3999bd7d9c6f4c49349a40d3b37c6b5e087066b4'
+__git_head_subject__ = 'Finished release v0.4.6.'
+__git_head_message__ = '* release/v0.4.6:\n  fixing tests\n  HOSTDEV-1547 reporting status inside the rpm post-install script\n  fixing test\n  added support for installing on windows 8.1'
+__git_dirty_diff__ = ''
