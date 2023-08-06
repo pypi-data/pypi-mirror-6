ec7da6918da82c788778516dce25917cd9ba4c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:44 +0000

    Add package doit-py/0.1.1/doit-py-0.1.1.tar.gz

---
+++ packages/doit-py/doit-py-0.1.1.tar.gz/doit-py-0.1.1/README.rst
@@ -0,0 +1,21 @@
+=========
+doit-py
+=========
+
+`doit`_ tasks for python stuff
+
+.. _doit: http://pydoit.org
+
+
+.. display some badges
+
+.. image:: https://travis-ci.org/pydoit/doit-py.png?branch=master
+  :target: https://travis-ci.org/pydoit/doit-py
+
+.. image:: https://coveralls.io/repos/pydoit/doit-py/badge.png
+        :target: https://coveralls.io/r/pydoit/doit-py
+
+Include tasks for:
+
+- pyflakes
+- coverage
---
+++ packages/doit-py/doit-py-0.1.1.tar.gz/doit-py-0.1.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup (
+    name = 'doit-py',
+    version = '0.1.1',
+    author = 'Eduardo Naufel Schettino',
+    author_email = 'schettino72@gmail.com',
+    description = 'doit tasks for python stuff',
+    url = 'http://packages.python.org/doit-py',
+    keywords = ['doit',],
+    platforms = ['any'],
+    license = 'MIT',
+    packages = ['doitpy'],
+    py_modules = ['confclass'],
+    install_requires = [
+        'pathlib',
+        'doit',
+        'pyflakes',
+        ],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
