a818cf4c770a845ee91ab0c65fb2077da14ca9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:06 +0000

    Add package objgraph/1.8.0/objgraph-1.8.0.tar.gz

---
+++ packages/objgraph/objgraph-1.8.0.tar.gz/objgraph-1.8.0/tests.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+import doctest
+import glob
+import os
+import re
+import sys
+import shutil
+import tempfile
+import unittest
+
+
+NODES_VARY = doctest.register_optionflag('NODES_VARY')
+RANDOM_OUTPUT = doctest.register_optionflag('RANDOM_OUTPUT')
+
+
+class RandomOutputChecker(doctest.OutputChecker):
+
+    def check_output(self, want, got, optionflags):
+        if optionflags & RANDOM_OUTPUT:
+            return True
+        return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
+
+class IgnoreNodeCountChecker(RandomOutputChecker):
+    _r = re.compile('\(\d+ nodes\)$', re.MULTILINE)
+
+    def check_output(self, want, got, optionflags):
+        if optionflags & NODES_VARY:
+            want = self._r.sub('(X nodes)', want)
+            got = self._r.sub('(X nodes)', got)
+        return RandomOutputChecker.check_output(self, want, got, optionflags)
+
+
+def setUp(test):
+    test.tmpdir = tempfile.mkdtemp(prefix='test-objgraph-')
+    test.prevdir = os.getcwd()
+    test.prevtempdir = tempfile.tempdir
+    tempfile.tempdir = test.tmpdir
+    os.chdir(test.tmpdir)
+    try:
+        next
+    except NameError:
+        # Python < 2.6 compatibility
+        test.globs['next'] = lambda it: it.next()
+
+
+def tearDown(test):
+    tempfile.tempdir = test.prevtempdir
+    os.chdir(test.prevdir)
+    shutil.rmtree(test.tmpdir)
+
+
+def find_doctests():
+    doctests = set(glob.glob('docs/*.txt'))
+    if sys.version_info >= (3, 4):
+        # Skip uncollectable.txt on Python 3.4 and newer
+        doctests.discard('docs/uncollectable.txt')
+    return sorted(doctests)
+
+
+def doctest_setup_py_works():
+    """Test that setup.py works
+
+        >>> import sys
+        >>> orig_argv = sys.argv
+        >>> sys.argv = ['setup.py', '--description']
+
+        >>> import setup
+        Draws Python object reference graphs with graphviz
+
+        >>> sys.argv = orig_argv
+
+    """
+
+
+def test_suite():
+    doctests = find_doctests()
+    return unittest.TestSuite([
+        doctest.DocFileSuite(setUp=setUp, tearDown=tearDown,
+                             optionflags=doctest.ELLIPSIS,
+                             checker=IgnoreNodeCountChecker(),
+                             *doctests),
+        doctest.DocTestSuite(),
+    ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
