5829f3a21cebd08c743a5312d9e9874a7b8cb234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:10 +0000

    Add package pyac/0.1.2/pyac-0.1.2.tar.gz

---
+++ packages/pyac/pyac-0.1.2.tar.gz/pyac-0.1.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='pyac',
+  version='0.1.2',
+  author='Kosta Harlan',
+  author_email='kosta@harlan.mayfirst.org',
+  url='https://github.com/kostajh/pyac',
+  packages=['pyac'],
+  license='LICENSE.txt',
+  description='A library for activeCollab 3.x/4.x',
+  long_description=open('README.md').read(),
+)
