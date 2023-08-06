d7bb89628922d716afed02135a21ebad32f0d4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:34 +0000

    Add package unicode_tr/0.6/unicode_tr-0.6.tar.gz

---
+++ packages/unicode_tr/unicode_tr-0.6.tar.gz/unicode_tr-0.6/unicode_tr/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf8 -*-
+
+
+class unicode_tr(unicode):
+    CHARMAP = {
+        "to_upper": {
+            u"ı": u"I",
+            u"i": u"İ",
+        },
+        "to_lower": {
+            u"I": u"ı",
+            u"İ": u"i",
+        }
+    }
+
+    def lower(self):
+        for key, value in self.CHARMAP.get("to_lower").items():
+            self = self.replace(key, value)
+
+        return self.lower()
+
+    def upper(self):
+        for key, value in self.CHARMAP.get("to_upper").items():
+            self = self.replace(key, value)
+
+        return self.upper()
+
+    def capitalize(self):
+        first, rest = self[0], self[1:]
+        return unicode_tr(first).upper() + unicode_tr(rest).lower()
+
+    def title(self):
+        return " ".join(map(lambda x: unicode_tr(x).capitalize(), self.split()))
