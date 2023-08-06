8aa5564d7fecf8e9e1236f8ce00e155b049b0552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:54 +0000

    Add package rambo/0.1.1.2.4/rambo-0.1.1.2.4.tar.gz

---
+++ packages/rambo/rambo-0.1.1.2.4.tar.gz/rambo-0.1.1.2.4/rambo/__main__.py
@@ -0,0 +1,3 @@
+import rambo
+
+rambo.main()
---
+++ packages/rambo/rambo-0.1.1.2.4.tar.gz/rambo-0.1.1.2.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='rambo',
+      version='0.1.1.2.4',
+      description='Ram your images into CSS sprites with Rambo',
+      long_description=readme(),
+      url='https://github.com/springload/rambo',
+      author='Springload',
+      author_email='josh@springload.co.nz',
+      license='MIT',
+      packages=['rambo'],
+      install_requires=['Pillow', 'argparse'],
+      scripts=['bin/rambo_sprites.sh', 'bin/rambo', 'bin/pypacker'],
+      zip_safe=False)
