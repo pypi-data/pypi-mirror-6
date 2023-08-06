bbe883fea6205e9a4dd659f7b99d60c1adfa0ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:00 +0000

    Add package Paperboy/1.0.0/Paperboy-1.0.0.tar.gz

---
+++ packages/Paperboy/Paperboy-1.0.0.tar.gz/Paperboy-1.0.0/setup.py
@@ -0,0 +1,56 @@
+from __future__ import with_statement
+import os.path
+import re
+import sys
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup
+
+
+def get_version():
+    dir_ = os.path.dirname(os.path.abspath(__file__))
+    version_pattern = r'''__version__ *\= *['"]([\d.]+)['"]'''
+    with open(os.path.join(dir_, 'dispatch', '__init__.py')) as init:
+        match = re.search(version_pattern, init.read())
+    return match.group(1)
+
+
+def get_long_description():
+    try:
+        return open('README.txt').read()
+    except IOError:
+        return None
+
+
+def get_py_modules():
+    """Don't include ez_setup on install."""
+    if len(sys.argv) >= 2 and sys.argv[1] == 'install':
+        return []
+    return ['ez_setup']
+
+
+setup(
+    name="Paperboy",
+    description="A simple fork of django.dispatch for use as a standalone "
+                "PubSub library.",
+    long_description=get_long_description(),
+    version=get_version(),
+    py_modules=get_py_modules(),
+    packages=['dispatch'],
+    install_requires=['six>=1.4.1'],
+    test_suite='tests',
+    maintainer="Jesse London",
+    maintainer_email="jesse@edgeflip.com",
+    url="http://github.com/edgeflip/dispatch",
+    license='BSD',
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ),
+)
