54be0d962a8a47460e012b5100d5a9580956602d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package csscompressor/0.9.2/csscompressor-0.9.2.tar.gz

---
+++ packages/csscompressor/csscompressor-0.9.2.tar.gz/csscompressor-0.9.2/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+
+
+extra = {}
+f = open('README.rst', 'r')
+try:
+    extra['long_description'] = f.read()
+finally:
+    f.close()
+
+
+deps = []
+try:
+    import argparse
+except ImportError:
+    deps.append('argparse')
+
+
+setup(
+    name='csscompressor',
+    version='0.9.2',
+    url='http://github.com/sprymix/csscompressor',
+    license='BSD',
+    author='Yury Selivanov',
+    author_email='info@sprymix.com',
+    description='A python port of YUI CSS Compressor',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Text Processing :: General',
+        'Topic :: Utilities'
+    ],
+    packages=['csscompressor', 'csscompressor.tests'],
+    install_requires=deps,
+    **extra
+)
