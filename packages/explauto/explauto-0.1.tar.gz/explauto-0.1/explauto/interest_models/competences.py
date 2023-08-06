2f8bdaa3f6f5f27a31bdeb3488539b6b86ecdefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:56 +0000

    Add package explauto/0.1/explauto-0.1.tar.gz

---
+++ packages/explauto/explauto-0.1.tar.gz/explauto-0.1/explauto/interest_models/competences.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def competence_exp(target, reached):
+    return np.exp(min(-0.1, -np.linalg.norm(target-reached)))
+
+def competence_bool(target, reached):
+    return float((target == reached).all())
