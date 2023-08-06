61cd311ae359bab52c501bcbe0044486c256f92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:18 +0000

    Add package penthu/0.1.1/penthu-0.1.1.tar.gz

---
+++ packages/penthu/penthu-0.1.1.tar.gz/penthu-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='penthu',
+    version='0.1.1',
+    packages=find_packages(),
+    url='https://github.com/justinabrahms/imhotep',
+    license='MIT',
+    author='Justin Abrahms',
+    author_email='justin@abrah.ms',
+    description='A tool to pipe linters into code review',
+    install_requires=['requests==2.2.1'],
+    extras_require={'pylint': 'PyLint'},
+    entry_points = {
+        'console_scripts': [
+            'imhotep = imhotep.main:main',
+        ],
+    }
+)
