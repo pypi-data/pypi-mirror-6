4eb872c82483216ce811c7ce7d894ec6094c2530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:20 +0000

    Add package neste20140402r/1.0/neste20140402r-1.0.zip

---
+++ packages/neste20140402r/neste20140402r-1.0.zip/neste20140402r-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'neste20140402r',
+    version = '1.0',
+    py_modules = ['nester20140402'],
+    author = 'wbbw1990',
+    author_email = 'wbbw199003@gmail.com',
+    url = 'www.seu,edu,cn',
+    )
