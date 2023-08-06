2ca524951aae79097ba127cdc2bbcd26fb69f8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:01 +0000

    Add package decorated/1.4.1/decorated-1.4.1.tar.gz

---
+++ packages/decorated/decorated-1.4.1.tar.gz/decorated-1.4.1/decorated/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from decorated.base.function import Function, partial
+from decorated.decorators.conditional import Conditional
+from decorated.decorators.events import Event
+from decorated.decorators.instantiate import Instantiate
+from decorated.decorators.once import Once, OnceSession
+from decorated.decorators.remove_extra_args import RemoveExtraArgs
+from decorated.decorators.retries import Retries
+from decorated.decorators.synchronized import Synchronized
+
+Function = Function
+partial = partial
+
+Event = Event
+conditional = Conditional
+instantiate = Instantiate
+once = Once
+OnceSession = OnceSession
+remove_extra_args = RemoveExtraArgs
+retries = Retries
+synchronized = Synchronized
