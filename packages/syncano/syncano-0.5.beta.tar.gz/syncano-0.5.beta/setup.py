e774845d1e4a8be45c609688af460acbefa429af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:47 +0000

    Add package syncano/0.5.beta/syncano-0.5.beta.tar.gz

---
+++ packages/syncano/syncano-0.5.beta.tar.gz/syncano-0.5.beta/setup.py
@@ -0,0 +1,18 @@
+# coding=utf8
+
+from distutils.core import setup
+
+setup(name='syncano',
+      version='0.5.beta',
+      description='Python Library for syncano.com api',
+      author=u'Piotr Czesław Kalmus',
+      author_email='piotr.kalmus@syncano.com',
+      url='http://syncano.com',
+      packages=['syncano',],
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: MIT License',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.3'],
+    )
+
