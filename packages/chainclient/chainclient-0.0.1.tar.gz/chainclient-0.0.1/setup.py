e8979ab69e8a8733f2aa069c0ea9025b7a255bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:48 +0000

    Add package chainclient/0.0.1/chainclient-0.0.1.tar.gz

---
+++ packages/chainclient/chainclient-0.0.1.tar.gz/chainclient-0.0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='chainclient',
+    version='0.0.1',
+    py_modules=['chainclient'],
+    author='Spencer Russell',
+    author_email='sfr@mit.edu',
+    url='http://github.com/ssfrr/chainclient.py',
+    license='MIT',
+    install_requires=['requests'],
+)
