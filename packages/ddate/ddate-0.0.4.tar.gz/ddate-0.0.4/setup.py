331f4a9d0d99e0188c1a8774c06890ef6af1193d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:20 +0000

    Add package ddate/0.0.4/ddate-0.0.4.tar.gz

---
+++ packages/ddate/ddate-0.0.4.tar.gz/ddate-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name="ddate",
+    version="0.0.4",
+    author="Adam Talsma",
+    author_email="adam@talsma.ca",
+    packages=["ddate"],
+    scripts=["bin/ddate"],
+    url="https://github.com/a-tal/ddate",
+    description="Discordian date",
+    long_description=(
+        "Converts and prints time using the Discordian calendar"
+    ),
+    download_url="https://github.com/a-tal/ddate",
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    license="BSD",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+    ],
+)
