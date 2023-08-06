583a72ef5e277c98c9d92a66c23c54f6d1e65465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:30 +0000

    Add package plone.app.relationfield/1.2.2/plone.app.relationfield-1.2.2.zip

---
+++ packages/plone.app.relationfield/plone.app.relationfield-1.2.2.zip/plone.app.relationfield-1.2.2/plone/app/relationfield/behavior.py
@@ -0,0 +1,31 @@
+from zope.interface import alsoProvides, Interface
+
+from z3c.relationfield.schema import RelationChoice, RelationList
+
+from plone.supermodel.interfaces import FIELDSETS_KEY
+from plone.supermodel.model import Fieldset
+
+from plone.autoform.interfaces import IFormFieldProvider
+
+try:
+    from plone.app.dexterity import MessageFactory as _
+except ImportError:
+    _ = unicode
+
+
+class IRelatedItems(Interface):
+    """Behavior interface to make a Dexterity type support related items.
+    """
+
+    relatedItems = RelationList(
+        title=_(u'label_related_items', default=u'Related Items'),
+        default=[],
+        value_type=RelationChoice(title=u"Related",
+                                  vocabulary="plone.app.vocabularies.Catalog"),
+        required=False,
+        )
+
+fieldset = Fieldset('categorization', label=_(u'Categorization'), fields=['relatedItems'])
+IRelatedItems.setTaggedValue(FIELDSETS_KEY, [fieldset])
+
+alsoProvides(IRelatedItems, IFormFieldProvider)
