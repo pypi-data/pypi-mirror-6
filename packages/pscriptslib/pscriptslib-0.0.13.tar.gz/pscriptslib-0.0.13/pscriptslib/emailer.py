021a4ce40d4097a77c9b0a28eec4d184e0f309c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:06 +0000

    Add package pscriptslib/0.0.13/pscriptslib-0.0.13.tar.gz

---
+++ packages/pscriptslib/pscriptslib-0.0.13.tar.gz/pscriptslib-0.0.13/pscriptslib/emailer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import smtplib, logging as log 
+from email.mime.text import MIMEText
+import pdb
+
+def send_mail(server,sender,recipients,message,subject="",password="",port=587):
+    # pdb.set_trace()
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587
+    msg = MIMEText(message, 'html')
+    msg.add_header('Subject', subject)
+    msg.add_header('From', sender)
+    msg.add_header('MIME-Version', "1.0")
+    msg.add_header('Content-Type', "text/html")
+    for recipient in recipients:
+        msg.add_header('To', recipient)
+    session = smtplib.SMTP(server, port)
+    session.ehlo()
+    session.starttls()
+    session.ehlo
+    session.login(sender, password)
+    to = msg.get_all("To")
+    session.sendmail(sender, to, msg.as_string())
+    session.quit()
+
+def _test():
+    log.debug("test message")
+    server = "smtp.gmail.com"
+    port = 587
+    sender = "shared.house.spicevan@gmail.com"
+    password = "gumnuts8"
+    subject = "hi"
+    message = "<h1>hello world</h1>"
+
+    # this can be a comma separated list
+    # of emails as one long string
+    recipients = ["shared.housing.spicevan@gmail.com","fenton.travers@gmail.com"]
+
+    send_mail(server, sender, recipients, message, subject, password, port)
+    
+if __name__ == '__main__':
+    _test()
---
+++ packages/pscriptslib/pscriptslib-0.0.13.tar.gz/pscriptslib-0.0.13/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+requires = [
+    'some_package_you_require',    
+    'another_package_you_require',
+]
+classifiers=[
+    "Programming Language :: Python",
+    "Framework :: Pyramid",
+    "Topic :: Internet :: WWW/HTTP",
+    "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+]
+version='0.0.13'
+name='pscriptslib'
+setup(name=name,
+      version=version,
+      description='Library for pscripts',
+      author='Fenton Travers',
+      author_email='fenton.travers@gmail.com',
+      url='http://www.python.org/pscripts-lib/', # url of project
+      packages=[name],
+      classifiers=classifiers
+     )
