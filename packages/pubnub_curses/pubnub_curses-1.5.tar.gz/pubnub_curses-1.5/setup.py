30bec630dd31d6aaa8ea4ee772f6062d7ac935b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:19 +0000

    Add package pubnub_curses/1.5/pubnub_curses-1.5.tar.gz

---
+++ packages/pubnub_curses/pubnub_curses-1.5.tar.gz/pubnub_curses-1.5/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+def read(fname):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+
+setup(
+  name='pubnub_curses',
+  version='1.5',
+  description='PubNub Curses Terminal Client',
+  long_description=read('README.rst'),
+  license=read('LICENSE'),
+  author='Dan Ristic',
+  author_email='danr@pubnub.com',
+  url='http://pubnub.com',
+  packages=['pubnub_curses'],
+  scripts=['bin/pubnub-curses']
+)
