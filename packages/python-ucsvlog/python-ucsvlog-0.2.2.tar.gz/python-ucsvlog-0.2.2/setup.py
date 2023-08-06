4ec11a4aedb383b9c3950b4c25b19beb6649a73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:43 +0000

    Add package python-ucsvlog/0.2/python-ucsvlog-0.2.2.tar.gz

---
+++ packages/python-ucsvlog/python-ucsvlog-0.2.2.tar.gz/python-ucsvlog-0.2.2/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-ucsvlog',
+      version='0.2.2',
+      description='logging in ucsv format',
+      author='Alexander Lyabah',
+      author_email='alexander@lyabah.com',
+      packages=['ucsvlog', 'ucsvlog.fields'],
+      requires=['six'],
+      url='https://github.com/oduvan/python-ucsvlog',
+      package_dir={'ucsvlog': 'ucsvlog', 'ucsvlog.fields': 'ucsvlog/fields'}
+      )
