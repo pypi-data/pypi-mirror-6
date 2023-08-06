2e616d7e9e7a776fa43fd0aedf075470dd28b1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:03 +0000

    Add package dogs/1.0/dogs-1.0.tar.gz

---
+++ packages/dogs/dogs-1.0.tar.gz/dogs-1.0/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dogs',
+      version='1.0',
+      description='just for test',
+      author='ted liu',
+      packages=['dogs'],
+     )
