30be21527d2611c093a54cef6843f96c79ecee88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:41 +0000

    Add package convert/0.1/convert-0.1.tar.gz

---
+++ packages/convert/convert-0.1.tar.gz/convert-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+       name         = 'convert',
+       version      = '0.1',
+       py_modules   = ['convert'],
+       author       = "newcseanc",
+       author_email = "newcseanc@gmail.com",
+       description  = "simple multiple-purposes converter",
+       )
