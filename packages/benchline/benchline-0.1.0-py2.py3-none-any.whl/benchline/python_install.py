a1023c6a1c3ff29f068a3905d384cbb273a61f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:44 +0000

    Add package benchline/0.1.0/benchline-0.1.0-py2.py3-none-any.whl

---
+++ packages/benchline/benchline-0.1.0-py2.py3-none-any.whl/benchline/python_install.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+#
+# Author: Paul D. Eden <paul@benchline.org>
+# Created: 2014-03-23
+"""
+Installs the changes to a python project to the local machine.
+
+Just a convenience script.
+"""
+import benchline.args
+import benchline.command
+
+
+def validate_args(parser, options, args):
+    pass
+
+
+def main():
+    benchline.args.go(__doc__, validate_args=validate_args)
+    benchline.command.run("sudo python3 setup.py install")
+    benchline.command.run("sudo python setup.py install")
+
+
+if __name__ == "__main__":
+    main()
