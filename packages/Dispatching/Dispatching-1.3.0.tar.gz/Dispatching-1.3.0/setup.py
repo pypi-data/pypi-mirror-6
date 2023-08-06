2b72af6e1e50ca063b31b006ca0f7bc7f8b82206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:55 +0000

    Add package Dispatching/1.3.0/Dispatching-1.3.0.tar.gz

---
+++ packages/Dispatching/Dispatching-1.3.0.tar.gz/Dispatching-1.3.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst', 'markdown')
+except (ImportError, FileNotFoundError, RuntimeError):
+    #ImportError: pypandoc failed to import
+    #FileNotFoundError: couldn't launch pandoc
+    #RuntimeError: failed to convert
+    long_description = open('README.md').read()
+
+setup(
+    name="Dispatching",
+    version="1.3.0",
+    py_modules=['dispatching',],
+    test_suite='test',
+    platforms='any',
+
+    author="Nathan West",
+    description="A library for overloading python functions",
+    long_description=long_description,
+    license="LGPLv3",
+    url="https://github.com/Lucretiel/Dispatch",
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
