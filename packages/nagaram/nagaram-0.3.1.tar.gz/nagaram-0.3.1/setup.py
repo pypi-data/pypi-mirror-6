1875f9406c1c934f1a6e3cafa61e8bd51f919e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:30 +0000

    Add package nagaram/0.3.1/nagaram-0.3.1.tar.gz

---
+++ packages/nagaram/nagaram-0.3.1.tar.gz/nagaram-0.3.1/setup.py
@@ -0,0 +1,36 @@
+"""Setup.py for nagaram."""
+
+
+from setuptools import setup
+
+
+setup(
+    name='nagaram',
+    version='0.3.1',
+    author='Adam Talsma',
+    author_email='adam@talsma.ca',
+    data_files=[
+        ('/usr/share/nagaram', ['wordlists/twl.txt', 'wordlists/sowpods.txt']),
+    ],
+    packages=['nagaram'],
+    provides=['nagaram'],
+    install_requires=['argparse'],
+    scripts=['bin/nagaram'],
+    url='https://github.com/a-tal/nagaram',
+    description='Scrabble anagram finder',
+    long_description=(
+        "Scrabble anagram finder. Returns words based on Scrabble score. Works"
+        " with both Tournament Words List and SOWPODS."
+    ),
+    download_url='https://github.com/a-tal/nagaram',
+    license="BSD",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        "License :: OSI Approved :: BSD License",
+        'Operating System :: OS Independent',
+        'Topic :: Games/Entertainment',
+        'Topic :: Utilities',
+    ],
+)
