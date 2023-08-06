24845e643a5d00e7d296ece6357863a012576808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:47 +0000

    Add package mcash-mapi-client/0.1.1/mcash-mapi-client-0.1.1.tar.gz

---
+++ packages/mcash-mapi-client/mcash-mapi-client-0.1.1.tar.gz/mcash-mapi-client-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+VERSION = "0.1.1"
+
+setup(
+    name="mcash-mapi-client",
+    version=VERSION,
+    description="Thin python wrapper around mCASH's merchant api",
+    author="mCASH Norge AS",
+    author_email="sm@mcash.no",
+    license="MIT",
+    url="https://github.com/mcash/merchant-api-python-sdk",
+    install_requires=["pycrypto>=2.6",
+                      "requests>=2.2.1",
+                      "voluptuous>=0.8.4",
+                      "websocket-client>=0.12.0",
+                      "wsgiref>=0.1.2"],
+    extras_require={
+        'mapi_client_example':  ["pusherclient>=0.2.0"]
+    },
+    packages=["mcash.mapi_client", "mcash"],
+    namespace_packages=['mcash']
+)
