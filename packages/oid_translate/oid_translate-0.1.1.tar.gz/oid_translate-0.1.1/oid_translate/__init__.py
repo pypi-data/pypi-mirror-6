fb0041c3611bf4116c39bcddef286c24c98ba6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:29 +0000

    Add package oid_translate/0.1.1/oid_translate-0.1.1.tar.gz

---
+++ packages/oid_translate/oid_translate-0.1.1.tar.gz/oid_translate-0.1.1/oid_translate/__init__.py
@@ -0,0 +1,19 @@
+import _oid_translate
+
+OID_OUTPUT_DEFAULT = 0
+OID_OUTPUT_SUFFIX = 1
+OID_OUTPUT_MODULE = 2
+OID_OUTPUT_FULL = 3
+OID_OUTPUT_NUMERIC = 4
+OID_OUTPUT_UCD = 5
+OID_OUTPUT_NONE = 6
+
+
+def init_mibs():
+    _oid_translate.init()
+
+
+def translate(oid, output_type=OID_OUTPUT_DEFAULT):
+    if oid.endswith("."):
+        oid = oid[:-1]
+    return _oid_translate.translate(oid, output_type)
