b14e0e299389e8c0e3a95c3bf67e05d53d28a0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:38 +0000

    Add package phabricator-python/0.1.5/phabricator-python-0.1.5.tar.gz

---
+++ packages/phabricator-python/phabricator-python-0.1.5.tar.gz/phabricator-python-0.1.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as infile:
+    long_description = infile.read()
+
+setup(
+    name='phabricator-python',
+    version='0.1.5',
+    author='Mohit Kanwal',
+    author_email='mohit.kanwal@gmail.com',
+    url='http://github.com/creativepsyco/python-phabricator',
+    description='Python Based Phabricator API Bindings',
+    long_description=long_description,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=['requests'],
+    license='GPL v2',
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
