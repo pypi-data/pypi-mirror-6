8345240bd658f8ec6658b630a7947d23df1f1758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:58 +0000

    Add package knun_movies/1.2.0/knun_movies-1.2.0.zip

---
+++ packages/knun_movies/knun_movies-1.2.0.zip/knun_movies-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name = 'knun_movies',
+		version = '1.2.0',
+		py_modules = ['movies'],
+		author = 'knun',
+		author_email = 'nero031547@gmail.com',
+		url = 'http://www.headfirstlabs.com',
+		description = 'A simple printer of movies lists',
+	)
