b99bd12e61bf3b710c9d420831305475490cf54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:29 +0000

    Add package python-remote-pdb/0.0/python-remote-pdb-0.0.tar.gz

---
+++ packages/python-remote-pdb/python-remote-pdb-0.0.tar.gz/python-remote-pdb-0.0/setup.py
@@ -0,0 +1,16 @@
+# encoding: utf8
+
+from setuptools import setup
+
+setup(
+    name='python-remote-pdb',
+    version="0.0",
+    url='https://pypi.python.org/pypi/remote-pdb/',
+    description='Remote vanilla PDB (over TCP sockets).',
+    long_description='''Use `remote-pdb <https://pypi.python.org/pypi/remote-pdb/>`_ instead.''',
+    author='Ionel Cristian M\xc4\x83rie\xc8\x99',
+    platforms=['all'],
+    zip_safe=False,
+    author_email='contact@ionelmc.ro',
+    install_requires=['remote-pdb'],
+)
