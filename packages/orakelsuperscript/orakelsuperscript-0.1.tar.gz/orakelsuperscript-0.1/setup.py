6051da10ab46c1e9341b70b37be6ed48b3c2ab4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:57 +0000

    Add package orakelsuperscript/0.1/orakelsuperscript-0.1.tar.gz

---
+++ packages/orakelsuperscript/orakelsuperscript-0.1.tar.gz/orakelsuperscript-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='orakelsuperscript',  # This is the name of your PyPI-package.
+    version='0.1',  # Update the version number for new releases
+    scripts=['orakelsuperscript']  # The name of your scipt, and also the command you'll be using for calling it
+)
