acaca221bb8b4d5066174c20f4a55b919867700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:59 +0000

    Add package root-numpy/3.2.0/root_numpy-3.2.0.tar.gz

---
+++ packages/root-numpy/root_numpy-3.2.0.tar.gz/root_numpy-3.2.0/root_numpy/src/_librootnumpy.pyx
@@ -0,0 +1,9 @@
+# cython: experimental_cpp_class_def=True
+include "setup.pxi"
+include "tree.pyx"
+include "array.pyx"
+include "matrix.pyx"
+include "sample.pyx"
+include "hist.pyx"
+include "graph.pyx"
+include "innerjoin.pyx"
