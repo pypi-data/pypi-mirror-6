4c1a0d3cd0a817fce6c400ccdc7223d325cb4b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:14 +0000

    Add package aerofiles/0.1.0/aerofiles-0.1.0-py2.py3-none-any.whl

---
+++ packages/aerofiles/aerofiles-0.1.0-py2.py3-none-any.whl/aerofiles/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+from .errors import *
+
+from .seeyou import (
+    Reader as SeeYouReader,
+    Converter as SeeYouConverter,
+)
+
+from .welt2000 import (
+    Reader as Welt2000Reader,
+    Converter as Welt2000Converter,
+)
