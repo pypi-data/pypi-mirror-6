966141c1416591fdcc6906efcde45ea695f6cae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package burlap/0.1.2/burlap-0.1.2.tar.gz

---
+++ packages/burlap/burlap-0.1.2.tar.gz/burlap-0.1.2/burlap/service.py
@@ -0,0 +1,108 @@
+import os
+import re
+
+from fabric.api import (
+    env,
+    local,
+    put as _put,
+    require,
+    #run as _run,
+    run,
+    settings,
+    sudo,
+    cd,
+    task,
+)
+
+from fabric.contrib import files
+from fabric.tasks import Task
+
+from burlap import common
+from burlap.common import (
+    #run,
+    put,
+    SITE,
+    ROLE,
+)
+
+@task
+def configure():
+    """
+    Applies one-time settings changes to the host, usually to initialize the service.
+    """
+    print 'env.services:',env.services
+    for service in list(env.services):
+        service = service.strip().upper()
+        funcs = common.service_configurators.get(service, [])
+        if funcs:
+            print '!'*80
+            print 'Configuring service %s...' % (service,)
+            for func in funcs:
+                print 'Function:',func
+                func()
+
+@task
+def pre_deploy():
+    """
+    Runs methods services have requested be run before each deployment.
+    """
+    for service in env.services:
+        service = service.strip().upper()
+        funcs = common.service_pre_deployers.get(service)
+        if funcs:
+            print 'Running pre-deployments for service %s...' % (service,)
+            for func in funcs:
+                func()
+                
+@task
+def deploy():
+    """
+    Applies routine, typically application-level changes to the service.
+    """
+    for service in env.services:
+        service = service.strip().upper()
+        funcs = common.service_deployers.get(service)
+        if funcs:
+            print 'Deploying service %s...' % (service,)
+            for func in funcs:
+                func()
+
+@task
+def post_deploy():
+    """
+    Runs methods services have requested be run before after deployment.
+    """
+    for service in env.services:
+        service = service.strip().upper()
+        funcs = common.service_post_deployers.get(service)
+        if funcs:
+            print 'Running post-deployments for service %s...' % (service,)
+            for func in funcs:
+                func()
+
+@task
+def restart():
+    for service in env.services:
+        service = service.strip().upper()
+        funcs = common.service_restarters.get(service)
+        if funcs:
+            print 'Restarting service %s...' % (service,)
+            for func in funcs:
+                func()
+
+@task
+def stop():
+    for service in env.services:
+        service = service.strip().upper()
+        funcs = common.service_stoppers.get(service)
+        if funcs:
+            print 'Restarting service %s...' % (service,)
+            for func in funcs:
+                func()
+                
+def is_selected(name):
+    name = name.strip().upper()
+    for service in env.services:
+        if service.strip().upper() == name:
+            return True
+    return False
---
+++ packages/burlap/burlap-0.1.2.tar.gz/burlap-0.1.2/setup.py
@@ -0,0 +1,32 @@
+ 
+from setuptools import setup, find_packages, Command
+
+import os
+
+setup(
+    name = "burlap",
+    version = '0.1.2',
+    packages = find_packages(),
+    scripts = ['bin/burlap'],
+    package_data = {
+        'burlap': [
+            'templates/*.*',
+        ],
+    },
+    author = "Chris Spencer",
+    author_email = "chrisspen@gmail.com",
+    description = "Fabric commands for simplifying Django deployment",
+    license = "LGPL",
+    url = "https://github.com/chrisspen/burlap",
+    #https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    install_requires = ['fabric', 'pyyaml'],
+)
