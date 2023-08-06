ed68b92273e99c3f08b490ec499453a9205d01e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:14 +0000

    Add package plone.subrequest/1.6.8/plone.subrequest-1.6.8.zip

---
+++ packages/plone.subrequest/plone.subrequest-1.6.8.zip/plone.subrequest-1.6.8/setup.py
@@ -0,0 +1,50 @@
+import os.path
+from setuptools import setup, find_packages
+
+version = '1.6.8'
+
+setup(
+    name = 'plone.subrequest',
+    version = version,
+    description = 'Subrequests for Zope2',
+    long_description=open("README.txt").read() + "\n\n" +
+        open(os.path.join('plone', 'subrequest', 'usage.txt')).read() + "\n\n" +
+        open("CHANGES.txt").read(),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    keywords='plone',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.subrequest/',
+    license='GPL version 2',
+    packages = find_packages(),
+    namespace_packages = ['plone'],
+    include_package_data = True,
+    platforms = 'Any',
+    zip_safe = False,
+    install_requires=[
+        # 'Acquisition',
+        'five.globalrequest',
+        'setuptools',
+        'zope.globalrequest',
+        ],
+    extras_require = {
+        'test': [
+            'five.localsitemanager',
+            'manuel',
+            'plone.testing [z2]',
+            'plone.app.blob',
+            ],
+        },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
