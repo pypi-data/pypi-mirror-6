44267cfa4f606ddb54e586102781b0f39a1b0385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:00 +0000

    Add package SQLAlchemyUtils/0.0.1/SQLAlchemyUtils-0.0.1.tar.gz

---
+++ packages/SQLAlchemyUtils/SQLAlchemyUtils-0.0.1.tar.gz/SQLAlchemyUtils-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+setup(
+    name='SQLAlchemyUtils',
+    description='Useful utilities for working with SQLAlchemy.',
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+    maintainer='Tyler Treat',
+    maintainer_email='ttreat31@gmail.com'
+)
+
