9e207ded1e264319583ae7037ea24b031443a65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:03 +0000

    Add package Flask-Captcha/0.1.5/Flask_Captcha-0.1.5-py3.3.egg

---
+++ packages/Flask-Captcha/Flask_Captcha-0.1.5-py3.3.egg/flask_captcha/__init__.py
@@ -0,0 +1,21 @@
+import re
+from flask import current_app
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.captcha.models import db, CaptchaStore
+
+VERSION = (0, 1, 4)
+
+class Captcha(object):
+    ext_db = None
+
+    def __init__(self, app=None):
+        self.app = app
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        with app.app_context():
+            self.ext_db = current_app.extensions['sqlalchemy'].db
+            active_metadata = self.ext_db.metadata
+            CaptchaStore.__table__ = CaptchaStore.__table__.tometadata(active_metadata)
+
