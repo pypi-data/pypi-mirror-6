01419d2c4bba6cbdd09a72f753c4c104b8181958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:57 +0000

    Add package django-OneTimePassword/0.0.2/django-OneTimePassword-0.0.2.tar.gz

---
+++ packages/django-OneTimePassword/django-OneTimePassword-0.0.2.tar.gz/django-OneTimePassword-0.0.2/django_OneTimePassword/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+#from django.contrib.auth import get_user_model
+#from django_OneTimePassword.conf import OTP_TIME_SYNCHRONIZED
+
+
+#class OneTimePasswordTestCase(TestCase):
+#    def setUp(self):
+#        user_model = get_user_model()
+#        kwargs = {user_model.USERNAME_FIELD: "admin"}
+#        user_model.objects.create(**kwargs)
+#
+#    def test_sha512TS_gen_user_seed(self):
+#        """
+#        Tests seed generate by sha512TS.
+#        """
