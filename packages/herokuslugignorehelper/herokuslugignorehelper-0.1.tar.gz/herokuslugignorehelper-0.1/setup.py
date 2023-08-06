3090a3ee8ae085efc8efe653a5987a1ce1dba803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:19 +0000

    Add package herokuslugignorehelper/0.1/herokuslugignorehelper-0.1.tar.gz

---
+++ packages/herokuslugignorehelper/herokuslugignorehelper-0.1.tar.gz/herokuslugignorehelper-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "herokuslugignorehelper",
+    version = "0.1",
+    license = 'BSD',
+    url = 'https://github.com/espenak/herokuslugignorehelper',
+    author = 'Espen Angell Kristiansen',
+    packages = find_packages(exclude=['ez_setup']),
+    include_package_data = True,
+    long_description = 'See https://github.com/espenak/herokuslugignorehelper',
+    zip_safe = True,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'herokuslugignorehelper = herokuslugignorehelper.cli:cli',
+        ],
+    }
+)
