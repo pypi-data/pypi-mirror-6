2d966676882f2308a526309306b157efc60d5096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:45 +0000

    Add package web.py-modules/v0.1/web.py-modules-v0.1.tar.gz

---
+++ packages/web.py-modules/web.py-modules-v0.1.tar.gz/web.py-modules-v0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='web.py-modules',
+    packages=['webmod', 'test'],
+    version='v0.1',
+    author='Petr Horacek',
+    author_email='p.horacek94@gmail.com',
+    url='https://github.com/PetrHoracek/webpy-modules',
+    download_url = 'https://github.com/PetrHoracek/webpy-modules/archive/v0.1.tar.gz',
+    license='MIT License',
+    description='Optional modules for web.py framework.',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "web.py >= 0.37"
+    ],
+    platforms=["any"],
+)
