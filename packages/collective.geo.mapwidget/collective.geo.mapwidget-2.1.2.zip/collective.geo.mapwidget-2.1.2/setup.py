f4185523894d5ed018f6372b988802ff1210977b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:32 +0000

    Add package collective.geo.mapwidget/2.1.2/collective.geo.mapwidget-2.1.2.zip

---
+++ packages/collective.geo.mapwidget/collective.geo.mapwidget-2.1.2.zip/collective.geo.mapwidget-2.1.2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os
+
+version = '2.1.2'
+
+setup(name='collective.geo.mapwidget',
+      version=version,
+      description="collective.geo mapwidget",
+      long_description=open(
+          "README.rst").read() + "\n" + open(
+              os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+          "Framework :: Plone",
+          "Topic :: Internet",
+          "Topic :: Scientific/Engineering :: GIS",
+          "Programming Language :: Python",
+      ],
+      keywords='Zope Plone GIS KML Google Maps Bing OpenLayers',
+      author='Giorgio Borelli',
+      author_email='giorgio@giorgioborelli.it',
+      url='https://github.com/collective/collective.geo.mapwidget',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['collective', 'collective.geo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'BeautifulSoup',
+          'geopy>=0.98',
+          'Products.CMFCore',
+          'plone.app.z3cform',
+          'collective.geo.openlayers >= 3.1',
+          'collective.geo.settings >= 3.0',
+          'collective.z3cform.colorpicker >= 1.1',
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+          ]
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
