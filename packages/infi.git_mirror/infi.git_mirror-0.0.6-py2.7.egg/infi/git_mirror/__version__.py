5d4d8d203ea2245c55727dc2938601f405cdc9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:37 +0000

    Add package infi.git_mirror/0.0.6/infi.git_mirror-0.0.6-py2.7.egg

---
+++ packages/infi.git_mirror/infi.git_mirror-0.0.6-py2.7.egg/infi/git_mirror/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.6"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.0.6)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '31612dbc108ce274cef7d68b1cad0bc3b767fa65'
+__git_head_subject__ = 'buildout.cfg: remove gitpy from requirements'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
