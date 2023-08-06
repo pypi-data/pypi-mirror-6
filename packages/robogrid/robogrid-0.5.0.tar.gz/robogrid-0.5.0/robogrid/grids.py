f90ed64b8269c192d2ec4c404f15a447be090f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:23 +0000

    Add package robogrid/0.5.0/robogrid-0.5.0.tar.gz

---
+++ packages/robogrid/robogrid-0.5.0.tar.gz/robogrid-0.5.0/robogrid/grids.py
@@ -0,0 +1,37 @@
+class Grid(dict):
+    def __init__(self, grid):
+        self.width, self.height = len(grid), len(grid[0])
+        for row_index, row in enumerate(grid):
+            for col_index, cell in enumerate(row):
+                self[(row_index, col_index)] = cell
+
+    def __iter__(self):
+        for row in range(self.height):
+            yield [self[row, col] for col in range(self.width)]
+
+    def __str__(self):
+        return "\n".join("".join(self.char(col) for col in row) for row in self)
+
+    def char(self, state):
+        return "#" if state else "."
+
+    def free_position(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                if self[x, y] == False:
+                    return x, y
+        return None
+
+class Simple_Grid(Grid):
+    def __init__(self, size):
+        grid = self.bordered_grid(size)
+        super(Simple_Grid, self).__init__(grid)
+
+    def bordered_grid(self, size):
+        grid = []
+        border = [True] * size
+        grid.append(border)
+        for i in range(size - 2):
+            grid.append([i == 0 or i == size-1 for i in range(size)])
+        grid.append(border)
+        return grid
---
+++ packages/robogrid/robogrid-0.5.0.tar.gz/robogrid-0.5.0/robogrid/robot.py
@@ -0,0 +1,93 @@
+from grids import Simple_Grid
+
+class Robot(object):
+    def __init__(self, name, grid=None):
+        self.name = name
+
+        if grid == None:
+            grid = Simple_Grid(20)
+        self.grid = grid
+
+        start_pos = self.grid.free_position()
+        if start_pos == None:
+            raise ValueError("No space in proposed grid")
+
+        self._heading = 0
+        self._x, self._y = start_pos
+
+    def __repr__(self):
+        summary = {
+            "name": self.name,
+            "w": self.grid.width,
+            "h": self.grid.height
+        }
+        return 'Robot("{name}", <Grid of size {w}x{h}>)'.format(**summary)
+
+    def __str__(self):
+        arrow = "^>!<"[self.heading]
+        result = ""
+        for row_i, row in enumerate(self.grid):
+            for col_i, cell in enumerate(row):
+                if (row_i, col_i) == self.pos:
+                    result += arrow
+                else:
+                    result += self.grid.char(cell)
+            result += "\n"
+        return result
+
+    def forward(self):
+        if self.heading == 0:
+            if self.grid[self.x, self.y-1] == False:
+                self._y -= 1
+        elif self.heading == 1:
+            if self.grid[self.x+1, self.y] == False:
+                self._x += 1
+        elif self.heading == 2:
+            if self.grid[self.x, self.y+1] == False:
+                self._y += 1
+        elif self.heading == 3:
+            if self.grid[self.x-1, self.y] == False:
+                self._x -= 1
+
+    def can_move_forward(self):
+        if self.heading == 0:
+            return self.grid[self.x, self.y-1] == False
+        elif self.heading == 1:
+            return self.grid[self.x+1, self.y] == False
+        elif self.heading == 2:
+            return self.grid[self.x, self.y+1] == False
+        elif self.heading == 3:
+            return self.grid[self.x-1, self.y] == False
+
+    def backward(self):
+        self.right()
+        self.right()
+        self.forward()
+        self.right()
+        self.right()
+
+    def right(self):
+        self.heading += 1
+
+    def left(self):
+        self.heading -= 1
+
+    @property
+    def heading(self):
+        return self._heading
+
+    @heading.setter
+    def heading(self, val):
+        self._heading = val % 4
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def pos(self):
+        return self.x, self.y
