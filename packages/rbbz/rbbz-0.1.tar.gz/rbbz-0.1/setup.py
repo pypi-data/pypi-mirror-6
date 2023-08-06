0c5aa4940ee8e158726032a4fcb1145362d73a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:08 +0000

    Add package rbbz/0.1/rbbz-0.1.tar.gz

---
+++ packages/rbbz/rbbz-0.1.tar.gz/rbbz-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+PACKAGE = 'rbbz'
+VERSION = '0.1'
+
+setup(name=PACKAGE,
+      version=VERSION,
+      description='Review Board extension for Bugzilla support',
+      author='Mark Cote',
+      author_email='mcote@mozilla.com',
+      license='MPL 2.0',
+      packages=['rbbz'],
+      entry_points={
+        'reviewboard.extensions':
+            '%s = rbbz.extension:BugzillaExtension' % PACKAGE,
+        'reviewboard.auth_backends': [
+            'bugzilla = rbbz.auth:BugzillaBackend',
+        ],
+      }
+)
