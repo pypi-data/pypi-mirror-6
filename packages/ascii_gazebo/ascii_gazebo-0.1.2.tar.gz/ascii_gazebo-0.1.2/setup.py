f8f4ee5f33ceb1cd5434ec5bd86e50d2020ef97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:24 +0000

    Add package ascii_gazebo/0.1.2/ascii_gazebo-0.1.2.tar.gz

---
+++ packages/ascii_gazebo/ascii_gazebo-0.1.2.tar.gz/ascii_gazebo-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ascii_gazebo',
+    version='0.1.2',
+    packages=['ascii_gazebo',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
