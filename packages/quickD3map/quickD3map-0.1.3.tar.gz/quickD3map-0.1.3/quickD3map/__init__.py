b3a7b83887fa10f838887fd97878c0a1809a8450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:20 +0000

    Add package quickD3map/0.1.3/quickD3map-0.1.3.tar.gz

---
+++ packages/quickD3map/quickD3map-0.1.3.tar.gz/quickD3map-0.1.3/quickD3map/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'zach powers'
+__email__ = 'zcharlop@rockefeller.edu'
+__version__ = '0.1.2'
+
+
+from .PointMap  import PointMap
+from .MultiColumnMap import MultiColumnMap
