5d53d1e0e345b3e1cb175f8f4ef26666b538d371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:47 +0000

    Add package django-sae/0.1.4/django-sae-0.1.4.zip

---
+++ packages/django-sae/django-sae-0.1.4.zip/django-sae-0.1.4/django_sae/contrib/tasks/settings.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+from django.conf import settings
+
+
+def patch_root_urlconf():
+    from django.conf.urls import include, patterns, url
+    from django.core.urlresolvers import clear_url_caches, reverse, NoReverseMatch
+    from django.utils.importlib import import_module
+
+    try:
+        reverse('tasks:execute')
+    except NoReverseMatch:
+        urlconf_module = import_module(settings.ROOT_URLCONF)
+        urlconf_module.urlpatterns = patterns('',
+                                              url(r'^tasks/', include('django_sae.contrib.tasks.urls', 'tasks', 'tasks')),
+        ) + urlconf_module.urlpatterns
+        clear_url_caches()
