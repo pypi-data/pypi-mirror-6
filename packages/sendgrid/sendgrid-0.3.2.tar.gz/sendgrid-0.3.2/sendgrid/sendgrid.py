6c3c10c8098ecd1c35299d132404a247231dbf01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:08 +0000

    Add package sendgrid/0.3.2/sendgrid-0.3.2.tar.gz

---
+++ packages/sendgrid/sendgrid-0.3.2.tar.gz/sendgrid-0.3.2/sendgrid/sendgrid.py
@@ -0,0 +1,72 @@
+import sys
+from socket import timeout
+try:
+    import urllib.request as urllib_request
+    from urllib.parse import urlencode
+    from urllib.error import URLError
+except ImportError: # Python 2
+    import urllib2 as urllib_request
+    from urllib2 import URLError
+    from urllib import urlencode
+
+class SendGridClient(object):
+
+    """SendGrid API."""
+
+    def __init__(self, username, password, **opts):
+        """
+        Construct SendGrid API object.
+
+        Args:
+            username: SendGrid username
+            password: SendGrid password
+            user: Send mail on behalf of this user (web only)
+
+        """
+        self.username = username
+        self.password = password
+        self.mail_url = 'https://api.sendgrid.com/api/mail.send.json'
+
+        # urllib cannot connect to SSL servers using proxies
+        self.proxies = opts.get('proxies', None)
+
+    def _build_body(self, message):
+        values = {
+            'api_user': self.username,
+            'api_key': self.password,
+            'to[]': message.to,
+            'toname[]': message.to_name,
+            'bcc[]': message.bcc,
+            'from': message.from_email,
+            'fromname': message.from_name,
+            'subject': message.subject,
+            'text': message.text,
+            'html': message.html,
+            'replyto': message.reply_to,
+            'headers': message.headers,
+            'date': message.date,
+            'x-smtpapi': message.json_string()
+        }
+        for k in list(values.keys()):
+            if not values[k]:
+                del values[k]
+        for filename in message.files:
+            if message.files[filename]:
+                values['files[' + filename + ']'] = message.files[filename]
+        return values
+
+    def send(self, message):
+        try:
+            if self.proxies:
+                proxy_support = urllib_request.ProxyHandler(self.proxies)
+                opener = urllib_request.build_opener(proxy_support)
+                urllib_request.install_opener(opener)
+            data = urlencode(self._build_body(message), True).encode('utf-8')
+            req = urllib_request.Request(self.mail_url, data)
+            response = urllib_request.urlopen(req, timeout = 10)
+            body = response.read()
+            return response.getcode(), body
+        except URLError as e:
+            return e.code, e.read()
+        except timeout as e:
+            return 408, e
---
+++ packages/sendgrid/sendgrid-0.3.2.tar.gz/sendgrid-0.3.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sendgrid',
+    version='0.3.2',
+    author='Yamil Asusta',
+    author_email='yamil@sendgrid.com',
+    url='https://github.com/sendgrid/sendgrid-python/',
+    packages=find_packages(),
+    license='MIT',
+    description='SendGrid library for Python',
+    long_description=open('./README.rst').read(),
+    install_requires=[
+        'smtpapi'
+    ],
+)
