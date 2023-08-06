af55b5c7de1a181692273f2b735b431786a634b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:24 +0000

    Add package python-tds/1.7.0/python-tds-1.7.0.tar.gz

---
+++ packages/python-tds/python-tds-1.7.0.tar.gz/python-tds-1.7.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from distutils.core import setup
+
+requirements = list(open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), 'r').readlines())
+
+setup(name='python-tds',
+      version='1.7.0',
+      description='Python DBAPI driver for MSSQL using pure Python TDS (Tabular Data Stream) protocol implementation',
+      author='Mikhail Denisenko',
+      author_email='denisenkom@gmail.com',
+      url='https://github.com/denisenkom/pytds',
+      license="MIT",
+      packages=['pytds'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+      ],
+      zip_safe=True,
+      install_requires=requirements,
+      )
