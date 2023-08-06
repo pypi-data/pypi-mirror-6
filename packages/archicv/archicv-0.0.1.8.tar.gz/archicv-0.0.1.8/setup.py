1a01ddbc9c2a6d692dfd7dd76f97dfd36fd62d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:30 +0000

    Add package archicv/0.0.1.8/archicv-0.0.1.8.tar.gz

---
+++ packages/archicv/archicv-0.0.1.8.tar.gz/archicv-0.0.1.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+      name = 'archicv',
+      version = '0.0.1.8',
+      keywords = ('archicv', 'ArchiCV', 'archi', 'archiCV'),
+      url = 'https://github.com/zhangxiansheng/-archi',
+      description = 'OpenCV on Landscape Architecture',
+      license = 'MIT License',
+      
+      author = 'Zhang Yiwei',
+      author_email = 'Karl1991223@126.com',
+      
+      packages = find_packages(),
+      platforms = 'any',
+      )
