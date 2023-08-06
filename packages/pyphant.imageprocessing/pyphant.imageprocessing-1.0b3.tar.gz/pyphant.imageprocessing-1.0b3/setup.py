ef013da9b1b2b34e7e42d38b863b5e95dbe7f4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package pyphant.imageprocessing/1.0b3/pyphant.imageprocessing-1.0b3.tar.gz

---
+++ packages/pyphant.imageprocessing/pyphant.imageprocessing-1.0b3.tar.gz/pyphant.imageprocessing-1.0b3/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pyphant ImageProcessing toolbox
+This is the ImageProcessing toolbox, that serves as an example off a
+toolbox for the Pyphant framework. In order to use it you must have
+the Pyphant framework installed first. Visit
+http://pyphant.sourceforge.net for more information.
+"""
+
+
+VERSION = '1.0b3'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyphant.imageprocessing",
+    version=VERSION,
+    author="Alexander Held, Andreas W. Liehr, Klaus Zimmermann",
+    author_email='alexander.held@fmf.uni-freiburg.de',
+    maintainer='Alexander Held',
+    maintainer_email='alexander.held@fmf.uni-freiburg.de',
+    license="BSD",
+    description=__doc__,
+    install_requires=[
+        'pyphant>=1.0b3',
+        'PIL',
+        'numpy',
+        'scipy',
+        ],
+    packages=find_packages(),
+    entry_points="""
+    [pyphant.workers]
+    myeentry = ImageProcessing
+    """,
+    include_package_data=True,
+    test_suite='ImageProcessing.tests'
+    )
