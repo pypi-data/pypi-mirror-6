3338832ddf4417dbcfa92f23a95e496abff55575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:37 +0000

    Add package zoort/0.1.8/zoort-0.1.8.tar.gz

---
+++ packages/zoort/zoort-0.1.8.tar.gz/zoort-0.1.8/README.rst
@@ -0,0 +1,32 @@
+===============================
+ Zoort
+===============================
+
+.. image:: https://mejorando.la/static/images/logos/mejorandola.png
+        :target: https://www.mejorando.la
+
+.. image:: https://badge.fury.io/py/zoort.png
+    :target: http://badge.fury.io/py/zoort
+
+.. image:: https://pypip.in/d/zoort/badge.png
+        :target: https://crate.io/packages/zoort?version=latest
+
+A Python script for automatic MongoDB backups
+
+Features
+--------
+
+* Backup for just one or all your MongoDB Databases.
+* Encrypt and Decrypt output dump file.
+* Upload file to S3 bucket.
+
+Requirements
+------------
+
+- Python 2.6 | 2.7
+
+
+License
+-------
+
+MIT licensed. See the bundled `LICENSE <https://github.com/yograterol/zoort/blob/master/LICENSE>`_ file for more details.
---
+++ packages/zoort/zoort-0.1.8.tar.gz/zoort-0.1.8/setup.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2014 Mejorando.la - www.mejorando.la
+# Yohan Graterol - <y@mejorando.la>
+import sys
+import os
+import subprocess
+
+from setuptools import setup
+
+data_files = [('etc', ['etc/myapp.cfg'])]
+
+PUBLISH_CMD = "python setup.py register sdist upload"
+TEST_PUBLISH_CMD = 'python setup.py register -r test sdist upload -r test'
+
+if 'publish' in sys.argv:
+    status = subprocess.call(PUBLISH_CMD, shell=True)
+    sys.exit(status)
+
+if 'publish_test' in sys.argv:
+    status = subprocess.call(TEST_PUBLISH_CMD, shell=True)
+    sys.exit()
+
+
+def read(fname):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+
+setup(
+    name='zoort',
+    version="0.1.8",
+    description='A Python script for automatic MongoDB backups',
+    long_description=read("README.rst"),
+    author='Yohan Graterol',
+    author_email='y@mejorando.la | yograterol@fedoraproject.org',
+    url='https://github.com/yograterol/zoort',
+    install_requires=['docopt', 'fabric', 'boto', 'requests',
+                      'python-dateutil', 'sqlalchemy', 'dropbox',
+                      'python-swiftclient'],
+    license=read("LICENSE"),
+    zip_safe=False,
+    keywords='zoort, mongodb, backups',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+    ],
+    py_modules=["zoort"],
+    entry_points={
+        'console_scripts': [
+            "zoort = zoort:main"
+        ]
+    },
+    include_package_data=True,
+    data_files=[('/etc/zoort', ['etc/config.json', ])]
+)
