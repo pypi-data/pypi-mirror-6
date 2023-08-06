0e11721b37a2616f74d8fb0e5daa408b00c4e11a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:04 +0000

    Add package django-seznam-captcha/1.2.2/django-seznam-captcha-1.2.2.tar.gz

---
+++ packages/django-seznam-captcha/django-seznam-captcha-1.2.2.tar.gz/django-seznam-captcha-1.2.2/django_seznam_captcha/widgets.py
@@ -0,0 +1,34 @@
+import random
+
+from django import forms
+from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
+from django.conf import settings
+
+
+class CaptchaImageWidget(forms.widgets.Widget):
+    pass
+
+class CaptchaInputWidget(forms.widgets.Widget):
+    pass
+
+class CaptchaWidget(forms.widgets.MultiWidget):
+    def __init__(self, *args, **kwargs):
+        defaults = {
+            'widgets': (
+                CaptchaImageWidget(),
+                CaptchaInputWidget(),
+            )
+        }
+        defaults.update(kwargs)
+        super(CaptchaWidget, self).__init__(**defaults)
+
+    def decompress(self, value):
+        return [None, None]
+
+    def render(self, name, value, attrs={}):
+        return mark_safe(render_to_string(
+            "seznam_captcha/captcha_widget.html",
+            {"name":name, "STATIC_URL": settings.STATIC_URL, }
+        ))
+
