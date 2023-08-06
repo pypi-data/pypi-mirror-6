8f457f64b2f11a3a52b8a69d4a4e5ec2c2a8a899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:29 +0000

    Add package gitgate/0.1.16/gitgate-0.1.16.tar.gz

---
+++ packages/gitgate/gitgate-0.1.16.tar.gz/gitgate-0.1.16/gitgate/daemon.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+import models as dbm
+import time
+import logging
+import sys
+import re
+import datetime
+import ggconf
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+on_commit = None
+on_merge = None
+running = False
+
+
+
+def clone_commit(new_sha1, old_sha1, file_changes, details):
+
+    """ Clones a commit and its files """
+
+    try:
+        old_commit = dbm.Commit.get(sha1=old_sha1)
+    except:
+        return False
+
+    new_date = details.get('author_date', datetime.datetime.now)
+
+    commit = old_commit.clone(new_sha1=new_sha1, 
+        new_author_date=new_date, clone_files=True)
+    
+    file_paths = [f[1] for f in file_changes]
+
+    for old_cf in commit.files:
+        if old_cf.file_path in file_paths:        
+            old_cf.delete_instance()
+
+    for status, fpath in file_changes:
+        dbm.CommitFile.create(
+            commit=commit,
+            change_type=status,
+            file_path=fpath)
+
+    return commit
+
+def check_for_commits(project, branch='master'):
+    sha1s = project.git_control.get_sha1_diffs(branch=branch)
+    handled_sha1s = [c.sha1 for c in project.commits]
+
+    for sha1 in sha1s:
+        logger.info("Found commit: %s"%(sha1))
+        if sha1 in handled_sha1s:
+            logger.info('Already handled')
+            continue
+
+        file_changes = project.git_control.get_commit_file_changes(sha1)
+
+        if not file_changes:
+            logger.info('No file changes')
+            continue
+
+        details = project.git_control.get_commit_details(sha1)
+        message = details['message']
+        commit = None
+
+        ### Check for special handling
+
+        # Check for "clone" 
+        inc_match = re.search(r'clone:([a-zA-Z0-9]{20,45})', message)
+        if inc_match:
+            old_sha1 = inc_match.group(1)
+            commit = clone_commit(old_sha1=old_sha1, new_sha1=sha1,
+                file_changes=file_changes, details=details)
+        
+        
+        # Create the commit and its files
+        if not commit:    
+            commit = dbm.Commit.create(sha1=sha1, project=project, branch=branch,
+                author_date=details['author_date'], author_name=details['author_name'],
+                author_email=details['author_email'], status='committed',
+                message=details['message'])
+        
+            for change in file_changes:
+                cf = dbm.CommitFile.create(file_path=change[1], change_type=change[0], commit=commit)
+
+        if on_commit:
+            on_commit(commit)
+    
+        logger.info('Done')
+
+def handle_approved(project):
+    commits = (
+        dbm.Commit.select()
+        .where((dbm.Commit.status == 'approved') & (dbm.Commit.project == project))
+        .order_by(dbm.Commit.author_date.asc()))
+    
+    for commit in commits:
+        logger.info('Working on commit: %s'%(commit.sha1))
+        if not commit.can_merge:
+            logger.info("Can't merge")
+            continue
+        try:
+            project.git_control.merge_commit(commit.sha1, branch=commit.branch)    
+            commit.status = 'merged'
+        except Exception as err:
+            logger.exception(err)
+            logger.warning('Could not merge commit due to above exception')
+            commit.status = 'outdated'
+        commit.save()
+
+def start():
+    running = True
+    while running:
+        projects = dbm.Project.select()
+        for project in projects:
+            for branch in project.branches:
+                project.git_control.update_all(branch=branch.name)
+                check_for_commits(project, branch=branch.name)
+            handle_approved(project)
+        time.sleep(10)
+
+def stop():
+    running = False
+
+if __name__ == "__main__":
+    start()
+
---
+++ packages/gitgate/gitgate-0.1.16.tar.gz/gitgate-0.1.16/gitgate/models.py
@@ -0,0 +1,176 @@
+from peewee import *
+import util
+import datetime
+import ggconf
+
+database = SqliteDatabase(ggconf.DATABASE)
+
+def create_tables():
+    User.create_table()
+    Role.create_table()
+    Project.create_table()
+    ProjectBranch.create_table()
+    ProjectRole.create_table()
+    Commit.create_table()
+    CommitFile.create_table()
+    CommitLog.create_table()
+
+def populate_data():
+    Role.create(name='reviewer')
+    Role.create(name='approver')
+
+class DBModel(Model):
+    class Meta:
+        database = database
+
+class User(DBModel):
+
+    email = CharField(unique=True)
+    name = CharField()
+    password = CharField()
+    is_admin = BooleanField(default=False)
+    
+    def has_project_role(self, project, role):
+        if role == 'admin':
+            return self.is_admin        
+    
+        if self.is_admin:
+            return True
+
+        if isinstance(role, str) or isinstance(role, unicode):
+            role = Role.get(name=role)
+        try:
+            ProjectRole.get(user=self, role=role, project=project)
+            return True
+        except:
+            return False
+
+class Project(DBModel):
+
+    name = CharField(unique=True)
+    path = CharField(unique=True)
+    devel_clone_url = CharField()
+    stable_clone_url = CharField()
+    last_merge_sha1 = CharField(default=None, null=True)
+    _git_control = None
+    
+    @property
+    def git_control(self):
+        if self._git_control:
+            return self._git_control
+        self._git_control = util.GitProject(self)
+        return self._git_control
+
+class ProjectBranch(DBModel):
+
+    name = CharField()
+    project = ForeignKeyField(Project, related_name='branches')
+
+    class Meta:
+        primary_key = CompositeKey('name', 'project')
+
+class Role(DBModel):
+    
+    NAMES = (
+        ('reviewer','Reviewer'),
+        ('approver','Approver'),
+    )
+
+    name = CharField(unique=True)
+    description = TextField(default=None, null=True)
+
+class ProjectRole(DBModel):
+
+    user = ForeignKeyField(User)
+    project = ForeignKeyField(Project, related_name='roles')
+    role = ForeignKeyField(Role)
+
+class Commit(DBModel):
+
+    STATUSES = (
+        ('committed', 'Committed'),
+        ('tested', 'Unit Tested'),
+        ('reviewed', 'Code Reviewed'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+        ('outdated', 'Outdated'),
+        ('merged','Merged'),
+    )
+    
+    sha1 = CharField(unique=True)
+    branch = CharField()
+    project = ForeignKeyField(Project, related_name='commits')
+    author_date = DateTimeField()
+    author_name = CharField()
+    author_email = CharField()
+    message = TextField()
+    status = CharField(choices=STATUSES)
+
+    @property
+    def can_merge(self):
+        _merge = True
+        older_commits = (self
+            .select()
+            .where((Commit.status != 'merged') 
+                & (Commit.status != 'rejected')
+                & (Commit.status != 'outdated')
+                & (Commit.author_date < self.author_date))
+        )
+        this_files = [f.file_path for f in self.files]
+        for cm in older_commits:
+            for f in cm.files:
+                if f.file_path in this_files:
+                    _merge = False
+                    break
+        return _merge
+
+    def clone(self, new_sha1, new_author_date=None, new_status='committed', 
+            clone_files=True):
+        
+        """ Clone a commit and its files """
+
+        if not new_author_date:
+            new_author_date = datetime.datetime.now
+
+        new_commit = Commit.create(
+            sha1=new_sha1,
+            status=new_status,
+            author_date=new_author_date,
+            author_name=self.author_name,
+            author_email=self.author_email,
+            branch=self.branch,
+            message=self.message,
+            project=self.project)
+
+        if clone_files:
+            # Copy the files to the new commit
+            for cf in self.files:
+                new_cf = CommitFile.create(
+                    commit=new_commit, 
+                    file_path=cf.file_path,
+                    change_type=cf.change_type)
+
+        # Add a log message
+        log = CommitLog.create(
+            commit=new_commit,
+            message='Cloned from %s'%(self.sha1)
+        ) 
+        self.status = 'outdated'
+        self.save()
+        return new_commit
+    
+class CommitFile(DBModel):
+    
+    commit = ForeignKeyField(Commit, related_name='files')
+    file_path = CharField()
+    change_type = CharField(choices=['A','M','D'])
+    
+    class Meta:
+        primary_key = CompositeKey('commit', 'file_path')
+
+class CommitLog(DBModel):
+    
+    commit = ForeignKeyField(Commit, related_name='logs')
+    user = ForeignKeyField(User, null=True, default=None)
+    created = DateTimeField(default=datetime.datetime.now)
+    message = TextField()    
