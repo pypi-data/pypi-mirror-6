d40ab2972055b6311639702619841c180244463b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:09 +0000

    Add package READemption/0.2.1/READemption-0.2.1.tar.gz

---
+++ packages/READemption/READemption-0.2.1.tar.gz/READemption-0.2.1/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='READemption',
+    version='0.2.1',
+    packages=['reademptionlib', 'tests'],
+    author='Konrad U. Foerstner',
+    author_email='konrad@foerstner.org',
+    description='READemption - A RNA-Seq Analysis Pipeline',
+    url='',
+    install_requires=[
+        "pysam >= 0.7.7"
+    ],
+    scripts=['bin/reademption'],
+    license='LICENSE.txt',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ]
+)
