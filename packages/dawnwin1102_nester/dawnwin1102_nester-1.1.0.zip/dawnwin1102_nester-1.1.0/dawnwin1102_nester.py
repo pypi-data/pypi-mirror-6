ad05631e044d85eab39748b2733e8850c1738db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:23 +0000

    Add package dawnwin1102_nester/1.1.0/dawnwin1102_nester-1.1.0.zip

---
+++ packages/dawnwin1102_nester/dawnwin1102_nester-1.1.0.zip/dawnwin1102_nester-1.1.0/dawnwin1102_nester.py
@@ -0,0 +1,4 @@
+def print_movie(newMovie):
+    movies=["mike","jack","john"]
+    movies.append(newMovie)
+    print(movies)
---
+++ packages/dawnwin1102_nester/dawnwin1102_nester-1.1.0.zip/dawnwin1102_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name="dawnwin1102_nester",
+    version="1.1.0",
+    py_modules=["dawnwin1102_nester"],
+    author="Leo",
+    author_email="dawnwin1102@hotmail.com",
+    url="",
+    description="my upload modules trying"
+    )
