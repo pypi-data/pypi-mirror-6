3c02ed257ec05aeba61bbd142a570863479ac6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:28 +0000

    Add package google_play_rank/0.1.1/google_play_rank-0.1.1.tar.gz

---
+++ packages/google_play_rank/google_play_rank-0.1.1.tar.gz/google_play_rank-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'google_play_rank',
+    packages = ['google_play_rank'],
+    version = '0.1.1',
+    description = 'Input a package name, then find the ranking for you.',
+    author = 'davidyen1124',
+    author_email = 'davidyen1124@gmail.com',
+    url = 'https://github.com/davidyen1124/google_play_rank',
+    keywords = ['google play', 'ranking', 'android', 'package name'],
+    install_requires = [
+        'requests',
+        'bs4',
+    ]
+)
