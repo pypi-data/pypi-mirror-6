62acea42ae9834d47338060a7a7781865e3d6ab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:20 +0000

    Add package gocept.testing/1.10.1/gocept.testing-1.10.1.zip

---
+++ packages/gocept.testing/gocept.testing-1.10.1.zip/gocept.testing-1.10.1/setup.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2011 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gocept.testing',
+    version='1.10.1',
+    author='Wolfgang Schnerring <ws at gocept dot com>',
+    author_email='ws@gocept.com',
+    url='https://code.gocept.com/hg/public/gocept.testing',
+    description="""\
+A collection of test helpers, additional assertions, and the like.
+""",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: Zope Public License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        ],
+    long_description=(
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL',
+    namespace_packages=['gocept'],
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require=dict(test=[
+        'mock',
+        'six',
+    ]),
+)
---
+++ packages/gocept.testing/gocept.testing-1.10.1.zip/gocept.testing-1.10.1/src/gocept/testing/patch.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2011-2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+import contextlib
+
+
+class Patches(object):
+    # XXX Maybe this should be a context manager, too.
+
+    def __init__(self):
+        self.attributes = []
+        self.calls = []
+
+    def set(self, subject, attribute, value):
+        self.attributes.append(
+            (subject, attribute, getattr(subject, attribute)))
+        setattr(subject, attribute, value)
+
+    def call(self, subject, attribute, old, new):
+        self.calls.append(
+            (subject, attribute, old))
+        getattr(subject, attribute)(new)
+
+    def reset(self):
+        self._reset_attributes()
+        self._reset_calls()
+        self.__init__()
+
+    def _reset_attributes(self):
+        for subject, attribute, value in self.attributes:
+            setattr(subject, attribute, value)
+
+    def _reset_calls(self):
+        for subject, attribute, value in self.calls:
+            getattr(subject, attribute)(value)
+
+
+@contextlib.contextmanager
+def Dict(dict, **changes):
+    """Patch a dict with new keys and restore it at exit."""
+    # XXX Maybe `Patches` should be able to do this, too.
+    orig = dict.copy()
+    dict.update(changes)
+    try:
+        yield
+    finally:
+        dict.clear()
+        dict.update(orig)
