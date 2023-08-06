5cf468c9162c3c4e54d1ee3e66ffd4af3ddc6946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:36 +0000

    Add package betahaus.viewcomponent/0.3b/betahaus.viewcomponent-0.3b.tar.gz

---
+++ packages/betahaus.viewcomponent/betahaus.viewcomponent-0.3b.tar.gz/betahaus.viewcomponent-0.3b/betahaus/viewcomponent/fixtures/dummy.py
@@ -0,0 +1,14 @@
+from betahaus.viewcomponent import view_action
+
+
+@view_action('group', 'action')
+def _callme(*args):
+    return "".join([str(x) for x in args])
+
+@view_action('html', 'stuff')
+def _callable_html(context, request, va):
+    return "%s, %s" % (context.__name__, 'stuff')
+
+
+def includeme(config):
+    config.scan(__name__)
