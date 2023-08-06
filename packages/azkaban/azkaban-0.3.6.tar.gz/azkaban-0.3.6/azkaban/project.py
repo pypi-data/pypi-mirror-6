1d8cc226cb356b711e88a656756c9ae1e64e81f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:46 +0000

    Add package azkaban/0.3.6/azkaban-0.3.6.tar.gz

---
+++ packages/azkaban/azkaban-0.3.6.tar.gz/azkaban-0.3.6/azkaban/project.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""Project definition module."""
+
+
+from os import sep
+from os.path import (abspath, basename, dirname, exists, isabs, isdir, join,
+  relpath, splitext)
+from traceback import format_exc
+from weakref import WeakValueDictionary
+from zipfile import ZipFile
+from .util import AzkabanError, temppath
+import logging
+import sys
+
+
+logger = logging.getLogger(__name__)
+
+
+class Project(object):
+
+  """Azkaban project.
+
+  :param name: Name of the project.
+  :param register: Add project to registry. Setting this to `False` will make
+    it invisible to the CLI.
+  :param root: Path to a root file or directory used to enable adding files
+    using relative paths (typically used with `root=__file__`).
+
+  """
+
+  root = None
+  _registry = WeakValueDictionary()
+
+  def __init__(self, name, root=None, register=True):
+    self.name = name
+    if root:
+      self.root = abspath(root if isdir(root) else dirname(root))
+    if register:
+      self._registry[name] = self
+    self._jobs = {}
+    self._files = {}
+
+  def __str__(self):
+    return self.name
+
+  @property
+  def files(self):
+    """Returns a list of files that will be included in the project archive.
+
+    This property should not be used to add files. Use `add_files` instead.
+
+    """
+    return [relpath(e) for e in self._files]
+
+  @property
+  def jobs(self):
+    """Returns a dictionary with each job options.
+
+    This property should not be used to add jobs. Use `add_job` instead.
+
+    """
+    return dict(
+      (name, job.options)
+      for name, job in self._jobs.items()
+    )
+
+  def add_file(self, path, archive_path=None):
+    """Include a file in the project archive.
+
+    :param path: Path to file.
+    :param archive_path: Path to file in archive (defaults to same as `path`).
+
+    If the current project has its `root` parameter specified, this method will
+    allow relative paths (and join those with the project's root). Otherwise,
+    it will throw an error. This is done to avoid having files in the archive
+    with lower level destinations than the base root directory.
+
+    """
+    logger.debug('adding file %r as %r', path, archive_path or path)
+    if not isabs(path):
+      if self.root:
+        path = join(self.root, path)
+      else:
+        raise AzkabanError(
+          'Relative path not allowed without specifying a project root: %r.'
+          % (path, )
+        )
+    if path in self._files:
+      if self._files[path] != archive_path:
+        raise AzkabanError('Inconsistent duplicate file: %r.' % (path, ))
+    else:
+      if not exists(path):
+        raise AzkabanError('File not found: %r.' % (path, ))
+      self._files[path] = archive_path
+
+  def add_job(self, name, job):
+    """Include a job in the project.
+
+    :param name: Name assigned to job (must be unique).
+    :param job: `Job` subclass.
+
+    This method triggers the `on_add` method on the added job (passing the
+    project and name as arguments). The handler will be called right after the
+    job is added.
+
+    """
+    logger.debug('adding job %r', name)
+    if name in self._jobs:
+      raise AzkabanError('Duplicate job name: %r.' % (name, ))
+    else:
+      self._jobs[name] = job
+      job.on_add(self, name)
+
+  def merge_into(self, project, relative=False):
+    """Merge one project with another.
+
+    :param project: Target project to merge into.
+    :param relative: If set to `True`, files added relative to the current
+      project's root will retain their relative paths. The default behavior is
+      to always keep the same files when merging (even if the new project's
+      root is different).
+
+    The current project remains unchanged while the target project gains all
+    the current project's jobs and files. Note that if the `relative` option
+    is set to `True`, files can end up having different absolute paths.
+
+    """
+    logger.debug('merging into project %r', project.name)
+    if not relative:
+      root = project.root
+      project.root = self.root
+    try:
+      for name, job in self._jobs.items():
+        project.add_job(name, job)
+      for path, archive_path in self._files.items():
+        project.add_file(path, archive_path)
+    finally:
+      if relative:
+        project.root = root
+
+  def build(self, path, overwrite=False):
+    """Create the project archive.
+
+    :param path: Destination path.
+    :param overwrite: Don't throw an error if a file already exists at `path`.
+
+    Triggers the `on_build` method on each job inside the project (passing
+    itself and the job's name as two argument). This method will be called
+    right before the job file is generated.
+
+    """
+    logger.debug('building project')
+    # not using a with statement for compatibility with older python versions
+    if exists(path) and not overwrite:
+      raise AzkabanError('Path %r already exists.' % (path, ))
+    if not (len(self._jobs) or len(self._files)):
+      raise AzkabanError('Building empty project.')
+    writer = ZipFile(path, 'w')
+    try:
+      for name, job in self._jobs.items():
+        job.on_build(self, name)
+        with temppath() as fpath:
+          job.build(fpath)
+          writer.write(fpath, '%s.job' % (name, ))
+      for fpath, apath in self._files.items():
+        writer.write(fpath, apath)
+    finally:
+      writer.close()
+
+  @classmethod
+  def load(cls, path, name=None):
+    """Load project from script.
+
+    :param path: Path to python module or package.
+    :param name: Project name. If not specified and a single project is found
+      while loading the script, that project is returned. In any other case
+      (no/multiple projects found), an error is thrown.
+
+    """
+    sys.path.insert(0, dirname(path))
+    module_name = splitext(basename(path.rstrip(sep)))[0]
+    try:
+      __import__(module_name)
+    except ImportError:
+      raise AzkabanError(
+        'Unable to import script %r.\n%s' % (path, format_exc())
+        )
+    else:
+      if name:
+        try:
+          return cls._registry[name]
+        except KeyError:
+          raise AzkabanError(
+            'Unable to find project with name %r in script %r.\n'
+            'Available projects: %s.'
+            % (name, path, ', '.join(cls._registry))
+          )
+      else:
+        if len(cls._registry) == 1:
+          return cls._registry.popitem()[1]
+        elif not cls._registry:
+          raise AzkabanError('No project found in %r.' % (path, ))
+        else:
+          raise AzkabanError(
+            'Multiple projects found in %r: %s.\n'
+            'Disambiguate using --project=%s:project_name.'
+            % (path, ', '.join(cls._registry), path)
+          )
---
+++ packages/azkaban/azkaban-0.3.6.tar.gz/azkaban-0.3.6/azkaban/remote.py
@@ -0,0 +1,427 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""Azkaban remote interaction module.
+
+This contains the `Session` class which will be used for all interactions with
+a remote Azkaban server.
+
+"""
+
+
+from ConfigParser import NoOptionError, NoSectionError
+from getpass import getpass, getuser
+from os.path import exists
+from time import sleep
+from .util import AzkabanError, Config
+import logging
+import requests as rq
+
+
+logger = logging.getLogger(__name__)
+
+def _azkaban_request(method, url, **kwargs):
+  """Make request to azkaban server and catch common errors.
+
+  :param method: get, post, etc.
+  :param url: endpoint url
+  :param kwargs: arguments forwarded to the request handler
+
+  This function is meant to handle common errors and return a more helpful
+  message than the default one.
+
+  """
+  try:
+    handler = getattr(rq, method.lower())
+  except AttributeError:
+    raise ValueError('Invalid HTTP method: %r.' % (method, ))
+  else:
+    try:
+      response = handler(url, verify=False, **kwargs)
+    except rq.ConnectionError:
+      raise AzkabanError('Unable to connect to azkaban at %r.' % (url, ))
+    except rq.exceptions.MissingSchema:
+      raise AzkabanError('Invalid azkaban server url: %r.' % (url, ))
+    else:
+      return response
+
+def _extract_json(response):
+  """Extract json from Azkaban response, gracefully handling errors.
+
+  :param response: request response object
+
+  """
+  try:
+    json = response.json()
+  except ValueError:
+    # no json decoded probably
+    raise ValueError('No JSON decoded from response %r' % (response.text, ))
+  else:
+    if 'error' in json:
+      raise AzkabanError(json['error'])
+    elif json.get('status') == 'error':
+      raise AzkabanError(json['message'])
+    else:
+      return json
+
+def _parse_url(url):
+  """Parse url.
+
+  :param url: http endpoint (including protocol, port and optional user)
+
+  """
+  parsed_url = url.rstrip('/').split('@')
+  if len(parsed_url) == 1:
+    user = getuser()
+    url = parsed_url[0]
+  elif len(parsed_url) == 2:
+    user = parsed_url[0]
+    url = parsed_url[1]
+  else:
+    raise AzkabanError('Malformed url: %r' % (url, ))
+  return (user, url)
+
+def _resolve_alias(config, alias):
+  """Get url associated with an alias.
+
+  :param alias: alias name
+
+  """
+  try:
+    return config.parser.get('alias', alias)
+  except (NoOptionError, NoSectionError):
+    raise AzkabanError('Alias %r not found.', alias)
+
+def _get_session_id(config, url):
+  """Retrieve session id associated with url.
+
+  :param url: http endpoint (including protocol, port and optional user)
+
+  """
+  try:
+    return config.parser.get('session_id', url)
+  except NoSectionError:
+    config.parser.add_section('session_id')
+  except NoOptionError:
+    pass
+
+
+class Session(object):
+
+  """Azkaban session.
+
+  :param url: http endpoint (including protocol, port and optional user)
+  :param alias: alias name
+
+  """
+
+  def __init__(self, url=None, alias=None):
+    self.config = Config()
+    if not url:
+      alias = alias or self.config.get_option('azkaban', 'alias')
+      url = _resolve_alias(self.config, alias)
+    self.user, self.url = _parse_url(url)
+    self.id = _get_session_id(self.config, str(self).replace(':', '.'))
+    logger.debug('session %s instantiated', self)
+
+  def __str__(self):
+    return '%s@%s' % (self.user, self.url)
+
+  def _refresh(self, password=None):
+    """Refresh session ID.
+
+    :param password: password used to log into Azkaban (only used if no alias
+      is provided). can be set to `False` to fail instead of prompting for a
+      password.
+
+    Also saves session ID for future use.
+
+    """
+    logger.debug('refreshing session')
+    password = password or getpass('Azkaban password for %s: ' % (self, ))
+    res = _extract_json(_azkaban_request(
+      'POST',
+      self.url,
+      data={'action': 'login', 'username': self.user, 'password': password},
+    ))
+    self.id = res['session.id']
+    logger.debug('saving session id')
+    self.config.parser.set('session_id', str(self).replace(':', '.'), self.id)
+    self.config.save()
+
+  def _request(self, method, endpoint, use_cookies=True, attempts=1, **kwargs):
+    """Make request to Azkaban using this session.
+
+    :param method: http method
+    :param endpoint: server endpoint (e.g. manager)
+    :param attempts: if current session ID is invalid, maximum number of
+      attempts to refresh it
+    :param use_cookies: include session_id in cookies instead of request data
+    :param **kwargs: keyword arguments passed to `_azkaban_request`
+
+    If the session expired, will prompt for a password to refresh.
+
+    """
+    full_url = '%s/%s' % (self.url, endpoint.lstrip('/'))
+    logger.debug('sending request to %r: %r', full_url, kwargs)
+    while True:
+      if use_cookies:
+        kwargs.setdefault('cookies', {})['azkaban.browser.session.id'] = self.id
+      else:
+        kwargs.setdefault('data', {})['session.id'] = self.id
+      res = _azkaban_request(method, full_url, **kwargs) if self.id else None
+      if (
+        res is None or # explicit check because 500 responses evaluate to False
+        '<!-- /.login -->' in res.text or # usual non API error response
+        'Login error' in res.text # special case for API
+      ):
+        logger.debug('request failed because of invalid login')
+        if attempts > 0:
+          self._refresh()
+          attempts -= 1
+        else:
+          raise AzkabanError(
+            'Too many unsuccessful login attempts for url %r. Aborting.',
+            self.url
+          )
+      else:
+        return res
+
+  def get_execution_status(self, exec_id):
+    """Get status of an execution.
+
+    :param exec_id: execution ID
+
+    """
+    return _extract_json(self._request(
+      method='GET',
+      endpoint='executor',
+      params={
+        'execid': exec_id,
+        'ajax': 'fetchexecflow',
+      },
+    ))
+
+  def get_job_logs(self, exec_id, job, offset=0, limit=50000):
+    """Get logs from a job execution.
+
+    :param exec_id: execution ID
+    :param job: job name
+    :param offset: log offset
+    :param limit: size of log to download
+
+    """
+    return _extract_json(self._request(
+      method='GET',
+      endpoint='executor',
+      params={
+        'execid': exec_id,
+        'jobId': job,
+        'ajax': 'fetchExecJobLogs',
+        'offset': offset,
+        'length': limit,
+      },
+    ))
+
+  def cancel_execution(self, exec_id):
+    """Cancel workflow execution.
+
+    :param exec_id: execution ID
+
+    """
+    res = _extract_json(self._request(
+      method='GET',
+      endpoint='executor',
+      params={
+        'execid': exec_id,
+        'ajax': 'cancelFlow',
+      },
+    ))
+    if 'error' in res:
+      raise AzkabanError('Execution %s is not running.' % (exec_id, ))
+    return res
+
+  def create_project(self, name, description):
+    """Create project.
+
+    :param name: project name
+    :param description: project description
+
+    """
+    return _extract_json(self._request(
+      method='POST',
+      endpoint='manager',
+      data={
+        'action': 'create',
+        'name': name,
+        'description': description,
+      },
+    ))
+
+  def delete_project(self, name):
+    """Delete a project on Azkaban.
+
+    :param session: `azkaban.util.Session` object
+
+    """
+    res = self._request(
+      method='GET',
+      endpoint='manager',
+      params={
+        'project': name,
+        'delete': 'true',
+      },
+    )
+    msg = "Project '%s' was successfully deleted" % (name, )
+    if not msg in res.text:
+      raise AzkabanError('Delete failed. Check permissions and existence.')
+    return res
+
+  def run_workflow(self, name, flow, jobs=None, skip=False):
+    """Launch a workflow.
+
+    :param name: name of the project
+    :param flow: name of the workflow
+    :param jobs: name of jobs to run (run entire workflow by default)
+    :param skip: don't run if the same workflow is already running
+
+    Note that in order to run a workflow on Azkaban, it must already have been
+    uploaded and the corresponding user must have permissions to run.
+
+    """
+    if not jobs:
+      disabled = '[]'
+    else:
+      all_names = set(
+        n['id']
+        for n in self.get_workflow_info(name, flow)['nodes']
+      )
+      run_names = set(jobs)
+      missing_names = run_names - all_names
+      if missing_names:
+        raise AzkabanError(
+          'Jobs not found in flow %r: %s.' %
+          (flow, ', '.join(missing_names))
+        )
+      else:
+        disabled = (
+          '[%s]'
+          % (','.join('"%s"' % (n, ) for n in all_names - run_names), )
+        )
+    return _extract_json(self._request(
+      method='POST',
+      endpoint='executor',
+      use_cookies=False,
+      data={
+        'ajax': 'executeFlow',
+        'project': name,
+        'flow': flow,
+        'disabled': disabled,
+        'concurrentOption': 'skip' if skip else 'concurrent',
+      },
+    ))
+
+  def upload_project(self, name, path):
+    """Upload project archive.
+
+    :param name: project name
+    :param path: path to zip archive
+
+    """
+    if not exists(path):
+      raise AzkabanError('Unable to find archive at %r.' % (path, ))
+    return _extract_json(self._request(
+      method='POST',
+      endpoint='manager',
+      use_cookies=False,
+      data={
+        'ajax': 'upload',
+        'project': name,
+      },
+      files={
+        'file': ('file.zip', open(path, 'rb').read(), 'application/zip'),
+      },
+    ))
+
+  def get_workflow_info(self, name, flow):
+    """Get list of jobs corresponding to a workflow.
+
+    :param name: project name
+    :param flow: name of flow in project
+
+    """
+    raw_res = self._request(
+      method='GET',
+      endpoint='manager',
+      params={
+        'ajax': 'fetchflowjobs',
+        'project': name,
+        'flow': flow,
+      },
+    )
+    try:
+      return _extract_json(raw_res)
+    except ValueError:
+      raise AzkabanError('Flow %r not found.' % (flow, ))
+
+
+class Execution(object):
+
+  """Remote workflow execution.
+
+  :param session: `Session` instance
+  :param exec_id: execution ID
+
+  """
+
+  def __init__(self, session, exec_id):
+    self._session = session
+    self.exec_id = exec_id
+
+  @property
+  def status(self):
+    """Execution status."""
+    return self._session.get_execution_status(self.exec_id)
+
+  @property
+  def url(self):
+    """Execution URL."""
+    return '%s/executor?exec_id=%s' % (self._session.url, self.exec_id)
+
+  def cancel(self):
+    """Cancel execution."""
+    self._session.cancel_execution(self.exec_id)
+
+  def job_logs(self, job, delay=5):
+    """Job log generator.
+
+    :param job: job name
+    :param delay: time in seconds between each server poll
+
+    Yields line by line.
+
+    """
+    finishing = False
+    offset = 0
+    while True:
+      sleep(delay)
+      logs = self._session.get_job_logs(
+        exec_id=self.exec_id,
+        job=job,
+        offset=offset,
+      )
+      if logs['length']:
+        offset += logs['length']
+        lines = (e for e in logs['data'].split('\n') if e)
+        for line in lines:
+          yield line
+      elif finishing:
+        break
+      else:
+        running_jobs = set(
+          e['id']
+          for e in self.status['nodes']
+          if e['status'] == 'RUNNING'
+        )
+        if job not in running_jobs:
+          finishing = True
