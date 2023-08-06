dd5ec4dc28688bfbc92f8d062dab8346b3e8db9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:18 +0000

    Add package retry/0.4.0/retry-0.4.0.zip

---
+++ packages/retry/retry-0.4.0.zip/retry-0.4.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='retry',
+    version='0.4.0',
+    description='retry decorator',
+    packages=['retry'],
+    install_requires=[
+        'decorator',
+    ]
+)
