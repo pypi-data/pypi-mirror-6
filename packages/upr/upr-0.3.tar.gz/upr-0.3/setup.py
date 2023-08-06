549b1335cde3879fe123aeec5747f4ab2670b01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:01 +0000

    Add package upr/0.3/upr-0.3.tar.gz

---
+++ packages/upr/upr-0.3.tar.gz/upr-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='upr',
+    version='0.3',
+    description='Basic UPR client',
+    author='Pavel Paulau',
+    author_email='pavel.paulau@gmail.com',
+    packages=['upr'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
