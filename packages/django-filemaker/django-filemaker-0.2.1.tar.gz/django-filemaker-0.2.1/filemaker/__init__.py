476904cfe5b0cdd7311ec51bdb9c001f973fdaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:38 +0000

    Add package django-filemaker/0.2.1/django-filemaker-0.2.1.tar.gz

---
+++ packages/django-filemaker/django-filemaker-0.2.1.tar.gz/django-filemaker-0.2.1/filemaker/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from distutils import version
+
+
+__version__ = '0.2.1'
+version_info = version.StrictVersion(__version__).version
+
+
+from filemaker.base import FileMakerModel  # NOQA
+from filemaker.exceptions import *  # NOQA
+from filemaker import fields  # NOQA
