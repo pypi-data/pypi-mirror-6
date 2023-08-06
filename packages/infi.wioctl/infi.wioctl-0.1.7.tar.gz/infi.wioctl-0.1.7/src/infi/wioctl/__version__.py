eef771eb08a147367a3db566427aac057b33afd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:15 +0000

    Add package infi.wioctl/0.1.7/infi.wioctl-0.1.7.tar.gz

---
+++ packages/infi.wioctl/infi.wioctl-0.1.7.tar.gz/infi.wioctl-0.1.7/src/infi/wioctl/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.7"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.1.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b6ff15b950416fea78f7dc7096c4c1beadfb303e'
+__git_head_subject__ = 'Finished release v0.1.7.'
+__git_head_message__ = '* release/v0.1.7:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements'
+__git_dirty_diff__ = ''
