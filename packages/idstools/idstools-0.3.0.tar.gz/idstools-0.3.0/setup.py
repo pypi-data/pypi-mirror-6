194e8e4493511bd5f2b755d6cf96b1872d20e28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:21 +0000

    Add package idstools/0.3.0/idstools-0.3.0.tar.gz

---
+++ packages/idstools/idstools-0.3.0.tar.gz/idstools-0.3.0/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except:
+    print("Error importing setuptools, will use distutils.")
+    print("- Script entry points will not be installed.")
+    from distutils import setup
+
+import idstools
+
+setup(
+    name="idstools",
+    version=idstools.version,
+    description="IDS Utility Library",
+    author="Jason Ish",
+    author_email="ish@unx.ca",
+    packages=[
+        "idstools",
+        "idstools.scripts"
+    ],
+    url="https://github.com/jasonish/py-idstools",
+    license="BSD",
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'idstools-gensidmsgmap = idstools.scripts.gensidmsgmap:main',
+            'idstools-u2fast = idstools.scripts.u2fast:main',
+    ]
+    },
+)
