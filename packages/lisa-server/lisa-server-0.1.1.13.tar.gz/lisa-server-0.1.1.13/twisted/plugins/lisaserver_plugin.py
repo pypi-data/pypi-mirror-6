380085cfd549efdfa4d5acc3625dc2c736b06b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package lisa-server/0.1.1.13/lisa-server-0.1.1.13.tar.gz

---
+++ packages/lisa-server/lisa-server-0.1.1.13.tar.gz/lisa-server-0.1.1.13/twisted/plugins/lisaserver_plugin.py
@@ -0,0 +1,26 @@
+from zope.interface import implements
+
+from twisted.python import usage
+from twisted.application.service import IServiceMaker
+from twisted.plugin import IPlugin
+
+from lisa.server import service
+
+
+class Options(usage.Options):
+    optParameters = [
+        ['configuration', 'c', '/etc/lisa/server/configuration/lisa.json'],
+        ['plugins', 'p', '/tmp/lisa_plugins'],
+    ]
+
+class ServiceMaker(object):
+    implements(IServiceMaker, IPlugin)
+
+    tapname = "lisa-server"
+    description = "Lisa server."
+    options = Options
+
+    def makeService(self, config):
+        return service.makeService(config)
+
+serviceMaker = ServiceMaker()
