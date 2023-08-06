6f44fed100ccc32da960f8e9b880d6c1df074fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:25 +0000

    Add package diepy/0.7.1/diepy-0.7.1.zip

---
+++ packages/diepy/diepy-0.7.1.zip/diepy-0.7.1/tests/diepy_tests.py
@@ -0,0 +1,48 @@
+import os
+from nose.tools import *
+import diepy
+from diepy import core
+
+import logging
+
+logger = logging.getLogger()
+
+here = os.path.abspath(os.path.dirname(__file__))
+db = core.Database('test', config=os.path.join(here, 'diepy.ini'))
+
+def setup():
+    print "SETUP!"
+
+
+def teardown():
+    print "TEAR IT DOWN!"
+
+def test_basic_import():
+    datafile = os.path.join(here, 'basic.csv')
+    rows = db.import_file(datafile, 'basic')
+    logger.info("Imported %s rows from %s" % (rows, datafile))
+
+
+def test_basic_export():
+    datafile = os.path.join(here,'export.csv')
+    db.export_table('basic', datafile)
+
+
+def test_basic_export_gz():
+    datafile = os.path.join(here,'export.csv.gz')
+    db.export_table('basic', datafile)
+
+
+def test_basic_export_tab():
+    datafile = os.path.join(here, 'export.tab')
+    db.export_table('basic', datafile)
+
+
+def test_basic_export_xlsx():
+    datafile = os.path.join(here, 'export.xlsx')
+    db.export_table('basic', datafile)
+
+
+def test_basic_import_xlsx():
+    datafile = os.path.join(here, 'excel.xlsx')
+    db.export_table('basic', datafile)
