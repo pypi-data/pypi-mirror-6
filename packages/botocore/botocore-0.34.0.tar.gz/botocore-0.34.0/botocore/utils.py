6a569385b12294e93442ae7d4a120500be8fd60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package botocore/0.34.0/botocore-0.34.0.tar.gz

---
+++ packages/botocore/botocore-0.34.0.tar.gz/botocore-0.34.0/botocore/utils.py
@@ -0,0 +1,105 @@
+# Copyright 2012-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from .exceptions import InvalidExpressionError
+
+
+def normalize_url_path(path):
+    if not path:
+        return '/'
+    return remove_dot_segments(path)
+
+
+def remove_dot_segments(url):
+    # RFC 2986, section 5.2.4 "Remove Dot Segments"
+    output = []
+    while url:
+        if url.startswith('../'):
+            url = url[3:]
+        elif url.startswith('./'):
+            url = url[2:]
+        elif url.startswith('/./'):
+            url = '/' + url[3:]
+        elif url.startswith('/../'):
+            url = '/' + url[4:]
+            if output:
+                output.pop()
+        elif url.startswith('/..'):
+            url = '/' + url[3:]
+            if output:
+                output.pop()
+        elif url.startswith('/.'):
+            url = '/' + url[2:]
+        elif url == '.' or url == '..':
+            url = ''
+        elif url.startswith('//'):
+            # As far as I can tell, this is not in the RFC,
+            # but AWS auth services require consecutive
+            # slashes are removed.
+            url = url[1:]
+        else:
+            if url[0] == '/':
+                next_slash = url.find('/', 1)
+            else:
+                next_slash = url.find('/', 0)
+            if next_slash == -1:
+                output.append(url)
+                url = ''
+            else:
+                output.append(url[:next_slash])
+                url = url[next_slash:]
+    return ''.join(output)
+
+
+def validate_jmespath_for_set(expression):
+    # Validates a limited jmespath expression to determine if we can set a value
+    # based on it. Only works with dotted paths.
+    if not expression or expression == '.':
+        raise InvalidExpressionError(expression=expression)
+
+    for invalid in ['[', ']', '*']:
+        if invalid in expression:
+            raise InvalidExpressionError(expression=expression)
+
+
+def set_value_from_jmespath(source, expression, value, is_first=True):
+    # This takes a (limited) jmespath-like expression & can set a value based
+    # on it.
+    # Limitations:
+    # * Only handles dotted lookups
+    # * No offsets/wildcards/slices/etc.
+    if is_first:
+        validate_jmespath_for_set(expression)
+
+    bits = expression.split('.', 1)
+    current_key, remainder = bits[0], bits[1] if len(bits) > 1 else ''
+
+    if not current_key:
+        raise InvalidExpressionError(expression=expression)
+
+    if remainder:
+        if not current_key in source:
+            # We've got something in the expression that's not present in the
+            # source (new key). If there's any more bits, we'll set the key with
+            # an empty dictionary.
+            source[current_key] = {}
+
+        return set_value_from_jmespath(
+            source[current_key],
+            remainder,
+            value,
+            is_first=False
+        )
+
+    # If we're down to a single key, set it.
+    source[current_key] = value
