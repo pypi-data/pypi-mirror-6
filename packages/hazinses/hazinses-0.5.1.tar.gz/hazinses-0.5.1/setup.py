2a859b5c4151244cd92a785561fe8d3a3bd0bd0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:07 +0000

    Add package hazinses/0.5.1/hazinses-0.5.1.tar.gz

---
+++ packages/hazinses/hazinses-0.5.1.tar.gz/hazinses-0.5.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='hazinses',
+    version='0.5.1',
+    packages=['hazinses'],
+    include_package_data=True,
+    license='BSD License',
+    description='A simple Django app for connecting AMAZON SES-CELERY AND BOTO.',
+    author='Tuna VARGI',
+    author_email='tunavargi@gmail.com',
+    install_requires=['django', 'boto', 'django-celery'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License', # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # replace these appropriately if you are using Python 3
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
