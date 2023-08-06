258c4e4016ada847352da4b92def71b472d3a1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:23 +0000

    Add package infi.pypi-manager/0.4.23/infi.pypi_manager-0.4.23-py2.7.egg

---
+++ packages/infi.pypi-manager/infi.pypi_manager-0.4.23-py2.7.egg/infi/pypi_manager/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.23"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.4.23)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '3fc928248dce3198e93b5217387a6b39e807f3a7'
+__git_head_subject__ = 'Finished release v0.4.23.'
+__git_head_message__ = '* release/v0.4.23:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements\n  update dependencies\n  fix hard_install'
+__git_dirty_diff__ = ''
