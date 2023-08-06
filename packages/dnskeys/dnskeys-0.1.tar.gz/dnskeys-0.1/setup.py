bae8a3a68ff921134962719b66d261ea2dee7ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:32 +0000

    Add package dnskeys/0.1/dnskeys-0.1.tar.gz

---
+++ packages/dnskeys/dnskeys-0.1.tar.gz/dnskeys-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dnskeys',
+      version='0.1',
+      description='A Python library for authenticating keys using DNS and DNSSEC',
+      author='Iain R. Learmonth',
+      author_email='irl@fsfe.org',
+      url='http://www.github.com/irl/dnskeys/',
+      packages=['dnskeys'],
+      scripts=['getdnskeys'],
+     )
+
