c05e8e493366027f49e7a03d30ef4f298d065f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:31 +0000

    Add package multipletau/0.1.4/multipletau-0.1.4.tar.gz

---
+++ packages/multipletau/multipletau-0.1.4.tar.gz/multipletau-0.1.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# To create a distribution package for pip or easy-install:
+# python setup.py sdist
+from setuptools import setup, find_packages
+from os.path import join, dirname, realpath
+from warnings import warn
+
+import multipletau
+
+setup(
+    name='multipletau',
+    author='Paul Mueller',
+    author_email='paul.mueller@biotec.tu-dresden.de',
+    url='https://github.com/paulmueller/multipletau',
+    version=multipletau.__version__,
+    packages=['multipletau'],
+    package_dir={'multipletau': 'multipletau'},
+    license="OpenBSD",
+    description='A multiple-tau algorithm for Python/NumPy.',
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    install_requires=["NumPy >= 1.5.1"]
+    )
+
+
