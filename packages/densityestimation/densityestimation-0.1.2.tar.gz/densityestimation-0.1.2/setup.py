2b53669aa97cb761f22225d8b702025124a87232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:03 +0000

    Add package densityestimation/0.1.2/densityestimation-0.1.2.tar.gz

---
+++ packages/densityestimation/densityestimation-0.1.2.tar.gz/densityestimation-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='densityestimation',
+	version='0.1.2',
+	authors=['B. Aldous, S. Krstyen'],
+	author_emails=['benaldous@yahoo.com, sarah.krstyen@gmail.com'],
+	package='densityestimation',
+	url='https://pypi.python.org/pypi?%3Aaction=pkg_edit&name=densityestimation',
+	license='MIT',
+	description='Density estimation functions.',
+	long_description=open('README.txt').read(),
+)
+	
