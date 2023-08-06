5269022610e06b12c1a2194d3c2841935379d5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:33 +0000

    Add package lrn/0.1.2/lrn-0.1.2.tar.gz

---
+++ packages/lrn/lrn-0.1.2.tar.gz/lrn-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lrn',
+    version='0.1.2',
+    author='Razzi Abuissa + Alex Brashear + Peter Yin',
+    author_email='py.peteryin@gmail.com',
+    url='https://github.com/lrn-guru',
+    packages=['lrn', 'lrn.api', 'lrn.repl', 'lrn.lrn'],
+    scripts=['lrn/lrn/lrn.py'],
+)
+
