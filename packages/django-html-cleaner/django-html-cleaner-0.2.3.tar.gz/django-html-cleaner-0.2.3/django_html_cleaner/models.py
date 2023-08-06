f49bc63a29023ba95efc6de86f90d0933a4e53b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:25 +0000

    Add package django-html-cleaner/0.2.3/django-html-cleaner-0.2.3.tar.gz

---
+++ packages/django-html-cleaner/django-html-cleaner-0.2.3.tar.gz/django-html-cleaner-0.2.3/django_html_cleaner/models.py
@@ -0,0 +1,71 @@
+from django.conf import settings
+from django.db import models
+from .cleaner import Cleaner
+
+
+def default_cleaner():
+    allowed_tags = getattr(settings, 'HTML_CLEANER_ALLOWED_TAGS', None)
+    allowed_attributes = getattr(settings,
+                                 'HTML_CLEANER_ALLOWED_ATTRIBUTES',
+                                 None)
+    allowed_styles = getattr(settings, 'HTML_CLEANER_ALLOWED_STYLES', None)
+    create_parent = getattr(settings, 'HTML_CLEANER_PARENT_TAG', None)
+
+    return Cleaner(allowed_tags=allowed_tags,
+                   allowed_attributes=allowed_attributes,
+                   allowed_styles=allowed_styles,
+                   create_parent=create_parent)
+
+
+class SanitizedCharField(models.CharField):
+    """
+    Use anywhere you would use a ``CharField``. Sanitizes HTML.
+
+    ``cleaner``:
+        An instance of ``django_html_cleaner.cleaner.Cleaner()``.
+        Provide your own instance if you want to do more than
+        just remove JavaScript and unknown/special HTML tags.
+    """
+
+    def __init__(self, cleaner=None, *args, **kwargs):
+        self.cleaner = cleaner or default_cleaner()
+        super(SanitizedCharField, self).__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        value = super(SanitizedCharField, self).pre_save(model_instance, add)
+        if value is not None:
+            clean_value = self.cleaner.clean(value)
+            if value != clean_value:
+                setattr(model_instance, self.attname, clean_value)
+            value = clean_value
+        return value
+
+
+class SanitizedTextField(models.TextField):
+    """
+    Use anywhere you would use a ``TextField``. Sanitizes HTML.
+
+    ``cleaner``:
+        An instance of ``django_html_cleaner.cleaner.Cleaner()``.
+        Provide your own instance if you want to do more than
+        just remove JavaScript and unknown/special HTML tags.
+    """
+
+    def __init__(self, cleaner=None, *args, **kwargs):
+        self.cleaner = cleaner or default_cleaner()
+        super(SanitizedTextField, self).__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        value = super(SanitizedTextField, self).pre_save(model_instance, add)
+        if value is not None:
+            clean_value = self.cleaner.clean(value)
+            if value != clean_value:
+                setattr(model_instance, self.attname, clean_value)
+            value = clean_value
+        return value
+
+
+if 'south' in settings.INSTALLED_APPS:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^sanitizer\.models\.SanitizedCharField"])
+    add_introspection_rules([], ["^sanitizer\.models\.SanitizedTextField"])
---
+++ packages/django-html-cleaner/django-html-cleaner-0.2.3.tar.gz/django-html-cleaner-0.2.3/tests/test_models.py
@@ -0,0 +1,40 @@
+import unittest
+from .models import Post
+
+
+class TestHtmlcleaner(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_blank_field_does_not_explode(self):
+        post = Post.objects.create(id=1,title="Hi")
+        self.assert_(Post.objects.get(id=1))
+
+    def test_char_field_works(self):
+        body = """<p onclick="alert()">Hello world!</p>
+                  <p>How are you?</p>
+                  <script src="/scripts/whoa.js"></script>"""
+        post = Post.objects.create(
+            id=1,
+            title="<b onclick='alert()'>Super</b> title",
+            body=body)
+
+        post = Post.objects.get(id=1)
+        self.assertEqual("<span><b>Super</b> title</span>", post.title)
+
+    def test_text_field_works(self):
+        body = """<p onclick="alert()">Hello world!</p>
+                  <p>How are you?</p>
+                  <script src="/scripts/whoa.js"></script>"""
+        post = Post.objects.create(
+            id=1,
+            title="<b onclick='alert()'>Super</b> title",
+            body=body)
+        expected = "<div><p>Hello world!</p><p>How are you?</p></div>"
+
+        post = Post.objects.get(id=1)
+        self.assertEqual(expected, post.body)
+
+    def tearDown(self):
+        Post.objects.all().delete()
