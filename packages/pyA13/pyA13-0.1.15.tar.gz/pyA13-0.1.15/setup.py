c8453253615477a0a07ac51b0ffd3beb01aef106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:35 +0000

    Add package pyA13/0.1.15/pyA13-0.1.15.tar.gz

---
+++ packages/pyA13/pyA13-0.1.15.tar.gz/pyA13-0.1.15/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup, Extension
+
+
+setup(
+    name                = 'pyA13',
+    version             = '0.1.15',
+    author              = 'Stefan Mavrodiev',
+    author_email        = 'support@olimex.com',
+    url                 = 'https://www.olimex.com/',
+    license             = 'MIT',
+    description         = 'Control GPIOs on OLinuXino-A13.',
+    long_description    = open('README.txt').read() + open('CHANGES.txt').read(),
+    classifiers         = [ 'Development Status :: 3 - Alpha',
+                            'Environment :: Console',
+                            'Intended Audience :: Developers',
+                            'Intended Audience :: Education',
+                            'License :: OSI Approved :: MIT License',
+                            'Operating System :: POSIX :: Linux',
+                            'Programming Language :: Python',
+                            'Topic :: Home Automation',
+                            'Topic :: Software Development :: Embedded Systems'
+          ],
+    ext_modules         = [Extension('A13_GPIO', ['source/gpio_lib.c', 'source/pyA13.c'])],
+    package_dir={'': 'source'},
+    packages=[''],
+  
+                            
+)
