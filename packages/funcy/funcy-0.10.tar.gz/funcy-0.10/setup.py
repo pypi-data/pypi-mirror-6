568b48706da29ce6b72c73ba551d24f6c195ae0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:31 +0000

    Add package funcy/0.10/funcy-0.10.tar.gz

---
+++ packages/funcy/funcy-0.10.tar.gz/funcy-0.10/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='funcy',
+    version=open('VERSION').read().strip(),
+    author='Alexander Schepanovski',
+    author_email='suor.web@gmail.com',
+
+    description='A fancy and practical functional tools',
+    long_description=open('README.rst').read(),
+    url='http://github.com/Suor/funcy',
+    license='BSD',
+
+    packages=['funcy'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ]
+)
