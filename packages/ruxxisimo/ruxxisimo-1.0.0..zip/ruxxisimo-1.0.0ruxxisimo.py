0f9affeda69c127ba914592294df98ed66138b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:06 +0000

    Add package ruxxisimo/1.0.0./ruxxisimo-1.0.0..zip

---
+++ packages/ruxxisimo/ruxxisimo-1.0.0..zip/ruxxisimo-1.0.0ruxxisimo.py
@@ -0,0 +1,5 @@
+def ruxxisimo(list_int):
+    for each_item in list_int:
+        print(each_item)
+
+        
---
+++ packages/ruxxisimo/ruxxisimo-1.0.0..zip/ruxxisimo-1.0.0setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='ruxxisimo',
+    version='1.0.0.',
+    py_modules=['ruxxisimo'],
+    author='russel.alfaro',
+    author_email='russel_alfaro911@hotmail.com',
+    description='A simple printed for lists',
+    )
