c082fc434aa2a9b95f9b1803fa9b011695ec0de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:20 +0000

    Add package django-stdimage/0.5.4/django-stdimage-0.5.4.tar.gz

---
+++ packages/django-stdimage/django-stdimage-0.5.4.tar.gz/django-stdimage-0.5.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='django-stdimage',
+    version='0.5.4',
+    description='Django Standarized Image Field',
+    author='codingjoe',
+    url='https://github.com/codingjoe/django-stdimage',
+    author_email='info@johanneshoppe.com',
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    packages=['stdimage'],
+    include_package_data=True,
+    requires=['django (>=1.2.7)', ],
+)
