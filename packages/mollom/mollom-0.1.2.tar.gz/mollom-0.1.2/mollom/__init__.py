ceb27f5340f24f67e973a1f0324f14d0dc51e557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:45 +0000

    Add package mollom/0.1.2/mollom-0.1.2.tar.gz

---
+++ packages/mollom/mollom-0.1.2.tar.gz/mollom-0.1.2/mollom/__init__.py
@@ -0,0 +1,1 @@
+from mollom import Mollom
---
+++ packages/mollom/mollom-0.1.2.tar.gz/mollom-0.1.2/setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='mollom',
+  version='0.1.2',
+  description='Mollom client library.',
+  author='Huan Lai',
+  author_email='huan.lai@acquia.com',
+  url='https://github.com/Mollom/mollom_python',
+  packages=['mollom'],
+  install_requires=['requests-oauthlib'],
+  license='MIT',
+  classifiers=(
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7'
+  )
+)
