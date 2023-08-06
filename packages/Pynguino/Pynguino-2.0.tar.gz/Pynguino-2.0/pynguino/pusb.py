a1db5f3fccf63576fdf22c4a0f711e3075e7432f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:15 +0000

    Add package Pynguino/2.0/Pynguino-2.0.tar.gz

---
+++ packages/Pynguino/Pynguino-2.0.tar.gz/Pynguino-2.0/pynguino/pusb.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import time
+from .ptools import USBtools
+
+########################################################################
+class PynguinoUSB(USBtools):
+    """"""
+    #----------------------------------------------------------------------
+    def __init__(self, vboot="v4"):
+        super(PynguinoUSB, self).__init__(vboot)
+        
+        self.dict_format_mode = {"INPUT": "INPUT",
+                                 "1": "INPUT",
+                                 1: "INPUT",
+                                 "OUTPUT": "OUTPUT",
+                                 "0": "OUTPUT",
+                                 0: "OUTPUT",}
+        
+        self.dict_format_write = {"HIGH": "HIGH",
+                                  "1": "HIGH",
+                                  1: "HIGH",
+                                  "LOW": "LOW",
+                                  "0": "LOW",
+                                  0: "LOW",}
+        
+
+
+    #----------------------------------------------------------------------
+    def pinMode(self, pin, mode):
+        mode = self.dict_format_mode.get(mode, None)
+        if mode is None:
+            raise Exception("Mode value are incorrect")
+        self.write("pinMode(%d,%s)"%(pin, mode))
+        
+    #----------------------------------------------------------------------
+    def digitalWrite(self, pin, write):
+        write = self.dict_format_write.get(write, None)
+        if write is None:
+            raise Exception("write value are incorrect")
+        self.write("digitalWrite(%d,%s)"%(pin, write))
+        
+    #----------------------------------------------------------------------
+    def digitalRead(self, pin):
+        self.write("digitalRead(%d)"%pin)
+        return self.read()
+    
+    #----------------------------------------------------------------------
+    def analogWrite(self, pin, write):
+        self.write("analogWrite(%d,%s)"%(pin, str(write)))
+        
+    #----------------------------------------------------------------------
+    def analogRead(self, pin):
+        self.write("analogRead(%d)"%pin)
+        return self.read()
+        
+    #----------------------------------------------------------------------
+    def eepromRead(self, mem):
+        self.write("eepromRead(%d)"%mem)
+        return self.read()
+    
+    #----------------------------------------------------------------------
+    def eepromWrite(self, mem, value):
+        self.write("eepromWrite(%d,%s)"%(mem, str(value)))
+        
+    #----------------------------------------------------------------------
+    def delay(self, ms):
+        time.sleep(ms/1e3)
+        
+    #----------------------------------------------------------------------
+    def delayMicroseconds(self, us):
+        time.sleep(ms/1e6)
+        
+    #----------------------------------------------------------------------
+    def allOutput(self):
+        self.write("allOutput")
+        
+    #----------------------------------------------------------------------
+    def allInput(self):
+        self.write("allInput")
+        
+    #----------------------------------------------------------------------
+    def allHigh(self):
+        self.write("allHigh")
+
+    #----------------------------------------------------------------------
+    def allLow(self):
+        self.write("allLow")
+        
+    #----------------------------------------------------------------------
+    def reset(self):
+        self.write("reset")
