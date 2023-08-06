59355750fc15b3da9ed1de8456eb84ffb65de4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:17 +0000

    Add package libarchive/0.3.10/libarchive-0.3.10.tar.gz

---
+++ packages/libarchive/libarchive-0.3.10.tar.gz/libarchive-0.3.10/libarchive/__init__.py
@@ -0,0 +1,8 @@
+from libarchive.adapters.archive_read import \
+    file_enumerator, file_reader, file_pour, \
+    memory_enumerator, memory_reader, memory_pour
+
+from libarchive.adapters.archive_write import \
+    create_file
+
+#create_memory, 
