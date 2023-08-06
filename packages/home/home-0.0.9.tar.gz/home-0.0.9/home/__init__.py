eaedbef06f5c2838cc49c9bbc45f1be3c85b73ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:08 +0000

    Add package home/0.0.9/home-0.0.9.tar.gz

---
+++ packages/home/home-0.0.9.tar.gz/home-0.0.9/home/__init__.py
@@ -0,0 +1,27 @@
+__version__ = '0.0.9'
+
+from flask import Flask
+
+from home.config import TEMPLATE_FOLDER, STATIC_FOLDER
+from home.dash.api import api
+from home.dash.web import web
+from home.ts.models import db
+
+
+def create_app(config=None):
+    app = Flask(__name__,
+                static_folder=STATIC_FOLDER, template_folder=TEMPLATE_FOLDER)
+
+    app.config['SECRET_KEY'] = 'ssh, its a secret.'
+
+    app.config.from_object('home.config')
+    app.config.from_object(config)
+
+    app.register_blueprint(web)
+    app.register_blueprint(api, url_prefix='/api')
+
+    db.init_app(app)
+
+    return app
+
+app = create_app()
