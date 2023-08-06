14364017d0f95613cbb31d033b0de5d7be2e6d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:48 +0000

    Add package nameko/1.6.0/nameko-1.6.0.tar.gz

---
+++ packages/nameko/nameko-1.6.0.tar.gz/nameko-1.6.0/test/testing/test_utils.py
@@ -0,0 +1,90 @@
+import eventlet
+from mock import Mock
+import pytest
+
+from nameko.testing.utils import (
+    AnyInstanceOf, get_dependency, get_container, wait_for_call)
+
+
+def test_any_instance_of():
+
+    assert "" == AnyInstanceOf(str)
+    assert 99 != AnyInstanceOf(str)
+
+    class Foo(object):
+        pass
+    foo = Foo()
+
+    assert foo == AnyInstanceOf(Foo)
+    assert foo == AnyInstanceOf(object)
+    assert foo != AnyInstanceOf(foo)
+
+    assert repr(AnyInstanceOf(str)) == "<AnyInstanceOf-str>"
+    assert repr(AnyInstanceOf(Foo)) == "<AnyInstanceOf-Foo>"
+
+    assert AnyInstanceOf == AnyInstanceOf(type)
+    assert str == AnyInstanceOf(type)
+    assert int == AnyInstanceOf(type)
+    assert type == AnyInstanceOf(type)
+
+
+def test_wait_for_call():
+    mock = Mock()
+
+    def call_after(seconds):
+        eventlet.sleep(seconds)
+        mock.method()
+
+    # should not raise
+    eventlet.spawn(call_after, 0)
+    with wait_for_call(1, mock.method):
+        pass
+
+    mock.reset_mock()
+
+    with pytest.raises(eventlet.Timeout):
+        eventlet.spawn(call_after, 1)
+        with wait_for_call(0, mock.method):
+            pass
+
+
+def test_get_dependency(rabbit_config):
+
+    from nameko.messaging import QueueConsumer
+    from nameko.rpc import rpc, RpcProvider, RpcConsumer
+    from nameko.containers import ServiceContainer, WorkerContext
+
+    class Service(object):
+        @rpc
+        def foo(self):
+            pass
+
+        @rpc
+        def bar(self):
+            pass
+
+    container = ServiceContainer(Service, WorkerContext, rabbit_config)
+
+    rpc_consumer = get_dependency(container, RpcConsumer)
+    queue_consumer = get_dependency(container, QueueConsumer)
+    foo_rpc = get_dependency(container, RpcProvider, name="foo")
+    bar_rpc = get_dependency(container, RpcProvider, name="bar")
+
+    all_deps = container.dependencies
+    assert all_deps == set([rpc_consumer, queue_consumer, foo_rpc, bar_rpc])
+
+
+def test_get_container(runner_factory, rabbit_config):
+
+    class ServiceX(object):
+        name = "service_x"
+
+    class ServiceY(object):
+        name = "service_y"
+
+    runner = runner_factory(rabbit_config, ServiceX, ServiceY)
+    runner.start()
+
+    assert get_container(runner, ServiceX).service_cls is ServiceX
+    assert get_container(runner, ServiceY).service_cls is ServiceY
+    assert get_container(runner, object) is None
