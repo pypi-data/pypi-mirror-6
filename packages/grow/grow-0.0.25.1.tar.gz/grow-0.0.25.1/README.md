12c0975864ec1e321838844545a5c7eff286f15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:04 +0000

    Add package grow/0.0.25.1/grow-0.0.25.1.tar.gz

---
+++ packages/grow/grow-0.0.25.1.tar.gz/grow-0.0.25.1/README.md
@@ -0,0 +1,88 @@
+# Grow SDK [![Build Status](https://travis-ci.org/grow/pygrow.png?branch=master)](https://travis-ci.org/grow/pygrow)
+
+Welcome to the Grow SDK! Grow is almost certainly the best way for teams to build and launch web sites. It's a file-based static site generator and content management system.
+
+Grow works great for all kinds of content-heavy web sites – fom small personal blogs, to highly-interactive marketing campaigns, to informational web sites for a large organization.
+
+Grow differentiates itself by being:
+
+- entirely file-based and backed by Git (so it works with your favorite tools),
+- designed for true structured content management,
+- designed with web performance and security in mind,
+- designed for developers, designers, content writers, and translators to work together,
+- fully international with built-in localization support.
+
+Using Grow, you can:
+
+- develop everywhere (cloud or local, in a browser or on your machine),
+- deploy anywhere (Google Cloud Storage, S3, Dropbox, GitHub pages, App Engine, your custom server, etc.),
+- launch web sites on demand or at a specific time,
+- manage multiple sites (instead of just one),
+- collaborate with people, teams, and organizations (via roles, review processes, and project organization).
+
+Visit the Grow SDK's website at http://growsdk.org where you can learn all about using the Grow SDK to build and launch your own web sites.
+
+## In development
+
+The Grow SDK is still under development and is *not quite ready for use*. If you're interested in learning more about the project, please visit the SDK's public website at http://growsdk.org where you can subscribe to our newsletter and be notified when the . You can also watch this repository for updates.
+
+## Quick start usage
+
+Full documentation is available at http://growsdk.org, but this four-step process covers the basic workflow.
+
+(1) Install Grow. (Coming soon: a downloadable Mac application for those without `pip`.)
+
+    # Install Grow in Python's site-packages directory.
+    sudo pip install grow
+
+    # Or install Grow for a single user (recommended).
+    pip install --user grow
+
+    # Add --user pip installations to your PATH. (Put this in ~/.bashrc).
+    export PATH=$HOME/.local/bin:$PATH
+
+(2) Initialize a new pod using the "cards" theme.
+
+    grow init cards ~/example.com/
+
+(3) Run a small web server for live editing and previewing.
+
+    grow run ~/example.com/
+
+(4) Deploy your site to the public web.
+
+    grow deploy ~/example.com/
+
+See a full list of commands using: `grow help`.
+
+## Contributing
+
+### Getting the code
+
+We recommend using `virtualenv` to work on Grow in order to keep your system-wide installation separate from your working copy.
+
+    sudo pip install virtualenv                   # Install virtualenv if you don't have it.
+    virtualenv --no-site-packages <dir>           # Creates a new virtualenv in <dir>.
+    cd <dir>
+    source bin/activate                           # Activates the virtualenv.
+    git clone git@github.com:grow/pygrow.git      # Clones this repo.
+    ...
+    cd pygrow
+    pip install -r requirements.txt
+    ...
+    ./bin/grow                                    # Runs the Grow command line program.
+
+### Running tests
+
+To run Grow's unit tests, run from the project's root directory:
+
+    python run_tests.py
+
+The Grow SDK also includes service tests, which test the pod server RPC system:
+
+    ./bin/grow run grow/pods/testdata/pod/        # Start a dev server.
+    ./run_service_tests.sh
+
+## License
+
+The Grow SDK is released under the MIT License and it is lovingly developed by the [Grow SDK Project Authors](https://github.com/grow/pygrow/blob/master/LICENSE). Our mission is to bring joy to building and launching high-quality web sites.
---
+++ packages/grow/grow-0.0.25.1.tar.gz/grow-0.0.25.1/grow/commands.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+
+import logging
+import base64
+import gflags as flags
+import os
+from google.apputils import appcommands
+from grow.deployments import deployments
+from grow.client import client
+from grow.common import sdk_utils
+from grow.server import manager
+from grow.pods import commands as pod_commands
+from grow.pods import pods
+from grow.pods import storage
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_boolean(
+    'skip_sdk_update_check', False, 'Whether to skip the check for SDK updates.')
+
+
+
+class DeployCmd(appcommands.Cmd):
+  """Deploys a pod to a destination."""
+
+  def __init__(self, name, flag_values, command_aliases=None):
+    flags.DEFINE_boolean(
+        'confirm', True, 'Whether to skip the deployment confirmation.',
+        flag_values=flag_values)
+    flags.DEFINE_enum(
+        'destination', None, ['gcs', 'local', 's3', 'zip'],
+        'Destination to deploy to.',
+        flag_values=flag_values)
+    flags.DEFINE_string(
+        'bucket', None, 'Google Cloud Storage or Amazon S3 bucket.',
+        flag_values=flag_values)
+    flags.DEFINE_string(
+        'out_dir', None, 'Directory to write to.',
+        flag_values=flag_values)
+    flags.DEFINE_string(
+        'out_file', None, 'File name of zip file to use.',
+        flag_values=flag_values)
+    super(DeployCmd, self).__init__(name, flag_values, command_aliases=command_aliases)
+
+  def _get_deployment_from_command_line(self):
+    if FLAGS.destination is None:
+      raise appcommands.AppCommandsError('Must specify: --destination.')
+    elif FLAGS.destination == 'gcs':
+      if FLAGS.bucket is None:
+        raise appcommands.AppCommandsError('Must specify: --bucket.')
+      deployment = deployments.GoogleCloudStorageDeployment(confirm=FLAGS.confirm)
+      deployment.set_params(bucket=FLAGS.bucket)
+    elif FLAGS.destination == 's3':
+      if FLAGS.bucket is None:
+        raise appcommands.AppCommandsError('Must specify: --bucket.')
+      deployment = deployments.AmazonS3Deployment(confirm=FLAGS.confirm)
+      deployment.set_params(bucket=FLAGS.bucket)
+    elif FLAGS.destination == 'local':
+      if FLAGS.out_dir is None:
+        raise appcommands.AppCommandsError('Must specify: --out_dir.')
+      out_dir = os.path.abspath(os.path.expanduser(FLAGS.out_dir))
+      deployment = deployments.FileSystemDeployment(confirm=FLAGS.confirm)
+      deployment.set_params(out_dir=out_dir)
+    elif FLAGS.destination == 'zip':
+      if FLAGS.out_dir is None and FLAGS.out_file is None:
+        raise appcommands.AppCommandsError('Must specify either: --out_dir or --out_file.')
+      out_dir = os.path.abspath(os.path.expanduser(FLAGS.out_dir)) if FLAGS.out_dir else None
+      out_file = os.path.abspath(os.path.expanduser(FLAGS.out_file)) if FLAGS.out_file else None
+      deployment = deployments.ZipFileDeployment(confirm=FLAGS.confirm)
+      deployment.set_params(out_dir=out_dir, out_file=out_file)
+    return deployment
+
+  def Run(self, argv):
+    root = os.path.abspath(os.path.join(os.getcwd(), argv[-1]))
+    pod = pods.Pod(root, storage=storage.FileStorage)
+    pod.preprocess()
+
+    # Figure out if we're using the default deployment.
+    if len(argv) == 2 and not FLAGS.destination:
+      deployment_name = 'default'  # grow deploy .
+    elif len(argv) == 3:
+      deployment_name = argv[-2]   # grow deploy <name> .
+    else:
+      deployment_name = None       # grow deploy --destination=<kind> .
+
+    if deployment_name:
+      deployment = pod.get_deployment(deployment_name, confirm=FLAGS.confirm)
+    else:
+      deployment = self._get_deployment_from_command_line()
+    deployment.deploy(pod)
+
+
+class DumpCmd(appcommands.Cmd):
+  """Generates static files and dumps them to a local destination."""
+
+  def Run(self, argv):
+    root = os.path.abspath(os.path.join(os.getcwd(), argv[-2]))
+    out_dir = os.path.abspath(os.path.join(os.getcwd(), argv[-1]))
+    pod = pods.Pod(root, storage=storage.FileStorage)
+    pod.dump(out_dir=out_dir)
+
+
+class ExtractCmd(appcommands.Cmd):
+  """Extracts a pod's translations into messages files."""
+
+  def __init__(self, name, flag_values, command_aliases=None):
+    flags.DEFINE_boolean(
+        'init', False,
+        'Whether to init catalogs (wipes out existing translations) or '
+        'update them (merges new strings into existing catalogs).',
+        flag_values=flag_values)
+    super(ExtractCmd, self).__init__(name, flag_values, command_aliases=command_aliases)
+
+  def Run(self, argv):
+    root = os.path.abspath(os.path.join(os.getcwd(), argv[-1]))
+    pod = pods.Pod(root, storage=storage.FileStorage)
+    translations = pod.get_translations()
+    translations.extract()
+    locales = pod.list_locales()
+    if not locales:
+      logging.info('No pod-specific locales defined, '
+                   'skipped generating locale-specific catalogs.')
+    else:
+      if FLAGS.init:
+        translations.init_catalogs(locales)
+      else:
+        translations.update_catalogs(locales)
+
+
+class InitCmd(appcommands.Cmd):
+  """Initializes a pod using a theme, ready for development."""
+
+  def Run(self, argv):
+    if len(argv) != 3:
+      raise Exception('Usage: grow init <repo> <pod root>')
+    root = os.path.abspath(os.path.join(os.getcwd(), argv[-1]))
+    theme_url = argv[-2]
+    pod = pods.Pod(root, storage=storage.FileStorage)
+    pod_commands.init(pod, theme_url)
+
+
+class RoutesCmd(appcommands.Cmd):
+  """Lists routes defined by a pod."""
+
+  def Run(self, argv):
+    if len(argv) != 2:
+      raise Exception('Usage: grow routes <pod root>')
+    root = os.path.abspath(os.path.join(os.getcwd(), argv[-1]))
+    pod = pods.Pod(root, storage=storage.FileStorage)
+    routes = pod.get_routes()
+    logging.info(routes.to_message())
+
+
+class RunCmd(appcommands.Cmd):
+  """Starts a pod server for a single pod."""
+
+  def __init__(self, name, flag_values, command_aliases=None):
+    flags.DEFINE_string(
+        'host', 'localhost', 'IP address or hostname to bind the server to.',
+        flag_values=flag_values)
+    flags.DEFINE_integer(
+        'port', '8080', 'Port to start the server on.',
+        flag_values=flag_values)
+    super(RunCmd, self).__init__(name, flag_values, command_aliases=command_aliases)
+
+  def Run(self, argv):
+    if len(argv) != 2:
+      raise Exception('Must specify pod directory.')
+    root = os.path.abspath(os.path.join(os.getcwd(), argv[-1]))
+    if not FLAGS.skip_sdk_update_check:
+      sdk_utils.check_version(quiet=True)
+    pod = pods.Pod(root, storage=storage.FileStorage)
+    manager.start(pod, host=FLAGS.host, port=FLAGS.port)
+
+
+class TestCmd(appcommands.Cmd):
+  """Validates a pod and runs its tests."""
+
+  def Run(self):
+    raise NotImplementedError()
+
+
+class UpCmd(appcommands.Cmd):
+  """Uploads a pod to a pod server."""
+
+  SKIP_PATTERNS = (
+      '.DS_Store',
+  )
+
+  def __init__(self, name, flag_values, command_aliases=None):
+    flags.DEFINE_string(
+        'host', 'beta.grow.io', 'Pod server hostname (ex: beta.grow.io.).',
+        flag_values=flag_values)
+    flags.DEFINE_string(
+        'project', None, 'Project ID (ex: foo/bar).',
+        flag_values=flag_values)
+    super(UpCmd, self).__init__(name, flag_values, command_aliases=command_aliases)
+
+  def Run(self, argv):
+    owner, nickname = FLAGS.project.split('/')
+    host = FLAGS.host
+    pod = pods.Pod(argv[1], storage=storage.FileStorage)
+
+    # Uploading to GrowEdit on dev appserver.
+    if True or (host is not None and 'localhost' in host):
+      service = client.Client(host=FLAGS.host)
+      for pod_path in pod.list_dir('/'):
+        if os.path.basename(pod_path) in UpCmd.SKIP_PATTERNS:
+          continue
+        path = os.path.join(pod.root, pod_path)
+        content = open(path).read()
+        if isinstance(content, unicode):
+          content = content.encode('utf-8')
+        content = base64.b64encode(content)
+        service.rpc('pods.update_file', {
+           'project': {
+             'owner': {'nickname': owner},
+             'nickname': nickname,
+           },
+           'file': {
+             'pod_path': pod_path,
+             'content_b64': content,
+           }
+        })
+        print 'Uploaded: {}'.format(pod_path)
+#      req = service.pods().finalizeStagedFiles(body={
+#        'pod': {'changeset': changeset}
+#      })
+#      req.execute()
+      print 'Upload finalized.'
+    else:
+      raise NotImplementedError()
+#      google_cloud_storage.upload_to_gcs(pod, changeset, host=host)
+
+
+def add_commands():
+  appcommands.AddCmd('deploy', DeployCmd)
+  appcommands.AddCmd('dump', DumpCmd)
+  appcommands.AddCmd('extract', ExtractCmd)
+  appcommands.AddCmd('init', InitCmd)
+  appcommands.AddCmd('run', RunCmd)
+  appcommands.AddCmd('routes', RoutesCmd)
+  appcommands.AddCmd('up', UpCmd)
+  appcommands.AddCmd('test', TestCmd)
