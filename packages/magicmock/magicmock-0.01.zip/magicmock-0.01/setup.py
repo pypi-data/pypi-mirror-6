58de6093f635ba4a8cdbcd3a75a4d2145b9ad4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:17 +0000

    Add package magicmock/0.01/magicmock-0.01.zip

---
+++ packages/magicmock/magicmock-0.01.zip/magicmock-0.01/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='magicmock',
+    version='0.01',
+    author='Mingze',
+    author_email='mzxu@outlook.com',
+    packages=['magicmock'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points = {
+        'console_scripts' : [
+            'magicmock = magicmock:main',
+            'magicmocksetup = magicmock:setup'
+        ]
+    },
+    license="MIT",
+    url='https://github.com/mzxu/magicmock',
+    install_requires=["httplib2"],
+    description='A generic mock server',
+    long_description="Fake http request and response",
+    )
