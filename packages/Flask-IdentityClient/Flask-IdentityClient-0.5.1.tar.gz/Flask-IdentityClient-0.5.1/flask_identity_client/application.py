766113915f6afe0635f17026d3ee191113a783b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package Flask-IdentityClient/0.5.1/Flask-IdentityClient-0.5.1.tar.gz

---
+++ packages/Flask-IdentityClient/Flask-IdentityClient-0.5.1.tar.gz/Flask-IdentityClient-0.5.1/flask_identity_client/application.py
@@ -0,0 +1,9 @@
+# coding: UTF-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from flask import Blueprint
+
+__all__ = ['blueprint']
+
+
+blueprint = Blueprint('identity_client', 'identity_client')
