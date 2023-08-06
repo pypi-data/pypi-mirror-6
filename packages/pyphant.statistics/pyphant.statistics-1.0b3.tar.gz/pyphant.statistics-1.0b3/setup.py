53552abfee6e9eeb13010e51e535c5f0a4594d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package pyphant.statistics/1.0b3/pyphant.statistics-1.0b3.tar.gz

---
+++ packages/pyphant.statistics/pyphant.statistics-1.0b3.tar.gz/pyphant.statistics-1.0b3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pyphant Statistics toolbox
+This is the Statistics toolbox, that provides workers
+for the Pyphant framework. In order to use it you must have
+the Pyphant framework installed first. Visit
+http://pyphant.sourceforge.net for more information.
+"""
+
+
+VERSION = '1.0b3'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyphant.statistics",
+    version=VERSION,
+    author="Alexander Held, Andreas W. Liehr, Klaus Zimmermann",
+    author_email='alexander.held@fmf.uni-freiburg.de',
+    maintainer='Alexander Held',
+    maintainer_email='alexander.held@fmf.uni-freiburg.de',
+    license="BSD",
+    description=__doc__,
+    install_requires=[
+        'pyphant>=1.0b3',
+        'numpy',
+        'scipy',
+        ],
+    packages=find_packages(),
+    entry_points="""
+    [pyphant.workers]
+    myeentry = Statistics
+    """,
+    include_package_data=True,
+    test_suite='Statistics.tests'
+    )
