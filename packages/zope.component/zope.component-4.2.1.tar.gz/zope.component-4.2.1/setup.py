24e5d84c28e14a73c1094d228bc5f17df4f36cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:59 +0000

    Add package zope.component/4.2.1/zope.component-4.2.1.tar.gz

---
+++ packages/zope.component/zope.component-4.2.1.tar.gz/zope.component-4.2.1/setup.py
@@ -0,0 +1,128 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# http://docs.zope.org/zopetoolkit
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.component package
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+TESTS_REQUIRE = [
+    'zope.testing',
+    'zope.component[hook]',
+    'zope.component[persistentregistry]',
+    'zope.component[security]',
+    'zope.component[zcml]',
+    ]
+
+def _modname(path, base, name=''):
+    if path == base:
+        return name
+    dirname, basename = os.path.split(path)
+    return _modname(dirname, base, basename + '.' + name)
+
+def alltests():
+    import logging
+    import pkg_resources
+    import unittest
+
+    class NullHandler(logging.Handler):
+        level = 50
+
+        def emit(self, record):
+            pass
+
+    logging.getLogger().addHandler(NullHandler())
+
+    suite = unittest.TestSuite()
+    base = pkg_resources.working_set.find(
+        pkg_resources.Requirement.parse('zope.component')).location
+    for dirpath, dirnames, filenames in os.walk(base):
+        if os.path.basename(dirpath) == 'tests':
+            for filename in filenames:
+                if ( filename.endswith('.py') and
+                     filename.startswith('test') ):
+                    mod = __import__(
+                        _modname(dirpath, base, os.path.splitext(filename)[0]),
+                        {}, {}, ['*'])
+                    suite.addTest(mod.test_suite())
+    return suite
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name='zope.component',
+    version='4.2.1',
+    url='http://pypi.python.org/pypi/zope.component',
+    license='ZPL 2.1',
+    description='Zope Component Architecture',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(
+        read('README.rst')
+        + '\n' +
+        read('CHANGES.rst')
+        ),
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Zope3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    namespace_packages=['zope',],
+    tests_require = TESTS_REQUIRE,
+    test_suite='__main__.alltests',
+    install_requires=['setuptools',
+                      'zope.interface>=4.1.0',
+                      'zope.event',
+                      ],
+    include_package_data = True,
+    zip_safe = False,
+    extras_require = {
+        'hook': ['zope.hookable'],
+        'persistentregistry': ['persistent'],
+        'security': ['zope.location',
+                    'zope.proxy',
+                    'zope.security',
+                    ],
+        'zcml': ['zope.configuration',
+                'zope.i18nmessageid',
+                ],
+        'test': TESTS_REQUIRE,
+        'testing': TESTS_REQUIRE + ['nose', 'coverage'],
+        'docs': ['Sphinx', 'repoze.sphinx.autointerface'],
+        },
+    )
