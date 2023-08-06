3021a96d73223eb2fb222ec7605507b47d93be2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:09 +0000

    Add package zeam.form.ztk/1.3.4/zeam.form.ztk-1.3.4.tar.gz

---
+++ packages/zeam.form.ztk/zeam.form.ztk-1.3.4.tar.gz/zeam.form.ztk-1.3.4/src/zeam/form/ztk/widgets/tests/test_doctest.py
@@ -0,0 +1,23 @@
+
+import unittest
+from zope.testing import doctest
+from zeam.form.ztk.testing import FunctionalLayer
+
+def test_suite():
+    optionflags = doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS
+    globs= {}
+
+    suite = unittest.TestSuite()
+    for filename in ['bool.txt', 'choice.txt', 'collection_set.txt',
+                     'collection_list.txt', 'collection_object.txt',
+                     'multichoice.txt', 'object.txt', 'date.txt',
+                     'radio.txt', 'uri.txt', 'text.txt', 'time.txt',
+                     'currency.txt', 'number.txt']:
+        test = doctest.DocFileSuite(
+            filename,
+            optionflags=optionflags,
+            globs=globs)
+        test.layer = FunctionalLayer
+        suite.addTest(test)
+
+    return suite
