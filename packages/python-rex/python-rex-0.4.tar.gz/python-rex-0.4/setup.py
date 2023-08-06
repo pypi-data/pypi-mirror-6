37dc9725099ceac29d5c8c713c161872f2a72b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:18 +0000

    Add package python-rex/0.4/python-rex-0.4.tar.gz

---
+++ packages/python-rex/python-rex-0.4.tar.gz/python-rex-0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='python-rex',
+    version='0.4',
+    packages=[],
+    py_modules=['rex'],
+    url='https://github.com/cypreess/python-rex',
+    license='MIT',
+    author='Krzysztof Dorosz',
+    author_email='cypreess@gmail.com',
+    description='Python regular expressions for humans',
+    install_requires=['six'],
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ),
+)
