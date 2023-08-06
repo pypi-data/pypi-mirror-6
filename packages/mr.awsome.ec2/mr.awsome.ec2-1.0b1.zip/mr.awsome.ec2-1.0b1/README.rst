f6f0a74df372b3910beac2f499f4f7107447d56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:05 +0000

    Add package mr.awsome.ec2/1.0b1/mr.awsome.ec2-1.0b1.zip

---
+++ packages/mr.awsome.ec2/mr.awsome.ec2-1.0b1.zip/mr.awsome.ec2-1.0b1/README.rst
@@ -0,0 +1,13 @@
+Changelog
+=========
+
+1.0b2 - Unreleased
+------------------
+
+
+
+1.0b1 - 2014-03-24
+------------------
+
+* Initial release
+  [fschulze]
---
+++ packages/mr.awsome.ec2/mr.awsome.ec2-1.0b1.zip/mr.awsome.ec2-1.0b1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+
+version = "1.0b1"
+
+
+setup(
+    version=version,
+    description="A plugin for mr.awsome providing integration with Amazon EC2.",
+    long_description=README + "\n\n",
+    name="mr.awsome.ec2",
+    author='Florian Schulze',
+    author_email='florian.schulze@gmx.net',
+    url='http://github.com/fschulze/mr.awsome.ec2',
+    include_package_data=True,
+    zip_safe=False,
+    packages=['mr'],
+    namespace_packages=['mr'],
+    install_requires=[
+        'setuptools',
+        'mr.awsome >= 1.0dev',
+        'boto >= 2.0',
+        'lazy'],
+    setup_requires=[
+        'setuptools-git'],
+    entry_points="""
+        [mr.awsome.plugins]
+        ec2 = mr.awsome_ec2:plugin
+    """)
