186e996f5a6b0f9dcccd26e4276058e3049fa136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:30 +0000

    Add package codeedu/0.20/codeedu-0.20.tar.gz

---
+++ packages/codeedu/codeedu-0.20.tar.gz/codeedu-0.20/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import os
+
+setup(name='codeedu',
+      version='0.20',
+      description='a simple tool to pick a random github repo',
+      long_description = 'example input: codeEDU python',
+      url='https://github.com/jxieeducation/codeEDU',
+      author='Jason Xie',
+      author_email='jxieeducation@berkeley.edu',
+      license='BST',
+      install_requires=[
+        'splinter',
+      ],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+      ],
+      entry_points = {
+        'console_scripts': ['codeEDU=codeEDU.code_edu:command_line'],
+      },
+      keywords='github learn read code',
+      packages=['codeEDU'],
+      zip_safe=False)
+
+#thanks to http://www.scotttorborg.com/python-packaging/minimal.html
