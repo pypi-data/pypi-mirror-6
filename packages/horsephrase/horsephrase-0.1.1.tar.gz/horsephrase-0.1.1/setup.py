8a467be241aa2c9ad9b5daa7f9ffaae4f0e046f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:24 +0000

    Add package horsephrase/0.1.1/horsephrase-0.1.1.tar.gz

---
+++ packages/horsephrase/horsephrase-0.1.1.tar.gz/horsephrase-0.1.1/setup.py
@@ -0,0 +1,30 @@
+
+from setuptools import setup
+
+setup(
+    name="horsephrase",
+    version="0.1.1",
+    description="Secure password generator.",
+    long_description=(
+        "Like http://correcthorsebatterystaple.net/ except it's not a web page"
+        " which is logging your passwords and sending them all to the NSA."
+    ),
+    author="Glyph",
+    author_email="glyph@twistedmatrix.com",
+    maintainer="Glyph",
+    maintainer_email="glyph@twistedmatrix.com",
+    url="https://github.com/glyph/Horsephrase/",
+    packages=["horsephrase"],
+    package_data=dict(
+        horsephrase=["*.txt"],
+    ),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+    ],
+    entry_points={
+        "console_scripts": [
+            "horsephrase = horsephrase:generate",
+        ],
+    },
+)
