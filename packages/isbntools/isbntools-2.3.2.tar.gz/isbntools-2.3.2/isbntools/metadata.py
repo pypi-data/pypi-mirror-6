ae4b6a8df26a69d866935cea68cd707cac3e08ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:03 +0000

    Add package isbntools/2.3.2/isbntools-2.3.2.tar.gz

---
+++ packages/isbntools/isbntools-2.3.2.tar.gz/isbntools-2.3.2/isbntools/metadata.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .registry import services
+from .exceptions import NotRecognizedServiceError
+
+
+def query(isbn, service='default'):
+    """
+    Queries worldcat.org, Google Books (JSON API), ... for metadata
+    """
+    if service != 'default' and service not in services:
+        raise NotRecognizedServiceError(service)
+    return services[service](isbn)
---
+++ packages/isbntools/isbntools-2.3.2.tar.gz/isbntools-2.3.2/isbntools/version.py
@@ -0,0 +1,47 @@
+import os
+import re
+import urllib2
+
+
+def set_red():
+    return '\x1b[38;5;9m' if os.name != 'nt' else ''
+
+
+def set_blue():
+    return '\x1b[38;5;12m' if os.name != 'nt' else ''
+
+
+def reset():
+    return '\x1b[0m' if os.name != 'nt' else ''
+
+
+def check_version():
+    """
+    Checks online if there is a new version of isbntools
+    """
+    try:
+        from isbntools import __version__
+
+        UA = "isbntools (%s)" % __version__
+        headers = {'User-Agent': UA}
+
+        url = "https://raw.githubusercontent.com/xlcnd/"\
+              "isbntools/master/isbntools/__init__.py"
+        RE_VERSION = re.compile("__version__\s*=\s*'(.*)'")
+
+        request = urllib2.Request(url, headers=headers)
+        content = urllib2.urlopen(request).read()
+
+        newversion = re.search(RE_VERSION, content).group(1)
+
+        if __version__ != newversion:
+            print(set_red())
+            print((" ** A new version (%s) is available! **" % newversion))
+            print(set_blue())
+            print((" At command line enter: [sudo] pip install -U isbntools"))
+            print("    or")
+            print((" Download it from %s"
+                  % "https://pypi.python.org/pypi/isbntools"))
+            print(reset())
+    finally:
+        print("")
