4da041b6ecdd51be7301439388ec9c70ea816b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:48 +0000

    Add package pypsdier/0.1.4/Pypsdier-0.1.4.tar.gz

---
+++ packages/pypsdier/Pypsdier-0.1.4.tar.gz/Pypsdier-0.1.4/pypsdier/__init__.py
@@ -0,0 +1,7 @@
+# Main functions to be used directly from pypsdier
+from core.ode_library import ode
+from core.pde_library import pde
+from core.installation_test_library import test
+
+# Versioning
+version = "0.1.4"
