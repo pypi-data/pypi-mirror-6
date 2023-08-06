ea116a6b18a3274dae9a17c1ad39d65cf850a337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:30 +0000

    Add package mobile-balance/0.3.2/mobile-balance-0.3.2.tar.gz

---
+++ packages/mobile-balance/mobile-balance-0.3.2.tar.gz/mobile-balance-0.3.2/mobile_balance/megafon.py
@@ -0,0 +1,22 @@
+import requests
+import re
+
+
+def get_balance(number, password):
+    s = requests.Session()
+
+    response = s.get('https://lk.megafon.ru/login/')
+    csrf_token = re.search(r'name=CSRF value="(.*?)"', response.content).group(1)
+
+    response = s.post('https://lk.megafon.ru/dologin/',
+                      data={'j_username': number,
+                            'j_password': password,
+                            'CSRF': csrf_token,
+                        })
+
+    content = s.get('https://lk.megafon.ru/').content
+    match = re.search(r'<div class="[^"]*ui-label-balance".*?((&minus;)?[\d,]+|[\d,]+).*?</div>',
+                      content)
+    
+    if match is not None:
+        return float(match.group(1).replace(',', '.').replace('&minus;', '-'))
