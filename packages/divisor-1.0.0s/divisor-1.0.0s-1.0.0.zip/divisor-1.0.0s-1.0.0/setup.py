ff41ceb156d85b307e394f13bd9f55b41ffade5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:03 +0000

    Add package divisor-1.0.0s/1.0.0/divisor-1.0.0s-1.0.0.zip

---
+++ packages/divisor-1.0.0s/divisor-1.0.0s-1.0.0.zip/divisor-1.0.0s-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'divisor-1.0.0s',
+    version = '1.0.0',
+    py_modules = ['divisor'],
+    author = 'group2',
+    author_email = 'jsh001111@daum.com',
+    url = '-',
+    description = 'Seek divisor',
+    )
