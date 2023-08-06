9a2ae49fd2d5a0daf6d0bd0dd92915679b8985f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:37 +0000

    Add package pykachu/0.1.2/pykachu-0.1.2.tar.gz

---
+++ packages/pykachu/pykachu-0.1.2.tar.gz/pykachu-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pykachu',
+    version='0.1.2',
+    packages=['pykachu'],
+    url='https://github.com/sievetech/pykachu',
+    license='3-BSD',
+    author='Sieve',
+    author_email='sievetech@sieve.com.br',
+    description='Monitoramento simples de tarefas em background usando Redis como backend ',
+    install_requires=["redis"]
+)
