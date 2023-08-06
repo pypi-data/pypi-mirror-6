dec9aab4a3558dc7a7a7589aa3e1e8d12ce1146f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:41 +0000

    Add package PyMoJo/0.4/PyMoJo-0.4.tar.gz

---
+++ packages/PyMoJo/PyMoJo-0.4.tar.gz/PyMoJo-0.4/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name='PyMoJo',
+  version='0.4',
+  author='Ryan Jung',
+  author_email='gradysghost@gmail.com',
+  packages=['pymojo'],
+  url='https://github.com/GradysGhost/pymojo',
+  license='LICENSE',
+  description='PyJoJo client library',
+  long_description=open('README.txt').read(),
+  install_requires=[
+    'requests == 1.1.0'
+  ],
+  entry_points={
+    'console_scripts' : [
+      'mojo = pymojo.mojo:main'
+    ]
+  }
+)
