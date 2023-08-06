9bf0ac06d909aa219a7b6b5c82346da1bd2b7804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:28 +0000

    Add package gitpass/0.1.3/gitpass-0.1.3.tar.gz

---
+++ packages/gitpass/gitpass-0.1.3.tar.gz/gitpass-0.1.3/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import distribute_setup
+from distutils.core import setup
+distribute_setup.use_setuptools()
+
+
+setup(name='gitpass',
+      version='0.1.3',
+      description="Gitpass is an extension of the Python standard library's " \
+      + "getpass, designed for keeping passwords out of your git repository.",
+      author='Dustin Smith',
+      author_email='dustin@media.mit.edu',
+      download_url='https://github.com/dasmith/gitpass/tarball/master',
+      url='https://github.com/dasmith/gitpass',
+      packages=['gitpass'])
+
+
