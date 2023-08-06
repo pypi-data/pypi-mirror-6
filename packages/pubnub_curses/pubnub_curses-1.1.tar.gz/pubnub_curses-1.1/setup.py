c0638b56afb9fb615a624a2cbd177572823d4b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:04 +0000

    Add package pubnub_curses/1.1/pubnub_curses-1.1.tar.gz

---
+++ packages/pubnub_curses/pubnub_curses-1.1.tar.gz/pubnub_curses-1.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='pubnub_curses',
+  version='1.1',
+  description='PubNub Curses Terminal Client',
+  author='Dan Ristic',
+  author_email='danr@pubnub.com',
+  url='http://pubnub.com',
+  packages=['pubnub_curses'],
+  scripts=['bin/pubnub-curses']
+)
