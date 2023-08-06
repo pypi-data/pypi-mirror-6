3f869f60d72a43fccaa423b62cb0c14dd29a8e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:21 +0000

    Add package konfig/0.9/konfig-0.9.tar.gz

---
+++ packages/konfig/konfig-0.9.tar.gz/konfig-0.9/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    README = f.read()
+
+
+mplv2 = "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"
+classifiers = ["Programming Language :: Python", mplv2,
+               "Development Status :: 5 - Production/Stable"]
+
+
+setup(name='konfig',
+      version='0.9',
+      url='https://github.com/mozilla-services/konfig',
+      packages=find_packages(),
+      long_description=README,
+      description=("Yet Another Config Parser."),
+      author="Tarek Ziade",
+      author_email="tarek@mozilla.com",
+      include_package_data=True,
+      install_requires = [
+        'configparser', 'argparse'
+      ],
+      zip_safe=False,
+      classifiers=classifiers,
+      )
+
