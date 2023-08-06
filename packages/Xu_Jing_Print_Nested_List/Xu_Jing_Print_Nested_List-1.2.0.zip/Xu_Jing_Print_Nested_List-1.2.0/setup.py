f14f444a3f95e6f11b84174249bfc688a556a300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:51 +0000

    Add package Xu_Jing_Print_Nested_List/1.2.0/Xu_Jing_Print_Nested_List-1.2.0.zip

---
+++ packages/Xu_Jing_Print_Nested_List/Xu_Jing_Print_Nested_List-1.2.0.zip/Xu_Jing_Print_Nested_List-1.2.0/setup.py
@@ -0,0 +1,14 @@
+__author__ = 'Xu Jing'
+
+from distutils import core
+
+core.setup(
+    name = 'Xu_Jing_Print_Nested_List',
+    version = '1.2.0',
+    py_modules = ['PrintNestedList'],
+    author = 'Xu Jing',
+    author_email = 'victorxujing@gmail.com',
+    description = '打印嵌套列表'
+)
+
+
