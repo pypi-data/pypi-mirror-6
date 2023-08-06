1152e9d666c927bef907f71dbe76bdea121d6ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:00 +0000

    Add package django-query-parameters/0.1.3/django-query-parameters-0.1.3.tar.gz

---
+++ packages/django-query-parameters/django-query-parameters-0.1.3.tar.gz/django-query-parameters-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='django-query-parameters',
+    version='0.1.3',
+    author='Jean-Marc Skopek',
+    author_email='jean-marc@skopek.ca',
+    packages=['query_parameters','query_parameters.templatetags'],
+    scripts=[],
+    url='https://github.com/jskopek/django-query-parameters',
+    license='LICENSE',
+    description='Django templatetags to simplify creating, updating, and removing query parameters from querystring',
+    long_description=open('README.rst').read(),
+    install_requires=['Django >= 1.4']
+)
+
+        
