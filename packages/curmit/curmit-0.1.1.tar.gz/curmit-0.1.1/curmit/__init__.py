61e2b80060ec352848f7a2cf001d915adb8dbeeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:26 +0000

    Add package curmit/0.1.1/curmit-0.1.1.tar.gz

---
+++ packages/curmit/curmit-0.1.1.tar.gz/curmit-0.1.1/curmit/__init__.py
@@ -0,0 +1,17 @@
+"""
+Package for curmit.
+"""
+
+from pkg_resources import get_distribution, DistributionNotFound
+
+__project__ = 'curmit'
+__version__ = None  # required for initial installation
+
+CLI = 'curmit'
+
+try:
+    __version__ = get_distribution(__project__).version  # pylint: disable=E1103
+except DistributionNotFound:  # pragma: no cover, manual test
+    VERSION = __project__ + '-' + '(local)'
+else:
+    VERSION = __project__ + '-' + __version__
