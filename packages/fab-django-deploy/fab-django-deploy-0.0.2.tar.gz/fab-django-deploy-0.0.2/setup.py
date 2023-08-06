1b70efff5031f1606d80666c66cac9850fc5bd72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:07 +0000

    Add package fab-django-deploy/0.0.2/fab-django-deploy-0.0.2.tar.gz

---
+++ packages/fab-django-deploy/fab-django-deploy-0.0.2.tar.gz/fab-django-deploy-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+version = '0.0.2'
+
+
+install_requires = (
+    'fabric>=1.8',
+)
+
+
+setup(
+    name='fab-django-deploy',
+    py_modules=['fab_deploy'],
+    version=version,
+    description='',
+    long_description='',
+    author='incuna',
+    author_email='admin@incuna.com',
+    url='https://github.com/incuna/fab-django-deploy/',
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+)
