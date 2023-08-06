2e832f12413e876c0d472d85b0dfb979ae989b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:15 +0000

    Add package django-prototype/0.2/django-prototype-0.2.tar.gz

---
+++ packages/django-prototype/django-prototype-0.2.tar.gz/django-prototype-0.2/prototype/settings.py
@@ -0,0 +1,17 @@
+SECRET_KEY = 'a'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+}
+
+INSTALLED_APPS = (
+    'prototype',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    'templates',
+)
