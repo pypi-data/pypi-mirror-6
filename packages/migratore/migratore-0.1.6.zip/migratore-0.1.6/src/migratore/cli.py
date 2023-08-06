1386f5fd6ad6e555f6be50240b9d3c1e359281c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:31 +0000

    Add package migratore/0.1.6/migratore-0.1.6.zip

---
+++ packages/migratore/migratore-0.1.6.zip/migratore-0.1.6/src/migratore/cli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import info
+import migration
+
+def run_help():
+    print "%s %s (%s)" % (info.NAME, info.VERSION, info.AUTHOR)
+    print ""
+    print "The most commonly used migratore commands are:"
+    print "  version          Prints the current version of migratore"
+    print "  list             Lists the executed migrations on the current database"
+    print "  upgrade [path]   Executes the pending migrations using the defined directory or current"
+    print "  generates [path] Generates a new migration file into the target path"
+
+def run_version():
+    print "%s %s" % (info.NAME, info.VERSION)
+
+def run_list():
+    migration.Migration.list()
+
+def run_upgrade(path = None):
+    migration.Migration.upgrade(path)
+
+def run_generate(path = None):
+    migration.Migration.generate(path)
