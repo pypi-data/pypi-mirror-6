3e1e1240b371e4ecf8a00830a5e35d81efad3cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:41 +0000

    Add package ramyaexample/1.4.0/ramyaexample-1.4.0.zip

---
+++ packages/ramyaexample/ramyaexample-1.4.0.zip/ramyaexample-1.4.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name         = 'ramyaexample',
+    version      = '1.4.0',
+    py_modules   = ['ramyaexample'],
+    author       = 'RamyaK',
+    author_email = 'ramyakrshnakolla@gmail.com',
+    description  = 'Four arguments',
+    )
