e406ade114a010efd8dcee290dab94c0054320f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:27 +0000

    Add package Trixy/1.0.0/Trixy-1.0.0.tar.gz

---
+++ packages/Trixy/Trixy-1.0.0.tar.gz/Trixy-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Trixy',
+    author='Austin Hartzheim',
+
+    version='1.0.0',
+    packages=['trixy'],
+    license='GNU GPL v3',
+    description='Python proxy server',
+    long_description=open('README.txt').read(),
+    url='austinhartzheim.me/projects/python3-trixy/'
+)
