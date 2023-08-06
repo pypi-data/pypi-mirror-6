de366ddcc2b68dadafe2d7ebec08e80dbdad40ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:09 +0000

    Add package imhotep_flake8/0.1/imhotep_flake8-0.1.tar.gz

---
+++ packages/imhotep_flake8/imhotep_flake8-0.1.tar.gz/imhotep_flake8-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'imhotep_flake8',
+    packages = ['imhotep_flake8'], 
+    version = '0.1',
+    description = 'An imhotep plugin for flake8 validation.',
+    author = 'Giorgos Logiotatidis',
+    author_email = 'giorgos@mozilla.com',
+    url = 'https://github.com/glogiotatidis/imhotep_flake8',
+    download_url = 'https://github.com/glogiotatidis/imhotep_flake8/tarball/0.1', 
+    keywords = ['testing', 'imhotep'], 
+    classifiers = [],
+)
