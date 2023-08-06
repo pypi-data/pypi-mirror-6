1973da15f3e5e9bdd75635cf17e4e7c1e45093e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:31 +0000

    Add package pyphant.tools/1.0b3/pyphant.tools-1.0b3.tar.gz

---
+++ packages/pyphant.tools/pyphant.tools-1.0b3.tar.gz/pyphant.tools-1.0b3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Pyphant Tools toolbox
+Visit http://pyphant.sourceforge.net for more information.
+"""
+
+VERSION = '1.0b3'
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyphant.tools",
+    version=VERSION,
+    author="Alexander Held, Klaus Zimmermann",
+    author_email='alexander.held@fmf.uni-freiburg.de',
+    maintainer='Alexander Held',
+    maintainer_email='alexander.held@fmf.uni-freiburg.de',
+    license="BSD",
+    description=__doc__,
+    install_requires=['pyphant>=1.0b3'],
+    packages=find_packages(),
+    entry_points="""
+    [pyphant.workers]
+    myeentry = tools
+    """,
+    include_package_data=True,
+    test_suite='tools.tests'
+    )
