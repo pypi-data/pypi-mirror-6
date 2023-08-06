73c8ab090630d944692d4fbd02bac4af86cf31de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:55 +0000

    Add package nose-descriptionfixer/0.0.2/nose-descriptionfixer-0.0.2.tar.gz

---
+++ packages/nose-descriptionfixer/nose-descriptionfixer-0.0.2.tar.gz/nose-descriptionfixer-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='nose-descriptionfixer',
+    author='Nat Williams',
+    author_email='nat.williams@gmail.com',
+    maintainer='Simon Weber',
+    maintainer_email='simon@simonmweber.com',
+    version='0.0.2',
+    description=('A Nose plugin to fix the way tests are described when the '
+                 'verbose flag (-v) is used'),
+    long_description=open('README.rst').read(),
+    entry_points={
+        'nose.plugins.0.10': [
+            'descriptionfixer = description_fixer:DescriptionFixer',
+        ]
+    },
+    py_modules = ['description_fixer'],
+    include_package_data=True,
+    zip_safe=False,
+)
