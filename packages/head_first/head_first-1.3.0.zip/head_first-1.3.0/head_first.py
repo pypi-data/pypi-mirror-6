76ce98214a3f47ecdb2e79c24fdc2959f479f9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:20 +0000

    Add package head_first/1.3.0/head_first-1.3.0.zip

---
+++ packages/head_first/head_first-1.3.0.zip/head_first-1.3.0/head_first.py
@@ -0,0 +1,9 @@
+def print_lol(the_list, indent = False, level = 0):
+	for each_items in the_list:
+		if isinstance(each_items, list):
+			print_lol(each_items, indent, level + 1)
+		else:
+			if indent :
+				for tab_stop in range(level):
+					print ("\t", end = '')
+			print (each_items)
