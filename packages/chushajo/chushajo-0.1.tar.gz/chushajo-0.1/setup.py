bc4730358e4c034d9ee7b35b981bdd71b78eaaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:15 +0000

    Add package chushajo/0.1/chushajo-0.1.tar.gz

---
+++ packages/chushajo/chushajo-0.1.tar.gz/chushajo-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chushajo',
+      version='0.1',
+      description='Parking API',
+      url='https://github.com/kimi1989/chushajo.git',
+      author='Kimi Jin',
+      author_email='jinyinzhen@gmail.com',
+      license='MIT',
+      packages=['chushajo'],
+      zip_safe=False)
