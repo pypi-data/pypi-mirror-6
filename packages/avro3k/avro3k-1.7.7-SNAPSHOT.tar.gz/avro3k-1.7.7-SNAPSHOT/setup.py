7b1da9e6830ce8c18707a079fc8e7c49f3589521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:18 +0000

    Add package avro3k/1.7.7-SNAPSHOT/avro3k-1.7.7-SNAPSHOT.tar.gz

---
+++ packages/avro3k/avro3k-1.7.7-SNAPSHOT.tar.gz/avro3k-1.7.7-SNAPSHOT/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- mode: python -*-
+# -*- coding: utf-8 -*-
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from distutils.core import setup
+from sys import version_info
+import distutils.cmd
+import subprocess
+
+assert (version_info[0] >= 3), ('This requires Python 3')
+
+
+def xsystem(*args, **kw):
+    if subprocess.call(args, **kw) != 0:
+        raise RuntimeError('check_exit failed, while executing: ', ' '.join(args))
+
+class TestLaunch(distutils.cmd.Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        xsystem("./run_tests.py", cwd='test')
+
+
+setup(
+  name = 'avro3k',
+  version = '1.7.7-SNAPSHOT',
+  packages = ['avro',],
+  package_dir = {'avro': 'src/avro'},
+  scripts = ["./scripts/avro"],
+  cmdclass = {'test': TestLaunch},
+
+  # metadata for upload to PyPI
+  author = 'Apache Avro',
+  author_email = 'avro-dev@hadoop.apache.org',
+  description = 'Avro is a serialization and RPC framework.',
+  license = 'Apache License 2.0',
+  keywords = 'avro serialization rpc',
+  url = 'http://hadoop.apache.org/avro',
+)
