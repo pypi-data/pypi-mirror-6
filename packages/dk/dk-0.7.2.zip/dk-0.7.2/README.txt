f88205b547678469bce68c982cfacac69deda473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:30 +0000

    Add package dk/0.7.2/dk-0.7.2.zip

---
+++ packages/dk/dk-0.7.2.zip/dk-0.7.2/README.txt
@@ -0,0 +1,13 @@
+
+DK utility classes
+===============================
+
+Various basic packages pulled from our source tree.
+
+
+
+Running the tests
+-------------------
+
+I use py.test to run the tests, so there might be some dependencies that
+unittest.py by itself can't handle.
---
+++ packages/dk/dk-0.7.2.zip/dk-0.7.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The Datakortet Javascript package, dk.js.
+"""
+
+classifiers = """\
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+Programming Language :: Python
+Topic :: Software Development :: Libraries
+"""
+
+import setuptools
+from distutils.core import setup
+
+#version = eval(open('./package.json').read())['version']
+version = '0.7.2'
+
+setup(
+    name='dk',
+    version=version,
+    requires=[],
+    install_requires=[],
+    description=__doc__.strip(),
+    classifiers=[line for line in classifiers.split('\n') if line],
+    long_description=open('README.txt').read(),
+    license="LGPL",
+    author='Bjorn Pettersen',
+    author_email='bp@datakortet.no',
+    url='https://github.com/thebjorn/dk',
+    download_url='https://github.com/thebjorn/dk',
+
+    packages=['dk'],
+    zip_safe=False,
+)
