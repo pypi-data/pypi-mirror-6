71c1853486402908d2b71735c0d5fb6822644834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:13 +0000

    Add package newprint/1.0.0/newprint-1.0.0.zip

---
+++ packages/newprint/newprint-1.0.0.zip/newprint-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup (
+	name = 'newprint',
+	version = '1.0.0',
+	py_modules = ['newprint'],
+	author = 'li',
+	author_email = '279639439@qq.com',
+	description = 'Module demo',
+)
