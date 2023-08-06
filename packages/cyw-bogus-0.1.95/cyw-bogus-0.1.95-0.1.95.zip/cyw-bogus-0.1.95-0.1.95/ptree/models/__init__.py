41c79acc8109ff08ec9aa6fe806f60e259cfef8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:41 +0000

    Add package cyw-bogus-0.1.95/0.1.95/cyw-bogus-0.1.95-0.1.95.zip

---
+++ packages/cyw-bogus-0.1.95/cyw-bogus-0.1.95-0.1.95.zip/cyw-bogus-0.1.95-0.1.95/ptree/models/__init__.py
@@ -0,0 +1,8 @@
+from ptree.models.experiments import BaseExperiment
+from ptree.models.treatments import BaseTreatment
+from ptree.models.matches import BaseMatch
+from ptree.models.participants import BaseParticipant
+from ptree.models.common import AuxiliaryModel
+
+from django.db import models
+
---
+++ packages/cyw-bogus-0.1.95/cyw-bogus-0.1.95-0.1.95.zip/cyw-bogus-0.1.95-0.1.95/ptree/views/concrete.py
@@ -0,0 +1,17 @@
+__doc__ = """This module contains views that are shared across many game types. 
+They are ready to be included in your  Just import this module,
+and include these classes in your Treatment's sequence() method."""
+
+from django.shortcuts import render_to_response
+
+from django.conf import settings
+
+import ptree.views.abstract
+import ptree.forms
+
+class RedirectToPageUserShouldBeOn(ptree.views.abstract.BaseView):
+    url_base = 'shared'
+
+    def get(self, request, *args, **kwargs):
+        return self.redirect_to_page_the_user_should_be_on()
+
