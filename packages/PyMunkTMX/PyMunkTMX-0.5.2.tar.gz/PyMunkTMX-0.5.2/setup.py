e42a6e011d158b4bb572290d1b7cb74c7658daa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:21 +0000

    Add package PyMunkTMX/0.5.2/PyMunkTMX-0.5.2.tar.gz

---
+++ packages/PyMunkTMX/PyMunkTMX-0.5.2.tar.gz/PyMunkTMX-0.5.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+from setuptools import setup
+import os
+import pymunktmx
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(name="PyMunkTMX",
+      version=pymunktmx.__version__,
+      description=pymunktmx.__description__,
+      author=pymunktmx.__author__,
+      author_email=pymunktmx.__author_email__,
+      packages=['pymunktmx'],
+      install_requires=['pymunktmx',
+                        'pytmx',
+                        'pygame>=1.9',
+                        'pymunk>=4.0.0'],
+      license="LGPLv3",
+      url="https://github.com/wkmanire/PyMunkTMX",
+      long_description=read('README.md'),
+      package_data={
+          'pymunktmx': ['LICENSE', 'README.md']},
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Games/Entertainment",
+          "Topic :: Software Development :: Libraries :: pygame",
+      ],
+)
