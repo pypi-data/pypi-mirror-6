2c26deb7a36f6fdf1e3bd1dda6d2d0a99b1d6c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:26 +0000

    Add package comicgen/0.2/comicgen-0.2.tar.gz

---
+++ packages/comicgen/comicgen-0.2.tar.gz/comicgen-0.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+requires = ['jinja2', 'pyCLI']
+
+README = open('README.rst').read()
+
+setup(
+    name="comicgen",
+    py_modules = ('comicgen', ),
+    version="0.2",
+    url='https://github.com/wutali/comicgen',
+    download_url='https://github.com/wutali/comicgen/tarball/master',
+    author='Takahiro Fujiwara',
+    author_email='email@wuta.li',
+    description="A command-line tool to make ebook of comics.",
+    long_description=README,
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[],
+    entry_points=dict(
+        console_scripts=[
+            'comicgen = comicgen:comicgen_script',
+            'comicgen-volumes = comicgen:comicgen_volumes_script',
+        ],
+    ),
+)
