0c23097d049521a368cbb4ad312a360e7a94bebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:41 +0000

    Add package transperth/0.0.1/transperth-0.0.1.zip

---
+++ packages/transperth/transperth-0.0.1.zip/transperth-0.0.1/transperth/location.py
@@ -0,0 +1,148 @@
+import re
+from lxml import etree
+from collections import namedtuple
+
+import requests
+
+from . import BASE
+from .utils import format_date, clean
+
+STOPNUM_RE = re.compile(r'\d{5}')
+
+
+__all__ = [
+    'determine_location',
+    'Location',
+    'parse_locations',
+    'LocationT'
+]
+
+LocationT = namedtuple('LocationT', 'name,code')
+
+is_location = lambda arg: isinstance(arg, Location)
+are_locations = lambda *args: all(map(is_location, args))
+
+
+def determine_location(from_d, to_d):
+    """
+    Takes two location objects, and returns a dict of lists of LocationTs
+    mapping possible corresponding locations and their codes
+    """
+    assert are_locations(from_d, to_d)
+
+    URL = BASE + 'DesktopModules/JourneyPlanner/JP.aspx'
+
+    params = {
+        'jpDate': format_date(),
+        'jpDirection': 'B',
+        # 'jpAMPM': 'AM',
+        # 'jpHour': '11',
+        # 'jpMinute': '00',
+        'fSet': 'False',
+        'fGadget': 'False',
+        'mode': 't1,b1,f1,s1',
+        'jpnMaxJourneys': '5',
+        'jpMaxChanges': '-1',
+        'jpWalkChange': 'NORMAL',
+        'jpWheelchairOnly': '0'
+    }
+
+    params.update(from_d.as_('from'))
+    params.update(to_d.as_('to'))
+
+    return parse_locations(
+        requests.get(URL, params=params).text
+    )
+
+
+def parse_locations(locations):
+    """
+    Takes the (pure) XML from the locations request and returns in the format;
+
+    {
+        "from": [
+            LocationT('<NAME>', '<CODE>')
+        ]
+    }
+    """
+    root = etree.XML(locations)
+
+    return {
+        element.tag.lower(): [
+            LocationT(
+                *clean(se.itertext())
+            )
+            for se in element
+        ]
+        for element in root
+    }
+
+
+class Location(object):
+    """
+    Represents a location that has not been resolved into a (LocationT)
+    location code
+    """
+
+    def __init__(self, data):
+        """
+        It is recommend you use one of the specialised methods;
+        from_address, from_stop, or from_location
+        """
+        self._data = {
+            '': 'LOCATION',  # this is required
+            'street': '',
+            'suburb': '',
+            'location': '',
+            'stop': ''
+        }
+
+        self._data.update(data)
+
+    @classmethod
+    def from_address(self, street, suburb):
+        return Location({
+            'street': street,
+            'suburb': suburb,
+            '': 'POINT'
+        })
+
+    @classmethod
+    def from_stop(self, stop_number):
+        if stop_number and not STOPNUM_RE.match(stop_number):
+            raise Exception('Invalid stop number')
+
+        return Location({
+            'stop': stop_number,
+            '': 'NODE'
+        })
+
+    @classmethod
+    def from_location(self, location_string):
+        return Location({
+            'location': location_string,
+            '': 'LOCATION'
+        })
+
+    def as_(self, direction):
+        """
+        Returns the input data as a something like the following;
+
+        {
+            'to': str,
+            'toStreet': str,
+            'toSuburb': str,
+            'toLocation': str,
+            'toStop': str,
+        }
+        """
+
+        if direction not in {'to', 'from'}:
+            raise Exception('tf casn')
+
+        self._data[''] = self._data[''].title()
+
+        return {
+            direction + k.title(): v
+            for k, v in self._data.items()
+        }
---
+++ packages/transperth/transperth-0.0.1.zip/transperth-0.0.1/transperth/routes.py
@@ -0,0 +1,61 @@
+import requests
+
+from . import BASE
+from .location import determine_location, are_locations
+from .route_parser import parse_routes
+from .utils import format_date
+
+
+__all__ = ['determine_routes']
+
+
+def determine_routes(from_loco, to_loco):
+    """
+
+    """
+    assert are_locations(from_loco, to_loco)
+
+    locations = determine_location(from_loco, to_loco)
+
+    assert locations['to'], 'No corresponding to locations found'
+    assert locations['from'], 'No corresponding from locations found'
+
+    from_string = locations['from'][0]
+    to_string = locations['to'][0]
+
+    return _routes(from_string.code, to_string.code)
+
+
+def _routes(from_code, to_code):
+    params = {
+        'LinkMode': 'Walk',
+        'StartMode': 'Walk',
+        'IsAfter': 'B',
+        'Priority': '504',
+        'WheelchairOnly': '0',
+        'Date': format_date(),
+        'SchoolBusVehicleType': 'true',
+        'BusVehicleType': 'true',
+        'Time': '11%3a00AM',
+        'WalkChange': 'NORMAL',
+        'FromLoc': from_code,
+        'FerryVehicleType': 'true',
+        'MaxChanges': '-1',
+        'TrainVehicleType': 'true',
+        'find+journey.y': '',
+        'jpnMaxJourneys': '5',
+        'find+journey.x': '',
+        'ToLoc': to_code,
+        'EndMode': 'Walk'
+    }
+
+    r = requests.get(
+        BASE + 'JourneyPlanner/tabid/233/Default.aspx',
+        params=params
+    )
+
+    # print(r.url)
+
+    return parse_routes(
+        r.text
+    )
