5f9ac8782cad6d2a39f72157a870598b7d274ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:04 +0000

    Add package ruxxisimo/1.4.0./ruxxisimo-1.4.0..zip

---
+++ packages/ruxxisimo/ruxxisimo-1.4.0..zip/ruxxisimo-1.4.0setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='ruxxisimo',
+    version='1.4.0.',
+    py_modules=['ruxxis'],
+    author='russel.alfaro',
+    author_email='russel_alfaro911@hotmail.com',
+    description='A simple printed for lists',
+    )
