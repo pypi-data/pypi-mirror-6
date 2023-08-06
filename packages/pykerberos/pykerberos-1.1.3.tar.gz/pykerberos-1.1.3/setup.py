689fe67678035dbcbcbdbe0a27cb7392a46df62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:00 +0000

    Add package pykerberos/1.1.3/pykerberos-1.1.3.tar.gz

---
+++ packages/pykerberos/pykerberos-1.1.3.tar.gz/pykerberos-1.1.3/setup.py
@@ -0,0 +1,56 @@
+##
+# Copyright (c) 2006-2008 Apple Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+from setuptools import setup, Extension
+import subprocess
+import sys
+
+long_description = """
+This Python package is a high-level wrapper for Kerberos (GSSAPI) operations.
+The goal is to avoid having to build a module that wraps the entire Kerberos.framework,
+and instead offer a limited set of functions that do what is needed for client/server
+Kerberos authentication based on <http://www.ietf.org/rfc/rfc4559.txt>.
+
+"""
+
+setup (
+    name = "pykerberos",
+    version = "1.1.3",
+    description = "High-level interface to Kerberos",
+    long_description=long_description,
+    license="ASL 2.0",
+    classifiers = [
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration :: Authentication/Directory"
+        ],
+    ext_modules = [
+        Extension(
+            "kerberos",
+            extra_link_args = subprocess.check_output(["krb5-config", "--libs", "gssapi"], universal_newlines=True).split(),
+            extra_compile_args = subprocess.check_output(["krb5-config", "--cflags", "gssapi"], universal_newlines=True).split(),
+            sources = [
+                "src/kerberos.c",
+                "src/kerberosbasic.c",
+                "src/kerberosgss.c",
+                "src/kerberospw.c",
+                "src/base64.c"
+            ],
+        ),
+    ],
+)
