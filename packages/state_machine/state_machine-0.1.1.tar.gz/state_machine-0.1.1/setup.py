e402090f2645d29a160c289b47bf73cc6312fe8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:01 +0000

    Add package state_machine/0.1.1/state_machine-0.1.1.tar.gz

---
+++ packages/state_machine/state_machine-0.1.1.tar.gz/state_machine-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+required_modules = ['mongoengine']
+
+setup(name='state_machine',
+      version='0.1.1',
+      description='Python State Machines for Humans',
+      url='http://github.com/jtushman/state_machine',
+      author='Jonathan Tushman',
+      author_email='jonathan@zefr.com',
+      install_requires=required_modules,
+      license='MIT',
+      packages=['state_machine'],
+      zip_safe=False)
