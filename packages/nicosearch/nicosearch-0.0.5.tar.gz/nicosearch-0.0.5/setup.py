a7a8679b95f4ebecab0f854812a37c4bd7469906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:09 +0000

    Add package nicosearch/0.0.5/nicosearch-0.0.5.tar.gz

---
+++ packages/nicosearch/nicosearch-0.0.5.tar.gz/nicosearch-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+      name = "nicosearch",
+      py_modules=['nicosearch'],
+      version = "0.0.5",
+      license = 'MIT License',
+      download_url = "http://backloglib.googlecode.com/files/backloglib-0.1.1.tar.g://github.com/ymizushi/nicosearch/archive/master.zip",
+      platforms = ['POSIX'],
+      description = "https://github.com/ymizushi/nicosearch",
+      author = "ymizushi",
+      author_email = "mizushi@gmail.com",
+      url = "https://github.com/ymizushi/nicosearch",
+      keywords = ["search", "niconico"],
+      classifiers = [
+                     'License :: OSI Approved :: MIT License',
+                     "Programming Language :: Python",
+                     "Development Status :: 4 - Beta",
+                     "Environment :: Console",
+                     "Intended Audience :: Developers",
+                     "Topic :: Utilities",
+                     "Topic :: Software Development",
+                     ],
+      long_description = open('README.md').read()
+      )
