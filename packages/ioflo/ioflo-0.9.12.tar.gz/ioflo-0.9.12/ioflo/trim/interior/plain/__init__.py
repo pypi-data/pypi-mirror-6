5ce0e9c6bbb2da02e1193d97e49a95f540e11bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:40 +0000

    Add package ioflo/0.9.12/ioflo-0.9.12.tar.gz

---
+++ packages/ioflo/ioflo-0.9.12.tar.gz/ioflo-0.9.12/ioflo/trim/interior/plain/__init__.py
@@ -0,0 +1,12 @@
+""" plain package
+
+"""
+#print "\nPackage at%s" % __path__[0]
+
+__all__ = [ 'controlling', 'detecting', 'estimating',
+            'filtering',  'simulating' ]
+
+for m in __all__:
+    exec "from . import %s" % m  #relative import
+    #print "Imported %s" % globals().get(m,'')
+
