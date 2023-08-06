5a32620810445c0d52e6ba528f98bc021eaaebfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:40 +0000

    Add package wtf-peewee/0.2.3/wtf-peewee-0.2.3.tar.gz

---
+++ packages/wtf-peewee/wtf-peewee-0.2.3.tar.gz/wtf-peewee-0.2.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name='wtf-peewee',
+    version='0.2.3',
+    url='https://github.com/coleifer/wtf-peewee/',
+    license='MIT',
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    description='WTForms integration for peewee models',
+    packages=['wtfpeewee'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'peewee>=2.0.0', 'wtforms',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    test_suite='runtests.runtests'
+)
