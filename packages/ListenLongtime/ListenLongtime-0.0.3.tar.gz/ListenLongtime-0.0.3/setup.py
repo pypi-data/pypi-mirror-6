f78ee0d3d994ff18df1fb9938c6e473fdbc21c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:55 +0000

    Add package ListenLongtime/0.0.3/ListenLongtime-0.0.3.tar.gz

---
+++ packages/ListenLongtime/ListenLongtime-0.0.3.tar.gz/ListenLongtime-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ListenLongtime',
+    version='0.0.3',
+    packages=[''],
+    url='',
+    license='',
+    author='maxvitek',
+    author_email='',
+    description=''
+)
