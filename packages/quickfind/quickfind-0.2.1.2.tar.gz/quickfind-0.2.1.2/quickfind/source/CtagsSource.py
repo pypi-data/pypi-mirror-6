7e478751541eddb035fec787d60accff7d386d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:00 +0000

    Add package quickfind/0.2.1.2/quickfind-0.2.1.2.tar.gz

---
+++ packages/quickfind/quickfind-0.2.1.2.tar.gz/quickfind-0.2.1.2/quickfind/source/CtagsSource.py
@@ -0,0 +1,113 @@
+from __future__ import print_function
+import os, sys
+from collections import namedtuple
+from .Source import Source
+from itertools import islice
+
+from quickfind.Searcher import Ranker,CString
+from .Util import truncate_middle, truncate_front, highlight
+
+try:
+    import ctags
+except ImportError:
+    ctags = None
+
+ENTRY_FIELDS = ("name", "file", "disp", "pattern", "lineNumber", "kind", "fileScope")
+Entry = namedtuple("Entry", ENTRY_FIELDS)
+
+class CtagsSource(Source):
+    def __init__(self, filename):
+        if ctags is None:
+            print("`ctags` not found.  Ensure library 'python-ctags' is installed")
+            sys.exit(1)
+
+        self.tag_file = ctags.CTags(filename)
+        self.base_dir = os.path.split(filename)[0]
+
+    def _entry_to_Entry(self, entry):
+        ed = dict((f, entry[f]) for f in ENTRY_FIELDS)
+        ed['file'] = os.path.abspath(os.path.join(self.base_dir, ed['file']))
+        return Entry(**ed)
+
+    def _query(self):
+        entry = ctags.TagEntry()
+        if self.tag_file.first(entry):
+            yield self._entry_to_Entry(entry)
+            while self.tag_file.next(entry):
+                yield self._entry_to_Entry(entry)
+
+    def fetch(self):
+        return list(self._query())
+
+class CtagsFormatter(object):
+    
+    def __init__(self, columns, surrounding=True):
+        self.detail_cache = {}
+        self.surrounding = surrounding
+
+    def __call__(self, entry, query, dims):
+        line_prefix = '%s %s   ' % (entry.kind, entry.name)
+
+        columns = dims[0]
+
+        details = ""
+        if self.surrounding and columns >= 80:
+            dets = self.get_details(entry)
+            if dets is not None:
+                details = "   " + dets
+
+        trunc_len = columns - (len(line_prefix) + len(details))
+        if details:
+            filename = truncate_front(entry.file, trunc_len)
+        else:
+            filename = truncate_middle(entry.file, trunc_len)
+
+        lens = len(line_prefix)
+        res  = highlight(line_prefix, query)
+        res.append(CString(filename, "cyan"))
+        res.append(details)
+        return res
+
+    def read_line_at(self, f, num):
+        return next(islice(f, num - 1, num), None)
+
+    def get_details(self, entry):
+        # If a file, print nothing
+        if entry.kind == 'F':
+            return None
+
+        # If -R or not a real line
+        if entry.lineNumber == 0:
+            return entry.pattern
+
+        if entry not in self.detail_cache:
+            lineNum = entry.lineNumber
+            if not os.path.isfile(entry.file):
+                self.detail_cache[entry] = None
+            else:
+                with file(entry.file) as f:
+                    response = self.read_line_at(f, lineNum)
+                    if response is None:
+                        response = entry.pattern
+                    self.detail_cache[entry] = response.strip()
+        
+        return self.detail_cache[entry]
+
+    def indent(self, s, indent):
+        return s.rjust(len(s) + indent)
+
+class CtagsRanker(Ranker):
+
+    def __init__(self, query):
+        self.q = query.lower()
+
+    def rank(self, item):
+        sname = item.name.lower()
+        if self.q not in sname:
+            return None
+
+        score = len(item.name) - len(self.q)
+        score += item.file.count(os.sep) ** 0.5
+        score -= 1.0 if sname.startswith(self.q) else 0.0
+        return score
+
---
+++ packages/quickfind/quickfind-0.2.1.2.tar.gz/quickfind-0.2.1.2/quickfind/source/Util.py
@@ -0,0 +1,95 @@
+import os
+
+from quickfind.Searcher import CString, Ranker
+
+def truncate_front(line, length=70):
+    reduce_amt = len(line) - length
+    # If it already fits
+    if reduce_amt <= 0 or length <= 0:
+        return line 
+
+    reduce_amt += 3 # for the ellipsis
+    return '...'+line[reduce_amt:]
+
+def truncate_middle(line, length=70):
+    reduce_amt = len(line) - length
+
+    # If it already fits
+    if reduce_amt <= 0 or length <= 0:
+        return line 
+
+    reduce_amt += 3 # for the ellipsis
+
+    start = (len(line) / 2) - (reduce_amt / 2)
+    end = start + reduce_amt
+    return "%s...%s" % (line[:start], line[end:])
+
+def rec_dir_up(dir):
+    if os.path.isdir(dir):
+        while True:
+            yield dir 
+            newdir = os.path.split(dir)[0]
+            if newdir == dir: break
+            dir = newdir
+
+def highlight(v, query, color="green"):
+    i = v.lower().rfind(query.lower())
+    if query and i > -1:
+        left = v[:i]
+        lq = len(query)
+        highlight = v[i:i+lq]
+        right = v[i+lq:]
+        return [ left, CString(highlight, color), right ]
+
+    return [v]
+
+class StringRanker(Ranker):
+
+    weight_f = lambda x: 0
+
+    def __init__(self, query):
+        self.qs = query.lower()
+        if '\t' in self.qs:
+            self.qs = [q for q in self.qs.split('\t') if q]
+        else:
+            self.qs = [self.qs]
+
+    def get_parts(self, item):
+        raise NotImplementedError()
+
+    def rank_part(self, q, part):
+        if q not in part:
+            return None
+
+        # Don't do more interesting ranking with one character
+        lq = len(q)
+        lp = len(part)
+        if lq == 1:
+            return lp
+
+        score = (lp) ** 0.5
+        score -= 1.0 if part.startswith(q) else 0.0
+        score -= 1.0 if part.endswith(q) else 0.0
+        return score
+
+    def rank(self, item):
+        part = self.get_part(item).lower()
+
+        agg_score = 0.0
+        for q in self.qs:
+            score = self.rank_part(q, part)
+            if score is None:
+                return None
+            agg_score += score
+        
+        return agg_score + self.weight_f(item)
+    
+    @staticmethod
+    def new(weight_f=lambda *x: 0, **kwargs):
+        kwargs['weight_f'] = weight_f
+        return type('StringRanker', (StringRanker,), kwargs)
+
+def simpleFormatter(item, query, dims):
+    v = truncate_middle(item, dims[0])
+    return highlight(v, query)
+
