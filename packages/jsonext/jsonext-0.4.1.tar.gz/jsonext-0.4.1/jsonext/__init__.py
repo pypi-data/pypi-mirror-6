0366b8f2804e430e380fcecfd13ee5c152fdc424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:23 +0000

    Add package jsonext/0.4.1/jsonext-0.4.1.tar.gz

---
+++ packages/jsonext/jsonext-0.4.1.tar.gz/jsonext-0.4.1/jsonext/__init__.py
@@ -0,0 +1,22 @@
+"""JSON-rendering helpers.
+
+This module provides mixins for the stdlib :class:`json.JSONEncoder` class,
+adding serialization methods for other object types, such as
+:class:`~datetime.datetime` objects or iterables.
+
+All these are ready to use by using :data:`~jsonext.dumps`.
+"""
+
+import functools
+import json
+
+from .mixins import JSONDateTimeMixin, JSONIterableMixin, JSONToDictMixin, \
+    JSONStringifyMixin
+
+
+class JSONEncoder(JSONDateTimeMixin, JSONIterableMixin, JSONToDictMixin,
+                  JSONStringifyMixin, json.JSONEncoder):
+    pass
+
+
+dumps = functools.partial(json.dumps, cls=JSONEncoder)
