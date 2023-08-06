dda98a4d46ee2e2d7d6404d0d24fdef1199dda6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:21 +0000

    Add package rutherford/0.7/rutherford-0.7.tar.gz

---
+++ packages/rutherford/rutherford-0.7.tar.gz/rutherford-0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.7'
+
+setup(name='rutherford',
+      version=version,
+      description="Atom feed for Tinkerer",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='atom feed tinkerer',
+      author='Sean Gillies',
+      author_email='sean.gillies@gmail.com',
+      url='http://github.com/sgillies/rutherford',
+      license='BSD',
+      packages=['rutherford'],
+      include_package_data=True,
+      package_data={'rutherford': ['templates/*']},
+      zip_safe=False,
+      install_requires=[
+          'pytz', 'tzlocal'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
