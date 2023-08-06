dbf525e7c3c7ec4f1b2e4f005b600a7d15cd6bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package infi.cwrap/0.2.8/infi.cwrap-0.2.8.tar.gz

---
+++ packages/infi.cwrap/infi.cwrap-0.2.8.tar.gz/infi.cwrap-0.2.8/src/infi/cwrap/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.8"
+__git_commiter_name__ = "Guy Rozendorn"
+__git_commiter_email__ = "guy@rzn.co.il"
+__git_branch__ = '(detached from v0.2.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'bbfc37a183f44928b260afe1823a7debcc458dc2'
+__git_head_subject__ = 'Finished release v0.2.8.'
+__git_head_message__ = '* release/v0.2.8:\n  buildout.cfg: adding setuptools to requirements\n  buildout.cfg: remove distribute from requirements'
+__git_dirty_diff__ = ''
