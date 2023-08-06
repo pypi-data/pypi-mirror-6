230b4ffb3d4cfd34e645cf8edc2bbfd52f38810a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:26:15 +0000

    Add package cumulus/1.3.1/cumulus-1.3.1.tar.gz

---
+++ packages/cumulus/cumulus-1.3.1.tar.gz/cumulus-1.3.1/cumulus_ds/exceptions.py
@@ -0,0 +1,23 @@
+class ChecksumMismatchException(Exception):
+    """ A checksum check has failed """
+    pass
+
+
+class ConfigurationException(Exception):
+    """ Configuration exception """
+    pass
+
+
+class HookExecutionException(Exception):
+    """ Failed to execute a hook """
+    pass
+
+
+class InvalidTemplateException(Exception):
+    """ Invalid CloudFormation template """
+    pass
+
+
+class UnsupportedCompression(Exception):
+    """ An unsupported compression format for the bundle found """
+    pass
