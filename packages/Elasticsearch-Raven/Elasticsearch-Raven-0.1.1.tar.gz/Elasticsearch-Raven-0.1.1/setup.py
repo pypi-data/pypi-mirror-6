383afee49fb4f53e60b019bc05a3cdfc15c9a8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:10 +0000

    Add package Elasticsearch-Raven/0.1.1/Elasticsearch-Raven-0.1.1.tar.gz

---
+++ packages/Elasticsearch-Raven/Elasticsearch-Raven-0.1.1.tar.gz/Elasticsearch-Raven-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='Elasticsearch-Raven',
+    version='0.1.1',
+    author='Tomasz Wysocki',
+    author_email='tomasz@pozytywnie.pl',
+    packages=['elasticsearch_raven'],
+    scripts=['bin/elasticsearch-raven.py'],
+    license='MIT',
+    description='Proxy that allows to log from raven to elasticsearch.',
+    long_description=open('README.md').read(),
+    install_requires=['elasticsearch'],
+    test_suite='tests'
+)
