21aab1a4ffe2926a03b42a129c0b73b6e818e100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:58 +0000

    Add package n3d/0.3.4/n3d-0.3.4.tar.gz

---
+++ packages/n3d/n3d-0.3.4.tar.gz/n3d-0.3.4/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='n3d',
+    version='0.3.4',
+    description='Utility for step-by-step application deployment',
+    author='Anton Grudko',
+    author_email='grudko@gmail.com',
+    url="http://github.com/grudko/n3d",
+    py_modules=['n3d'],
+    install_requires=['pexpect == 2.4', 'termcolor'],
+    entry_points={
+        'console_scripts': [
+            'n3d = n3d:main',
+        ]
+    },
+    classifiers = ['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Build Tools',
+                   ]
+)
