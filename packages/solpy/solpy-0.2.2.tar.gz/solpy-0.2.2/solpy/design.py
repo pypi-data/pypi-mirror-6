b165a7ce1b43b844e9612ccf28b299b997d1bb1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:08 +0000

    Add package solpy/0.2.2/solpy-0.2.2.tar.gz

---
+++ packages/solpy/solpy-0.2.2.tar.gz/solpy-0.2.2/solpy/design.py
@@ -0,0 +1,155 @@
+import copy
+import geo
+import epw
+import pv
+import json
+
+
+def fill(inverter, zipcode, acDcRatio = 1.2, mount="Roof", stationClass = 1, Vmax = 600, bipolar= True):
+    """maximize array"""
+    tDerate = {"Roof":30,
+            "Ground":25,
+            "Pole":20}
+    name, usaf = geo.closestUSAF( geo.zipToCoordinates(zipcode), stationClass)
+    maxV = inverter.array.panel.Vmax(epw.minimum(usaf))
+    #NREL suggests that long term degradation is primarily current not voltage
+    derate20 = .97
+    minV = inverter.array.panel.Vmin(epw.twopercent(usaf),tDerate[mount]) * derate20
+    #print "MinV", minV
+    if inverter.vdcmax != 0:
+         Vmax = inverter.vdcmax
+    smax = int(Vmax/maxV)
+    #range to search
+    pTol = .30
+    inverterNominal = inverter.Paco
+    psize = inverter.array.panel.Pmax
+    solutions = []
+
+    Imax = max(inverter.idcmax,inverter.Pdco*1.0/inverter.mppt_low)
+    stringMax = int(round(Imax/inverter.array.panel.Impp))+1
+
+    #Diophantine equation
+    for s in range(smax+1):
+        if (s*minV) >= inverter.mppt_low:
+            for p in range(stringMax):
+                pRatio = p*s*psize*1.0/inverterNominal
+                if pRatio < (acDcRatio*(1+pTol)) and \
+                        pRatio > (acDcRatio*(1-pTol)):
+                            inverter.array.shape = [s]*p
+                            t = copy.deepcopy(inverter)
+                            t.minV = s*minV
+                            t.maxV = s*maxV
+                            solutions.append(t)
+    return solutions
+
+def knapsack(items, maxweight):
+    #http://codereview.stackexchange.com/questions/20569/dynamic-programming-solution-to-knapsack-problem
+    bestvalues = [[0] * (maxweight + 1)
+                  for i in xrange(len(items) + 1)]
+
+    for i, (value, weight, systemDict) in enumerate(items):
+        i += 1
+        for capacity in xrange(maxweight + 1):
+            if weight > capacity:
+                bestvalues[i][capacity] = bestvalues[i - 1][capacity]
+            else:
+                candidate1 = bestvalues[i - 1][capacity]
+                candidate2 = bestvalues[i - 1][capacity - weight] + value
+                bestvalues[i][capacity] = max(candidate1, candidate2)
+
+    reconstruction = []
+    i = len(items)
+    j = maxweight
+    while i > 0:
+        if bestvalues[i][j] != bestvalues[i - 1][j]:
+            reconstruction.append(items[i - 1])
+            j -= items[i - 1][1]
+        i -= 1
+
+    reconstruction.reverse()
+
+    return bestvalues[len(items)][maxweight], reconstruction
+
+def design(reqsStr):
+#def design(DCsize, panellist, inverterlist, details):
+    """parts selection algorithm"""
+    #create all valid inverter panel combinations for location
+    reqs = json.loads(reqsStr)
+    validC = []
+    clean = []
+    zc = reqs['zipcode']
+    #for inverterModel,inverterCost in inverterlist:
+    for inverterModel in reqs['inverter options']:
+        #for panelModel,panelPrice in panellist:
+        for panelModel in reqs['panel options']:
+            system = inverters.inverter(inverterModel,\
+                    modules.pvArray(modules.module(panelModel),[2]))
+            configs = fill(system,zc)
+            for config in configs:
+                #config.price = sum(config.array.shape) * panelPrice + inverterCost
+                validC.append(config)
+                print config, config.array, config.array.panel, config.array.output(1000), config.ratio()
+                #print config.dump()
+                reqs['array'] = [config.dump()]
+                plant = pv.jsonToSystem(reqs)
+                output = plant.model()
+                print "year 1 kwh", output.annualOutput
+                config.yearOne = output.annualOutput
+                #simpleLCOE = config.price/sum([output.annualOutput*(.97)**n for n in range(20)])
+                #print '$', round(simpleLCOE,3),"value",1/simpleLCOE
+                #config.LCOE=simpleLCOE
+                #config.value = 1/simpleLCOE
+                del output
+
+                #value, weight, systemDict
+                option = [(int(config.yearOne), int(config.array.output(1000)), config.dump())]
+                #hack to allow semetery
+                scale = int(reqs['desired size'] / config.array.output(1000))
+                clean += option * scale
+                del config
+
+    #Sort in size
+
+    #create list plants of all permetations
+    #knapsack problem where weight is system DC size and value is LCOE/inverse?
+    systemWeight, systemSet = knapsack(clean, reqs['desired size'])
+
+    #constrain by space
+    #I think space constraint is probably a tiling problem or maybe packing
+
+    #model varaints
+    #this probalby needs to be queue based and would probably benefit from
+    #@memoize
+
+    #constrain by production limits
+
+    #rank
+
+    #return list of top 5 solutions
+    reqs['array'] = [subA for v,w,subA in systemSet]
+    return reqs
+
+if __name__ == "__main__":
+    import inverters
+    import modules
+    #sketchup may be a good way to input this data
+    testreqs = """{"system_name":"HAPPY CUSTOMER",
+        "address":"15013 Denver W Pkwy, Golden, CO",
+        "zipcode":"80401",
+        "phase":1,
+        "voltage":240,
+        "service":200,
+        "tilt":25,
+        "azimuth":180,
+        "inverter options":["SMA America: SB4000TL-US-22 (240V) 240V", 
+            "SMA America: SB5000TL-US-22 (240V) 240V", 
+            "SMA America: SB6000US-11 240V"],
+        "panel options":["Axitec : AC-250P-156-60S *"],
+        "homerun":150,
+        "space":[[10,5]],
+        "desired size":14000}"""
+    shade = """{
+        "shade":{"0": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "1": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.6, 1.0, 0.2, 0.0, 0.1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "2": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.6, 1.0, 1.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "3": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "4": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.9, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "5": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.3, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "6": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.2, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "7": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "8": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 1.0, 1.0, 0.1, 0.0, 0.6, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "9": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.9, 1.0, 0.1, 0.0, 0.1, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "10": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 1.0, 1.0, 1.0, 1.0, 0.8, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "11": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 1.0, 1.0, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]}
+            }"""
+    proposed = design(testreqs)
+    print json.dumps(proposed, sort_keys=True, indent=4, separators=(',', ': '))
---
+++ packages/solpy/solpy-0.2.2.tar.gz/solpy-0.2.2/solpy/tools.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2012 Nathan Charles
+#
+# This program is free software. See terms in LICENSE file.
+
+import design
+def factors(n):
+    #http://stackoverflow.com/questions/6800193/
+    return set(reduce(list.__add__,
+        ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def format(inverter):
+    """'9769.5W : 13S x 3P : ratio 1.22 : 314.0 - 552.0 V'"""
+    DC = inverter.array.output(1000)
+    ratio = DC/inverter.Paco
+    return '%sW : %s : ratio %s : %s - %s V' % (DC, inverter.array, \
+            round(ratio,2), round(inverter.minV), round(inverter.maxV))
+
+def fill(inverter, zipcode, acDcRatio = 1.2, mount="Roof", stationClass = 1, Vmax = 600, bipolar= True):
+    return [format(i) for i in design.fill(**locals())]
+
+if __name__ == "__main__":
+    import inverters
+    import modules
+    zc='44701'
+    zc='27713'
+    #zc='44050'
+    #zc='23173'
+    #m = "Suntech Power : STP245-20-Wd"
+    #m = "Mage Solar : Powertec Plus 285-6 PL"
+    #m = "Mage Solar : Powertec Plus 245-6 PL *"
+    m = "Mage Solar : USA Powertec Plus 250-6 MNCS"
+    ms = modules.module(m)
+    system = inverters.inverter("Refusol: 20 kW 480V",modules.pvArray(ms,[11]*6))
+    print fill(system,zc)
+    #system = inverters.inverter("Refusol: 24 kW 480V",modules.pvArray(ms,11,6))
+    print fill(system,zc)
+    system = inverters.inverter("SMA America: SB7000US-11 277V",modules.pvArray(ms,[14]*2))
+    print fill(system,zc,mount="Roof")
+    system = inverters.inverter("SMA America: SB8000US-11 277V",modules.pvArray(ms,[14]*2))
+    print fill(system,zc)
+    #iname = "Shanghai Chint Power Systems: CPS SCE7KTL-O US (240V) 240V"
+    iname = "Refusol: 24 kW 480V"
+    #system = inverters.inverter(iname,modules.pvArray(m,1,1))
+    #system = inverters.inverter(iname,modules.pvArray(modules.module(m),1,1))
+    #fill(system,zc,1000)
+
