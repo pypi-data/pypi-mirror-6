3d914cd1b1bf5a8a132a0e087fb3eb0315291343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:46 +0000

    Add package mysqlqueryresult2dict/0.0.1/mysqlqueryresult2dict-0.0.1.tar.gz

---
+++ packages/mysqlqueryresult2dict/mysqlqueryresult2dict-0.0.1.tar.gz/mysqlqueryresult2dict-0.0.1/info.py
@@ -0,0 +1,14 @@
+# package information.
+INFO = dict(
+  name = "mysqlqueryresult2dict",
+  description = "Create dict from MySQL query result text.",
+  long_description = open("README.rst").read(),
+  author = "koyachi",
+  author_email = "rtk2106@gmail.com",
+  license = "MIT License",
+  url = "https://github.com/koyachi/mysqlqueryresult2dict",
+  classifiers = [
+    "Programming Language :: Python :: 2.7",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
