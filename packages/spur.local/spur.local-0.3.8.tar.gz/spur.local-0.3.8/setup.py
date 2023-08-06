139b74a2f021283c0b911d88636ad61e05551385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:41 +0000

    Add package spur.local/0.3.8/spur.local-0.3.8.tar.gz

---
+++ packages/spur.local/spur.local-0.3.8.tar.gz/spur.local-0.3.8/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='spur.local',
+    version='0.3.8',
+    description='Run commands and manipulate files locally',
+    long_description=read("README"),
+    author='Michael Williamson',
+    url='http://github.com/mwilliamson/spur.py',
+    keywords="shell subprocess process",
+    packages=['spur'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
---
+++ packages/spur.local/spur.local-0.3.8.tar.gz/spur.local-0.3.8/spur/results.py
@@ -0,0 +1,37 @@
+import locale
+
+
+def result(return_code, allow_error, output, stderr_output):
+    result = ExecutionResult(return_code, output, stderr_output)
+    if return_code == 0 or allow_error:
+        return result
+    else:
+        raise result.to_error()
+        
+
+class ExecutionResult(object):
+    def __init__(self, return_code, output, stderr_output):
+        self.return_code = return_code
+        self.output = output
+        self.stderr_output = stderr_output
+        
+    def to_error(self):
+        return RunProcessError(
+            self.return_code,
+            self.output, 
+            self.stderr_output
+        )
+        
+        
+class RunProcessError(RuntimeError):
+    def __init__(self, return_code, output, stderr_output):
+        message = "return code: {0}\noutput: {1}\nstderr output: {2}".format(
+            return_code, _decode(output), _decode(stderr_output))
+        super(type(self), self).__init__(message)
+        self.return_code = return_code
+        self.output = output
+        self.stderr_output = stderr_output
+
+
+def _decode(raw_bytes):
+    return raw_bytes.decode(locale.getdefaultlocale()[1])
