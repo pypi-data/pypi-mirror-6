c8048a10b0db965674984174022eb33446e8f4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:03 +0000

    Add package libchaos/1.1.1/libchaos-1.1.1.tar.gz

---
+++ packages/libchaos/libchaos-1.1.1.tar.gz/libchaos-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'libchaos',
+        version = '1.1.1',
+	py_modules = ['libchaos'],
+	author = 'libchaos',
+	author_email = 'libchaos01@163.com',
+	url = 'http://github.com/libchaos',
+	description = 'a level control of nested list, clean a little bug'
+     )
