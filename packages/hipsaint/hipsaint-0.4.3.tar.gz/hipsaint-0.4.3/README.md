018ee20959529c7bf03c1f1167c3508937555809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:15 +0000

    Add package hipsaint/0.4.3/hipsaint-0.4.3.tar.gz

---
+++ packages/hipsaint/hipsaint-0.4.3.tar.gz/hipsaint-0.4.3/README.md
@@ -0,0 +1,53 @@
+#HipSaint
+
+Push your nagios notifications to HipChat using a simple command line tool.
+
+[![Build Status](https://travis-ci.org/hannseman/hipsaint.png?branch=master)](https://travis-ci.org/hannseman/hipsaint)
+[![Downloads](https://pypip.in/d/hipsaint/badge.png)](https://crate.io/package/hipsaint)
+
+Implements [HipChat message API](https://www.hipchat.com/docs/api/method/rooms/message).
+
+Inspired by https://gist.github.com/2418848
+
+##Install
+
+Through pip:
+
+~~~ sh
+$ pip install hipsaint
+~~~
+
+Or clone and simply run:
+~~~ sh
+$ python setup.py install
+~~~
+
+##Usage
+
+Assuming you use Nagios 3 add the following sections to commands.cfg with `<TOKEN>` and `<ROOM_ID>` specified and macros delimited by `|`:
+
+    define command {
+        command_name    notify-host-by-hipchat
+        command_line    hipsaint --token=<TOKEN> --room=<ROOM_ID> --type=host --inputs="$HOSTNAME$|$LONGDATETIME$|$NOTIFICATIONTYPE$|$HOSTADDRESS$|$HOSTSTATE$|$HOSTOUTPUT$" -n
+    }
+    define command {
+        command_name    notify-service-by-hipchat
+        command_line    hipsaint --token=<TOKEN> --room=<ROOM_ID> --type=service --inputs="$SERVICEDESC$|$HOSTALIAS$|$LONGDATETIME$|$NOTIFICATIONTYPE$|$HOSTADDRESS$|$SERVICESTATE$|$SERVICEOUTPUT$" -n
+    }
+
+To send less verbose messages to hipchat set the ``--type`` flag to either ``short-host`` or ``short-service``.
+
+Additional commands is available through:
+~~~ sh
+$ hipsaint --help
+~~~
+
+Edit the Nagios contacts.cfg file by adding or editing an existing user and adding the notification commands created above:
+
+    define contact {
+            ....
+            .....
+            service_notification_commands   notify-service-by-hipchat
+            host_notification_commands      notify-host-by-hipchat
+            email   /dev/null
+    }
---
+++ packages/hipsaint/hipsaint-0.4.3.tar.gz/hipsaint-0.4.3/hipsaint/messages.py
@@ -0,0 +1,73 @@
+import logging
+import requests
+import socket
+from os import path
+from jinja2.loaders import FileSystemLoader
+from jinja2 import Environment
+from hipsaint.options import COLORS
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+
+
+class HipchatMessage(object):
+    url = "https://api.hipchat.com/v1/rooms/message"
+    default_color = 'red'
+
+    def __init__(self, type, inputs, token, user, room_id, notify, **kwargs):
+        self.type = type
+        self.inputs = inputs
+        self.token = token
+        self.user = user
+        self.room_id = room_id
+        self.notify = notify
+
+    def deliver_payload(self, **kwargs):
+        """ Makes HTTP GET request to HipChat containing the message from nagios
+            according to API Documentation https://www.hipchat.com/docs/api/method/rooms/message
+        """
+        message_body = self.render_message()
+        message = {'room_id': self.room_id,
+                   'from': self.user,
+                   'message': message_body,
+                   'color': self.message_color,
+                   'notify': int(self.notify),
+                   'auth_token': self.token}
+        message.update(kwargs)
+        raw_response = requests.get(self.url, params=message)
+        response_data = raw_response.json()
+        if 'error' in response_data:
+            error_message = response_data['error'].get('message')
+            error_type = response_data['error'].get('type')
+            error_code = response_data['error'].get('code')
+            log.error('%s - %s: %s', error_code, error_type, error_message)
+        elif not 'status' in response_data:
+            log.error('Unexpected response')
+        return raw_response
+
+    def render_message(self):
+        """ Unpacks Nagios inputs and renders the appropriate template depending
+            on the notification type.
+        """
+        template_type = self.type
+        inputs = [x.strip() for x in self.inputs.split('|')]
+
+        if template_type == 'host' or template_type == 'short-host':
+            hostname, timestamp, ntype, hostaddress, state, hostoutput = inputs
+        elif template_type == 'service' or template_type == 'short-service':
+            servicedesc, hostalias, timestamp, ntype, hostaddress, state, serviceoutput = inputs
+        else:
+            raise Exception('Invalid notification type')
+
+        if ntype != 'PROBLEM':
+            self.message_color = COLORS.get(ntype, self.default_color)
+        else:
+            self.message_color = COLORS.get(state, self.default_color)
+        nagios_host = socket.gethostname().split('.')[0]
+
+        template_path = path.realpath(path.join(path.dirname(__file__), 'templates'))
+        env = Environment(loader=FileSystemLoader(template_path))
+        template = env.get_template('{tmpl}.html'.format(tmpl=template_type))
+        context = locals()
+        context.pop('self')
+        return template.render(**context)
