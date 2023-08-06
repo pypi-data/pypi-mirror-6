099a50200bc75536937e4d6adc148e5d067006f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:45 +0000

    Add package pyinit/0.0.1/pyinit-0.0.1.tar.gz

---
+++ packages/pyinit/pyinit-0.0.1.tar.gz/pyinit-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup	
+
+setup(
+	name=u'pyinit',
+	version=u'0.0.1',
+	description=u'A cli tool for initializing a python module',
+	author= u'jarvys',
+	email=u'yuhant534@126.com',
+	py_modules=[],
+	scripts=['pyinit']
+)
+
