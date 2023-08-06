7a005a960ae999c026ad146eaba4a9dddc2feadb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:52 +0000

    Add package PyWENO/0.11.1/PyWENO-0.11.1.tar.gz

---
+++ packages/PyWENO/PyWENO-0.11.1.tar.gz/PyWENO-0.11.1/pyweno/__git_version__.py
@@ -0,0 +1,1 @@
+version = 'fc08daf4aab7a8bb27b02b6ec78d2af36dfe3fc4'
---
+++ packages/PyWENO/PyWENO-0.11.1.tar.gz/PyWENO-0.11.1/pyweno/__init__.py
@@ -0,0 +1,13 @@
+"""The PyWENO module."""
+
+import kernels
+import nonuniform
+import symbolic
+import symbols
+import version
+import weno
+
+# import points
+# import nonuniform_codegen
+
+import cweno
