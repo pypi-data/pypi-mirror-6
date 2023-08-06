f9bee9f4dcc93686e05c9f2cf39386c691e22749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:22 +0000

    Add package headintheclouds/0.3.1/headintheclouds-0.3.1.tar.gz

---
+++ packages/headintheclouds/headintheclouds-0.3.1.tar.gz/headintheclouds-0.3.1/headintheclouds/ensemble/exceptions.py
@@ -0,0 +1,8 @@
+class ConfigException(Exception):
+    def __init__(self, message, server_name=None, container_name=None):
+        self.server_name = server_name
+        self.container_name = container_name
+        super(ConfigException, self).__init__(message)
+
+class RuntimeException(Exception):
+    pass
