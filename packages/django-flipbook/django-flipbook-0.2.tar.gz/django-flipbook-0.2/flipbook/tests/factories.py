2a9d51da641df2b58be7f9571b7f9d194a6e058d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:34 +0000

    Add package django-flipbook/0.2/django-flipbook-0.2.tar.gz

---
+++ packages/django-flipbook/django-flipbook-0.2.tar.gz/django-flipbook-0.2/flipbook/tests/factories.py
@@ -0,0 +1,28 @@
+"""Factories for the ``flipbook`` app."""
+import factory
+
+from filer.models import Folder
+
+from ..models import Flipbook, FlipbookCategory
+
+
+class FlipbookCategoryFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = FlipbookCategory
+
+    title = factory.Sequence(lambda i: 'name {}'.format(i))
+    slug = factory.LazyAttribute(lambda a: a.title.replace(' ', '-'))
+
+
+class FolderFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Folder
+
+    name = 'Test Folder'
+
+
+class FlipbookFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Flipbook
+
+    title = factory.Sequence(lambda i: 'name {}'.format(i))
+    slug = factory.LazyAttribute(lambda a: a.title.replace(' ', '-'))
+    folder = factory.SubFactory(FolderFactory)
+    is_published = True
