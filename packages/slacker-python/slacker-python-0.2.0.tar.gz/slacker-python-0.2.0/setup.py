7ec6df43cb3df9157ae658a1d3fa1a8c9f123a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:15 +0000

    Add package slacker-python/0.2.0/slacker-python-0.2.0.tar.gz

---
+++ packages/slacker-python/slacker-python-0.2.0.tar.gz/slacker-python-0.2.0/setup.py
@@ -0,0 +1,26 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_desc=open('README.rst','r').read()
+
+setup(name="slacker-python",
+      version="0.2.0",
+      author="Sun Ning",
+      author_email="sunng@about.me",
+      description="python client of slacker RPC",
+      long_description=long_desc,
+      url="http://github.com/sunng87/slacker-python",
+      license='mit',
+      packages=['slacker'],
+      package_data={'': ['README.rst']},
+      install_requires=['pyclj==0.2.1'],
+      classifiers=['Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Software Development',
+            'Programming Language :: Python',
+            'Operating System :: OS Independent']
+    )
